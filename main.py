3ae33ab422d331f1761fdda8e6794fff98503b03
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:31 +0000

    chase oil more respect smile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
