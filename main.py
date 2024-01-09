334d3e757392719db7729d0b12bd979b2ed64f0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:59:42 +0000

    difficult hang contents clue ring

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def convert_to_hex(n):
+        return hex(n)
