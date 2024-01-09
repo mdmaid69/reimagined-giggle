33cb8cc4d60ca5544d49ba6c5aa390edb6abcabe
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:37 +0000

    further thin game gentle interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
