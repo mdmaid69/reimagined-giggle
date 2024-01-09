0531b1d89d2f813a1b189f487a189fb25f806565
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:02 +0000

    total suppose deserve scrap after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+print(sys.version)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
