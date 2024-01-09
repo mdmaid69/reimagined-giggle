7a06d2f3761e58668f37ac33b1371c2f024597cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:47 +0000

    while split together bank bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import random
+print(random.randint(0, 100))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
