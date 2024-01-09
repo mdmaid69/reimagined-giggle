10d7d19f02063ce274db8ae085bee09263a7d905
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:25 +0000

    star thick on major with

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
