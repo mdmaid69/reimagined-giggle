0327e67370681a728f17069625f56892f0bd8c3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:46 +0000

    under everything emergency address direct

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
