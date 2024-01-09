8cf7aebdf19a9480bfc36a0ccec8d6d122267352
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:07:40 +0000

    mistake board to cut arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
