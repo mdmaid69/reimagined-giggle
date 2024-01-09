2ca8977a31fad65bc3a9a91ebb7c4829f7dec4b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:57:12 +0000

    lookaround rob pen shape among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
