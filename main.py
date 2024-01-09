9064b716284c384b7ecc33b33acea7ca1b68d85e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:04 +0000

    inquire direct grade often justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
