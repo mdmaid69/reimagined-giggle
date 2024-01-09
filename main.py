41084e1ff19881cae57fb47602cc21b2185b20c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:06 +0000

    getaway fit base wave probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
