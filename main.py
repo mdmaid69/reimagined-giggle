9c109f99ea7a870fd8b5f87ba3349bbb7bfea418
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:40:33 +0000

    winter strength needle unhappy Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
