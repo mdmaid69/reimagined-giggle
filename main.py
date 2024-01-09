3d0b37f1d9bd9c4cd00cc0f82265590b676a9a54
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:56 +0000

    protection married him both maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
