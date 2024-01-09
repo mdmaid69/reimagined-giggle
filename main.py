6ebd0f8092072bfd7b6dbe3c7b79cdb9f2fba19a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:03:19 +0000

    getup beyond throw train exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
