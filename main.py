81eed56a9ea5059daca1a7e98046ff397bd71033
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:25 +0000

    shout face loose position formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
