6eb2619c1bf471b4b2d2c145f6a27bd3d22d9233
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:41 +0000

    calloff thanks married close one

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
