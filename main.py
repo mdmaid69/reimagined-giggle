f1cbd4319289d8486160acc3918787b89bdb24b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:02:10 +0000

    wrap centimetre mean content wrap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
