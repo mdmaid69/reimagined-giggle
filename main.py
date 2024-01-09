4a0d13f9802a6f2be385b2cf539ff1ba675c06c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:24 +0000

    fly task successful weather beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def reverse_list(lst):
+        return lst[::-1]
