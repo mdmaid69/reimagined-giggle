5491daebce2ca17e30141d9c698ee038b4fe9cfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:30 +0000

    wear what promise fence month

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
