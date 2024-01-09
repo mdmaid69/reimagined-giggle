51704c96211085a5c15c01dcbad4e160bb9cb6fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:04 +0000

    business tip ring deaf please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(10): print(i)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
