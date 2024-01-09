66437c663b5f12d03c23016137134ee204aa5a52
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:00 +0000

    this polite mail practise account

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_binary(n):
+        return bin(n)
