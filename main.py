6cc01044e421cf14c1408ded069574f73dad52c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:05:07 +0000

    suck relax score steer criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_list(array):
+        return list(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
