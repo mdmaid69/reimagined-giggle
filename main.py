7647e7c47e77354c547b74c9ba907ae9b522aec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:25 +0000

    object correct sister concerned none

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_speed(distance, time):
+        return distance / time
