ade36e312cec96f7c60392bf1fd06796715b3c92
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:28 +0000

    child force careful protection invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
