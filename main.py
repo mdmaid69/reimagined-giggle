7d1a5aefb1b9a049508a0fb0ade0dbbc35eca768
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:46 +0000

    Ww film positive winter trap

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
