087b812352da4c7006dcfd61186e8000415b4fff
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:15:54 +0000

    sore wherever settle month break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
