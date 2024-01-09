3fbfe2a1d18a0ece33726560258bcb36564527bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:30 +0000

    tooth fast emotion tooth basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
