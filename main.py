c7576f96788737ac35d204e8813ae766f94d9824
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:25 +0000

    serve tip mouth through record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
