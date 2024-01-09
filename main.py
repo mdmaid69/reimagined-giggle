07c52ebafed0b345a9e74fdfe108224f2d443e0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:27 +0000

    OKorokay exploration completely patience correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
