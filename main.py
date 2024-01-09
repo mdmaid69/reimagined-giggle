b6f3bd82cfccd6a01dc0ded5412f89b96f2be5b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:27 +0000

    formal reach pronounce sit standard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
