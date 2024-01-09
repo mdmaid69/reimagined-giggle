1539fb1bec945dbdedc1b2557505d1d50ff2a7bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:50 +0000

    force mix easy breakdown flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
