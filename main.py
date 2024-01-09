39a7fbaf90ac3f6052d026529f760023a99c13c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:04:56 +0000

    release advanced borrow oil comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
