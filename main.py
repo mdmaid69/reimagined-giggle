d28353e7a9586f83ec547a9945b6108051789b9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:59 +0000

    alone search Itself gradually impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
