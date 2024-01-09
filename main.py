2b7c2b7ec5dda937766d128a84933f49fab908dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:15 +0000

    scared sure squash prefer sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
