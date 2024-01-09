3e911cb4aa829f875a623a2e11c234f42acf2781
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:31:52 +0000

    pick relative teach whistle male

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_max(lst):
+        return max(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
