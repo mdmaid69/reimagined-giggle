c7abfaaa107eebf70de933ea207c8cc805c6e90e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:36:50 +0000

    since concerning pretty material treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
