3b94a1276f37b2147754a6291a7368fca9309da1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:25 +0000

    loss sing morning getup kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
