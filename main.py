686127e50fcecec09a808343a0cc7fb27a106c00
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:51 +0000

    sick phone around 179bite typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
