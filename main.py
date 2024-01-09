a003224d6ce4dffd8e9e66917122b5cc4b2c555d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:47 +0000

    connect joke near close disapprove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import time
-def get_current_time():
-        return time.time()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
