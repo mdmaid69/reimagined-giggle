88acf30300bcc006f53c79ef8d28d1f0b0731e7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:45 +0000

    damage complain star evening please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
