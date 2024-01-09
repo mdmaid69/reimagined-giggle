1ce57f35e47bc914b608c81a9fe1b012e81618a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:36 +0000

    bother ready history open giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
