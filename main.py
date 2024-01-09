d0662d3dbd22d2927e28cd37c41f1ec49ac3be86
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:06 +0000

    thinkover idea remain up might

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_stack():
-        return collections.deque()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def cube_number(x):
+        return x**3
