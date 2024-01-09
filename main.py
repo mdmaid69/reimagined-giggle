c17fcbac3a3ceb906f36fa158f1dc340799b0bc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:59:08 +0000

    snow swallow mind this else

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
