daec4659dc6fcf65c412fa88c1fda021219774a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:48 +0000

    doubt goon stamp free target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
