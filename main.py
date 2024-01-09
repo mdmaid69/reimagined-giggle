e5d770548b26a7e1f1c5428cbd8e76a8d7d41816
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:14 +0000

    correction hurt face stay telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
