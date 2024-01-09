4f61719d4be5c8c533f9ac50ef86833507a1a026
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:48 +0000

    range avoid reject disapprove greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
