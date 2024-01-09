ef37a52eac9cb305517976e5a150fb2f970be403
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:05:46 +0000

    natural copy prize picture flight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
