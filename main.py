b82345a3b6e14059c059a28fd92f26f03160c4a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:18 +0000

    always bringdown rare hate murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
