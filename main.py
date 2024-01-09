824b701b8099fb359c4d96d42abc08b368e0eca8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:06 +0000

    lookback(on) connected callup deliberately upwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
