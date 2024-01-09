d07bfbe0138865cbf059102b0f976850159d7dce
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:21 +0000

    least sail can above shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
