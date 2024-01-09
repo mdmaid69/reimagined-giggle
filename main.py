319f96db8493b504d8e3ace11960044c965b9f8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:32 +0000

    half protest obvious insist water

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
