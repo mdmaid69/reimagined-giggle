0e75bea8521fbee6b0ce2bba521f9279c07fbf5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:36 +0000

    hurt run lookaround clue rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
