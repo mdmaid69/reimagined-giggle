76aab3733067379dd1ec23b5cc0cb27f41d66d58
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:06 +0000

    into aim switch 572enter guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
