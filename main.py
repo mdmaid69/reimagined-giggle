269a898f6a2718dd30eeffd63f6e26b8f9dc5e7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:05 +0000

    yes offence break famous system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
