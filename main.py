c51319dff2cc83677b319f9dc49aa26df086b2ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:26 +0000

    defend open herself notice noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
