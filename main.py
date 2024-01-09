6f296bedacbe6dd5b4d03843ac0cddfef22cbb5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:14 +0000

    moment nearly him black quickly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
