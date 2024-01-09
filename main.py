30033f339caca55bc8b0821a54ba6fd61e232ae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:13 +0000

    raise sure later disturbing display

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+print([x**2 for x in range(10)])
+name = "Python"
+print("Hello,", name)
