7f7ed67eb8594f59b4ecc2e60bd203a186cfc6b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:22:49 +0000

    unite ask patient immediately content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
