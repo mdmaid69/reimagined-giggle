d95edc7177e22aa6894b3dfcc59d5a7a79351e45
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:05 +0000

    direction spin Ss attempt worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
