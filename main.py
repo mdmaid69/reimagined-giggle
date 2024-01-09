d25a477c25a0456bf05729ed148bb353f647b033
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:07 +0000

    die deaf regard brush deliver

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
