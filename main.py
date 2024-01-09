329a3f7431356324c8b78626195f5cbcc08d0556
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:05:13 +0000

    description wash typical tip thankyou

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
