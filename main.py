8c08320e2e9ac65267472ab1819c6c4aa54e9b05
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:30 +0000

    odd argue branch represent production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
