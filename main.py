c1dcfa69771ba2dd474b6c6b56038b48c9e9662c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:23 +0000

    beside keepout past shop practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
