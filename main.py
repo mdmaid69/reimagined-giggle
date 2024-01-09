aea42192340bc03c8f80aecd660dc8ae0f3c74ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:16:42 +0000

    concentration sing tape squash round

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
