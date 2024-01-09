987fc44d2a4ab85747154d79a8bad72514ac27cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:53 +0000

    especially complete confident too comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
