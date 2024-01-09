b6f772b9795abdc898efa28427497f952048fcf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:55 +0000

    round comefrom trick study wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
