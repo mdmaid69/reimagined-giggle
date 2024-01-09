d8132331197c4d5106dfb64224fedf721fafb94b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:00 +0000

    appearance settle backwards reaction regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
