8443e9e3707e99d36ca1dc25f630c9e6e9b58571
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:32 +0000

    hear dig slightly turnoff tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
