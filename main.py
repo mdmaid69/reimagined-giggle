a77d36f15c13b5b71445facee0667552e747ea68
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:42 +0000

    talkback before roll improvement post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
