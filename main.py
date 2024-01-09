ed6b6b1e63282df0757e981123e34e9bf9941682
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:56 +0000

    finish I stop confused ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
