9d666b4b4d7ed9385b8610b18189164524c97827
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:25 +0000

    regional laugh post evening difficulty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
