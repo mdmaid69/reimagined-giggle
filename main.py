ed113c1ce0d674bfbffa7936b7ef560341cf9c7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:38 +0000

    favourite statement anybody wide some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_min(numbers):
+        return min(numbers)
