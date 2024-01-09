c9c1bc92a2cb41aad2c89f892caac7abebe22bbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:37 +0000

    declaration single easy lunch out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
