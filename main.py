eb2f2f2bb05c4caa018068fc5b44560e6e29887b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:06 +0000

    judge music instrument rough compete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
