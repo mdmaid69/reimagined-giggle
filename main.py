7001139e0a516a17a0ded1b9fbb3e220f6af9d06
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:22 +0000

    back spring mean store earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
