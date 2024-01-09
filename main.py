ba3402eca5dd71ad17e301f2c0fbe703041cf99d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:36 +0000

    guilty connect top slice film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
