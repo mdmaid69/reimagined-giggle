e39ea8d7a02de3a2c8dff092c84da1bf9234d88a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:22 +0000

    plastic broken regional common street

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
