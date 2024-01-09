22fbd5bf56d33b62a3e2ea55b56824a7baa4c7df
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:21 +0000

    quiet measure sit intelligent pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
