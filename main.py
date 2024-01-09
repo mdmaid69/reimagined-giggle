132c1b40dc7b3251db2aef50b6ccf7806e5c3aae
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:22 +0000

    limit spare embarrassed as equal

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+print("Hello, world!")
+text = "Hello, world!"
+print("Characters:", len(text))
