e6875f370580c0cbdeefffe53055e46f27ddc0b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:36:22 +0000

    various walk produce release shame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
