9d5fa2db02dc611544707b9757a5eac084052c56
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:45:48 +0000

    close serious song example alone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_floor(x):
+        return math.floor(x)
