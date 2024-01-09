4d58c8b30f5d9fc455fc0fa6fb40d203af4f0e27
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:25 +0000

    roll read rubbish love vanish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import time
-print(time.time())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def count_words(sentence):
+        return len(sentence.split())
