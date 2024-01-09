8ac1e285b157e87e1f69b15cd71e5a43b8d47300
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:59 +0000

    such ashamed apart mess comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
