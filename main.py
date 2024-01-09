7da8ec2d0a2aeaa6db998b14925c63e991471e9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:58 +0000

    repair boring sleep least scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
