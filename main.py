5c4a33fb5b09d7f9ecf900e8a48c6bf9f59da902
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:38:35 +0000

    study patient damp steer bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
