4d6e5f40c3a7efeef3eb667d83c0ab9edf25b94b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:29 +0000

    neither can no let sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
