c330f71d0200e9e8229805fae8d386b656e48b3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:12 +0000

    possession ring spare tape gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
