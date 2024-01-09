144e550b994ff8e4d20b2736cde0d858b766160d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:00 +0000

    comeover vanish earn message relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
