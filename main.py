3b105f2005503671bd2eaca36867f2dfafea55f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:23 +0000

    actually open cause distance visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
