b97dc37b513e45322b5f900cc8a58c611c0304e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:43:13 +0000

    deliberate fast often sell react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
