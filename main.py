fa97cf59819d52b98f246fb8c3e305fe2698e856
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:05 +0000

    drink Yy love juice boat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
