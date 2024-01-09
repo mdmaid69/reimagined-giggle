3c6f7ca283ca22350373876da2a729440f92afc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:39:24 +0000

    sharp tradition disappointed clean quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
