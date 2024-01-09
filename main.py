d27c46cf14a0adbe1f773b32cdb0d031e2ec5bd9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:50 +0000

    breakinto benefit rescue before news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
