a211b995bf97126a1e92f5b856eca9ae21f163b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:14 +0000

    emotion turnaway why workout stamp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def is_even(n):
+        return n % 2 == 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
