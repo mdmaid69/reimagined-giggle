bb8c8c9362fda132587aed80c7a714da40b99787
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:47 +0000

    no back course practical complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
