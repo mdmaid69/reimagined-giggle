feaf5bde5d37d9ff44c7568f5d22b11b34f996aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:33 +0000

    loss unable suffer attempt well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import re
+print(re.match("h.*o", "hello world"))
