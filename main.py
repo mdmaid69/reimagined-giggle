91a6b47e14aa9001befdeecbbfa0f4d52bb5c5f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:23 +0000

    past calm present measure border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
