4c14f4b35e5e1a5d5563416ac13afdbe241a0cc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:14 +0000

    broken point appearance people material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_list_from_array(array):
+        return array.tolist()
