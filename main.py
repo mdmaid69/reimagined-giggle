ec200037e427713910d263aaebf7f3ba7a4108e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:32 +0000

    failure old unlikely use example

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
