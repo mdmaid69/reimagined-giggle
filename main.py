0d056bb1000530c7c3029fb3c2b0f6c9d1ca1590
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:27 +0000

    chat operation border remember arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
