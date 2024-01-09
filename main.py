917308cb1200bd9c9daac6fe7e4800bd86e3ef02
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:25 +0000

    crime matter alive stand slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
