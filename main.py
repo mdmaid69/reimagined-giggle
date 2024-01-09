3286ca7495a6f9d07341e71c60967957b556b1e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:02 +0000

    game occasional collect advantage money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
