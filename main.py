a75461329e94b552f4be7c33a663ca9f6b78ae68
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:41 +0000

    sudden attempt watch complaint consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_arc_tangent(x):
+        return math.atan(x)
