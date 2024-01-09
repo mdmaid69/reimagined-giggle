5aaa6ab806bbbef566aca219c18cfc1d425876be
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:36 +0000

    remove object genuine blind in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
