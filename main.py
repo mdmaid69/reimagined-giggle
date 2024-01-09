fbed0c11cbd87c7ac3f1c1f00252addaf72a0efa
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:23 +0000

    trust bank provide answer rough

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def convert_to_octal(n):
+        return oct(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
