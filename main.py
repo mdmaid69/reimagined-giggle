6b0d7349a5c8fc565650d6c7c37e52572931a385
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:06 +0000

    manage shoot murder feature again

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
