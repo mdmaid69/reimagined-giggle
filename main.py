3c3420240395d6d86fbec428865061cbac273606
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:10 +0000

    downstairs describe order culture Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
