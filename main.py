2be3417e580ed4ece70e7ebd8f678668362df6a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:55 +0000

    avoid escape hello slight disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
