47892232d8fc45821401a1eb7deae1f26044fdbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:02 +0000

    beat cough mean here swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
