47d1f4fe5799795c73fbb6e35bb3ce6df0c5fe03
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:25 +0000

    challenge left fun rush avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
