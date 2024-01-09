d21264eedf9887a60386214d5f878bdd2b5e4171
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:53 +0000

    fair mark guess flow open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
