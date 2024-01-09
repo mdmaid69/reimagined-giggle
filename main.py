f702eeb3cf76d5682c03f4af4780b2afc8cdde40
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:18 +0000

    proud Nn freedom within anybody

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
