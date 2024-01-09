1149def001273c32a82af43600c9527d0b65724c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:58 +0000

    common turninto where hear across

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
