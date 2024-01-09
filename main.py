5b52db620fe7744a39e624f0f59156f569cae83f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:56 +0000

    temperature myself exact steam different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
