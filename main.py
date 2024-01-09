1bdbf533c9be6b7201c2df11c9031759b36f392b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:12:08 +0000

    favourite sometimes region class disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
