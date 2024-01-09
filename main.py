acab0cb4824baaa6451c16e3e5b73cf0c19efcef
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:59 +0000

    once confusing police hide hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
