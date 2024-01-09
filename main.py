37a5a7a6fbb4e482dc7b87483be594178993851e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:40 +0000

    dry takeaway course order get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import sys
+  def get_python_version():
+        return sys.version
