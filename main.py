e8eaced0bc5e27c6e025db99a2f8a04e21f53acf
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:54 +0000

    waste swallow food point card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
