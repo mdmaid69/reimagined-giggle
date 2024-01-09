ee8ab59abc65af3dd003d4a19b3d3693410e00fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:03:33 +0000

    go together beside understand lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_cosine(x):
+        return math.cos(x)
