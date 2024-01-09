d72e903d18423f7d8dcce2689bbef1539a1b05e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:06 +0000

    lot physical moment quarter plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
