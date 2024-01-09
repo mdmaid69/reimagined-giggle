a6d22393c946d6112f276ff926a522fb00fd4db3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:38 +0000

    record dress work continue police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
