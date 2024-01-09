6b18218a6c9589f44c22334dc6092fffada6d6e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:37 +0000

    mark pity answer ride tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
