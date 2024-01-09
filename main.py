9ce9a7b76e5f1c962a4e018dc835360649fe780e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:03 +0000

    spray comeoff scrap early difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
