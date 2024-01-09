5dc7e642774f0751a8a5d75ca9e0ca3edfa7e251
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:42 +0000

    save raise any solve put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
