24eefd7693248a0775548426475ea05a4cfe3192
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:00 +0000

    whom shine skin healthy holdout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
