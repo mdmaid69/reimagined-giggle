3777dd30c9950d797400342d4fa8f5b4ebe28dc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:21 +0000

    climb loose remember dinner hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
