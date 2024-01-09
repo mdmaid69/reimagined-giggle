8f63ec2e7755b856fe3df4940c18365474257fe7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:19:18 +0000

    front light alive substance throughout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
