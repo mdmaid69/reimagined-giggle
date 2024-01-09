add088843f5b03f67886e8aeb5fe89ce66ff054d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:45 +0000

    drive prove beat print care

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
