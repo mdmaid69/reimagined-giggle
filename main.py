12fac9a67bbc48d4d69b25ce57991a133abcd91c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:18:40 +0000

    Ll goback(to) crowded public curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
