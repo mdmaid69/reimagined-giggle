97a3b3cb6058ffa59d985bab62b45a0612ffe364
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:37 +0000

    sink I trial come criticism

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
