fc2cb4339b9eaf292a04ab5d2dd7b33a890de238
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:57 +0000

    smell trade world behaviour wrong

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
