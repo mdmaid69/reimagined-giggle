ed0196649b6ff3212302b1686d0b9664f79862f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:28 +0000

    serious keepoff letter bill variety

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_max(numbers):
+        return max(numbers)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
