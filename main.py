8a9d61693b9ab3a95b8379d85a6fddcd12725fca
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:06 +0000

    bored brown Ee continue board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
