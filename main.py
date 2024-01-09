21173143a67c4d3c72d73df4b8accd23c8189bd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:51:42 +0000

    muscle pot past protection supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
