e4f491ad39ade31bd7e638cfd5004871b1c3722a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:55:55 +0000

    uniform share intend sky hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
