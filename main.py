309528de751af6834bf676ba5df3654174dca191
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:42 +0000

    wing theirs feed phone wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def count_characters(sentence):
+        return len(sentence)
