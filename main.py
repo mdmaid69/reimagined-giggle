9e0c38f5284f9e032942a95a14b3aa3355742bb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:56 +0000

    bin point concentration measure people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
