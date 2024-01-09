a92af6de46481f4cc7eac79f66905d296bb6e7a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:57 +0000

    bottom comfort somehow unkind interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shutil
 def copy_file(src, dst):
         shutil.copy(src, dst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
