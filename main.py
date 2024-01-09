e2e2d9bb71d55fb438cab387805cbae43e7784de
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:32 +0000

    recently argument means imagine middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_queue():
+        return collections.deque()
