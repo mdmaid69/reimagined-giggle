1150153828a4a8ec8a4d8010fcd3f2e091aaafc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:14:01 +0000

    crowd dish keepup science form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
