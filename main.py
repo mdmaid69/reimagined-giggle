040e7796d9a33cc1e390a70d92e5555efa6570a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:56 +0000

    spot emergency translate advanced call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
