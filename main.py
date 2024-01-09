492a95b2cb90290ac536c783b94794eb5d88f32a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:42 +0000

    include false concern nor mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def reverse_list(lst):
-        return lst[::-1]
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
