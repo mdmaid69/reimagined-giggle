c43de1a7049d0dd10406af6464d3441328afc1bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:08 +0000

    male discuss Tt do intend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
