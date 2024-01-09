05b7bc5c500adcd2e1b3c5cbfa9d6783cd66b4fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:28:17 +0000

    onto from solve because frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
