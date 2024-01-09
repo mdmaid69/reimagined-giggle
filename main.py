48867e698afa2b7000374f9a5bd20efc41cc14bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:50 +0000

    rest discussion congratulate everyone freedom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
