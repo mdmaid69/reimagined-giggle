2aa98bd95d13698d4ac822ef5ca206488938597b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:29 +0000

    only smell pity joke way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
