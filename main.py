2f89cd62cb461392fcaea8f96fa2b20dd0b6ecd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:43 +0000

    holdout mysterious indeed blow scheme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
