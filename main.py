27c934084a5691ec3f48e701f8129b14e5fc217c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:31 +0000

    advise marry tie will discovery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print("Words:", len(text.split()))
