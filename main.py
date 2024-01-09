d37a150619f909b79bdd983c57b8a7997aba81fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:45:35 +0000

    argue display oil claim late

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
