f8913b5fc8faf65d942208168f9d54b65763a37a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:58 +0000

    politics stamp develop push place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def square_number(x):
+        return x**2
