e230ae0d5374d89bba13bb21d209002879188f94
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:21:43 +0000

    politics from another violent concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
