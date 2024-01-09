4c015d545e128834bee802bdd79bb58adffc277a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:09 +0000

    pity getout sound lay 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+name = "Python"
+print("Hello,", name)
