d9e4dbdc713d7c249e22237f32965a0bc51cfbac
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:02 +0000

    neck match church experiment slightly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_item(array, i):
+        return array[i]
