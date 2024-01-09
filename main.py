ad3ffb8150cb2aaa5947e21f683da814f264b288
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:54 +0000

    pay left invitation 573entertainment gift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
