b80805ebdf8934f397367671cd77e8244269f2ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:59:27 +0000

    fall talk situation you pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_exponential(x):
+        return math.exp(x)
