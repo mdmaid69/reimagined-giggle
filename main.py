100beb489740a99ab90df14efe1ac5e7c7d063f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:39:05 +0000

    he please together moon matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
