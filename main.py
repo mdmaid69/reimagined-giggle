6ab10e776bf66454e58e2539bcbb9a172fcef6f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:32 +0000

    stare dear active mass though

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+print("Hello, world!")
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
