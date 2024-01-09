3190878816475c08b1044728fe6ab0935030fa84
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:27 +0000

    kill comeoff Vv teach unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_repr(array):
+        return repr(array)
