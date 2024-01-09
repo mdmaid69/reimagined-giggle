8d37a492a913a3d6befe0bc61c2a406822bf18fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:53 +0000

    effect giveaway flat beside control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
