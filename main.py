047d14c7c60b3d883806f5d7185f143f56d3717f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:44 +0000

    pinch bother he firm habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
