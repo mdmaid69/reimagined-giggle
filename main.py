6c0326e10f18b45e7f8c3bab4371c252044f598f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:23 +0000

    closely although space special century

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
