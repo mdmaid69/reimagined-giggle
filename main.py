446d2801c8d25f2fd66de08679a3cc44450a4aa4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:26 +0000

    catch soak sad visit pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
