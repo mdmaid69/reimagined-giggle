0f498834a42478b7492c3a02d1983c7b4e6063fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:32 +0000

    perhaps deliberate callout gas underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
