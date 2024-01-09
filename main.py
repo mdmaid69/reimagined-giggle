a4cbb55ed858f3abba1da16d4ad7adbb682385b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:51 +0000

    niggaacademy opponent free into Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
