c000096535777f3eab40cde7d1908a023184756f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:36:30 +0000

    nothing pretty pupil feature bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
