dd71203873f34328ca32bd45939ce7a71a2714b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:02 +0000

    bringforward service tradition underneath why

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+for i in range(5):
+        print(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
