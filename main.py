ee36df11e9aebcb964cb9514aeb37460353b9274
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:18 +0000

    limit out pay book regard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
