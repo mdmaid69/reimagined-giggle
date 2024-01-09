0535448cff1451ab69810fa136c64fd9f871369d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:52 +0000

    sensitive bet although wherever extra

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
