bf08bfc559067ec92610ad36e54f199d3eb5b417
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:25 +0000

    consider bank objection welcome service

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
