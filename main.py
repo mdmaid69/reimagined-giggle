c36bb346081f77dd68c65e5c15782f3e0607a69d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:45 +0000

    sensitive receive neither clean reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
