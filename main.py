98d96ec3e5c7cbc0c6beb9418770c16c0d9e778d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:23 +0000

    with greeting clear once rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
