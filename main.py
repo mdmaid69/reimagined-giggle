e707704e055c324ebfb3bdda902f543c9c1c91ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:42 +0000

    by inform be cry own

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
