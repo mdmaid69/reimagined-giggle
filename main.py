72e7e672f528c516ff5934ebdb8a7add09ebece8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:14 +0000

    place keepoff Hh outside brush

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_length(array):
-        return len(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
