5fa01245f14179fce19a3556ee1dd28ce9b18e88
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:11 +0000

    deliver goodbye sick keepfrom necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
