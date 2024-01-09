6ab8976a7ee0aca6a77295210bde098d58def424
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:52 +0000

    rich block cheat catch model

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
