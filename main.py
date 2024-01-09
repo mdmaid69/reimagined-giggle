28219de7e5f5a318d985305150309321b088503a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:55 +0000

    nothing offend this fail exam

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
-def get_current_time():
-        return time.ctime()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
