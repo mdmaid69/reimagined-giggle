4b01b2a13104b0852305ede23a9bca5f6356bf78
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:49 +0000

    offend cheek beside either settle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_int(array):
+        return int(array[0])
