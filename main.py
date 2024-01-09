9027e93dc265056e436fa9a634959b867e1b4204
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:14 +0000

    match drop squeeze bother understand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
