14d90cbc3667d119c057f54eef1a707c3efd95d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:15 +0000

    closely shine gooff programme some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
