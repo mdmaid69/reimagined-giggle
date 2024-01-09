269db105655963cd32d3a9ebeb1002392d703642
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:09 +0000

    lesson sentence criticism touch beginning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
