e37da41c2b7edf847b0179324072c995de02a91e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:48 +0000

    thumb thumb profit guess second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
