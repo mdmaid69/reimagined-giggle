ed2685ea2c8afa6ba4ba88014406bc25fe1e6f54
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:51:23 +0000

    until nothing float comment her

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
