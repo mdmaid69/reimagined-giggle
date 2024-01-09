4c28a5346af26f7fffdba254f3d8fe957bc26b36
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:28:41 +0000

    admire schedule bringup call matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
