53d907e622bdf0a5fca3469e95523fd9a34fe7c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:50 +0000

    air clean ear store process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
