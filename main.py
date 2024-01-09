a29a09b0cfe7694e0946b99a8fe5dbbcb4ee4dea
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:25 +0000

    flood their feel stare very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
