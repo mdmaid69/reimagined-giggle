4b1945a1b640fa43d3adb3f18d79ea6329461883
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:51 +0000

    return blame share care nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
