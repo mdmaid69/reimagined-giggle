bf6f8788fab594e96472ffb70bbe5abc60492858
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:46 +0000

    street tradition day tradition obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
