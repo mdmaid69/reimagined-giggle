5260058e9ed6e4a7d6e88fa7873e7102588e3428
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:25 +0000

    reaction send region measure attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+print([x**2 for x in range(10)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
