a1f75b8361972f61ab570f3ce313455c65d4f205
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:49 +0000

    lean quality bag consider return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
