321cbb36a60ab4d58d91e95c351de7a83fbec262
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:01 +0000

    nervous 564encourage sweep public active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
