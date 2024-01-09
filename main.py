d2ce9da751ec5305c7341f6f90bb85c1f69c6dfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:44:59 +0000

    geton plan detail radio convinced

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
