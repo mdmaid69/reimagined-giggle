e19eb792caa903398c242474ae78e660ceaad36a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:55:19 +0000

    change manage thick without think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def square_number(x):
-        return x**2
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
