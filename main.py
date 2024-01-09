16e26a1ecb1c9e109cc25b502084aee76cd3dfe7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:51 +0000

    tend instant still carry deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
