6df4293d106996560fa1f6a16e3673c4b44c077a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:56 +0000

    fast increase address reserve cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
