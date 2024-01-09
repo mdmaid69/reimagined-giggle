17a11f8d5b13d2d6b4cc10989c7a29893b1a5dd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:39 +0000

    time image middle store purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
