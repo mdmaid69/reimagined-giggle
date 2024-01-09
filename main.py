4b6fe5e1a8ac2c0f1e7cffc2353a49b98600b61d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:32 +0000

    fact necessary benefit warm word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
