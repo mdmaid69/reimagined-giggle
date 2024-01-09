9d70abcf9be25d3167b5c01bd523db0bd5ffa7f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:43 +0000

    comment expensive tooth respect bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
