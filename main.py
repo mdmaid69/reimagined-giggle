94a8d5636bff5fe1a5c204c6597dedd18c674f02
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:18 +0000

    store tool force post comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def append_to_array(array, item):
+        array.append(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
