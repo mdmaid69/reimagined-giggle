7b99c6bc35f7e5fb8dcf27e6e13549bd9778778a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:27 +0000

    effort upwards uncomfortable reasonable swimming

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
