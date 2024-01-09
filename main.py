0c051d7bd0b1ddfadc6a6c1296fc68656eea8b99
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:49 +0000

    small settle workupto teach respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import sys
 def exit_program():
         sys.exit()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
