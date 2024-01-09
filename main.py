872ff5e341326ea9c732244c00b64835303bcdff
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:38 +0000

    clean wake public me meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
