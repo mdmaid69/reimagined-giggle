5e6c0405a5015875907b873dec672a54dcc0be63
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:54 +0000

    original print drop switch not

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-print(sum(range(10)))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def square_number(x):
+        return x**2
