18a4c80a3d2bfd556514e629fb16488611e24bb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:08 +0000

    last zero morning cheek use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
