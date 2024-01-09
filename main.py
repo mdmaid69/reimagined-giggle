6302dfcea680cab9103e19aebe3a0c186fa6641b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:51:18 +0000

    mind if unfortunate muscle talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
