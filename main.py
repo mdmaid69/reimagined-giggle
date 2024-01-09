e394f67073baba6510edb83fb9bffe4efdb38f2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:40 +0000

    step effort wash very every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
