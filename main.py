93f7bf82b729eb7c0cc103de314f9d126902e58f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:52:18 +0000

    anything justice illness beginning unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
