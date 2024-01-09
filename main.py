747242b047ea20c6b3374e413e12f0fec3184af9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:33:42 +0000

    common sport judge pause lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def count_characters(sentence):
-        return len(sentence)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
