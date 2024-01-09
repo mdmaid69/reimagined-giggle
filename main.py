bf8f3f53b0ddb3a3ce46cb129bd1eccad8058f44
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:04 +0000

    interested street disagree play hit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
