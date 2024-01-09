ac0b8434da257606c1be1aa4bfa70d8e92c8b6d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:03 +0000

    shock myself breakthrough board under

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
