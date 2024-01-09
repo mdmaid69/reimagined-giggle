660e13e4cb443d4f8a47f0f23eab157508c992af
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:03 +0000

    forgive Ii accident uncomfortable collect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
