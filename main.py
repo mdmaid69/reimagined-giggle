da68cffe1524779ab184020e6f56936535817c9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:26 +0000

    boring early lean smell dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
