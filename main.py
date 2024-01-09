d32760fe60a510589f00734a7e2e17d00cd24bb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:06:42 +0000

    shall hide invite sleep nature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def split_path(path):
+        return os.path.split(path)
