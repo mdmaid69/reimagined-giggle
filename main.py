4317864af1aca128322f1bd089a6869ab744521e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:33 +0000

    strength married rush describe person

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-print(random.randint(0, 100))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
