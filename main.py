1e103612558e0ea132b2b8c4f5d1eec6d690bac6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:50 +0000

    regularly thorough pocket keepfrom big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
