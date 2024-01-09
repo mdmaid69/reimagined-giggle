c569bd9e2dd9d08889a5465f025f248634539847
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:54 +0000

    tooth badly familiar hardly plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
