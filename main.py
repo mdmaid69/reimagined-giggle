522a8fc5bc3dba43edba86a283e2385862741866
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:31 +0000

    match schedule tight form wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def reverse_list(lst):
+        return lst[::-1]
