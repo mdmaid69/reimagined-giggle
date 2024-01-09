8baefed0e410011c421636ea2bb42a9bf45c00da
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:38 +0000

    behaviour statement letter repeat finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_min(lst):
+        return min(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
