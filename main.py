b83af7599bb870d643ce11fb884479a7bcee8a04
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:28 +0000

    transport regret performance practical bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
