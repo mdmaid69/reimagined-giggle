5f8fa6517ff79fe7ddaf0badb289269fc585d16c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:37 +0000

    degree piece past after total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
