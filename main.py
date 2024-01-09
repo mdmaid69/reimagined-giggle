961ebd78bb6ab4c05ff52ec12234aa90ddc7283e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:44 +0000

    breakout understand round name copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def print_python_version():
+        return sys.version
