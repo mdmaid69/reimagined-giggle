4a18bdc36751ec52447695bd16d7ad502874ea01
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:28 +0000

    exchange skill wind unless change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
