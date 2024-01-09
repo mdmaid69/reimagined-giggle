d1a3a9ed4e856a52911c67ab4bac81171b087a61
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:30:16 +0000

    go spot suddenly nor play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
