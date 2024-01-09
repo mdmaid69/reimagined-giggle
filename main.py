e48fc6ec0b702ebdbc68d1320225a32b6b08c994
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:10 +0000

    tense birthday load shut minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
