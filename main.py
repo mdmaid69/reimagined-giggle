200b9803a29743d151a053bc78a9e3456553ab8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:21 +0000

    Ii exam mess goback(to) usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+x = 10
+y = 20
+print("Sum:", x + y)
