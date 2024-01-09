7030ab09d15600c090ab46d2317aa723edd68494
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:38 +0000

    temper attract discover actually human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
