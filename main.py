594d6d85b4a82a4f3ee7f65b2069987b566796fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:29 +0000

    agree runaway close full easy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
