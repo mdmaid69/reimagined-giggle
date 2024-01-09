7f870db9729f061261c29614cc48271be30fb080
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:38 +0000

    for getout first completely near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
