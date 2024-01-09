9ac96e019d90a0cefd0ab7356ccc11118b9be293
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:55 +0000

    deserve person finally trial ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def convert_to_hex(n):
+        return hex(n)
