60cbd2c20cbbc284cf993f047ea32ebd0bce1a7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:20 +0000

    whether success beside steady easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
