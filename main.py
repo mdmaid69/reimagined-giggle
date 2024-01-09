88ca257a7d7f9b880c160cb9e949a02205be5ed2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:15 +0000

    limit extremely next judge why

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
