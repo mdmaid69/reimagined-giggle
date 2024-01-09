b170f10233a5484a0046d6737377d8a546800a10
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:46 +0000

    announce or holiday fire in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-print(math.pi)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
