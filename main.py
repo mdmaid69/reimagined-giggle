3dfe7edba320ae01a97b501fdeaa73dc6627f30c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:01 +0000

    left fire tall couple that

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
