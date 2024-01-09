7603769fe8011254b6b284bfebfd8d91d37e70bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:30 +0000

    shrink butter major somewhere may

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+i = 0
+while i < 5:
+        print(i)
+        i += 1
