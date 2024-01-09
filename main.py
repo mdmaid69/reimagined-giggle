b4d7f5bd851f172f2ff518a512598545186866b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:19 +0000

    standard nervous thorough secret bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
