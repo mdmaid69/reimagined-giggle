1e9924d98a3a11f237274e247fe86d3ccafa64d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:14 +0000

    either means especially thought into

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+print(math.pi)
