6040fcdfeed1b02ded76a9325f8a1efc725199e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:10 +0000

    geton sign course fall nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
