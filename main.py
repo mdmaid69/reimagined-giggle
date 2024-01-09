86de7e758ba90e3938ffd4f392bbc4d5fddfa9f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:40:26 +0000

    computer hear thank afternoon each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
