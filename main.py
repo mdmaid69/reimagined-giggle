71ad3c5ded9afa3741a1a3d5e88fcabcaf2042db
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:39 +0000

    satisfactory accidentally near buy feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_complex(array):
+        return complex(array[0])
