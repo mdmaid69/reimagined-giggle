da97c09baac984a854a9a8aeccaea6268d0cf2de
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:07 +0000

    mess silent flat event extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
