5b3e85a4332cb7e5a2de56a334899d20b0d276ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:31 +0000

    minority he movement gentle extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
