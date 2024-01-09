dc120d377afe7b4849282cd7b1b007d6184f37ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:36 +0000

    freeze young little move watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_int(array):
+        return int(array[0])
