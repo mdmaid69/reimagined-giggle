c815f6c37724f5add05dbc75cea4ef94f27f1e5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:26 +0000

    meeting believe slide large mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
