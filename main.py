dc76973d487c9bb2a2428dc03594da99b468c027
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:42 +0000

    cold communication to culture wait

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import time
-def get_time_since_epoch():
-        return time.time()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
