c0eef1878210193ecdd5a4eb9201f0815ae51788
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:33 +0000

    passenger embarrassed purpose open cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
