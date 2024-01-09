17c5aed30ffb9cd98b07a5a9dc5ad1f575bc6e98
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:09 +0000

    coat always keepfrom case foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
