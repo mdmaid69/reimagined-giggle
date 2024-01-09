0155fa25dc5db2f38116c72b3ac9131729eea4f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:21 +0000

    runaway suffer decision escape use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
