c46caac50e17cc4a162bb4ac294e9f3558a212d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:40 +0000

    flat store you even introduction

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
