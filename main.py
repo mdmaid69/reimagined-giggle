7ab85de128233a90683fbc0cdc4822f63eda45f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:09 +0000

    Yy brave quickly accommodation turn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
