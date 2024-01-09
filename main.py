8675adb7bbc018686b34d5993dcc7fe87c145c19
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:06 +0000

    usual scale book somewhere everyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
