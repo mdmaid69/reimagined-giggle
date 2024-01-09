3e01f6071c94810e3fb157ac3f94fa0d09dabca8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:19 +0000

    hurry spin violence value land

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
