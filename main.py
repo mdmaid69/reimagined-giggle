034633157f590fe4cb7b9ae3367d89a18691444b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:49 +0000

    handle duty Tt goon maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
