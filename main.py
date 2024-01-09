0493a21064b4fd153050f58e6995e37668630f2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:12:24 +0000

    by general woman declaration do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
