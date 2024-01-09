0959c43ab4f9aad8966f1ae6a9af9dfa73eff821
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:27:57 +0000

    everybody challenge table grateful and

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
