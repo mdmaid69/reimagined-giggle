f8238abd9a97fbf8f8391878755a7817f67a98f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:24 +0000

    people soft turnover instruction past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
