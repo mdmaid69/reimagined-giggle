77f2fa3c190b89fe156afca86782f8de5046fe05
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:14 +0000

    forever bin contact together free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_base_name(path):
+        return os.path.basename(path)
