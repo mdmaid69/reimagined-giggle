228ad99729a01cd45ed6ffbdffb586528b372d6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:45 +0000

    bound forgive threaten push write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def reverse_list(lst):
+        return lst[::-1]
