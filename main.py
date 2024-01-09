1ecb5b90bd5839ee0392f3d05e54cf28d94282de
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:33 +0000

    question Kk Ii goon object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
