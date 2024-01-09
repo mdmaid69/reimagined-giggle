c9ac834b23e7adedea9d8f53f5ba2287c4b51ce0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:31 +0000

    turnback so fly basically free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
