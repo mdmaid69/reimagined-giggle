029f44e765dd1e25de69b999e8e7e07d046dcb9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:18 +0000

    practise fast during factory free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-print(sys.version)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
