6e2a2e0a78803481a7a514e080bcb036697f9579
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:41 +0000

    summer anywhere expect adjective Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_min(lst):
+        return min(lst)
