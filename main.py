c36ed5842040ee6e40f38d45b22705e556e2d6c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:21 +0000

    control attempt ever smoke we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
