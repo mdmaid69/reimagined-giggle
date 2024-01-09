dded02e0fce114a7d84eaca12f97ec549022fa0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:30 +0000

    fun suppose see out lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
