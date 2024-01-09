5babef44804a346f41fd6a4a60b8ff1b1ef68588
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:46 +0000

    obtain warn they claim tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
