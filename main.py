026b6ccd0307261c4db2c4f81625689b589341a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:40 +0000

    date concern yes snow recover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
