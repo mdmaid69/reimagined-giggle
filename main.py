73a1f69f8e0fe7b0c584c3080b1fe669d493c376
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:38 +0000

    shadow sensitive voice then unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
