9757faf204eb60cea4e356c08c95aff0827b74d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:23 +0000

    bottom sign spring pay switch

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
