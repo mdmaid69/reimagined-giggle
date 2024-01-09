89cf9d1a056299969d4fc146e82c9096ab580b00
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:17 +0000

    read row succeed heart of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def multiply_numbers(x, y):
+        return x * y
