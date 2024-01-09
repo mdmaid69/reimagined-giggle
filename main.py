4fafecaae2f02154eae0fd857117e0729f3231a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:02 +0000

    satisfactory beginning instead else soak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
