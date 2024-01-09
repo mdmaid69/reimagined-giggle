03df2164783cd3bf44e92caa3c65f72de915b48c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:52 +0000

    acceptable some drink interest properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
