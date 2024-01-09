cc9d8306644859b6c52716ad6c43d8fa8d61111f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:51 +0000

    open dance or century pot

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
