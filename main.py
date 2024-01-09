465ad2edf4245252be7b9d9113857e853a2b6dd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:00:48 +0000

    impossible language content rest brief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
