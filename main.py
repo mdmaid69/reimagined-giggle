1336cb99b8acd3b572b9eca3be0dc6b2a213bb25
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:14:04 +0000

    cinema save pretty no concerning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
