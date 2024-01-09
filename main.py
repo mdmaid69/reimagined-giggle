f27f3cd6004af00a71a6008614e9723311b0e5fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:33:08 +0000

    dish very hospital bang often

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+for i in range(5):
+        print(i)
+def add_numbers(x, y):
+        return x + y
