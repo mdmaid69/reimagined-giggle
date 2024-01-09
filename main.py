19a9332be28ca6b10406853bf77e11d99811459f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:06 +0000

    runinto original real getaway organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-print(random.randint(0, 100))
+def list_files_in_directory(path):
+        return os.listdir(path)
