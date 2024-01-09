6d3347a3cce85b90be4e40a4219ab311dd764d2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:13 +0000

    value post shopping arrange shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pressure(force, area):
+        return force / area
