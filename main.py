b0308cc4f0f821899522e8dd92e972299d960358
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:13:52 +0000

    chin talk tough arrival fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
