b4c4026d5546c062a9cf8207832a0b31e7bb64ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:59 +0000

    payment equal limit properly wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def greet(name):
-        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
