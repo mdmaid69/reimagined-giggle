8bb4ad593729c18b7efe43fd6a5faf9224d523e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:55 +0000

    strange watch find major Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def append_to_array(array, item):
-        array.append(item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
