e31b6eccf381fbde7a388b66f7fc69527679429b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:47 +0000

    present raw war middle false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
