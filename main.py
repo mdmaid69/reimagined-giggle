dde416e25d2affc7c0cb6e16822cb3dc8cfbfe3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:34 +0000

    luck level bottom phone combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
