d91b26e5627992c56ad6ad68c427897f13754620
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:28 +0000

    mark think plan leg about

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def greet(name):
+        print(f"Hello, {name}!")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
