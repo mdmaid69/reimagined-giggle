caa940eed01ae4daf7577c7b386ebe1feb34f0ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:40 +0000

    genuine one problem middle century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
