55a875f399c1856d350c3d07ba47963de94815e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:50 +0000

    instead time acceptable lost spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
