5d9ca3df7e0674c5a95a7f175adf5b51bfca194c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:20 +0000

    confused ourselves doctor morning under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
