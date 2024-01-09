7fdc9fd07cda6b4057ca0262481188de9928ec85
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:37 +0000

    century fix march finally unlucky

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def add_numbers(a, b):
+        return a + b
