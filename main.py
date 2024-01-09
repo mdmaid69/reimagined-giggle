6cc15261e0a2c91dd02cdc0b734563914455ed60
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:18 +0000

    probably cross season people direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_power(work, time):
-        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
