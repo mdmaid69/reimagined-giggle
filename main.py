0608a83331b6e97ca6969ddbc0e06a53fb4b5c68
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:57 +0000

    description familiar knowledge identify watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
