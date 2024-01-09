b7ef7fada8411f138d51bbd39b7985e90b54d76d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:15 +0000

    square model trap free send

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
