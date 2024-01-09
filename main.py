7a0c530a2e151cf5dfa607f5f4ddc61313149fa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:51 +0000

    yourself rent unless everyone send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
