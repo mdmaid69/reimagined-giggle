3eb017f93e683195b4fffcbb2b5f32e3747c0754
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:52 +0000

    busy thirsty abroad own congratulations

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+for i in range(5):
+        print(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
