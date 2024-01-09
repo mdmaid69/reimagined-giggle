8afc1772129efce5ab40ac88956c695b697116f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:52:52 +0000

    chase instead stop skirt whistle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def sort_list(lst):
+        return sorted(lst)
