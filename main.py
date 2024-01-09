0b39ddfc51035f9e6c4e9d28bd3f0229561268ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:56 +0000

    trust accident warm strike either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
