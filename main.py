a14a204e00a049a77a28bedb4fc04318e988ed71
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:09 +0000

    sorry nor recognize listen 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
