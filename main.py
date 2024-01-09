9c6ef4b7f10c4234b5ddbe054eeba2913b335865
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:01 +0000

    partner old beginning pinch board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
