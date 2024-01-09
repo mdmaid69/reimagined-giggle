246649c049402d1ffa1a192c426f383ea6a98ed2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:55 +0000

    everywhere fail warning tend except

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
