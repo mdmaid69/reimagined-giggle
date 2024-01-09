476028c104a102427f307958b4f16c8cfdcdf170
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:52 +0000

    special door might pollution summer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
