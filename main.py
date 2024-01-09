7ff1e94449d37655ce54d37468dbb648b1d0c88e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:52 +0000

    balance inside able angry apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+import sys
+def exit_program():
+        sys.exit()
