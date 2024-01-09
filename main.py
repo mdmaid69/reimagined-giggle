dbf28285f01a529f452854a795c2b66a9723c947
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:14 +0000

    modern offer judge thinkof quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
