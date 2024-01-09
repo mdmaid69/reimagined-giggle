af4ec597c006e8a2e61c5fe1f40eba2477ccc6de
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:23 +0000

    strike human frighten space answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
