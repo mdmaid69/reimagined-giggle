3c5b5fc130e1c424a8ef388373ff70c400ca529d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:08:11 +0000

    modern still Ll shop very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
