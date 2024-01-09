0a14a72e66f1cfac3c3c282fd1f1f80b92ec6425
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:10 +0000

    off mean turn round yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
