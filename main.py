0c8b578aa3171dbb75f56458bdefd3915930e0fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:29 +0000

    come suit separate study runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
