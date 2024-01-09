433cbc176e02924858f74408371d34bb715f49ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:15 +0000

    tidy himself loan confidence every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
