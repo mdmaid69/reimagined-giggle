fab2aa87207f8fa801779d91423811236b89a629
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:53 +0000

    lucky recover familiar Rr friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
