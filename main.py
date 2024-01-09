28e213549174e345591cbd67d17ef617d624bf2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:49 +0000

    weigh poison funny basically sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
