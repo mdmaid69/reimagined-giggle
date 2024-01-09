3ae7e2d2209842be8605de7d2b11c70e06b4975a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:01 +0000

    legal downstairs environment record pleased

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def add_numbers(x, y):
+        return x + y
+def is_palindrome(s):
+        return s == s[::-1]
