b150ddf644978be30c7a0af42c3d82b6bcd3fc73
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:47 +0000

    fact through Ss replace pile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
