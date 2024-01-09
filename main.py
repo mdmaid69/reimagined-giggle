6eecf332788bba859598e85ce1e08c5c11192a22
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:15 +0000

    pipe match bury sure consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
