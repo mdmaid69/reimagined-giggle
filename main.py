ed32b973ab34720a904620fec4cb848516917b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:16 +0000

    necessary receive fold some life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
