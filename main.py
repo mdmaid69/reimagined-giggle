7647331e67b195c7e6f046669b907e93baf91ae8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:51 +0000

    great test record money ill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
