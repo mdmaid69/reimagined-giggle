4c13e7ed076792b6ffee6a1015a90f4bff79fab1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:48 +0000

    mind afternoon shade ever false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
