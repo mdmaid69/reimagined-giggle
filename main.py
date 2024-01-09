e2391d013148b0190cf79c616a4f299472cc9312
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:29 +0000

    recognize use disapprove Vv connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
