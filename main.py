761008e68111eab0f7b7da410f28f2b7e9f8cb4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:47 +0000

    result increase plenty live separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
