feb5cd9b9da3f8e74553f18f84dc612af33bf83b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:32 +0000

    contain immediately plastic exchange skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
