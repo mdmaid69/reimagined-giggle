9ff5d6c3e33288f907fd84a57fba2a77cbcef1da
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:55 +0000

    region holdout switch comfort something

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
