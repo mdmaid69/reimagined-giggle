61e35c309796c8348b50dfc3bca43c45a2bc558f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:47 +0000

    sorry mistake declare pause you

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
