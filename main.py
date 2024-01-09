e0ed31a9bdba9445def2528ff3c91a0096c24d18
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:08 +0000

    note height convince attention problem

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
