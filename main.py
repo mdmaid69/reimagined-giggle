b5fa7784cda9f51770f8b9a8f8a3a403d8802887
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:04 +0000

    eager poor bath delight sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def get_array_as_complex(array):
+        return complex(array[0])
