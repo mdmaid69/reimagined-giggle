15859f0f171655b5b6e44ebb380e98a5a510d12d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:14:10 +0000

    measure risk come steep down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
