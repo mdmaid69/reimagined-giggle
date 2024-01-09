a425e9bbd10d169b914d70fd574f41da21aeab2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:26 +0000

    lose kill joke preparation skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
