b94d04b997165923aea6acc8fbaeca369e71c294
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:55 +0000

    break slight goaway star firm

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
