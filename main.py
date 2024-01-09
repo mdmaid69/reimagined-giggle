d9e4b87ce81b901cc8173dcb649b05acdf8a04f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:04 +0000

    wash run lost influence discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
