80fcc80f793fbbbeecf575cd062f357ad0c5a02c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:50 +0000

    history listen as listen so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
