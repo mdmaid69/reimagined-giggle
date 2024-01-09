cba335d2e5df4f679a0f5668ed40b3115976298e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:25 +0000

    very embarrassed Nn situation ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
