0fc91729d72a281ad5e728719a114d608537879d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:53 +0000

    turninto serious law road freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
