f2e7db3296a0eeccb40c8f3fa0fc9277857e3162
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:27 +0000

    stop smell nice actually point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
