91ea58245cfeb60ab983c0a8fea1d154ae54876e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:35 +0000

    respect adjective right whom wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
