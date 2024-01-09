a63198cfb3cfefe6762895bfe5e7532da4ba315c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:31 +0000

    recent respect splash occur for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
