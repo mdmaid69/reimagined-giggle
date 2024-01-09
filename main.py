b39f4c0a28e81ff8b214e47033e14c651d6834c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:51 +0000

    cheap order look inside prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
