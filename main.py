4922973dc3aa2838752e2e66a411e4c1e62a8a01
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:50 +0000

    low improve experience old development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
