a25d1754f14c0ce7da97f54169d8eb9622e94885
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:53 +0000

    concerned journey speak damage everybody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
