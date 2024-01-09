ecbb8b8f62cda0b5d1e533f8479a4d5c8a89f162
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:22:33 +0000

    them file strict plain kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
