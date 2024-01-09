56a47ee2cf56a3262b9d6ca45e4217a96519e344
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:44 +0000

    should degree exploration wait disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
