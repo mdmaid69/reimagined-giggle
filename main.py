5b654f389110590a12a8fddce0940db6319a127f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:40:16 +0000

    order minimum follow centre emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
