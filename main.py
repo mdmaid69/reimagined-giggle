a9ad46c0a30e0e5dd2779240db77566d3462c883
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:18 +0000

    true female by excuse bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
