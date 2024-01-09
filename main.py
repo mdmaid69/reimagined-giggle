90ec4ff788f4c1eb569daf413610c6ae7ceab556
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:04:30 +0000

    murder skin some some sweep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
