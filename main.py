d17909de7cb1a38a7f09cdf3f93a1a26c3e3a960
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:24 +0000

    571enough as disappointed now noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
