a86ac4263c3814835cd1f9a5d8d045c2869d151d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:28 +0000

    warn wheel bread your continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
