d198ec022f6e43aae87482c47d2b36d170b6534a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:04 +0000

    often phone middle side machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
