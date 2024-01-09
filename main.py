90eae55d0afdf44d756a830a4ac4c6b2a2c56d77
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:32 +0000

    translate left criticize afternoon dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
