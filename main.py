225a477476e1e69706f23f14c863a2b70a8924d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:48 +0000

    delight excited couple work bear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
+import os
+print(os.getcwd())
