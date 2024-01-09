62aacbe277563fec0e3f6182aa4d6da37f91431b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:30 +0000

    cry point action straight takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
