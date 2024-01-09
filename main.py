28f93db258005d2ec0d8b43f9d09a387e57a75be
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:08 +0000

    future bus majority ear feel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def square_number(x):
+        return x**2
