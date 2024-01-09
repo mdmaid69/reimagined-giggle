f22c720ac278ab3cf015c9a3727659c06bd41dee
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:49 +0000

    across too centre answer low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
