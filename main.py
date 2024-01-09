0171fa2db330d590c31b3203c216da6e20737c84
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:51 +0000

    attempt comeover sore word suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
