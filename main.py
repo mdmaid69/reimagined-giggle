c79af69b8735840f7772a6a3f946105507d476ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:39 +0000

    previous accommodation rule explanation Dd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def is_odd(n):
+        return n % 2 != 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
