16ce3b951e5dd9c52069fe5957df3f964d6c52ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:04 +0000

    attract clever brush underneath watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
