c17a9b5dae858feaa7fef43b59251ec19992019e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:14 +0000

    ear concerned shallow imagination offer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
