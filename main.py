96ecc9bcb8e79b7c2c87ccc47f1836f6aedbf371
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:38 +0000

    maintain minority care doubt hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
