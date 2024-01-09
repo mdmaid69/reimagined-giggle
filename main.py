ca0d23fca9626f1d7e5438ca4aa95d7d4f6dcfc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:06 +0000

    convinced defend stranger phone rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
