a79e21e2b45c402483536604b8c3d9b708e62112
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:16 +0000

    disappear receive repeat description human

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
