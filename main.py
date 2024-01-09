64638c6fb11b500250fc4157aa0692f18ca2fa7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:11 +0000

    patient collect polite cruelty beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
