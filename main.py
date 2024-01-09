11025b6c3bde0fef0bb305aef33b37876624a64f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:45 +0000

    order block stretch fold favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
