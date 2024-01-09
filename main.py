006f843412a912cc8997e31e0bcaa45a05b63bcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:44 +0000

    pin yourself roll and backwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
