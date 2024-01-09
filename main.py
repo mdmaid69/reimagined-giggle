e34d9f4bcea357037fb10e002e85d169b507f709
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:13 +0000

    miss valuable undo ignore language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
