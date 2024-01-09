1b4ba01dfc2211cd3781aacfbdd095d72d4f108a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:04 +0000

    drive guess ill platform gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
