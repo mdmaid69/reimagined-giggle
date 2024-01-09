988e8d07465e6f3f509fa8407b6d9dece7128cef
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:57:30 +0000

    noise own steep such convince

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
