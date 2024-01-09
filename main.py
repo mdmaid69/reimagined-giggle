29f373054716b0625862652b1f1c64f28e1a2d4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:04:52 +0000

    worth national this doubt frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
