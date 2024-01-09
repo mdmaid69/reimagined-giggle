43835a29605c63a0899c970c655a749c4f10edb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:53 +0000

    trip cost cross discover power

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
