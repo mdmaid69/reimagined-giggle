7ca7020d33db9e2875d0836816f07cad02a944da
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:55 +0000

    trick extraordinary illness ability pinch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
