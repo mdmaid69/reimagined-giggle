80dc075ee169a894ecf4920749ac6f696087b675
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:38 +0000

    her empty threaten custom project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
