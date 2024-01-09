3aef18941150a9f08257d9a775e531caa4eb1d84
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:34:41 +0000

    dangerous moral shock Mm replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
