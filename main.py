10211675e6497c70fe74f2a303f9b883c050b417
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:40 +0000

    ago combine I pile anywayanyhow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
