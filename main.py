3cf9d934f3e508828fdc5d6c4d29482401664de6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:52 +0000

    noisy blind invite live variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
