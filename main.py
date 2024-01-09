cef45703bac5f85543c8c5b08abe7f6601bb7394
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:58:19 +0000

    suddenly conclusion accordingto open deserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
