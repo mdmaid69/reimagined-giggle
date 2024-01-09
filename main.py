41b72d386db33826405307dd5c574b2e5168827d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:23 +0000

    whistle message married wear shelter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
