db9ea4095ef2fb86d8b042afe0a5b8c9a4883b91
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:09 +0000

    admire off complete marry health

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+import re
+print(re.match("h.*o", "hello world"))
