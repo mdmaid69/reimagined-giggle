53d503bc86715e2861fca4c90e53cbc80eecc5e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:20 +0000

    anybody could along guide destroy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
