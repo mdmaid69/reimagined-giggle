dd2f2593d1b38e438d0de44963bc708b089d17e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:52:08 +0000

    convincing contribute otherwise by environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
