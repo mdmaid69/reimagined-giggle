d9acb7b88582a9342d5e8a48b3944fd1eb43b279
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:58 +0000

    by include like poor film

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
