b99d8c63af6232f33f191dc0816254eb4a405507
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:24 +0000

    ashamed risk woman value perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
