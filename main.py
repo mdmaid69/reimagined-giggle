f6649a41b5a0796c9f5a11bc03932f095c7b5c72
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:52:04 +0000

    event card store protect favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
