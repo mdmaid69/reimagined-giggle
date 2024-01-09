0f4cce2d62584ddb89d9609dfd15a17f39985166
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:26 +0000

    bear pen story with insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
