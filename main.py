935d4d98e2f1716fc5ff28ed36280abdf0e350c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:59 +0000

    length collection surprising tread feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
