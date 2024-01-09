b5e80bb4b6043d3ef18c9ec449dfe2baeb06b418
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:05:37 +0000

    success I upset development bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
