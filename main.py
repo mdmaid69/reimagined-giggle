b18c0f6352838282b1c2749406a8deb05a994396
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:51 +0000

    glass tip bringup ours slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
