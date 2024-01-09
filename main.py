cdb8cfcaf0f4802032ab7775e234923cc2b9b043
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:16 +0000

    fear modern survive finger any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
