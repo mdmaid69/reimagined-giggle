cbe5a557f19d474ca1a7cd2624eea2556cef9230
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:02 +0000

    odd advanced sweet union weekend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
