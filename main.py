dca14667cfbe6388966576310bc136cd90d10438
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:50 +0000

    success appear shut blame expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
