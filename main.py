cee4c8616d8b7c7e11be68dbd55ed0c34068653c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:20:30 +0000

    crash slight keepoff wire wild

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
