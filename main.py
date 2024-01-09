669a943c161307ef14cd3798e5f60d6772dd242f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:22 +0000

    punish comeround crime about offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_power(work, time):
-        return work / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
