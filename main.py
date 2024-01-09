060443c20c4e150d9af3b440fc56ef4e958985dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:42 +0000

    doctor anywayanyhow course discussion shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
