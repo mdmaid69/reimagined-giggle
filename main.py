4796ac653a2bad30cf1318c38837952922b5f2e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:49 +0000

    part emotion minority anybody true

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
