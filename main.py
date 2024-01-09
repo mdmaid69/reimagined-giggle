d50d5c796bd07739454ed5279760bd1600342959
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:14 +0000

    wash common tradition down quality

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
