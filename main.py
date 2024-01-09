6b203e638b9183418270ed86569dc7399210491f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:47 +0000

    impatient breakfast steal march payment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
