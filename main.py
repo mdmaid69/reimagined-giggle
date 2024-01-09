30d0831a8923fa0a195ea67d89b593acace0b602
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:46 +0000

    flow allow except after tone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
