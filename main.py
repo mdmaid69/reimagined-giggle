b46a1e7276f4a485ab095479078b67ab7ff72edf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:10:03 +0000

    serve description whole occasion concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
