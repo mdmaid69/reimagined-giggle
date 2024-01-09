87f61233679127bf8f702a9ffe3febdb880ce4b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:43 +0000

    border hardly hide offer cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
