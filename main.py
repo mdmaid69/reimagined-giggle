fe91541050797fe1f404973f7c757d905c5e19d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:35 +0000

    sure shake react bus exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
