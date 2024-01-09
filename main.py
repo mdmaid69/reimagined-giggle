91ba8d4fb0548d90404f600033084ef02ba7cd3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:19:28 +0000

    568energy only hers comfort judge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
