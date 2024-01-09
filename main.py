40f8bc33af8cef99dc1d88ff2af70cf72f560f23
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:52 +0000

    allow stiff cry guess embarrassed

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
