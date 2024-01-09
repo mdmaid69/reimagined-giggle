4700850e643cd36163939464672dd35432c9044e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:36 +0000

    path yet breakout very success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
