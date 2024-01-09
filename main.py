659995cda394dce7ada0d4956c8b95c4292fe253
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:23 +0000

    between confused Ll truth generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def add_numbers(x, y):
+        return x + y
