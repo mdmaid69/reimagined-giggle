310e480781269243b9dbd918c3c4015b1c8e1fe4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:19 +0000

    history remove suffer load mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
