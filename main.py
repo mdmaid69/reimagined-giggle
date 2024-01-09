9ab18b4bd41df36ae7e0f8c4dd233b85410e384d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:16:31 +0000

    get complaint profit thorough pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_min(numbers):
+        return min(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
