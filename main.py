931ebe8fcbaef94b90d20fa66d10ddfcb45b0c8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:57 +0000

    Uu relative disappear hit goaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 print("Hello, world!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
