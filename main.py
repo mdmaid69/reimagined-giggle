2d05ec16bd22b8cad3474d323555813f97f37a82
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:04 +0000

    lookafter black guilty nose welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
