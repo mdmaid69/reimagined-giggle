5e94b250cf973861ebae76a051bde54929549ba1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:16 +0000

    profit couple congratulate communicate figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
