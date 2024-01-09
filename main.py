35c55483fc21549a7037fa4e024a363721e7cc6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:45:18 +0000

    speed middle television them sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
