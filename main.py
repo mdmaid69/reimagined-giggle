a76ad34732bd94d2aa6160a3759c229aa5ce18a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:17 +0000

    block error hit concentration blame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
