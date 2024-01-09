27861bba8b944e080fea675a6311ad4428feeb92
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:32 +0000

    ourselves below interview shock worst

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
