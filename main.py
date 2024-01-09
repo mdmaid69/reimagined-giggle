b7ec13ec2e14f8a1dc4d56c2768fea4fc67aa20f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:02 +0000

    dance entry base slightly fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
