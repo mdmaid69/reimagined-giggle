5d93f22f9507fcb85ec364856e90b934d3e89ccb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:06 +0000

    mouth Itself shallow notice generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
