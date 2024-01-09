ab9d3f5ac7d772ea90ff4b149fe40e7a4b7de96e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:26 +0000

    plate now progress fasten uponon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
