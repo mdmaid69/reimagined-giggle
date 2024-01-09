9a352d6046a6515d25c002806ab69bea712641a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:19 +0000

    morning my advantage condition holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
+def find_min(lst):
+        return min(lst)
