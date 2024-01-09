d42cb37d09f337d53f18d4360edfbdf889cc9bb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:57 +0000

    convenient getout rob for funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
