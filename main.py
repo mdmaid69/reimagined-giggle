933de9929f55263d0b8a3145a08519274b51ff82
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:12 +0000

    switch average Yy but container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
