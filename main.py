bea8290f0409b208aeb666d3731bb2cfe15f0cbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:00 +0000

    own decision buy child weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_gamma_function(x):
+        return math.gamma(x)
