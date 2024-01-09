17e807adb7654bf4e4df031718b838fa2dd08626
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:04 +0000

    phone share wish catch Bb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
