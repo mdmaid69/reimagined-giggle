41607449f91ed0131ea6b62fa5cf6b2a1d4dc323
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:33:02 +0000

    right before bright family only

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
