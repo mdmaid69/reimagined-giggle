43fec21d8c9f541803a225168b0fa9ee209b1882
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:11 +0000

    swimming takeback fortunately prevent pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
