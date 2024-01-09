e80b5afa4eb8e8ebadabe5a4742689be2e42cf99
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:36 +0000

    software race programme talk married

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
