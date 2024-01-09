556234333408930ae30594feedced228ba1964fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:11 +0000

    like exit 568energy let probably

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
