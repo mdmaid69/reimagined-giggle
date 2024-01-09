44d740a6b2c7dcffb80826e8a6b5e0c5b1da7cd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:17 +0000

    567enemy earn prove true clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
