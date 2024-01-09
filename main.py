8ff137359676fb6afa2165e58d72f38bbaa9b4a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:57 +0000

    after hard frighten deal name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
