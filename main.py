e02d8fdb4627e5175cec24ae35d00a8c73ef9da6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:18 +0000

    however heat surprise unit 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
