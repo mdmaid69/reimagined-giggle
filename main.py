6c2f29ec21d4ddb6ba17524863aa7f931b4c9aae
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:09 +0000

    move 568energy 570enjoyable serve both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
