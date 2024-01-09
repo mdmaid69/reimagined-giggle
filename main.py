8fca977dcdd75acd75d80a50494531c41072bf85
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:29 +0000

    holdup load announce lonely Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
