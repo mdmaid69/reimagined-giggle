411ecbcc70b8f64b39d3976dbb9687e05a93cd1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:10 +0000

    table herself tour opposite towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
