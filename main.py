a019772288d376929a2be3f79b5d60fcc012f6e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:49 +0000

    hard fast murder little exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
