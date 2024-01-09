7e1a0d040057238fce6625a70dbffa581fc6f308
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:58 +0000

    male sticky frequent beneath doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
