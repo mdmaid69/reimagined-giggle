fe12324a36db9b880e45a2e14f83778d8165e090
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:04 +0000

    frighten print negative however speech

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
