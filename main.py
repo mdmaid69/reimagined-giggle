d8e55a7d62b5d0eaa64f8b6180b7b83c52735c87
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:41 +0000

    interview wide pick throw nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
