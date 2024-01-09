79dc2dd0c892d725f4483617ecebd924222031a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:31 +0000

    well sticky hers responsibility fire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
