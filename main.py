8dd4b2c478d6ecea3b179035510be9df3c0a46fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:07 +0000

    circumstance clean sink visit refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
