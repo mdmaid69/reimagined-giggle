58ecd3f0de078a7bcfe8e08e07c09cb8d5a661e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:41 +0000

    breakfast brave breath trousers else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
