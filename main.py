f00c9904551d458c1e3b0545d83f14c4a49d4db5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:46 +0000

    insist owe activity hurt on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
