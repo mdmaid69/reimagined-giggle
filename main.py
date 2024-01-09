a2a58a52a7c06d59f4a696460b23cf7ffef3ab9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:18 +0000

    smile weather air for undo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
