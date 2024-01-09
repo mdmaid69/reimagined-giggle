f9e7824457c51c1bb9e72cb36d684ef532fd926b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:13 +0000

    flat tomorrow stamp emergency holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
