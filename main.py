3e12d53e82ba604b79f4424e9d08ab24c2f59c18
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:00 +0000

    room shock aware succeed impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
