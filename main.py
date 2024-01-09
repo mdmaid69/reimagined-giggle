b10bfb4c29d84b8e65249d8ef334bc72a95b638a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:33 +0000

    ill range flavour whole ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
