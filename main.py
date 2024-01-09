5ee34797eac5c746c9b9892b88297ecaded2441a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:24:03 +0000

    address plan demand convinced appeal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
