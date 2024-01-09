019ee56cca4f14667c41c7ee4917a04561f14aff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:30:53 +0000

    along true well it blind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_str(array):
+        return str(array)
