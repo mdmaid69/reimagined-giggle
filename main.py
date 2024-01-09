37d178cd0516e970eabdf4ab23ae03ce051990af
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:06 +0000

    personal history yourself copy match

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
