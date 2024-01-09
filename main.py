813df59018738826380a34266ffce4ae719a7eac
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:36 +0000

    load run final advanced case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
