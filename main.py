edd5dbccd2b3c93ec98e3225e62bc54969205a7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:41 +0000

    week considering truth zero takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
