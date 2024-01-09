d203da61a9a908e3eb2c49c4cd4a9e75aedd7409
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:48 +0000

    bush dream entry pencil pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def cube_number(x):
+        return x**3
