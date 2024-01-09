2c032016a4afb1dcef375e59a877ed7d34f21df4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:54:14 +0000

    post dinner go under soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
