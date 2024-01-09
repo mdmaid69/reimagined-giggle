0dffeacdec128f2b97f44d50b9aafa7bd310e965
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:10 +0000

    male apparently rush inform worried

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
