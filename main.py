8ca1b15220beda040d3369f093ee2c270065e586
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:36 +0000

    figure tense follow song switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
