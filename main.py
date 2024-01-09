0080ca37791a837b7f0d6da368aa6192bf9a0341
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:56 +0000

    copy increase later shoot basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
