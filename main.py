243b3ea9335f38558e983669ecd6f58a8a9ccd59
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:50 +0000

    runinto whisper song drawing rely

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import array
 def get_array_as_set(array):
         return set(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
