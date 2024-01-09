38c1914f960e42500e180f1a8d5bc4ef588e0e68
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:25 +0000

    your afternoon giveout month instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
