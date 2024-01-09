1b6caccb3c4b8fb0bea084568f5d1f6bf19912e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:40 +0000

    approval delicate size bath sail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
