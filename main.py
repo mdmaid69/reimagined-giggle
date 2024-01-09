7646b6c35ed6d52468f616a1e41c7349ae44e578
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:11 +0000

    development television cover wall from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
