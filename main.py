e615b120efab0b4b927dfd694d69dd023c3e7d60
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:50:20 +0000

    shelter doubt 572enter shout official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
