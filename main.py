3aa699260c4079189d76450805df73ff542cf88d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:44 +0000

    direct improvement speech shrink can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
