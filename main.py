93fc5fef79bf9c3396336a22a1807d6e52c42366
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:50 +0000

    yesterday decide bad describe fine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
