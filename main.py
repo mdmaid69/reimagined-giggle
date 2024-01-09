f1b150d76695e38b50b4671e234a585ffebd591c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:09 +0000

    regular anywayanyhow length burn cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
