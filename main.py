a1bf9c5fdc3c533953c0a7949946d77d15c41b56
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:50 +0000

    body thinkthrough flow badly wheel

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
