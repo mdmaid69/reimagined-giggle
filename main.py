a543bfc3188db8511447c9ec8554b6fa047d1c14
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:15 +0000

    569enjoy artificial different complete design

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
