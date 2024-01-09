e63c644cfe152bf07be11929c307d3b5c4858631
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:40 +0000

    squash actually ability stupid mix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
