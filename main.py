1140506cd593e3c8e7c9c8457be10657dd32f1c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:45 +0000

    delicate invitation prepare dull warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
