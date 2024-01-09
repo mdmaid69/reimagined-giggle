efe50fffc66102e40e38f5962cc32dfc31a7346f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:46 +0000

    proper feature prepare picture waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
