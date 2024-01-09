d82b326cdf736fa51846aa77bc6466fee2a529de
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:46 +0000

    circumstance tidy workout quiet star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+print("Hello, world!")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
