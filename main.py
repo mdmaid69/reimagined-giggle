258e1023535db3dc11af4e3302f2321378fca618
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:46 +0000

    hour secret generally wear leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
