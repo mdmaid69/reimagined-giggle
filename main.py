0a9bf9786742e69e86092a6c6d99eb351069b0e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:06 +0000

    command bread down many stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
