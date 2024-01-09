36958f8cc53257819514e09d1f61667520030cac
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:30:33 +0000

    sure border practice ill club

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
