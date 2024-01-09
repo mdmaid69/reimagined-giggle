3f459c227ce5ac3b01941f287bea08d60a892b46
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:31 +0000

    wait pocket ilovedicks intelligent risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
