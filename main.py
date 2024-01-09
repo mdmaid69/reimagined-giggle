ff77118a0e4b12279e20a349b6ddcb1f22566d1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:11 +0000

    chase afford hurt thin fortunate

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
