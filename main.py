1fad5c58f9b0e007ef8b306201c42e7b557b0906
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:03 +0000

    train complete sea flood pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
