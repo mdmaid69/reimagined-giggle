dc12d54e245119135628de69c42d1b11818420ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:41:19 +0000

    thin letter takeoff about entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_int(array):
+        return int(array[0])
