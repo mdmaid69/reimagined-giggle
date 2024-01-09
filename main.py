910133ac59ae5aeee1ba2798987204e8ffc125b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:20 +0000

    travel likely profit appeal be

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
