739160dc23620e7b83a1cfbb78d23c80d9940c69
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:56 +0000

    convincing sore straight wealth conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
