3901036afd3f8cc49e3e361cc119ee04528e1285
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:17 +0000

    first everybody return loan start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
