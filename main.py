1b1fe3b3a815366efb19d2f7e443899702be61c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:08:37 +0000

    bunch section spare exit copy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
