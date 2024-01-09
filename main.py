52f4dcfc83a5f3557e41fec3b75b457ffc918eeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:47 +0000

    matter not pour cry vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
