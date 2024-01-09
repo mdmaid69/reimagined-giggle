573cd6b237211236cb723a3f3f7ceb5a79534e97
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:00 +0000

    suffer strict appearance imagine getout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_rectangle(l, w):
+        return l * w
