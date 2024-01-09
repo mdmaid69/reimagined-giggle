92539a6e30751ffe3a2cbb30a3bb45427a8e4ef7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:22 +0000

    write mistake window tidy reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
