801ed42bee0baf84356401076dd9bf51ea10e835
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:57 +0000

    Ff dead too through and

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
