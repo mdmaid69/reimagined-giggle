3759f2a0e3482034c3b00a5924a55ead2dc585c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:19 +0000

    reduce harm say classroom regard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_work(force, distance):
-        return force * distance
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_octal(n):
+        return oct(n)
