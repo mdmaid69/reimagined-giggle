cc18a28c2eff74c23f6f0ded98c45619a43a5f46
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:26 +0000

    guard dead clean favour invite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
