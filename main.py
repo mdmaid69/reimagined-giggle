5d2cbd9bf4c46a8bb92fc8e1555086671877779f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:44:45 +0000

    drive accept include lend already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
