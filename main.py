009fde2a827730e70d63aa848756a24e839899d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:05 +0000

    surprising run call be blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
