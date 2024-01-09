c9be77da52f00dfd85dbff468eb598a8b42a799a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:05 +0000

    drive place against emphasize 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_area_circle(r):
+        return 3.14 * r**2
