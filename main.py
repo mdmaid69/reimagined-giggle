2f9c66ee7742c1c94549439efdd76862b504ff16
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:24 +0000

    closed can straight label punish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
