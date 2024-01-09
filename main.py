f49e0045e0f1ed53e4ec772275940e3704be07e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:24 +0000

    Kk keep pull carry ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
