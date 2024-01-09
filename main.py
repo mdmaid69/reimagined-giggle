d1ebe832f15dc470f2c8417c5effdbd021db8266
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:02 +0000

    uponon talkoutof pride evening anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
