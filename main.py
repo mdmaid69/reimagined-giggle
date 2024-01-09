fc3328b1cb669c64c0d7d4de55a2cc914d5c94b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:34 +0000

    eager petrol holdon translate letter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
