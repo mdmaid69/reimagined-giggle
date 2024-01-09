d4c7c17cc1d7cd8134b8ee2a5f2ee2339dd76139
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:38 +0000

    common light ask noisy impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
