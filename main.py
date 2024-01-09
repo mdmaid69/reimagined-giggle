c2d66c2235ccd4406b91c39475b6e224cb16c8cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:21 +0000

    finally base give entrance other

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
