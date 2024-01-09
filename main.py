2a83180b3d57577a9953c65257916989f6f66066
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:54:32 +0000

    boil range deliberately climb spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
