12d1ae7e736ddc6af7a95e2ac32769f73194a93f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:14 +0000

    sink music concentrate fasten cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
