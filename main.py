4feb4f7b3b97ed2071f2df60e315b81b18dc7cfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:30 +0000

    our gas sharp bound noone

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
