9c4cb0238471782c39dce56f38f0c1e569330b4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:23 +0000

    interesting travel software statement comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
