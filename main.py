f77f097ec0461cdc456ab4666e45031e514f9e12
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:51 +0000

    burst broken plan taste street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
