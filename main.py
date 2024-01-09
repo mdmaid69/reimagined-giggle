a1bb32f497f9cbab583f14befea3a98cec0d7bda
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:28 +0000

    part create extraordinary slice boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
