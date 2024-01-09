1f70755e4d9fca2db558eda3d6b8371a251abda7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:47 +0000

    train rush even although phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
