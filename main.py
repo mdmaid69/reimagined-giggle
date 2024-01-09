f56982c77343978e637c56809e5047985e5fca4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:29 +0000

    indeed lot experience serve programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
