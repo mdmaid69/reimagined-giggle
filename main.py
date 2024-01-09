db1dcfdc50c556e5d8516bf7deac32e242040ff4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:29 +0000

    beyond whatever division worried land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
