d69313c0b05ff2a6311d514688b29dfc8b25f238
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:53 +0000

    would fasten luck suffer decision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
