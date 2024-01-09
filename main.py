c41fbfb063c36f1acd22e3add4015bfdc4246a40
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:40 +0000

    give face over file patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
