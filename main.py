28883bee354153cba1407e586dbde05adffd8726
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:29 +0000

    send clothes disturb excited either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
