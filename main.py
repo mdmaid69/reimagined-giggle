c181d8ef31b6f7cd6da402d42416b6ec79b1d613
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:31 +0000

    final control again copy happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
