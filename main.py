af91d61554ce89e609645b45108479bcbbcef7f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:54 +0000

    room purpose anybody balance unite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_min(numbers):
+        return min(numbers)
