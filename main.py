444dc2dab45615c4898d7ebbf5a42fe04105c331
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:21 +0000

    available sentence meanwhile comefrom conclusion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import sys
-def print_python_version():
-        return sys.version
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
