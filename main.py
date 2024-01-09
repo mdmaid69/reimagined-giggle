ab5eea750c68d9e4055b5d266f334d521a675035
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:23 +0000

    her move surprised him guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
