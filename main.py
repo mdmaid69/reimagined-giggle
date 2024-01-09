e8a585ffffbe805a86b746ec91eeffc93408c2f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:17 +0000

    run confused entrance means slight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
