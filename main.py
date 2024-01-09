fa590e9bd37960ca472b78e3a4666577cbafebfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:53 +0000

    light final pleasant pupil use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+name = "Python"
+print("Hello,", name)
