a19d27058c1ff762de4b9b001f39fddd413ca953
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:41 +0000

    attention money phone involve secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
