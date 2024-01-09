2b3a7c0e424e8f5102af3d07eacfe6dfc8bfb5c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:14 +0000

    prove join yourself it I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
