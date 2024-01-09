59eff7bd168e2b06f25fb91aa49625733def706c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:53 +0000

    lost obvious themselves sister television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
