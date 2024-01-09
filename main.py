7090242426a520d68a080ddceef6da89746b1ddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:02 +0000

    fit pour unfair comment voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
