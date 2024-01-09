485f09b0755f9362881c3fe93ba5efa74354973d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:37 +0000

    dress middle central lead entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
