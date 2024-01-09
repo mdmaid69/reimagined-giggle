8d81290b46652b6d07388595ebfba0d94581e7b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:32 +0000

    complete normal talkoutof question happen

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
