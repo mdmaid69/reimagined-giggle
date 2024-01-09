4b1708431fd7abdce3d632565f652728d6c32a00
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:37:09 +0000

    danger deliver embarrassing enthusiastic hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
