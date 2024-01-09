cd6e16f0664999ac019a352fb4fa0fe228184ddf
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:28:41 +0000

    taste winter month quickly spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
