4f0826ae1ec95d295ab048712df16b817010d0fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:38 +0000

    touch buy book pile ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
