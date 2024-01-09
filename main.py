27582ef3304073d75d7636f7912d3e99cb4ddc9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:49 +0000

    nervous anybody any calloff war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
