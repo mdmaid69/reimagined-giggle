927a1d78d7da2ffbe1ca981215a5acfd0befa7d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:36 +0000

    catch deaf general authority report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
