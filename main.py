e66b1e13b71e0a833dfc90490ad99b501d6f593e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:34:37 +0000

    season break bill defeat 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
