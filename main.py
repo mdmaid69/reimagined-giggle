b62039ce845753fabd04dfab5a8ed6bb444ae45a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:52 +0000

    dance allow cough mark intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
