78b7af0f2cb9bed6d40ffadd05350a6b307ceb7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:16 +0000

    state frame consist content content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import math
 def calculate_factorial(n):
         return math.factorial(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
