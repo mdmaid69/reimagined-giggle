31982e39b441725e30a74f561752f1a396f85dd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:28 +0000

    limit unit usual organization before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_counter():
-        return collections.Counter()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
