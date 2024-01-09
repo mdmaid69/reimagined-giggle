830da35fd6e13c6f4177642530d8930ea1b52693
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:30 +0000

    down late related please custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_odd(n):
+        return n % 2 != 0
