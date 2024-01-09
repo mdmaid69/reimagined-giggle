5a9f1b07a2e78d688b766d1f0b0252a1e24bc2a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:18 +0000

    makeinto getout pencil tip eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
