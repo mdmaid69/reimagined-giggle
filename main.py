b94046c9308339a1de8699f148e8f5008811933b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:25:55 +0000

    mystery quick rush rest need

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
