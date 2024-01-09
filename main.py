3165c6c3d5de9e09717575a4295711df3b25210b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:49 +0000

    forget history exact rate street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
