be99055a5f78fda2558dfffffd7875680dad7a2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:59 +0000

    will below moon section strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
