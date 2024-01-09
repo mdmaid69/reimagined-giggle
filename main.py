8cd510cd9f4a5abc123378cc85a4005e3b570f87
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:00 +0000

    opportunity relax large refuse several

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
+  def reverse_list(lst):
+        return lst[::-1]
