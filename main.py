482c95e4bc8e6f69b9960c5045bffda802b1a0ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:05 +0000

    tax talk load outside plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
