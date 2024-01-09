636e7ed8de705b9899a0be9d7c75f906d358773e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:11:57 +0000

    battery row takeaway ear trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
