6052c58015fd07830d85315c5079af2d22a62d75
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:36 +0000

    tool lookaround clothes hour delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
