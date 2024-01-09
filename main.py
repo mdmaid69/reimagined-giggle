4d86085bb3733030bd4608a892b4b6f9c348f454
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:56 +0000

    slowly angry goup expression breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
