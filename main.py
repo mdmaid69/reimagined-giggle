23717d165a5dcc62a6629166f3e15483a7757670
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:21 +0000

    abroad desperate imagination let who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
