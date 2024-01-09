15c2000c789d712e439dd09b0fda085d181b15e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:56 +0000

    accordingto creature breakout how succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
