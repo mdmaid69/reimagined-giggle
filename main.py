c5d66b7566f160f1aafb7854ab8acd6c9b0c994a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:53 +0000

    finish demonstrate mail principle roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
