ee85863ff9da82980fc5f1e97bfc40b1f40abac8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:47 +0000

    dress free hurt money true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_speed(distance, time):
+        return distance / time
