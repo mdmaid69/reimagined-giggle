ec80065efb6a73dda648bc628b3fd8b7dcb29649
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:08 +0000

    turnoff better unless worst tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
