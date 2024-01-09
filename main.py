a70e2a50cdff3b7a69a41106b3abdad6a672c236
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:42 +0000

    hard legal mine force maintain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
