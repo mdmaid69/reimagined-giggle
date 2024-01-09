4cb0ef8a28347812d605da50a877cc3c96a29393
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:35 +0000

    pupil centre most pour bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
