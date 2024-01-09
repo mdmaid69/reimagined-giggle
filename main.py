2c8e5ce86f96e4fe9373a8c30314c74e804872cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:02 +0000

    concentration best control motion getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+print(os.getcwd())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
