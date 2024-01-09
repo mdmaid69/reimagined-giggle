096e15f6dd56e2e517721b00be18a0f7b57f7421
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:40 +0000

    improve kind behind follow makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
