006d3e8e04dc01a397df186dd009c425ec561743
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:38:27 +0000

    nice experiment objection typical complain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
