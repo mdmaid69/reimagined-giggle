0dcaa6f838cdd2f1cd5083f75c07de7324622ead
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:29 +0000

    payment introduction pity holdout roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
