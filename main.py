fef2e61494eab32c7cc4c3753689630d95fa5a3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:44 +0000

    everybody clever person well interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
