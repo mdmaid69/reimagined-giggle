89d5488d81d713c367c15c7e3c06701ce89b4d9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:42 +0000

    art reply getback call pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
