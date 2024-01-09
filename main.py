b400005750e5721788e4856ad5ecf35fe0896633
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:32 +0000

    key urgent steep empty makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
