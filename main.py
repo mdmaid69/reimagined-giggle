71366f44074d4c8b0e1768142437ff1de42add7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:15 +0000

    slight apparently downstairs collection ashamed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
