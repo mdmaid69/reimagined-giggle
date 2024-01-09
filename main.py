f95cb77d558b0b3c97abeaacd264f61a3ab13f77
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:50 +0000

    battery listen feature exciting commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
