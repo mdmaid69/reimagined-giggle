711336254a4c0a028dae5c14530222e6556bae60
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:52 +0000

    makeinto follow difficult follow connect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
