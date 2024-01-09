306ea2c035655234c69f05db66f7bfa354927a65
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:40 +0000

    pure use tear shade share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-print(math.pi)
+def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
