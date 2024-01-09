ab286702a19601f71ad588b6f59bb85608239908
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:55 +0000

    autumn handle fast brown fork

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
