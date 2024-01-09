84d98e9150b28d7af00bc275ebd92851f0b74fe2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:05:45 +0000

    square advice but Qq polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_gamma_function(x):
+        return math.gamma(x)
