6385ff4ec151503d2cfd55f074b5e8d1d68bbb7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:49 +0000

    authority same goodbye store fair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
