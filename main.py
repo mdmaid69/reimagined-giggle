a0320b05b8c908613a93c40f739567a741e17fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:34 +0000

    gradually giveback label maintain back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+print(time.time())
+import time
+def get_current_time():
+        return time.time()
