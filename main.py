7c175496b3ff47c8837ddfdc8a49a75020a3b8fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:53:09 +0000

    march throughout letter rest ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
