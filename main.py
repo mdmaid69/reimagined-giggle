d25001e19f49e568a3134fbafa696200c983488d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:02 +0000

    contact breath represent shallow copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
