3b146f0d0596cc99fb815cb55d54db491a362e2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:00 +0000

    suggestion exact floor sort worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
