00ba6d84336dae6830588ea50ca98b59027cec26
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:05 +0000

    reply crash know squeeze pollution

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_as_complex(array):
+        return complex(array[0])
