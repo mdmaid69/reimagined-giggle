71755d60450decc3105c1e4f45830719c8ac814f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:47 +0000

    direct pain centre clean simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def exit_program():
+        sys.exit()
