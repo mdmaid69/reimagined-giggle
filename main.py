3b45c5f8a1b8e60ee3a6e0f561098e6d771a2735
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:44 +0000

    impressed crash habit sound guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
