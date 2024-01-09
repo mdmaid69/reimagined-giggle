7bf7f957d93e271db23e81d49e25379c2b0e0579
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:40 +0000

    busy promise manner test refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
