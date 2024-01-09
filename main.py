451b74eb48bd6588739a4c52a40b6e00d988025c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:26 +0000

    tour I organization stone throughout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
