64b44fc58b303769e03fca08a4b9ea1be5f33eb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:52 +0000

    different factory bush eager keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+print(math.pi)
