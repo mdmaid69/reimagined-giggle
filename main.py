050e2e004f71a0f048bb89bf96c3ef656632f469
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:07 +0000

    environment crash noisy less start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
