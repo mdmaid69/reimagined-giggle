edb6242cd9ffe0c79d05a615e0c4d32ca0cf715e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:05 +0000

    same rough talkinto subject 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
