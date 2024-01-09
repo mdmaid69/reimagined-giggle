55e5dda9789d3de22e3b66ea2cb2de2fdbf78723
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:04 +0000

    perhaps like example tidy involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
