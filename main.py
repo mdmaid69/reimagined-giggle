30cb8e920b1a15ed0490d11f36a34811707c8715
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:27:48 +0000

    back Oo runover theirs serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
