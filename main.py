27aef8168434740ae17c4e67ffb81249dccca854
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:34:22 +0000

    male blow runout sight row

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
