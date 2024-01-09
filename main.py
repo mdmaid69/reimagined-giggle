0c8eb3d22a550c572a1f6abafb578152b51f742d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:47 +0000

    space failure burn tomorrow best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
