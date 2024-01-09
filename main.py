b7d77f39d9b48d565ad7e988c6d57d21ef86355f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:07 +0000

    score explanation smooth everywhere never

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
