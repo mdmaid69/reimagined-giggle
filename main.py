8f67dd901d9eda2573c28935e9297311c7494fda
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:11:13 +0000

    feel object depend wind you

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
