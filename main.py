1cdd147e91a0196efac1f373da8967a9e6343ccc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:56 +0000

    ride complete payment smell Jj

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
