6427acae5cfc7650051bff87f7e3ae079df5981b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:57 +0000

    people light sister fear complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
