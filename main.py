cc63c11037bc74625d2841b6f7379ce4624dfa4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:54 +0000

    cough job pain snow finish

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
