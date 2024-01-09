cd57798ac8470c6bcdf2c491b0e832e823518144
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:02 +0000

    once temper judge exercise software

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
+def calculate_density(mass, volume):
+        return mass / volume
