224ee089601cbbcdbfe260b96587e7cf0c64652c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:16 +0000

    hospital mean like plan call

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
