abb9eaf56a9607ccdba712f3adbf600f12501318
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:18:38 +0000

    public refer sick trap such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
