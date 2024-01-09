733f1e118efd309d292da9a1ba9ac343fda49d91
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:49:50 +0000

    tired disappointed floor waste insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
