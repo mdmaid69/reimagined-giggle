c109a3c58fcc546b278175068761c0c5e98d7afe
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:21 +0000

    gap community book abroad divide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
