0890c68575c054808a58d08f5759d704f98d297e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:36 +0000

    drop limit constantly his peace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def reverse_list(lst):
+        return lst[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
