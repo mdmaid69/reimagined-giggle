81abffeac7c95a2b8edb0387da2fb242ae3a5426
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:43 +0000

    tonight receive balance runover centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
