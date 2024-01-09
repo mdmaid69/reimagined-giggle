3c1b5b6ed00e4078dfe7ba7a3d4fe8f4b5460b98
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:33 +0000

    bet bit train advantage visit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
