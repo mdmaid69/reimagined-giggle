5ad2f33c8da45f4c79a50778197f224918a95cc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:21 +0000

    race thanks noise callat hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
