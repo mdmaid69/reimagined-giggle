10068068bcb29834118b33413a359c6c806d4bef
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:35 +0000

    set extra replace apart further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
