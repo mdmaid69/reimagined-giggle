fd0be47a5fc20c95387ea15fb72a2d55361c0e22
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:09 +0000

    lonely healthy Ii until common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_speed(distance, time):
+        return distance / time
