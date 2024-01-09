4c7f9dc04bd86d3f5a0109ddf57ec84b8cafafd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:06 +0000

    heavy dance child beside eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
