68c305ba32e475b8a3b1a2c81343c0f4a0a19abc
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:52 +0000

    Jj contribute style full flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_binary(n):
+        return bin(n)
