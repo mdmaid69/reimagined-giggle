f696a37f2860be9f54cc04fe8fbf564933ef249e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:15:07 +0000

    beside with occasional discuss forget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
