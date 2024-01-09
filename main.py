3393a2991760a8dae226fff336d7a92bfc506775
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:35 +0000

    concern scale usually exact circumstance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+import datetime
+print(datetime.datetime.now())
