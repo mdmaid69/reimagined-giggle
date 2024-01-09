733df7c49324688bb99493f9aca39eb0297df207
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:04 +0000

    reduce grade address loud fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
