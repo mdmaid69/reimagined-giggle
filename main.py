22acd2257085a910b6d5257590cad1efaab65b75
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:58 +0000

    beginning action himself upstairs own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
