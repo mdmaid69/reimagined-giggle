e6946c1aeda5f5d71e146bc35f70d9b10c362190
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:14 +0000

    environment lower doubt name partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
