c488c26281c3f67344658b4554428bc901836abf
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:13 +0000

    prepare accidentally judge contain colour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
