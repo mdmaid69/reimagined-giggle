8a77301140b5bb656a0b0dc1a7faa6f195ceb5be
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:10:55 +0000

    sign send politics stiff closed

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
