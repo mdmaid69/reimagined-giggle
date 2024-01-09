1c69840ed3f5ad3a6cf070cb10369a0428162162
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:24 +0000

    eye unlucky crash exactly worry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def multiply_numbers(x, y):
-        return x * y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
