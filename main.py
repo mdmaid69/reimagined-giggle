6f898fca6292287bccca7c675f072279da65f6cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:10 +0000

    first makeinto kind middle Uu

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
