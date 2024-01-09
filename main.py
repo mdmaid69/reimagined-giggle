4ba0bda913751e119d34bc900058f0e2274e5838
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:06 +0000

    meaning match also deep comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
