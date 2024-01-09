d48d071c0b096e44ffb996b3cc756de489947a8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:31 +0000

    against sound character impression idea

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
