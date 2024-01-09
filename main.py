de700bd9f5e769c64a97bf733ca78c2d7b5005a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:12 +0000

    climb fold into question hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
