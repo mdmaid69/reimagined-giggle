c927eea338fb2c4e307c05a7a39e6a2de6993c7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:36 +0000

    regional Ii equal other simple

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-def convert_to_hex(n):
-        return hex(n)
