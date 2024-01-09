7f9e9c3cd626d18607120713cd7b33d023cf0323
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:14 +0000

    certainly strength comeoff impression poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
