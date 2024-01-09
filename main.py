be7ce5192747a987956842a04855644dc04a2e3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:07 +0000

    opposite train material touch food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
