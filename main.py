6c02fa180fd0f204f325304caa8fb843f22afab3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:09 +0000

    fight responsible maintain sit hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
