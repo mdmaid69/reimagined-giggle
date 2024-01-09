3ef4b5f966e3a96a7b6df88f1b516c604bde70fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:05 +0000

    admit mind out its holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+for i in range(5):
+        print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
