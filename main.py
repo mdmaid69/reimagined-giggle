c3d7904cf32ae8998037e8ebc9b1eb5ef9fb11f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:55 +0000

    practice he bin tour touch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def is_odd(n):
+        return n % 2 != 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
