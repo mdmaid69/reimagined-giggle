0e63a0ec06ffebb86bfbb5c5fe3a16b78afdb8b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:04 +0000

    geton occur original supply practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def is_odd(n):
+        return n % 2 != 0
 def calculate_power(work, time):
         return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
