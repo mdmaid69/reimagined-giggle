39fc39c81e791a1a33fdeb8f647f8132b6cd95a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:21:22 +0000

    away tune suppose wipe bored

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import time
-def get_time_since_epoch():
-        return time.time()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
