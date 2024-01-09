cecdbdbed4f834fdfe21129864e491733c0908a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:48 +0000

    trip range ours prove hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
