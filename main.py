36683c76defecd2845d465d832d1cb89a930ce69
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:48 +0000

    trick off naturally guilty raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import numpy as np
+print(np.array([1, 2, 3]))
