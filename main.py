66725a360107cc30ec6c11c16543aa3ea9ce6181
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:34 +0000

    relax whoever belong bet willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
