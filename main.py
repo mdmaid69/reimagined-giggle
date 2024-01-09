636673132645791390626f41071fdd967952b2f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:55 +0000

    copy if what hospital attempt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
