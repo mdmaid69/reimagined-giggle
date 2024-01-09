783f49aaf5c5788d2a9895fc7365fa9ff7dbb03a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:56 +0000

    regular stare gradually form increase

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
