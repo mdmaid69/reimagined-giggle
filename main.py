0f95aa3092f916b0dc27266eaaa4fdbd9c007394
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:49 +0000

    politics mother wind zero model

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
