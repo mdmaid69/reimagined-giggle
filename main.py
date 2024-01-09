135ebd6d2d84fe5e79345068b6a11f72a429323e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:33 +0000

    failure small I shoulder through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
