226a95d5e11aee173d5a74113628e6d330e86fc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:17 +0000

    crisis whatever nothing above to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
