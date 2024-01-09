678de5b1adae61275da24867c7931ef1251aca0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:34 +0000

    tap card future live basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
