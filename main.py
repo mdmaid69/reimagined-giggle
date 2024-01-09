bda53d3d9437be3a057c11ac81ca4d010e6f9e57
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:20 +0000

    turnover 564encourage band comeup swimming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
