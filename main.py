9c8804800f460055135eb8a342e3aeff12bb1a73
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:51 +0000

    work ought invitation pump go

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
