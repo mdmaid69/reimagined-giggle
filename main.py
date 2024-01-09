f64a0b09d7c0e2bd7281e8606ff6caf6d0efdb38
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:48 +0000

    beyond achievement once minor raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_current_directory():
+        return os.getcwd()
