268cc83dad5bca06337aaab1d9b17f73179bacde
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:06 +0000

    steady shut successful bet deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
