4aa2b0bffbfd005bdfbc3676daa52fb6fe26d6b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:41:07 +0000

    dry rare deliberately trust story

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
