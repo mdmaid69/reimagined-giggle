14b0ceea77485f84bf29fbd16ee75f4941d084e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:26 +0000

    though thinkof nowhere trip leave

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print(sum(range(10)))
+def calculate_acceleration(speed, time):
+        return speed / time
