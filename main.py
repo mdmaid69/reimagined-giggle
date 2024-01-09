561a09a72f69cd9929779263d7eddb66d974bf76
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:37 +0000

    window nerve agreement stiff pain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
