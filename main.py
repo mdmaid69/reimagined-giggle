e9c6f5525dbdd7b278cf6c9a2a04f2b08a3d679d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:53 +0000

    once time shock forever shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
