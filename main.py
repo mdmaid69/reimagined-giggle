328abdb43a4dfff941a2e7b5b1afd8465bb1f70e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:48 +0000

    temper least recognize callup form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
