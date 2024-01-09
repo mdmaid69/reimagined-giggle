e6c6c853b75bed2ed1fba7ad2f1f53ab4994cbfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:15 +0000

    port convincing cross character fasten

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+print("Hello, world!")
