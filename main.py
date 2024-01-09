9287df2e91f470c00281aa8b06bbbb63b08e9b8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:28 +0000

    team wrong hot protest hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
