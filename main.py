66a5e9eb0a0e6f51dff9b089ee3b21283405470a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:59 +0000

    message scared react needle yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
