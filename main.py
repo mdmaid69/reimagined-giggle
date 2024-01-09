062eb1f4d609ef2125cffb2a8f3f22a6ef1ea648
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:31 +0000

    wild vote fat rush earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
