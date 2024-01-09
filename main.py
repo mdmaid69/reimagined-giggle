076a528dd5a0cb4017f26bd2e5ac2c581dd58ce1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:39 +0000

    splash send beat neck obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
