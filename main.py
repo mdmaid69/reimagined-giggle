40c08fa11beb600528fef199a1ce542b0a02ebfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:39 +0000

    thinkover Rr medium lot sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
