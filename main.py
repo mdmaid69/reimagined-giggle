1824b3f43a66220fd49b47fc860bc285b998b41b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:11 +0000

    deliberate claim trade takeback comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
