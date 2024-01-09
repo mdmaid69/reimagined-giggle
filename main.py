61a73f43720a567befbd5a4ad18170a7d93f8e20
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:37 +0000

    fond explain watch nice become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import random
-print(random.randint(0, 100))
+def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
