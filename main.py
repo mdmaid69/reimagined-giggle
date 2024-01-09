5b51d6461fa5597725caa605724a9f7fb38efb00
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:54:37 +0000

    load hard hang serve idea

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
