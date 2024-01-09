65f0bbeb3fe48f3f8a1095741f91d6448b128e85
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:09 +0000

    treat tip character common anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
