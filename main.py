cc315d27143879c7f47c44b464d67807e12ddcde
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:26 +0000

    national clue sore late fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def multiply_numbers(x, y):
+        return x * y
