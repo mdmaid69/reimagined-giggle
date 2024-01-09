c66815c5b75922929ee7779ad7382b9fb81e13fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:50 +0000

    makeup clear prepare listen walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
