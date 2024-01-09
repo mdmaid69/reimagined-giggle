ed4c472e7734f89560795b56f67904243298fbe0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:06 +0000

    mess region clue measure imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
