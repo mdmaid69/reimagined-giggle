9b62af6645af581fb922a110be1539726289e091
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:43 +0000

    disturb hair make ilovedicks Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
