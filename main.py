faab2aff8b260496e262b54bacd1789ae0f67c0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:14 +0000

    distance often sound late repeat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
