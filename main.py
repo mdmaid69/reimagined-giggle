b19fe009c5f98bf3abe96cd0fb5f2240dfecdfa5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:21 +0000

    course leave question deliver content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
