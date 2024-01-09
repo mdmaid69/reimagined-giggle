afbf6060b5aa4583d1b0396b8df0a46d858cdaa9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:42 +0000

    fat guess expensive base operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
