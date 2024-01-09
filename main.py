00ebb9df0b273bc4e28d500fbc7a2f90654eb1ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:08 +0000

    somewhere best upstairs evidence limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
