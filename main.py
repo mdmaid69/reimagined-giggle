ba6cfd8ea3e867267ea634e171899a30fb38dbc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:05:42 +0000

    very opportunity minimum waste situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
