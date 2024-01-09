3abb6d2651355f99544b4c9e626e5409285e626d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:11:24 +0000

    ready safety remain declaration side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
