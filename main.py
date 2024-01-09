975acfb4de58d7c121581b78bf08803226ca5d8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:51:46 +0000

    strong rarely sure criticism accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
