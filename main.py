f540babc32101ea6aba07765fecef42a83aaf20b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:22 +0000

    recover before past hers benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
