f60c282db2a093793e87162d4b6974a9e738be8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:25:27 +0000

    dig radio concentrate fit university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
