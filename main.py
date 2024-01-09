5673595fb7d8f73877e8c1e3087e4a1ddc964851
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:19 +0000

    Cc cheek dish keepup holdup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+def square_number(x):
+        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
