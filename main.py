ddadfcc5dba91ff72c60e64dd5df9559d2e6699a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:39 +0000

    mad test full natural splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
