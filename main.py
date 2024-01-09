ab726ba5e35b7729ab349bd706851cad2155cb54
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:39 +0000

    promise tense treat couple know

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
