385ed911bd9f7d084133dd354d609232e8bbd19f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:52 +0000

    likely choose large spring stay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
