921f05dbeede19c9a9bb57379c6d4cb5f142a0e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:15 +0000

    begin base especially winter Hh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
