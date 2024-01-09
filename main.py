b3c40fa7c14295bd6f68fef202671cc96b9bf339
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:13 +0000

    shop dress protest trust tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_length(array):
+        return len(array)
