ff21f31d304a195beb3f66681558ba55eabb5ca1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:01 +0000

    trust callup famous frame yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
