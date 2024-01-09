fda68aa4073dc6824a8bd64fee05ae31f0ea55f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:44 +0000

    taste injury behind approve comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def convert_to_binary(n):
-        return bin(n)
+def add_numbers(a, b):
+        return a + b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
