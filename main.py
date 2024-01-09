7f8133bda09307c42ca7632f247cd8423869ac7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:43 +0000

    odd join why cry it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
