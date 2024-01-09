eb83af3b3b5bdb4d5fd1a1a77ec073953ae3b6ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:36 +0000

    question angry wise delight thing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+for i in range(5):
+        print(i)
