0615a760623e83c9e4b455a237d2f2e4466a11d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:03 +0000

    suddenly law apart shop pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
