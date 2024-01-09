cbd3676701c781b29d468ac12dcd51e8ca68292d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:47:58 +0000

    strike tidy yourself work deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
