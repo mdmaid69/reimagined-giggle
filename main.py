5e82ce4e664b73c113131e82b529422e4cc46865
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:14 +0000

    limit thirsty active punishment I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_array_length(array):
+        return len(array)
