42b356281cb7d6101fe9b158f0aa28de9632e10a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:31 +0000

    share experiment leg exaggerate too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
