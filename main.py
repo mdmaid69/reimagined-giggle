817494bc1b6e243893607d29bd0c7a753f7d918e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:07 +0000

    empty man declare spring left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
