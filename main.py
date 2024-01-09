8339ac3b939108f20ef13de04ae48cacf5ebb38c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:02 +0000

    lead gap transport loan quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
