3cd24b86755ff87e783cc538a54deb6ee4385ce4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:42 +0000

    recently unfair trick together its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
