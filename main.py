c4429aebb356cc2bc52d5d015c665c7eb165a4dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:33:57 +0000

    on violent light tense dangerous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
