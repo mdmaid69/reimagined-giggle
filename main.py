21d40eb3f7f4d8712f2f0aefd3a9a259f1fcf67f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:22:45 +0000

    mean accept play die excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
