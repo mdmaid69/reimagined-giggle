54fe515902ff72ed30b2b459cc1373c7566e37fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:13 +0000

    please wind clean to group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_user_list():
+        return collections.UserList()
