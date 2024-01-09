48c48237ddb8d15713d3c76f781ad1cb7ae408d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:24 +0000

    side half fix reserve false

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
