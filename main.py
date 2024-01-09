1541abde8d0e87e7c7cb61371e5d65aa22a0f7dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:27 +0000

    appearance physical light man ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
