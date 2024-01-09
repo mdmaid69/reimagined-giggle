2d3bdbae362061c6f0b86f78b3402c2e6407fa83
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:04 +0000

    machine plan address put raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
