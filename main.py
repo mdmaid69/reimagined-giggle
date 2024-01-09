b50089ab51b69769dce2890119d41b0fcc77e898
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:56 +0000

    deep wall speak forever game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
