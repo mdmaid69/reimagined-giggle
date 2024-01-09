1ebc44dc5ad43a3c5a76298b9e374a5884e6a90d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:29 +0000

    breakinto failure size partner main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
