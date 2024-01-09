50bfd393c4f275c3d2d17e948e8cde35fe754006
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:18 +0000

    half long understand importance avoid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
