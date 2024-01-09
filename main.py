f5543a280841302f351e5add9db590e2cd48a2e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:35 +0000

    neither familiar safety rub outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
