64c1d7f0a4044308836a14fb09d15a743fa2fb42
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:15 +0000

    nice smile will act important

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
