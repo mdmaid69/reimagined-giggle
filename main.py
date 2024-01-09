7b343fa54fa681f51d2e9b008fc4e91829e90801
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:10 +0000

    maximum watch search neither steady

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
