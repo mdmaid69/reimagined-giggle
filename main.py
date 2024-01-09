7c183076aca94bf187a3fec403a2d6dc9df14592
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:57 +0000

    introduce care bright progress turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
