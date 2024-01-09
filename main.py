4dca438e1a9fa7d22f1e8f15e5afab490a91b26b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:39 +0000

    mine refer now unable address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
