f2e857a1ed43584a3bb3a6b0d6ec44550634bba2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:43:24 +0000

    sort shoot cook peace spring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
