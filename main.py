abe8a2b243dc17b258ab624b20b2e9795f860e65
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:18:05 +0000

    no discuss arrangement winter authority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
