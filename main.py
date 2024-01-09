d0ad1a376ba2ec91d3f3b17e89c4340ac514a902
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:50 +0000

    sorry claim damp sleep raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
