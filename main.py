46b5155ee19d7fcb67e4b1850b162231bc3542c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:08 +0000

    top look rough crazy jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_array_to_string(array):
+        return array.tostring()
