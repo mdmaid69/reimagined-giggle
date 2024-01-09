74007eb65b562875e0632a055030ca0cfce66967
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:44 +0000

    estimate needle simple certainly unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-def is_palindrome(s):
-        return s == s[::-1]
