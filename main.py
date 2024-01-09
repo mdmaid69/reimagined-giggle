b53295703681243077174ca1bda6ac414fffe8e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:14 +0000

    lesson relief reduce although level

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
