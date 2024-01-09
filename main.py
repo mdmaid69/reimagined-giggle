ed375be1b2b3305ce9d0faafd16b8fc6b22b43cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:42:38 +0000

    bury afterwards reject very respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_current_time():
+        return time.time()
