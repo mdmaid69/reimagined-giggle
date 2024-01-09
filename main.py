bab761dba609c4d49ef27c4a6ed72878e14e24a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:18 +0000

    until slightly emergency regret union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
