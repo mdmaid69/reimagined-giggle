418804a6b69958c9040c53a4ec74cebfc6fe3be5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:16 +0000

    holdout innocent direct wise copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
