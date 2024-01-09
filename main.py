5458630a41169d5d3caf6ae08cf47c3162df2313
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:04 +0000

    favour ought computer complain create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
