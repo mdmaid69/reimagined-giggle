d7c586c12dee0ae79f14af3471619c809a2fbc5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:12 +0000

    quantity behind dear outside myself

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
