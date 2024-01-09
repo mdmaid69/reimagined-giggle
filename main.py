6f3bfa04d5d287dd3192e691084c8c28937ed317
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:43 +0000

    ahead cruel there congratulations fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
