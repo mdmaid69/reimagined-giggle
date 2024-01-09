7a782b69f409b8caf1121973f79ea6f28611a14e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:33:49 +0000

    underneath Qq means sweep damp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
