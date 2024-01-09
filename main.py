1ff478ac5a11e7f731e84fc052fb5101cfb78d75
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:49 +0000

    achievement support serious drop run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
