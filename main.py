e418d5e6b06c390d58ebaaac9e4c68d7ab1c19c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:40 +0000

    woman everything outside gather duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
