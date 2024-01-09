5ef952fc6bd608b1c2a40b61dd063b276ee39a34
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:54:48 +0000

    steam by pronunciation reasonable leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
