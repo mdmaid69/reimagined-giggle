04aebfcd6fc9ab1e9441681e58432c0ef8acfb49
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:57 +0000

    forever also wrong low run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
