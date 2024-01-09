dcd7fb19ed15670957a0b2aa76b6f06bbc81fa72
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:25 +0000

    pure together takeover consult contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
