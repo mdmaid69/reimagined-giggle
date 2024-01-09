eaf7a20ae85e9a8ffde13f55f9c78b32e51605c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:43 +0000

    everyone once division public remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
