fb6b61a2d97c0993ae6732d65f2c76f7837c59e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:20 +0000

    tidy fail beside runinto evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_pressure(force, area):
+        return force / area
