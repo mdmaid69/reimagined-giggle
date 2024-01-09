d6fe9a8ed81d1e888646fd6de14525e3653dd4a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:51 +0000

    keepout paper perfect bye sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
