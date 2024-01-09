7762a831866eab3068e23554bb1e8a7ac0375059
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:41 +0000

    tall past lost cry inquire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
