4a1a4718a169ae829b11fc188b30366950313369
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:01 +0000

    play delicate claim turnover either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
