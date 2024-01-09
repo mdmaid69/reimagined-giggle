6e200484240495b403d27be46be4c132e2527c94
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:47 +0000

    who mean spite path concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
