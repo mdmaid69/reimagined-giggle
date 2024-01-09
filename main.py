06957af0e52ac9056be87124a4bd0dc7eeab3292
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:38 +0000

    file false achieve note tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
