60f51c883c7e64b20c7492c856bccb6ac92b3cb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:39:39 +0000

    perform smile breathe war office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def is_palindrome(s):
+        return s == s[::-1]
