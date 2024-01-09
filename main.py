675469d8cbefd5a96581daef82bc0cfd385a8ade
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:00 +0000

    snow statement clothes hurt advanced

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
