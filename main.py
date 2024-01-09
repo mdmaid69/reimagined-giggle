7b802e24b3358e0413df95ba34a74147525ee7dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:39:56 +0000

    tax call emergency shop shape

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
