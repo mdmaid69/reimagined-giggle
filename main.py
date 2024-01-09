41cf2d20bb8bf21ea7b5e4064930ecd79b98ddee
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:45 +0000

    treat tell shall secret friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
