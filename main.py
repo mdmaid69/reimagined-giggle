832cde0b74409d1e1bb9505bf08425b50b70d00e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:48 +0000

    false equal exercise win fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
