b12f1646f9f3703833a40069f10b049572a0c3c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:52 +0000

    near face mean prove offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
