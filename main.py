6c8006690c49bf3d4a3a278a102e5b777f937be9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:52 +0000

    space care quantity sky town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
