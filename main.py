c8f9b7683f0ddb7856ee7221bfda7ccd125c0c89
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:16 +0000

    shock healthy freeze human eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
