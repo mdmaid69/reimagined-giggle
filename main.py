6701bb45da8af038ddbdfb3ae1e5ec5cc9f60688
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:00 +0000

    clean regard speech transport bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
