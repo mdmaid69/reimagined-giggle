17b1cff9617153633126d0c51a8369b43eea22c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:51 +0000

    bound deliberately anything habit single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
