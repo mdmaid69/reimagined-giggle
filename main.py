ed3e4231331b910b6db1e1f6fb71d40b7366bd1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:32 +0000

    bottom refuse along official finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
