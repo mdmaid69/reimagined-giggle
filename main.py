8ea7064f0cab4fbac8bcf97e4c07896bff55d645
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:01 +0000

    unit question especially let getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
