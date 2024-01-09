226cd4cf9e0fa6ba39d53cc9f67f00ccc56af0f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:33 +0000

    trust doubt tear part many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
