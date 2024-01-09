fc041fca234ed06cc787c671fa9315d6cc39123c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:43 +0000

    national exam any appeal become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
