f2b7a32def7b984b4884a079a4340b32d6a0ce01
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:14 +0000

    give star mention listenfor coffee

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
