5c7a15c246e15392947e91b0013ea362954e6760
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:03:11 +0000

    challenge rescue pretend public disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
