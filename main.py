4da09e0030a4f206828b9bc4d588c969ae1209fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:19 +0000

    actually few intend train stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+def roll_die():
+        return random.randint(1, 6)
