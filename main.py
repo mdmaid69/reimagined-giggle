ce42ebe391aaf561336b91d73d1c24d6d217d1b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:57 +0000

    getback frightened or debt pull

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_set(array):
-        return set(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
