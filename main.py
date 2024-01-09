a3b7419c4a376b91a782b1e104fe378633a89423
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:24 +0000

    commit wire might wind spite

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
