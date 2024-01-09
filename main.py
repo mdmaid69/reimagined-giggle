b5ff153da7fb0cdcdb50dfedd3e4a74721dc18ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:54 +0000

    mistake cheque tend sometimes reply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
