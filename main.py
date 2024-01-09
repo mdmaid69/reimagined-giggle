ca74a2d8fa81a4cfeb00e23811f44cd386f38c2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:57 +0000

    physical bury content sense bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
