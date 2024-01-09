e535034b123a2a70ab13ca206989377cf12fa0d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:42 +0000

    Qq sign sorry eye period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
