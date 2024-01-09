c96684dc93e1ba3c6f982b427380d18302d720dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:39 +0000

    must themselves reject chat otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_union(list1, list2):
         return set(list1) | set(list2)
-import collections
-def create_user_list():
-        return collections.UserList()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
