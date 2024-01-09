64a2c868e83a7b911e9ef8d6a1c5f7faad70a5cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:02 +0000

    congratulations than ear shoot example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
