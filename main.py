3cad30bd29b39a7d6722baeca13a7437cc0a03fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:47 +0000

    warn bringdown straight bottom ashamed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
