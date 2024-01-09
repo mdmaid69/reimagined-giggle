36616976f7535d361605d0b330f5cc463d798863
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:54 +0000

    date borrow practise disagree block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
