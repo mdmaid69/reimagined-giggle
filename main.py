7a044dcffae6a70a1317b01f92c5ae2aef2e17d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:49:26 +0000

    apart sure direction circle skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
