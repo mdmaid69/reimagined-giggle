4e04613c8385022d745d3270b5ec52cc3a5a71a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:50:21 +0000

    poor freeze steam explanation heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_item(array, i):
+        return array[i]
