1600756794eac41f25456cc7f0f2edc2a309b74e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:19 +0000

    idea indeed extremely sad bus

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
