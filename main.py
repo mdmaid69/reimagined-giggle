1e8969fc70b6ce553b5a07cda0388bb3d33d4c36
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:13 +0000

    but ticket pretty pain thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
