59e61a74cd6e9900b5792aad8a7b6afd2787753a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:19 +0000

    weather suit intelligent mad print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
