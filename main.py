c70671e1b1659a5c61ffc0e01e9672ce0ef9dbed
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:01 +0000

    muscle everyone stick accordingto less

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
