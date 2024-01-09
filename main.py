bcbcd0f464e2fb862234c601236ac7e8dfde6a33
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:17 +0000

    Qq in effort make criticize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def divide_numbers(x, y):
+        return x / y
+import json
+print(json.dumps({"name": "John", "age": 30}))
