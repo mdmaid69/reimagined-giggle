1679be5d2ef5bd8e992b7235e94065141c539092
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:53 +0000

    black touch it question gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
