5b15c248811c2cd3367bf1703057abdc7047ecd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:04 +0000

    smart unlucky owe forward length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
