0292d9905d23361b18493c5df1e9d2f43ac8545c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:20 +0000

    edge him special quality almost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
