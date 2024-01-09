bb0d171f804c5757007b48593ab75f148b6a2621
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:57 +0000

    many plenty admit plain little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
