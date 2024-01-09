70622518f2a6230757e3381aa51ace2d15f77273
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:59 +0000

    creature offer artificial lead fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
