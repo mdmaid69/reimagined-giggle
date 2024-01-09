945572204819f3d96a883ebf1b6efcca723c239e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:14 +0000

    behave central keepoff finger confusing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   def remove_duplicates(lst):
         return list(set(lst))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
