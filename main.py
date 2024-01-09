c67688f6e78cb1eda2bd36b2a9fb605e4353cdd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:40:58 +0000

    vanish traffic breakin shall Bb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
