6a38440e1daa75326f743c15cfed1a743fbb3a9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:16:07 +0000

    frame silent breakthrough sound train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
