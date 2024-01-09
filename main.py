8ced3835b54adc177c86cddd73d7334d074cc83d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:32 +0000

    so opposite unfortunate fast compare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
