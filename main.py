bca5aa58f5628101b46b60d9dd55270d2cead66e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:21 +0000

    exactly hurry quarter bear strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-print(datetime.datetime.now())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
