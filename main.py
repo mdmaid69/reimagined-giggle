98e059fdf906fdacb2af2124a42fca9acf3e213a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:15 +0000

    slice breakout dance set disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
