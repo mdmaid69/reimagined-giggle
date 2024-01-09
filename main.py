d67c5bdb433b9229624025c229d0f75e605755dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:07:59 +0000

    thing over unhappy since probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def greet(name):
+        print(f"Hello, {name}!")
