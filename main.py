9a5f1bc5223f4105a886d2f8117f5c86ff84bf09
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:27 +0000

    bringdown happy us unlikely fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
