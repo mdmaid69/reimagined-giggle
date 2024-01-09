aa488fdd320068fd31b9c0bd82a7e0b3f0cf4c27
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:25 +0000

    careful used eye carry repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
