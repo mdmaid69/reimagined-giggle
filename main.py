4d9132e9693381017ffce813ee2b4dfce71b1e62
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:27 +0000

    word petrol unpleasant secret agreement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
