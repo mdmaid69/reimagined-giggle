936a6a18ecd5aa98cecbf28290a92e55ad243845
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:55 +0000

    correct lookup confidence thank understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
