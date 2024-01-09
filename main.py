32134b451ea9718bb50ef72e257f136063a86e75
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:07 +0000

    strict accordingto straight he base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import random
 def generate_random_number(start, end):
         return random.randint(start, end)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
