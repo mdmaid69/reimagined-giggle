e57768d4e7d0b6ac6f2b9888b7cf7ce5a0bc36f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:49 +0000

    criticize pretty ticket drive fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(5):
-        print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
