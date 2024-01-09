961de555693749f5c1e0781a8ab0b82fa65c816e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:45 +0000

    scrap line bar fear reasonable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
