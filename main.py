57d7d426c7b3017e922b4468bfc12ae5cab0da82
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:19 +0000

    read criticize invitation unkind comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
