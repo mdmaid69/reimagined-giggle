bec95208bad09f5be9ed47feb794c91d413e1aa8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:13 +0000

    fault cost government answer bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
