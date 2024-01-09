357494c2a4a16e01d4e8f75c56bc6f0cefcd3b3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:56 +0000

    fire frequent second rid once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
