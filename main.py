2ea9ccb8a3439f9a5d6f883d483448efd309702c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:19 +0000

    hand house not do practice

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
