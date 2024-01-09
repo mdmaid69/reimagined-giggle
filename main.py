6caaef62d02e4bb7517690dfb7ad25025bf1624e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:31 +0000

    turnon taste experiment common greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
