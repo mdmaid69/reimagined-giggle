7cd2b364554f61d27d568741e51d0ce86d0722fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:17 +0000

    smooth miss Ff relation success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
