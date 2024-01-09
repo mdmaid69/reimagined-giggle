bfc3479d73b2ecb5d67508dd13843fd0659eef81
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:56 +0000

    mark correct behaviour combine between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
