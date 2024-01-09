9eafc6943b3314c26899ef1f887ef8b4e1f35155
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:20 +0000

    Kk clear crowded leave gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
