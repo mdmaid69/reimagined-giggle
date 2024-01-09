ea342e5f11dac06ef4967eaf3a78c962ed5346d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:40:52 +0000

    concern modern past find callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
