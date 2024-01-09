006084b3bdd62e6af8462a34ad247e53c7aa6894
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:41:43 +0000

    successful confusing dead innocent good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
