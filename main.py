fc5f8e14b5b6961c31635ebeaf7ff0463b186437
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:28 +0000

    right than goout personal wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+import sys
+def exit_program():
+        sys.exit()
