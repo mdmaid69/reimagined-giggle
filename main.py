3c6c11865a296231ed75fad5b49dc8bb80833b46
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:49 +0000

    tidy float climb unit scare

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
