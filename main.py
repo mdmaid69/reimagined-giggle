a6a976691e030b6d8453e34ec9b8030660728f13
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:49 +0000

    interruption excuse influence occasion tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-print(os.getcwd())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
