acae3ea7ca9e30f8a762f77b6253bea3215cf7ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:22 +0000

    chase even quarter science down

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
