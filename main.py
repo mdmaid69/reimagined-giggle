4eeb1a7b80da2dd27f19c71349a401c8c1eb548f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:45 +0000

    unable smoke match total bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
