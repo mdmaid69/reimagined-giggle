e1b2080bad86b77a5599382ccd604529c738b8f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:03 +0000

    silent grow bound rain later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
