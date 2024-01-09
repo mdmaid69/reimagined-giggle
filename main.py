c5cb62a005b0e29ad3388c99c5340bf0d659c8cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:34 +0000

    serve place smell happen my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
