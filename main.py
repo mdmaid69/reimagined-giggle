a0a74034b02412b097578d7a504a27c4e2392e26
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:13 +0000

    yours usual hollow thinkthrough have

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
