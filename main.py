2034cf5ed1cd81ae69bd3b4c532227f6ba078795
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:54 +0000

    from lie mark announce add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
