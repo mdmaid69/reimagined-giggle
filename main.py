b2bd57a2210987645a648c948cabe3d16be7db49
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:33 +0000

    mad combination marry cough narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_float(array):
+        return float(array[0])
