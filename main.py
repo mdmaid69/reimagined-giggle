d081036140927ed66176577b476f4fc51f04643e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:46 +0000

    point union noise exercise post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
