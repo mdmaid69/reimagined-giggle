ad51b06323f61085e3675a58bc7b385294ae6562
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:01 +0000

    brain underneath communication float lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
