c942dba3e6edad151f88cb18f2aef630b42b80ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:30 +0000

    bin gradually sore odd mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
