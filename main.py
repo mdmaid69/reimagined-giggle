50820768d94f5de34f84cb24cb8d2f40ed043e0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:12 +0000

    argument post present exactly increase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
