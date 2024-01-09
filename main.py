a6139af0acf601234937d816a46ede1bd8f06279
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:49 +0000

    food century mine broken culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
