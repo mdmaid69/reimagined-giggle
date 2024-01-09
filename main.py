a6c70ddec9a4564ce1cfe061a03288a3c5f698c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:16 +0000

    period exist squash brain stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
