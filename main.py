68776d5281f1a74844288a42f56ddecbfbd5a984
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:45:28 +0000

    comeoff cause feel weather realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_list_from_array(array):
+        return array.tolist()
