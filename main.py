9c8812891925a255c380f45b9bd0c6668c7a11a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:19 +0000

    goon evidence taste possession achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
