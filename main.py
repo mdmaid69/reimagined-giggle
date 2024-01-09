a254117c29d18eb4bb93d859b1e32a3e3863994e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:21 +0000

    married walk cold key equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
