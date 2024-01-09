0f5c6e6375ecd3ab661a6d7cc4e775f171da5e74
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:09:31 +0000

    Tt fairly plenty independent careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
