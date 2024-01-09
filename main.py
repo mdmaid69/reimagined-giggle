b70efa6fd4e108b751bf90d35ed1fd80dabb1e55
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:10 +0000

    notice almost number without relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
