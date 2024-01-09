b662bcd5d17eae1edd4c3f8adf5dc4b6524fe12a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:17 +0000

    grade month do approval familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
