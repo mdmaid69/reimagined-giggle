56bfca008ba8b944ab46737bbf31ff80af7f5e03
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:19:33 +0000

    turnaway lean sign exchange stand

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_words(sentence):
+        return len(sentence.split())
