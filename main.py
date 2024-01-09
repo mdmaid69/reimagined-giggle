60fa09a3eb9f57b99be4ca88b0447a1b6be1ee5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:56 +0000

    distance turn close nothing instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
