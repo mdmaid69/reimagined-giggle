82cd67e17b4abb0fa75f63483ff79e699df0be7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:37 +0000

    before late remove hollow wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
