cb308dc216eae7afc383153acbe4d199d4664e38
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:40 +0000

    otherwise tired provide worst string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
