f8f3c4d50dfe31d6085257500e996ba64e92a107
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:13 +0000

    left only gather concerned desperate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
