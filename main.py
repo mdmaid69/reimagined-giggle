81d1a667c38cf86aaecb36cae0e8ad632cbe7a34
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:51:48 +0000

    good friendly raise me notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def is_odd(n):
+        return n % 2 != 0
+  def subtract_numbers(x, y):
+        return x - y
