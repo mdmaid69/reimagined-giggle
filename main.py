43dbaacba8ba6d93d8136b564ebd43a9cf241451
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:44:05 +0000

    estimate watch confusing left without

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
