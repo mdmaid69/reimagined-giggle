1726b05326693230f053ed93fb9282ab6f964563
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:04:56 +0000

    instruction eager ready soft temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
