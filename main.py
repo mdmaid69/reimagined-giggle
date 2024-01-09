709b87923a89ec8ba7c4704e1597b6736903fd38
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:31 +0000

    runover cinema centimetre debt female

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
