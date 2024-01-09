8f0ef4f1e86dff32a794a142176300408145aadb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:03 +0000

    inside Ll obvious watch war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
