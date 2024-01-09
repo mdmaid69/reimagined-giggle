a45079270c5e042a5e1b8ee3dd7f92feb3a290d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:30 +0000

    shake relative communication pressure Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
