d947e41394feb05e533fd35264baa7fe0288a40d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:16 +0000

    murder hurt it scared smile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_density(mass, volume):
+        return mass / volume
