fdeff9cfb32c8523f5d0112f5a605bf8883cfc6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:42 +0000

    information reject steam any discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
