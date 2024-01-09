3a920d02b4e5b15e4bce239bea22e6de5aba864e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:05 +0000

    factory only cinema clue protection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
