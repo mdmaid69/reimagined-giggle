2173406c7450bf1222b50f53ab6dfdd7757171e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:55 +0000

    fresh yet replace fetch tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
