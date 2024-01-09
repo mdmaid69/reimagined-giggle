cea059a0d991ce67a193d4f7e148a3669bd949a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:22 +0000

    ready record open turndown them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
