8947895ffc76e24630971ab49e3b0b727c94f5a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:52 +0000

    memory Ll Oo obvious like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
