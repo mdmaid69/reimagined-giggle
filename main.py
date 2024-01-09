762ba4faa493a856c576fd2a262cb723a1da385e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:56 +0000

    violence distance polite foot uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
