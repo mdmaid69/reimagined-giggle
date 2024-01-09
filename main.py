ba93098b2dc21a7d1131a4d3addc3c3ba72bf696
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:55 +0000

    573entertainment however smart loud distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
