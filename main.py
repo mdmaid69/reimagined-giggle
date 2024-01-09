f44da0f25f4a17ee850c5a4b93272aff367739a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:48:39 +0000

    interview attention will before anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-print(random.randint(0, 100))
+import platform
+def get_python_version():
+        return platform.python_version()
+from collections import Counter
+print(Counter("hello world"))
