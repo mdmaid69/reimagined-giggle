9f77b2569312230876429b86f3693db5e08a6dd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:08 +0000

    food lie 179bite ready central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
