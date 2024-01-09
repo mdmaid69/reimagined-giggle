7c814b7ad8e7dace69f1bbe0900a643e07864685
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:09:39 +0000

    thirsty responsibility agree ground workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+def multiply_numbers(x, y):
+        return x * y
