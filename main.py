b63a6cd4b00ffbef32b3f56dc695defc37cd08d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:36:25 +0000

    study far brain mine vanish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
