b4fce72db987c4d641c4ff8942912dca54c9b245
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:32 +0000

    excuse accidental area part chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
