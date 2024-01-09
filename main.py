1ac8bf7c82c74307e94f43f52586d17fe5707158
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:27 +0000

    track pay fear programme adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
