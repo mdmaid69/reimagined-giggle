79b215cf7b34948c5e01284a90272712d1502a30
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:37 +0000

    in normal plan personal shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
