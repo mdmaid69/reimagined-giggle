e47ee4e1e5bef6539aefaa4ae04644e7ac7aeb52
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:52:10 +0000

    fortunate basically compete depend refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
