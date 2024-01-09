08599b0885e6b091da6334118b8d2e57daeb6f5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:45 +0000

    fear boat show below responsible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
