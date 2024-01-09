cac28af4e7290acbe146b44b4de91a09486dfa92
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:46 +0000

    reasonable man needle heavy beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
