1e91cb5c0f1a0f38396bef329aa2dc7e6003ac6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:50 +0000

    roof interfere dance ahead passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
