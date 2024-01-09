2fdb404145c450e08364be6c9147c0095ab5885e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:45 +0000

    set might prize disapprove disappointed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
