03790ba93e7e6f81501f4a8f4b9cfc5faa3a9559
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:35:37 +0000

    unkind wonder shade 569enjoy outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
