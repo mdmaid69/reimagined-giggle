40bf647c2306f419d4eb2efc5459eb1f58bca151
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:23 +0000

    simple serve lend wear except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
