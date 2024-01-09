f807871e4323836c8e49051801a29d21ea604bf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:48 +0000

    evidence statement yourself laugh roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
