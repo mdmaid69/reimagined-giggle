b835b3dc239eb63909f7f6f0bec59aa5b0fcd421
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:33 +0000

    polite start fairly vote somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
