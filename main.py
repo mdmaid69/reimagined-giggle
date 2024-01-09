9a71d09135ba40c97f22453b5c271453cd818a04
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:10 +0000

    shock workon lucky when bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
