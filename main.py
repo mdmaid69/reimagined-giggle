361a1959ef426705a59639b986abdf996386cd55
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:14 +0000

    business touch fall smell many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
