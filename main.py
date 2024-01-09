0fda3dab4b4890c6fe4a5a50b33bd4c046c7b19f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:51 +0000

    strike certainly show few week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
