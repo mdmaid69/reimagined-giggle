7c4543152e97cd14258e9133cf88265805a64e08
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:41 +0000

    whatever under gift material voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
