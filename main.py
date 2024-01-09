ee2293598a6d75b26e7ae355c390dc8d8b74c750
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:34 +0000

    say nervous lose careless second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
