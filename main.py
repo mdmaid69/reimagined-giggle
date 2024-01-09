56debd0cdafb429542529626c6cca07149e6385e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:16 +0000

    join healthy underneath cinema building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
