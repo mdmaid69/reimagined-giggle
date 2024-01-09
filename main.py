88394f06cfcff086ed053e1b9c2abb40e04558b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:56 +0000

    reserve either rid swimming rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def count_elements(lst):
+        return len(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
