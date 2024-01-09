b3172ba498c8bbb05c2136327aa4777f754ee9dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:28 +0000

    up chase blue blow whom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
