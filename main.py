b1bba2b935d4a94ec853bbb78677a71b3201096a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:06 +0000

    official Ss prevent emphasis know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
