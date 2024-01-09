f728b83b08d3c2607dc7042475b27c21f51101ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:11 +0000

    listenfor smile fear here unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
