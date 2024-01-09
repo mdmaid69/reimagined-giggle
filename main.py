7bf7beb3ce4f95b48903088b433469246c4a94bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:50 +0000

    immediate shelter pure separate preparation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
