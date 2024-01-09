347251359946f0a3a6db703dfac23fce0c695658
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:33 +0000

    few bath experience giveout thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_area_rectangle(l, w):
+        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
