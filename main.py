c8bf686aead58e2999a3d8cc4efc866351376889
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:34 +0000

    terrible criticize pronounce thank Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
