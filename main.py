99383d071dfa0870f7c9ea7af9e16ff1b7e955a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:10 +0000

    refuse appear what keepout march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
