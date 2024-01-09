7f296fe9df3d19491d00063c3e6bd800f3021d7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:52 +0000

    approval gift short me movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
