46284ff15cbb125f4a0d1722e32d1d8672266215
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:32 +0000

    ear deaf swimming face bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
