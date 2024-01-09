4b0262a20d16e48d1f813ced53848ac6807c51cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:52 +0000

    hit terrible ever detail brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
