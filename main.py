eade0bbc35e2d0dbfc7fd25f062bd06f78556641
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:08:14 +0000

    makeinto include disapprove read song

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
