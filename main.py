777b0fa75c65b32945db59e96d3cdb1f24e0eaff
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:03 +0000

    grow advanced reply recent part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_even(n):
-        return n % 2 == 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
