cd17d579d09db3fa6d111acf18a37cd45f32560d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:20 +0000

    responsible seat under quantity lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def append_to_array(array, item):
+        array.append(item)
