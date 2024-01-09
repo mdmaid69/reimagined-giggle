8bccaf68086d22e67935f1496babacb820cb9ace
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:25:10 +0000

    basis away height rarely intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
