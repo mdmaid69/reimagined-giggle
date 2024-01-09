e6dad99a9bba01f5fc84f14247d6d546aa11eb42
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:28 +0000

    fun underneath earth onto step

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
