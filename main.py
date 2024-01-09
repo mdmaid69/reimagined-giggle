163b3d0d3c2abe19bb9c69bbf90fdf2766dd96c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:31 +0000

    style exploration guard shock lot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
