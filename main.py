c6bac8a38c163547d17a779882ccc9332c6a8748
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:25 +0000

    hide too bank careful any

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Words:", len(text.split()))
