0665627410c3a191ef3191a76dbc0e284ed22d56
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:23 +0000

    colour holdout bottom use yes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def cube_number(x):
-        return x**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
