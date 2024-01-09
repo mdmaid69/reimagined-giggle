d3c11a3775d591ee570571ba1ca1ac63338c2b4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:36 +0000

    high free rain pipe stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
