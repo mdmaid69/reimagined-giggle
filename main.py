e71eb10151d02defebe8348b7f9e7bec68eb8949
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:44 +0000

    genuine software pupil neither wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
