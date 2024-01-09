71a73592eb617ff7bdf26916bc4b1cd1e751133c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:14 +0000

    will breakout friendly fat noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
