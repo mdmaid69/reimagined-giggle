e6b5171a322f6344f602549274d706c7b7470305
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:32:29 +0000

    apart memory bang remain danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
