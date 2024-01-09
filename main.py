c9ca91bd5d8f35e4c8b3db88826f9910fa5c6321
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:16:04 +0000

    reach file precise step seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
