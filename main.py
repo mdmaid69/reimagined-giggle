384cf742cf4a9aacfa0ea8eeceec5ec02e6989f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:56 +0000

    good drink greeting allow deal

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_max(lst):
+        return max(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
