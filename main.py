f1301ca8c7d53e5ca4ddd71fc2ec6c569eee2960
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:30 +0000

    care threaten several watch notice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-print(os.getcwd())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
