09f986300ae469cb5f6ec044d513cbe30e15f2be
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:04 +0000

    agree exit must slip disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
