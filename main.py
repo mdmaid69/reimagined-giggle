419be1ab1b4445a324fdc34a313185f07b83ee9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:51 +0000

    once coat collection include herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
