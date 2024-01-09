d1c6861269b253dae0e49add2df42db25e56caec
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:10 +0000

    space careless shut crowd offer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def convert_to_binary(n):
+        return bin(n)
