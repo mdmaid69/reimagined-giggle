fcb6046c5e641ae4869c3b631b98bcb59e88c020
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:41 +0000

    programme lock produce adjective care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
