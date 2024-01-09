79eef630da96e9425d5a93f4a442a86e33f08854
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:15 +0000

    lunch breathe drop on touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def find_min(lst):
+        return min(lst)
