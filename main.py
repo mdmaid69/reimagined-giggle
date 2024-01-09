0f2ca007090af1c1f1888fb969084e692e965a15
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:11 +0000

    success best sharp head Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+print(os.getcwd())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
