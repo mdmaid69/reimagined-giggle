d4b8d4c03a27e2695205ab822ae15b705cee9bbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:36 +0000

    ticket responsibility 573entertainment add speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
