47e4be78f7b08c3276b3bb4008ef423a4bec1af3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:00 +0000

    ball correct law fill music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
