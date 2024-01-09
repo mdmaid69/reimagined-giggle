e4f392219dc229fc282071d196b02d640c368ffe
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:38 +0000

    positive getoff object onto loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
