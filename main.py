f98feb43c3dbc58e2e00d21899de5a9697aa0214
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:03 +0000

    above turn excuse event undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
