6a6e9d95f33408b0e16c39c6ef1d89d4ddf75deb
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:05 +0000

    wash form nervous your runout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
