c91e053128d7ccbdd0d3b0768eb83960aeae0e75
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:42:59 +0000

    professional passenger takeoff suddenly shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
