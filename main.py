6ca6129886b3c73e9bb397eb5bd0cfae3cb01eed
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:16 +0000

    contribute basic comein promise normal

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def square_number(x):
+        return x**2
