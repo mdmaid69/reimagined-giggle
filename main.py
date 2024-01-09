32986909e60f45d04fb6ebcf1400e33ddd74c379
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:28 +0000

    convince plant poor test luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
