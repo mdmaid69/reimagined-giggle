9740aac47d47ab0d5a4d448f5b21c2c281e6430a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:00 +0000

    Qq only pleasure deserve never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
