58beb222dee4ad7e0b53efa3c5d1f7076bab58e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:15:30 +0000

    doubt disapprove birth medium keen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(math.pi)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
