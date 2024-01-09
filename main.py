9f6bdd3a0f8db847f8e78ae420a66e4f5cf46e53
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:55 +0000

    sick criticize duty confident reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
