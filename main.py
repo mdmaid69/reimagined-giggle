2c54530665bdfb89cf064edf0e4e9549514cf72a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:52 +0000

    cope equal at again crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
