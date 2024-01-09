caa9061f3689318fa3b29abaf3f02076814ca054
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:33 +0000

    light mistake squeeze party exciting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
