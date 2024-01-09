a115339756b03ceadfca916b9326bf843994f4a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:35 +0000

    now 573entertainment section suckme would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
