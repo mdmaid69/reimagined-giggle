02b47dbedab0de4ebead314ede61f2019e39d0c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:52 +0000

    left thing her minimum ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
