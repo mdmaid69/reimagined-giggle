2e27c8a05b1c9a7abe28e52f459a0cc536875599
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:27 +0000

    either hang spill then bottom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
