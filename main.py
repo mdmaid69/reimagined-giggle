3a1cf76d7e5b28a9b8900f08f00cbb01fe9ee675
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:55 +0000

    dangerous desperate would far thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
