d9df7052ac0bb83bb7012564a0484b92b5dd17f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:08 +0000

    measure glad goout skin tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
