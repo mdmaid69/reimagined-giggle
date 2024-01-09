48e0ce5e08ef9a701d68800c3c033b0ee9c363c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:41 +0000

    recovery land make 567enemy gooff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
