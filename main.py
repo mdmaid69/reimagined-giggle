e06ad4eca385b55bbf5267c66be612d6bc0ebabb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:43 +0000

    rarely watch personal phone regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
