573df00bf03e0c71181821cdf1eeb86c1b0625b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:05 +0000

    fetch pot far before where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
