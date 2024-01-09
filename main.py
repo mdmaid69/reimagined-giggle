9671be0c002e78f5099fdaf2e91509e83d43f492
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:53 +0000

    sound gate contact card plant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
