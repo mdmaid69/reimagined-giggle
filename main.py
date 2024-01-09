20e89bcc09c6daf67055c8c96cd6776004773a0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:19 +0000

    winter broken fight see work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Characters:", len(text))
