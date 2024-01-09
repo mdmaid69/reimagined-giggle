33af0cd0d4d4e605c3a083b1d19f48bb1c60a0fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:30 +0000

    Ff burst error term cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_item(array, i):
+        return array[i]
