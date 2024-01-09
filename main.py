e379af341e359a1343cba40b4de9dcfdc4ab3585
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:55 +0000

    cry body standard allow willing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
