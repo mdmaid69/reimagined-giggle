b66223652e8f908747ca4a9e8e75ca5cab5210ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:21 +0000

    loss person measure another commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
