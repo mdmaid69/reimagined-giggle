0f3ce40176be0c2b1c99504d3745a15bec161b1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:33 +0000

    talk Tt arrival frame claim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
