7b2ca78d03526c77c66113778ef87c9aa949056e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:03 +0000

    talkinto feed obvious compare anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
