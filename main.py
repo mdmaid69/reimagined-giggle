1854a122e181bd8cbb6351232c08c6dcc72f8ab2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:35 +0000

    immediate getoff look sick large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
