80db06b080603301ef2ae01d8b4f3b6f612a6992
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:50 +0000

    respect forgive together 567enemy glad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
