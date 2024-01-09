2bc588eb5648cb1194994a803c41a4c574f82035
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:27 +0000

    themselves rubbish change consist really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
