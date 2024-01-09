87d986b8c57432866810398763db49a915922160
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:27 +0000

    fine comfortable open persuade take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
