b251bf9d816f4bcab1ba67d9cfdd971c8929902f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:57 +0000

    physical colour call goaway Gg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_circle(r):
+        return 3.14 * r**2
