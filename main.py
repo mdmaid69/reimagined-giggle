b9ef589c255ffc2f09ec2ca8c07813089ca695f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:14:50 +0000

    neither urgent poor name attention

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
