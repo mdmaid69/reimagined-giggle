5852e6ede338e6199be77798202934447f89121c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:34 +0000

    charge apply takeoff note brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
