5418483a3c7e24ab12a11a5a040a2f49e164a18d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:33 +0000

    over powerful accident embarrassed smile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
