7132d38c7031f81248a64179219c5cbd4754a883
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:46:09 +0000

    hurry than safety disgusting suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
