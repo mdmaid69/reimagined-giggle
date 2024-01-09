484db78c486f7bd036ed27f4e91dfb039d296771
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:24 +0000

    before though opposite makeup prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
