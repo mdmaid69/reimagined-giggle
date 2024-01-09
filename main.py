19da5087395f91c1707aa5144c83f6c9898afb2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:56 +0000

    abroad he touch slice belief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
