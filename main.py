6370e9a4f80a9b7bec4dbc04ed1e19d19072a094
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:39 +0000

    bus raise wish leg century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
