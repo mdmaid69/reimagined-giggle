a6f847b0de2743e87099497cf6977482bb5ac4b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:39:41 +0000

    off example official transparent than

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
