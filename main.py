aa03d83fc778579928956bff7b98369a19b4e84c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:26 +0000

    niggaacademy over stranger affect ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
