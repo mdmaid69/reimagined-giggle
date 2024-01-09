63404cc13ed0b7c76e356a8d6f98dc690b7d7954
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:38:55 +0000

    whistle cruelty male emphasis until

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
