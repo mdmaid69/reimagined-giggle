3aea1f52b255b0f49df393199d7ca5308f427f62
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:04:41 +0000

    hand forward large uncomfortable breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_speed(distance, time):
+        return distance / time
