8291ceaa938ba0e3dad1fc5e2a029085c67538c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:12 +0000

    quality distant mark exploration final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
