3d9c09d9e41d624735c3e29def2f5ba303512245
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:03 +0000

    give far course beginning crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
