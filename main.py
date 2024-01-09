74aa66bcf564417dca8eec5d604af3ebbdea85fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:38 +0000

    single century sign usual guess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
