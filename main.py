51de5166a35d774354a5657417644f1e960e9c67
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:45 +0000

    on degree estimate top gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
