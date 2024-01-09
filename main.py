ff85d63d3e0835c8d9306986981b1da88e055df7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:03 +0000

    workon reduction immediate inquire drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
