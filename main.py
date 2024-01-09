23d89390b0fd5146e6ffb2cad2bbfa80ef55a626
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:30 +0000

    wide influence approval better blame

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import collections
 def create_ordered_dict():
         return collections.OrderedDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
