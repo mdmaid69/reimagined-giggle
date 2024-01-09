4798cc2c6ce7077f887cb3fd7819a8db8d735836
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:52 +0000

    concerning rest dry flow freeze

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import time
-print(time.time())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
