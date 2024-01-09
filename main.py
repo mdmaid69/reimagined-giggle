28b4e2ec55aab2b2b23cdd396bab0c7932074f44
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:01:02 +0000

    familiar visit trip of takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def reverse_array(array):
+        array.reverse()
