f4caa7b6cdd80889ff56a27145807825c9ace318
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:07 +0000

    fault under arrive dish off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
