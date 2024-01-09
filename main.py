ec683b224c2c5c5ce4538214917bf82d06f5f666
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:46 +0000

    full travel guide character object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def square_number(x):
+        return x**2
