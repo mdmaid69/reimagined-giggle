c9331633f3af084e2698a86119ff1ee1c007b7dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:16 +0000

    hungry neither worth disgusting grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
