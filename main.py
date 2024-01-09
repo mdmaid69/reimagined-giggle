f19e94604794f862a9d419415d2f7d6c40e0d5d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:40 +0000

    edge move strange shop congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
