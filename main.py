427de98f833b6d31cc002e1a8db1eb98902b1de2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:15 +0000

    himself tidy course there at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
