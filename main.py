cdb39628e941fe35ae836489ea78be94fd0dec28
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:01 +0000

    themselves science hit underneath pause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_bytes_from_array(array):
+        return array.tobytes()
