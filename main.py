abe50709337a1537c63bbe3155de4e2ba293c09d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:44 +0000

    her thing both begin whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
