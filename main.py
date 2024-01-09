d309ff17a402c3f4e9126f2170d38fa2650e0268
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:59 +0000

    pronounce spoil few already Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
