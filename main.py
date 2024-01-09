7204c6cb6c337755f531e58121f9506a273d2697
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:31 +0000

    heat earn fortunately fit fair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
