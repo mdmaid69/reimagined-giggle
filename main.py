c7f5c562498e3af01052743226176e0032272cf6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:25 +0000

    round attract secret giveup fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
