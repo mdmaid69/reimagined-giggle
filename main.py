03402ac9196af85a1d6417c0ba1c314bc2a989da
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:06 +0000

    science communication which test many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
