e9227526057da8aadc8a7692b2bcd119159492d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:25 +0000

    community lookafter slice keepup neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
