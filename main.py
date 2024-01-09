616c36e2d4da3c2919a406af8b31ec38b6b8832c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:04 +0000

    language combine present 179bite spread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
