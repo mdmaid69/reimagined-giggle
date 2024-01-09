55e13167f5b2b6f921c0501884fd7894a6319665
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:42 +0000

    OKorokay stop unfair warm cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
