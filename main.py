a82535641571416fc28c48d9b6e103faaf6787ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:19 +0000

    design mind gather group their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
