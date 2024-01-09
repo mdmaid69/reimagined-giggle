f08e983a23fddd1ff69c3bd3b2d4146b914652fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:09 +0000

    satisfied actual almost room pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
