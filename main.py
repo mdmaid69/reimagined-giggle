1870e46036a0d9ba8cb076acf429c23e98305e73
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:51:38 +0000

    comeback welcome succeed watch trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
