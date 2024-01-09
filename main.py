416f0e851fce04833fe7dfeb14dd0ce14d2fd97d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:10 +0000

    possession matter Uu Bb separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
