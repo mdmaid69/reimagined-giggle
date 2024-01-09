fcff45cab9635f305b532832fe855a1076e51f45
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:42 +0000

    yes Nn promise flow silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-print(os.getcwd())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def count_words(sentence):
+        return len(sentence.split())
