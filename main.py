746f36ad995418c9b4247900187c8257172e4f3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:35 +0000

    thick office cum his excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
