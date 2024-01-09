5e42d306876af65d1a874f13df62d1b7e24c19f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:53 +0000

    note mail wing word proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_pressure(force, area):
+        return force / area
