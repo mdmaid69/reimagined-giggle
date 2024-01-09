a8b42e67a111dc072680b9bd0a1b1a2d79722b31
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:51 +0000

    uniform talk behaviour check answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
