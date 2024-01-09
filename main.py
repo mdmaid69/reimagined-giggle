c9ede33b7cea6df0b81c9ab5fd041043f12a50bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:30 +0000

    spite loose want exact complain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
