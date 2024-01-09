92c200c7718307f648572613c2a0a76a54288fd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:32 +0000

    start shake fill claim introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
