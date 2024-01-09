9210bad09681470507e2fd04e6b3b1edc97f4d17
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:57:18 +0000

    leg just mail goon unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def subtract_numbers(x, y):
+        return x - y
