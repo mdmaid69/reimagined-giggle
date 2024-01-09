f464acacc22a19727af9c87ed7c500d129a1cf47
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:53 +0000

    friend sing sink breakinto owe

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
