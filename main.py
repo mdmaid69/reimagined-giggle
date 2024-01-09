2cb203e2914869bd08df28da1b953599afb20a85
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:06 +0000

    road suddenly why centre mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def cube_number(x):
+        return x**3
