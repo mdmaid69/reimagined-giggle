4c669a14a49b13ddebac490f0c6cae47328bbd42
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:07 +0000

    temperature by sore suck vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
