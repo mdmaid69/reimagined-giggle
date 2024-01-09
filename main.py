a0d900bdafb55bc4ae4aceed5837525046109211
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:51 +0000

    he bury such pair cry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
