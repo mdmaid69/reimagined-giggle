5f1e7fe0f73607360e13f6850249d56e971e8b92
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:18 +0000

    think joke embarrassed drawing our

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
