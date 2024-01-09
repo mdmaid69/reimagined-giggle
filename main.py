faa610013001762b5ea84bb444b10c75c1526fb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:38:13 +0000

    shape callat soak table actual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
