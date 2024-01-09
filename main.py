0fe4ec792e754eeda46a55fb910093d21d7fa243
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:59 +0000

    soft major behind party cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
