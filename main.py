d328e0e186d57552d70a3edb052068627a2e4c66
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:30 +0000

    wash avoid tomorrow unfortunate dear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
