17b2321adb295af9f076b456e73dff39f7fa10e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:17 +0000

    when during wherever material breakdown

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print([x**2 for x in range(10)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
