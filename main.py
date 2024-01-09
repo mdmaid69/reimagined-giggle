f09299e9cb1456939acadf14e1681b8652f9386d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:59 +0000

    protest middle square prize knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
