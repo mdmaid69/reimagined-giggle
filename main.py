2f8c151b8c4f1a919f5692f6131a4855e2c25a23
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:09 +0000

    name interview ground long complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def remove_duplicates(lst):
-        return list(set(lst))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_acceleration(speed, time):
+        return speed / time
