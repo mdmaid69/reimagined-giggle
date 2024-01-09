61794afd356df76d0b627e906c91b99ffcdd6aec
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:52 +0000

    callat hang match let situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_max(lst):
-        return max(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
