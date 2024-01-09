14c36983fdea21ccb962d1cdf0122b39c7e0bfec
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:11 +0000

    everybody unkind last zero exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
