664d29741ef52ce3d5f203156e1165c1baa4780a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:24:31 +0000

    ill food hot up precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
