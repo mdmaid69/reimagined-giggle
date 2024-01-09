4779c8bb97d2f302644a9790d8559e29bbb99a2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:25:23 +0000

    concentration world small cough convince

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import datetime
+print(datetime.datetime.now())
