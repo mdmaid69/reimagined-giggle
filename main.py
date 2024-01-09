0456b6766f0e84efe10fb4718441cbf9cd92cf59
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:16 +0000

    rough occur poor rely tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
