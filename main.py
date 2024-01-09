1cfa66df315dd5647c7f7e7166fec15aa3db3459
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:12 +0000

    lie back mother promise mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
