7af1f93bc3390ea99a9205c1e07b3d5f34daa5a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:29 +0000

    bored inform past would for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
