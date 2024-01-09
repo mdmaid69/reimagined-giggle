9f8ede193615243561250db1a1ab1efef166a679
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:49 +0000

    ground raw worried expression petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
