008f191efd182669bb7bffee6e5a42e6b148ac11
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:47 +0000

    worried many reach bringup pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
