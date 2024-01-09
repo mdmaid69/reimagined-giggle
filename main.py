1bd1162ea1c02c59295a3479a20fdea9cbf17248
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:59:16 +0000

    every rise flood generally soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from collections import Counter
-print(Counter("hello world"))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
