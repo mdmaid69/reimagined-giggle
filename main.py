5e3ad5dbac6694e9751e6bacb0deb7775719f95b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:09 +0000

    experiment intend under stick joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
