812e7a076b36daee11758bfd4998dce5db6e0b7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:19 +0000

    fight pay finger make below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
