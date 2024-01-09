84b090f67003df16d62a729351808fb4f95d09ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:37 +0000

    fashion on squash area search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
