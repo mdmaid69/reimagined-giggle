c243213fef300b07679e3b4996a6ca589c765136
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:04 +0000

    vanish run wave market back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
