5272d418ed1544b8227cf227b25b90149d85c06d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:44 +0000

    ride free belong winter keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
