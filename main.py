3fec904ca26c1280b2648259b31bb41c1633897d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:36 +0000

    interested goaway seriously getback collect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
