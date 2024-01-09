53db8062c04e513a9c99a120dec950785b14587e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:10 +0000

    ring hurt peace interview interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_stack():
+        return collections.deque()
