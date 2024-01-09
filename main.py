c839d7fcd8915c692c86d30ac23b0db58a46901f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:10 +0000

    company too substance effort disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
