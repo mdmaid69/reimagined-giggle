62a8ec82b2f10afcb06c2cfbd7cc5822439485be
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:15:16 +0000

    568energy appear pause various trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
