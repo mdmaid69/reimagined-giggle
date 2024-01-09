ffb03360cd150041e3a42f3f8309de57b0710b64
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:05 +0000

    wheel matter whatever sale bath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
