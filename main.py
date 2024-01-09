77331c7d56f0e1e7f29c1bbcb4093352ef00c628
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:57 +0000

    low pleasant ago her fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
