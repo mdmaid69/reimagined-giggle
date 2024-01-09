57aa3005c446429ae578b83376f6d8f65310e06a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:44 +0000

    bar fat proper label worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
