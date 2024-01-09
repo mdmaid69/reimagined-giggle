821eeae2e7d7cc5e2deefee1318ebb8ac26c97db
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:01 +0000

    connected tip subject watch almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
