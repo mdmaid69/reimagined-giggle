7a054d46f40a3110f41cbd06f92798f69d7a7e05
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:12 +0000

    down room thinkover act without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
