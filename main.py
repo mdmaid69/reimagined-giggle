845077600a4890b06be9bd66cd3d8798f89d65f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:06 +0000

    bottom big keepfrom lean holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
