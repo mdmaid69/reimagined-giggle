763538fb42d87bff1154c290dceebefe829430a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:33 +0000

    we suggestion classroom mind criticize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
