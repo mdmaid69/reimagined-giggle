445e4be0813687e79733178992fa03c7d2eb9adf
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:23 +0000

    standard check quality Jj combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
