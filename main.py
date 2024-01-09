f4d9bd5a3fa0449fa6f549556c948a620483f8b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:42 +0000

    last material job nurse exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
