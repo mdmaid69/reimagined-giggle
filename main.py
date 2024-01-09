7795ab1103b2c109af7357f9a840b7ac5ff1f4f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:29 +0000

    run lunch yesterday slice slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
