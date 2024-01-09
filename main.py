c8371c7574f460ef5a119bcfc90611740120a5a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:50 +0000

    plain store confusing dick large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
