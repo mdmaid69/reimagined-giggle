3bcfcde2e8ee7d767506ea9e619ef11c3fc2bf56
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:21 +0000

    eager ought reply bang appeal

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_user_string():
-        return collections.UserString()
+for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
