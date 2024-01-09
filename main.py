7809de4ee52f11ea2915ab647068c5ac0ad8dddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:22:15 +0000

    risk sense tone sticky estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_speed(distance, time):
+        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
