3d65100e36eaa5b401b7501a66e6ba001078a846
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:09 +0000

    tense pair right pleasant nerve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
