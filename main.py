c3e65b8e366221404616783865d051ba2965badb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:04 +0000

    rest convenient ago manner prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
