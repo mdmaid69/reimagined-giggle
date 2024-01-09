ae0691a735e194ffea30c2cd584fdafe07e8ce00
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:54 +0000

    leg room nerve warn fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
