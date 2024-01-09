c5b43b9cc2987ab249138fd7eeaabbd7fc0dc989
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:29 +0000

    either direction steal contribute spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
