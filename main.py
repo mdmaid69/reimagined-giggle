0e0dc74ff8fac492dfadf6ce6146f40ec17bf932
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:35 +0000

    Jj afford within right breakup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
