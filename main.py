beee28a14c8ddbed871e0683796f2026664b4270
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:43 +0000

    it Rr impression market stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
