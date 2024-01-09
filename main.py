331f5effb5687cfa69f5bd4f980af86772a16185
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:53 +0000

    winter occur distant criticize breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
