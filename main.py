c1c517055b3b07ed335f53bd95113366300f9aea
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:49 +0000

    bread something man land dream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
