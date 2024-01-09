e3ff628b34b629f6caaee109d0aabd8d7da6fd97
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:52:07 +0000

    opposite favourite confidence whole suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
