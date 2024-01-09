4a71f11f0d71954dab7febbb1adbeccd4c119666
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:09 +0000

    schedule sell responsible drawing favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_max(lst):
+        return max(lst)
