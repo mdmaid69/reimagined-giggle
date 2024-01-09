420e42411b977f0441f1757500d68aeddc240c6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:21 +0000

    feel set across key surprise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def is_odd(n):
-        return n % 2 != 0
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
