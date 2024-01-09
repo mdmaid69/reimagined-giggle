72fe1a45719a7e45fa716ff9c05b6325850d3cba
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:29 +0000

    spill sad raw order through

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
