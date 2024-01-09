3bf4ffba6b747bb1a2f98ff851ac032df5e9105a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:30 +0000

    fly content children pretend whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
