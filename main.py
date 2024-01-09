1ad55c0d2ef1e8ecc4a57c26f258000c177893c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:22 +0000

    real lesson adjective uponon ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import sys
+print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
