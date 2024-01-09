de2e1ba42ff273baba279fb3c517935452565fd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:50 +0000

    position port dig development neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
