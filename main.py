5b5c667e635563e3a7af2298b3a776066c8ad864
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:54 +0000

    care let wide key practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
