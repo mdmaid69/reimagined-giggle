9371a9c59c7140717c41ae483a33c44e9f761a62
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:00 +0000

    squash artificial within react interest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
