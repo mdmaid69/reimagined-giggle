adc30673a95ef3560a02242332b8f4059278967c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:02 +0000

    along collect constantly healthy phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
