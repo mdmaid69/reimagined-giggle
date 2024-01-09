8068aef7fb039fd2d69bf80ee64e975f23908a56
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:23:10 +0000

    schedule key word need sight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
