ba05adae6ec626d3ad3ef71d8505edf2923cdf92
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:29 +0000

    back visit uniform uncomfortable at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
