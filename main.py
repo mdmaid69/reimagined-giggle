f187e90600dbc71de7df90cc9afb41248fdc2a89
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:27 +0000

    tax hurry treat compete cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
