9f9f28fabc2ac76d7a0b4ac51183ec60a0703653
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:17 +0000

    bit noone treat wild deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
