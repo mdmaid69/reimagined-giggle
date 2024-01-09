ebcb031fb395aee0e4df28f4a24c13bc3e08b5f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:12 +0000

    by turndown nearly polite choose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
