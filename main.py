16b1b7e99fcb8ed89de9b2729a7b15491a52e7d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:33 +0000

    interrupt lookup backwards Tt 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
