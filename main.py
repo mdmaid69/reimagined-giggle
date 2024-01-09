6fcb3df78a7b29806bd8412c02b89520f2449afb
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:41 +0000

    impatient cum hard workon wet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(x, y):
-        return x + y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
