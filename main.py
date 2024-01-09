2370cc01e5c19d19a486830c759aa6efb24704c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:57 +0000

    mend the partner die excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import tensorflow as tf
-print(tf.__version__)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
