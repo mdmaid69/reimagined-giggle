e28de269da5c065df2aec330d7d66234e8c63451
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:12 +0000

    pronounce separate true free close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_counter():
-        return collections.Counter()
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
