ce493fdb291294b2e2a023d6f14fe285b79bff4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:34 +0000

    past call recover level comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_stack():
+        return collections.deque()
