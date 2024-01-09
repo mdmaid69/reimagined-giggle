c8ad39e6dc1cef0e94452e9f9fab5ca1188ea1aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:26 +0000

    exhibition upstairs scare mouth plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
