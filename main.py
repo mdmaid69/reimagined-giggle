50a7e4baf85734b96304e71a2e46faa291926ef8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:00 +0000

    criticism usually union truth not

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
