97c50b711b7405adec496003a97f3179d04b6211
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:46 +0000

    cry dick label suit message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
