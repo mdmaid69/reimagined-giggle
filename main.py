fd4c08700282596f8352eba75fcc0ad0ea20aca5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:26 +0000

    separate building plenty surprised path

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
