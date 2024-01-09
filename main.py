21343663162d9674fc549e8a97aea64c5cd8e9cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:34:23 +0000

    half pleased wheel dry rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
