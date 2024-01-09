e73407a1b657718c555cfb23f3947c49b98c2492
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:34 +0000

    only stone laugh chat admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
