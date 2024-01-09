6d4e342ce698adb42c9b31cee2788630ab1469c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:33 +0000

    dream juice notice study Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
