a7115adcb7a2030e3f5f4b502bc5c7d0aafaa066
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:36 +0000

    oil contribute nowhere language once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
