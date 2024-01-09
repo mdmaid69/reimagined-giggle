6ca78129a69280e1f85c1fb1e3358aca9691c958
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:16 +0000

    various send interesting instead bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
