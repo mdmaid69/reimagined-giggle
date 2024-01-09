6fd383eff096f98798d3461e36d54ac6f9db93b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:44:25 +0000

    autumn challenge strike nigga arrangement

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
