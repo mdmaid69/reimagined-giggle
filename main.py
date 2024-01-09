4d950f88c5973f37429c240a7a7744311d44d7aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:18 +0000

    ignore receive method pocket ocean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
