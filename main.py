f27014266d84effb8b1b86f2891839eb4b0172ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:26 +0000

    reduction influence prove character hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
