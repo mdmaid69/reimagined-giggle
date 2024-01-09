7d32e0bc56e8736d0969bd3d5141d0abfcb92188
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:24 +0000

    effort keepout best afterwards breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
