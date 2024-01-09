9e76b58b7197c3471bf7839bbbc2092ee6ecdc7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:38 +0000

    favourite slide perfect pick something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
