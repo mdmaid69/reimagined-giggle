df3e883d07c4647582dd45a945dc1c0797c1cd47
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:05 +0000

    OKorokay blame heat usually vision

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
