ceab6920eea2fd0da25b32e706e31f79b9480053
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:42 +0000

    emphasis border equal stiff cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
