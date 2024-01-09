479f84feedddbb0fc2f036e6e95b55ccd98badcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:39 +0000

    match emphasize nowhere convenient inform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
