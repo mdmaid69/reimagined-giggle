397c830d80f658f95d2e12f8d2ec3075b3947b08
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:33 +0000

    terrible thank mean each cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
