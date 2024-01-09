1f33556065426f0381612bf93340e4efd25eebab
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:21 +0000

    difficulty otherwise side opposite suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
