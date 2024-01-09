cef3d6ce73b95f006d30b178a6dc70d4df101ad0
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:51 +0000

    earn scream may thing pen

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
