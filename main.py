8038012c12b32a336a53707f94a38a52b6f0bc86
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:03 +0000

    visit stick aim false among

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_min(numbers):
+        return min(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
