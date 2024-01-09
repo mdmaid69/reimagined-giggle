d9863683b37d4cf927eda969dafca01efef587c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:56 +0000

    back holdup Ss deep meet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import numpy as np
+print(np.array([1, 2, 3]))
+def convert_to_binary(n):
+        return bin(n)
