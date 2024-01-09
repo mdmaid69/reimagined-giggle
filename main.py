bf48fea2a6f94386419c390d558752f3d1350730
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:58 +0000

    record regional nature rush relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import sys
-def print_python_version():
-        return sys.version
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_words(sentence):
+        return len(sentence.split())
