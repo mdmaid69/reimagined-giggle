16b8930da5fc09480f6e3f89c5ff61c3c587a67a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:54 +0000

    leg children whistle happy vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_bytes(array):
+        return array.tobytes()
