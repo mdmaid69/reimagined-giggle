6754fa62b3a8f00c3402b9c5fed314fc9699fa31
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:33 +0000

    makeinto pollution pump level emotion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
