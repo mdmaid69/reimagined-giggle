b3982de65b9fa5e2cc928c30d5ed4e7bfffcd5b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:39 +0000

    light system surprised bit thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
