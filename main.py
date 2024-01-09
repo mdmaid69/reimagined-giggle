2019777269959bad05cb68ea792ea2b4e3a2910c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:59 +0000

    lookback(on) wash whatever collect he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_list(array):
+        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
