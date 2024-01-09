ca2071efed0fcc875b09582e15bbc58ec2595cda
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:38 +0000

    wear command expect relative generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
