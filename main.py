a4430aeb4cd082d3d6aaa6d0c3b82be209e68ce5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:17 +0000

    question sore score file go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
