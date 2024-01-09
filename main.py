9c85932f3506d4f78be54e9e4409ee0477c62011
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:19 +0000

    seriously empty sticky form gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
