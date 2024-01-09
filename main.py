47ed94d9c463141b7f189f3e0047e3b71749665c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:11 +0000

    tend used somehow cruelty keen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
