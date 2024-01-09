18f80867869957c023acf979c45d899cee0ad89b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:18 +0000

    slice shopping argue target by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
