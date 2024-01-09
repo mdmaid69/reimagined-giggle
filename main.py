76248bf401b57c34f44bb17e80498e7d27793bb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:44:04 +0000

    noise risk front raise father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
 import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_priority_queue():
-        return collections.deque()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
