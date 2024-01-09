e1ec16f51c138f8fdf33c0f90836285acabd2b25
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:09 +0000

    sound under deserve thin offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def append_to_array(array, item):
-        array.append(item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
