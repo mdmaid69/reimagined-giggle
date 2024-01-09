26474376c4787759d93ec5558ad5959f04ed809e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:27 +0000

    cap single certainly breakinto file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
