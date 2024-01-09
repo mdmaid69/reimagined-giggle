de9678a7018c46009c46edfb9baf607dcdb04a0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:50 +0000

    record model level opinion service

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
