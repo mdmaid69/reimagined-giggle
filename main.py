cbca1228203e1d5b37044c76720aec4df7db4146
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:01 +0000

    matter how detail trip quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
