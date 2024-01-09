95f5ce7c43b44b0ddbc13eaa6d768225950c96e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:03 +0000

    possibly force limit especially crowded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
