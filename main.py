9333cf04e9c62d1d9d06cbbfeeb4ee08175d5378
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:17 +0000

    possession hollow 179bite call unable

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
