90b60f1ebe0d9cebb7531bedd78dd339601b0b48
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:12 +0000

    star card land quantity brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
