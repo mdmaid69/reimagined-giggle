3735b8f007bd144acc702609f5ad627769aba3c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:02 +0000

    cross paper odd pronunciation satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import numpy as np
+print(np.array([1, 2, 3]))
