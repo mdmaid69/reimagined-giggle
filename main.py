e5c6762a80e675a5d91d60fccdf99e2e601cb034
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:45:07 +0000

    related feel bin reserve real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
