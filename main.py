dfcb86959da1e6f6c9219849e9cb69a7914f1a6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:41 +0000

    mistake joke be this worse

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
