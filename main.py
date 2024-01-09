03bc86dc9475144384d7f890064484f0d1e43760
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:55 +0000

    insist belong steady contrast careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def print_python_version():
+        return sys.version
