be32a069f8e8771a77a26180cb0eb369eb3b1d23
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:11:54 +0000

    remember command season weekend attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
