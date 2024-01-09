44c2190c6065f49d6654554756a4394c812655e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:46 +0000

    leave class disappointed basic error

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
