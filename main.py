f09720741d7f45dc7f95f6e868a07c26a336cfd9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:12 +0000

    runaway comfort tone single black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
