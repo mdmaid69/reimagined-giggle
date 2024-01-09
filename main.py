c9b9bc34f8efdafd54cd7ed660475bc737b56857
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:48 +0000

    thorough spread normal division question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
