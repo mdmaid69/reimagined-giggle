3dde1c1118c4b6e1f38466638749204322b9bc70
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:44 +0000

    pair increase Zz often tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
