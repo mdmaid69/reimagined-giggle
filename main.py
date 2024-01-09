ece30c0c86ed1ed970d94cbc38c0984210eceb77
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:45 +0000

    ever worse travel after gooff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
