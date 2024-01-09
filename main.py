78f5e27d04a207fc7de0a9adc9d865a582fe7108
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:16 +0000

    abroad usually getaway neat chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
