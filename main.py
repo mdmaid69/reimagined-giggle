855e0dbd5f9e13e165832c378b3dc813e5b581b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:10 +0000

    once bar especially success clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
