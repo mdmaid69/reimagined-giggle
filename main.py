0c626fe1f489ab7290a298fa8cd7929023f261fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:00:37 +0000

    sometimes eat wish arrange movement

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
