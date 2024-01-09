5402d8cbb34588f0413021f18665e0a71315c91d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:03 +0000

    height uncomfortable wipe afterwards point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
