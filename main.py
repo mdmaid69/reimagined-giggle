9e6660e6e83785ae8dcc139dc32a55a021b7276a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:37 +0000

    expect meet odd voice connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+print(os.getcwd())
