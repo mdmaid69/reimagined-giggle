932ce2b5027d89f10d1fd4eb86e08b1ffc9ac084
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:12 +0000

    means borrow likely habit clear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
