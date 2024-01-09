bd93d451335f2b466dececad0f728689cf35528d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:47 +0000

    flow bringback sign ring wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
