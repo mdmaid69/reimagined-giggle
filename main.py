1c8ab733a8520db721c1a6853a15b2ff6e86d2e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:19 +0000

    platform rank turnon glad 568energy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
