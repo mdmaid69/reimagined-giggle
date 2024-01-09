5552753e51bb18cf184daa60bc6d6647d194fc9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:16 +0000

    home compete skin lock possession

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
