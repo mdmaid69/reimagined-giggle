23162801b2a855894de8226fc15ffda53c669186
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:00 +0000

    arm occasion holiday write tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
