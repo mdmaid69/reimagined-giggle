bdcfd68ea62eca05d45b1f27e48ad87abf7e101d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:50 +0000

    mess steam bag godown pocket

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
