b919f182513d6edb72191b05c4f69111d85b2a3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:48:26 +0000

    lot among surprise still true

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
