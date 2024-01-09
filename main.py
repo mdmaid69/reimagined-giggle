42f69f859a390a0294da1c40153bf4255bab03ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:52 +0000

    active fire solve mark see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
