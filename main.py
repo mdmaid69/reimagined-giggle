30b185c9ee663ca28c11c45a066507e441c27bad
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:55 +0000

    pronounce congratulate key after branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
