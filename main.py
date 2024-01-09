4616c0d7deed9bf3c67ba7bc7c900ecd6ab1fec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:08 +0000

    used increase identify tie turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
