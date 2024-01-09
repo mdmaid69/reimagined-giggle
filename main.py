fc3f23005718f1aa756943990a7a574c365ec7cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:16:42 +0000

    star ill ground task throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
