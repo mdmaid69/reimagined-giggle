d53f305cf43bc7a75ab771b9dc120b545f4a09a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:04:02 +0000

    mess last similar snow ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
