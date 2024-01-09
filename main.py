1976021c7f9d11cb933aff99acfe3e61a76f52b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:49 +0000

    pinch pick sleep never winter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
+def divide_numbers(x, y):
+        return x / y
