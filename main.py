021d71e86a3d5e38943f4794b34f84f7fd2fdcf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:40 +0000

    thinkof hurt story exam trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def add_numbers(x, y):
-        return x + y
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
