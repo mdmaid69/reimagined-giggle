2f2d137192748c264dc9a75acc20e95e49bed1e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:08 +0000

    push nurse less main unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
