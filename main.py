c5a70e9354a1afa76a5d144cb38c7ec263ca14f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:01 +0000

    trade thought post travel often

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
