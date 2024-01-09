071bf8727c4ee4e5ee4deddb12a8b84d144536c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:18 +0000

    load best present last understand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
