780dffa36ec33d3b6804d40e92aaa76d995974d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:09 +0000

    post breakthrough score honest contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
