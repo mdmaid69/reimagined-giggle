f05490755f196e94cc024f3fb334cc89a734f8f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:32:33 +0000

    somehow contrast clean giveback offer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+import tensorflow as tf
+print(tf.__version__)
