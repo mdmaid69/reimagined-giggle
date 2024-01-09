4b9f98ae495deaa684ab2cdc45727c88a6754f48
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:32 +0000

    exploration physical only field report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
