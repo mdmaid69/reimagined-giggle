e2eccddb97219d6b0b42c40371d8ebfb147f73f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:31 +0000

    male straight environment complain Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
