4ab92e9c5dffe4c2ae455c491bf32578b65e1ac1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:35 +0000

    recommend its may challenge crowd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
