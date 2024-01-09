f099aa0f9e947ef5d23c2fb695a82c0d3327643d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:32 +0000

    takeout actually burst open Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
