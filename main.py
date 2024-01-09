0137ac12190dc2a801406810496fca796e1b0521
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:07 +0000

    television tall special sale steady

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
