be672ee2ce93313bfac15891d540ad3438f61a64
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:04 +0000

    lower big notice tap something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
