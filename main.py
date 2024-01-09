efdd639c3839f80c19767c74e4e000e93ace1c51
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:23 +0000

    answer tradition show dance recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
   import json
   def convert_json_to_dict(json_str):
         return json.loads(json_str)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
