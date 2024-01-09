28504c02e4f4cc348531fd6126109400c90b23bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:25 +0000

    field brush breakinto cross within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
