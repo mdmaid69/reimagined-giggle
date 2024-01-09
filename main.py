1191376cd8651da6f4f4d1be3873b93cd6b65f70
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:05:44 +0000

    meaning passenger by begin hold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
