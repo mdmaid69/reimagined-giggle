fbb1319ee753b058a7663a5e6a6fe10210d7d30f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:23 +0000

    supply scared lost store high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
