838e22d42e71b6fe981799558567c40ae169f0ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:03 +0000

    distance modern present go along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_area_rectangle(l, w):
+        return l * w
