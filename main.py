6f3a75cd0e324c471e821665293effad6fef6612
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:01 +0000

    formal food prize fly dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
