e014eac18848d0d23949591b2b7a4423e99c02df
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:13:05 +0000

    between price gradually hide warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
