4fe49d6bd67234cb8a1c18b17b5e440e9b71ebf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:38 +0000

    quality difficulty game fall natural

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
