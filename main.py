f2c3feb08f4d8f095dcccd6b871262867cb2dd18
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:57 +0000

    possibly sight acceptable sing know

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def print_python_version():
-        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
