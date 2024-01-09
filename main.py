902352763e2fc42343db5cf5dfad9ec9a89a8f9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:57 +0000

    vote while violence protect squash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
