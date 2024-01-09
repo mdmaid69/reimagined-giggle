10737815b697bc83372d9c694fa4adfc46c0206f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:00 +0000

    stretch result period prepare waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
