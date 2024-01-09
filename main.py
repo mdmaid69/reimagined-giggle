b03ef5ba614b48d85aff47f9b90328bcee56b4a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:40 +0000

    song familiar shopping idea crowded

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+name = "Python"
+print("Hello,", name)
