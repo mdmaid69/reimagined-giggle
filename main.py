41181b50f4f62b3f7e0bdd7a2c82a6474f099bd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:56 +0000

    quantity bang common agreement before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
