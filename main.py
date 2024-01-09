81cb777c22a53553d366efe75e169bb5e864db7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:43 +0000

    machine block refuse tone deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
