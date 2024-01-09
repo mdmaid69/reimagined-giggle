9b27add3d32de00ad54b2ab8442446d4c6b45342
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:06 +0000

    reserve person luck most temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
