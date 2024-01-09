54e702dd10f29f6db4090d36beaec2cc0bbc1d3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:36 +0000

    field occasion place draw loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
