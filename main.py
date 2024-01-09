3c056746b51de17e4853db77f05dbae89f923c52
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:07 +0000

    correction lookup yes announce takeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import json
   def convert_json_to_dict(json_str):
         return json.loads(json_str)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
