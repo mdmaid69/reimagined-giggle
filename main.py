bd919abc6b1880ca11b5e65f5f6f46ba8d92c996
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:46 +0000

    treatment sport intend rarely event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
