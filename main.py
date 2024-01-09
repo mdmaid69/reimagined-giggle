d42b4edf55c69428fccb98f0c64111b92ce5f9fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:30 +0000

    fear shout no smile throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
