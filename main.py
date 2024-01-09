0ba2a7f99b5a199fd2132437c2dd282babaf6431
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:10 +0000

    bringdown maximum favour environment 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
