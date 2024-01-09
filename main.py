33f8c9d8853150c9e8c68a909c14ec0fc04cd8cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:27 +0000

    married book begin present old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
