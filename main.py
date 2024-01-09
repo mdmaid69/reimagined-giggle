33a620d0dae1ac633b9afd789b6c827bf191eb57
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:57 +0000

    behind power hour proper boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
