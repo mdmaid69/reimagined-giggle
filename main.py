dfbb4806108daf5a11f814c54e498f006176152e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:20 +0000

    vision accident hot straight consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
