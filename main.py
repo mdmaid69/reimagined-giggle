dbf2014ed3566b99e66ee9febe008adb8ca29863
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:42 +0000

    with accordingto from murder butter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
