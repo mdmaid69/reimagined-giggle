57b5ac25ecf6e146a757aea714cebdb1178c1e42
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:35 +0000

    story discover lookafter towards sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_str(array):
-        return str(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
