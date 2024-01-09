631bbc373992ce7387b7ca0f3e7fe874f5581733
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:20 +0000

    autumn meeting size language bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+for i in range(5):
+        print(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
