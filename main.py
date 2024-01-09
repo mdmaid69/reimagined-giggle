9ee081971a99d5f726dd09e34aa1b074e4118ddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:59 +0000

    equal official delight keepoff study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
