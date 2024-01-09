3a418599a76c0871341a0befbe9b9e74602696e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:07 +0000

    definite cut accident steer brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
