9bce703c2f71b1779218b79a3d8d1d344c8d7fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:39 +0000

    go turninto continue match test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-print(random.randint(0, 100))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
