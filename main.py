f51da01e04a29790e127b67ac12d4309c9f6d4f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:34:05 +0000

    spare grand object Rr else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
