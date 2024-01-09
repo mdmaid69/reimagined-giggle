9e424f00798b8bdd29823e2d34240c4a909b01b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:33 +0000

    lonely many hand mix trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
