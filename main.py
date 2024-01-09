190fdf2dd390cb89bb860a0d1e9edc4ab1fb4ae6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:51 +0000

    introduction hope notice wish please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
