b7e0898e1556947600c82350a62a1fe21baf0d4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:39:46 +0000

    interested getback fail switch anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
