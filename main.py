e80f2198d143237741d2b20edba2a63543d39909
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:58:49 +0000

    term stamp bye willing connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
