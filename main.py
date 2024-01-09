8ef6d2d4ebede2f533381406acd5b1de33a4f82d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:43 +0000

    really trust raw bunch eye

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+def get_time_since_epoch():
+        return time.time()
