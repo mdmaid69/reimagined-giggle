9104065df95750846029776e53c92eb450c98dea
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:25:13 +0000

    prefer blind read moon Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
