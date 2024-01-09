1b4bf74d5f4d6818dfd0aba92170c8f9774eebbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:21 +0000

    fashion distance shadow drive herself

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
