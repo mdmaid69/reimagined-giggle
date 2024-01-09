582142b097c4a34599c8f26f02ce3d5e8398fcdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:22 +0000

    comeback roof refuse throw lean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Characters:", len(text))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
