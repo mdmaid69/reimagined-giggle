de53a668583e6fe24861af733ef38c2f3a0bc539
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:24 +0000

    gas guess term ill correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
