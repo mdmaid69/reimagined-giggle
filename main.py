e3047d94cb355de0faa20d52a57e84d74735b8a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:24 +0000

    comeup check mind add identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
