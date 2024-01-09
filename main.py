5fbe12faf29e4bf38649d64cc769efa4e8e00129
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:35:50 +0000

    practice concentration favour ocean embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
