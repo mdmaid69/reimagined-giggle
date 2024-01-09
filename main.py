cd54e46a88903ccaac9c24747b7ae7e1bb031a34
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:10 +0000

    soil callat pattern instrument scene

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
