d524bb79813d4cda3690744c107b0fa9ce1116da
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:55 +0000

    wait recently duty arm month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-print(math.pi)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
