5ca0c4b65c849b1b3099056359e0cc6e22232e79
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:09 +0000

    attract mistake change 179bite coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
