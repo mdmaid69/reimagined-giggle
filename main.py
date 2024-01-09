374317c77ef811fbfd986745bd9014daceaa1f3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:08 +0000

    show share sea unless brother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
