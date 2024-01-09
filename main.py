b3c5e77d221255b6d9a075098f1b54469da2ec05
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:24 +0000

    nor onto risk centimetre bread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
