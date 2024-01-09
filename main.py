7cba9f6b3695393b5cb83607a2eea6656b97299d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:08 +0000

    neither with meal stage lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
