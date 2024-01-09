6c2d2c71b8c3e316c92f255750868956096be3ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:57 +0000

    grow could excuse battery floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
