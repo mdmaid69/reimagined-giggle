d78782d7340fd1a347793e8a73884e57439385a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:05:07 +0000

    stare cruel stiff tonight quantity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
