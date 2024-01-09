dd02d98e18a398927e8b1e98dd3c0e3a8021fbe0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:46 +0000

    may contribute along such turnoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
