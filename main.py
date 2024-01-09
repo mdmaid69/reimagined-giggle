fac6dcbdcc5292f9c40f5bd5903d03e27f895b68
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:54:11 +0000

    occasional famous friendly bit round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
