2e5f85e09194748904c44bf4d3eb9cb0dfa4aa9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:13 +0000

    Mm 572enter goodbye takeout plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
