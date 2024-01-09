99e37c5b524319e2babab0aa3ee9680150103608
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:45 +0000

    extremely respect perform considering difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
