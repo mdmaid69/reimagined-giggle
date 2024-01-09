9b24e4cf5971d277410f8136f6f6e78f2c39a804
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:04 +0000

    have nothing cross fear artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_current_working_directory():
+        return os.getcwd()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
