c53473c85d45e1c7255e644283cbeff7893873a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:33 +0000

    moment night freeze aim breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
