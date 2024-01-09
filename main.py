ef924cf53c73a5ec338121e13e8787587a323cb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:49 +0000

    round forgive row pair reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
