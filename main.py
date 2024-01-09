86389352ba2687e553846e2044780e73a94ab220
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:57 +0000

    punishment silent proper anywhere bet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
