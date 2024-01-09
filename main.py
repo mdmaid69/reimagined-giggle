c442c5b36177bfd751e4c602f80ca05a3515b236
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:35 +0000

    able build give contact near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
