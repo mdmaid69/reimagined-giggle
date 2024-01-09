1a67ba9f5c923e6cb5c4127c4faa95c56b78c7b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:12 +0000

    mail kick swing keen impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
