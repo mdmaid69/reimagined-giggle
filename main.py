92bc868109fc4a489c836551dc6d1d7db5340cbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:45 +0000

    hard must approval back round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_str(array):
+        return str(array)
