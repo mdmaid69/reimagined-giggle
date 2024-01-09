61c6eacdefaf151901cc80ead86415af12c40714
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:16 +0000

    concern learn freeze goon season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
