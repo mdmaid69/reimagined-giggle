a76044eb503c1e4300f813a0a991746638cb6bd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:51 +0000

    everybody basically software roll method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
