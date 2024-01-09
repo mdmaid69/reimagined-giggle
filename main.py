182cf183c8a84220648941d9fdbb1b13d9cd8c3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:24 +0000

    machine petrol rain count ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
