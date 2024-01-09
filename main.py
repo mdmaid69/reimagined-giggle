1df3b3285f92d2b4a5c332d5dbd106079ee1557f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:42 +0000

    mouth Vv several so imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
