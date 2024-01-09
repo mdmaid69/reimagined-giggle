217fef075205d28ca355b14e7e7433a43de1f6eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:44 +0000

    increase doubt suckme uponon cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
