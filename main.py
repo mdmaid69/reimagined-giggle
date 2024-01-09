34dee53d67d50473c28d5ddbbe7e6533bc751164
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:42 +0000

    replace either programme getout train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import sys
+print(sys.version)
+import array
+def get_array_as_repr(array):
+        return repr(array)
