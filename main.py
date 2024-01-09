1d3f86ea04dc29ad09274450caf1303b66d9918f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:28 +0000

    guard combination clear touch stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
