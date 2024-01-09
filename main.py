0f77f40fbf434820c4856dd852b1f45d53adaa2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:40 +0000

    persuade frighten passenger right argue

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def square_number(x):
+        return x**2
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
