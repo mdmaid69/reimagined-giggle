278db902ed9336b85ed9dc377eae37aa68c1898a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:12 +0000

    street age happy advice boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
