e2d0c090c05fee4deee9dadf3e5b1327f7d13546
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:49 +0000

    unfortunate turndown design evening only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
