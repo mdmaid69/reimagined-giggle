5ec894cb6edf40994ffa5b6bafb13bc50174e631
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:16:21 +0000

    geton contribute dream circle but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
