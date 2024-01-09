ab8d25cc0c989fc6f93f31be14680194775470c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:41 +0000

    interruption careful runover me object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
