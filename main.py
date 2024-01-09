dbc4a5a1d6f3d7debabe5772b2253e200b8b010c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:45 +0000

    seem whole contrast stare risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
