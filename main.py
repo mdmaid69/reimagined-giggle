c75d6020f8dcdf6aa66734d78526d0313476ab47
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:20 +0000

    somewhere proof fat dull disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
