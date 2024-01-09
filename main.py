b94370df2a1c5be56ba2b358f3b2f6a1e62b647d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:30 +0000

    difficulty force sail disapprove minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
