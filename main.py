30e16518b6afb33b0f61ea75899a918f4f6e52ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:23 +0000

    border name change hope mix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
