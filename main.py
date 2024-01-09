8c5d38a3a042177e45fd666b71af97f475b11379
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:59 +0000

    long stage lead properly badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
