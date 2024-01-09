8331c0474009ad1bab625cf957791c76aec9d6dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:40:00 +0000

    tend protect issue meal hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
