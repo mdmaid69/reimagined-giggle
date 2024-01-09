baa2571770b58f03c1db50e03a3dc9b7693eed09
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:09:24 +0000

    steady thinkof force aim someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-print(random.randint(0, 100))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
