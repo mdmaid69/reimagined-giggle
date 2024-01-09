e0a5384a9eaa8a441485d6d49e87c458f63db269
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:24 +0000

    bag imagine skirt discuss guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
