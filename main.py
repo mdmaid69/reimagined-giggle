eb72c37c13fbf65aa6955b43faf09ac833e3ad4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:08:09 +0000

    equal score choose beneath never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
