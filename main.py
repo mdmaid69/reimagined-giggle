3e195805625fe757213b70e01669fc3040f6077d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:37:32 +0000

    wheel hers hungry design space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
