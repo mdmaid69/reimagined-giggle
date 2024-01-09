23473960e399f325b6b5a91c5ca6f6f440d223e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:08 +0000

    obtain poison fashion open beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
