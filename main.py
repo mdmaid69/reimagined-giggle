461128d32bcd7cf8d7c5734de9cbf84a4aabec13
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:34 +0000

    recent so turnover formal smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
