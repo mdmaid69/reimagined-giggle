60654b2747676dc11b9681cb88ce1220729e4ca2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:27 +0000

    truth short blame herself use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
