ad8cd2475bf9d0cb07ad2a0182384d73a1af505e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:02 +0000

    man present string record finally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
