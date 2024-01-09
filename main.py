7e6d9b08453ecf5c052a31de3eafa83f4747d7a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:25 +0000

    better thing clear care organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
