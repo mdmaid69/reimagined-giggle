8bbe269920358b0eaba2e9fb0f006a0ef09bddff
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:38 +0000

    earn operate principle stiff snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
