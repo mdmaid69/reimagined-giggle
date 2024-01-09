5c807652b709e21706ca37036f8869aeb036dd15
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:14 +0000

    schedule unusual interested body professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
