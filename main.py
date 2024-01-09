3c35883619196b36af596356803caae52806cdb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:35 +0000

    command excuse sting watch thumb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
