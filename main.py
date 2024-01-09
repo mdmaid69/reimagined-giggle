adb0362b4e545b54dec1654647a3c430442f067b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:48 +0000

    none soil by gas bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
