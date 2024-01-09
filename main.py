d2fc58f38773408293450d675d12269438abdde2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:54 +0000

    cheek press wish spring turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
