26ff407ae4bbd491e631c4adce58445db6b6bfd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:59 +0000

    base gift either practise fortunately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
