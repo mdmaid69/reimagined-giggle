454f283a20d259c957e2ba2b79e7aa7a3ce96457
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:11:23 +0000

    range doubt word left pleasant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
