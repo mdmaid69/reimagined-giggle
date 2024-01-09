e741a70fbf129494f8562c7cc291abb049c02d48
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:58 +0000

    task upstairs serious criticize person

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
