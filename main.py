8507382fca5a90d455c56972a1669eab978b992b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:46:19 +0000

    regard Qq keep party cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
