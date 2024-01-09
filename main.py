6f3fb67b017621fe368839de0749757f0263307e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:23 +0000

    rely discover abroad deep rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
