935e71034eaa217c8ac52c29701f9290fe97c140
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:29 +0000

    comfortable success bet slow man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_power(work, time):
+        return work / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
