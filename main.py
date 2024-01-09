6d717bcdd7fccca4b573b17517cf6498d633a8b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:33 +0000

    scared children tall move common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
