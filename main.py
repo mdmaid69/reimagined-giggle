bf4f8633ca2b85a65813ff95d3bd8b4db333233a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:52 +0000

    tear careless needle cheque equal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
