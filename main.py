ff56f29aecd99ae0f43496bdd199b79ac7488f6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:30:16 +0000

    dream priority home change command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
