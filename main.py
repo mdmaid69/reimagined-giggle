0d1671aecc3007c10141a2d1ed0a44ef41b4d031
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:44 +0000

    equipment yesterday explanation question answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_as_bytes(array):
+        return bytes(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
