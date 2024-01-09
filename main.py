b97c6560187a06fbbf5524e7a95be309287489c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:00 +0000

    but guilty ask duty disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
