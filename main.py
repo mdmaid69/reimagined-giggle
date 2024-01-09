89796cd75d2fb3e92b37bef59b962b140bb2f3f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:47 +0000

    Jj keepout absolutely town personal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-print(sum(range(10)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
