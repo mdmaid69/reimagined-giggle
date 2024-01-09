a2c508e0259952f391462091824dfc804663bfd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:50 +0000

    forward course reasonable deserve will

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
