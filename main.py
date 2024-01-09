a6177d0f0961b16a34bc4d634032cea89877fe0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:15 +0000

    minimum reasonable occur true book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_buffer_info(array):
+        return array.buffer_info()
