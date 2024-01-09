b1f5ec6bcb5f01ef13f77fe368b3d74d169e5708
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:32 +0000

    cry mind connection soft quick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_octal(n):
+        return oct(n)
