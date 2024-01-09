f2731130e88f53c68de15baf9da7d4f8aa7fb178
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:05 +0000

    lesson tool land arrange full

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
