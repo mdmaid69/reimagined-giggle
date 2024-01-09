7a71c8ce366185f0bb468d5c4ed86de53172cc12
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:51 +0000

    swimming steam remove holdup open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
