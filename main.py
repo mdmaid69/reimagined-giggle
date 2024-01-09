2d0b5a4a31e75437269973a879666f6a9fbc8f0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:01:41 +0000

    obtain sting plastic regular dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
