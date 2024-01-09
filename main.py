ec72090a172b063aa44567dfb42ecf3427910513
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:29 +0000

    feeling although top 571enough guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
