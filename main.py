a3812bc2f9009002701b236931eeee14e7d854f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:56 +0000

    suffer way properly tread fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_complex(array):
+        return complex(array[0])
