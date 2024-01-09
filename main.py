b5dc9624678797cd06c1576f38641f471f60abca
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:45 +0000

    exhibition through noisy quit yet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
