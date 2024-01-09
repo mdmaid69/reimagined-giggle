df30cded112ef6072a86f8cec36391d6c8a29659
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:22 +0000

    that music boat school offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
