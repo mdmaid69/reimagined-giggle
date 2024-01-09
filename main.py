c5a14ad01cb3150dc919d4f92593c40ced4c33b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:47 +0000

    door intend success continue bunch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
