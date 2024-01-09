e9576102d8356db556c0127d75027e739fb2c2f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:26 +0000

    deaf figure terrible phone it

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
