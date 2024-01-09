06a9b211e8d20104c2499fafb90d3d696a8b0914
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:44 +0000

    both holdon ourselves upstairs proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
