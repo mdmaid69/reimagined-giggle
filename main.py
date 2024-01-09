7bb9493c9dca0ebe1363c7861f8d9e7512bac234
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:30 +0000

    callat rid takeback project count

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
