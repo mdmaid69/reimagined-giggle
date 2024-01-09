53cf8716ddd57f3bf3acf291324e97faedcc0ed8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:06:51 +0000

    minimum peaceful goodbye destroy able

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
