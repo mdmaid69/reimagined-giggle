37c3f1d6d4b45bd68b3b32197e1ac5189c0e8fb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:42 +0000

    miss juice place roll comeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_str(array):
+        return str(array)
