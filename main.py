89c87223fec967fa62a80337fd2d8d3ca9f8a596
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:52:34 +0000

    winter low freeze immediately trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_date():
+        return datetime.date.today()
