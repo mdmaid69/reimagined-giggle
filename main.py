c0d0662acccf09bf0e50c7fc1afa484d73f82a68
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:44 +0000

    politics region experiment scrape insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
