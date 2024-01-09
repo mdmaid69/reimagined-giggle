88662c5404e58cc6b8b290393477cb8b230ccc3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:49 +0000

    legal contrast warn space slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
