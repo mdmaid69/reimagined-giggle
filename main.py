57287c0b73b86afff6d8218357b066a23f38c071
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:37 +0000

    fashion ring explain conclusion familiar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
