c7a6fa908f5ed2339501f4f150c06df5ac00c520
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:25:07 +0000

    takeoff challenge house mind past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
