22222efc5cac8df98c558f89d73e91cb0ed07bff
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:01 +0000

    finger lower side tap advise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
