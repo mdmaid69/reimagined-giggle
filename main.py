6a27e9a0ae58f209781de5812e5364a931bbe4c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:59 +0000

    order powerful hand instant ourselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
