58a8124cee9cafb6fe48fd34fd414ec5b632543c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:59 +0000

    personal treatment mistake near either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
