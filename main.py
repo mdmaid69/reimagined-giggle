f6cf7c29df467a805cee00fe2cf3698523273665
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:20 +0000

    free slice post bush command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
