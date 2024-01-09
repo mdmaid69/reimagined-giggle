eda51397ac6ccbe85f32bd120a83e6d4b636fb19
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:02 +0000

    concentration serve grateful exaggerate machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
