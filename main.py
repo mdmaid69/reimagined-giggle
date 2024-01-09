be8570f0390a04a67f12f83043bf3f6f31562016
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:54 +0000

    minute Nn pot mistake temper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
