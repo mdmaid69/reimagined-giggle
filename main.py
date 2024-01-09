5c9aaa2a0b033ca520aa119b883e5e4e8c8e97a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:41 +0000

    chance refuse start spread late

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print([x**2 for x in range(10)])
+import collections
+def create_stack():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
