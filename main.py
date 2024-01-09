5d527d18cd2ea4bf7e2eed382f66e1494aaa220e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:34:17 +0000

    lesson development wonder nervous poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
