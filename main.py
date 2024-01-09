491dcf5774c342026b57da60bfc70c846c70dd68
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:11 +0000

    create badly can risk moral

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
