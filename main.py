afe8b745830023b43abdfa7943366e4f14524602
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:51 +0000

    thorough march punishment obvious connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
