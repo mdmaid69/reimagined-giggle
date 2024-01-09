0280bb434b79993847a90aa221c380f169fcd4a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:55 +0000

    noisy convenient correct collection marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
