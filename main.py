73ccac6c24eefeb6b78e8d7e1e2a93e38d8222c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:37 +0000

    direct science floor imagine punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+import numpy as np
+print(np.array([1, 2, 3]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
