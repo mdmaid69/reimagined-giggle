5fdf91e86fb3d8bfb03f2a967649b260b9d33454
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:10 +0000

    brain sense delicate empty 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
