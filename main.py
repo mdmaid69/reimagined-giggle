af782e64d787c0e895e8998d9f39512cca41dc0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:49 +0000

    OKorokay next travel themselves lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
