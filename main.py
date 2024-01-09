9f9b9970ad81e6bfeb3dd668665e77cdf21229bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:31 +0000

    strike later disappear cost peaceful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
