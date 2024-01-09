f8aa6c05ace0c5380713481bd62c59a2073d8c05
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:33 +0000

    which meaning trousers that crowded

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
