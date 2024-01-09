44ccffa3897f64c752004c4443f4a4c8eb41fbf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:33 +0000

    raise marry inform remove sting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
