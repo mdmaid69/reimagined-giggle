a8cff2bad53fd374aa3154b08b47617b7da95a54
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:21 +0000

    relationship comein area total discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
