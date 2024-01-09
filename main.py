c71953036a97999842f5dc9dd3bb0b82ce95a18f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:28 +0000

    loose hour excited statement opponent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
