65606fb25eb97085688ad036279f369ae68a8338
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:49 +0000

    land bringup chat scrap alone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
