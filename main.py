5b17bc89aadb80c38973bc14ada3fd23b7734339
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:39:39 +0000

    vote breakdown comeout care mainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
