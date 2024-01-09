8f6e223cc31c5f50751f3e6744946e863eb07800
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:37 +0000

    pinch whatever ocean gooff friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
