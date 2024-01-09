93dfbc75e7b4b8dd1c687d94acc3aaa06e59b2b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:44 +0000

    target yours deliberately size much

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-print(datetime.datetime.now())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
