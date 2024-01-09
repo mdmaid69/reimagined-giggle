3da6f95f2de47ff71b31a43440cd65e56c6fcd62
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:01 +0000

    bad persuade communicate organize measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
