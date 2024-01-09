3ee16159a801448c6d29785fa0123dac262c99dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:02 +0000

    embarrassed scheme sale appeal moral

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
