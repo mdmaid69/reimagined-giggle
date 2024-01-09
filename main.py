e090b29d330fd6c7d7c3cfef43c8520b3db98949
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:54:35 +0000

    while sign floor summer moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_current_directory():
+        return os.getcwd()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
