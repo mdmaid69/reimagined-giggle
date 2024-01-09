f084443c0ed5541b32e8f47444b819bf7007d378
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:47 +0000

    exaggerate guilty appeal public about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
 def get_array_as_set(array):
         return set(array)
