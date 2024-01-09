0c03f6c46232964c5bbc5e32978d0128faad0e03
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:09 +0000

    willing birth general straight sign

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
