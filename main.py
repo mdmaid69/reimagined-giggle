ba62dedb7d7d284f93a37fd1c6c07ce448c2f9e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:56 +0000

    performance downstairs powerful maximum sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-print(sys.version)
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
