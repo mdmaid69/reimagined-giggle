d2b598748fad394b322a7499354d39d5c99ca0c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:05:08 +0000

    clothes learn official reject many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
