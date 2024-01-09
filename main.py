f252084ed74b27fbdf46e0e4d7e50b6c9ac60459
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:34:47 +0000

    juice heat impossible calmly worried

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
