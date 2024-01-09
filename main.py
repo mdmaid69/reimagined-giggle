f5ee37e25402a164ed386563aff33e3ec26ffc85
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:39 +0000

    godown me breakdown petrol on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import time
+def get_current_time():
+        return time.ctime()
