57681d4ef85e733bf54a78592184d5e3f2d14b95
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:45 +0000

    look some programme wire increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
