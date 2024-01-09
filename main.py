ef8c1a4e17de5f30ff7a26991775bb375d443deb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:50 +0000

    half everybody port avoid calm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_work(force, distance):
+        return force * distance
