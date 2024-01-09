e62ff9534e4874086530ee5e0c22b9628e4303e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:30 +0000

    appeal recently my around bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
