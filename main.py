1ecdee124a78fd358af5fb107052de40af16398c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:08 +0000

    pupil join switch return total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
