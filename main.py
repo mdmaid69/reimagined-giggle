de97c9398ca4f7f6b035adceeb02af5b5f117cce
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:00 +0000

    crazy plan worried thumb goon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
