3d35acc4f5dfdfad128691194414ef5c6ceabd9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:41 +0000

    use pause praise do stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def add_numbers(a, b):
-        return a + b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
