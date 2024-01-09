d91a231453ee1a879379d6d177f7b6739474b3e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:06 +0000

    mad point behaviour style even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-print([x**2 for x in range(10)])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
