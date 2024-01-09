2660607665e8d848b9a15e6fbe7a22efd5353f4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:06 +0000

    actual land our open challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_index(array, item):
+        return array.index(item)
