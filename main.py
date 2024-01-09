2dda4e12e33cbddcfd1a5217986e4a04c03fafa3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:48 +0000

    any burst break switch extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
