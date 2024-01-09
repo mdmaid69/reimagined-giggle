c9b5ef25e0e5d4551275c423b0b49a849eb9414e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:26 +0000

    survive pocket luck pencil string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
