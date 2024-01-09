26183dc93ca1a107f114cad238fa3b99044f61fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:09 +0000

    film unkind increase talkback schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def is_palindrome(s):
+        return s == s[::-1]
