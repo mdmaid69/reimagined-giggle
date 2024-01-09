1d603604142a5e0343f8395eb514ff862d0e7f3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:55 +0000

    big vision poor further short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
