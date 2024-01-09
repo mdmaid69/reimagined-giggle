74c7d593328f92c29bf6b4f6ba52338086f8a3f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:19 +0000

    shine plain uncertain OKorokay keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
