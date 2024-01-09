2dac8ee18ebd47f5d4c1aff249b28dfb6ffdb604
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:56 +0000

    hurry actual surprised sometimes stretch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+print(sys.version)
