bd05b0e1ef0d1d97ec8161eeb576a3706b74171f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:01 +0000

    burst mistake might cough shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
