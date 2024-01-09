99b1c83c0ffb775f54d8027a1cc9dc0053101d75
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:37 +0000

    sky cause it smell direction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import platform
+def get_python_version():
+        return platform.python_version()
