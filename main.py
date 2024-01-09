1e5b4b14c119e73464f3796793507c3bf22bea90
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:40:29 +0000

    less mix close sure necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
