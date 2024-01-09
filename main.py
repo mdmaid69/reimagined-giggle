fe775183d41bcaef0a718c8ae9ff9ad1ae7b535a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:16 +0000

    ill blind loss pick belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
