9ebfd9e5f54338e56f3b6372b20c0111e11c070e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:52:34 +0000

    plan deliberately store row crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def extend_array(array, iterable):
+        array.extend(iterable)
