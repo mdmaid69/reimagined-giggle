2560eb8c2e9d00abc8f78e2849c956e2eb6eae07
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:52 +0000

    long doubt operation stop either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
