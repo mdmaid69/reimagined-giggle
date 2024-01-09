b6aca1d144f2c08ba33833ca70968f4149fb29c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:54 +0000

    outside problem innocent hello lock

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
