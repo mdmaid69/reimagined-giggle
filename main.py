4dee33e1869480f94fe698f6061c4bb6b427842b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:06:45 +0000

    underneath prefer relax depend say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
