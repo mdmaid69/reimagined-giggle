a5489e0df60454fb9930dac38900dd7aa7c5bccf
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:37 +0000

    air brush rush rather smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
