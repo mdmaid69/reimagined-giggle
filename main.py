dabe5c3807312caac1530e9d9c43183a7a93b66a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:16 +0000

    tragedy bringback spread weak bit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_bool(array):
+        return bool(array)
