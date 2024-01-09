a37b8f260ee7c825022bb222e576f91f76b5aaf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:57 +0000

    back strike agree busy thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
