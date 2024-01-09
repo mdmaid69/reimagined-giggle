e6ff1a1147e2ce9135fcfd69a551c8415558268f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:56:11 +0000

    transparent impossible before glad themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
