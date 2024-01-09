1fa981f6b8cca5f964f8322b916b353e71b6079c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:27 +0000

    shape waste over thinkover worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def add_numbers(a, b):
-        return a + b
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
