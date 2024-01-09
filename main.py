af84a134118f220d8bdc64c76eda9183d2950f62
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:23 +0000

    damage field brother Jj lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
