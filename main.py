45e8c6952c6412076cf2960cba88efb473b4f6af
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:54 +0000

    better respect wing wish present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
