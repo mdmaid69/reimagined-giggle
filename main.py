6b2a16cc275f2dc895005f05309fac7706b79ba3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:33 +0000

    important Gg reach punishment thinkof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
