ea7c898f68226f3417076b9e0337dc651964568f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:50 +0000

    winter average error split right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
