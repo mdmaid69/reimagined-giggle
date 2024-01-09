4616d1550f792e1c6e3a83972c3e4172db0eed5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:33 +0000

    order pick telephone past forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
