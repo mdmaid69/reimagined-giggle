93705a943fc9d08ebc215bc6e0e5528da7d53926
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:40 +0000

    spring punish exam later turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_queue():
+        return collections.deque()
