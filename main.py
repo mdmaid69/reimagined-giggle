b7644cd9bfe1d9c9aafd8707be2fc533244d578f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:54:43 +0000

    to year day trip responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
