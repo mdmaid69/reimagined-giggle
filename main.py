3013b7766de033c09332e432878a0302e6f7b2ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:30 +0000

    couple sing kind shut less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_tangent(x):
+        return math.tan(x)
