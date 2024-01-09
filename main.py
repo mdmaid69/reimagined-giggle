3c2b8d6855fd496f75461a6497c191102f06ec92
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:14 +0000

    begin minority influence wear family

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
