5b5e4a2c0991a9110bb4f464f067db6391fc7060
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:18:23 +0000

    tap turninto or especially history

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import time
+def get_time_since_epoch():
+        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
