aac73c15b3c9443d43c3d89d63ad1dda663d9cf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:36:36 +0000

    silent contribute delicate exercise on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
