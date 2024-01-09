b7df3f1be125067cc44c1b039beb9087af44f5e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:45 +0000

    fire declare tight nice meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
