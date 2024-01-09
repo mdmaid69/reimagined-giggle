f6b4af97253f7d082eb5cb2bb9195d1b893f6b23
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:53 +0000

    schedule lean quit sister actual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
