5ec9a61c7b792775773cad0c41e91a301a23a2db
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:23:11 +0000

    round goon whisper theirs share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
