584091795e4ec91fc430db134a37adf7386f7636
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:51:10 +0000

    sport cold kick problem bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
