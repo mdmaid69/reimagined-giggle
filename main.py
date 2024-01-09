c75f07d16263677475e812525b7f84e836ea4ec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:34 +0000

    rate limit burst desperate another

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
