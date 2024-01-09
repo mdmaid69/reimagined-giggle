0b8344807ccea6434f7ffcb6b181831413ed7275
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:48 +0000

    her kick habit somebody turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
