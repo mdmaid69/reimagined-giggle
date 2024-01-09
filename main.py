611d08ee82708ac1d61a882fb7185a1987c3e407
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:36 +0000

    tape record completely mainly field

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
