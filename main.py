b23d5e1c49441ff1b3db690be210c0b700f4a709
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:58:07 +0000

    smile convinced interested improvement along

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
