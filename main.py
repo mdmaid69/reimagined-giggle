de7c1b0b0fe6f8ef0005307aa9a887b3445a1792
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:42:41 +0000

    scream prize die block experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
