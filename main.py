7578e8e2658bc462ac00e9cb0138bbf97b91d51f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:28 +0000

    ball know front alone minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
