613044f516ce8239bf800c1cdb558ea03c46c155
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:55 +0000

    cover better juice at autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
