c313e7ecea28c54e101bd6b361691254190fc2cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:41 +0000

    company city fail possible image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
