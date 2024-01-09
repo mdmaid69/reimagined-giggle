07e3a6735d7345a7c25088d177f7df45fc2ca59a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:09 +0000

    shopping realize makeup art unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_binary(n):
-        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
