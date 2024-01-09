7fa411a54d3951bb38d5f010124759da55998f60
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:10 +0000

    appeal regularly slightly catch official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
