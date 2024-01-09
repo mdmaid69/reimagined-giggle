af13779fdf075cba7b119cfc20f67f6f5deed5a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:37 +0000

    politics broken offer path suckme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
