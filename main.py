2297add2fddaab5c50d2aa0be65f72886b051c03
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:31 +0000

    smart fold quarter team path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
