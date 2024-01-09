3a41a2311c84d39761bf9ff24847b70749992dd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:52:22 +0000

    conclusion roof innocent declaration foot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import datetime
-print(datetime.datetime.now())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
