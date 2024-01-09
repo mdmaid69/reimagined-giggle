d0b5274f9bb715c894317893d3642bca4356b157
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:29 +0000

    autumn pity wake issue guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
