52e16c9599c5cbd64f9ce52e9f522c469aaeb039
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:45 +0000

    limit runover through leave schedule

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
