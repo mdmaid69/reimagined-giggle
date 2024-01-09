49b20cf14ca6bb3189408cd5ae5a83b449f41a08
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:10 +0000

    used bound whistle even challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
