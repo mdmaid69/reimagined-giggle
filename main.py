df9eeed05e5c2ad06f29e7901f3031d20a6a60f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:28 +0000

    snow offend sign system father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
