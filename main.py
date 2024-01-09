cdd1b0cf7492f31aea56b849fa21410ea056e8eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:47 +0000

    doctor marry term walk sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
