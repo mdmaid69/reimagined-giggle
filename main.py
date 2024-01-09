b656d876f2804e38f46faad7af46d1f92aee6097
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:33:10 +0000

    juice tidy deserve suggest image

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
