d6654791573e750707c9367da60498aad6a0d75b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:21 +0000

    risk part city display shallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def clear_array(array):
-        array *= 0
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
