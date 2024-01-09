24c73632a33298083f90317f14d50a03cf6cb8fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:22 +0000

    goup violent advise progress constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
