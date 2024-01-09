00d16e70748984f246e071af26de0b1cb4e6b112
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:00 +0000

    Ee mention whichever expression help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
