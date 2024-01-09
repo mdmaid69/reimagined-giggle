3529f83938945f50dc69f89cc803891147f9bb91
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:18 +0000

    reserve number search consider glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
