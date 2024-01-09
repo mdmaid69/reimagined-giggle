e63ce2e3eb646c257e508863bef0732c4dafb14e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:47:26 +0000

    scrap soil quality whenever shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def exit_program():
-        sys.exit()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
