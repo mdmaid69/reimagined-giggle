b1916a8d66a9547c65be2b83dd7bfeded3a9758b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:25 +0000

    programme proud this callup prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
