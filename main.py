6795703b42cce9851fa99c9054bfbab34d169646
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:25 +0000

    wonder getoff theirs song far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
