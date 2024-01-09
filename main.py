113b3977e1d110d015612b558414c25f1e9a9fb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:00:24 +0000

    indeed eager transparent level poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
