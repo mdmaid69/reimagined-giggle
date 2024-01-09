c12bfd45c2eca8b7d7020822dec5ace5358631bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:06 +0000

    tooth shame turn spite its

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_power(work, time):
+        return work / time
