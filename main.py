4d19377af2793173b6cc0d3bbc562955ddb01a70
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:55 +0000

    dry represent along shoulder criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
