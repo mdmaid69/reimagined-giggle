42a9828184c1e0dc4ed61dac98c2df244ea33247
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:32 +0000

    avoid welcome figure sleep report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
