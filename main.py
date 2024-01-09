8bc226f7ff6cc9954d225377e2324fee7d03de51
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:47 +0000

    turnoff to failure serve lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
