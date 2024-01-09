1ae549a425e9322fb49fdc1b9d8041de10953767
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:11 +0000

    thought land upwards weigh yet

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
