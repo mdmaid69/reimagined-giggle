09f940d4b8d013acc1fab06d4569103097a5e01b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:21 +0000

    comfort church simple best bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
