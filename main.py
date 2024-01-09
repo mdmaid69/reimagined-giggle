d92fe34956af9d3e1db7fc0ffd617ad808112373
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:50:05 +0000

    noisy character accident today seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
