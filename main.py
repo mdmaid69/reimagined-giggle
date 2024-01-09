1243796ca2bdd12716083a03ac188921a78b2013
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:24 +0000

    Ii live choose properly replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
