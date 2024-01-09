2e3b8653d512cbd6b7546d49443b745dadc73a41
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:48 +0000

    pressure runaway trade laugh believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
