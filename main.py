b1ed2845871a3609a20d85bfe4c95278a5ba3dcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:13:01 +0000

    exaggerate no blame child target

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
