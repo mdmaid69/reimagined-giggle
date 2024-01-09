78bff5d59732d9e22f10d85f1fa0baa9579539d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:38 +0000

    change game skill respect right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
