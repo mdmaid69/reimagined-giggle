a1ac5702f39d0bb9200e52ee8bc845223669eec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:41 +0000

    use unpleasant bottom tie couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
