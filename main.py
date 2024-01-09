dba3c7bf0e4db11e83387f927bf48edbf2c003c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:36:18 +0000

    declare Tt will better strike

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
