901252c8ab0479683d00365473d99874fa3bf005
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:44 +0000

    young home world clear rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
