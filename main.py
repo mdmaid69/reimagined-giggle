2972c82974140f4dcaf99cc03ccb4c9eae0fca72
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:31 +0000

    after keepfrom interrupt shoot close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
