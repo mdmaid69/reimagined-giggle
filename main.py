a90eb087039b19aede5aa73240d782407e5f87f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:02 +0000

    organize them and collect ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
