b84859b5a6eb81afaa52fb02bfb25f34ac4dc072
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:26:54 +0000

    collect skin summer occasional better

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
