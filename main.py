cdb32981a03090c0eedc7dfb0973c32d263ba9e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:27 +0000

    beside smell knock motion innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
