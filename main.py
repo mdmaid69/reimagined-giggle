bd1b50177a946bf56a2d29a62a439895acaf9708
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:46 +0000

    callat basically fashion race moment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
