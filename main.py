dfd0247d2fd3565992a7ccf2f96163f9e71eead5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:17 +0000

    ticket give but sign press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def add_numbers(x, y):
+        return x + y
