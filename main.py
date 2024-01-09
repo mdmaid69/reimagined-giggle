d5200878d1478c1258a418599a4a42457840e12c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:08 +0000

    interfere number often unfair lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import matplotlib.pyplot as plt
   def plot_bar_graph(x, y):
         plt.bar(x, y)
         plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
