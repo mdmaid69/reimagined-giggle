a1eee0b2202fa37f1b59df59de200912925fc54f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:39 +0000

    wish walk remain base exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
