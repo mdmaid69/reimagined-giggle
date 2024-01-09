693a69978f9bc87b9fec740320b7cc6fa1ab30f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:45:58 +0000

    scream wind warning quick aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
