69bca15d3868c18722971a30dec0507ca8d83e50
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:34 +0000

    angry round upset possession section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
