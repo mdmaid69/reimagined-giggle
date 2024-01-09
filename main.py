0208b4170ed9c0cb9fe93ebcf6fe56d215fab215
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:46 +0000

    564encourage water comein wind close

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
