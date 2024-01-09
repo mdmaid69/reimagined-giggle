b1892209efc3da7016baeb0e935a71e767ccee1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:11 +0000

    Rr very secret hurt development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
