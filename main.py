c294e33c9bb3b4cbb8ec25479c7d6fa09ae881f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:39 +0000

    drive creature early breakthrough port

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def clear_array(array):
+        array *= 0
