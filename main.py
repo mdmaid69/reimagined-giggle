0c36364b361292eeafc2d92d034b666b75eb89f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:07 +0000

    characteristic thirsty responsible colour to

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
