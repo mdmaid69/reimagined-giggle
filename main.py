285ca264b651476eac7d5da09045f5d5948fbd5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:41 +0000

    busy communication pollution sharp Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
