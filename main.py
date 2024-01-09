1e7142e8c77cc3252ebc6d4fc6bed5a0b953201d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:14 +0000

    before board tend worried why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
