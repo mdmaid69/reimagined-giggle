738cd1a41f9af7013a54eb8e11f7c8ac4691a8ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:10 +0000

    tight ashamed muscle search interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
