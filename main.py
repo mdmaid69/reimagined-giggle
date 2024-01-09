cfa069aed8f4b2c818311b62ecbbe34cf42d0142
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:03 +0000

    zero power figure present disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
