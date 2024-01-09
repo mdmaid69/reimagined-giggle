bf761a5344759ebde17fc4c7ab992df8be26638c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:21 +0000

    furniture woman seem start objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
