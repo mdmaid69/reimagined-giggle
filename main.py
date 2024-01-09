e4ae274c90b0d26e7814269304f5479194621484
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:18 +0000

    with smile reply storm sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
