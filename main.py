7b757f9d31f01e3a4ba8047ae3b0ae71f3e18921
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:52 +0000

    calm store lie trick once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
