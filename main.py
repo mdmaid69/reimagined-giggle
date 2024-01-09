91aa1c90fef1b2b3a1089fa6621c8e3a1c56238b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:15 +0000

    available nervous aim will tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def sort_list(lst):
+        return sorted(lst)
