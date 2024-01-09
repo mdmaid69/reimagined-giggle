1210824a9bef86142b01030ca0489ef36b3c6a66
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:01 +0000

    advantage show OKorokay weather borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
