f388ca0d8ead7263f86356bee9688e529aafd782
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:36:15 +0000

    grade travel aim right fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
