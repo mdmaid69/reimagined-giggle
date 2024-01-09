2e794ca89f090f780fa80a9314b8f820fa95ab6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:45:05 +0000

    steam strike pipe spill middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
