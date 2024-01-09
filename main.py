a13d5570555704eaa75fac2efc13170e67e8c741
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:29:16 +0000

    gift memory characteristic active schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import os
-print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
