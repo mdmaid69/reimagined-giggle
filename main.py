288df1fefa8094ef8ad27613fbcd9091ac11d422
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:08 +0000

    right charge 179bite convinced real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
