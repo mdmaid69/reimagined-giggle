466fe7cee8e5a60c56c67e60e746daa43af219e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:35 +0000

    question eager evidence relief meaning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
