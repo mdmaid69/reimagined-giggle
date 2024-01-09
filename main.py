34ca727d7f36b7c4a3715843704a9244c3a72cc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:35 +0000

    stupid confused Mm lead bush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
