c05335c3774801da4c85b707fa4c87a5cf4e6224
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:47 +0000

    pile computer wherever bag tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
