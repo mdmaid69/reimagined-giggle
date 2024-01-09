ef316321d86042fcc4b54cc7a8ba9b92bc0ffb16
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:16 +0000

    divide left afford punish used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
