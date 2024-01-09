879c7908303e594e2eb03da363a501ee9e37ee13
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:53 +0000

    education practical relative about depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
