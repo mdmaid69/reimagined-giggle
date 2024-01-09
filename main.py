c87e78b3e6873d3e90d8ef489ac312b464c6dcab
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:51:40 +0000

    protection content attitude street some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
