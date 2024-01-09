7a2e3bbdaa5fe4b04a40f70c128bf6295be51236
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:36 +0000

    official theirs school praise strict

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+print("Hello, world!")
+def find_min(lst):
+        return min(lst)
