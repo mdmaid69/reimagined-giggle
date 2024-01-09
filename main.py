a694f4f5cd080280c2bb75948b1cdd8f476bec30
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:44 +0000

    opposite church below fold shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
