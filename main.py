a5cb7a2c58a5ecad940222c193f364ade0ac9fd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:29:27 +0000

    save spin lean until become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
