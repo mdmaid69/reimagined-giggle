6d9bd94e5f43a002690fa2b7d301f66483eac3a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:45 +0000

    grade fence moral recently able

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(lst):
+        return min(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
