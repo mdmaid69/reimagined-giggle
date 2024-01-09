ed3ee769059ef9f8650f4898963f93eb13cdbec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:12 +0000

    public sometimes turnout event separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
