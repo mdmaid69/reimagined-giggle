6de0535db975a11c16fb980d7960514e9bd95bc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:58 +0000

    very maintain ahead phone fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
