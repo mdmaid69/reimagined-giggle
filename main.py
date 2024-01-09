f596fee3632a15fbb3a643c968c595273750b004
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:57:03 +0000

    recognize collect confusing takeout quality

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
