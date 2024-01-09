e5e7ddd55bb363fc84506686eb79541884819ac2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:52 +0000

    patient know talkback progress estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
