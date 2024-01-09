e3d70fcb4b2255330c4e755b3d67dc6a8949646b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:57 +0000

    callback invitation getup lookup backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
