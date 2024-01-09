c93e28e1f4af28b7a19c84ce63df2392473615f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:32 +0000

    dinner club phone tired score

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
