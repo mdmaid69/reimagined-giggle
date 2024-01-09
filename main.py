28a1ad0a7f8bc6a30835d68a8a4ca2b4b0c4a696
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:46:22 +0000

    holiday friend leg holdon true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
