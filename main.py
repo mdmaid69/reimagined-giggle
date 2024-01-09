7fa0776175ff9081032a08e74815c5b2160b4e3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:38 +0000

    calm advice stretch flight violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
