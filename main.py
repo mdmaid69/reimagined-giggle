6f58b0a4bb480c31dbce324f105a67ab96ea772d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:25 +0000

    hard perfect point brush reject

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_min(lst):
+        return min(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
