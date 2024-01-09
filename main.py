3a1461c6905c6dc78f135bfb7fd85bd251ed9085
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:35 +0000

    bye early full destroy talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
