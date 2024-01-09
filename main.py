d0d82c506268f5d8c5f8be869c25c5ce349984e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:07 +0000

    anywhere region underneath circumstance sail

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
