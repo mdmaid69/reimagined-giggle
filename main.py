e3f787f69dfc206702c4a4e55de15ed6ee417f6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:16 +0000

    reasonable contribute say damage pleasure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
