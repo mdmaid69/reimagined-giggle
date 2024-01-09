ed8c2210f7612479b9d60e815982e3a117be652d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:50 +0000

    lesson comein fast morning mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
