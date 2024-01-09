0861a60a41fe5b851ead0781b1b90ed16ebe8ff2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:05 +0000

    breakthrough unit invite guess declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
