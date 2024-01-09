9551f51cbf6a9f384ae35dc6cfbd979efc096cd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:25 +0000

    challenge commit concerned information quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
