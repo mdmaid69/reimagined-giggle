67252a5d87e133ac02e14edd3efc475055132134
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:54 +0000

    through comein much decision anybody

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
