f0fd1a000918fbea3ca6bf3d4bf54e7caed5eddc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:43 +0000

    clean handle promise plenty declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
