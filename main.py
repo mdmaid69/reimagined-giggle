c605b9499964bb10991a1ff362e390db192a2480
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:46 +0000

    none whisper mark guard ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
