e33b47b7b5062e9142d1cadc4f582c7493f85b6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:08 +0000

    event afraid mood storm violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-for i in range(5):
-        print(i)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
