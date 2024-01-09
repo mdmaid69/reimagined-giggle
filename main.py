c9554c6680c529a702f10f81880209ce0e72c001
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:24 +0000

    feeling preparation tooth substance eye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
