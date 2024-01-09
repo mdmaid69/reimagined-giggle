b79042901e4c2b136af43bde2b07c21e9244758b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:32 +0000

    guess school situation song perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import getpass
+def get_username():
+        return getpass.getuser()
