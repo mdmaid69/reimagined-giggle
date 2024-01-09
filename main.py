f39d9050d4616112dae48601dce03905e09feac4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:50:05 +0000

    government complete month equal least

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
