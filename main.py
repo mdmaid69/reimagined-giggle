8ca88bd2748027a933801bfcd5e3240069eab63b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:20:33 +0000

    talk something education Ss translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-print([x**2 for x in range(10)])
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
