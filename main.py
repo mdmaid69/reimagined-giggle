4f3a9a16e4ae50384c60d015194b74bf60515aab
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:25 +0000

    positive no knowledge wheel far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
