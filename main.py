1ad6a2417099750bfe3aa4c034c2d498f9c49b45
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:02:39 +0000

    challenge single port breakdown thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
