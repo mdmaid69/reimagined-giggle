f3461b2aae457605066b4b5567d2cf9bf42dc151
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:10 +0000

    situation material serious mess perform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
