e9b5d34f55dbf40be0c1cb6866d684c4be4fba0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:30 +0000

    Yy personal exploration inside practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
