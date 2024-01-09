bc0329e86f068e47d320477c6a73901545c2db30
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:53 +0000

    separate race comment clean tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
