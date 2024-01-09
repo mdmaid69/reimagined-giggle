82f0f83a74e02d8816c77a3d3c099e83b5e97910
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:03:27 +0000

    ring knock increase check under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
