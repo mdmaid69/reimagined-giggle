e090f8fce9485d75b38fc323f55f86b606c0d15e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:25 +0000

    generally area closed estimate share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
