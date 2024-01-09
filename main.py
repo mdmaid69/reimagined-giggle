96c6892e1cd0e5d52b0dbafb02ce705bccc7af65
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:58 +0000

    worry talkback behaviour regular emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
