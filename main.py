d10b63ee3c98baf8165872eaec95257169b0a831
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:36 +0000

    hour correct cover get risk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
