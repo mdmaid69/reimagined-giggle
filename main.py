7e0423170d3d2147dcf311309a2f4a6af24c8fd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:43 +0000

    colour sound good wake rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
