0c09ae43c2e8c366175a0ce3db885862dd14bf80
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:18 +0000

    train do nervous meaning rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def divide_numbers(x, y):
+        return x / y
