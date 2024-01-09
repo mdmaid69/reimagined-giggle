33593430739687ea0405df74aed3a89ea4fa056f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:58 +0000

    lesson impressed charge afford wall

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def convert_to_octal(n):
-        return oct(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
