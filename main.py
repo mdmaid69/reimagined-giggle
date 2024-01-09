274c0f361734c659df58d175fe6ab567b67ca76b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:56 +0000

    instead burn priority past 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
