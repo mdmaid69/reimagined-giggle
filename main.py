e8fa2d1dc606d4dcaa004cd23ad75971743803df
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:05 +0000

    start copy 573entertainment force book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
