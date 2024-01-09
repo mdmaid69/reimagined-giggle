afafd4d28a259e1a1974ad4777c020c7a0bf7cbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:49 +0000

    absolutely wise instruction attempt week

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
