261b995ea819003a0ce1b97755017c34bcaa93da
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:37:02 +0000

    week niggaacademy spot seat steep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def find_max(numbers):
+        return max(numbers)
