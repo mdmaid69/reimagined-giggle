ad1c7428dd2ef90442700a15b0fdc0554a67330d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:55 +0000

    already shock thinkof thanks movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
