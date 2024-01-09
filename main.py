0f1ae1003886ef07235b96dc8e85cd2426384089
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:23 +0000

    breath experience false ordinary criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
