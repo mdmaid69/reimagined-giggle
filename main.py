bbc7f9fb3292ad154e133609bb9d2c60ee2f26f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:01:37 +0000

    if science bill past suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
