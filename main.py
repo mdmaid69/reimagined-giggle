bc6a24aba96269e5ef7323dcfb9605cc99ab95bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:53:38 +0000

    unfair hour urgent combine relation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
