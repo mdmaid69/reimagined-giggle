7cfc3424211094b75b3fa7d694e732d6a12a3a6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:31 +0000

    probably recommend comeround somebody after

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
