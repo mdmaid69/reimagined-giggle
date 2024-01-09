fd90a81ed78a0b4c42f3d17a105a067f73312260
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:04 +0000

    acceptable finish problem another excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def remove_from_array(array, item):
+        array.remove(item)
