2f8e302a653791beee5f11e05ab1adbe6c780de0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:18:25 +0000

    ball immediate any body depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
