df3944d8a87eeb0805336db8346d61878dd06805
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:33:06 +0000

    many cheek ahead false argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def square_number(x):
+        return x**2
