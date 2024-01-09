2ef0cad23730b77c100c680856b9bc79169fe0c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:49 +0000

    object ring post total identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
