bc012705af611947427f2220281e60a31beed8c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:07 +0000

    control edge ours hand successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
