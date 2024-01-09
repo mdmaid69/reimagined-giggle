c4123db6fc4b3c30056b568a7085e0622bef8cd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:05 +0000

    steer crowded back sail past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
