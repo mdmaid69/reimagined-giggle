b1167b5e7ebeafc6fa2e9cfccfe877ec25ec801f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:38 +0000

    drawing plan OKorokay compete search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
