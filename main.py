16f24f56ca29349e2cbef6ef9eff57a1643a1cc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:36 +0000

    shopping company reasonable when base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
