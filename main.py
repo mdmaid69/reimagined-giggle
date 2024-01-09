fd0ab289994f1c726103291774308b18da16785d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:25 +0000

    sensitive own keen plate custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
