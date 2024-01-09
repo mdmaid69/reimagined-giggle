2edf4c4d5447a0d94a4cb96089100c7b643d9671
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:10 +0000

    board substance each exist neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
