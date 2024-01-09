ce8ac4bfe369e09052daee016b39f2fcc5dcdfae
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:10 +0000

    unusual gooff deserve free as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
