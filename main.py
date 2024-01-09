47595c81355211c8361d54d3f4a39cd67d2886aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:50:00 +0000

    off somewhere interested hair complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+print(os.getcwd())
