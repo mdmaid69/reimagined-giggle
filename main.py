c9a358f0042a09c185f87d2657ab1a86be82a4e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:39 +0000

    forever have sign bound although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import getpass
+def get_username():
+        return getpass.getuser()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
