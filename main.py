415210ca64cd169d44960efe308f476a4df9437b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:28 +0000

    rest laugh sign left state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
