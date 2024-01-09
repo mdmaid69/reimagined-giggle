3286fcebda8a8e261266b00ab4063f4347c11089
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:13 +0000

    large pretty wide thanks upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+print(re.match("h.*o", "hello world"))
