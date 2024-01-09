bbf4ff7efadcaad32f06e6af25cf3514be54ec34
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:02 +0000

    pinch conclusion appearance sing grade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
