c978f3c00e5504a5fb9aa18c0027b800bd082daf
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:53 +0000

    behind mind importance cost mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
