13546a3e56ee71138a54c243584d65f0897b003b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:20 +0000

    grateful bridge sell seat sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_max(numbers):
+        return max(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
