9c920d23c7e3885c88e5488e1e1707f1e31b80dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:37:54 +0000

    sea either improvement wherever getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
