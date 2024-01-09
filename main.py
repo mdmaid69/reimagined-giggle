5adec8dcc1e7fc171c66b4722c422c4d541fe27f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:57:10 +0000

    admit improve badly watch lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
