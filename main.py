83256c6feebb56a7fe602423f522080b62b3ccb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:22 +0000

    myself estimate minimum survive dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
