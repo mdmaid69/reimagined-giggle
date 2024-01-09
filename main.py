4bc985519d70d3c186dd77bc8d26e6c291d76ee5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:22 +0000

    force order shake ever tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
