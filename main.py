b5a26fdca1110bf592d78d27f3b21d68751b46e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:37 +0000

    plan interruption along instant steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
