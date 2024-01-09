425a426b810e9e918a769ecffb3f46844e69e82b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:01 +0000

    embarrassed influence count bend communication

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
