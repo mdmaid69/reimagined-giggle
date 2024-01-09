ffe6b7db3048f0ce9074bf7890994ab05e9cee88
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:20 +0000

    as 570enjoyable discovery steal peace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import sys
-def print_python_version():
-        print(sys.version)
+print(sys.version)
