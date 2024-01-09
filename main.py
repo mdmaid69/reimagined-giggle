21b74c160675a6b187d15b73ec20317a350af6cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:07 +0000

    school worry light available possibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
