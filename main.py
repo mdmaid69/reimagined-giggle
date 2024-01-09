30c5189926db4f9606fa9ace3bc46b3bebce7477
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:48 +0000

    lead learn breakout push thick

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
+print([x**2 for x in range(10)])
