e8af45076e132d0ace0177220bb20437d2d57886
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:29 +0000

    it already bear variety sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_array(array):
+        array.reverse()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
