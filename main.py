bf041b0494ee588c2485fb096ed755f1a6d86192
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:08 +0000

    tune really adjective criticize disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
