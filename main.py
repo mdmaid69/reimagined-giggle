9d5cacf2050ba2289c934d3d1255c9458f3ac2d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:16 +0000

    else pour name always television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
