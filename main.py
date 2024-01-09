b2c987307fdc4b3048184135d25387a859b8b16b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:12 +0000

    morning crazy niggaacademy minimum rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
