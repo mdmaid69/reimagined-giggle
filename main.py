2494471d1e1023e7f54cae8f2da7cc51d6ee1866
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:24:29 +0000

    pile tragedy broad his glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
