150228ebb8c7e90ed5414310e23a0de271681b72
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:32:48 +0000

    thinkover keepfrom continue once sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
