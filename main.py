0d35389e7d646d2c5696bf205a5607df91a4998d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:23 +0000

    heavy coffee collection common pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
