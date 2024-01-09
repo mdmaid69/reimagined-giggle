b145cf59e6b7fd97e2a3824c060a1c7531c90c4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:55 +0000

    quality on lie error bus

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
