aadcb98e213844dbfb76537dfa660dcf1569b0bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:14 +0000

    from trap storm tomorrow practice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
