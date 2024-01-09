dddf1851a1e93c65bd630bac844cf7ef5a67332a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:14 +0000

    size brave start write estimate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
