2ad527ab4e2caceadee10d4942d2b141c3e7f13f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:57:41 +0000

    fast give side reason build

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_username():
-        return getpass.getuser()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
