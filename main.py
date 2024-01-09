53ac28f92539bf39a3bded43f0e3d5148e1ccc6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:37 +0000

    moon throw someone note near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_sine(x):
-        return math.sin(x)
-name = "Python"
-print("Hello,", name)
+def calculate_exponential(x):
+        return math.exp(x)
