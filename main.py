fe1caa1fe4407c12d29c7b55090bb7c50686c812
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:33 +0000

    light sorry clothes also under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
