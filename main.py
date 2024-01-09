3483d4451c2e9f46938daf546d6dc75be5419d59
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:30 +0000

    heavy design physical write separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+x = 10
+y = 20
+print("Sum:", x + y)
