4ee1729bd5bbd78a3fd176bda90251938b63f70d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:32 +0000

    Yy my further original cool

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
