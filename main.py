a059b3c887cec69fab7fa34ae0c3e041bd96158a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:27 +0000

    language so beyond up tonight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def square_number(x):
-        return x**2
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+print("Hello, world!")
