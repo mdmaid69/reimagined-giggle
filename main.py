0b03e5259517b3fde6932e47d357e4ec9a6c14f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:44 +0000

    only steady workon thick account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
