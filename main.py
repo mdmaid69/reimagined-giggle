3b0c7f085b0f755b3a8f89b811295583c4117ef2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:07 +0000

    afford callat able breakin flood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
