5cbd004d184cbe4828b50593829ae3f0d97b3b8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:29 +0000

    visit emotion spread first trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
