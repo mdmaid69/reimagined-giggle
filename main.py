5a38dbd1a028da67ce4c8d013842b3fc49c6501e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:22 +0000

    crash prepare cut announce generous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
