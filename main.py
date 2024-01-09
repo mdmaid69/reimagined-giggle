7fd75c500cdd3c416d662b6325bcc3c4b93625dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:08 +0000

    comfortable plant deal at yes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
