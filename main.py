155b2209e4054237d794090123bfc1d5aca467d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:38 +0000

    Gg peaceful agreement basically friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
