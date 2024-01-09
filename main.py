cb7594c6e7f75508997119c6bc1415294e4b2d8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:04 +0000

    cut special report confidence my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
