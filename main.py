b94e31ec770a75ee7f16c620e57f5a50d794a699
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:30 +0000

    disturb callup comment introduction poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
