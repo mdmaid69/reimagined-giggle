8c45ddce7b7a8adff9720da5160df0872e9c1c8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:27 +0000

    afford forget complicated inside maximum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
