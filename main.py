83889c80fc0610973f62695d6ed87066c420cab9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:24 +0000

    stick centre confidence manage see

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
