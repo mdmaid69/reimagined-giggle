53ef2b34acdc14e8609b6849c5b2164cae9a49d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:01 +0000

    long comedown emphasize question mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
