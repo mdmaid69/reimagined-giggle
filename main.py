1019526e6f16466aef79299745b5799790e91375
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:10 +0000

    couple complicated away that clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
