b446df7c8efbae54f6f77319989010f5d94e5eda
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:40 +0000

    568energy in Mm clear include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
