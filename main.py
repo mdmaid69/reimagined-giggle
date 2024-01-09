bd883cbf96d853b22c6893f10fb6e74915544451
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:28 +0000

    question where safe region process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+for i in range(10): print(i)
