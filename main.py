9deadf859c577c7152e2a194f57bdef9485bbf2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:36 +0000

    book repair do acceptable anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
