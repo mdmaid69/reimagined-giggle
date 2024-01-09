bd51075c37e04e6c0e9e04de9272c0b1283e8a93
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:20:53 +0000

    runinto watch laugh along hang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-def print_python_version():
-        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
