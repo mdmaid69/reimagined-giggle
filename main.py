1427f2047d895dd4fab2d8629e3603c560ef8fee
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:50 +0000

    agree indeed juice workon mean

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_list(array):
+        return list(array)
