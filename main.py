cb624bfddaa034ab4ec39222e67f6fc5c3d1eb48
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:54 +0000

    hit OKorokay matter vote rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
