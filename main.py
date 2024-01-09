e760b8ea36a1e85a9a6d2d5dc6ee2113bf30a73e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:10 +0000

    tip protection suckme report opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
