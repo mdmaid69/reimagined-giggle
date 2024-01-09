52fff0bfe3642bb92ef9719e59ad3284eaf31e19
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:30:46 +0000

    crowded secret Ww tell memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
