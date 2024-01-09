51c325aabfbca6deec510407bb9d3c9f6266faba
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:34 +0000

    challenge view getback phone friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import sys
+def print_python_version():
+        print(sys.version)
