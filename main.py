d7df163c82260e3c44a4a828cfd89669fe4ba7d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:41 +0000

    discovery hold tongue visit century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_max(lst):
+        return max(lst)
