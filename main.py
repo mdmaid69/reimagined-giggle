4ee03f9b62a592363082e5f8df66a26bf647d14b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:30:44 +0000

    range rob various emphasize telephone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
