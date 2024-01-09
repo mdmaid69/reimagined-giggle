e39d14a3b23dc9346bc64fb7b20ba146e9f10bb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:00:18 +0000

    exercise bound minor shop awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
