7ada71458d8d617c4b59f9cfc83a4d80cf69ace1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:31 +0000

    trial jump cough ear step

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
