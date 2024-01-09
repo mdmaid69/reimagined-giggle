095ac06a7cfefa6a4dc87168e7f98a7f9982eb39
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:38 +0000

    any book rich damage factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def get_array_as_int(array):
+        return int(array[0])
