0da112c4aae5ff3592ae5322d60813056054f908
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:09 +0000

    chat pretend needle style Cc

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
+  def subtract_numbers(x, y):
+        return x - y
