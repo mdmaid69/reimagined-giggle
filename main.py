b2d655c38600257038a4759f16a22de1fb5d3389
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:45 +0000

    aim problem keepout weigh partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_pressure(force, area):
+        return force / area
