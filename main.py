88c54afb947236543bc9f8dedbfa99ac790c1f18
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:28:37 +0000

    hope listen yourself complete ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
