fae498be7276cb3f807d7d34abfa1aec3fa3d77b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:05 +0000

    Ss suit bored level luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
