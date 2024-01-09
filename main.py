bd488c602afd3ba5b180705c5e1c75ea560a2f3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:42 +0000

    must string only proof follow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
