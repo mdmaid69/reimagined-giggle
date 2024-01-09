e15033a6f864bb16de1acef6eaae7ad320f89d38
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:21 +0000

    turnaway cause quarter consult below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
