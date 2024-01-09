d90911545019a619d272af9f29d30fc04b8ff3b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:46 +0000

    pretty lesson thumb silent split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
