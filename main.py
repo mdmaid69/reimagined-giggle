483b993268f0cd8824cf1b65192e918d1aa0ba91
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:38 +0000

    low comein herself on play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import time
-print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
