1b8d9b2780836bd0ebb8e875c19b845cc67b4e22
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:39 +0000

    run flight science even follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
