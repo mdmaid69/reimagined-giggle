03739e4e41cef4e64dc6de205a300456483a8ffb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:11 +0000

    strange smile wall swallow guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
