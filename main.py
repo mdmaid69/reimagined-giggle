e7083fd1cc6bdf20c380e820a1ec34113019da48
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:11 +0000

    front calloff so away separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
