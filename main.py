399ec14ef64875a218abe6828f2766bc37e0a1ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:28 +0000

    build will work excuse length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
