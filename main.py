3a7bef203fd5389f07b1040b529b18b333353a6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:43 +0000

    Tt crisis freeze convince tense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
