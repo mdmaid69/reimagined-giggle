b4507ca9da35ff51db5fb5714c60a945bd98b9ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:02:14 +0000

    again hardly weigh base stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
