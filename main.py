41a294866c58fb005d4a04f4042b6bec38338a76
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:57 +0000

    inform contain hang Vv yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
