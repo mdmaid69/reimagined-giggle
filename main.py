beb8d95e86ce3d00cf914aeba6d69e5edccabc46
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:51 +0000

    appear change face combine material

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
