b1347953de48cffa8924a91be3a50ded78ff915c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:47:29 +0000

    pleasure comeover throughout workon limit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
