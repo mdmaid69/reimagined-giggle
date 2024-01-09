377edcef5a712f30790530a02b4a459dfc6860cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:48 +0000

    table right agree ever various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
