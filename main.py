bdde0f9469e12bd40a723ca65d502edb94eda348
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:30 +0000

    them broad very dish commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
