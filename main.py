b41688d4e5aeca86514d09696c973066715ed6a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:38 +0000

    thinkover available club funny shake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
