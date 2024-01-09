2ced6276105809eed77ea207ec703f98f42f3407
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:45 +0000

    side dream invitation minimum smile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
