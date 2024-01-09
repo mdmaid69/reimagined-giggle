e2ef4959130cc360c3a81d936af06ad40c5e70fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:57 +0000

    wave word may various lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
