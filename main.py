fcaf005864dd89f3d99c884f632e9843aea0547b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:43 +0000

    house machine moon scare trial

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+print(sum(range(10)))
