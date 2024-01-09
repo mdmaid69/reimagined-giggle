6679f2214a8f433d03a721c1195d95f31d146c7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:43 +0000

    bring challenge recent delicate centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
