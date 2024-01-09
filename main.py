128137d35a0dd177515fbe0a3fae8a63d7b6453c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:20 +0000

    still waste kind shall calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
