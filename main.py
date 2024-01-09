aaa9cc178e6a5a5db0842c63e79b26c3437b90e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:55 +0000

    official consist custom wealth flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
