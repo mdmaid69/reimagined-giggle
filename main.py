036d785f54e42fa220fa158b0091a7679480e193
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:10 +0000

    trick meanwhile admire trip stay

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
