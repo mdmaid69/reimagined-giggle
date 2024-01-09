20963e4b78a196f270ab0b081aeab21e53f2bd96
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:05 +0000

    couple special failure copy sentence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
