9230c7f9af18764eff985dca72a082da86f07465
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:12:23 +0000

    sound object unusual experiment lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
