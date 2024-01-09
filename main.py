5e046397d994ed782ef134b04a51db6d4ff206ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:48 +0000

    key test label bus key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
