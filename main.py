18b348cd5c7cf15f91e5b374ccc61bba4682cc2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:58 +0000

    ocean judge pick impressed anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
