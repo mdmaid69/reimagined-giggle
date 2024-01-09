67ae0ef9752cf98b889aa8aed0dca6d8d9915b39
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:32 +0000

    means language cost generally surprise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
