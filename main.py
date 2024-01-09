d4ab30a635ceea15f48a3a1c06a8888223e566b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:59 +0000

    object scheme quantity ball stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
