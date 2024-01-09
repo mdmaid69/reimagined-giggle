ae5836e9ff3e817ec3dd333c20a8492ae65896cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:29 +0000

    fast hole dangerous score path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
