7a20d40436fa1c39b47216d97c1e1054049c5c8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:28 +0000

    exist really trade turnback event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_floor(x):
+        return math.floor(x)
