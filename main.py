a4015f28889b8edea89097c53f4867561c7a27a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:16 +0000

    visit next care school paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
