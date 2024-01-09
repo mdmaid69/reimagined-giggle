0a5a77e62d8f2961aa46b60fdbb08f15c1bd02b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:57:52 +0000

    lesson replace please same takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
