ce4491c7d399482259658eea2d342782cac7097e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:08:03 +0000

    attention prefer name over deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
