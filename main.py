bee129ce4eb2352d8f4d06aa9b1cb6eb5336d6b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:20 +0000

    attempt bringback large evidence label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
