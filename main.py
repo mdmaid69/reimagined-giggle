ddef390cfe2c79e23661c9e857beaacb89ca419d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:23 +0000

    suck cost rubbish day expression

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
