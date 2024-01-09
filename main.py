fc5293567eb9d34d28e00439de510fdc26555223
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:10 +0000

    unfair she shallow indeed ill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_string(s):
-        return s[::-1]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
