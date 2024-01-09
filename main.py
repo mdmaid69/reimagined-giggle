c1543ee0134f7ffed9ca05905ea57a6a352f17ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:16:04 +0000

    follow do connected seriously number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def split_path(path):
+        return os.path.split(path)
