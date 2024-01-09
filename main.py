9b25212bdfec8c2a8caea23becffbadbf6d6cb2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:16 +0000

    modern waste sharp steady colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
