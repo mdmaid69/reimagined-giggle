dd5cfc944997aecb8b2c06d8a77791a7a445f0d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:52 +0000

    bag pressure interfere quiet lunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
