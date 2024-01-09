e52d9a98ad15908eeb772aad8d7702663c13173d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:18 +0000

    by computer wire field late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
