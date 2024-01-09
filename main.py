cf2cfd36975d2bc6db5c3e3cc26d9ce0a7d18e22
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:40 +0000

    chat record protest gradually rate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
