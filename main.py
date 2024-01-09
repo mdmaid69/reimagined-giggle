cae745b252bbf1a73e13f9cecf6a151ba9e06499
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:46:17 +0000

    joke belief sticky demand quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
