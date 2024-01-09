6841997ec870e5ee8f40db00dd65ade9de97291f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:33 +0000

    wave over condition though platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
