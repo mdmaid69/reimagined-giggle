6dcd1a961957c355bedb8f16e0f995f0ff4d5f36
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:08 +0000

    invitation effect neat cover agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
