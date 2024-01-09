1e5bf0a44a32bf837f6ffbebba8997ea388bac61
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:42 +0000

    want rain keen reasonable unite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
