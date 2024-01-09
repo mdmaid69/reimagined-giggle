c29dcc4fa97b6087482c32d413702700548ff108
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:47:25 +0000

    pipe platform about like early

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
