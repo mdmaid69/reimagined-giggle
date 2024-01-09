79fb9412fe954f773b5aec14e79080bd9c45fcc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:49 +0000

    meet pencil breakin error arrange

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def subtract_numbers(x, y):
+        return x - y
