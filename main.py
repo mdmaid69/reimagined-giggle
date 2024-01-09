3cf680545d1366c43cc8317b5a5c9f6776919f15
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:12 +0000

    takeover handle lot off cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
