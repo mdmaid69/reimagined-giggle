2e70ab515a6fc12339e129bf699878fa503b3b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:02 +0000

    notice serve band Yy appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
