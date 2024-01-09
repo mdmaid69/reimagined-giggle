c9ce93aea6d0e36db2f436e33553aea72a091060
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:16 +0000

    size shadow pure tend consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
