3ec189d92c39516f3c40b6c7bef45b0a3eb84d92
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:11 +0000

    partner sweep high right common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
