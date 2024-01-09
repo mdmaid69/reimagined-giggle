00df849c57f398fc132cb395942771e91f88040e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:44 +0000

    badly centre tomorrow forward back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
