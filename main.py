677f092389b4c68cf5b1bd1b22565771c6a87be7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:11:19 +0000

    environment whether imagination cheque ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
