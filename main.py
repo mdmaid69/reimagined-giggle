2485e4c577b040be78db5bd998946db25c936e80
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:53 +0000

    dear accidental cool move night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def sort_numbers(numbers):
+        return sorted(numbers)
