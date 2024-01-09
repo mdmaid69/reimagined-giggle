19996b9ed2057bf653b8f322604b05486e4bb492
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:45 +0000

    sister remember record criticize except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
