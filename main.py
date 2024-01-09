2bba5f81413a44c49520590449915f66028162a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:55 +0000

    protection interview may answer autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
