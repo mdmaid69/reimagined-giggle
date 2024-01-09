1fb6aee5b0980b9a421c7fc656f62de85407d238
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:08 +0000

    shape number shoulder unite someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_rectangle(l, w):
+        return l * w
