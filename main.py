a1ec76ae6a2c0484d4e03918108ec568a7e0e401
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:45 +0000

    burst full steam close tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
