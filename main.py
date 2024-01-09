9b8a3b23cd47a5f7f16e80420857f226a3fe4a8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:42 +0000

    rubbish obtain tooth hour press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_max(lst):
-        return max(lst)
+def is_even(n):
+        return n % 2 == 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
