42370ecedf44d40fb5c81bc00f3f7c11b6910f26
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:02 +0000

    any physical off afternoon free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
