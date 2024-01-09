1f8f8c1e43405e29f94f2093926488fcb20d6609
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:04 +0000

    science inform hard save interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
