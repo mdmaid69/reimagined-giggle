3bfae4cd51bd36e624a7f200b6ec2ff4053f8ba4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:25 +0000

    gooff back true might chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
