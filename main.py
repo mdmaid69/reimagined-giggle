93a0ce61dc1698a842229be124270bfdc4e58bbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:44 +0000

    chance failure excellent regret least

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
