63491a4f498c6930d11e7ed809aafed7baac6d1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:32 +0000

    both straight perfect direct getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
