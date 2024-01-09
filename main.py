6ca38dfc218b66bdae09f379a64236e38757cdfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:23 +0000

    apply have have dick sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def reverse_array(array):
+        array.reverse()
