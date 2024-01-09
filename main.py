806b8d26acde6f64b69ff08301b25578652e02e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:55 +0000

    cause lookafter cost funny behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
