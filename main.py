6f86b85a1b1bc303314b42a478f0a723aafe87db
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:05 +0000

    event why design attach noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
