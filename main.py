fe03e2b3009161d495278a830f8df41a6aab17d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:29:45 +0000

    comfortable breakinto arrangement maintain pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_even(n):
-        return n % 2 == 0
+def get_array_slice(array, i, j):
+        return array[i:j]
