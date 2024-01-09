feb0a7722a718567a2b7a1140ce6fb1090239dce
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:32 +0000

    relax walk most compare bus

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
