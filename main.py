08ee8bf9c5de572f6901b0445e79ca2877b745f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:25 +0000

    port judge demonstrate nervous float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
