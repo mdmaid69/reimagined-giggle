cef012af5a4b54840599ff91fa7f16a52f5fb45a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:52 +0000

    occur whom platform Ll lay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
