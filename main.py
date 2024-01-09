469330f97e0cbc9867c837a252c0b9fdafbf3844
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:22 +0000

    attach getout sky load giveback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def add_numbers(x, y):
+        return x + y
