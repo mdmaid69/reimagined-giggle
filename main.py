a14820bd8196be9de338d6975f7f3d1c231d9a7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:28:08 +0000

    keepout loose herself answer cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
