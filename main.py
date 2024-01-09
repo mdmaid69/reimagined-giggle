8922b5bb31c62a98046bf9d0e6ec96fafb03decb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:27 +0000

    side smile secret small exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
