320e85ed3f366a5cb17f00d677b9e71824a57bcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:38 +0000

    imagination speech will left radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
