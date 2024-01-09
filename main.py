086921a5da2b6cc2a4b111dafef2de26b981619b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:27 +0000

    though still mood mass instead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import logging
+def log_message(message):
+        logging.info(message)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
