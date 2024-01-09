8b3edd1e714c1aa6958866174a4b34665c9ad282
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:59 +0000

    man Ss soil measure hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
