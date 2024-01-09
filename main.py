03e5bfaef3f6dc3b97ba6e5f629808dbdd99191d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:44 +0000

    afford nerve taste tap present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
