6d3fc8c15c3f70a5f7abd44a04e71056e8061791
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:22 +0000

    odd drop health thanks uniform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+def square_number(x):
+        return x**2
