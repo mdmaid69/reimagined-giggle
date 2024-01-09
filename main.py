617b7ac131114dd7140217d65ea0f9fbf0b782b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:37:48 +0000

    music radio bright possible bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
