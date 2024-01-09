782daf7d7a5cd21db21d744852955d698c5965e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:37 +0000

    interruption exam dress sit Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
