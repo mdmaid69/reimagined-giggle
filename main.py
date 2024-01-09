d82465ff69afc5d9a21ae37605c77e59b54812bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:32 +0000

    need only away deserve prize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
