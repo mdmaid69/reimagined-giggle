8ca6d1553969bf00aaaf89d3f45cf96060cbf827
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:10 +0000

    public copy train cinema knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Characters:", len(text))
