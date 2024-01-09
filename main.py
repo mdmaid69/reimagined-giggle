48c6a5e00d0713f4de1897ef91e9a4b59feb48f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:32 +0000

    Yy cruel bill height container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
