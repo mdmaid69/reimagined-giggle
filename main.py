9b074ed902e38854e2d3bc5f8f625241b2b20c93
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:20:21 +0000

    trip confused leave familiar dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
