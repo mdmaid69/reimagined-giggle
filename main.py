024baf50f43a71647f30dfb6bfc0d8f2441e315b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:58 +0000

    her platform cold include head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
