43afe237b58c48b303d47f35042bf86611962c75
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:55 +0000

    system offer course leg present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
