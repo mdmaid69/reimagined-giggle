fdaa7bf6fed69a579d42eaacb7577593a79ed095
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:40 +0000

    enthusiastic actual think contain steer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
