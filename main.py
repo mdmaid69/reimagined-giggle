54014fef6fa748c1f0c0bf9facf4e31a62feaf1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:58:47 +0000

    trousers rank room delicate total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
