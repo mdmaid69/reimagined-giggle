12d36945210b7525532c1ad4ae1e1d9d606132dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:00 +0000

    towards score Vv tragedy year

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
