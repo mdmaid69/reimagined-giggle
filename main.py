5278623268727f77f98ebaa5eab8458e136c0e42
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:06 +0000

    shop contain classroom club react

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
