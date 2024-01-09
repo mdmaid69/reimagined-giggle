a59189f310421f502134f1b24425a1bddfbd29c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:05 +0000

    enthusiastic lift will bit begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
