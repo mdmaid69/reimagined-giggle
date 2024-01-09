7c4e129aca717c2750b7ea6d05db9c71b50cce94
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:27 +0000

    culture keepoff constantly rush this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
