af46fe68f020d3b502f87970245363e8fc89fc35
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:54 +0000

    under forget poison comeback sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
