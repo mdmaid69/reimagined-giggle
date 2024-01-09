31e66178aa183ae19624dc8ce1d52ad8abab90da
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:27 +0000

    related front fit content against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
