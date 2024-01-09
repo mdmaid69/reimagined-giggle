60fc2a40cbf8b550982a3ccfa23f6194f337e59c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:32 +0000

    unit about always silent strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
