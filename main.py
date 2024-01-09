6904c0ea6a7612368005765e26f67795ddc2ae02
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:46 +0000

    go bus right fine home

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
