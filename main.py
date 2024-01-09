1c876006d2f06c86606477a2435d3705a49ed536
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:14 +0000

    difficulty least compete instruction process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
