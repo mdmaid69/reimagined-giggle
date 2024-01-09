e19d46f271a05a629a4d6d792f0aa1f96e127300
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:50 +0000

    nice mend cross best capital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
