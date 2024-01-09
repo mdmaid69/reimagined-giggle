7a13d48ba5bd79c6373d663f6063aeadfe504412
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:32 +0000

    level Ss brain back understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
