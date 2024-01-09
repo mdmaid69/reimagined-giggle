35b196416e74122c4f319ac9c0b2369cd4dbe440
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:02 +0000

    sale yesterday obtain money birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
