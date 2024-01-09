2b413fead5da4f0058089e711da58a27a7118cdb
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:31 +0000

    improve flood chase continue hide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
