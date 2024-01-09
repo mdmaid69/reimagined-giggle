b461e45a6ace97c9305f6a911d13b84b5540f435
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:12:00 +0000

    sit mysterious ring support once

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
