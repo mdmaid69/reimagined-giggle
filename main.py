84fa5f29d95629b9a4e0014f8f81c963257b3f0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:26 +0000

    as benefit vote risk perfect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
