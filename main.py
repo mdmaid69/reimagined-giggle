481220afcc3e06c58544606cfd9cc4ead45a9586
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:21 +0000

    national achievement fly belong point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
