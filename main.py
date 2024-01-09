75ec4b6d344c92d7bc8a8812a5aa8db010f06fad
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:00:24 +0000

    limit national discovery again play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
