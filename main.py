35f502ce60001cd106aa722a3284cebf8eb33319
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:42 +0000

    protest total arrange such maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
