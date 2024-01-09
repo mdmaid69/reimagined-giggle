b3914266dba87626b1582d25ca73956fc3af0f4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:25 +0000

    middle go terrible between yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
