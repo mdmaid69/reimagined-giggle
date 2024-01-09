7bc0887ffde5d67849fccc432384853ea51649c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:32 +0000

    cool polite file design intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
