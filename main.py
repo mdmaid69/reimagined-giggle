0b92c7dd123f11056e89abb01f1edcb3f5c4c677
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:05 +0000

    contain answer I half plate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def convert_to_hex(n):
+        return hex(n)
