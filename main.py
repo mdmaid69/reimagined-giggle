b140aff2df2ee7947e89e0c7d9c79c1ce7f97d13
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:12 +0000

    rule section eye frequent break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-print(random.randint(0, 100))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
