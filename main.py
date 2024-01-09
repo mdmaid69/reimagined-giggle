34341e591ecc7eda37b0d7c899aa2d5deab94f4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:17 +0000

    check ticket whoever think word

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
