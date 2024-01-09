297beb6bdd3d2650d291c4a30c7c62041a4ee0f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:31:19 +0000

    send relative fine strength first

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
