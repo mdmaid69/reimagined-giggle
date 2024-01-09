8a81eba3233e9573964dbec99a06eae6171bae08
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:34 +0000

    jump arrival reaction matter help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
