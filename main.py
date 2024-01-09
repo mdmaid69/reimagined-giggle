d333559afd0919a8e5cc30c7fdfb1f46434fc30d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:46 +0000

    colour brother feeling wild unite

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
