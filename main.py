6586c7314889cbdffccbe118454804b864e765c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:05 +0000

    split switch split roof how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
