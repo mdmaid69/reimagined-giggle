fbf3b59776b4489ef008ea46f0a656cb175d8645
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:21 +0000

    radio read judge busy whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
