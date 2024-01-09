8cb410a5e456f14b78dca1030bbde788b0177e07
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:20 +0000

    careful far urgent board think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
