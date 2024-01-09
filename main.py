bdf264eddc05620024ca3fef99dd675494987130
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:40:44 +0000

    still would single music like

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
