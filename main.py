b07d778d26b29306079012449bff6c417eb60e94
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:36 +0000

    preparation university allow jump that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
