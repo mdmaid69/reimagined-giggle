ec266c74770fd4a6685db75c36599f12af838d4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:05 +0000

    565end factory poison hers free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
