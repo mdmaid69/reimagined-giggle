8cf96bd45873f7e68a7cec8ab6426bf6a439763c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:50 +0000

    bet yours basic continue rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
