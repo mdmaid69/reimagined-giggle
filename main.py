0f0149e97fc1db022b5214788b5b7e8285641f20
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:13 +0000

    argument heat effect doctor tax

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
