2bbd2bb1723bcc1338eb02cf720a75e09c1360cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:49:11 +0000

    close house you people mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def is_even(n):
+        return n % 2 == 0
