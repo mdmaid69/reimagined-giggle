c00a11a604e822e9f9470c8f1b5f8a766d1b3432
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:03:06 +0000

    month cheap easy single mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
