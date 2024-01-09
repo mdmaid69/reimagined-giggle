b1cade05d9cd0faa97ce41632b271d6ac808ae8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:23 +0000

    crime telephone add communication dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
