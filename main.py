20b5bf97b8fc6630b2f20a0c65d6e5f835b57a6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:27 +0000

    calmly ground slip figure stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
