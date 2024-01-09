9b42a3d25a7035ecf300c345dc93a31888e2f566
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:37 +0000

    line shoulder fast real damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
