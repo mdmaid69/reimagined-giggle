8738e59de868a3a2e71ceb3705a10fd506f7029f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:33:16 +0000

    ashamed person fair but niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
