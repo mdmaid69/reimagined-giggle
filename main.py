cffa238a1702b761fa06e93af14ac06029298b06
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:11 +0000

    direct challenge run remark influence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import time
+print(time.time())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
