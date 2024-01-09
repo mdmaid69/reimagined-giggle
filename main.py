c72e9dadf39a3b35b3f5aa0caa899d19ca91ad97
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:32 +0000

    against equipment taste ashamed and

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
