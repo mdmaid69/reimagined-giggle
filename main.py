3881c3c346593d8ea885f2e7c64deaa2a429453a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:49 +0000

    represent goback(to) light education motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
