2c48f005f94550ed1bc76b50a2069455a95633d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:38 +0000

    medium exploration sick interrupt frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
