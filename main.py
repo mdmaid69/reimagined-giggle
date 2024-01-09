ba2ba7bebc0ea6e57daa8e20ce4976a693e0128b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:42 +0000

    type bit dead describe admit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
