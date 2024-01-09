8830c151f078ca172a3deaa394a6b584315a41fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:22 +0000

    path new everything crazy risk

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
