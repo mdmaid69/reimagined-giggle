e8576777426c3346b28d71104a04f85bc00c64df
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:03 +0000

    pass copy comein workon mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
