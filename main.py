49fc2c544a74f76c58a540a18cf7a519599c6b1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:27 +0000

    mistake hard mess convincing container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
