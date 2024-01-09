f52b775fda8368fa15c1c821b0c58c6910c6012a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:37 +0000

    manner attach danger punish invitation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
