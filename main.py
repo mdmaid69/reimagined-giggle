4e22cde58d19237ca5e08dfba8913662fd19d45e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:19 +0000

    form escape block odd lost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
