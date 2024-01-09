cbbab216c7693867eb5a3f6b3b59b8b7ef48d7df
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:51 +0000

    thinkover powerful all upset hers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
