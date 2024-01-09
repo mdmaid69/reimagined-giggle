97a65bb0b0492faea9e10c9c8e3f23bc86b8083c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:52 +0000

    statement mysterious if young die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
