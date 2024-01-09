7b4f5c594538003a070751c256d208e4297f1fd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:42 +0000

    fun conclusion lock respect fortunately

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
