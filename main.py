0d681f4100e6789e458a24e3a5f4a3c3075beddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:37 +0000

    bad identify expect save deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
