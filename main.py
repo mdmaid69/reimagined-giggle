7e68f9085e4b1d715f6d32958dc2a6c3a2d81642
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:53:00 +0000

    peaceful view soil sink pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
