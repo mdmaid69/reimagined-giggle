9cdeca542eda1db2a4bc75d7bf08a834d72b4b2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:10 +0000

    turninto themselves base appear display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
