d8a2964c287d01c421ccdbf54421e338b4c8bca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:30 +0000

    damp nearly sudden bill keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
