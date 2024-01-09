ad98cb5f899f0a2e08d6c38a1633ef649781d95a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:57 +0000

    clear personal week slice inside

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
