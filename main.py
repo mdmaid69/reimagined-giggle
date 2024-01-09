353cd8cb1abd3b0f3da876ffcd20952189cc3972
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:17 +0000

    tax convince opponent interesting above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
