0ec55826d4a2a3728dc71290eadd1a588deed8b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:37 +0000

    impressed whole afterwards cough interview

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import platform
-def get_os_info():
-        return platform.uname()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
