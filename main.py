fd75ad251828d4aeaf290f28479943383c1b054e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:40:58 +0000

    authority catch upstairs notice mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
