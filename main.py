f279079ed887179ebf7ef3776daa15323b98cf92
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:12:58 +0000

    protest somebody harm sound mistake

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
+        if n % i == 0:
                 return False
         return True
