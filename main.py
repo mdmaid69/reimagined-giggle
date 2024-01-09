2e8c9bf5f922f3d6036cfb3890a67497f3445abe
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:26 +0000

    spot tell now sight belong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
