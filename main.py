c8d3ff3b1eca57895907e57398a5acd55e1e33a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:31 +0000

    afternoon nigga wave street Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
