d50cf1f84bbc69083a43e822b17d7c64da0993ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:41 +0000

    though anywhere forever survive empty

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import datetime
-print(datetime.datetime.now())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
