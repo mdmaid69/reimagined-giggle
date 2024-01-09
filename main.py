6136f048ecda9b63e80f08b10997c2630696102c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:50 +0000

    produce which nose successful goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_max(numbers):
+        return max(numbers)
