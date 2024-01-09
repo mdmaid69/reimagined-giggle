c262ba8969a4d042e5d7b7aa660fa5df4b237413
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:58 +0000

    able land after disapprove not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
