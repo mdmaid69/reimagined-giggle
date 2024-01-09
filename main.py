2b0b1856d45db1b24b911c6f44191affe2a5527c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:10 +0000

    actual calloff meal company bringforward

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import getpass
+def get_username():
+        return getpass.getuser()
 import sys
-def exit_program():
-        sys.exit()
+def add_to_python_path(path):
+        sys.path.append(path)
