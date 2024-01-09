3f756645ab3bdd3817ca68edb5acbf76e7b9f1a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:38:16 +0000

    region load good ourselves stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
