d4b6ef9891b02e18d03e99cda8f80ef9951a120d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:08 +0000

    stir share car hurry cum

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
