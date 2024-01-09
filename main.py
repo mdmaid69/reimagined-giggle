5353c4691e4ca72044490b051185c0907b608b1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:14 +0000

    surprised male door loud any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
