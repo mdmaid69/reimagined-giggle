ae387760cf3fdba0a44e0566847261f3d8f8727c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:02 +0000

    basis rid should by maybe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
