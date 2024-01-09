19a6ed6c09b88b1c7bea95cc2a8b89b5b975a066
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:41 +0000

    add study pressure one more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
