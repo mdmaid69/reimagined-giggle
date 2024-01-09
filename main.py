41811728d71e999d580dae9d6b87305a48d74cda
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:08:21 +0000

    he pride debt apologizeapologise term

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def add_numbers(a, b):
+        return a + b
+def multiply_numbers(x, y):
+        return x * y
