214973cf603e541bb73c3fcc6da7b20ffc4474e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:05:14 +0000

    private above thinkof shake brother

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
