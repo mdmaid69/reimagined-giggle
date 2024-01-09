4bc42d2461277a2c9b085d8d93b46edc0a789bd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:51 +0000

    software law plain fear none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
