c57cec0dc349dad651e775ccd75c616cee87d463
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:18 +0000

    thank position survive prepare cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
