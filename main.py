99635b556b86e946f74fe68e42b94460e27aaf9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:53:09 +0000

    hang brother damp foreign alive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
