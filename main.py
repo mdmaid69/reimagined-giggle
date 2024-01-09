b63d3f113045f589d2dc00aac494f37c0836ec28
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:22 +0000

    takeback recently unkind back scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
