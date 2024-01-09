7b91d510ba1317f656aeb6d96ace5d4e51dc42bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:55 +0000

    hide process smile maybe thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
