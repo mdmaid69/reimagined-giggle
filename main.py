425ffa9ae39176dc8bb2ed48ea29b085fe13cb32
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:28 +0000

    contribute relax thin comeout wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_base_name(path):
+        return os.path.basename(path)
