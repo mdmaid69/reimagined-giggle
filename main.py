a5d873fd101d78244661814cc076baf099e087f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:36:49 +0000

    open rarely yourself minor add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
