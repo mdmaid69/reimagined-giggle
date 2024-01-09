3e0dc083c7d9d3c01b4bbc8fb741d06ceb307529
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:30 +0000

    anywhere slowly without meal happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
