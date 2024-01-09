4d046e225657688556044b4ca733fa605130ed1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:35 +0000

    shop shoot under comment tape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
