34a59e23e230bac5b60fc5ec16c13238b7288406
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:06 +0000

    concentration who unable ordinary reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
