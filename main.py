7e7bcef21f7547b6110f960f210cda60d8e5d629
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:19 +0000

    chin discovery life somewhere contrast

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import collections
+def create_priority_queue():
+        return collections.deque()
