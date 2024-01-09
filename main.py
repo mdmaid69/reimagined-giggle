f3a65413b9fcaf66193584e2906b08dbdaf92e27
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:06 +0000

    some content light poor peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
