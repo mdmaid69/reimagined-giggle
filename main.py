ae254fef67be478ff5861591b1c174f785ee30b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:58 +0000

    uncertain sudden snow over trousers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
