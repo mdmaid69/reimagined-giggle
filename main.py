ecb72cf2cd26b43b64411c591fd4cde8d0121ae3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:58:55 +0000

    cap neither show rather unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
+import platform
+def get_os_info():
+        return platform.uname()
