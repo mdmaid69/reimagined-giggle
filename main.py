b3c2e522565fda8f5a9bad436233a4f5e60edcab
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:12:51 +0000

    brown firm speed contrast convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
