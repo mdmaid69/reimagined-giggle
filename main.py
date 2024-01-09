c94131df9b85c8913a00a367c0a31260e7adfbdb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:37:10 +0000

    order along happy unlucky scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
