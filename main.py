03bbc939fc60b83c6a3a31f72246cab393f64d08
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:21 +0000

    intend aim gooff into meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
