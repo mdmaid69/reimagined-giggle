0e41d2bd0bad8981dece80d3479b61ef97ba75db
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:26:51 +0000

    discuss shake load hang statement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
