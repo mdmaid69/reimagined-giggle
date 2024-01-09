3d702e24c9ab95ef0c635ba9f40b76e32cbb08f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:25 +0000

    rain skin high bottom factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
