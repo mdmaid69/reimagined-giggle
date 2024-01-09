62952faa31729a25f11fc3b9ad8324da744c478e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:11:10 +0000

    female wake spring calloff impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
