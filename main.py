b44475f8f98337de72ef4d9c46b3552ad872eeef
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:22 +0000

    tooth bright just Hh fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
