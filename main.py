b4ab14fdfd345a9b771e473b7692999ddf938068
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:50 +0000

    busy giveout careless depend watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
