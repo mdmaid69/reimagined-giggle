785549d383e86e6624533acb87fcf3a69d2dca39
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:40 +0000

    manage mark plan equal address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
