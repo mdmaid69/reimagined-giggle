f345972ff71171d1ce21681e97278d4fa89f281c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:29 +0000

    fly silent week slide snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def count_words(sentence):
+        return len(sentence.split())
