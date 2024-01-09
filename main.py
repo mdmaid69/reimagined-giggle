8be038c43c40085201ef7fb13b285ef3ecfa777e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:53 +0000

    565end interest free free mystery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
