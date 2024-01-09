9f90af4ecbf5a4188c8cedc1cd343375c69994da
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:18:55 +0000

    pin meanwhile mix fork quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(lst):
+        return max(lst)
