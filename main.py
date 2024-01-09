cf6c7137df72f0d1996f04f8bf799481ef5ecae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:36 +0000

    swing used lead keepup coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
