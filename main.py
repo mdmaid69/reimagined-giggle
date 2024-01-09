91f1ae9020508a7fc79d2a321382066e609b4478
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:52 +0000

    crowd past pronunciation form perhaps

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
