a28474cc2dcb068df23c4a1aa997ac131cf8cf72
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:50:12 +0000

    criticize shape exact pretty error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
