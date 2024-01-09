8e51fcec29eced36f77a2a9a5824597e88a2634c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:19 +0000

    temper proud sentence born appear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
