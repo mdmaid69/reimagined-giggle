2ad431695c3b4295966a0cf278e3f097ba546490
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:06:03 +0000

    target Gg either Qq such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
