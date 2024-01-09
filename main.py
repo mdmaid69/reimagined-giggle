5841d00bfa0f15b9c64ff50a7a708095a8112e92
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:09 +0000

    fact beyond culture limit second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
