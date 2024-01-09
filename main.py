ea5cf8eca3a99ed633b5866d0dae7558f1e7b584
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:15 +0000

    temperature before active congratulations exciting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
