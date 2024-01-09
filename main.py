cedce61f6162a633f92dd11dd7b8c61bcb17eed5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:24 +0000

    polite crime swing space across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
