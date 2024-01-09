a7375b9619e38f76eb26c3912b9ef6fa23034be4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:43 +0000

    maximum card seriously bath 572enter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
