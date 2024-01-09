7837bba80183ec3ec636acf9bdab87374b42037d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:33 +0000

    beyond normal fast turnup taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
