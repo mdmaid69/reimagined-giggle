f2d5bb7467d8251a7e7691889995e7877d6c848d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:36 +0000

    related memory turnup terrible slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
