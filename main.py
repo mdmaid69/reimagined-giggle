3fb4acda2e6cca512478498ef9964639caf13421
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:00 +0000

    scream service scream within other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
