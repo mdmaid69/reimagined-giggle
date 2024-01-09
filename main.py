20fe005415bdf6a1f6d5a54dd5da968ed02f809b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:48 +0000

    admire set raise of interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
