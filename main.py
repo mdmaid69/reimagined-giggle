dda7c390851a22607efe510f6bfdd4b00c14115e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:16 +0000

    intend eye purpose fairly avoid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
