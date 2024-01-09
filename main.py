1396dc67dfc2e80860528f57ff2d5d65bf421068
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:00 +0000

    chase space base callback improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
