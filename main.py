3b4ac4126affced362649bf9148ce808a666fa1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:40 +0000

    temper sale scared average open

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_floor(x):
+        return math.floor(x)
