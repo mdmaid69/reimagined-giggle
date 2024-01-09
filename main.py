94a009e75fc7e5e843bc12a81c58ba8d2df88693
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:01 +0000

    noone send expensive time chin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
