42b914089993f3210a8679f1ec81a123ce97fe9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:19:12 +0000

    exaggerate prefer care chase border

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
