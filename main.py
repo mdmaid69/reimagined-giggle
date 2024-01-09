ab673c02e9719c24ce78d5c7947f0e2db7dadff6
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:39:49 +0000

    neither scrape over shade only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
