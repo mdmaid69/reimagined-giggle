eb572829aaf9fb7d2d13f40220d40deba5b84d83
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:37 +0000

    repeat instead left several keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
