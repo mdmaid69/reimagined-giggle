81b6c34b806d74d43619af6396194dc957ffdfc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:21 +0000

    bush important emergency night pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
