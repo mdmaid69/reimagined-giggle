be83c051f0d0839ce41416cefd33e65dc41f14fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:21 +0000

    spot wealth machine war giveaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
