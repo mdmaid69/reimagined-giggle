78f61c20c009420c7e9aa7fa21e3f70ae984debd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:10 +0000

    pocket worst relation sky hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
