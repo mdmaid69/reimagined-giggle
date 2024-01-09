c4aee3a12061774a49560b5dcde4b642b594532f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:32 +0000

    you this shadow cross bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
