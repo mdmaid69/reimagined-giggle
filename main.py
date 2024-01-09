e31d6bfc5e0277f5583ea8604bd853748a885d17
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:06 +0000

    pain platform large can what

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
