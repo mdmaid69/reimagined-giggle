5c65d075beed4974c4efaf9978368700e7da540b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:03 +0000

    wake say spoil care typical

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
