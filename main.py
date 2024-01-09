f7b56724424d3e98e2576cbf793fbb406ebbda0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:20 +0000

    downstairs ashamed any account report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
