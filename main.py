94145fa2eae5d1bd643c3ea002141bb962572a15
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:27 +0000

    choice flavour drop why avoid

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def delete_file(file_name):
+        os.remove(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
