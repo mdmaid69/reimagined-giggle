9d47eb0769ccf682b5d9a3d516b0f7f8ca53b405
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:17 +0000

    practise image support its connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
