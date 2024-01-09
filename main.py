c2d0b1a1e55b5ccd86ec779051db4f290d522b73
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:25 +0000

    punishment middle behind although gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
