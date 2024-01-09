9abc1c760eff104a4f7a99d5fde081858a841fec
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:07 +0000

    name dear often brother plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
