c5d85e57b0e695d1d0e427559a16284e3c5def0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:24 +0000

    office conclusion type love pile

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
