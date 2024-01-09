1f6b913622974f53adf4bb2cf58781cd76121866
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:45 +0000

    makeinto wander half someone shall

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
