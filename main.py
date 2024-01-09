3084f161b00523d28a56489ea78a7c328bbb4a57
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:14 +0000

    lend event branch through once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_current_directory():
+        return os.getcwd()
