97162366148b2f11e4532cbcbdf6624ca76af42d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:03:44 +0000

    maximum street hollow nonsense left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
