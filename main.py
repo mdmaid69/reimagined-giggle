23f07f4b090db186d254e6eba30610d0a8f3f356
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:18 +0000

    turnover runinto main although crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
