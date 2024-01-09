61fd63e6e5f19e6d5d0932bbb23a28dd44a0abfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:16:49 +0000

    study scheme wait just among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
