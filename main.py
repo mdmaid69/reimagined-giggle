1db5aaaff28e38bb93830cc1cbcd52f392e48f50
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:15 +0000

    table production pause maximum area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
