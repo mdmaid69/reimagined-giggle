032c4ae325ba93b1c9e3bf1f155e87cf15ab6454
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:27 +0000

    mail one street in thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
