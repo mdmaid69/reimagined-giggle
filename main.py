5620181215a4f695554f47999958fb6a0cb7847e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:11:40 +0000

    pinch party skirt drop steady

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
