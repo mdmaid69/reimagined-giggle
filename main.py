37c38a68ffff4d9226cddc9810a48bd4dd046a9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:43 +0000

    otherwise send dig off please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print("Hello, world!")
