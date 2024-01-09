fe4306e5ee7a7e49371c878b102b57c5d58c1cf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:24:10 +0000

    top light send ability many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
