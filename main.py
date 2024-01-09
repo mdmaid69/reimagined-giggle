ccaa72852a236720f8fcd959bc364b288b66f1dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:16 +0000

    blue late payment teach gas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
