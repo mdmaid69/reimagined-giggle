4779852237d5ac3cd94e0e6de5ea9d42674207b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:28 +0000

    criticism traffic wheel variety boat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
