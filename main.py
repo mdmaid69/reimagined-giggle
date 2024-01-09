57c825e38ddac4970c76a4b16f6381d7e5d06beb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:08 +0000

    similar polite guide train offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
