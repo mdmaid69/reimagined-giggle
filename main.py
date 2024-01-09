127bcd455eb61ae20017c168d216e7eeff3ca085
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:49 +0000

    wake command mess stick to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
