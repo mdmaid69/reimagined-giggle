87c8864be004646aa52a164beedffe0c7c4073ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:45 +0000

    afford idea cool contribute travel

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
