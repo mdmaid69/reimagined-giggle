70d33b7ebf1fca1868c10231f2b261f28f7dd70c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:39 +0000

    forward ignore method task family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
