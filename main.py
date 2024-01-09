90b8f21d396071cb7308f660c2bc9d6eb24cd985
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:57 +0000

    please moment each steer Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def clear_array(array):
-        array *= 0
+def get_array_buffer_info(array):
+        return array.buffer_info()
