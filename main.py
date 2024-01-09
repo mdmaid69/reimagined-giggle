ba0477faf90c54e738cffde87dca7e53aa253077
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:09 +0000

    cruelty comefrom shut certain Dd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def add_numbers(a, b):
+        return a + b
