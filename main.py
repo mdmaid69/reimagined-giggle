f11d8c406a3d229eeabe74112034a0715610d2c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:40 +0000

    high on Pp delicate welcome

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
