dd4d87ec3170690f473028f476d72ed280f03cf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:47 +0000

    art decision discussion command worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
