2a46cdc69df8f8a1c26d427c690a887ad5239dbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:55 +0000

    way steep loose soon on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
