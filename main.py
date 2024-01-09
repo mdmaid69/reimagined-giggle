3b94d54cce605ec44429d98f93888350e6b9f930
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:30 +0000

    within he everywhere hear disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
