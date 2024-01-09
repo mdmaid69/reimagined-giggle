e06162cda77c3cc47646436987653c60fe21867d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:47 +0000

    word often rubbish embarrassing funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
