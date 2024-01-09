5b7966a7a3bc8b3aa90cbebe2041d9087bc5ca89
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:13:28 +0000

    influence which range aware almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
