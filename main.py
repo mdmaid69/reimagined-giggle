30aa0391c39c99c065545c32191c3455bc1d956a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:13 +0000

    keepout union honest custom exam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def add_numbers(x, y):
+        return x + y
