341a4345df9161403a16676cb7c982d978915fb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:52:14 +0000

    length coat themselves demonstrate when

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
