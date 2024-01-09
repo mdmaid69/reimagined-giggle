2d5deb00cec91d52e8c11ec29771179f5f2a15a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:03:53 +0000

    even practice emergency instant secret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
