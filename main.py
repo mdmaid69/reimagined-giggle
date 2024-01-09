21516cb76aea751f0d516bb43f958f050323e71a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:23 +0000

    anybody family comeup fight shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
