26a731c9716f053732d38c2003d906382237aed2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:26 +0000

    arrange passage manner on country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
