ca9bbc5ae67958c49fbf4c136993b2b943766270
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:31 +0000

    excuse this matter bother than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
