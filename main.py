50a515a540d56ee864116359bce7bfd8eaa03edb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:16 +0000

    holdout splash develop godown material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
