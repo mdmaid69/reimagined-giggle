29c4ddbcb7b7325cd4c43e4f291ed9357c5b9751
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:36 +0000

    public bill block interview wind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
