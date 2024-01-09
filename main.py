f8626e37cac20a2ffe3ca9f23c72fbaf8ab0d12f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:29 +0000

    clearly may little claim near

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
