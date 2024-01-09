a7d159cc2675eec032f8f3608b73f8ab2b911d50
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:42 +0000

    warning shine scene breakfast listenfor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
