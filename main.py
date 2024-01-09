622790c68d554930f577fded67e31598b7fe5679
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:04 +0000

    prove bath control round colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
