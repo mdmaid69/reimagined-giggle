25e7506ac747521bd908a3c913816fcde1f57cd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:02 +0000

    underneath label family against vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
