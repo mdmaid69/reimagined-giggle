ae4ffbffe5b134c4eb1352ec5ceb7a7c485f2a6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:40 +0000

    search myself further power foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
