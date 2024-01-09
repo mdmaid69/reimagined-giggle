3ed4d1c46d2e8be2c36fd68bfe19e8206a24decc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:53 +0000

    fix takeoff vision anything clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
