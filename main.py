0db26724623ed8eb041ab929d188b4c2290c4cbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:03 +0000

    nonsense right actual argument decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
