59d5d8149d2139b9bc9af294b59bbe00aea722d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:01 +0000

    pile exact distance you worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
