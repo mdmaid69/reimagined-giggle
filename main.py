cfe0d6727f8dc17a14fd824690cc170c1a209e99
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:56 +0000

    embarrassing switch afraid surprise all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
