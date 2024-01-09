250848e6303299d2a17515542f487c2c3669ed70
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:28 +0000

    common underneath trick official various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def square_number(x):
-        return x**2
+import time
+def get_current_time():
+        return time.time()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
