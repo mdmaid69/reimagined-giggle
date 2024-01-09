968c8242735c0e312a990c4ea5c1d55b1e65d365
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:31 +0000

    tell regular prize contrast reduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
