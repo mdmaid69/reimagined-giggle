1437ec95d12522fc5f92db702b014dc5e1163a81
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:12:12 +0000

    cost calm corner dress image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
