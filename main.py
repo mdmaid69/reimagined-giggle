5df75c3819bc6ab6a4a4f6d4dbab41a4d5a0a91b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:38 +0000

    anywayanyhow godown distance complaint accordingto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
