b4cdd4ec00cfd043b609fd0d9fccdd1994e1391d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:02:15 +0000

    promise company charge contribute right

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
