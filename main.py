be300ec65b9a0fdc63df06b97eb5058b694aa773
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:41 +0000

    means work disturbing story much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
