1ce59a4909ea96046e7a78cfb885e8d95eb9017e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:06:59 +0000

    explanation natural month bang pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
