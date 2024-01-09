de0ce475d66a756aff552d947144649150a05db7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:56 +0000

    failure for entrance name slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def subtract_numbers(x, y):
-        return x - y
+def count_words(sentence):
+        return len(sentence.split())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
