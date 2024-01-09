87b383342590092eecc1edf7e3ebf259824172f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:16 +0000

    wing kill passage hollow culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
