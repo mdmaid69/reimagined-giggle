03e1c53ce1ccdf363830a3aff2c06bd45501ab14
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:39 +0000

    without classroom call collection open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
