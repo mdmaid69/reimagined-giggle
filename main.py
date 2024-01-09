3dc032818e79272e99e808ca4ca77044c4d2c5d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:58:55 +0000

    Dd apart bar week short

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_min(lst):
+        return min(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
