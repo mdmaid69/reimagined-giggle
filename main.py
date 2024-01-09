3d4764969b658705ad27a30e2edb869568565ea4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:13 +0000

    getup mention square sweet willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
