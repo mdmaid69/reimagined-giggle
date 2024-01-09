64a9dfe3e972e7cfa8060c8d3251b50c59f5fdd9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:49 +0000

    general mix back stand forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+name = "Python"
+print("Hello,", name)
