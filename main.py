c4e7738a315f85fd1ef6a09f3bb680eb35f90460
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:46 +0000

    try grand whole cook about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
