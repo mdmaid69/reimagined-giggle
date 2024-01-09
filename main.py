d00c4171a4f79085144bdb859d4315900f54226d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:50 +0000

    accordingto pain kind about father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
