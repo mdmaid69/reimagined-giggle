1e04894f00dabc41e6dd99d95a812fc29c6e2028
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:16:33 +0000

    relax Hh embarrassing crash temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
