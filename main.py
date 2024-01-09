0d216e914edc31f5fff7986326fb4ef69e26254c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:25 +0000

    danger right trust lesson mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-print(math.pi)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
