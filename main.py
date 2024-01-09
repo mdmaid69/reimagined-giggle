8c2e63028aac151dd9a6939c2703ea3354a7bbc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:43 +0000

    moon soak ring satisfied plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
