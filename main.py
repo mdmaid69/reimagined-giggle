2b5abb6a734dd39376946c37410c9579886e9f44
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:38 +0000

    that wear transparent activity appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
