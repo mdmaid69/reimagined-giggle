608e2be8fc9fd77716d06591f9d101b9a30ad5c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:50 +0000

    shop contents wear vote joke

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
