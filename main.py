d8eba4443475e51ced94f2210bd0bfa012ccf075
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:05 +0000

    certainly turnoff plenty formal long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
