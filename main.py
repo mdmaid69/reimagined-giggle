d7f5d7fb58a7f9f4ddc537b7920a49a836a58e48
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:47 +0000

    special exercise sensitive branch fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
