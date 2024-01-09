db03e7cc59a2852e87067ceb2c6bbaf117632801
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:09 +0000

    behave excited buy Tt mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
