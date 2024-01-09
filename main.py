e247fc631166ae57a7161c1cf5cff43f8ae83596
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:52 +0000

    rescue file account dull permission

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
