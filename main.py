b0480d3b106e073550b12cea51a946f51de6b987
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:42 +0000

    neat disturbing generally needle glad

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
