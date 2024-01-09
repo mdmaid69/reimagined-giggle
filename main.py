f1fd4cebbf36e31898b192e3ef3eb66f4f762de1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:10 +0000

    ring time thankyou mouth period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+def split_string(pattern, string):
+        return re.split(pattern, string)
