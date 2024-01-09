ec3660b0d971eab7492e3e733e3f2b2e7dde1b58
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:56:02 +0000

    whisper tip edge point offend

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
