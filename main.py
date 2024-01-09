d892000ef8d725caee2ca22fa7ba7e7c9e8233ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:43:22 +0000

    laugh hang notice damp secret

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+for i in range(10): print(i)
