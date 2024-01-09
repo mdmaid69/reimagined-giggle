d08211f279a6ee56d0384baf4fb1a84421ba41ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:55 +0000

    favour world bang excuse experiment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
