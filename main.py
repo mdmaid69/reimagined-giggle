cfd25a776d354a4eb56777b22496d8f42abf7491
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:18 +0000

    down wise instant prepare reject

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
