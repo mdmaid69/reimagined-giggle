f737f50886cfb1973e0f07ea33dbcc88f428271b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:22:43 +0000

    ill system wire doctor keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
