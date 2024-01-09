be9b5e68cc942a170f049f7acda0d85fbf0635dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:23 +0000

    plan warm serious suit willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
