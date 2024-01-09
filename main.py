74a993db0d5192f58e872c5cd9f012c0812fd79c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:33:17 +0000

    connected club together trap related

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
