00b2c92e0e0820f002c3bebb067dc8f6ce5aff45
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:58 +0000

    opinion carry gap free different

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
