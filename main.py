422dfab9410786acb526b2ba0161bdcc640ebef1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:53 +0000

    round my steal stir sail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
