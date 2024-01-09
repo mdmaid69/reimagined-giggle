717fcee1904c1184a6445daceefbd018907d6ba8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:31 +0000

    raise journey charge bottom major

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
