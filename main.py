bbd5d821a2b399526a02fd2520d694a1f7a9a899
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:05 +0000

    simple deal patient repeat right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def clear_array(array):
+        array *= 0
