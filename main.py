ebcb65ab7e9d413ae16fd829b38e1002faaddb1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:17 +0000

    sink normal unable schedule people

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
