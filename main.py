9e67e25da3dd9afedbecca86410e3b98cef54ec0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:19 +0000

    Gg beyond deserve block that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
