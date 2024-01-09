fbb7712c5e70750b95c2a33964bce4de3cebf01a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:38 +0000

    within comeback nature ability risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_user_dict():
+        return collections.UserDict()
