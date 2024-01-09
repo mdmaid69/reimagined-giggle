4ebec56c98004ddf98ba1d866d7a914d9509a71d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:00 +0000

    charge insist behind watch allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
