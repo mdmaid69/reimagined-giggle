76bf5e6e9acdce28a22cba8c2e3054af34e62702
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:37 +0000

    basis shallow innocent because national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_str(array):
+        return str(array)
