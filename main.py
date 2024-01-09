3ae5c836c83ad855ac3df6bfe8750bb53ab405f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:57:12 +0000

    rush thinkof far praise conduct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
