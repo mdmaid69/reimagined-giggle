465e21683f6be8682274d356d87b550854d63486
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:40 +0000

    personal teach trust debt gap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def divide_numbers(x, y):
+        return x / y
