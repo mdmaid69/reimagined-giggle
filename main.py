eb6f1fca639cfb6de6d922d688d3f0acea3111e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:27 +0000

    certainly protection attention same closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
