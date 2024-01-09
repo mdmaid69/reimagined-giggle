fc183bfa3127cd56dfd6650f1cc1fffa11ecb8b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:15 +0000

    reason destroy flow doubt give

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
