82da6bae18b8d6d2a8aa822bdee0dc000ec74de0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:33 +0000

    thirsty flow forgive name getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
