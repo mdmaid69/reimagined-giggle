cd30d84aa9306cddbf3aaf6f9e71bcdb79737a87
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:23 +0000

    destroy satisfied broad smart full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
