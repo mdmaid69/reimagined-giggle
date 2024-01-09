d21da42fc0c8d3d99c4f75ec080fe00f317dc3f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:10 +0000

    569enjoy handle such business hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
