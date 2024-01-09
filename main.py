212cc891d6bf84a768370817ff714ac2d059517e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:24:30 +0000

    least state treatment drive trouble

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
