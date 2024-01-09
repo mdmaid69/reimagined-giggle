4161c82196f72b953ea60698634e91b58dbd760b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:47 +0000

    impressive lesson snow needle take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
