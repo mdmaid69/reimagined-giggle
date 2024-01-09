2a15fd3ebdf53a497f3ca693e4befc1e5a40f4e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:43 +0000

    run turnout exist detail confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
