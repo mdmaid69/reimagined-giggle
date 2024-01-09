0877b4f87a4d0f2f475712849720ec3de61583b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:25:59 +0000

    shut actually cheap choose sad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_odd(n):
+        return n % 2 != 0
