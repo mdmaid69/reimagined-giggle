4fcdc50d6c15489ef139909423b51a606d4cbae7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:12 +0000

    callback realize Tt flight tap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
