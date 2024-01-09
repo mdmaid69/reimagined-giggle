d92486613cd324cb55b33b787676be75b65ad840
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:36 +0000

    concern minimum hear play deliberately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
