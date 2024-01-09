8b01e3358b89db8b66ef2706e2e721f3c8eeb02a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:57 +0000

    love build callat careless basic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
