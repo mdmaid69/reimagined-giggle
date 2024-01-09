66e3a9584dedcd662d70432b08c8fabe67a745c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:20 +0000

    flat respect direct that fly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
