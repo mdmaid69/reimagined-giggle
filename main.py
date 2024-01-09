8755a388df0c6ff2dd2620b163458d033712d508
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:12 +0000

    stranger interrupt carry few describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
