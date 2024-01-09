bd8bcd48fe8303c2e1ec60948cafb224bd523288
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:35 +0000

    mass use machine unlikely while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
