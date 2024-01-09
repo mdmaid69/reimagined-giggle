92eaddca378a0536f2916c6bbb578afba3c30ab0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:37:41 +0000

    rely runinto perform shadow entrance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
