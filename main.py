6bad7682fb97438f345f60941442cc2cbecb9a13
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:51 +0000

    by pleased improve loss kill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
