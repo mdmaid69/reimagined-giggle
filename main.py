2c39a72257f719abe24d25f2e65b8312599ede23
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:05 +0000

    television realize ring risk moral

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
