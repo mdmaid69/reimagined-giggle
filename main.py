c77fd8df0b571107a2737c33db7046409302bbc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:10 +0000

    emphasis kind Tt separate getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
