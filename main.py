0abf0fb96b1b30ff7888108593e859303b55f40c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:58 +0000

    late callup face radio custom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
