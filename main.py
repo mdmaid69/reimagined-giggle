fa803985c1f4ca274bce92d0d2c666d09d2f584f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:20 +0000

    pause snow shout frighten sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
