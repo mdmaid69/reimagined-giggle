1c51b0dafc6730f65ef8cbd7b10c76b4c680a1f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:03 +0000

    slightly section because fire cool

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_exponential(x):
+        return math.exp(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
