9417bec88b7c4e07ef52fb17bf4c2e9b7692690c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:30 +0000

    region bunch wash stop valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
