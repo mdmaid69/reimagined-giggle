b89c6e3cfb7b234b895d648fbbdb14a8fbc3e668
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:17 +0000

    judge neither major keepfrom fit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
