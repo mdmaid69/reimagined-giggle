a5c25efdc7c9716b2f36bd50c96c22a8c69dffa1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:41 +0000

    tense weekend suck pocket pollution

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
