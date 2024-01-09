a5dc8a29a760d2fb0a62a032dfde1efe3413ae1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:59:55 +0000

    dream scream nervous human clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
