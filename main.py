e21895a5fd2e847523a15adf9ddba8e0ba604197
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:41 +0000

    fit settle butter laugh arrive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
