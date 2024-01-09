293854a4049d44173b91076391a7c0a1721994e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:44 +0000

    nature powerful mass must standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
