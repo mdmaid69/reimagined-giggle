aa17e9c1ecc3e0aa13dbc39789c1f34ed36e7294
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:53 +0000

    runout flow after upwards event

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
