51c772e043d80d3080e98510c97e59d18b942dc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:56 +0000

    rank fortunate remove meaning copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import random
+print(random.randint(0, 100))
