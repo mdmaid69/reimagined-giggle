9e28e095ac1186189ba0cf2ac345c603352e35cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:32 +0000

    position keepup design plan tired

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
