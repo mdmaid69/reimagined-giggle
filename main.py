44ac078a307abb1b5a3c675fa0cfeeec4308b182
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:41 +0000

    anywhere about weather identify combine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
