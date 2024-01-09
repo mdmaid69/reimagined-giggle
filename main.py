d5535d4c73b0cb619aa827014045e5f4416400fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:06:45 +0000

    everywhere protest several through dick

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def clear_array(array):
+        array *= 0
