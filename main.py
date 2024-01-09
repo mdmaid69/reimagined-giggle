4e3020e83b92caed339693da75b6a05b496866fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:41 +0000

    cruelty gate charge slice slip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-from collections import Counter
-print(Counter("hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
