bfb45cd0cfaf0efa3dfabb30af09feeab98adf81
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:12 +0000

    that couple circle mention in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(x, y):
-        return x + y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
