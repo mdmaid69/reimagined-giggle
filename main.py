b4e8bdab365370875f75810498ebabc400f867b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:16 +0000

    use shout promise doubt continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
