db19ed0fce1511ae85212c33a973b77527712fe6
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:01 +0000

    defeat independent keepout count deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
