5ec579a8a9aa0b2b8fb10fc7b7973284c43db02e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:11 +0000

    myself city else cut accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
