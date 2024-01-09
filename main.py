d49ea70affb142251c9e65be151aca2906797774
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:09 +0000

    number still greeting journey either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
