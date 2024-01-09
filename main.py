f886b44e0818e256a79abcd923db36de3fca1e01
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:51 +0000

    drawing joke thinkthrough comeout to

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
