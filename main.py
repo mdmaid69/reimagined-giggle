21b54e140d84245942d3dc471ab88b1b6f613652
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:03 +0000

    boat giveback accordingto letter flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_max(lst):
+        return max(lst)
