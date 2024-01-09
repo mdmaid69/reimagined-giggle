8bd42cc35c6c915f1f5c15416602f4c6fb2c1829
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:40 +0000

    physical pipe plan add mother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
