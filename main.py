b9654ef69e8175f1382f0558198955c1186f2912
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:54 +0000

    back last project insist the

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def count_words(sentence):
+        return len(sentence.split())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
