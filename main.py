4745ee21b781b8de3344899f88a92aebaf845407
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:23 +0000

    lay flavour thumb such doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
