f08736d584e40ebcbab3cd8c825105313f629431
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:52 +0000

    getout correction without precise crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
