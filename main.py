e16fa17f51d6d40c09f0af7417e5655001ecb5c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:37 +0000

    spoil about introduce nose relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
