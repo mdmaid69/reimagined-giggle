d5ac7c6504228edff1db5b1dd53d065097cbc9eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:34 +0000

    need swallow feel approve straight

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
