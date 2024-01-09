ca8128957674e41036b123ba5e39c8af7aefc3a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:55 +0000

    once minority drive remove callout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_odd(n):
+        return n % 2 != 0
