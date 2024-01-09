1462f25569144c3bfe1d3c92539a6316a905197e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:40 +0000

    word score sort unfair form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
