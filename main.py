bf205b1dfa5bf7a34796da53629da9c5de50da4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:44:15 +0000

    possibly but brown rule speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import platform
+def get_os_info():
+        return platform.uname()
