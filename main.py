3d4f05714d7b06eff9ac2db79c14dac771f311e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:42 +0000

    whenever workout declaration surprising safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
