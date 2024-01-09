260ad7afad34f1ac48844bb3d08ba95a973b49da
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:58 +0000

    tape approval type attractive its

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_palindrome(s):
+        return s == s[::-1]
