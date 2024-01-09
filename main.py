4733fa3bae59f710f9f241d978b023c0da2b643f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:03 +0000

    vote cinema trap bringforward head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
