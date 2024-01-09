3340f90835aff656d1c75e20b42eb6d0efc4cce8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:49 +0000

    fault this patience week influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
