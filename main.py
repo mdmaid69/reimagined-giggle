e9dfb8f0fb54979bf874ca23ee8f94eb8ee9cae9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:37 +0000

    some love wheel seriously by

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-print("Hello, world!")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
