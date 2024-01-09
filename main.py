5f67def7eb2e9785a60fe376d9df56e226e86522
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:58:29 +0000

    drink talkinto destroy feature sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
