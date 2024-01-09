9da81e5be83c573942e097fa5868d9577c7943b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:13 +0000

    abroad deserve deep call bring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
