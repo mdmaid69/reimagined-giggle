f9f6737c8fc1e88b8cc7ae1b4aad2c46015edf2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:15 +0000

    string rest relative criticize stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import re
+print(re.match("h.*o", "hello world"))
+import time
+print(time.time())
