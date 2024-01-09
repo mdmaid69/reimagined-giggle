6a28b0e8ee5a41c441081ea6552bcc7fc900e275
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:41:36 +0000

    quantity city freedom degree let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
