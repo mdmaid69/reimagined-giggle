f046a1a0518da5acb668d97a1890c8d8e06df7ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:15 +0000

    between crisis condition beautiful unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
