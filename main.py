ba5aad7e7753cddeec0b6f817b06103d16689cae
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:54 +0000

    talk birthday average worth reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
