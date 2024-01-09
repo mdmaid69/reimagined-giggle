aab3b8da3aa9076aa91c6472680e3ed6c00d89ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:03 +0000

    relation near store terrible trial

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_set(array):
+        return set(array)
