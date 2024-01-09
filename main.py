9925894bd2f9b08dac89d5f33188c2b7fef35436
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:28 +0000

    home inside light begin come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
