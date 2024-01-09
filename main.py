7e6b531b0da724bbdd8e2820389b5a365f8f322b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:22 +0000

    instrument report contact pull ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+print([x**2 for x in range(10)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
