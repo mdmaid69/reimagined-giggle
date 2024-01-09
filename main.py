cddfba4e838cdc5d7f2385c9c116784cdacb2f3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:56 +0000

    culture anywhere shame sit fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
