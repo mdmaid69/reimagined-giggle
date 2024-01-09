134147e7d9d6c4048df3904d07ed98b81b6280e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:57 +0000

    anywhere formal ring comment concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(10): print(i)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
