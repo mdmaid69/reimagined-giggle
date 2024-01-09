2be8badd15807e73a34622db0497fc2c1971641c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:35 +0000

    sticky rise succeed challenge mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
