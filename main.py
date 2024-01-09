7e46a229d068eda9956ba02d1ab63344c5691544
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:57 +0000

    fold callback tidy minimum opinion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
