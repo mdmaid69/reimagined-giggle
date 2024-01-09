224cb8d67de11bf5e8a43a3ae62e5e8dc1e168b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:42 +0000

    house government minimum face emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
