268d0a43e9bbb66b258b475188f6e3a9b63185a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:00 +0000

    top switch famous breakinto plenty

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
