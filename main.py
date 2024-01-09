94474d12dc16db5b0a45922205a6755ce223c16a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:38 +0000

    order commit 566end congratulations hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
