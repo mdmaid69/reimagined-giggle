0c38c470edc983a0bca222ec33640cc975e7d64b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:38 +0000

    identify imagine burn sad escape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
