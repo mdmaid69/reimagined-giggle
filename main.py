c1ae711ba79601f2512cf5119616f725a05cfef3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:26 +0000

    present change treat fast worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
