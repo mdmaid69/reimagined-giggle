45a4deb9413cab9fa6d8783503651979b5723529
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:28:39 +0000

    Ww go big squash absolutely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_list(lst):
-        return lst[::-1]
+def find_max(numbers):
+        return max(numbers)
+  def subtract_numbers(x, y):
+        return x - y
