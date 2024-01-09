85504484bcfa16b202959755b2cbe441aff8ea17
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:36:12 +0000

    best become may boat stretch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 def calculate_work(force, distance):
         return force * distance
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
