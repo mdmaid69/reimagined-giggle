3b1de12c261180691336f6136ade579ddbdf2997
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:07 +0000

    exact murder steep world of

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+print(time.time())
 def calculate_area_triangle(b, h):
         return 0.5 * b * h
