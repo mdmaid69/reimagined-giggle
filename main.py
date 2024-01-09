a2cd70e078ca581221011cb9014f455bdda84dba
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:56 +0000

    share pool forget fill keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def square_number(x):
-        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
