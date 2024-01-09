a66198f6cb892707d63c63dd0a53ec5630ccf055
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:10 +0000

    weekend next involve rid exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
