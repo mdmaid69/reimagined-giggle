2fac9195c6d20dc8770446cd3a8c4a4a61201ec5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:37 +0000

    Itself breathe wherever treatment easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
