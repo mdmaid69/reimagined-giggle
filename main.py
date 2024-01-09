9b7448bac69093aa94ab66e81525791a4cc0eb8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:07:59 +0000

    wash lead whistle port carry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
