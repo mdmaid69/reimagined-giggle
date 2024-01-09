dbb81d9e5a211f2ced6cc71297206ef4cc86a052
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:38 +0000

    travel shoulder persuade bit consider

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
