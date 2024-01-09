e8446cd33d8ec01ba1ebfb125ee9254c144a5ad4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:51:04 +0000

    Oo soft address as turninto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
