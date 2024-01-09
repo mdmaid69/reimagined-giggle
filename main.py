f7083f7d709da9469a7c3c631fa62ca4c4a6bd97
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:55 +0000

    hope after close health boat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
