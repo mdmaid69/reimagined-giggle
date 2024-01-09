ca2d681d88c6baa59dbf643f28663e5b9587a0be
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:08 +0000

    threaten thorough moment simple cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
