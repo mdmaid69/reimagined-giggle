c636d9d44c1a859684053cfcb6af26b3c65e2fd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:30:37 +0000

    protest drop history rent slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
