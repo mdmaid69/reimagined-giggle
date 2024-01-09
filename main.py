5afe367ef0434f3cb05d7e6e4e5dd4bd319601b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:12 +0000

    work free public behave face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def change_working_directory(path):
+        os.chdir(path)
