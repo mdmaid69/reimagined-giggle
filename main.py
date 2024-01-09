db40e3cde0ab023cd7ca8fc46a6a87b19cd6d57a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:39 +0000

    total disturbing hurry whoever valuable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
