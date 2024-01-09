31d6d70ddc3b95346529edfcbb39fbf5b5a45e1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:23 +0000

    gift turninto somebody estimate shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
