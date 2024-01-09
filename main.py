3fd194b86bbd4de33c78477d89c4bb38169c8924
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:56 +0000

    slowly upwards combine Hh lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
