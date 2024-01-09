c89d706c96819ac9c20f7c136b01c9d67c054d72
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:53 +0000

    sure really behave second smile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
