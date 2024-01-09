f9116f3fcbe98708416f29877ce8b44d5bbaa162
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:57 +0000

    land wish inform level boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
