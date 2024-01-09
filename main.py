4b06aa1b857ea809f1592da4792a5d236e4211db
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:49 +0000

    follow embarrassing perhaps keepaway area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+print(os.getcwd())
