ae043f7a2446129b663661fbe9cf335b8b379aa8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:20 +0000

    order judge development phone average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
