46858505172fc47d9936c2161c617925e75c20f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:39 +0000

    plan beginning dark chin now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
