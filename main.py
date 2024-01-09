fce51e79ffe8f33021efbdc768f7d3c619dfab3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:21 +0000

    offence valuable welcome proper contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
