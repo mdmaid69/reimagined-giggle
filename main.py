8b760376dcfe61a5febc205ca10bed4456e6b70a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:09 +0000

    build tape worst under business

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import datetime
+print(datetime.datetime.now())
