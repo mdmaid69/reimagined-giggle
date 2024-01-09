7e0ee11e52afa635703c53fdc9f625a2793f236e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:01 +0000

    development off typical healthy result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
