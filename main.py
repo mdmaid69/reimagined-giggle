786e50d0a018677506100ceaefe306061ac71bcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:54 +0000

    judge release wire equal pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
