29bd305d73aefd703da3c23ab4263912a81dc0e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:03 +0000

    main direction actually gap something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
