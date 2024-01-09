5b705882aed70b75a8e991fab26b15bb530f5baf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:12:26 +0000

    light test level say worth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def reverse_string(s):
+        return s[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
