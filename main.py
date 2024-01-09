535a71bfa7903f63dccab3d71a17b3ae33a0be80
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:55 +0000

    fortunately wind plenty rest range

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
