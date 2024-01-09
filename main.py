9cdf3ee21ead589c4cf062c3f7cf8cbad68f9441
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:08 +0000

    ourselves mistake think already must

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
