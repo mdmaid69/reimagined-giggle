3d79e13e968078f50b94c5e2960e472f34354e15
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:01 +0000

    education office steady creature equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
