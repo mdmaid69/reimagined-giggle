36fcf2d69fb032a741fd93e9514cf23094b21116
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:34 +0000

    sure hear cover ride improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
