fcdcc3048ae58e06c056c40e228af13f98ff3694
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:07 +0000

    increase closely steal bound familiar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_max(lst):
-        return max(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
