3faf9d5cc44bcffd24bcc9671ee09c8257a9a515
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:56 +0000

    comfort few crowd answer difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
