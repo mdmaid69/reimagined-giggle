0b85a9b4a8822f902e323613c4facbe1e6d9ebf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:40 +0000

    someone emergency certain bang bend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
