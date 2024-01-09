749ae700a82254e73402187e0736d6a153717434
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:37:32 +0000

    amount drink picture offence study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
