9a9dbbaacb38bf47f59bcd26b5b9795a8f15ef8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:48 +0000

    breakthrough everywhere translate limit knock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
