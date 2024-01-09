23fe310fc2df3886d1d05d027114102cb3c25110
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:20 +0000

    accidental light weak little runaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
