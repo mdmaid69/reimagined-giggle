7b462b9d63a07eeac36688c421aa0a316a18ffa2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:59 +0000

    thorough capital behave talk lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
