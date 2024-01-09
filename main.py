101c82556a2afb7da497d029f24b76ded8f02abe
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:16 +0000

    circle dig direct turnon touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
