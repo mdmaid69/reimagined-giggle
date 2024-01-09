8dc1c716c827a28c56041f9077d35e7b051101f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:12 +0000

    line raise spare sting male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+print("Hello, world!")
