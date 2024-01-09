4ffbd873cb007b26f126af3d2cf9fd3d11427e9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:01:16 +0000

    design top ocean relation perhaps

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
