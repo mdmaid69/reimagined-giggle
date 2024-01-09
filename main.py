72a88f4444a00b85317c16646d5f2e8262c6e7ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:34 +0000

    abroad surface slow before rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
