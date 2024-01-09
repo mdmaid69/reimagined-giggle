0cc652a64831fbc9be10fb8be039b4b9b4055cff
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:08 +0000

    birth early month nonsense will

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
