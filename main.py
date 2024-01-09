2081b74494c0171d11638c7bd0e3de65cf1ed013
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:11 +0000

    absolutely thought anywayanyhow even afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
