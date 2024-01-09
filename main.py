67568fdcc3391baaa8b627570b6b5153085be659
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:24 +0000

    allow quantity skill know drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
