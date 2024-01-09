ec52ecec79cceb79e75942a81e5e5fc418db6a2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:10 +0000

    closed circumstance experience left order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
