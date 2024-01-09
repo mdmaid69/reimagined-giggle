96e6d891c1a4adbf5695324e69bc85878fb8d807
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:01 +0000

    obtain scrap move need related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
