ddf0874d77e287e15110e9b8f607c7c0813e2416
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:26 +0000

    wipe use will negative surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
