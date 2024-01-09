d528101250bd96a13183c98884ab26d575fc8579
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:26 +0000

    wrap care weigh visit rely

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
