be1f3cdcadf0c6203886c599e7cc3559342899b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:24 +0000

    stare connected flat succeed teach

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
