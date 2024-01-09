6fee1d97664b98c3a49eefe457edb25192ac2491
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:34 +0000

    table unhappy smooth bread stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
