4278bac8c15880250547218ee48ce15c2f364a38
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:15 +0000

    sign nature mistake design drink

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
