3daff43f16d9bd1a063a5fb56419d8e2bcca511d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:06 +0000

    unite rough love neck completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
