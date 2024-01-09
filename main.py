3c77740dc4ed1410639b683c0c276f89717d86f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:29 +0000

    possibility catch emotion smoke middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
