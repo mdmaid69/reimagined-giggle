bfbb20d5e8bd5e5c2f8afa28cb69f2d4b7e86886
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:26 +0000

    brush city closely post Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
