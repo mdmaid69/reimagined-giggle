25fd0df24857f944257dc39cc727ceef66d849fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:57 +0000

    towards seem loan afford comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
