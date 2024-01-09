ba5b921d63240e6de1564c15bac8207b4467568d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:47 +0000

    female practice keepaway chief remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
