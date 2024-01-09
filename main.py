07cfe7731ca026d3c0eff1cd15b6da025192b4f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:51 +0000

    sensitive lunch Ll laugh night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_length(array):
+        return len(array)
