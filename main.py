30c8aeee39d4dd1b111664ad772ab149da0bd5f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:51 +0000

    age hope basis ticket combination

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
