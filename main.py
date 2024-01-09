e5cdac6f50060168f7edf19f12580f7f14f2e425
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:24 +0000

    tune before recovery like makeinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
