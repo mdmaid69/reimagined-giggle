21fa6d00c5ec2295ff84d9367d4be2cb2c270d87
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:03 +0000

    still dish claim clearly let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
