807f77c33cdf4d51c05399b998ed0f0ab7f49801
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:31 +0000

    late fire just straight nerve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def is_palindrome(s):
+        return s == s[::-1]
