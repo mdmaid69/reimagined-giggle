a20df3d73079fb6a2e7f61b01b13fff86d94ef33
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:39 +0000

    fence yes unit system skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
