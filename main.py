9a83b5f0a301ff62543466a74f800ca0626fd147
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:32:27 +0000

    566end school real respect storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
