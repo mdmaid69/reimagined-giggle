28cf507ace5a0e69c049049aa9f257a3bf0a83c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:59 +0000

    persuade action image fat to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
