359a6fab20120239195f0be82b5ab786d42f87de
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:50 +0000

    period section Qq plain drawing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def cube_number(x):
-        return x**3
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
