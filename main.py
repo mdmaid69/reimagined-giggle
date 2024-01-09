34cde4aba9aa49717fce91cb5bc4645fe205e512
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:54:34 +0000

    straight statement story mess cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
