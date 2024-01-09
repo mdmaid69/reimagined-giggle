c67e5070d532714936ebaa4caa01631f8710e5de
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:01 +0000

    sensitive design concentrate passage record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
