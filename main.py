6b2920c758968273ab083dc34a953dcd59e0d19d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:50 +0000

    earn tool cover top equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def divide_numbers(x, y):
+        return x / y
