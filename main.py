86e6931cdde3fc4e297e7123b73a0b55f163ffaf
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:16 +0000

    minimum Cc learn unless hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
