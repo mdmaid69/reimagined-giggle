6f169492e44abba5f0654d4c1bac89267a0537e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:10 +0000

    hurry stupid organization shoulder land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
