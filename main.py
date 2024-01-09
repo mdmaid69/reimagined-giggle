641d6efae072c89b8c9696c9b07043ba551d1a15
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:10 +0000

    fire century manage pity satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def remove_duplicates(lst):
+        return list(set(lst))
