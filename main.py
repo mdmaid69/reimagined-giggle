e03f4a81c9141a03de7ef830da9588f638d977ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:51 +0000

    weak remain principle wheel height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def cube_number(x):
-        return x**3
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
