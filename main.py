eeda6b8d9fb51b8f34d8590ffc5dcccf7be0e97a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:32 +0000

    mark decision happy far imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
