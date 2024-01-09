51d55b822d6773197826ed836507360f7dd9b37e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:12 +0000

    border either fine maintain wait

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
