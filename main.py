2adafecdcdf53af1c4a2e1fcd0cad59788dcfb20
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:09 +0000

    spill decide mystery before ocean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
