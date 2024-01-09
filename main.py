9a2bf2e0f01bd7674d12426adb4ff14f880f0dd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:42 +0000

    law small copy hit criticize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
