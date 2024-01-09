01f99da716d6e90ef3c4ef0084cd24bd396c5e06
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:47 +0000

    yesterday necessary criticism split whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
