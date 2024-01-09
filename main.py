47895d66864e1287c2d921cd96f7e8320f97dc29
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:49:00 +0000

    connected claim mine recent ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
