aec0ec1411c176e55f7c614c2fdacf23304c4f51
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:50 +0000

    after swallow bringback worst willing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area_circle(r):
+        return 3.14 * r**2
