c7821e79b8e2180695fb4b1c42d26e88c6c3c06e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:15 +0000

    open programme mind will frame

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
