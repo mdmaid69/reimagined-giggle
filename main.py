1d958e9afd3200dc57467684863d3d130c9fa576
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:01:42 +0000

    responsibility my brain nice eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_length(array):
-        return len(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
