b8b3caf55219c03ecaa1fb05b89740da5f56622e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:57 +0000

    light sleep guard satisfied but

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def clear_array(array):
-        array *= 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
