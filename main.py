b095aebc106e1ae39b02602173d7ccee6ee3bff4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:01 +0000

    can remove write usually honest

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def remove_directory(path):
+        os.rmdir(path)
