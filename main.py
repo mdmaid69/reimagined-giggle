ced232567b93d03ee8175bae59714612719ec41f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:25 +0000

    turnon mine thorough least person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
