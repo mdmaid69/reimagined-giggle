6203e41cc7895bac03a8e9d1b8def30f8169ae3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:30 +0000

    anywayanyhow complaint tomorrow willing downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
