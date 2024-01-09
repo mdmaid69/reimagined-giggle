817448fb96f0302662afc9a718c093b57874bb37
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:44 +0000

    add chase stay too family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import platform
-def get_os_info():
-        return platform.uname()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
