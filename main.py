71e101aae24a1f2371c4ccbf208582f94a6bd1c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:33:29 +0000

    passage meal once lend beside

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def square_number(x):
-        return x**2
-import sys
-def exit_program():
-        sys.exit()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
