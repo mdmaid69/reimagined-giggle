1974580f0ebb18e11eda24133ec7c2b3c71667ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:37 +0000

    once design edge normal sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
