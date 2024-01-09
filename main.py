a5bc29f29d79fc9c4134119e3f15eb4829ddfb5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:38 +0000

    understand responsibility rough travel factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_power(work, time):
+        return work / time
