a78d14de06227539a991fece8db33fa940d9d797
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:37 +0000

    second load normal praise back

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
