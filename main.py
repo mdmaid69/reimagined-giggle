10304e5750e8594015df89c256a5e5ec82b3bd06
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:21 +0000

    bill conversation branch quantity bus

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_even(n):
+        return n % 2 == 0
