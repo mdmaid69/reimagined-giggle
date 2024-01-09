aa472eddce6b41e3d826d9e1eaf2a4dabe0b7c4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:00 +0000

    blame centre shallow danger arrive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
