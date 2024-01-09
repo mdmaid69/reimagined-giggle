27f3ef6e768c5bf00af9e538567598ec7831cf07
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:16 +0000

    settle language holdup crash runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def delete_file(file_name):
+        os.remove(file_name)
