388811e027957fe6d1f20a3c252313377764156f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:20 +0000

    fire move female be complicated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
