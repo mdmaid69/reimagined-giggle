64fde29df80a5bdf7470931c13459dfc3f37df8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:47 +0000

    thin private advice almost argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def add_numbers(x, y):
+        return x + y
