23110c5b73e04517a63b29382985c00a1f09f621
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:46 +0000

    ride tour classroom trade wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
