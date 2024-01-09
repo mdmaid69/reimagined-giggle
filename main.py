7b365e81eb5c0d902c993068c61cbd9ff5b28d46
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:48:13 +0000

    consider clue help Vv brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
