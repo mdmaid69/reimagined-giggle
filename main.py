495aecea7fcd1e102bd256baf9acf4570b79a233
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:16 +0000

    mess become too godown plan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+x = 10
+y = 20
+print("Sum:", x + y)
