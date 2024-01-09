647504146776a56d5dc26c618267b73e535d77a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:06 +0000

    create even this fork base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def is_palindrome(s):
+        return s == s[::-1]
