aa447cb1a502618652c74df545fa482cbf93efa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:24 +0000

    breakdown stretch pause class proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
