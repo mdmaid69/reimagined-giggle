f90a63f020711d56ca975b24723f85798dc37193
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:05 +0000

    uncomfortable beneath purpose little wait

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
