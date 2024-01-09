b56e9a66133ba0ef233512b471d63fbeffc1e592
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:55 +0000

    light sell public disappear tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
