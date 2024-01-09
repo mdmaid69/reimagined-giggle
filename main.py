94490c33692ea2037add8565c77be8bc43e3a83e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:07 +0000

    quantity uncertain wild crazy or

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
