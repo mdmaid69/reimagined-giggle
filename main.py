4edd80dc37ed45d2c9d347361ad45d051a6d9dd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:12 +0000

    takeback swimming knock runinto or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
