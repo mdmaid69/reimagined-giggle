e45c93fda851e209251783b31054d43a26cef8a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:11 +0000

    command fashion discuss official wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
