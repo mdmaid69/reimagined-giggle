232f1878bc989342fafb8f43994e8ad215decc14
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:32:03 +0000

    come project act delicate before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
