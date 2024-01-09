29fb090ef077717684680e52ebb75baaaf0f32d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:44 +0000

    frighten head advise command wake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import tensorflow as tf
+print(tf.__version__)
