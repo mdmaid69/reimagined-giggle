482a14867ec6bd15f85e14b34df412ad51fbed82
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:12 +0000

    common share win birth turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
