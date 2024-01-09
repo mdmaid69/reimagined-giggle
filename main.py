cd3343b3a7213672678b83545922947636998437
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:11:51 +0000

    early preparation bottom fly Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
