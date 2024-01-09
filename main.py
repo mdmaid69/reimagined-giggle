4969c3f0713456f50385610898681ed0b51f99fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:09:10 +0000

    exciting its hate law comfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
