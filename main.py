3a55dc8c71ec40d2c1485b90699ca1b5b74ecdc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:59 +0000

    prepare determined burn remember flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
