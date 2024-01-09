0e1d03a24b0e487cb1eddd7f6bcaca0c6d5120f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:45 +0000

    off part paper worse Ww

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
