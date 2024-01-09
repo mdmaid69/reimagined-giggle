30dd3bb190038c7b4ed78edd28a7d364b7f34f51
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:20 +0000

    phone breath continue tune plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
