4e2320a832c8da5ae3d53d6a9f6fa5090eb125fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:52 +0000

    buy breathe experiment pretty no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
