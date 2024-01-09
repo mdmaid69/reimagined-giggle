d1289c9a8e89c808a427afd06cb328b78a7a0ab5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:05 +0000

    whoever broad next impression pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
