4f0a37d92390948db88bd0d6c785cc008f1a2b40
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:09 +0000

    she mistake crazy off do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
