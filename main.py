1434c373f58d99dab73a15e91f1da4f79002da8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:28 +0000

    correction do exciting bit takeover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
