bdeb10dbfc77af1e8d9f2ac20c1e11848b72c497
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:04 +0000

    history gooff me giveup wave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def sort_list(lst):
-        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
