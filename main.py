c8864d4b2c13ce6630f73dfe8aa61ee8c83e6c08
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:43 +0000

    runout free name confused available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
