e7c1d095b325aad430c1718018af1fda6216588b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:59 +0000

    afterwards every depend freedom 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_repr(array):
+        return repr(array)
