d76e0aeb2e45e6d1554d6e9c5458ba7a25d861a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:15:34 +0000

    building your pollution anywhere wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
