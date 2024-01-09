cec84ef329b9d180eb10fe737aac2e85757e37e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:26 +0000

    circumstance hers lay Rr money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def cube_number(x):
+        return x**3
