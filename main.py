f2cc97ceeee06444dbaf99699eba82e390d6acde
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:33 +0000

    follow wear post scare thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
