cb11f19472331913f5eee8e1b8922ea2326a9e5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:55 +0000

    oil wall pressure area principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+x = 10
+y = 20
+print("Sum:", x + y)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
