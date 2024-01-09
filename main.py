ea2f9f10ff20d405e20dcb83af9d8394ecc6553b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:56 +0000

    late Itself used makeup ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
