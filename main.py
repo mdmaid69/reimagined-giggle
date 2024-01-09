be673028d4d4bf50325a94e927d280bdb23294cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:10:15 +0000

    stare false level plan combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
