23e9272ae7dc2fce964bae5529a22dfd310afe1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:14 +0000

    unless through share wide hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+print(os.getcwd())
