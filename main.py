17ac8393eafce4fe968e9d1141137f4ac36eef37
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:36 +0000

    present crash display stand telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
