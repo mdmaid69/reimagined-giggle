c56ae7be967238a26c5c6eb96219c80dd0705d22
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:07:06 +0000

    purpose measure damage appeal somewhere

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
