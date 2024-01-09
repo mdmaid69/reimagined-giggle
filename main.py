6fbb48ee2a5246bd7c51b480f8893243606ef5f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:48 +0000

    sticky after concentrate fast anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
