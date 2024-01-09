f3d31487072f7538c69151454f5a377cf8bf5ffe
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:36:31 +0000

    herself pinch excuse thinkthrough callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
