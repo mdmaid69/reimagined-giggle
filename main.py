3479a0508aa1e4199cb22a2ef08878b303e6d03f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:33 +0000

    violence careful loose expect somewhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
