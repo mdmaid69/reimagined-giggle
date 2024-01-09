aa4b69bc4e6b33dd94a2e8144fd99bd324aa6162
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:20 +0000

    direct pay thorough day mysterious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
