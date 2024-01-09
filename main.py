3c7c6ca2c86cb6ef02b29aa198f7934de19f01bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:49 +0000

    raise match behind group be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
