27d7d3a7c5f40a3b5a2657270fbea7ded521de57
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:02 +0000

    pen ground their occasional opportunity

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
