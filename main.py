c565094fbbd163bb4c86b85a3de735811a30d2ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:57 +0000

    violence confidence coat rent combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
