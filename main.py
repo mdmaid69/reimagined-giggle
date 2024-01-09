846b452d4f19a98f0e630c0ec083eace08a06a0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:44 +0000

    separate giveup type firm accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
