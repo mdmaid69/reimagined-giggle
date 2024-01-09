b7b3ebb988a5d1e111bde68e24866ce3952adb53
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:43 +0000

    worse healthy divide bath pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
