b43a8cd6d867e5602d1be04dcf6d36e1ab46843e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:19:45 +0000

    cool bill fresh beautiful near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
