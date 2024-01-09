2e3373c1ca976fa46dda7a1e628fe6ca3ea2c660
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:51 +0000

    raise visit trouble confident wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
