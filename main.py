46678c01ab8a351a34a3de56935b354a47ec0e24
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:10 +0000

    getback sudden cut begin fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
