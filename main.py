282ca55b2b1b7b7e6d233b1d7c6e11a8d32ab39c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:01 +0000

    appear word knowledge value progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
