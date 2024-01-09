943bdc6ddde706bf74e001f5320229e52a88ada0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:09 +0000

    reserve intelligent game everybody tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
