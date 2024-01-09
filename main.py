7fadcc0fb902e62c7e28b0076df16cfb38553fbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:38 +0000

    upwards proud minimum Ff turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
