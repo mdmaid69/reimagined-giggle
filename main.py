470401f5441b4e4ec2b56fce85892e504353e02a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:13 +0000

    public true wall stick bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
