9898b6ab9e5f268818d1174d7ce4038c6ed525c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:23 +0000

    charge influence entry alone squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
