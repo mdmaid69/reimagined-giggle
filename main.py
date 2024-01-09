ba550b9f631535e4a9096945ef0ca29121403265
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:24 +0000

    woman complain love brush load

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
