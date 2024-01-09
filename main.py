9088aa62befe115d4980d95b49afe0f640657004
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:14 +0000

    thumb takeover main someone edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
