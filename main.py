8d293ebe01e5d045f3d22ad9d5d2473e4a5f136f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:14 +0000

    quit by holdon sweet brush

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
