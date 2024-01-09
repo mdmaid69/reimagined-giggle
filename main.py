8fc418a362732e4c05ad6acdc334f5a81a726725
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:58 +0000

    loud obtain luck pile hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
