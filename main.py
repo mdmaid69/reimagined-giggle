db7735d66abe2ea3850beb7da0fa347904acef65
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:01 +0000

    sail slip music bringup journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
