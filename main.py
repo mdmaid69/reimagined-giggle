c6bcb6ef7c8f6341ffb612309ae9c34c36a7b3a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:32 +0000

    test dry sing city certain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
