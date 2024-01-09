53cd42ef3fe732ecd35fe3ff8f1d93774270635f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:38 +0000

    prepare join smell error in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
