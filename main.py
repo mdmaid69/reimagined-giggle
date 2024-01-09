bf6f69bb016f26398f0bf3dbf743d8489ec7e4d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:20 +0000

    father payment least geton brain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_odd(n):
-        return n % 2 != 0
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
