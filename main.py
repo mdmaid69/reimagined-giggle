72de5298e269d7e717eab053ac8c9c01ea8b7dfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:40 +0000

    star everybody factory order frame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
