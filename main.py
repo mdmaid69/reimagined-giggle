8b46f10d081bb0ae17a973ddad0418fb75535ce3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:05 +0000

    bringforward apply join Oo emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
