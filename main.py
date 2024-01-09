97329cca5e10381fab4a5a5201b7d797d69baa0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:59 +0000

    throw forever bury only rich

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def count_elements(lst):
+        return len(lst)
