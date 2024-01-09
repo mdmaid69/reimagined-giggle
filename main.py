ffd4fedc03380e464fc4f7983bd4541d21909a11
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:27 +0000

    splash possibly scare forever warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+import datetime
+print(datetime.datetime.now())
