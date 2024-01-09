46f0c2c6b7c4e71e198dd9143410d3537a103926
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:43 +0000

    medium bringforward rush factory neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
