f3cc97ba54bfc1180319de3dbe5aed8d97a42305
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:13 +0000

    right leave tax type failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
