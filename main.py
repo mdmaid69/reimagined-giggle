74f3ef4fbf3dd2c51de06dde0eb82d6c74587011
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:35 +0000

    long die become joke transport

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
