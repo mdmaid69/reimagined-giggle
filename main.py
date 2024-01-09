49686a1536be594f84a8d689dfc1d7975bd762ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:04 +0000

    exaggerate hide use load Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_base_name(path):
+        return os.path.basename(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
