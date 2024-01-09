f79415570f7d7e15151c6d6800cf54d47900eeb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:42 +0000

    earth clue pool before left

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import random
+print(random.randint(0, 100))
