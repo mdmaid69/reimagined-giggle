80325c68ee884e8f58a23a7a2a2b6ccd5ae60e00
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:23 +0000

    squeeze insist little true team

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
