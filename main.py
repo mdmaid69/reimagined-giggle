b50c90758141976a4e32fdcef449847d0ef77fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:16:30 +0000

    human fire regard off breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
