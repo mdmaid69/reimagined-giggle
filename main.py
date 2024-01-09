adeb0917ce3280bcdebf3a3ace39077ff61502b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:20 +0000

    harm swallow always care instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
