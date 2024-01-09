b12df0eed707ce057737e1ae23a1ad9842ae3157
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:55 +0000

    lead drop cheat centimetre him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
