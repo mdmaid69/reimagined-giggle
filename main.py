b4051ee7a3a6e8930ad024ee7e747840738d7976
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:00 +0000

    properly square even congratulations difference

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
