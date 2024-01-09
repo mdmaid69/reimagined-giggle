2ad86eab94d634e238645ccff772b7bfcbd2e614
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:20 +0000

    change shallow software crash shop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
