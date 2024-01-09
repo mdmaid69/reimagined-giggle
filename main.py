401b16c152d4aa6fbcda6ec3f64f0576353165b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:20 +0000

    interview 567enemy knock pick permission

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
