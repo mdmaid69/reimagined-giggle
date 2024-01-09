0200ff943b43aaefd4401336d7c44a1756784f42
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:17 +0000

    matter up live sound friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
