4e5de18b27f9580b87295fc08d5cfc15f9c2dda7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:59 +0000

    except basis fond thin print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
