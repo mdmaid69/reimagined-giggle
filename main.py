ff717d2510247913e5a84d9a035217e181034617
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:23 +0000

    design within 572enter hurt pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
