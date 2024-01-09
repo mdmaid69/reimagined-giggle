a073cc4b4e9208d5bff3ec221f0a658d07b86c6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:10 +0000

    thing season rule male run

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
