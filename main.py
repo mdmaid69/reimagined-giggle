b4254690ff6ff2e0fd121e52350fcb6d52999f27
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:04 +0000

    news comeoff love battery in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
