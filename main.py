49a61532132e44cf5732d59f645dfd4a4e21250a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:33 +0000

    gift desperate poor bridge road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
