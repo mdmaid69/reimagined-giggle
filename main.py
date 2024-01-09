0d89ea51bc754bc1061b5d8e9c3cc2cdb989dd09
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:59 +0000

    minimum appearance tragedy pronunciation loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
