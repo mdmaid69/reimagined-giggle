cba98082b9558c5a64e8c3c118a8ec86f52e43dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:31 +0000

    jump slide there regard happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
