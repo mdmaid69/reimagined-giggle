c3ecc40ff710b6cdc312ab86a82caea32d02976a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:40 +0000

    complete quiet report proper fight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
