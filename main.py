389bab47438ef3a6918f5e5d31ac2e6a684377cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:52 +0000

    fierce breakinto train luck computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
