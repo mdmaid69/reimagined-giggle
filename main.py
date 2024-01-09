bb97310f3dc1ea31b8f9a6bd2a796f55e4de5782
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:25 +0000

    battery instrument grow glass pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
