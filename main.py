79f4bd3556828204d6d9950e6f689e9683ecf115
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:07 +0000

    same speed minute wide needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
