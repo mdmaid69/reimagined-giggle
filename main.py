ee9cdea36637b36443fa5ae37110c244f348c809
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:12 +0000

    position emphasize direct unlucky about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
