0c0705fcb74b68a30fe17238cf9c3dd0c9be8956
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:06 +0000

    which row dark limit emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
