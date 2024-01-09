89f0c329d49f85101d91ffda5be3e73612f53853
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:25 +0000

    available expect alone young pipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
