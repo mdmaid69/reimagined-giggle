d2a8ca42cfbc07f9739682363dde8813736d4ad6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:54 +0000

    project immediate emphasis complicated behave

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_area_circle(r):
+        return 3.14 * r**2
