f982b77e169cebb19fcfaa8198f6be37ec05b85d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:11 +0000

    mainly age shoot afternoon translate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+print(datetime.datetime.now())
