846328a9f45e79cbeeb91d83688332ce1a7a7b5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:44 +0000

    her responsibility hope tread false

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import time
-print(time.time())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
