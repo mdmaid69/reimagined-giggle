d5c94e94314ab1fd3b599e041fec7adb70667073
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:30 +0000

    positive equal nor considering well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
