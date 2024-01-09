8cd36a0695966c8944737eff9a3ceb88632b3712
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:26 +0000

    come otherwise point never unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
