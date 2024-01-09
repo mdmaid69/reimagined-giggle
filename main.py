d26a86cebaeff87110cd3e9e5ee9970311866dd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:47 +0000

    chase lookup correction tape embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def convert_to_binary(n):
+        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
