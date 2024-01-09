4912f754f9ab30334876cfad242fe2eb9c19e428
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:27 +0000

    embarrassing ground live wall justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
