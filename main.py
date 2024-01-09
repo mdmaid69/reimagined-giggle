b5926b31cd1de434c12f782bb036356d8130aae1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:57 +0000

    feed regret whichever outside switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
