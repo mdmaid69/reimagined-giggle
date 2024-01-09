082fe8e05eae466ffefd8e97d0d3c3cbc296d29f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:33 +0000

    uncertain free continue prevent land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
