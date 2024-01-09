ec048419a23bccabb6ed6fab7a1494c566862735
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:08 +0000

    foreign difference level convinced top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
