dcd80665d6a19be1f4d2240543f9b04ce8fcaea3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:10 +0000

    chief just brain team surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
