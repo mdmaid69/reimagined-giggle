1d88365b6a9c66b3709c7af728489d6080cfe5f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:04:59 +0000

    comeup choose mail inquire nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
