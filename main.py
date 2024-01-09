f7640c4614c786a615b49d0e2c777f5547a983fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:00 +0000

    centimetre blue some basis answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
