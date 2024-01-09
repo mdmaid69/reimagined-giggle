f4611fc667cbd08de04c990728542943c42fae4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:21 +0000

    pencil sort lock achievement off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
