2b4e3b1381339910bd1fc30ea3b4ee32a7cc0138
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:35 +0000

    agree clearly cope explanation achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
