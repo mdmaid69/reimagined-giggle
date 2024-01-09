b0c96ae4e2a2cb4e3d65cfa85d667129d8edf5e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:10 +0000

    rob plastic bend afternoon mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
