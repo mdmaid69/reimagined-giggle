891efd6669c3a4ca2c433658485e07245c3f9fd9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:10:12 +0000

    getaway laugh switch base gentle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
