fcacf305acba7d493f68b527557ee108e20bf6c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:20 +0000

    crime protect desperate pay freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
