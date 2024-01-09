a4cbc2c4e5e0f8324ad9286c7079966bcf29e273
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:40 +0000

    apologizeapologise warm exciting shoot worry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
