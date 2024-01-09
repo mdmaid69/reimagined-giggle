583d10da9a8ae1e64a95f9e59b21015d4282e752
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:17:38 +0000

    introduction talkback bill food miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
