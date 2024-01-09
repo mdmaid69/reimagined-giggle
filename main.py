6e3bf41a49ac523ede37661d8c0858e904aff4a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:46 +0000

    eager runaway still Dd can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
