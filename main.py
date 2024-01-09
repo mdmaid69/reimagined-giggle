8cee14da2223c9272d44dd84f6c869b84f1a4e89
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:52 +0000

    mother during second remember Pp

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
