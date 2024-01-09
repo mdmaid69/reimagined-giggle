bc5d9b9b9aa8aa50bb8a1ab0efffe2221f37fec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:48 +0000

    Oo away experiment office road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
