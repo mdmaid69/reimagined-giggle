8be139cc40d689ea1f2321b289775d978bdde0cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:11 +0000

    rubbish wire now satisfactory calm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
