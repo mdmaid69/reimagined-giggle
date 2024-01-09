315b4f0cad449744949ca1da45bae01837ac8c0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:06 +0000

    cause style sound might circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
