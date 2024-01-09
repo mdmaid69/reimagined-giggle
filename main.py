50221ae6aed39ba92f596119cd247fbe120d529a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:42 +0000

    block aware date instruction race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_list(array):
+        return array.tolist()
