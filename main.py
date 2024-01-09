c31d788bf771b435e70d4f046de7441923f07c27
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:32 +0000

    stiff empty fall proof Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
