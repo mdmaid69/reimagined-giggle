25ea93bd2b832310464dbbed1468df91a81f674e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:37 +0000

    university order wherever store today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
