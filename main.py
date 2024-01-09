cc2628eded723741762aac23a10027ed3b92a71c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:42 +0000

    sale threaten goup today earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
