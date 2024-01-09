7c55b1408ba45e4bc889a97bab7256e836a55cc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:04 +0000

    nose arm godown turnaway authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
