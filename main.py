d1b58854b9123fa7c1d2a6a7817e50653c356950
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:15 +0000

    Dd similar attempt best hers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def divide_numbers(x, y):
+        return x / y
