9dee06af86c16e17bf561a9284ac658781b71399
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:41 +0000

    trick steam makeinto fault cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
