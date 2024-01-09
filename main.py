2fe4b2b64bdf59e6be81f97799f637a208b5399e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:49 +0000

    sticky live later proof special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
