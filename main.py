7660c4d87a49caa4dc73d367b1be424db85d18e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:27 +0000

    string inform will who be

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
