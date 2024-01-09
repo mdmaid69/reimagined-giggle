b3e4aaa364c5401e162fa89404f76c89b47e583d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:42 +0000

    lift post still grade possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+print(datetime.datetime.now())
