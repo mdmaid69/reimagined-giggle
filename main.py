b6d26b1951832735b01002a442c930b016cf8a30
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:59 +0000

    correction stretch geton carry throughout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def remove_directory(path):
+        os.rmdir(path)
