500dd99b161df3d95981152be28bbf67de8bc1de
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:54 +0000

    down objection reliable fire fond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
