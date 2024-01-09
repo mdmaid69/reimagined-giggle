fee9dc5b0a5a6848b71042c5198267fc92639035
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:21 +0000

    talk from class opinion criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
