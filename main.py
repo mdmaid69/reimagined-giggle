531a06d1ee6df44e64e2b3b214f4b9dd8a4dc6ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:12 +0000

    peace we learn square offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
