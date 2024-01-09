dee382d091f0c7973b9214f1af0a0aecc93bb77f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:05 +0000

    plant black weak error sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
