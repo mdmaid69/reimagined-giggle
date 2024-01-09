a7a1f7f50c80e96f19515cb471cba0cf0318057c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:41:34 +0000

    risk quiet tap to arrive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
