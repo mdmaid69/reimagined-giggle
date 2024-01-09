fbfae0e6f4d46c97f2e0c46f7f710c3090296a7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:17:28 +0000

    stiff food separate set doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
