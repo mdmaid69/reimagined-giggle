2afab12bb6f49ec3ceccd2bf1ef4bc5e61d234cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:08 +0000

    far priority Cc keepaway its

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
