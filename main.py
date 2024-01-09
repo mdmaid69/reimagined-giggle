9332a461ae5c76ce466a2f6e5277572eaf8528f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:46 +0000

    weekend match during fear comein

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-print(random.randint(0, 100))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
