37128fcf3dca7292bfc6d4f7ac4aa0afb35f2a6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:27:42 +0000

    soak alive splash violence fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
