da40d0689ae80c33a408bdf824beafb5ea361f54
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:14 +0000

    near goout language excellent trap

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
