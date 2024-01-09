de7d81807573fccc42d484f286d6fb477f367a14
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:00 +0000

    speed tip cause would Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
