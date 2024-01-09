a9d21e142e2fc6439a66aeb19c11c02b3967335f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:58 +0000

    fit likely problem any honest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
