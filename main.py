602bf5689f5abe666f389af5cc15505d5254e35d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:16 +0000

    public laugh add you she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
