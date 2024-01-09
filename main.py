cb43e72697be0beb5f69750f0da63f48af29d031
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:08:40 +0000

    safety belief tonight hear luck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+print(sys.version)
