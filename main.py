890a6b79ee8801937f6082a3c8d207c81a18064e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:52 +0000

    side hope wash top tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
