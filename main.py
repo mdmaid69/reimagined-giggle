f06ac99288feb038f21458d216beff959a25b8d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:19:52 +0000

    late represent standard pure difficulty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
