8970f8a9ed385eff28acb19f489032e8a582fe8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:14 +0000

    hers its gradually light punishment

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
