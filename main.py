143b5fba576218cacfaabf03f9a047d4ef66221d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:28 +0000

    emphasis result branch conclusion bringup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
