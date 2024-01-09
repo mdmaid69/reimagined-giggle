9486e8be61306fc9aadfecfe58f632a4da4a08f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:39 +0000

    pollution shopping ride behave remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
