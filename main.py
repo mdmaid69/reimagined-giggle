48295fb74975e530b8e4006a9ed415ee166bb616
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:25 +0000

    feed patient name city deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_slice(array, i, j):
+        return array[i:j]
