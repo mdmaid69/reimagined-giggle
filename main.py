5b33aac190ac5f8c9c54760e4c9d77a64649153c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:07:39 +0000

    Qq trousers most Zz purpose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
