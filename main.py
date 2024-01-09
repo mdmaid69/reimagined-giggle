1d74bcb62761a7087966dca9cb294d38e43f5d3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:53 +0000

    simple face exit once please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
