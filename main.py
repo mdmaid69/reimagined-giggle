16c13bb393e943417facd4b46a44065d94786f5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:10 +0000

    blow punishment whether keepfrom teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
