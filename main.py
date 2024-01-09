42a5f3330ee460fc4eedfed1f26805d432d43056
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:34 +0000

    official takeaway reply telephone giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
