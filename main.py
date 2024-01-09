c9daf7c1e9bd2d36dbbc4d0e425b9084815bc482
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:26 +0000

    shop copy our finally owe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
