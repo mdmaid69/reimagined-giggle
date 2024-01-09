5974363d0de6b8b3b5218cd89d01bca3a0693892
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:24 +0000

    none great wish earn tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
