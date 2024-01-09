9c18c9a886b5980a943a951c147ef942b4ba85e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:25 +0000

    just point swing soil jump

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
