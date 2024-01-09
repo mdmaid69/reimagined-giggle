d8b54388f3df480d91bd5413590fe286b785bddb
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:22 +0000

    hurt arrangement moment story wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
