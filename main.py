1dcb204df5fc3d0c2f60539bf2918cd99c4e6d4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:06 +0000

    report fit getoff progress cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
