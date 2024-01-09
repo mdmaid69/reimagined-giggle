cfeb00400e5a646e4a31bf4def0d2b987c935811
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:47 +0000

    feed goback(to) leave action gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
