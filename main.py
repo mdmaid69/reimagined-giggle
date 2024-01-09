23db782caa2f26b821055c4eef4da65e50b4edb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:14 +0000

    arrange care air relative loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
