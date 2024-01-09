219395160a3d109078fce0bc7668f5ad6d219105
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:49 +0000

    base payment fast wet admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def split_path(path):
+        return os.path.split(path)
