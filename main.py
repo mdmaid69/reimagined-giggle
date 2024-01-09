6bbdf8d973f770bf940428cd84d8d8c20ebca6c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:23 +0000

    threaten near recent kick night

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
