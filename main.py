6b519a3bdca64bda5aa0a72de81923b4ca2ff0c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:57 +0000

    dark famous branch completely post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
