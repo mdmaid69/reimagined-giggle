7b10449d6a952531bb75f3c874a055bfdc8414d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:30 +0000

    common drop copy sore describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
