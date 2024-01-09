3d22ce2644a8bbe67ba42526f597cea061bd4b99
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:48 +0000

    568energy worried environment culture fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
