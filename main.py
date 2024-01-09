91371eb2d3501917d8f1ae03e11062a99e27106c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:22 +0000

    determined needle border light shade

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
