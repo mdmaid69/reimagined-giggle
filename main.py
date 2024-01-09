223a6547f5dff06bdd81f10fada1f7d4881d753f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:00 +0000

    on through last line nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def multiply_numbers(x, y):
-        return x * y
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
