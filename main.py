e41bb2b5687f7c78d38d84a4f6ef4946deb7548c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:03 +0000

    about side soon however during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
