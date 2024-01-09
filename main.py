55ed417625a1d6eec672eebf2e21cccbda4417e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:57:25 +0000

    Cc drop used nice cost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
