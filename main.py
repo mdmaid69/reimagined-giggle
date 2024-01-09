c4217a0e3110e235be08154fcd659eaa6202761b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:54 +0000

    anyone book send lonely stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+for i in range(5):
+        print(i)
