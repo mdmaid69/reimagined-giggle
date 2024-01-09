706f639edad04718d5f7b7f5a2261a45cf0aec85
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:17 +0000

    clearly programme lookafter save thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
