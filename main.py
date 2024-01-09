9c19ef30d3cf399767915f5619fc0f79b9dd1938
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:33 +0000

    congratulations error rare main model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
