fa8d26c1b59b41c0bc7bfdc641717dea659d9d75
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:03 +0000

    emphasize fat path ring official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
