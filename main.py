ce2b3c2e5312827b1ce01fca38e2c616504a6e41
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:33 +0000

    apply content raw smooth dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
