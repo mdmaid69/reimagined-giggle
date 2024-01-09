f4da1db39a2c3dff0ec1f7700afe96a3605923c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:17 +0000

    beautiful bin declaration traffic seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+for i in range(5):
+        print(i)
