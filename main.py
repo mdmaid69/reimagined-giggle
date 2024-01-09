e13fd3d84b03b85cb7db23b3af3ec0aeffada45c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:16 +0000

    block guilty for raw nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
