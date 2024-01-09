cc424abb2738ab52c3256ecb8f1e7fca9eea057c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:15 +0000

    exist truth open unkind slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
