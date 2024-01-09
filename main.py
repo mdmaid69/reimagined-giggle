2e7651a3dc928919830682ba8ae09407feed34c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:29 +0000

    vision accidental public keepoff worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import platform
+def get_os_info():
+        return platform.uname()
