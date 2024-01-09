5dbd27546baaeb3a8e411bba20c351b8e9cbbadc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:24:42 +0000

    slide piece coffee tend why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
