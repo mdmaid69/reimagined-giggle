b810a3d28fdf9abd88c014d9b4d2bcb0e69ef9e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:42:37 +0000

    roll sign target mystery tear

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
