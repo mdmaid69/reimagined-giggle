349d54d1ef84ec0a5a258ee11a5f7f4c01ba3c3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:56 +0000

    properly branch worry transparent loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
