7f322a1defb512496f095876fecf91ca840e8a4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:29 +0000

    willing sight weather hard attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
