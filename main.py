ce142cd7d528ec6f2cbf4d70b8ac9303835dac63
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:43 +0000

    rob past ticket discuss crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
