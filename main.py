970d688ea3fd993de783b98dc7aef2bc12e1a0ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:44 +0000

    born treat neither discover later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
