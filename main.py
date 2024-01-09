fd182f318aa17c644c7daca2cc709fb3543525b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:20 +0000

    yesterday scrape used breakout comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
