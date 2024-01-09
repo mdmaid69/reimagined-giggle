979c307933ee543a5ff4612fe9f59ac67fa847e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:51 +0000

    poor scream freedom shade entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def cube_number(x):
+        return x**3
