51f005bc545ab22adeb46788c79cd2062f8354b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:44 +0000

    less send that turn avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
