ead1f00efeae298ae03574cfe16690fe41c6cc8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:13 +0000

    perhaps burn our pencil gas

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
