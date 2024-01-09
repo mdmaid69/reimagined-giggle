364a6cf6047880ffcf04f70be44b50474010020d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:50 +0000

    example present eat record warning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
