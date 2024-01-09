bf0d8bd4dcaed8b30a1383082f4286d40c3a21a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:04:10 +0000

    contain love unable pull mess

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
