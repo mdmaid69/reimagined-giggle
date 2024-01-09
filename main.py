3d9b83469f88baad19372063f94304a538afa5c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:56 +0000

    pin intend bound near plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
