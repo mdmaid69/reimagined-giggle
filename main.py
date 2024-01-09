5c90c029d44f2e7ca25b44920a02acf2ae00141b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:58 +0000

    activity stiff stupid shut set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
