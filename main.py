e31820eb3307cc07d239a2201e0bdc41ece2ee2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:51:32 +0000

    win weak thorough cold crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
