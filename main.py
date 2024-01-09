44c01c7b8d607f5453886110e98618da8fe8b5a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:57 +0000

    undo definite experience who thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
