cd3e77c8d1cae0b38d368203602c92f75009b96d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:02 +0000

    trap fun brother boil invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
