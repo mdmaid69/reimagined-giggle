b8825f6befff4242d1faa1f8a66aa87c798c865b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:54 +0000

    recommend solve scare cheap sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def add_numbers(x, y):
+        return x + y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
