6f2e680b9cbe3b7e32046b2cdd19aa8c5346a8aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:33 +0000

    equal ready tune date cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
