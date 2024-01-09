bd16b86922ca618ffa59f9bc0dcbbfbceb9c7709
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:40 +0000

    reject preparation hold beneath undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def convert_to_hex(n):
+        return hex(n)
