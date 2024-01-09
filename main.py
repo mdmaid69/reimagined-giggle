cc5a5057d5756bf303e7852717ab921ec1296a4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:07 +0000

    road pool laugh straight surprising

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
