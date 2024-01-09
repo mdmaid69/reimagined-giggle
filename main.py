cad7c59891402c848cccd1514778d9c0e34d7e48
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:47 +0000

    Oo taste learn perfect introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
