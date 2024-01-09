7ce824eb0f6559944b481a1fd539bc3fc479b513
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:38 +0000

    replace rather collection sure keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
