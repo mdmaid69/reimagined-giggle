1623eb099c0ff2e2271315b6eb592798595a3449
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:42 +0000

    mail emphasize side teach Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
