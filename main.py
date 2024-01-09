28ed64dda405a9794a7513c30b76e7eb7dc781d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:36 +0000

    minority down welcome rain maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
