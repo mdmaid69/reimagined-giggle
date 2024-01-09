21585ebdfbf5599c91204a2a9653c27f992b0bda
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:54 +0000

    must beneath somebody head congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
