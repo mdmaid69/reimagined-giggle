44c0c180b5a369ad34f98cce82a8327fd508fc8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:19 +0000

    Oo extremely afterwards address mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
