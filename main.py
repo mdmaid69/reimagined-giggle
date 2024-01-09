149fcb0883f5703e4d4664933114b1c6d22bdf32
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:32 +0000

    each birth do interview sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
