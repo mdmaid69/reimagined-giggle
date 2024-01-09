a1ea527e29f4c61543a0c184d2de1b4388df5baf
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:21 +0000

    argument little piece many stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
