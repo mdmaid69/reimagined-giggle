b3a077eb1fbb75ec8bcf4ad79a071f717b080cfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:13 +0000

    deep pool at translate use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def reverse_array(array):
+        array.reverse()
