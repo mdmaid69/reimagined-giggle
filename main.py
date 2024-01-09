e6547995c1be9cad72e543035f3981bb2fcd522b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:47 +0000

    week other deliberate fast force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
