d7ef515aec7af8568c64d6095fe46131e8b495ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:08 +0000

    flavour mix near several introduce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
