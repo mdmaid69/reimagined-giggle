9b91a4715bb35d8ca9d4c8c51f08cd7a3c73e70b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:24:01 +0000

    warn dare flat of chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+for i in range(5):
+        print(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
