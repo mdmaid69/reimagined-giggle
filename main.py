7f1264d6ec085cd5735a4707533b4d7579640189
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:59 +0000

    beginning scream 567enemy possibility fit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
