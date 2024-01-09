3580f608a4df798fc8d00f3ad8c160472ed2cba5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:55 +0000

    during skin zero example edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
