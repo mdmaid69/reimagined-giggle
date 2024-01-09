871f203bd0383e17858e1bbda98359643c054e20
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:29 +0000

    tidy convincing talk communicate unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
