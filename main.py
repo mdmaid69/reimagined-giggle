bfc70f45e5523d9982adc4307f074081d52ff1de
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:58 +0000

    priority unkind read deep story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
