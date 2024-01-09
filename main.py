0a84cb0e860deeacf4037e1f4216bae501815a59
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:09 +0000

    crisis female always comein creature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
