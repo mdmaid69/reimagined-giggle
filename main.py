f5ad780d0f7113762b7bb3f05bbbb1ed0c4c06d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:05 +0000

    communicate thinkof wrong authority partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
