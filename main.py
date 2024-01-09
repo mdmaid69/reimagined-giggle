6e078259606184b676cdbcf192fc908913efa686
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:16 +0000

    club friend pour train worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
