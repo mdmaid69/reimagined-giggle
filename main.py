965d198ad8a83e8e14046b214a72d0d2ad06e383
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:58 +0000

    inside sting long curious forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
