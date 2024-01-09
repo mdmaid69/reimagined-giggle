a8a331e0a557b77b1774798859d93cfddc953d62
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:35 +0000

    slightly offer clean I solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
