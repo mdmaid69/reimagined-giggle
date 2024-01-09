ac9e869428106d1327906d8bb0725831173180dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:32 +0000

    nurse breakdown unless keen Zz

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
