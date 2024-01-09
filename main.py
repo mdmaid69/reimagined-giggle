60a2fc324c73e940897264abb70d6b2354291a30
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:11 +0000

    mend agreement touch basis too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
