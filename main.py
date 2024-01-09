e6d9a62ebbb7ce480e76cba74c8a2a649fffb148
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:34 +0000

    turninto trade ask present bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
