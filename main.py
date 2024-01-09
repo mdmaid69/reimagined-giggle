d8e2713c4207ead67e028a000439bb1a02761938
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:27 +0000

    funny independent tradition speed in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
