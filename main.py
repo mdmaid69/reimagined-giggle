cb846ca52ddd87a096f799d5395e64079d529982
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:08 +0000

    earn slip exist so serve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
