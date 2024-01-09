53961d8f717efed4e4abb25c7960872c5ca5c2f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:32 +0000

    or finally report stir foot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
