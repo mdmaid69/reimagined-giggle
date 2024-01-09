baaf510ac3e12554bb5ab22b80bc73275944f7d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:09 +0000

    nearly Mm develop correct sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
