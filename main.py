defa62ed60b877b131d05804bc6a5f926c117285
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:00 +0000

    raise mind holdup worth last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
