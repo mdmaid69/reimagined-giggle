ffd993453d7717137d064ad21bffb316658c8d09
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:55 +0000

    calmly advanced light moon takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
