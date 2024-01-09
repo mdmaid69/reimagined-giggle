8f68572893368a46385cb10506d64904559101c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:08:26 +0000

    so scared police match later

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_list(lst):
+        return lst[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
