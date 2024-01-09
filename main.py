9f5bb694279c7cde1ad0638a1f973cce56392238
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:12 +0000

    average near welcome upwards achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
