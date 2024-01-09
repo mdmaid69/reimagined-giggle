e9207385bb18f0dcbb4b6e415174e0b4958cd07f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:23 +0000

    outside rest turn shake deliberately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
