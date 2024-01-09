686268020b7d263b03864a2f2231cff2b05f6212
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:54 +0000

    carry middle ignore treat invite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
