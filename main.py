272c74b3aa53076696070b2e6a00aa05243c04f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:34 +0000

    gooff getout add he worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
