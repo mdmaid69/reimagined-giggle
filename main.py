b13cf975db223f0bc6cbdd6e8d652c936bca85d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:56:44 +0000

    separate copy healthy out bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
