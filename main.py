dceea9146b97429ba7dce94f6c0c74eac1085499
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:43 +0000

    still rarely search pressure branch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
