f36ba3fc4df88839275103defb6b5df0d91c401b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:04 +0000

    behave earn office cheap Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
