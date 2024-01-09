532a8d6249384f64371e843b36eae03516ab379b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:54:44 +0000

    example whistle speak Hh ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
