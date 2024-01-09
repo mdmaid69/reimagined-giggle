95be1e8ada2dce915ea10590033d4135ca0543c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:13 +0000

    pot welcome curious entry capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
