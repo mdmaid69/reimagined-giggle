a16427b2c701be73190a8ea49bb903bda6c86a16
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:30 +0000

    allow doubt takeaway point young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_item(array, i):
+        return array[i]
