a9afa8a3eda6b9dfcfdd90d0beec6a8137eefc55
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:08 +0000

    remain stand kill perfect want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
