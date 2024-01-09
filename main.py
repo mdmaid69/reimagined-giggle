da479915d36cef7d8eb4bac6d907a7effee32995
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:26 +0000

    rough but exit deep receive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
