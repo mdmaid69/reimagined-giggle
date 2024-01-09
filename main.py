3eed12745f924a5ab5126350d114f5c253db05cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:39 +0000

    tap anyone public harm variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
