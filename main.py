95e62ba296839a3af46376809e40d3984a2f97f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:05 +0000

    against air model switch heat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
