6492bd7ad04d14329869dbe30ffb9848f94b4a32
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:30 +0000

    satisfactory comeround or attract confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
