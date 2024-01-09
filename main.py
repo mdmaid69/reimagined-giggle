fae259b7c73dcf1eed026a3e7373b5d420869c20
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:58 +0000

    practise frighten health wash kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_length(array):
+        return len(array)
