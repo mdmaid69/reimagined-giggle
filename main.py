6451d46330f317ffa188fd770e06cca6432a323a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:50 +0000

    nervous talkoutof divide bath change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
