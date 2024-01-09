27f60ae86d8f67166cbf709c52aa023db0d0bb27
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:30 +0000

    climb direct breakdown deal sit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
