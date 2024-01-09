9c8533ca91b93d0ceec877255f938aff30da002e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:27 +0000

    takeoff progress now school sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def is_even(n):
+        return n % 2 == 0
