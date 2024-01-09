f7dc0b61374dac0147f7244d86b435bd94807ef3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:11 +0000

    lookup copy entry turnover persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
