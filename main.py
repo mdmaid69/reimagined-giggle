99f63b28e0967a1e38738fc2de543a4fd51322cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:04 +0000

    share strike patience 566end completely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
