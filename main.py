dc5eb9b060d82a903a6b8c470f23183c58b644c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:07 +0000

    workon harm murder kick seat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
