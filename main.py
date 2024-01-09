8051959e66eeb86530c867ef833405b43be73f44
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:47 +0000

    though mixture car 567enemy fence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_queue():
+        return collections.deque()
