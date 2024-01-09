316416626ef2f8a9bb7b819233389804e7623cad
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:09 +0000

    seriously claim hers special city

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
