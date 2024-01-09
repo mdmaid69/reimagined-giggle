1117a5d437d5b468fd5567bed83489e968c6c879
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:50 +0000

    cook settle hope spring centimetre

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_max(lst):
+        return max(lst)
