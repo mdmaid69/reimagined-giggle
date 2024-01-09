e76778f345561ec2d6b47c7e5c82fb79afa7f5f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:37 +0000

    crash grade unpleasant lonely arrangement

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
