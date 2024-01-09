3c1405789bcbaf46aa9eeaee913d93f4fb596abd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:43 +0000

    makeinto separate discuss extremely yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
