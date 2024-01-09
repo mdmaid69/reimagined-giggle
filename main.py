c05430e830c9f22737ed74c13e862c4e1ea0628e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:28:43 +0000

    sister foot attach justice hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
