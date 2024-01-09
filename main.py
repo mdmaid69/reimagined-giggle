a20aa4e49148f8fefe4441450475ae54cecddcf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:34 +0000

    experience tidy blind bunch hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
