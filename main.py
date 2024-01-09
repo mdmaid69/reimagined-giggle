c1af2fe11742abd3591bc21f59451ad19c630261
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:04 +0000

    bag control swimming film plate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
