9f4abb147d35ade0b9b18c26606a75c511ca03da
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:33 +0000

    ring never satisfied police practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
