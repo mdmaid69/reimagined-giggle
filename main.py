ca176b4b020b54faa780a9f452899f72244f1726
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:47 +0000

    breakfast survive valuable match minute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
