c766d05125ae876991670972343fa1616db862c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:12 +0000

    least up afraid gift fond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+def sort_list(lst):
+        return sorted(lst)
