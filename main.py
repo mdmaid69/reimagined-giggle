b221e51573be398bef41679fbe28dc96bcd11381
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:19:25 +0000

    plain defeat glass wash spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
+x = 10
+y = 20
+print("Sum:", x + y)
