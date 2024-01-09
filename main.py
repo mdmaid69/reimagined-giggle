ee3c7d73b6919ce13785fba4c3fa634953cf9909
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:54 +0000

    storm guess arm famous artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
