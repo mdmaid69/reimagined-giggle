e7371330b045d6963c7a46bf3862110ded052291
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:30 +0000

    appear grand well remark nature

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
