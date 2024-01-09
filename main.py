14d6f0a3cd3f9127e18250a93c38af088410840e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:37 +0000

    car development air central comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
