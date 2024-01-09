1f839824dac1500c09e2d0ab3103d6dffc30ca85
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:04 +0000

    race belief danger probably follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
