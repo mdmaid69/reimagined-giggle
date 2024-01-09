012ad8bd7c454b06a753af7b5db333d856d462c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:52 +0000

    react sea shop ticket compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
