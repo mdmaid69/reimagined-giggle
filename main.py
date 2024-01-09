ab27be074d5b71aa797bfc57b0e98a994aaf5735
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:51 +0000

    justice image mean charge cross

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
