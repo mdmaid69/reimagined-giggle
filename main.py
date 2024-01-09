2b38d0870336e7b22ba28fac748ea15c891ca9a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:16 +0000

    busy bright undo knock off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
