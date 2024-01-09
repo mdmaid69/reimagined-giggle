6b79b885c85f096deaebe7e9caad82ec9c55bf99
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:32 +0000

    steady comein both post case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def get_array_typecode(array):
+        return array.typecode
