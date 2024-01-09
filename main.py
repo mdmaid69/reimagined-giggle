92ddb99c6097822c34429e25ced8010bac0ebb8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:21 +0000

    remark somewhere colour getout mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
