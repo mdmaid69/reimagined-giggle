1066f0b04b6359a0be0121eee345558cb3abe970
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:57 +0000

    excuse criticize on comeround steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
