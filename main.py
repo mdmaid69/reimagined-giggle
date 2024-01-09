625978f692403337f2e7370ce90c1e91ddfc0a05
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:37 +0000

    back relative describe she when

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
