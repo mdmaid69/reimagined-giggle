c0e0f14e65759a119392b092540fc1e941f7f36c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:26 +0000

    afford upset fairly every goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
