06dd77b874947d642205e4ccd25658f630a58c58
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:52 +0000

    walk gentle moment easy pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
