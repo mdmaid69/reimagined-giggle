7b6a631bc31fd85cdac1b2097492d22ceb085a28
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:08:10 +0000

    slice prepare order meeting fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
