46e1a1887e8e1854a7216d21ed6437701f1f3c66
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:52:01 +0000

    late but ought breakup book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
