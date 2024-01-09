12f11e33cc0850da3b755e46d5bfaf8136f2929d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:26 +0000

    common crime strange environment freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
