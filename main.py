272c9dd5ca14780e8d74ee793b5683d9a9c29030
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:12 +0000

    because constantly knock church hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
