3b76ac51ed9e0090a6b1064c8332a4ea5d0555d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:52 +0000

    mouth murder OKorokay disapprove edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-print(sys.version)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
