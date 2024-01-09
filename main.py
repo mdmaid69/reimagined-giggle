e0b6bf659f3aea8191343c0390e974ad67cbd564
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:01 +0000

    pass part meet relief cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
