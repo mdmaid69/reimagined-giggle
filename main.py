b3e1509183eb3259ed2c62bf7bf5f291430a59e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:18 +0000

    bus remember cover empty game

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
