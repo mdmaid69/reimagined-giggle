dddc3015d7acb7e6fd9efeda282d54cf7c924e0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:10 +0000

    mine flood concerning practise bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
