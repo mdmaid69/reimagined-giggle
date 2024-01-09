5fc0dd419d5de53a1332df93fb393f189af78095
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:09 +0000

    punishment defeat press quantity finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
