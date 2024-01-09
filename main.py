3d59e2fc1a54360fa7d7919b21280618dd70bd41
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:19 +0000

    hope pattern greeting short worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
