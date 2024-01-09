26241118828c2c3623863100f415108c704c57a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:18:21 +0000

    morning age eye leg immediate

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
