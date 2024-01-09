0fb5d7c70ba752da139246032a6c205fe77a99e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:38 +0000

    watch again each content talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
