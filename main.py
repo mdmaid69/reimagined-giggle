1a9ecf8a2ec07a4cd27ad761d496654a7f71252c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:40:34 +0000

    week touch breath peaceful news

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
