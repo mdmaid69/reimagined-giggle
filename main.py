08a8176fe5b7793afdfd0049a5a41852c38396f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:20 +0000

    holdon boring lose unlucky rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
