8a81b11901719fa2fa6a7899ebf1f920d7df62df
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:52 +0000

    decision lead getoff under concerning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
