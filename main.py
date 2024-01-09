23f7e89d5755a8c9af56711a9306147f54e6c3e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:32 +0000

    smell border familiar near memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
