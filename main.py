a91de0228225273f85103b90d6dc9cce1a4528d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:59 +0000

    embarrassing hungry large around average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
