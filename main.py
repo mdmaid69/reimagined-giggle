4dffa850ffaafbba9212d903d103031498e232c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:38 +0000

    success achieve name pour second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
