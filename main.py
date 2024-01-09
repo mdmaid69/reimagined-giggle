ae9f43b14f5b4e238c7cf7a7816d7d4cf91d196e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:22 +0000

    long task tonight shape worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_words(sentence):
+        return len(sentence.split())
