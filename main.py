e240cc064d5ff18359653b202c5b23238eb6410b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:09 +0000

    appearance right married pain cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
