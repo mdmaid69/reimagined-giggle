cbbbf6131b66781659118ca4a257b1386ebb444c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:36 +0000

    empty now Ss air summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
