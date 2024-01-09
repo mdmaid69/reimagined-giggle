1dd133f5b00a6e8de147ba26e20c4d00144a9c52
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:34 +0000

    command shadow summer condition apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
