e7ffb2d174bf2f143a8d1572f43b8f9d042b7984
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:51 +0000

    television use thin their her

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
