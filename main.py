1d6ed88b576693332b7fff56f5487ff192c24f77
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:12:59 +0000

    just result goout act comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
