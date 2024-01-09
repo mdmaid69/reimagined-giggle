2293566fcc8dcd07674fbcabd65a65263f24a954
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:19 +0000

    shoulder congratulations traffic fly keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
