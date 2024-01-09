5ca82680c09f40df9f5eac207967fe5d1f81dbe3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:01 +0000

    reason exciting badly its pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
