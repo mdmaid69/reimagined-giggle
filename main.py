b7114146718a69966046ec9ecafe14c48f92b61c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:57 +0000

    tune please free straight accidental

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
