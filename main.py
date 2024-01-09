abec530f37d87c8121e4cedcc5e280000100ddc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:54 +0000

    refuse age goodbye schedule Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
