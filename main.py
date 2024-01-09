5a0ff3dafba70a1d6bd07be1fe22411f7b6103c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:08 +0000

    although apologizeapologise sale culture tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-name = "Python"
-print("Hello,", name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
