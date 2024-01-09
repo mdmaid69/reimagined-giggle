10d4a3913a334f8b91e99ff0671494486512ebf2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:04:44 +0000

    past square even learn best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
