c8ac2d5e39f4420a5996ff4a8472a83a3a1f76f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:29 +0000

    satisfied unhappy road consult centre

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
