5ba6d718327975068e19675ec902af360d452e1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:01 +0000

    recover lookup understand she squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
