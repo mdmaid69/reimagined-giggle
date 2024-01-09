cf6c744fadca2a4154da1519c250e739532c34cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:37:40 +0000

    cut offer birthday divide in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
