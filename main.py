29cb188afe9502d130de3b9b84c36c63b8b35dcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:35 +0000

    accidental slice cold sale season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
