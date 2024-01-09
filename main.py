2d0eb40d32145e8e5af534d148428185d866660b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:31 +0000

    important press extraordinary unit thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
