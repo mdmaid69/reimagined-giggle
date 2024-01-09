4dadb041f34dd751c5a4269ce76dfeda46c737fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:52 +0000

    chief onto either slowly great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def count_characters(sentence):
+        return len(sentence)
+from collections import Counter
+print(Counter("hello world"))
