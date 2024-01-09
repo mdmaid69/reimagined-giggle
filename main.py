ef97f89623d9a65767b1acbd342a3cd1872832f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:38 +0000

    escape through Itself fork hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
