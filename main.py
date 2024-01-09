dd0362790f140f007f7c162f4c5007a6f3fc2b5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:30 +0000

    confidence judge repeat side themselves

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
