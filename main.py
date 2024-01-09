bd6bfc41d704d8261990d100b9df69d123b475c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:37 +0000

    cost reduce care base decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
