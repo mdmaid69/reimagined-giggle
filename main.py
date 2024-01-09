07c80075cfbed613b20bd7599985d80e21c3ca89
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:16 +0000

    spill invite desperate view rarely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
