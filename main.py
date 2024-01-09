ab26ccf5cc97d2891edd8093105560de8de8a89d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:36:59 +0000

    accept attempt remember comfortable crowd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
