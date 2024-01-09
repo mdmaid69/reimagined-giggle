35d6f923cfe75f29ed5b070a4b12aa9b02e1abc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:54:12 +0000

    load cheque side appeal plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
