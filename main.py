9b7956030cc64c843c10b63ef203e946c9661591
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:16 +0000

    partner save burn even listen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
