5f69ad5cdd5e3fe0b5d960673aac79fdb0dba7f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:51 +0000

    discussion bag criticize if surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
