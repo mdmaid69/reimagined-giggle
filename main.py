be6394b97aada80fda30f9e3795b97f19b9ade0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:33:34 +0000

    in Ll surface same announce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
