82e31ad1b20f7876b58e7e5238fdd2f95ac2848b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:47:57 +0000

    new moment crime strike bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
