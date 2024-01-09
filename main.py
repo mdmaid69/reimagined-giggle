7146d5df3bb24b6f5eebee146b16e63c7bd924dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:21:00 +0000

    extra expensive lock unlucky general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
