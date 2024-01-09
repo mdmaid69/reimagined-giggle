36be9c68ee2309120eec91a24bc8e3e4a71ccb68
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:41:36 +0000

    around often demand powerful hurt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
