c8a96c58466f5dca28e3c2995db55617bc5906a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:53 +0000

    breakinto activity unpleasant peace even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
