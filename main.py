7d0c686c3c70cc2386cef9276b59ee389ba90660
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:13 +0000

    achieve transparent approve callat breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
