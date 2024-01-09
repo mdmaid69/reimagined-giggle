2c91a6774d828cc7270d2632697649fcf200be5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:25 +0000

    favourite surprised around I introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
