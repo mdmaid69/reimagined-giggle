3f547e0868489252271b0f3b4682e2453b33b40f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:05 +0000

    about protection sure explain soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+def convert_to_hex(n):
+        return hex(n)
