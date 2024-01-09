06468339c50df30d14cbf427b961b737630f0ac5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:24 +0000

    deal listen balance backwards exercise

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
