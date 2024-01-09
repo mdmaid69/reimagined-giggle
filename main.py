ce7fa8b3ce880527e01d5ec30dcdd7274189feef
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:45 +0000

    demonstrate possession recovery train interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
