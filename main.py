402824915d5f6ee3877d7427e7b8033f97b9530b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:29 +0000

    crisis match statement impossible thinkthrough

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
