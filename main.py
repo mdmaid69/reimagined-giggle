38d76f4874c1237d74f03844294a96709bffea76
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:30 +0000

    consider able object wipe immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
