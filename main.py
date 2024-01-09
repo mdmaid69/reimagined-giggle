2a127c62abcc6ed0149ea1175a9b9eb1fd5bc165
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:10 +0000

    age satisfactory drop people well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
