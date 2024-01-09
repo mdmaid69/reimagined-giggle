963be02db7adcaca0c97691bdbb4d331a43cae7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:04:04 +0000

    can rest knock turnback sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
