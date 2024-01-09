f2d9ea97e89ee89bfc6dfc5ab014fe4fc2efae22
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:12 +0000

    drop pronunciation clean principle bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
