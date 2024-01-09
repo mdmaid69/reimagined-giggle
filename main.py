ec937eae664b417761a2d5d99ac7ed23c989a1ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:28 +0000

    term none rate early clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
