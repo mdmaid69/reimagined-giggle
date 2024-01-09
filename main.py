be0adaa5202d3b48ff5ff5d9beffc0a980dc695e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:05:21 +0000

    minority rest surface hurry collect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
