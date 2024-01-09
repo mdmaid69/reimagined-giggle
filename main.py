7967849d7854491305b650b100ea18d25810cb47
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:16 +0000

    keepoff sky call stay since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def divide_numbers(x, y):
+        return x / y
