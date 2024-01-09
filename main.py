9399ab2cecbfc6a50ae9951ec39699de921d0b96
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:38 +0000

    belief gather upstairs blow instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
