db8a75ea4f2c37f9391a0b800dc0459409bf6eb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:01 +0000

    memory hair both belief fork

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
