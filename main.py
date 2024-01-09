38373eb4c3d935466c6826af4dd48df50ba5e64d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:59 +0000

    children accept join period mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
