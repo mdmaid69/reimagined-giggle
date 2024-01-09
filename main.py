498f82946660124066983458349ca393d05c62ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:18 +0000

    exam straight catch memory community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
