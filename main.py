bd3b4eef65a9282324917f794a9bb4f052203ad2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:45 +0000

    stop no anything could suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
