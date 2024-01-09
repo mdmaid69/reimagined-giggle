d11c1f88a9c5c677e22132af2e2f6903a5461224
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:31 +0000

    while pause report thinkthrough offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
