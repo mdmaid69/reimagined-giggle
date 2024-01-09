dc9dab4eea66f6e9dde0e90e9fe24cfa82cc5471
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:08 +0000

    correct sound commit name invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
