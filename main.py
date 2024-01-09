35f65d3a188a1be6c1336492c157c9b1822cbd63
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:45 +0000

    snow loud flow range impressed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
