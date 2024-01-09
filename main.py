1c0dccf8a718a96b68173b35d41092496ab2e496
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:16 +0000

    otherwise intelligent measure breakthrough acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
