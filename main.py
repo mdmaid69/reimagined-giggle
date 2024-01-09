edf974f64a0416a45ce511ea0d12b6db8e0a8432
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:58:59 +0000

    bin drop band false calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_density(mass, volume):
+        return mass / volume
