33421cb806da1fe072bae80f4d3b95435764868c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:14 +0000

    mean in most physical meet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def find_max(numbers):
-        return max(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
