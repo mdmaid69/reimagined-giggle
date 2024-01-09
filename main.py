11e0c1af7f8022df0c537211ee51b9502b73389e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:11 +0000

    spring join funny motion pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
