053e0ab2de541929c9b5de921fe05ff2082be371
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:14 +0000

    send possession list ball fortunately

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
