b8c834167317d686fe773c3de2a5498be36ddb55
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:44 +0000

    appeal opposite watch weak seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
