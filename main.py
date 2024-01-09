cafee78a73b597969f9ce1790a8ccd01e37ca295
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:53 +0000

    note keepaway thick some course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
