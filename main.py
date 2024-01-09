3dc5f7f6f7dcb602b21a669645524125e5008867
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:42 +0000

    apply teach impressed terrible fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
