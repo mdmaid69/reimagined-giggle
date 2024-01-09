1866ce92b77b71dd10841fc313d6a97f90aa0df3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:16 +0000

    goodbye exact complete here warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
