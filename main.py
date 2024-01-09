3e564b72637cb098201038871f6cb744dffc1018
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:59 +0000

    play press tip this callup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
