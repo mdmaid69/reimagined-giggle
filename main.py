abb73e8b352f3ff0be8201b8357d3eede69655b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:18 +0000

    mine exploration brain hair considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
