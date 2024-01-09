8d356b52e5a82210953bdbb4c5735b817ae0f256
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:07 +0000

    dance pride swing share bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
