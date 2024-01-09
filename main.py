b8cdb5940cdc166b3a7606699d3b84b43bec08eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:56 +0000

    powerful direct save water test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
