e18d5042465cfa2cec7adc25d59f1ab5d2425908
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:30 +0000

    bank cheap home previous breakup

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
