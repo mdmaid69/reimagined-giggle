e209cf35ff01261421a8e823bae314d044453f57
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:31 +0000

    platform brown illness plastic create

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
