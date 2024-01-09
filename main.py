f08a844c4d24820e6bf64766f90f9c2dae74cdff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:54 +0000

    beginning take fear patience exploration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
