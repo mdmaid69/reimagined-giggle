211961418772044f509ec4717c07a553919dfbd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:24 +0000

    breakout print remain man insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
