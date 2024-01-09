78b6847432ea86a1f6f9b70f9739198778495f87
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:04 +0000

    morning him town uniform block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
