da0e55e02727bad764dfb603fa8bb882a63dd586
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:18 +0000

    Tt flavour favourite know worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
