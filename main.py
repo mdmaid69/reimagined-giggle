704d8f152471b4a68306683e2f94ca0033eb906f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:39 +0000

    survive doctor crowded production frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
