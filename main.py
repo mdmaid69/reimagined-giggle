22dfc3b12f22fcc5fd3e104779d95ca76246f11a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:10 +0000

    separate address right study set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def count_characters(sentence):
+        return len(sentence)
