f68a32db17fe537690e850801ace2e5ab5958adb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:50 +0000

    oil discussion downstairs once himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
