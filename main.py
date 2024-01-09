bcf84aa6d1b2d146b8fb51153f6e4486bcac2890
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:52 +0000

    rate zero support ring surprising

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
