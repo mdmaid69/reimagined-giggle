63ca7fb23d92f794426f70bdf67e3a4a5667b6b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:20:59 +0000

    shallow fork charge exactly straight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
