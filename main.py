42df43d771deca4a0db3dc2d89fb69be3eaeb420
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:44 +0000

    praise show handle compare smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
