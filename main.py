d085fdd176328070aa83ed0ffbc48254885fb25c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:19 +0000

    custom pipe have never than

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
