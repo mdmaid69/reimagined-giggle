7d7ddcb97cf07ab5163a0d1b37192cdabfc6ecfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:56 +0000

    sore mother edge concerned tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
