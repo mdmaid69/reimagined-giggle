051cfdbd9cec04b1013647c05ce3bae296e79c18
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:52 +0000

    image convincing afford long recent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
