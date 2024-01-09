0e64540f42ef300976b2638451a80317dd163041
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:41 +0000

    throughout impressive back operate strange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
