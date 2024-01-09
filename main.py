a37c9a03f63c8c098dd341325ac6328ef8c47e79
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:09 +0000

    rate possible sort by concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
