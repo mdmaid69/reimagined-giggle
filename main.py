eb798849c1b1586559432f881527cbb555496f2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:36 +0000

    tense within contain shallow information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
