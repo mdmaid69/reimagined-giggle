8f0f9e9566058f2bacda29621e4d876de7510cbb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:03 +0000

    accidentally naturally force past project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
