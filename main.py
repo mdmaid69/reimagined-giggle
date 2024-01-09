8ae80f4c0bbedd7e8e8b01f7e9ad6124c38ebd86
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:22 +0000

    keepout negative normal direct tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
