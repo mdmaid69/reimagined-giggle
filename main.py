5e10447dc8200b61e93fe9c818181b3eb817f5af
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:39 +0000

    kill generous curious love in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
