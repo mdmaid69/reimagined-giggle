3615591a30216e7b03ad3043d33756290cb8622c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:04 +0000

    whole simple vote flat search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
