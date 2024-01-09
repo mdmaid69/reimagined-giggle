4c7a962a72accc73b3c88b3ee2bfbbebc8ec796f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:47 +0000

    quit carry but takeout century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bool(array):
+        return bool(array)
