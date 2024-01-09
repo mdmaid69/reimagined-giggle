5144698410a23beedec66d0e052298efc7bdecf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:51:38 +0000

    cum mother criticize secret original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def remove_duplicates(lst):
+        return list(set(lst))
