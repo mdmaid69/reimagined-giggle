5385cc70233fe2fdbd384fe27ce559df28b1ee95
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:05 +0000

    direct goback(to) block hot respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
