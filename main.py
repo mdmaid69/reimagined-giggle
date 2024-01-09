8330448ee66e1a580facde7fc6d13f2264e35f88
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:20 +0000

    continue average equipment argument formal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
