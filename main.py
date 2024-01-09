1012bc07a8ee5482839f87f578530c6e060298f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:06:23 +0000

    win pretty 569enjoy without undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
