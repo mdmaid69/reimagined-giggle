57fd938fb34cda7ab7ba5f7d4a57203c0e866dbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:26 +0000

    up house powerful chin laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_min(lst):
-        return min(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
