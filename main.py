742f21246b3fd43704d28cbc1f6d8d7034f97bce
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:38 +0000

    step zero mistake declare project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def count_elements(lst):
-        return len(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_max(numbers):
+        return max(numbers)
