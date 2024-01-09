52de2a0ba11f6b397a7cf7774e94f79278831a90
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:03 +0000

    clever dress total plain race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
