4c624acb6426d0942d7160a3fe8ab414cc5aaca8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:14 +0000

    Ee only service pocket I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
