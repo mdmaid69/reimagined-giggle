45b5e504d4486a1f5ac8bcd535bf040d5c85ec16
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:47 +0000

    cover shop guide miss knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_area_circle(r):
+        return 3.14 * r**2
