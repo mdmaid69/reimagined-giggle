49729807e4473651671ac4ed40578522cd51f05f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:03 +0000

    only sort intend hard avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
