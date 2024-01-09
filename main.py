67f16553f800b8567c32aea841877a2802baff23
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:21:20 +0000

    confusing apply roof provide pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
