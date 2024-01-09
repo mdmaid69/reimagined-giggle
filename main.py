5668d0cad1f6051e85935db29b6dec09425cbaf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:22 +0000

    talkback sea noise maximum along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
