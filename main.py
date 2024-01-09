0f192cab29f24fdebb0a1b7b4f30f5368c84aecf
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:56 +0000

    behind smooth little movement intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
