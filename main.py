5ca5f1ae81ec9b96319736f868ad373592727602
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:35 +0000

    age pronounce drink store catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
