4864ae895676c4b2a24d542d1b0ec69a8f4e05d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:54 +0000

    Hh since rush tool bother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
