5b93aa6afeeff494e6e7c3aa00851f09734f5797
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:50 +0000

    house something clever protect pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
