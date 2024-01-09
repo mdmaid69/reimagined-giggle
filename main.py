c28f36066702f66d9f5baa83befc81ce16260b46
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:11:10 +0000

    news intend drink wide yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
