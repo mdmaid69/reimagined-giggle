7d369e2e5fd014f129916960778b51745a4f66bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:35 +0000

    gentle Gg estimate receive space

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
