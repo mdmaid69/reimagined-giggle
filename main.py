d941bcb3b96555cf84428ed777274d7cd4b0ad07
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:35:31 +0000

    certain majority ignore punish comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
