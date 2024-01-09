eb494a3e279241945f2dd6579f544133a2f4de36
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:32 +0000

    callup sail run point dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
