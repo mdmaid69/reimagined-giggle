ce0a9e8e7e690eed17ca7c0640d22a4ef8ec868c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:41 +0000

    complain equal turnback could sensitive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_list(lst):
+        return sorted(lst)
