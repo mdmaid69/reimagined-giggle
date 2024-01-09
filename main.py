71e6493f0e08719abc0222f8e200e0e3fe77768d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:27:23 +0000

    Oo delicate system protection teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def add_numbers(a, b):
+        return a + b
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
