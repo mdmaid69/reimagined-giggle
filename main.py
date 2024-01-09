fe2a60521d2f644a46bcca3949ec90d1f38f2f07
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:17 +0000

    shock I wave fat cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
