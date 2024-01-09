3bd14dce8f1cdee4ce5113f950ef3fd68863fad5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:39 +0000

    disappointed apply top fire improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
