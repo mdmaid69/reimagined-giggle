b1fd95599d27e5b2534dd3c7baf47b2f39bb82dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:23 +0000

    related lie bank remove company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
