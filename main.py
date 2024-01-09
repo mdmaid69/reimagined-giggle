29e0e4b16bf0410dda5b8311b83c77f925a20c34
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:33 +0000

    release walk tie choose takeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
