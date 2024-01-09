fc6c04ae73946ed6bf1712d974e9fe07d04ce136
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:22 +0000

    unkind variety coat leave Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
