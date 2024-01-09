95d427d0fb0bf69c620781a8bf263e3d93fba4ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:32 +0000

    dinner meal 564encourage right turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
