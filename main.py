f93e07c4d9249ba02f0fa6b0ae34d14e9f0c007c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:45 +0000

    steam total promise otherwise up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
