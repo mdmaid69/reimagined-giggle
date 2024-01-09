083344a7561e33e7180db9798d0bff69b0233809
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:23 +0000

    must mainly both connected and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
