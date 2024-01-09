108fa6f2784c7cb692b81ac532cf76bb2a89ef3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:40:12 +0000

    consist bear rate nervous likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
