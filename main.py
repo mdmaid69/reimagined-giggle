b3c791f52f1f341a3f0dd5e5804eff328a69f626
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:05 +0000

    slip information frighten let on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_binary(n):
-        return bin(n)
+import math
+print(math.pi)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
