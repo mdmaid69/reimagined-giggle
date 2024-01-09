4344745661fb7aa8372a0856b00f30d879fa3f01
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:10 +0000

    well line remark capital act

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
