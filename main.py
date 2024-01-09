3fcfa494a721ce476fc0871f8ecbe90a7c40ce6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:56 +0000

    steer basically picture convincing please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
