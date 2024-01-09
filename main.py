8be33c4f002db929c1473dea8656fadaec646d97
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:01 +0000

    capital top plant empty room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
