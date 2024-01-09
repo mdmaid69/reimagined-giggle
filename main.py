2f32173975b4a14e622436b826f00e0424117927
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:35 +0000

    fire wild high crowd invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
