20271f9e5e560e46d81bef29b6615b73750286bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:15 +0000

    future guess strange most boat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
