a2bc70031e922fd62906d78c62cd37ee1f6960ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:30:57 +0000

    avoid giveaway dance miss bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
