8b0e3ca275656f89719f2244234c483bd44ebde4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:45 +0000

    responsible below fond comment used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
