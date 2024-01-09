69b359f708b1a7cc1df7e5743cb1353cc225f00c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:29 +0000

    terrible regional takeback hardly recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
