ebe1d9e148b006d024632677239e065cc7723d66
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:50 +0000

    such tired go all temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
