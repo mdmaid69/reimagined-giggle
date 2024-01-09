ad6c1d7b0fcc7fac44c91ee9a0bc4d6b70aab46f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:03 +0000

    float everything success rubbish last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
