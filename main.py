2ae26d3dbe5b7fb39f0abbcc401f11f5a7e3dae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:25 +0000

    wave criticize level price study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
