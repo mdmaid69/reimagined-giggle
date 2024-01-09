576cb3e9835232719f398b51b90bfa2d53438bde
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:03 +0000

    wash quiet explain fault crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
