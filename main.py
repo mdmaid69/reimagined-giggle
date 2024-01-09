8d52f06f2048febc1282628e3b71b8fd24698c25
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:32:43 +0000

    bag thought divide face fold

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
