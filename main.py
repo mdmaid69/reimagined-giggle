f08e0fd7e0b42aa5c0331603a45e8bcbcafc2cf6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:37 +0000

    excuse spot tell cut sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
