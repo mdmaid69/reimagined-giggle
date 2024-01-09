81b91731d45e181266a9592c0cb016d77cc1d550
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:26 +0000

    regional even other keepup ashamed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def reverse_list(lst):
+        return lst[::-1]
