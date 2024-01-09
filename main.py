012da58f1316f908920f71bdca0452f95ab5fb60
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:56:39 +0000

    possession concerning gift stretch take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
