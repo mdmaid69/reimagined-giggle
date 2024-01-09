6380cd7c3b595baebc8049dd4813b1d7de021dcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:33 +0000

    it blind swimming completely exact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  def convert_to_octal(n):
-        return oct(n)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
