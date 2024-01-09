0251657ccb2f959d24dd1b1e84aaeb472864a245
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:29 +0000

    plant really mystery welcome medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
