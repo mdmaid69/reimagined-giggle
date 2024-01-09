721867745252901ba7d9e591f48534a56ae16166
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:58:14 +0000

    you club mention hear obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
