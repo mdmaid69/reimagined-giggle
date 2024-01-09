657cd55c50642619274a587851d0be4855fe3399
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:07:49 +0000

    breathe nothing mail apparently relative

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
