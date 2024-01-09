b4b0427ff21edc42e7ba2392cc3222a765af51c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:20 +0000

    level occasion themselves history action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
