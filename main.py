39eceb8694d0d979af449cc299ed6b80f4689a73
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:02 +0000

    late back pin hold switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
