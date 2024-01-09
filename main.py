f55586e219596996469201b80884dbc512e1c6bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:16 +0000

    roof trust band steer interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
