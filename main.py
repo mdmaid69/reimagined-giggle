cdcea64e320bd50424dc5bc4510af6424637f5c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:59 +0000

    cover state single miss Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_list(array):
+        return list(array)
