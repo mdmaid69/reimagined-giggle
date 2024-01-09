4c0802f2db8d92173ad05cf453d5402619f26c61
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:21 +0000

    platform underneath convinced continue match

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
