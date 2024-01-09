adfaa1ddb7dd21253cd1359104e67429d88e2787
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:44 +0000

    quickly switch large crime uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
