3ca732a6ba57d75fd9d573610237d069d1aa65f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:07 +0000

    innocent unite official gap himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
