14c03e3d21771be18023c1e44b2b902e0176f9f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:39 +0000

    better expression lead this unusual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
