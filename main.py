cdc63d21dae893a7f00bf2b0ecfe93dd88bb401e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:01 +0000

    birth position exam beyond hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
