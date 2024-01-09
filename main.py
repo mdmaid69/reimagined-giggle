cf68703cf2a2fdc1f8ec30a94d8af19989690f69
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:52 +0000

    while please simple poor even

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
