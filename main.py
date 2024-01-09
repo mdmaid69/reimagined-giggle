3189329d1fe7b4ba05bee191936025596393ac8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:50:46 +0000

    lucky introduction treat light compare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
