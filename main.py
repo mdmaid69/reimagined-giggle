9705709c827af0a569258de6a04fd5dca0f0791e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:43:30 +0000

    neither bar reduce embarrassing reduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
