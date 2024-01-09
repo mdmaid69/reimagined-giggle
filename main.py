abd223340166895c9f5aaa6e27e47926ab63cf7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:05 +0000

    factory survive taste factory poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
