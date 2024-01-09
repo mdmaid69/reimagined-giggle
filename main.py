bb784bbbe9c88d3c7becb65ea867347e7835bde9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:30 +0000

    rent school food sail square

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
