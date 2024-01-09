c59cf25e779f044e48909e90dda1e3804f59805f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:08 +0000

    on ilovedicks cruelty basic talkback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
