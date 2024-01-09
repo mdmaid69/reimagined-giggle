dd2fdfd72de4166ed94979a6a13d9b9ef02e7f76
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:39 +0000

    ticket Nn neither comeround fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
