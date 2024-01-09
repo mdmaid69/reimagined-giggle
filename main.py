ad327dbf3b3dcc0e6db84baad902e79d905a6a14
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:11 +0000

    learn count bear rare consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
