e82c09a84a805d8119588c135fd474e4d4919bf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:14:28 +0000

    care whistle neither angry slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
