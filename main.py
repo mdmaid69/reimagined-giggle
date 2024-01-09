e60cf497bf7e55f017b340a870f183c40c8fb721
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:14 +0000

    count lookafter please little concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sine(x):
+        return math.sin(x)
