7969bdb36c19da05b933722f9a38e3d7762f0fd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:02 +0000

    towards open achievement confuse whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
