781fc41c207c9e7bf6e0fdffece5eb5595752dd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:36 +0000

    later period fun lock pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
