26f4b76247e1eedd2c65d7a3bbcdd9bf05368b89
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:46 +0000

    judge plenty show announce make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
