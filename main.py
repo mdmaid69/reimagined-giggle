c866e5058d3f015c08b0fdf269d51eb51db0f154
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:52 +0000

    my occur duty service stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
 def calculate_work(force, distance):
         return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
