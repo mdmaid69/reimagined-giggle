3cbd334399552ed7ea9f26cfcc2b72ace90dca4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:36:44 +0000

    at space getout its sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
