17fdb3a28b2879f90fe6afcc30997c9eff311986
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:19 +0000

    respect deep beautiful happen contents

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
