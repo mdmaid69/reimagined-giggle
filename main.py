bef379f7aefa717500d547174c8d77a1c79f6869
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:30 +0000

    tonight declaration activity change church

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
