17c1a39214b9513788c27f1c18588a725441d70d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:35 +0000

    prize this leg straight whichever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def reverse_string(s):
+        return s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
