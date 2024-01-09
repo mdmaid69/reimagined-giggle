ba7fa4f6ad54ad1acb70b0be03e7c953e93a11ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:57 +0000

    under talkback offend operate climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+print("Common elements:", set(list1) & set(list2))
