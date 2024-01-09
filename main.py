21a727c084e6b78091778b4a7bec31ad096dfd1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:24 +0000

    tax light point pay century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
