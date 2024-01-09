ea0b067a9be37b4966ae4c528a61678725c29c81
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:13 +0000

    himself snow through Uu soft

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
