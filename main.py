da05613c4c47d22e9a8d13ff6deaec062910a721
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:26 +0000

    project ill correction tread wire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
