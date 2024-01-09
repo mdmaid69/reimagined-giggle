eebe297d0cbeefc1178c71f0e98f499aaadee587
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:55 +0000

    seat minor black line shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
