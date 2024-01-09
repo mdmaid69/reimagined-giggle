7ee67b38106ae561f12fb82af5af631d22dd6b1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:41 +0000

    fault inside damage satisfactory sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
