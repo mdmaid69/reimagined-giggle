68f67dbd8a289be92d48d2118fac99a3daec93e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:17 +0000

    mouth funny pretty soon keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
