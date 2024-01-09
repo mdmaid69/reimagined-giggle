a5e629640bb6e4ce1d3112dfc0c3b682e00edd36
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:46:32 +0000

    discovery holdup 571enough at fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
