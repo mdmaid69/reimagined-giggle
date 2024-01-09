a7f2654997ab27483c842e74f2a16df1860759bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:02 +0000

    sick suck protect stick rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
