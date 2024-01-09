17a0a7c249c0d2ee8f179957935615a140ecb13a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:24 +0000

    slightly sound much avoid principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import random
+print(random.randint(0, 100))
