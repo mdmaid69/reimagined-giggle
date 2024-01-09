365ecba927013e08433ee039ee552cbf65def25f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:34 +0000

    loss chief hurry until 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
