6fd955386c4920b07d157c0c38ff0c8eadd5c7e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:24 +0000

    science card remember draw unfair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
