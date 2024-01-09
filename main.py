3aca429540d29692b7d7e61313cebb7150a85d28
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:05 +0000

    level general opposite equipment slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
