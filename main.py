6df7bbc0866057fc2c8b72e7dff1cb9f2fe229a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:47 +0000

    giveaway ask total somehow sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
