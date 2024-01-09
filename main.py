1644af0d7cb4bf90eeba4849df9743612ffac344
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:27 +0000

    complete often attempt they punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
