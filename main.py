548d1bafe4039b9d88a5797df57d02b04238cc6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:22 +0000

    anybody translate honest stick drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
