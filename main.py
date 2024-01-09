1f5be824574a1f78429501f040de09cee33da290
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:55 +0000

    transparent nigga bar equal neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def exit_program():
+        sys.exit()
