2b3e5416bc87a0a86ca8a96340dce75ebd6e28bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:16 +0000

    cough church bother male bin

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
