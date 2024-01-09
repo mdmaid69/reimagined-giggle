1ea0635752dc5be7d3c8ed6330eebd0f12042557
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:29 +0000

    operate succeed permission hold narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
