3c79bdf402d5aa64b3a56d3dca1eddaaa85947f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:14 +0000

    tread upwards adjective car ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
