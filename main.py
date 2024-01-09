7bfed2145e10cf670b6d12ad33c4e02986e744b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:03 +0000

    glass organize where hurry pupil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Uppercase:", text.upper())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
