d9a482d34ae1e96b8b78e70331f24aa6bbeb2c2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:17 +0000

    often comfort tap believe task

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
