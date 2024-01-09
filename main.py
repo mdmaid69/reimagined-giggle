cb7ec1b4312b5387e3b693a03d4567b1549e8289
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:30 +0000

    breathe skill else increase behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
