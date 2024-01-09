6ed713335eb36790d9b155cf67a154462b025b5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:19:58 +0000

    exact present shoot trip indeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
