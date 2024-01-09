c047d3b055ee951eeb66b9456fecc348ffc5257e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:01 +0000

    complain trip near welcome acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
