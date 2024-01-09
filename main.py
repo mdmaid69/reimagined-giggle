3d02b260af42fdcf4c71819fd124449fd4ca3d77
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:37 +0000

    mistake before excuse school rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
