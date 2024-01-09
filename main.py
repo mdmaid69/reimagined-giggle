3c7d2e384d951e933399a0bbf067252bf575fefc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:23 +0000

    hers sink this breakthrough spoil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_volume(length, width, height):
-        return length * width * height
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
