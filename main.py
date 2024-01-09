4091dfbfd3020e23d7a13cc8b90b20913e3bbd2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:44:47 +0000

    565end apologizeapologise provide course badly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
