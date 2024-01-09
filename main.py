8ad67e77c147e3269a146ecfa1df4b2b87de46c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:45:01 +0000

    translate bear final dance especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
