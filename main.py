b6d15d806e59a527fedf7d8de4e9f79f3236c939
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:34 +0000

    sight refer large mend hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
