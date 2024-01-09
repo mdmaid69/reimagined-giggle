a37e8339581e05532476f0e9d236a6b888120a5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:52 +0000

    cheek safety noisy complete turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
