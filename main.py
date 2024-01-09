32bb5a5283edad86ebbc38dfe2f130291d5338db
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:41 +0000

    awake table basic fact twist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
