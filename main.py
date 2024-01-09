f6c9e85645822f06a175aa6b5113d7c63558b5c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:41 +0000

    Nn climb stir within stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
