740f95eaaef0b8a57124e660daf26f8bb71ee036
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:35 +0000

    check everybody high course relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
