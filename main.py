27413a678053056916dfe7869d237206e27f839a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:55 +0000

    prefer concern boil loan record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def add_numbers(x, y):
+        return x + y
