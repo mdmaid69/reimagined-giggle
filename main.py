6a7170f1b585417e87df976054e813b167cb210b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:58 +0000

    record takeoff among pen duty

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
