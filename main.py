28874f4c7874dd505aa706759a9a358b78e327f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:25 +0000

    prize call smoke unlikely suffer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
