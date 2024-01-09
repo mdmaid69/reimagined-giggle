738f91df1ce293adbaeb39a368e2a89f88675dec
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:50 +0000

    along moon hardly go flat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
