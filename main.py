9f33086d3650278d19f6369a03cdb5af89a761f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:36 +0000

    rush careless guard cut autumn

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
