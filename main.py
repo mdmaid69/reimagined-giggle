c749bb3470c6a8272fca16edd0a00cfa07f137d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:23 +0000

    join whistle corner trade effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def clear_array(array):
-        array *= 0
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
