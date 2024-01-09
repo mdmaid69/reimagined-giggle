6cdf0feb072fa4f9344766ac2e7c4fd9ec2c8b7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:12 +0000

    address though kill stupid hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
