155251452e4b9b17244282ff421487c5d78e7c99
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:09 +0000

    close apparently occur actually disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
