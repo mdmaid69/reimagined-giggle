c0eb5a954601f8a94850d355689403144c8e0680
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:29 +0000

    leave minor exciting and their

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
