9e7bd42ab087cced657fad22ee9f27ac980e4350
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:01:11 +0000

    bread acceptable emotion prize already

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
