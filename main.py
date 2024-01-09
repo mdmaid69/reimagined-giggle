dac448018d14540991fe3efe1b16f33a129e7517
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:18 +0000

    judge open ready expect collect

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
