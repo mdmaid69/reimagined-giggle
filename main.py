626d90b3cd958518a269f16e7e97cc975ec7098e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:17 +0000

    exchange card ticket nature lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
