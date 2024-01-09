c9aee517837f9fe524aee2c7f68e2c07a03fbe4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:56 +0000

    hang frequent point purpose treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
