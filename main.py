2959ee30c76b31c774ca47efe1ecd20ec07bcc73
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:24:44 +0000

    tight keepaway record strong join

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_unique_words(sentence):
+        return set(sentence.split())
