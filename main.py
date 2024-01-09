ac31ddb2c796cab07b4fb2264189f267b0d820bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:40 +0000

    good community violence authority geton

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
