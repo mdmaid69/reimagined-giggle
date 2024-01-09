0166eaafbcf75e60f0a0783eb1913efa361765c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:46 +0000

    charge scare join woman lost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
