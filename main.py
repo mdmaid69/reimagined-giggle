14a90ccc2b9018b23a63aa38254bd45f9e668864
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:44:13 +0000

    profit confused strict final tall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-print(sum(range(10)))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
