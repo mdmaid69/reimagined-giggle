2f85b136d1068a8f3667be4d1763be17e9ed7cbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:50 +0000

    letter exaggerate birth term extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
