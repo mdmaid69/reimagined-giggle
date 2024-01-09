1ad813cb4ad2b556c76846bfd98cb26bc30afe3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:25 +0000

    fortunate flow intend away instruction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
+  def square_number(x):
+        return x**2
