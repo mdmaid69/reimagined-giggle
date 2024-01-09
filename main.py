03e1533835d62026d72f6a196f0c5f305d4f6ca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:07 +0000

    water get long expect film

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def is_odd(n):
+        return n % 2 != 0
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
