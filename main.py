16dd00424b5a83795a60cbdf63df68f3290bbb39
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:52 +0000

    fashion moral swallow club none

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_area_rectangle(l, w):
+        return l * w
