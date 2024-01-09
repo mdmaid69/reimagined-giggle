882da816d431c657be8f1939bc8395c046f72ad9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:27 +0000

    around peace between otherwise danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
