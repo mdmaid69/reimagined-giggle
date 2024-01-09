a4f1cb862dbd1fca5d462cb58fbc64abb3291ecf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:16:07 +0000

    here shame event succeed comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
