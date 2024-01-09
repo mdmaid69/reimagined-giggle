150f685a04520e87ca6b9a16ca7b0c3d5b9b0367
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:25 +0000

    earn difficult order earth family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
