5e47afcd15086bf1f3c88c4190858f5e9b93aeca
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:03 +0000

    disagree shape prefer confusing promise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
