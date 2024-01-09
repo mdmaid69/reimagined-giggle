0d451dac3db2171eb6c4ab55d0fc6a7bd1505b3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:07:31 +0000

    anywayanyhow sensible complicated regard sure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
