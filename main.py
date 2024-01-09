1106019c00698a9319537c2ed5837ced3fdb9546
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:25 +0000

    match not lock refuse several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(numbers):
+        return max(numbers)
+def calculate_pressure(force, area):
+        return force / area
