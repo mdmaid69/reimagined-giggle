233c36d66e7a08976a33498b6d8eb5b79ea538d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:39 +0000

    war sometimes compete eager birthday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
