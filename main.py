d4c4ebf00feac12a0316aabbe664861696081aef
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:04 +0000

    hear unite question eye guide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
