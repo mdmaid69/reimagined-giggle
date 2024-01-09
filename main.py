ea21d2b586efefcc9b23b3a086d8154a4c5113b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:45 +0000

    speech great survive drawing fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
