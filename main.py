34fe513f604d6416c7827fc0c8a3c3644f9235e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:49 +0000

    fight effort clearly shock valuable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+print(sys.version)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
