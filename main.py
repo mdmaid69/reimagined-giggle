c86b9f91bc423e376fdfddc4b1b836763f95f205
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:24 +0000

    fear urgent length brush tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
