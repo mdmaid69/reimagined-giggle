3fe146ce17c07b77697d7b78e7935de2a8ef318a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:56 +0000

    visit quick left broken typical

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-for i in range(5):
-        print(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
