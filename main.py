5377e343f70fbb6d765b4890e16979756dd408ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:54 +0000

    middle bank weather powerful Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+for i in range(10): print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
