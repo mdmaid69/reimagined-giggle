485666b46e8035520ff405fccf82dc1b8682bfa8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:47:16 +0000

    evening silent heart inquire score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
