25bc34ba6c168b658c80705aa310dd2028ae0d67
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:27 +0000

    trial sail worth throw provide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
