3d0ef5b4c065dd7f84ef38105669f39358c9566a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:40 +0000

    deliberately software factory hold scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_length(array):
+        return len(array)
