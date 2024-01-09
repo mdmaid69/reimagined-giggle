dffa0a5bd749b6438b2cc59f884e219fd29d4a86
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:28 +0000

    comeover sell sound takeaway drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
