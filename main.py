c4b29b43c9ea1d6bf2685d870cc5fb8f7a42e2c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:13 +0000

    persuade manage poison importance slip

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+x = 10
+y = 20
+print("Sum:", x + y)
