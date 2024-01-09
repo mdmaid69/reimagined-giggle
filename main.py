cf40464c66e740d71666bc5879682e43f10dc709
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:03 +0000

    create gap offence whole appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
