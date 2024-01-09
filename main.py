23e994b3b3c353b29effceb8af2100cf736085ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:24 +0000

    themselves argue fetch combination ride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
