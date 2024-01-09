304f9a09f529d0e99eef91af8ec133d6956aefac
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:07 +0000

    needle entry continue disturb skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
