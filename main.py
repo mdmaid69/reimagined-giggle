10ad34e9cedd2a309879ef7448e6462814e97808
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:38 +0000

    unpleasant soil regard sensitive beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
