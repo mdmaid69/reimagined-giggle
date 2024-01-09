5f53b0c87e04a4ae05e0b9045a3e2eadf97d49e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:32 +0000

    sign loose tax comeoff open

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+for i in range(10): print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
