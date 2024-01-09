4b3748e3251a8c0598de78d5939b90e2ad8e2a30
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:32 +0000

    sensible poison wide freeze contrast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def convert_to_hex(n):
+        return hex(n)
