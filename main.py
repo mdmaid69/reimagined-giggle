6e21c44315b9ab4cae04ae3b074ddf1c9cbd7e1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:11 +0000

    future free deep lower drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
