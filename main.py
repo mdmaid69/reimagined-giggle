dd806ce26a234132e08fad8fc95407afe6f7e54a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:11 +0000

    instruction react trip agreement 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
