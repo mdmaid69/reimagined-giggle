14a334c912852e7c21f21b5bc19858e1c250f3da
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:59 +0000

    rich goback(to) smile rule best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
