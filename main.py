51b04b84371cf7417a86aea01126d09ec440e6b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:37 +0000

    print moment pleasure task acceptable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
