662cd91fba5865562ad96225aeb30a7b5d666094
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:05 +0000

    say kind exactly crisis thing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_area_circle(r):
+        return 3.14 * r**2
