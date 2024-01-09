c5e9fcae8fc313fd287671d78e0f6d3640528c28
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:34 +0000

    safe question sea estimate lonely

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
