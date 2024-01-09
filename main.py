85cd01ff025952dce694ce8a239dbd1c1ea0bb18
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:33 +0000

    achieve comedown transport slight lead

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
