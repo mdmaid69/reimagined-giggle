5142ec24ab80a5fa77816515c395e5fb66d1b6aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:27 +0000

    advise treatment comeover company taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_octal(n):
+        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
