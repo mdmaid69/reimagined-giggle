4f523917a8e489f831274c04c2ce24e1d98fd20a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:39 +0000

    press line ear division draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
