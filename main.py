26999c8a32a612f92cb7bd31f12726175ee017c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:41 +0000

    approve cry best how priority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
