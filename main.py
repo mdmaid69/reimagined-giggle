f8948c982b21699fb5241bd4d8dd7a1b049f5fb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:36 +0000

    convincing freeze somehow safety climb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
