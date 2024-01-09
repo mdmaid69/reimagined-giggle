36e30e3dcb7ddc4b9881805cd3250bc0becb5950
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:41 +0000

    pretty surprising test very naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
