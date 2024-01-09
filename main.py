8a421bc6f49fab556e67b5d78211b5fc130f1dcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:38 +0000

    plan cough receive imagine arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
