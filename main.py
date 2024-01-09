537cc5722ec8090ff26edc16d0e8e1a06e1aa5bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:15:01 +0000

    firm callat emphasize equal building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
