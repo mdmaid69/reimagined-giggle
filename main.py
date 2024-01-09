a38da28b892ae83df60de22788efd948e20af527
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:03 +0000

    undo speak importance mention cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
