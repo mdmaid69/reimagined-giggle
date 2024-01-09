af0bc576bb7695e93c86580e4669e7ef69dabe72
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:04 +0000

    arrange its no fat this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
