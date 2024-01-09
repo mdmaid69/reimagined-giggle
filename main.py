32b77687e846499e3db1bb7911fc520d0a1040ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:37 +0000

    project one badly seat figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
