2a13c9ab7646d723de8562ef0f30c454f6def467
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:22 +0000

    fold behind workout base performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
