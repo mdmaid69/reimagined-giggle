e9c3fc7a3cfbbaa85bac927795f49609c180efd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:27 +0000

    sing chief never convenient confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
