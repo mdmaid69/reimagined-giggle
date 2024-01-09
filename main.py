b68b397bb74581a213635f3d0edaa564a10fc097
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:05:19 +0000

    term fear off sink fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print(sum(range(10)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
