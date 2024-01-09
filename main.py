1f8e6894f582e7211ead1f2143af9bbe2b9cd513
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:58 +0000

    emphasis unfortunate available myself sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
