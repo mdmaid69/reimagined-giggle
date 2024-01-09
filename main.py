84191f7ee8c425296762099e9109313da3da5798
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:17 +0000

    Uu room smell themselves empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
