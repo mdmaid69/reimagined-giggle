5797dfab00eabf973fc044249397412f53f72754
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:28 +0000

    everywhere justice heat behind slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
