95c4ac78382a1b4349009de8f38c9441597dacd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:54 +0000

    question air imagine opportunity shock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
