e8213bbb3fbc6a207044b9878b010531e4bd6db4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:14 +0000

    Vv cheque pass concerning total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
