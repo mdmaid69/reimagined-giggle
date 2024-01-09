70ccbb2e0fbdab3016d86c72c149a126bf55ec91
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:40 +0000

    estimate safe bringforward game shoot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
