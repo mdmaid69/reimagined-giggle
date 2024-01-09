8d639e27ead19226a9cbba205a513cb6591d08be
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:11:15 +0000

    since train debt plant splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
