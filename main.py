41335131533f2ce7d37be9a6597efd0cdfef0b40
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:27 +0000

    aim word especially community effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_acceleration(speed, time):
+        return speed / time
