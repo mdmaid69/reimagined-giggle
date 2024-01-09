cba74eba917aa805049c945a1f861edb4f68dc3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:00 +0000

    book past live advice onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
