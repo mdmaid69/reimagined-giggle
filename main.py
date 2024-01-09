548c2f88c39a19e54a3bd57380c0852ae9a0bf2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:44 +0000

    science sort possible battery night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
