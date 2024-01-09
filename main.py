dde3d2b1cde1e8db958f2421d0e985641011e86f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:51 +0000

    close otherwise wheel getup play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
