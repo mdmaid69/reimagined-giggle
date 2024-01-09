e5ccff24e9b26e4f7ef8094e5c36e4b76fb29527
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:34 +0000

    thing track lean peaceful post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
