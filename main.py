f05c3e29226d24dd2296d0f968f9e9e53f4febe3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:26 +0000

    pull over afternoon present pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
