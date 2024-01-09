e6886a1020e2bfc20e4904cdbb6686115dc5c3f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:17 +0000

    system lot wipe direct harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
