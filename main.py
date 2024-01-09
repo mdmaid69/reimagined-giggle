911a678082f335801456892c2f60c426cf3bf246
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:51:57 +0000

    dance path nowhere exchange thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
