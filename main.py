965ee69f67692cffa7e70f7067c823746dbc4f60
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:16 +0000

    forever situation quarter fly recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
