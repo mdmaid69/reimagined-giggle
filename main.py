4707e29a4dd22a3aadd55d716e27193c61bf8ce3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:44 +0000

    consult season pleasant tip hang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
