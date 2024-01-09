46500511446d4f827f6c48c8d93d08c780ee7bfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:30 +0000

    voice wave much she find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
