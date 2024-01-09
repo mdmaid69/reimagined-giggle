6ab3225a1b0ac7840ce4ea8e14eb11ea779458a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:05 +0000

    people slip happen height command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
