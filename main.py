7fcfbed1ceb77f576b3cb418d201a8c42e4e76e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:43 +0000

    block laugh place hurt roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
