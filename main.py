7cfded2835f316b55030ed13a33621a72186fb84
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:10 +0000

    Ii convincing confusing war personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
