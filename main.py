670f11589df7de1dfb31a175a2cd555a237029a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:53 +0000

    disturb rate let squeeze near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def check_if_array_contains_item(array, item):
+        return item in array
