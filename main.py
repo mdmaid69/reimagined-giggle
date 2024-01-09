9fb0e9f76c5186924ccf658cfaab789fd932f654
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:04 +0000

    care over send disappointed exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
