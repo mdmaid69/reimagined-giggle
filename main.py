c1354085c5f17af60edd855a40bbc6f8d0dd2fa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:50 +0000

    themselves cum pure once swing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
