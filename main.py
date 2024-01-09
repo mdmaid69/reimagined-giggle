0c6de595bb22a7e5374eb06fc42c9b0a9a4aefab
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:11 +0000

    afraid comeover feed plan on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
