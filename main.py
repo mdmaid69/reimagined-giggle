107c4d6920a13a9825439812957c47eea12e0b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:27:23 +0000

    centimetre offence responsibility convincing noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
