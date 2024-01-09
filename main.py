cc51108871390a332df3e51456b65e653529be03
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:13 +0000

    project sink equal excellent quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
