3490f0b392d0e2f6069fe82dfedf49b3f6d07ae3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:38 +0000

    right except hand plan inquire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
