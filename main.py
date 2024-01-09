4e87f5fe0ab821bedc22b9dfe81b21b2ba70a018
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:46:23 +0000

    interested peace ground hour rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_power(work, time):
+        return work / time
