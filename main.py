b4976e3a635d6add03d1de008d19ee4f80c17ceb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:53 +0000

    say shame spread eager development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
