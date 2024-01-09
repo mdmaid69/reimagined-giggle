7d8e29547f3b543dd90eb5c4fe397b66f0234c78
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:42 +0000

    party precise mark you any

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import time
+def get_current_time():
+        return time.time()
