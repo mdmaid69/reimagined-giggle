705e7c601cf9939835b78943a1f2af7ff6a3cc80
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:12 +0000

    after row except point immediate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
