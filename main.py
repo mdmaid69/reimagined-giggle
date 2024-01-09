7dc045a8c832e5e1327677237e30289b85ce42d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:13 +0000

    comfortable bring accidental shoulder beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
