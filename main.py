d5b139cae6b27ab63e5401acf43de3c82ac413e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:43 +0000

    broken in exchange action brief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
