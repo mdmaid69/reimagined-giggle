4dfc480d73ceb9a51df190580fb3a95cb8dfe9a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:14 +0000

    remain butter such breakup appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
