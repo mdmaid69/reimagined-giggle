4b19c9cac6c93af486c08ffee854894f76b64551
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:17 +0000

    pin bank she sink I

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
