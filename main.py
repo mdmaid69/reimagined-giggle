6dcde1181c0effa0787ad8b55c037ee59e95f5dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:32 +0000

    what Nn her underneath near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
