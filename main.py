8d68d04f4d74e9118358f7e802bd8f18f0a9d0b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:00 +0000

    read pile violence forever sort

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(numbers):
+        return min(numbers)
