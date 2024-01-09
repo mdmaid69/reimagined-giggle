3acaa7218fb2d0d1981e69edf492f66f9d15f61b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:10 +0000

    invite rush him means suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
