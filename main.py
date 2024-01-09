fe519a995b9a37db0c1587a5272fbad9cd732e40
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:59 +0000

    mark worst quarter have beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_queue():
-        return collections.deque()
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
