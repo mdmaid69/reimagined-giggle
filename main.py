d14a448207c497e301592ea2148c8c920815cc77
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:34 +0000

    Yy branch race turnoff belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_bytes(array):
+        return bytes(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
