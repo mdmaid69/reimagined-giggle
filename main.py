d233ee2d48458c74cc7bcf6385dab31c69fb0610
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:16:13 +0000

    trap simple discover human first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
