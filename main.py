701a30497680a4a06e53ee068a28ef188549277e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:07 +0000

    mysterious clue translate pattern scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
