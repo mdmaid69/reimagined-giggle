70ca5614b57fce3d0b2bca5b606e86dff3f6c959
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:02:43 +0000

    valuable complain active base something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
+  def sort_list(lst):
+        return sorted(lst)
