ed14efdfc0992a6913d1203c4f4f591d7ff1bf6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:53 +0000

    music see discussion proper pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
