1fa7084b4846e22dd249076d53dd98e4fc54a36b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:23 +0000

    ring happen active exit approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
