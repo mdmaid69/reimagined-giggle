d175df615e5b3058f170c3dedc1d2fe53d267a8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:55 +0000

    quickly politics spot concentration radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_work(force, distance):
-        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
