f680e3b3f50d98c18431ebe08a3a2c323cdbd07d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:01 +0000

    date chance reliable dear walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
