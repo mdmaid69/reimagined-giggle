d8c3ec61caf65a07730f73a7cf473d2aa21b7e93
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:47 +0000

    learn responsibility turnback proper address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_characters(sentence):
-        return len(sentence)
+def subtract_numbers(x, y):
+        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
