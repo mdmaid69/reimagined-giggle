1d79a0c16afb6cb338cc8ea9f9003fff71726441
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:11 +0000

    mistake impression punishment lookback(on) kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+for i in range(5):
+        print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
