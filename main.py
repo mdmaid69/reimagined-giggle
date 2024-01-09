89e5d0c0d80a5592aae2cb104d0a61b16ba0c888
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:26 +0000

    edge power bag earth mistake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
