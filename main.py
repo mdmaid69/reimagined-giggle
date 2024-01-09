0375b745924ab5fc33db4430582a773a006ae60e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:19 +0000

    later past special lend final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
