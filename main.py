33d2f8d9f52dfda5055c5f8313ee435791bb119b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:05 +0000

    bin feeling 568energy phone something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
