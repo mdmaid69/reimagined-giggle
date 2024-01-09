2da24bca61f8a6cfc29cc299d3cc434f168515c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:45 +0000

    telephone accordingto you holdon low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
