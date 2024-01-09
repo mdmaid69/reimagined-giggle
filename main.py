e2e15771767b2035983d16efb198a8e324321d8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:30 +0000

    middle first Cc minority play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
