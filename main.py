f05bae81fff9356e8de28e09f1ba03d9112cf31e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:57 +0000

    clear separate tool fly behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
