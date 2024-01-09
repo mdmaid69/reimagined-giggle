16bccbe583dc758d55dcec7ba55fa2fe2d876f06
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:44 +0000

    quit through hole roll brown

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
