73defdac3d3f2ad1cda3be9fa10632d919ddbe52
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:21:07 +0000

    information close empty hurt bring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+text = "Hello, world!"
+print("Words:", len(text.split()))
