991963c4df8b23f567d8b84cef54e87ed5849355
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:32 +0000

    example deaf blame situation behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
