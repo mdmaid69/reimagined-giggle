45beb5aae7dbfdf74c94af67cbd1afe24d9db8f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:21:37 +0000

    switch chat nerve regard reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import platform
+def get_python_version():
+        return platform.python_version()
