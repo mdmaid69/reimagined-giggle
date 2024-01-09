6d109944112ac9b0409256818843fab5b6d500e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:42 +0000

    later holdout could shine hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
