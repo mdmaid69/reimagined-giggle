90cee37f445dcb5176584d0af4c617675ae3a0ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:23 +0000

    strict exaggerate either workon clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
