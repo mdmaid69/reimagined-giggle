935130bae7d1f0d0d543932e4b19ed36e6e8cda2
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:30 +0000

    bringback many convenient rarely information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
