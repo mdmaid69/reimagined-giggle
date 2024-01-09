1d661ad87334b57b495f159a31132f266dcfa162
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:51 +0000

    season frightened down shadow explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
