56e098a88b9a4a2338e62285d31817a0e6a83fc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:35 +0000

    crowded goup short by slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
