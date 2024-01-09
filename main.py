4d652dab941ce29e2c25f7d0fd9d420862cc0ed4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:25 +0000

    accept material hardly bringforward edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
