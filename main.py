0f4f0bcf6431b3486d5d5cc5ffe67e5191ba4e11
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:11 +0000

    and ear meet burst part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
