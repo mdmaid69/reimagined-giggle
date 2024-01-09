75214a47ca59e8a9d19dffcd21f5419e8b0380c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:43 +0000

    punish desperate sport full share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
