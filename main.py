2e40740c6e3273b404d4339bb21cbe71091d03c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:26 +0000

    protest term come usual accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
