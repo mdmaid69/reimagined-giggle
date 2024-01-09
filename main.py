d2a70c8d9ee72b6930f94aff40240ad0313713cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:49 +0000

    bringforward through otherwise makeup Ff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
