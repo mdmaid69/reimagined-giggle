77cbcbfbf24c0208690de81a6b0b5d2c9bc28367
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:25 +0000

    chin touch challenge promise protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
