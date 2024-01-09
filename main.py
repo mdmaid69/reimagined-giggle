81c7bf927c2dc831f4a5b86d6eb1a53303bf55c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:26 +0000

    judge correct it reject let

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
