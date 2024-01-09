3a299b876a9f6b515b655378a56ef9fc043d4f1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:02 +0000

    tend their help spare today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_density(mass, volume):
+        return mass / volume
