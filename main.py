888bb4932f9fa83dd00199957e1d98cb8b14e4bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:21 +0000

    method ever cruel forward pleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_user_string():
+        return collections.UserString()
+for i in range(10): print(i)
