bb91c8da859b6076252616260cd3cf0c65be6c20
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:37:06 +0000

    typical alone empty appeal like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
