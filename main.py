5b0314f8633a1d5d3a34e9e060a2e7dfc489c3e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:53 +0000

    fault remark leave clean forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
