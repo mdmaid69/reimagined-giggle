4660557932ba5a275da72e3c0191eee62dcdfa01
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:31 +0000

    fence wheel talk previous slightly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
