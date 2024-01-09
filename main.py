cfe2493b9cbc85654822c7d4128c1af7dd88d3ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:34 +0000

    false turnoff war inform eat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_min(numbers):
+        return min(numbers)
+def sort_list(lst):
+        return sorted(lst)
