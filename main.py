7962938ac1c7125cadeca96356b88456e4ae8344
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:23 +0000

    far she opponent few bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
