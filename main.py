a5ab5619c48324db8fe6aa0a993b3f05ba679555
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:56 +0000

    supply blame yours second act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
