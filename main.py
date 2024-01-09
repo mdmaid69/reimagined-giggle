1b7e1c2ce13a5498b237d695cfd97ce0ae25226f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:37 +0000

    somehow first drink doubt manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
