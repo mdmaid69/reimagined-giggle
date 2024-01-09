e3bee6657a7846224071ec02773b8498cbad2104
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:31 +0000

    kind themselves calloff basically except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
