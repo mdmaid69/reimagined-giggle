495de6602cfbc3df90f6690a2f059e382105acc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:56 +0000

    doctor figure much cheek she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
