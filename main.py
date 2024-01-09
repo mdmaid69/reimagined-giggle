aaeb6795854a6b394b57ebf5372a462fcd074532
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:25 +0000

    interest create birth runaway show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(sum(range(10)))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
