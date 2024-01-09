1604164631ce394842b3eebb9acd70f539c1cc4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:39 +0000

    lookback(on) exchange likely forget transport

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
