cc42d979fcc42b41f621216fa2971842dde58cb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:13 +0000

    opponent call weekend taste careless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_average(lst):
+        return sum(lst) / len(lst)
