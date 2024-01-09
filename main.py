d0fa70794652de64487ef725019910dd284d912d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:29 +0000

    entry past dry lean centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
