a5dd2f2c2408b294afb65dfee35e86d569a2a522
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:05 +0000

    school my reaction turnover guess

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
