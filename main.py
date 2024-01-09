57bf38ca8ef9c086521a0830dc21fc748e24693c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:39 +0000

    move bush classroom plenty pretend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def cube_number(x):
+        return x**3
