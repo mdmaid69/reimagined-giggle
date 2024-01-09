9b572b0b550a5929eb03e67dfa9bf1249e6320bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:00 +0000

    tomorrow please enthusiastic cheek suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
