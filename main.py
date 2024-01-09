64343429b146f015b4e4a84dd3f5e9fdf9b37629
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:30 +0000

    surprise direction gentle save one

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def clear_array(array):
+        array *= 0
