b78905b39c21ab0530ca62ca595c0a6d8934852d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:13 +0000

    takeout read alone mood carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
