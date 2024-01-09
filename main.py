f2bbfb09a81e15fe2f4bbb3bc68d234aebab5a64
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:41 +0000

    problem crazy performance check whether

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
