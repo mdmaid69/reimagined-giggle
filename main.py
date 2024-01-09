69d95eab4fa2f2a86f4420e3c8d0d1a62a8733ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:15 +0000

    will partner anyone advantage regular

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
