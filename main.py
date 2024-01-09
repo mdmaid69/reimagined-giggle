c89e780ec549bd56a92dcd76c61c8696f6110058
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:34 +0000

    harm flat advanced frequent content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
