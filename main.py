4799d715c2825f2e2079b1a47a46cf1bd9895623
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:23 +0000

    by goback(to) hide play protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
