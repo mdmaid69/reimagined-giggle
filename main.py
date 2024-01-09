6c50f11be9f81e9224914d79e7cd84dae47fb45a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:39 +0000

    on pour in dinner autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+import array
+def reverse_array(array):
+        array.reverse()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
