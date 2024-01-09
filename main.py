b24e4fdae457b6e7605e75c5025fca2bbad747b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:11 +0000

    lead clean common 564encourage watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
