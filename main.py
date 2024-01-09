b409eb2f06209491f3ad2dee1bd348790ff94733
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:22 +0000

    rarely share direction board mess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
