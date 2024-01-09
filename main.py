820d0490d976c73c9898c33d6f581a702b2cbad6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:40 +0000

    available mood snow left track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
