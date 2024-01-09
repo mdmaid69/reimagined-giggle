604147de2b887f7f3951d8a6e4d3ec70140ab3f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:40:15 +0000

    across issue often object difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_base_name(path):
+        return os.path.basename(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
