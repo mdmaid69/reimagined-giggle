1d8cb55a1ab45ea37b1061690f657ddf3257cde1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:27 +0000

    deliver steady 566end because freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
