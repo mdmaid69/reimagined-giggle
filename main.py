27ccde80c956cb21c610278dbd35173d23437294
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:19 +0000

    label drink transparent capital great

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
