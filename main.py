9130ed294190775e9a08b021b91af8c54a77fd68
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:16 +0000

    group brown place arm minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_speed(distance, time):
+        return distance / time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
