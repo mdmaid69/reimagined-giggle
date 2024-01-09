03457f54e20395e91a6aaec7cc9a707d856e727e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:43:02 +0000

    various nerve quick violent disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def convert_to_binary(n):
+        return bin(n)
