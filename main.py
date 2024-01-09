e97ce6ff97a195bcdf9a0bf36bdf5aca49cdfd64
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:47 +0000

    much environment block brush shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
