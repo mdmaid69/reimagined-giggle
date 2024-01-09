ab23e8490560d1717ab4f9d7ecef5b7d2b6fd7fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:55 +0000

    style miss standard later spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+print(os.getcwd())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
