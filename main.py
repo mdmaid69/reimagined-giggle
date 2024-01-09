e3b09958656dac44fe456542767c6b66c46c5f14
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:26 +0000

    compete otherwise coffee path pull

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+print(sum(range(10)))
