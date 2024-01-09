e7c41953d84e0e6e589145c8f4e9819fa70152c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:18 +0000

    stick turnout lucky yesterday force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
