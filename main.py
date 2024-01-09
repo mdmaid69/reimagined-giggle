382beee1c784a7d52dccd58d0b590183dd844d8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:08 +0000

    field need flat experience have

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_elements(lst):
-        return len(lst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
