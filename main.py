427e202753ebb866c204b630b6936619081d4b48
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:05 +0000

    pretty to kind slice them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
