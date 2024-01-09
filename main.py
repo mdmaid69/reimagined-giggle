a373ff119d2854652cd1fdc3fa30914fc2888b8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:13 +0000

    exit runinto machine tone keep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
