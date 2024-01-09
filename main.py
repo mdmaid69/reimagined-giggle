48b39a80625573abc303b94c0d6322969185f4f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:16 +0000

    somebody contents furniture swing clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
