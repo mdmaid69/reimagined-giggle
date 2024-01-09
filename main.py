06a9cfa8ad1fbfe3a541a656203c74031e170443
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:46 +0000

    abroad contact remark piece crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_current_directory():
+        return os.getcwd()
