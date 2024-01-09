2aacd955bd7be5ff1b7d35a9b388e1cc0c346213
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:29:36 +0000

    duty run run study prefer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
