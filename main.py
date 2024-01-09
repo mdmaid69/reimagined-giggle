dca82ac1f7676ad4cb7191d8eacddb19eda51006
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:37 +0000

    round take last month hurt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
