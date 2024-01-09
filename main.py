c8621aaeb32378b9986f40ab77344ad8009a4be0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:05 +0000

    cough settle immediate beneath communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
