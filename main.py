893c31d8fd053c9dc59e1a5a127fb4f84d34597b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:50 +0000

    rise discussion under blow compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
