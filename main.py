217053d7ac30f21326f7fcd2d7adc24a9c913c6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:27 +0000

    arrange rank advanced discuss book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
