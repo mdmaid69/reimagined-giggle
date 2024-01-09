fe663232aab237823e1d7d3b9198c80cac93e9f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:23 +0000

    familiar say quit mood weekend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_density(mass, volume):
         return mass / volume
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
