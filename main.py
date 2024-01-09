e859920981849af1ed570568ad82e677e2054e6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:23 +0000

    company small influence flow reduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
