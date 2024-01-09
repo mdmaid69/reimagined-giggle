374ec2515cbb01c0bfc997fbc543bd7d2ef8e7da
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:44 +0000

    cap custom time worried note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import time
+print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
