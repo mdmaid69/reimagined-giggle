9d27eb83dbc15f0889ac52328c38ce0b5e777f95
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:55 +0000

    unable debt substance pride cap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
