716bb0a51c4523d52c6c5e017eca1fd2b71540ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:07 +0000

    equal disturb operate teach recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
