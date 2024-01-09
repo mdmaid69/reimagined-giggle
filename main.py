8e29b5685f2c1e394886a5cecf94e5c67246bf55
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:40 +0000

    owe crash large pay capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def convert_to_octal(n):
-        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
