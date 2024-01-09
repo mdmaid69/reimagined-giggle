a0c53513c4c2e11807460013ca143eaf62713a08
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:28 +0000

    play market hurt full maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_gamma_function(x):
+        return math.gamma(x)
