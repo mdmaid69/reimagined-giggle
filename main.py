a030b3a8d62479a099f1f931d141e69a985ae932
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:21 +0000

    increase opposite turnout hello middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
