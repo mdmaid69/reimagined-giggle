3cf8d4133c0312d98f5e1fdb25381eab8abe98f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:39 +0000

    centre image objection service spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
