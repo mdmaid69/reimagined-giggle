76cbe4aca1f8aeed73ab68d2608680a20618af66
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:08 +0000

    television lock awake listenfor none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
