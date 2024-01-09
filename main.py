4978b35ff1162f32bdfc5215e3e227fee64b2057
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:24 +0000

    house reliable Gg protest clever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
