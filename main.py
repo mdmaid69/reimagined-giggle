085cd8aebecaff2353229ca8e22a451117c1b602
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:44:38 +0000

    defend perfect apparently Ii paper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
