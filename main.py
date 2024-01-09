4c3c269f2ec13444d172fb6c1653aaf2360c4d74
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:33 +0000

    change use reach clear split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def square_number(x):
-        return x**2
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
