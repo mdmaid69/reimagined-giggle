7ee3e41ce73695e20e173189d7e1c6dc86fae56b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:10 +0000

    raw wheel past swim upwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
