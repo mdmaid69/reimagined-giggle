1cd835d4c67b3a772fe0c0826cf19e363f844d86
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:07 +0000

    finish not spin poor Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
