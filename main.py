34828673c212617ad691c7cbd2670f1c1528eb30
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:52:47 +0000

    fat illness workon secret event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
