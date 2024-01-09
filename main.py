e5fa81409459a4eb22e8f2b752fdb4847e24f53d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:04 +0000

    opportunity couple mention turn scream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_base_name(path):
+        return os.path.basename(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
