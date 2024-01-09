2b8b84b330d96f68bd1d4891a4420051abf3315d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:20 +0000

    mess plenty arrival friendly model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
