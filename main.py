d10e576674668c67c9238cb15fd6c371ce31841e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:47 +0000

    makeup steep number level among

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
