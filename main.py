1ec06b81b6c42674f680ea84647443cf045b39dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:55 +0000

    divide loss relationship rob plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
