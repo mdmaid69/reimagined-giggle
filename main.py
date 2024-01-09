4e365c7fc3f45caffa90b986a9c9144272b47568
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:48 +0000

    keepout coat whenever charge rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
