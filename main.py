2af37ec7928392c3c13f2253f54ba1b5e8918023
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:13 +0000

    suffer regional meal previous discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
