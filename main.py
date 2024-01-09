280054ff00e84038a73f0845112e2d9834d52e89
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:35 +0000

    shout surprising question fly disapprove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
