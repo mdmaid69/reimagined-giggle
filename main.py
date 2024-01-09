9e1296bdb6085fd20fcecb7ef959d8995b332260
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:52 +0000

    getaway pin picture excuse lend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
