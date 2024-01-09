19b48ed48d334974d696f397050ddb938ced70ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:45:13 +0000

    complain complicated upstairs tip what

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def square_number(x):
-        return x**2
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+print("Hello, world!")
