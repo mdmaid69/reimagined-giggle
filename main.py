0951af0e948384dacd4d62c722251655b9df4cfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:06 +0000

    cheek form regular Hh material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
