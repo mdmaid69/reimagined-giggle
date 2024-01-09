9626911ee186636aac8edd8fc99032cdfbc0d999
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:29 +0000

    sail light hungry reject makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
