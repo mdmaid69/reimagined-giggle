3cced44f34288a8efa30d11b351acc60c0856e5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:34 +0000

    flat my major clear seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def clear_array(array):
+        array *= 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
