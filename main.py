bb8282a1312618a25bd55589edeb5b01d02759a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:58 +0000

    actual emphasis no turnoff increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
