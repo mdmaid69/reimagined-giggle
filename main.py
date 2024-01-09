5eaf3ab55786f17ad03065edd171ac1c94b7f2fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:23 +0000

    themselves moon exist comment change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
