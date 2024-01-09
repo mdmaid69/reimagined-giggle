32f22a89c4c1963778963ee68bff0594a15804fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:38:38 +0000

    scare breakdown frequent error gentle

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
