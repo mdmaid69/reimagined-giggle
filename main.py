9d83e2b89b4a7f30823df73c1d4c7322589ab57f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:56 +0000

    difficulty error hate push dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
