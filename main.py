a98e485640ca34f55506207de0048dfc8274110b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:28 +0000

    unit keepoff mother fast direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
