05fdfae86bfb64f05a05c6cca6dfd373b99c61f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:00:13 +0000

    wave minority top situation figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
