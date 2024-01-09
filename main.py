7e5dcd565f99b58b79054a59f7b79620919e8dca
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:45 +0000

    education like attitude authority friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
