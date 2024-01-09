ba769ae556fdd1434406a9907e1c22bbe53f2b92
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:37:09 +0000

    along in record deaf excited

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-def remove_duplicates(lst):
-        return list(set(lst))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
