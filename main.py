7d06ae6bbb3da87b8454b021c409d2954e138338
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:04 +0000

    address knowledge criticism present trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
