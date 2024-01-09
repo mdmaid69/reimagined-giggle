9758b67dca89b36fb0bcfc66ad7461ef782647cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:39 +0000

    left outside refuse silent tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
