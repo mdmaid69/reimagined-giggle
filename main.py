5c3d917f172a5d7cc8544a972f2250bf2a39d53a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:39 +0000

    no positive dance use plastic

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
