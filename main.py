7227a5d4ade44d2a683af3c21cba1c0bc10fec3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:29 +0000

    repair pen trick closed talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
