fbe9e449e02fb6d3d14e8cd111f0b8aa9893da22
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:15 +0000

    makeinto giveup workon gift hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
