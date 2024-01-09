2f500185e747cf031ca75eca1ef5aee806845aa6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:58 +0000

    strong machine house pretty nature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
