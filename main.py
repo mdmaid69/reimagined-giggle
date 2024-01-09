ce38f0f21c9f2c7754a18c0761c5813fe4370852
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:51 +0000

    shadow glad thumb step loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
