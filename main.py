3e45a56cb4e4e52fe97eb67c32664b1ef59ad393
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:00 +0000

    offend song cruel situation remove

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
