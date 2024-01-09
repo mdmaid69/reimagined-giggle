5ae1b11c9067ac932bafb8823d30fdc95100ebc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:49 +0000

    disturb brush cry rush thought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
