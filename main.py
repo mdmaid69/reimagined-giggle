7fee9a3c0a2938b18a916dd529b2c10427bcbbe7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:08 +0000

    worst criticize warn there aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+name = "Python"
+print("Hello,", name)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
