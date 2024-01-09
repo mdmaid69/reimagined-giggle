209a1afcf2c5b23abb299ab631ddea8f157e6307
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:52 +0000

    base service steam depend pollution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
