8f69ebd660f172eda012eb2909dea8b2d2544032
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:55 +0000

    happen persuade protection turnover black

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
