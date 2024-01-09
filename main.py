c818eff6844499135c9a30d25d327285bf034446
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:00 +0000

    week reply rely convince 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
