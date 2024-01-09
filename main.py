27754486e0c1851c5e4b83f9d5d25d9101c14db0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:20 +0000

    impossible call whistle proper computer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def split_path(path):
+        return os.path.split(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
