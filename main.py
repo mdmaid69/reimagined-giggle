2210c9291428b6a636dd708f6a3ac83cfd956628
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:54 +0000

    strong wire acceptable interfere key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_odd(n):
+        return n % 2 != 0
