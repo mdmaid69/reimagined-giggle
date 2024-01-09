3982ba0e15a8f748c6d9bc0f5a0e10ba6135d52c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:59 +0000

    tough his pay responsibility play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
