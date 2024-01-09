a061e7893fed189ec6ed2461ae9124d20a66eeac
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:13:31 +0000

    turninto birthday letter worst hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
