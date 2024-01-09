ba5609fa6a9db6419e7f97050c6c7d99134de77a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:43 +0000

    course direct happy production store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
