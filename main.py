402b9d0bf87ef0c102aefe03992c360a1241d6cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:59 +0000

    use drink weak tongue situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
