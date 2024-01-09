bf7cfe35ac158a50b924b8108232cab4f93ac8e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:33 +0000

    cough trade eye me edge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def add_numbers(x, y):
-        return x + y
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
