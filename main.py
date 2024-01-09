a5c8467f7d3d7bbb80bf7bd2e5793e1ba15ea7b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:32 +0000

    style luck butter question no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
