1415e7c56844856a4980a66dac59f36551ff09b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:59 +0000

    hard rare behaviour spot top

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
