e51f53d4d58d1937ad3a87730b0d108499a32f87
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:50:31 +0000

    circle tall pretty contents performance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
