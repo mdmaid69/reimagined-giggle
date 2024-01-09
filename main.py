0f6c8c5668da32fc09210688cf624ae4058ec045
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:58 +0000

    delicate fun part handle can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
