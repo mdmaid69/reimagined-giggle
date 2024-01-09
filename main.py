bb19f21662af9a4337c1e4d2e1857adce8ea5eb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:56 +0000

    connect say gooff key story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
