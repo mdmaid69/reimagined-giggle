154d21fd157312b0196f5ea03c6adfc210d8f7f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:50 +0000

    actually sort thumb 566end city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
