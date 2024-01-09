6aca8230cd52a77efb265c5e7e05f5cd214f7ff6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:07 +0000

    thinkof through because spring lesson

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def reverse_string(s):
-        return s[::-1]
+  def add_numbers(x, y):
+        return x + y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
