e41e6e4c78883420589acf66c60122b22cff38ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:09 +0000

    country think roll brain stick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
