fbe4dd03e01fdff46079c98396a194c1074d643b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:40 +0000

    manner wish artificial year except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
