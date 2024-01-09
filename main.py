d7ace00c84c42986d7cfb0f876d61298edfeaec7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:18 +0000

    exaggerate gentle card calm wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def add_numbers(a, b):
+        return a + b
