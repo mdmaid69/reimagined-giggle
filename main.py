36e01e557aca7d7be80c20112462bf954e7695f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:01 +0000

    bottom sudden ready class already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
