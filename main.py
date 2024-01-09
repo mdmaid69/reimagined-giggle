2c0aa29019dd1127f1d96592d37d55a8d352f45f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:25 +0000

    lesson basic basically difference harm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def clear_array(array):
+        array *= 0
