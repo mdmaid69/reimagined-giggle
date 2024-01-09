b85808de1c25d31c492a004bcc55d7675cc71aea
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:57 +0000

    port sell loss cry they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
