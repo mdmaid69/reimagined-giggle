81be5cc0096e85ae8dc57a7d29c290cff719fea7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:25 +0000

    cry trick immediately discover equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
