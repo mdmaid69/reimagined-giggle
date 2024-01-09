4b6d5468970f7d3c38eef24281304dad68a3391f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:59 +0000

    welcome except feature sick often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
