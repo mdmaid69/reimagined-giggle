c4b9cb0711b196b86fc76b3eedc4f6fdbb84a036
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:01 +0000

    rule special name clothes beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
