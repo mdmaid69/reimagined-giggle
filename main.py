f539275fa172122caea8302c077d66342a0065e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:07:38 +0000

    terrible feed smooth range score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+text = "Hello, world!"
+print("Words:", len(text.split()))
