733d54f61a70251165775b7bce44d2ab5cb0b517
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:34 +0000

    drink perhaps only plain run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
