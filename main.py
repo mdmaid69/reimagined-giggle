63cbc20d7c19c84a6afde4684847b72f8ff14400
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:23 +0000

    choice tip round basic attach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
