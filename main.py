9d496a8ab1ab4178a96c2f12bc09258b533b4d76
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:56 +0000

    getup male quiet lie temper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
