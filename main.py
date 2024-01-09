4c7f78f70053755dfaea7d9d484e58ba9a6bfc1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:55 +0000

    race whistle image crash news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
