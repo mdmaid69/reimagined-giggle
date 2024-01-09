9115529ee05d44bde250e3ba42de8b396e12c736
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:13:21 +0000

    challenge ashamed tragedy move pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
