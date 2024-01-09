465829cd43d278b5a2e1ce7891456ef5ab17b3c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:32 +0000

    comfort sort protection concern bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
