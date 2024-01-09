f15f53187f90566dc29ecaf73f0a679fd8cbb9b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:37 +0000

    description wake return window remark

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(10): print(i)
