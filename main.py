e44d1a3b30df499cb349928907861fb59ee740ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:42 +0000

    usual sometimes familiar alive tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def change_working_directory(path):
+        os.chdir(path)
