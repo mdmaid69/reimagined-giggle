9e7899c57adbcd761e135df47e7089d1ca76a189
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:47 +0000

    influence why float female before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
