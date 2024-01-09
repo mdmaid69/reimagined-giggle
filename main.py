47c3df437bdf4a81cc367fb41e4370fd164999c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:07 +0000

    attempt friend clear recover gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
