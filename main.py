fdc8578f11f3db89d2159e2db94cb345847827ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:38 +0000

    world charge coat personal point

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_area_rectangle(l, w):
+        return l * w
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
