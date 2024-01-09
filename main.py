c14faa45cf7f24feb5231d48ca1809179afc0698
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:06 +0000

    eye view prize find expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
