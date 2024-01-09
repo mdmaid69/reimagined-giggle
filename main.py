523ebe5017e82ec052548bbc2278bfd0f67be228
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:13 +0000

    estimate repeat inquire pure copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
