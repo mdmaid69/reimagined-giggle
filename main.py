13bf84895eaa9b3dcef1216339bc3ac83ab6a44c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:19 +0000

    best admire question improve concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
