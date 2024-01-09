f0fc15c4bb6e655ac58c1e1051e757ed9ac330b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:17 +0000

    dance occur split bear float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
