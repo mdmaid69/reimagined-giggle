532a094ec6799c44a68d9e3506600838419bcee1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:49:47 +0000

    public solve refer pleasant block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
