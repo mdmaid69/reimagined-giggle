3eac5ebfbf6c0f19492675d11509df0ef166bf60
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:31 +0000

    reply rush rubbish change space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
