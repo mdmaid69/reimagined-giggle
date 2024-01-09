3e886fefb333a35f0141c4cf086e739583dcb030
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:03 +0000

    pour takeover damage thin surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
