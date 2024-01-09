8d843db99a56b44c27c743589191e1cb9d26aad7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:49 +0000

    hardly woman relation gradually press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
