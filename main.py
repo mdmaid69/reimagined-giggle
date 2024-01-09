b06515d4533bb844ee347437ae3c068c58353ad9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:20 +0000

    actually off market destroy train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
