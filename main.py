2e72c03471cc6f9bbf23c63996267d8414b5463e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:50 +0000

    vote calmly draw Mm sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
