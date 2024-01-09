613b4954f96149e124715f2f79cec362f06dd387
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:22 +0000

    follow past discuss attractive wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
