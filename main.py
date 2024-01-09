18fc1b3688efbed1b9f06cd18b5d207a7a361037
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:28 +0000

    mouth consider rough cost on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
