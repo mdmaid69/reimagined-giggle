7d2febb06c81f899d71fe8ce7fa00bccecc461c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:53:14 +0000

    almost public consult explanation spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_int(array):
+        return int(array[0])
