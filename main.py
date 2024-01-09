014b618e2f0ff4e5caf501a1f61204e428249452
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:02 +0000

    between imagine light rarely trick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
