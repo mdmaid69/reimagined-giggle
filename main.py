d1ba641ddfabbda0982c8d938265b958a77d306d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:31 +0000

    disappear result talk power true

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
