aeff650f110a53ea70a34d7d91faaa220191541e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:01 +0000

    direction model relationship unkind meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def remove_duplicates(lst):
+        return list(set(lst))
