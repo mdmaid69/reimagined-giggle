df3cbbe26455d26ad584182614bbd0a2eccec046
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:24 +0000

    give dare recently thinkof great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
