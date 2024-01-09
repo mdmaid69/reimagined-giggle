b441a85d54c442ff3335f640c77e1a19cffedb4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:55 +0000

    same prepare region ago transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
