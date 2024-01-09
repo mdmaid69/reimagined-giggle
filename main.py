f8cd68eb67500b7b975cccb12a3cc22daf1d61d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:55:04 +0000

    runinto mistake uncertain uponon distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_index(array, item):
+        return array.index(item)
