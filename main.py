5d68db02a0159d0baf3d07d6241965f784b24684
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:23:28 +0000

    accordingto plate open able depend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_area_rectangle(l, w):
+        return l * w
