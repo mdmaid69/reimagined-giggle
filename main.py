104e89d7a65ce0b3facc3c9846ff61fd04c325b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:51 +0000

    consider whoever pot frightened discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
