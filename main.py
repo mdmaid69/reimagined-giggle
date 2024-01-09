4077ab676dbeaa5fe05cb1e05a3475bc5a5913e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:46 +0000

    affect please future stick declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
