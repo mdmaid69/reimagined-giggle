9b84b4fe0bf9263cb122fd24fb6009d48a95ea70
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:30 +0000

    cry share offend even tape

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
+print("Hello, world!")
