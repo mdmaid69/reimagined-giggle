6f9b1b6ed22e18152a954244fa9d587ccaf211f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:39 +0000

    relationship platform wherever fill bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
