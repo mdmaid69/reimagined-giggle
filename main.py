65a486294790808f6f2ca10a72951c31ae48d62f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:24 +0000

    appeal further holiday attract game

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
