4d02dbda1a460897bc524eced2fc4a92bad3a295
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:13 +0000

    failure mark squeeze turnover fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
