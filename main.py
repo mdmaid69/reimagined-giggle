fd8b86ccfec731162acef253e952d9b39711fa7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:48 +0000

    preparation home cook damage dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
