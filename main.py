766ee816597996356dfef49d6c18845f957706a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:28:13 +0000

    pass bring by balance alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
