1862d7706946e56b2524dabd7745530a38957d65
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:31 +0000

    name talkoutof own Dd contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
