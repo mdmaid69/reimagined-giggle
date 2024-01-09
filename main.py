3616c284ef38f6cf981011e6feb414186bbc2898
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:41 +0000

    another giveaway bored key turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
