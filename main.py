2dd929b82028efb7312000caff116cfddd3a9ed1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:18 +0000

    little blue lost sad influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
