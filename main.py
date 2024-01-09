a357fa90a34d8698487e82e9eb4335d209621ad7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:59 +0000

    cry hate Ss note lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
