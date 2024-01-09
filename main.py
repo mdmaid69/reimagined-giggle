37e36afdb9c4cc2fcb398b5828e8b2cbe750c006
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:01 +0000

    actual scale peaceful convince sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
