daa73370bad598c3ddc4b361df3d7b9093516287
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:39 +0000

    special emergency swimming so curious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
