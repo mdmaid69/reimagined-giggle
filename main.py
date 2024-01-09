50cb157f9e579d124f16fe43a064d5df46a20147
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:48 +0000

    join who fresh mean person

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+def find_min(numbers):
+        return min(numbers)
