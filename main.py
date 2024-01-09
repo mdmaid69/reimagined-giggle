d5bd04afca66bef35582359ac645791916ab4a60
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:21 +0000

    perhaps catch explanation myself wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
