19b855fab258a84ffd901736067e54f7ce8b54e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:20 +0000

    ignore rest commit certainly introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
