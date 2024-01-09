3436e224600cb62ea31b4ab2e4bd4ffbd1dcc269
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:39 +0000

    fast close nurse community combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
