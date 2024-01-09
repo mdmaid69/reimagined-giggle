0000d84afefb4e9d07936fc20cbf04fa586172a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:22 +0000

    remove equal serious attractive chat

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
