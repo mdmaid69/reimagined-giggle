6d870362f6a5ed1da65c1a68e85f1ac3ad83f080
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:50 +0000

    coat lower measure same cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
