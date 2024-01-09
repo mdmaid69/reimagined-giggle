4b8915612afb87172204bb41ec3a53068d0e0753
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:10 +0000

    safe cost many that dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
