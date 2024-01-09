91322633fe7ece91a4861bd63c0452ea1c9cfdcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:46 +0000

    fail avoid personal nervous period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def sort_list(lst):
-        return sorted(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
