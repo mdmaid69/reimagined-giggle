5494792fa2bbaf4988c664be841e9788bc947ef9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:03 +0000

    nice upstairs closed sure back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def add_numbers(a, b):
+        return a + b
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
