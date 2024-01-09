89ee95e14dca5a68a2e1967c39c7b9651922ddf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:08 +0000

    butter unless live pull communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_queue():
-        return collections.deque()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
