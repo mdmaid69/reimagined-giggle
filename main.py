79c2cc22eac5fab846975ac4fca693d20c1ca558
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:50 +0000

    hurry crash strange bang sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
