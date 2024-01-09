4e5e43f5846740a441183a03bdf75faa911b1f61
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:48 +0000

    once unable family possibility stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
