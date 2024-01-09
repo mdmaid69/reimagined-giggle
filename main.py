f111c5a4afdc2ae0894e9618c02c2753d85ff92d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:32:40 +0000

    extra see indeed pay lean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
