8985f5253134c93d04e86692d31caa5e0e55980f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:39 +0000

    power still drink fear exhibition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import time
-def get_current_time():
-        return time.ctime()
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
