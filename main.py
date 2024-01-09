efe4cd22fe26dcda25e539355920c1e201ea29f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:26 +0000

    confusing birth actually usual truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
