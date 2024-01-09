26efb3f64e2c1d41d317dfe5b0d8b3a451b5b202
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:59 +0000

    environment colour health their bound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
