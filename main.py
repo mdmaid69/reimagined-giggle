33fc476ee3a37d6ce33624fd24985ad7eca4e8bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:37 +0000

    pull office announce rely pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
