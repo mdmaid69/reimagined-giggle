52277c1de73e5b1dda03813027859f2c24bdfed4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:14 +0000

    polite often do trousers unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_directory_name(path):
+        return os.path.dirname(path)
