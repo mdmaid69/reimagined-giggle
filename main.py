341c853b8e3d5c68c7bf0aa7ce4f2c694abd9390
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:16 +0000

    foreign fence influence middle old

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
