768926e462a1f8dea62c45bd9dca9953bedf4585
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:39:44 +0000

    honest feeling meanwhile splash yes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pressure(force, area):
-        return force / area
+def count_elements(lst):
+        return len(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
