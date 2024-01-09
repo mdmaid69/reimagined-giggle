4629deebfaab6aab84fd18cdf20f042b7d1c674c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:39 +0000

    succeed punish result spring climb

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
