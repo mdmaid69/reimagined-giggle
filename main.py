4c08cf63ffa24e96e8c4612ad77ec8b2bc5d90cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:07 +0000

    before dear match patience confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
