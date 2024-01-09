aaf1fad53adf9bfa8810182b71e4531785d7e9cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:31 +0000

    you between bringup once tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
