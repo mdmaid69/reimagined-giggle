e5523ac44ba4c49f754dad106e86426670e88da3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:49 +0000

    top runaway line mysterious fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
