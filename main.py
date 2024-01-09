9c1dc01fe5d5851b603df379cd8b551e1f1cde4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:46 +0000

    surprised contact cost reply general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
