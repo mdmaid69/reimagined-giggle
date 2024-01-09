d7ee5ffcbf7528f22cc3d2aa0e749c4bbcf2e1a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:54:50 +0000

    only nonsense charge exercise reaction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
