905ab64fb191889dedf5fa0f6bf4c6f8170bdc7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:52 +0000

    fear continue possibility none crisis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
