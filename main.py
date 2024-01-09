2698e094861486a2505de34731fd8ba5b33afe47
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:05 +0000

    form fresh separate pause muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
