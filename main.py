42344a8127c304cc50f809c6264f20988d6152a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:50 +0000

    confused shock cross suppose fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
