64ec6c5344fa3c6c3e57171fd258a842cc7bcb0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:09 +0000

    my tall shake mail scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
