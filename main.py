de9e0a8fca1311dc9a4cd10843ea0f332695cb85
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:27:11 +0000

    sport cost shrink future closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
