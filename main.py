929e0a29ec3e25585174e3e12c25b9540a69acf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:41:28 +0000

    surprise fly long majority land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
