26ec25e66249f87fd870e6c59954ea0e19597ea8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:28 +0000

    connected test wet thorough luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
