808a17f0a6e145b8f9648e901c86af2ee765940e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:51 +0000

    mail conclusion must build praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
