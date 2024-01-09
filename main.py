7dc32cb99c285a9f7a9b720a5d85afb52e052875
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:46 +0000

    shout rent fire suit speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_queue():
+        return collections.deque()
