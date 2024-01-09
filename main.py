47fe577c58a179287500aacb138ab4a65dc0997f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:28 +0000

    fit play motion fasten simple

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
