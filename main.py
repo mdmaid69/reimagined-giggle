a185c42a546eb59a543d58bfdce9b10e3f1caa35
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:06 +0000

    warm fast trouble wind once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
