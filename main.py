b5e6618e49b821d62bb4d2401b2b6c6719d53965
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:54 +0000

    plenty without makeup undo loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
