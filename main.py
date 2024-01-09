1f4ebc5a3c21e3af174ccea31f8475832ffd92c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:22 +0000

    direct snow fast shake rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import tensorflow as tf
+print(tf.__version__)
