cb06580deb85a7a13f9441c6289de13820fdcb0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:31 +0000

    scrap increase odd dress work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
