d4d861ae65f713fe803584547a61be509c3292af
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:06 +0000

    say acceptable exciting pure path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
