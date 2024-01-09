3297bce4e195e19981201c255d2f9b1ed5b75e87
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:45 +0000

    possibility march most wing hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+print(sys.version)
