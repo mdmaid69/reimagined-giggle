6f52eca8aa6e6d0043f0d5445a2510f8153deed5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:30 +0000

    child board accident breakthrough makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
