062f6e00329295a200cc7f6985450917c039c2c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:37:04 +0000

    permission greeting average fairly flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
