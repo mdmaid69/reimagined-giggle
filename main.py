3d66b1b65d0407288720fb3120ad187445e47bfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:37 +0000

    giveout proud hate treat apparently

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
