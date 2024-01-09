e190bf24b758f4f984fe009594231ab19e82dd0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:22 +0000

    odd concentration wet value 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
