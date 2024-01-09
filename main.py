d14fd3ef0de1370344f40d094a2bb1099169eb90
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:56:47 +0000

    soil tread lead expensive public

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
