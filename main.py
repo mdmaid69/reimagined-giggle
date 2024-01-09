2776bb6633a46734e232c3e5d416de7567755637
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:11:02 +0000

    fierce pretty scream pressure head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
