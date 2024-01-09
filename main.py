52c63bc71d774dc8cf7d0eff58e486ab89087546
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:51 +0000

    suit fact interrupt throughout gap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
