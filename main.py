3edade673b789288ebd087a9981a1f26c236b52d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:00 +0000

    past pump same takeoff ask

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
