8a9e3fc654d9fa64bdcff09ab422ffd935f87496
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:31 +0000

    commit idea inquire mainly guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
