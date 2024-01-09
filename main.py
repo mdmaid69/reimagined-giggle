90711dbcb0c4b969ec307ec6b63f92ed3ffa109d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:44 +0000

    contrast division level trust giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
