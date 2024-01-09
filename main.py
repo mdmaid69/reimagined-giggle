e44f2f160e8271bbaa6339d2df8431ab7e91997a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:24 +0000

    famous beautiful amount base opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
