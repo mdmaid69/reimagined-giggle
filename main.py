72a301064235e40719f97494dc24f7d3b10e5e15
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:26 +0000

    problem business concentrate car female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
