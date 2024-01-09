5adb7bc6e726b3f7dd395705537c7d27032fc317
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:08 +0000

    spot translate shock somebody situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
