27aa404d65062ac7df770694642d03f8e3ab3d20
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:51 +0000

    lend lesson defend further radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
