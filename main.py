9f4790451e17821340a01e26c9acf06a62494c33
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:01 +0000

    week unpleasant worth own love

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
