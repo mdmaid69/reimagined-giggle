499b70bfb12286e5e8ea048e166cf69cfc067882
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:05 +0000

    talkback father dinner squash food

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def count_characters(sentence):
-        return len(sentence)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
