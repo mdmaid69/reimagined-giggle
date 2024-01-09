718219f40c46980f71552e8fbc7e9e2c77d183b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:30 +0000

    quit getup pinch acceptable extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
