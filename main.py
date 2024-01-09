b3fcf91f68c6a230cea8f6974b7f4921e28f78d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:15 +0000

    comment anxious programme clean or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
