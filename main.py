b3d87711576ee969065fb148ff73c88e321d6b8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:37 +0000

    job worry please lift breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
