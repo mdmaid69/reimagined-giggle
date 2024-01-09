4f21f5fce04cf6af32c1036f0aac72cc054fe75c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:57 +0000

    flavour around payment muscle let

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
