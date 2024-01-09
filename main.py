62a34f20658670ec6e1f3a50225bab4bb10f675e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:05:14 +0000

    command scared accordingto partner 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
