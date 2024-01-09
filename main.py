38fbbcac05d4111617bd79c813f2c50442cd4fe3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:19 +0000

    cry justice level friendly fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
