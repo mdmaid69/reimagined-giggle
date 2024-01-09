cc065e01918a41e5bc8cd6817d5d0c1044247a6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:06 +0000

    follow cold shout clever building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def print_python_version():
-        print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
