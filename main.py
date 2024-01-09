63362e4cfc72e32a13b38cdf1799d8778fc609e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:51:52 +0000

    tie murder favour warning correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
