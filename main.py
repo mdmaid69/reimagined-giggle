67d5608edc8349f72d5918e32b128ace490f61f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:31 +0000

    downstairs field post trick report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
