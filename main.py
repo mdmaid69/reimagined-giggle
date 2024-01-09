13a84e5ecfc9fda07751f01f296398f0863409a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:34 +0000

    charge me fun decide top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
