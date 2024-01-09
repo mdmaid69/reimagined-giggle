49b3133bc2a58f0470d80d0aa7659056a4ebff81
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:41:53 +0000

    rise popular clean claim school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
