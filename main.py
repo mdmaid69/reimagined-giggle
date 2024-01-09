7bc93ae7f218b128587928df2867eee753d47740
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:40 +0000

    wide favourite light value man

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
