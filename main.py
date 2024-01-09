0eadbed75a369ce8869c0e3434e9e011f963991e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:37 +0000

    Oo butter risk division sight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
