cae463732fc9d2d89a16a881d1bd657dea77c1ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:40 +0000

    theirs habit error production giveaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+print(math.pi)
