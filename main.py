c1d9f2e55df8ebddd254e61161fb9c8d48677f27
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:00 +0000

    call know independent free father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
