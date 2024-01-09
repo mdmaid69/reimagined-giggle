185f2b0bdaa039b68ff504aedc1aa8b2730f1c7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:44 +0000

    slowly comment argue voice claim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.path.getsize(file_name)
-for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
