02ace4b78b7fe7390a72ce75610b85aba5d4adbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:22 +0000

    famous equal system lookafter afterwards

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
