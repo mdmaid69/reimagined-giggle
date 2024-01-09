a07dddf6fd0705a47376f661bfc42c1865a09923
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:06 +0000

    cope stamp job fierce shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import time
-print(time.time())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
