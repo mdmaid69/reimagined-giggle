bd8051862556e245765ec13dd4f732247eba5a51
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:16:42 +0000

    aim hard shock whole chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
