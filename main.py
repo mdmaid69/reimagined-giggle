2d92996b09074336dc024bd8a95e78387b885993
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:47 +0000

    necessary themselves deliberate stiff detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
