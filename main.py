597a75d268ad00ec553f0543820a8fdaec09e5c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:42 +0000

    instant tune pollution break offer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Hello, world!")
+import json
+def convert_to_json(data):
+        return json.dumps(data)
