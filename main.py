f791eebae8ab3e38c0c357b876f7b95f831ba05d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:51 +0000

    plenty enthusiastic sorry skirt grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
