1b3fc7f440664e1a83eb30245f4f1b4f115f5f12
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:58 +0000

    weekend sink Vv you reduce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
