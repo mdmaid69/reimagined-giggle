1d40de0926b8e00dbb7f9f500d39b469bc8555b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:15 +0000

    almost poor nowhere lock eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
