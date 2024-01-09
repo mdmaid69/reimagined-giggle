ef47b8350ae6d33ab568e7db7755dd1a51a7b508
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:24 +0000

    mass benefit Itself post hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
