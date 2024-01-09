3e96564f4040e0419d35b62b384ed00526aabd4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:40 +0000

    provide swimming mine hold instrument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+import collections
+def create_user_list():
+        return collections.UserList()
