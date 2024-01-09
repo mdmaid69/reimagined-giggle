f4bc6a1bdaa6bc8ceb98eccf669bcf5753fd17f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:25 +0000

    wheel matter form dangerous swim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
