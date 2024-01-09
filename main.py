96f85a25e032062d15d13a63f9acc364e1a78e9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:03 +0000

    fairly mark absolutely settle address

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  def reverse_list(lst):
-        return lst[::-1]
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
