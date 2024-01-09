75dd5defd60935fbe9022b3b9439fff9556d88ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:44 +0000

    holdon passage pen fast exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
