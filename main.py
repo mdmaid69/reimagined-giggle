927fa9464fe9ee5ef3bdb0a50a81bea4802f2462
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:56 +0000

    winter convincing description spare attract

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_pressure(force, area):
-        return force / area
+print([x**2 for x in range(10)])
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
