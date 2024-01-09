6c053c0c9a352c8f642a16051f02b7daf4d77778
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:18 +0000

    soil callat paper complicated harm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
