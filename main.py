06d600f86a5edc09981cc9640199d1b5adc20231
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:57 +0000

    rain indeed then thinkover persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
