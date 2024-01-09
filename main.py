48c4014a5d510053eac613a62d302263f666aac0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:34 +0000

    impossible drawing glad embarrassing loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-print(os.getcwd())
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
