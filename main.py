1bc556835f1efdd2d52ab9f0ca054c28a78fd0ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:42 +0000

    now length reasonable profit skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
