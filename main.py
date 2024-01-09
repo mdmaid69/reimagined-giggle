acee869a77bae5d1daf7c97819b7a41b2b983067
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:58 +0000

    respect worried deliberate poor prove

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
