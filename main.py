e23c66d0b3490989e870affe33fe9932df07cd1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:17 +0000

    emphasize full someone information ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
