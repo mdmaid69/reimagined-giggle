60e7d1aa515bb10b7713d924fd8b89f1cd4a1d5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:37 +0000

    shopping clothes study display cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
