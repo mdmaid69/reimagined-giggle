e2eda971e45e0cca488c51e55cd3095caa17c51b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:33 +0000

    kind best set official this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
