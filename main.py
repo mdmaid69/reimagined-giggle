55c36ac9756409b78bab8d1856a6faabe3e08133
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:57 +0000

    show benefit safe offer today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
