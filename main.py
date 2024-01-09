d0f0ec114a9806c5738da98cd0b8339ebb21d4bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:39:35 +0000

    pronounce behaviour flavour slow pen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
