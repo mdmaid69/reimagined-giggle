c824b6bc2de6087250140d5613297ca5a11c6ed6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:15 +0000

    confidence intend equal cum mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
