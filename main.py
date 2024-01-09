52cdbd890491e528064d4a2f94a037aba7cfeb36
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:36 +0000

    lead train welcome someone part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def divide_numbers(x, y):
+        return x / y
