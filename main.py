3c9a3e36b735023d87a9a2bd7173e61ba543c4a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:49 +0000

    car backwards Mm practise takeaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def sort_list(lst):
+        return sorted(lst)
