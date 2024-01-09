299b356e1a04f6e1b2c546dcddea44b20f4c07ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:14 +0000

    history judge move comein region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
