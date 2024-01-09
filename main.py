d897508d8e6bcabf612a3192e9a37aba75776341
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:54 +0000

    satisfactory fasten tongue pronounce past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_length(array):
-        return len(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
