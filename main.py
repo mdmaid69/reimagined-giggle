b362e9f3fe35f8ae92e07b4ba6490a9c8cb0f07d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:58 +0000

    free method word appearance left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
