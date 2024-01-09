f8591b365f6d9d17e2a6637b038280ce2e8100bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:13 +0000

    shout call emphasize clean could

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def reverse_array(array):
-        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+text = "Hello, world!"
+print("Characters:", len(text))
