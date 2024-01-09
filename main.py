48d4ee85764beeba084957e92bc7572c1e080068
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:57 +0000

    settle consult open no judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
