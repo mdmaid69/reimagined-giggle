253a2d4d6ac50b828a076e7d6f1dc6b7fbf7a2ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:57:01 +0000

    crowd image task neat pure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
