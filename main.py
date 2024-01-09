c0cff583b961a005127b2b6f914f4e0ea81cb31d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:40 +0000

    follow disappointed married estimate declare

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
