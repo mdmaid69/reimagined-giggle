6262350d11f734bfa0ab90dae36480b323744bab
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:30 +0000

    hit roof hole appeal oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
