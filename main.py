547af2f79f129db7697c0de4eba77e5c6d5a0df4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:23 +0000

    each wish pupil miss finish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
