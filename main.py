b000fd56517cff62f6bae711ce9e60df66f9b7e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:05 +0000

    praise OKorokay may earth occasional

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
