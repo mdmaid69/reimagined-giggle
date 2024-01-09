944dfd3e24e762cb3e8e21f67ef51b08f4c8b81c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:19 +0000

    injury Jj mean unfair concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
