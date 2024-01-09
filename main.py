83951de6bf124f6881ba712c7c80c8a1701ee19c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:57 +0000

    heart unable border too weigh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
