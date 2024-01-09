1753f84af354bc105ad210b5b0a44ffb2157f38c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:12 +0000

    offer square real getup frame

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-print(os.getcwd())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_volume(length, width, height):
+        return length * width * height
