6d484085585e3256e6bb34d7fa0945c4c087667e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:14 +0000

    value store train agreement trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_array_to_bytes(array):
+        return array.tobytes()
