a8d2cb358a0e73c8d8b5a41a8c3c584b6a90a23d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:05 +0000

    exchange surprise leave brave nervous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
