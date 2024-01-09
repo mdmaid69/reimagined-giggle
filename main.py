5f998e7a3a7f9a274f268a2432eed839e62e4a7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:22:54 +0000

    course normal same work bar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
