a490d587a8ce5d5ab82554c3d44c4e0eb8fa261f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:15 +0000

    trap promise ordinary touch central

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import random
+print(random.randint(0, 100))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
