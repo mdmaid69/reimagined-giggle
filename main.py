9f32c928cccb0657d955e235384e847da44782c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:56 +0000

    roof person 573entertainment muscle party

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
