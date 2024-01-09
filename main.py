3853f9b161c48c7382f52cd94cc957fa8abd7566
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:39 +0000

    middle late temper true friendly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
