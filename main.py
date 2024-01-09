b42465c689a37b0bf889283e50cb3382b0d0b469
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:44 +0000

    tired deliberately makeinto confident appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
