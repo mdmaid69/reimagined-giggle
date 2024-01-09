c4b75d4dbdcde493f853d0a5f4c1bcb99a3de511
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:07:03 +0000

    practise certainly satisfied hold pleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def reverse_array(array):
+        array.reverse()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
