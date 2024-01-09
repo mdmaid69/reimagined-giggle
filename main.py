a776fdc61ae71dbbc3b7179a9945f8cceff23ad7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:21 +0000

    rent lift dish next where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
