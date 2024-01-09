74d7d0e23a06a9fc36e9aca14088b36316f9f1ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:23 +0000

    other impatient along willing bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
