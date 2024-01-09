01623cf2d483d1c97ad8865c69682b025efff235
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:29 +0000

    they follow spill innocent pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
