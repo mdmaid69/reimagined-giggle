e41b214598d3ec8e0fdc2ae7e690302f60c55c60
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:48 +0000

    centimetre wake row success promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import sys
+def exit_program():
+        sys.exit()
