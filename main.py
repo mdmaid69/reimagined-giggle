4b1d4cfca5f2713b0042f09c3bdab3f89ede2287
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:50 +0000

    mail space in teach present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
