60a86639eaa2a94e220a8eee4ff01e9923ed19af
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:55 +0000

    appearance touch possibly sleep accident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
