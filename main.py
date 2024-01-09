5d1f9862002d475673c209ca10a9024163b99923
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:01 +0000

    test write depend problem cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
