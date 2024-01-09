66398138ee0e1e7c4f558ece26c5466151f47507
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:58 +0000

    so happy yourself active comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
