fcfdf3cf6e1082b4b5500f396b20b89839465297
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:10 +0000

    convince goodbye sudden slice turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
