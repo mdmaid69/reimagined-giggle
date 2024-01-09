665e6495756a01464aa3959ecb7a2a19ee379cd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:18 +0000

    around waste estimate crisis regret

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
