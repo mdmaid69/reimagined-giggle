d1deeb2c1e149b82b349340423c411c36bfb8338
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:36:16 +0000

    Ff sky afternoon hand fill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def count_elements(lst):
-        return len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
