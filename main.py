165598b8688301e561f4bc71b762b1b33e9924af
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:01 +0000

    colour must fly land embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
