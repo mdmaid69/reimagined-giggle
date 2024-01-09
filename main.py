fbac26188876aca6977aa2bb57045f94b9235ce5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:36:20 +0000

    interview destroy listenfor excited rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
