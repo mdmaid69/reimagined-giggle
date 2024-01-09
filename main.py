137b887b6b2ab11808eeb206aacce7959dfe32e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:33 +0000

    base point win any opponent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
