0bb8c8c699fc4b24f3665b7c7a62452f38e20b5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:50 +0000

    excuse top related finger feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
