81837791eb10c0c8d90dfc5fda7a0156f4335e2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:59 +0000

    intend radio usually mention closed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
