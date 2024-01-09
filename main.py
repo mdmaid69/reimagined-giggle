22781021a48799a6ea2965e4135fe9cd89b51ca4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:26 +0000

    why pick correction everywhere win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
