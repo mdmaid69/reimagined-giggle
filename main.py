5f7f5b75a2119fc9bc83b45d08ae999a1c356e9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:36 +0000

    noisy of prove ignore environment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+for i in range(10): print(i)
