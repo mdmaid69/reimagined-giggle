a8bf8e5bfe6f2767705291703ce808ad96821f22
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:31 +0000

    run patient road successful attempt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
