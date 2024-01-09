272044fb623a02ec7dd31b6a3221eb76073e95f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:00 +0000

    left commit team on vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
