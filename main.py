20468f4b6912d90f35dcfd095dd05207ada77ac4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:04 +0000

    pump appear table transparent bring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
