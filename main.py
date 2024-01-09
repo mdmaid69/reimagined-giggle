a778730c90ba33e701c0023a4c325d1ee7908369
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:30 +0000

    advantage getback temperature release less

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
