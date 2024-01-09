8224b142c27556ca5836414cf01d0019f6a7fd41
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:34 +0000

    face turnover love getoff dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
