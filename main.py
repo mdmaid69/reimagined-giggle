896a3e958506236b402579357fea166128da2527
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:37 +0000

    expect rate attractive possibility worried

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
