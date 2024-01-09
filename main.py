9337b43d5446995ee9558467ca43c193431a9df8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:24 +0000

    Ww hope mess help finally

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def convert_to_hex(n):
+        return hex(n)
+def find_min(lst):
+        return min(lst)
