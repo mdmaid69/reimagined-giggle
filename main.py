3c6690d902736842905ecd7481819a26e9439b5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:34 +0000

    level type tape once event

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
