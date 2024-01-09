90a9138d3150b12cf4c4c445b7773813ee33bd0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:26:56 +0000

    later history clean friend afford

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
