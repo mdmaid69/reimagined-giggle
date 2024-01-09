8b54d9e8b35fc21599406aafc82d4cdeb1f30e73
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:06 +0000

    talk waste contribute forward hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
