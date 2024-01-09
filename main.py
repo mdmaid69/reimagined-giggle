b88530c674b7cc88818384cbd9581cab7c6fc2b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:20 +0000

    bit permission television curious home

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
