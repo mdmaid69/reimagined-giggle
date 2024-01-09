b1badd36495887712cb84bb20784555876175c7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:37 +0000

    opposite taste capital good tone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
