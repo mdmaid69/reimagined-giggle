e9556c5f2f6d836b6f8c1860bf53defa25900a96
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:10 +0000

    shock communication swimming same black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
