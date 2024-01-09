5af0f7daf52b3ebb825b3c80addb2b1ad68ed660
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:08 +0000

    normal track will world knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
