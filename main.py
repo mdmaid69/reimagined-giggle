df4911b01d92a6e2b89f29f6365bf6b520d70a1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:02 +0000

    declaration cook ignore boring tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_volume(length, width, height):
+        return length * width * height
