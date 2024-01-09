89fff518d6b14cfda83bf1f403f25dcfe996cbdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:41 +0000

    public round Zz never book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
