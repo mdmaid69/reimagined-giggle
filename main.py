bb83ae01f3c879f7eb71e24cf9b6f58184470ca4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:01 +0000

    from case original meanwhile eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
