d199c6bb653117e196c294783b7c355864a7b3d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:37 +0000

    behave eager band stay change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def reverse_list(lst):
-        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
