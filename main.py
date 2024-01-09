ba51261da9e2fdaefa8304d6bd2da47abc25feca
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:16 +0000

    steer manage plan ready home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_pressure(force, area):
+        return force / area
