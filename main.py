4a182aaba7ef22ebd587d5fb2b8d71a2ac133825
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:18 +0000

    gather head public place oil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
