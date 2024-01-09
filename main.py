1dffbdb502e9116e7ea0fa4c9a1b053655ec3df0
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:02 +0000

    wear slowly soon turndown sharp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
