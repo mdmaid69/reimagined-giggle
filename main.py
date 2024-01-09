9166876403f057949ef251b5d27453b10d2fcd72
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:13 +0000

    base deserve fear motion keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
