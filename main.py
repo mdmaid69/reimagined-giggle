9672ace6fe559c7f91abb3e0f82ea76ade8214f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:11:22 +0000

    thinkof petrol stand bet big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
