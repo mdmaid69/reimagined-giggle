21687a43a01e692f0ea583b4b1a25acbf1e2d087
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:08 +0000

    fast thirsty swim quit typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
