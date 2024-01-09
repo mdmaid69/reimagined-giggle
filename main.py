f252e5d50a18a399dc7e46ab54c4faa6e68da870
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:30 +0000

    future niggaacademy round bus even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_characters(sentence):
+        return len(sentence)
