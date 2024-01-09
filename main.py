4ae5afdf227d4ba780d364910decedec7859f6e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:52 +0000

    little swing develop right properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
