1078ce192e11351cb4e92465743fb3d00fb1574a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:26 +0000

    skirt handle even Oo giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
