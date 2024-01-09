ee48d9b60c41d18383092a7860f6f9fec9a6caef
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:22 +0000

    callat female afternoon good debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_typecode(array):
+        return array.typecode
