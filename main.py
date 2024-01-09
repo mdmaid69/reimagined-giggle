b26bfcdab81a3ef84966a00091c09394b3fa54c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:18 +0000

    plan dare brush ring squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_union(list1, list2):
+        return set(list1) | set(list2)
