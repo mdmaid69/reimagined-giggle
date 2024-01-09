54f75f4c2d62c4c999ba825d5935db6468c29064
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:48 +0000

    value mistake least adjective supply

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
+  def get_current_directory():
+        return os.getcwd()
+import socket
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
