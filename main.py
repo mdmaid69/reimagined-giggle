f650ac9858a390e52eae719a4442533f06b67053
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:20 +0000

    worth dangerous claim pump nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_power(work, time):
+        return work / time
