e5b5b6829e3389997ec2722ef7a04f2f2cfe03a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:55 +0000

    deep direct afternoon accidental market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
