2f648eeebc461664153fa62ce2cc5db4c49ebf07
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:24 +0000

    secret nervous trust light satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-print(os.getcwd())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
