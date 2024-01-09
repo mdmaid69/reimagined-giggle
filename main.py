2f416c53998ee1772b1df8000c1fbe8039784682
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:17 +0000

    famous mysterious both dead favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
