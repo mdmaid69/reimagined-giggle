54ff11ec72a57cd258c02dcb38d7ed1c22b710e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:57 +0000

    stone colour children sleep lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
