1e6f280de089132e1dcff5b84c5dd7ee1c84aa73
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:06 +0000

    belong uniform increase contribute wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
