8ddf334ac2c10c25decab742ef19d1f2a1d2bbeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:26 +0000

    after smooth drawing neck bored

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
