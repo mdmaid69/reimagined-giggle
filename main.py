40ce02f728900bab4eaf3a5813623cbb2577ff4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:49:00 +0000

    nonsense rest mistake right wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
