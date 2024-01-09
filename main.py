dec3f8545d2e8d9f1898e84b43bef5ac0a26d879
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:11 +0000

    brush uncertain offer stiff characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
