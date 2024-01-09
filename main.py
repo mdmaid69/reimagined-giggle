0f1ca8fd444276ddb4600f71a13e98dddabfd58f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:41 +0000

    underneath any underneath bend laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
