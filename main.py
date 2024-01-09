f73c0feccbc58a62b1b2d534a010a234ba6012d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:54 +0000

    giveout study damage height guide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
