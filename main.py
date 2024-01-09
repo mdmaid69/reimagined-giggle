ae37646674e688227fd73ceb553f36d0afe21de7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:03 +0000

    comeoff stupid room bad choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
