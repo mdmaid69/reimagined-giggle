35c5b265512417612e367678de4553212894fa06
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:56 +0000

    error general collect boring experience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def get_current_time():
+        return time.time()
