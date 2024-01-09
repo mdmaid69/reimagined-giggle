0ce4af9e8aa0d3bcd6edd08c2f7050397fac56b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:44 +0000

    fortunately swimming exhibition house sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
