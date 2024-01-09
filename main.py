ca72ce0d2bd443bfd720aaa4e7ff767b0c9f06b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:31 +0000

    public swallow answer run boat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
