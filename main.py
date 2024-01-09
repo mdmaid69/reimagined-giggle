59c97eadf82a5049846b74f716c922543de1f24e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:27 +0000

    impression only message pretend fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
