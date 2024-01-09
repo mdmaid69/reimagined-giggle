7036362cbde58c64889a993ed2649a27dc858d73
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:06 +0000

    track course tall content wipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
