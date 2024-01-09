e871ad564494005ebedab4f096b132587f0fda18
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:32 +0000

    middle minority positive drive ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
