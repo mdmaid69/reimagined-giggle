cafb0b56f49aff641549884baf3603c705d8653b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:53 +0000

    line rank black skill learn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
