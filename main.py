626b2fb02db35bb9eeddd42159d31ae2b4b3f6f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:23 +0000

    personal area freeze around but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
