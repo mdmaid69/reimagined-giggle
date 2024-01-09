7e29075a42e40fd8a070ff352151f3b42e575c34
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:29 +0000

    flow famous pause past crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
