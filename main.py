87448a8804d1b5a294fdd8f7c9e8d3597cd30713
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:21:02 +0000

    hold creature then meanwhile constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
