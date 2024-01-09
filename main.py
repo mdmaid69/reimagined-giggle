70fd997cf94964d59b9a187dc64d4fb5f0563f4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:47 +0000

    fall gas care bet beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
