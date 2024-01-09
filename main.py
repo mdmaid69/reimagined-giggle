093499d640eadaf089f0dd3e4b4fd5dc63e9f1da
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:54 +0000

    argument anywhere unable problem holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_power(work, time):
-        return work / time
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
