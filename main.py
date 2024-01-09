65b753eadb58af1310a8a6d8b77a4d4852a49568
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:37 +0000

    soft thirsty uncomfortable shall clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
