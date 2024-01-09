4cd16d0ec75b1e214fd03827e180370d3832d967
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:03 +0000

    careful put declaration memory plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
