4ff8fc1a66ed1b2dc826054e65b2c6322ab9afe2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:13 +0000

    gas bite event Hh until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+def is_palindrome(s):
+        return s == s[::-1]
