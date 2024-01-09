df07b9be56d919978f0ddab7e71cdb3f84a9780e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:22 +0000

    file sleep free frequent short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
