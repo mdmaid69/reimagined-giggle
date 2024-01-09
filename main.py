210b4706743c2c6617e820120ca074c2829087a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:08 +0000

    float better familiar long image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_work(force, distance):
+        return force * distance
