70b2f7e9a6e5e0be2c3a2484b65260cfa5381528
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:45 +0000

    temperature identify bored become interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
