18b7323af92f87b4a1430e1b2a7692ebfcccb5c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:10 +0000

    perfect mark story big comeback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
