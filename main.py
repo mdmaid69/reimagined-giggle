eac28127bafdd38a35c7baa7043c5276b86163b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:48:28 +0000

    complete measure band clean 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
