68beb26611abbfc784393bd0f2edd05b4cf20ebe
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:36 +0000

    disturbing fit meaning bread scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
