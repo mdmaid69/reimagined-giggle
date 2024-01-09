5821625603abe7a73d372fd04b3c57b1caf65f1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:57 +0000

    discover other fairly clean base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
