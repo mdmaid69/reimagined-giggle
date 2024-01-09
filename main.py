7750f59b3c50a9e4279bc2005a539b6fe0c3b42c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:02 +0000

    concentrate support exchange voice yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
