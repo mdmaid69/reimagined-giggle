22ab2e3adbef6ced52c84f2303a3f7a36e432b0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:17 +0000

    risk old flood poison mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
