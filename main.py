8964110408789dc1919fc6d14551e769a6eded21
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:34 +0000

    towards special recommend ordinary interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
