c27bfd1d4b58a2f5dcbccf7902932ef5896dc0b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:36 +0000

    appeal sad majority achievement display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
