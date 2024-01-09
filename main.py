29aa888b954ad88a2d7ee4be95139e71c9683bb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:13:56 +0000

    room 569enjoy importance settle on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
