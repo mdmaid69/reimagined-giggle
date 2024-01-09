69781a8c84bf482633194ad1aada796ad5b49a39
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:45 +0000

    bother mess watch aim important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
