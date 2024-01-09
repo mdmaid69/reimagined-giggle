a6720d365c15f4b89329567059e4d9cfc753edcc
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:05 +0000

    direct correction refer nurse unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
