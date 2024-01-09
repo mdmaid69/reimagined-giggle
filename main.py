dde9a158ee73a8fe9fa8b5c08bb8682da0db98f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:24 +0000

    plant morning sort true underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
