2f7ad76b6aebe374ba58d265f01737716b71d3c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:10 +0000

    runinto Mm again lonely constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
