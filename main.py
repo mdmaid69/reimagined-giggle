dac116eb216dd45c43516c088fa10db58a55f945
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:23 +0000

    shop draw name discover turnback

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
