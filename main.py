f2a64083bd90202b39c8823799aaae2cc35f0315
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:45 +0000

    bag giveup proof taste sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
