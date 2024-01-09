9d56e0a0ae3ca03ad3f9c589a5e9bca69ae939f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:01:02 +0000

    fast insist few worry plain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
