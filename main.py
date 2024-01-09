17edc4cc7ddc26104751896c2146d35b4bb99301
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:40 +0000

    will flat use gradually listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
