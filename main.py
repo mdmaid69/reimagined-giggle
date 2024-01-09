88176f4a124904f723bce183e8e91fcf4e4c341a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:47 +0000

    smooth use substance clever eye

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
