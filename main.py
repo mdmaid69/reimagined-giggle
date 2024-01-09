66d24734044e5d9aac5f809a7214de37fb258026
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:07 +0000

    closed invite condition money care

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import collections
+def create_counter():
+        return collections.Counter()
+def count_words(sentence):
+        return len(sentence.split())
