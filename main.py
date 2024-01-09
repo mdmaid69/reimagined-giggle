4a7254ef33488e8ad581a3465b1bdbce95d444c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:40 +0000

    discovery believe last city let

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
