9336be65164a0da28724382b00f1fbd552c6e253
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:53 +0000

    repeat shoulder behind admire mouth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
