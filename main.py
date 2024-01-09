f3ddc417c06bfc147a13f1fe9f506d499dba375a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:30 +0000

    day rest unlikely black even

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
