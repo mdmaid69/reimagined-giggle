756eedaf032ccf8277d6a766ebb3cf7fd86138da
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:35 +0000

    as much through increase order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_hex(n):
+        return hex(n)
+def reverse_string(s):
+        return s[::-1]
