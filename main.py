8495ff368752e9e3595437a68ce9cff0ce7b27af
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:24 +0000

    old male transparent cheap perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
