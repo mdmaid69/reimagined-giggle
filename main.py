cbab89431b6cb775720c70c5890b98239bcecd8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:12 +0000

    birth rule pair throughout confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
