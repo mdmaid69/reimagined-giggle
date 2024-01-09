e894109e2af21c1a7688119b606617202c4c472f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:23 +0000

    importance cover among crazy receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
