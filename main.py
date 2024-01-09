e8cd71e5decf6c22cbba6d36d9ff46a90dd26fd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:53 +0000

    friend middle 566end project never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
