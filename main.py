966915e1fb08be878891e9b556a4fc4959ca5904
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:20 +0000

    hello blue frame workupto distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
