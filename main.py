5c1840120daeaf73922f9afc76c07e7e056a6609
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:26 +0000

    way region afternoon sticky man

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
