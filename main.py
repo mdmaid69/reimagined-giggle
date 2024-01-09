a459e2e49cdcfa23801a758ea31d81d5de6445e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:27:51 +0000

    confuse snow recovery inside medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import getpass
+def get_username():
+        return getpass.getuser()
