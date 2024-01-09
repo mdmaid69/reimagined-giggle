71374ec4f23b8f55cf9cd90afde03c1e618f72d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:50 +0000

    opinion friend walk adjective opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+print([x**2 for x in range(10)])
