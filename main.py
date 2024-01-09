932f96f07ccb683340d8dbd5857bfa633b7e8fb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:34:28 +0000

    environment 567enemy load party spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
