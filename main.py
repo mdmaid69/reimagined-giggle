172edaeb8bd563824e39e0ac746dc52c59598678
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:19 +0000

    correct disapprove see refuse save

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
