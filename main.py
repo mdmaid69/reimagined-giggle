dbffa1913fe07835615099f790c4a588d69aed82
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:46 +0000

    frame spend answer accommodation ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
