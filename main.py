92d4d094c5bb4101d0714f96f36bf9c7749a8753
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:10 +0000

    marry exhibition need custom receive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
