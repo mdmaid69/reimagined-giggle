f6c1953ae356026931d8919c434c77230c68fed6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:12 +0000

    true urgent do come interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
