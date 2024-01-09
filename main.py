146b46d4ee603b2035ae2e8188b10afdbe4b5f85
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:19 +0000

    across cry fast hour worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
