9ea90fae1045b176d24a41a9a47dc40d76587c03
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:10 +0000

    better correction account hair guard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
