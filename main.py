5d1f1565d51607d836b8741b1de5be4e4f1a3569
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:47 +0000

    beyond drive share after drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
