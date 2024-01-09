56ce77c24bc630d70a6a2e53fb2f64a2c2cb95f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:44 +0000

    protest turndown criticism organize calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
