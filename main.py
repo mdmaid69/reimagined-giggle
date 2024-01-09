5563ab64aae8a859b6bed0cc6dc44543b4f7f04c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:31 +0000

    Itself film breakin exaggerate tall

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_pressure(force, area):
+        return force / area
