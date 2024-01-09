cd632a68b60e826cf3111150c0175d8070f14b2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:39:21 +0000

    card holdup much remark smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
