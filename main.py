70dc130b2e1211504fbea481b6aed01131afea18
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:07 +0000

    minor welcome study holdout Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
