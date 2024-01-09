3b8e31675a0e00f0da18b56e7fd16563a86ace44
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:52 +0000

    quiet soil list cruel region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def add_numbers(x, y):
-        return x + y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
