86f67f5cd9c10c9a1cfbafb4bfc47d79d7cbd412
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:18 +0000

    actually shrink ashamed familiar whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
