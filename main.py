36636210ca84918ec09ecd8fb954558360ad168d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:45 +0000

    apologizeapologise want 566end criticize excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
