da1822a38d4ca1a8ae9df92ceb128823534bf6ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:19 +0000

    connect but pinch holiday offence

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
