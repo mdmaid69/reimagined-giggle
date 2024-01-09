e28af414356484a8b4324eb37f077a0fbfd7e074
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:57 +0000

    national hit create impressive statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
