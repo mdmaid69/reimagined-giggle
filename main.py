54608fb6a42713845e63e07b7ab89c88b7e5afdd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:31:29 +0000

    move worth sight confuse remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
