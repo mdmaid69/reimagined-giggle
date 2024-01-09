b3ad99fe465a94e0f95ffda92b7cb0ecb9d1c3f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:42 +0000

    ring prevent discuss strong example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
