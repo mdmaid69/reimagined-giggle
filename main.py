eb271ab1adaa017106445f9e04ce0054bd1a64d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:45 +0000

    entry consist book act himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def sort_list(lst):
+        return sorted(lst)
