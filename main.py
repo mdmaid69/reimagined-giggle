ba12605d8a6b85b4965dcb01807c07248a755aed
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:31 +0000

    concentrate to fall advantage deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
