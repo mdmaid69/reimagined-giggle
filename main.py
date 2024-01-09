c1ab3898df4cc520fbda7b649328542a17f7b647
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:24 +0000

    show bury tough Vv spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def multiply_numbers(x, y):
+        return x * y
