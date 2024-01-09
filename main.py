6c22cde7ee7103f5e27bee2dd98ae643072a6133
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:41 +0000

    tongue contrast deaf squash base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def extend_array(array, iterable):
+        array.extend(iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
