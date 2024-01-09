25185bad8caeec6ba131c1227ff0cdaf0d1e1faa
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:11 +0000

    murder present change exchange yes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def divide_numbers(x, y):
+        return x / y
