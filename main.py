cf4ed240afe0c0bf982e96a847010bcaadb46ea0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:46 +0000

    square confused distant wealth raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
