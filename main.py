ca8523ac6ff82c8191948ded4d0fa3a069ba1727
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:10 +0000

    bringback his legal wonder pattern

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
