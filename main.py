3df2ad8ac8f6984582b9987ed515ef0df4bbf2ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:03 +0000

    plenty path thank cold fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
