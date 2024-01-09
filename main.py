4c85489ace17c3ab5e220bdea1d1051dd8f8f429
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:03:37 +0000

    relationship minimum left block tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
