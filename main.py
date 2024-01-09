d60dfbfc26125d64518d4c8dff52bb397bc93042
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:25 +0000

    man deep keepoff blow culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
