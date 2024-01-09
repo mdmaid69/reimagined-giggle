2328b5b8b6f8b1dd86da947e48187c48d89e8f7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:17 +0000

    pin mind exact star hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
