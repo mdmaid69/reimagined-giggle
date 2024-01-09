be1b261386948384fd8e71ef1db230831e6f3438
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:48 +0000

    continue burn neither everyone idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
