74df489389f48141c1e8d05578fa9d972d8d4e08
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:51 +0000

    branch shut medium increase earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
