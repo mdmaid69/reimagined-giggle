d9592cab2369568cec4f08c3cef63a39109dc5af
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:40:52 +0000

    argument shopping environment mood large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_user_list():
+        return collections.UserList()
