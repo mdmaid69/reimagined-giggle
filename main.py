8649c282330d79db64750d26c45fb99937a91635
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:32 +0000

    about object level immediate interested

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
