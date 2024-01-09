334f8053a943910bf1129e44c7709be78ce7a775
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:42:29 +0000

    when come form straight feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
