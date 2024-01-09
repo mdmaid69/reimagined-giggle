c67ec5cdbec5c47f6ef60c5487cdadad417efdc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:49 +0000

    obtain talkinto Bb wish neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
