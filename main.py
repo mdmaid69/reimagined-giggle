61c1c6abfeb57cf8b6faf45b1f6e6b39e5b5757f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:19 +0000

    busy uponon attitude speed Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
