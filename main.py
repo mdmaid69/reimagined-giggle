18b73911eb12d311dfa610d6c76bf8a5780c9d01
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:49 +0000

    vote nice high moon behave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_volume(length, width, height):
+        return length * width * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
