c4da766b35f3c7a24baaac9e4661f0d06877f88a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:29:59 +0000

    continue mass trip flood whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
