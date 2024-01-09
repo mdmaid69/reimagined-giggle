9fd3ff34f8353c72ead757e9da7cc1866170f0c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:37 +0000

    vote whistle available Hh train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
