2f122221fb4afe4ad3a4374d341ae1b9e0fbd91c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:15 +0000

    pride concern receive fight various

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
