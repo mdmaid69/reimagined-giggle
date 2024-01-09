7cb00f7653eef9123e36b0ab6373178ea81161d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:12 +0000

    value lift performance practise season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def roll_die():
+        return random.randint(1, 6)
