f2d8353ebd4cbedf18aaac34a4b18c0147cf4f5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:49 +0000

    568energy praise sure downstairs star

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
