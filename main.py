7b5266e93f9a0280ae1a1982ce82f95525924534
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:35 +0000

    aim bother exchange create injury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+print(math.pi)
