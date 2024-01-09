829a60bdc2d8f1d77643a1801a78e2544ff90901
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:53 +0000

    Itself serve disappear cheap black

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import getpass
+def get_username():
+        return getpass.getuser()
