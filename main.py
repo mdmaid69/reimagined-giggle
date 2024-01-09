abec5d3f6e75c7ad8c7a06f1027e9cb9fb6557a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:44 +0000

    also rare favour country godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def convert_to_hex(n):
+        return hex(n)
