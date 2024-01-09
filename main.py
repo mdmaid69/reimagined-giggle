70af235788150e81eac5142b481cf752dc662f72
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:43 +0000

    because taste either comeover talk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
