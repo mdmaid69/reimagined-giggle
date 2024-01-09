192b71f8bd87158d10cc2ace3f988755ed043eeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:41 +0000

    base sensible opponent comedown appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_string_from_array(array):
+        return array.tobytes()
