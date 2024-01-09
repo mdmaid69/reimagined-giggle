7cd1ba1298fb1081a2e92c185fbb08555b8cce06
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:33 +0000

    left unable sign course many

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-print(os.getcwd())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
