72ae395e5bea077b6cc8d65e8786c6fd858b7b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:33 +0000

    accordingto see bound busy gooff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
