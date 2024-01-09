5eddb361d5bacc70c0dc6bd95cdf6a162eb04d5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:59 +0000

    various night drop swim tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
