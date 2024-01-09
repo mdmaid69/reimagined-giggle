ac5a847070a55a62944cc612ccc1aaee4aafc929
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:28:52 +0000

    recover degree star Tt shall

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
