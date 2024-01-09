9e9f291898cfa8b79e28ce61ded5e7a1235fb4a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:45 +0000

    spend exchange female approval permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
