3862f5dd7979ab4d8271894fbf86232e1ceba524
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:21 +0000

    eat welcome pity black polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_square_root(x):
+        return math.sqrt(x)
