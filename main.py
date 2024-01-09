0a2055003960e21e4141511435854cf921ead7ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:35 +0000

    right form transparent cook situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
