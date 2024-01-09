229b399d9738aeccb033fee717d4671c46be66b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:28:28 +0000

    hospital content sticky healthy early

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
