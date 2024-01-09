ffd2c9239a961b2ff7a5b4208d14fc36247ea169
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:45 +0000

    row manage opposite store collection

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
