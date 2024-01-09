aaca9c73e8e216f3996d67aa93648ef5b70070d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:26 +0000

    represent bend takeout surprised thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
