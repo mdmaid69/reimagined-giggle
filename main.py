82a3c7b42fa4914f6f42c6fe1d4c5731feae6a0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:26 +0000

    ourselves shake unkind hers admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_length(array):
-        return len(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
