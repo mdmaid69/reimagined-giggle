101a0916f24c34f5005369cf007f85689bceeebf
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:20 +0000

    relation completely similar correct drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
