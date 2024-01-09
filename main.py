20af3c16821c36110bb4b98b03df4e5ff11b334f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:47 +0000

    practise sensible satisfactory split communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
