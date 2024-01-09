550d6007c8d8fb44f96d58ec84c3a690f4c9a585
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:30 +0000

    bunch clever environment Itself rough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def reverse_array(array):
-        array.reverse()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
