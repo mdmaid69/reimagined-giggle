1b3aa9b861b6456fe91b4ecb5fe2f5d5f54fa603
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:57 +0000

    worth glad command also name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
