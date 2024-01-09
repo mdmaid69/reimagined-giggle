4057fca7154dec6ae655afbcc441341539722c5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:58 +0000

    offend possibility inform takeoff side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
