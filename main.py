5301928c198bc0987bc52345f24b85d24fb3d6be
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:03:12 +0000

    seat take rest into godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def calculate_area_rectangle(l, w):
+        return l * w
