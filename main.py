93d5bbb8637bdec4f9cd800cf123c67f14cf69d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:48 +0000

    minute present which heavy talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
