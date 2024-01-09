ddb38ddb92eeeddc053eabf903a03de984be3332
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:47 +0000

    handle tongue method pick identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
