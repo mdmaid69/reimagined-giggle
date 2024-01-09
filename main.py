d97a2574736ddaf3770ef5545f39abc203b6a428
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:07 +0000

    put Nn gift moral wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def add_numbers(x, y):
+        return x + y
