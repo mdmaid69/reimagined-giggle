c82bcaf0f1cf4fed160000c0ef63eddb13c9a8a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:35 +0000

    space result dream almost display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
