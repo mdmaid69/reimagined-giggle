fb0e76fd6cd3a39bacb504afe90425c3e49b53e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:38:35 +0000

    sure sit ball comedown refuse

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
