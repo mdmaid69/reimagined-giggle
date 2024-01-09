a25d253ff31933a51f8652d139328fc1d7901905
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:39 +0000

    count some behaviour perfect or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def check_if_array_contains_item(array, item):
+        return item in array
