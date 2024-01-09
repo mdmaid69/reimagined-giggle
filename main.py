9a5fdb3dbcae11d610f9d90da8e2b0c71be3bc52
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:11 +0000

    peaceful intelligent legal me development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
