370a02d060fc796f4d73d5a9b9d20267ccfc8dc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:56 +0000

    progress apply occur you flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
