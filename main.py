020b1d4ba67aeaf227b8d853f27399ada6afaf3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:28 +0000

    consist laugh rain runinto confuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
