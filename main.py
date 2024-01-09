efcf1b8b3b2051d5231650e9a3da308045bf2953
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:38:25 +0000

    order result yourself afraid generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_current_directory():
+        return os.getcwd()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
