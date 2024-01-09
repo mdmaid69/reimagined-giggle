f5221a80644d660715cdc581f2ca0ceccd58b84d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:29 +0000

    black second weak around drive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
