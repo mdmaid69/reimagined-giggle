0d1bcb481da2ab2a80b82c875ddee86041043a8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:41 +0000

    suckme ahead stick stupid cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_set(array):
+        return set(array)
