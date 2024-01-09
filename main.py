b684a2de4cd7cad430a589d346a658c8b0a05bab
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:33 +0000

    hurry admire I criticism warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
