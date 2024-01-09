987cd650f45b02c143e298a0bed1cca18824853e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:44 +0000

    pencil frightened glass formal mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
