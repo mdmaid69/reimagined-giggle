7f921e4977c0d076bb8f1d3416b7c7ccc0a3ab77
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:40 +0000

    break control previous influence offend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
