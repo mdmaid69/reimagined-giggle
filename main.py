ba627cfcb2f1926cf019defbeb8c8887861389a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:38 +0000

    save 569enjoy identify pause far

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
