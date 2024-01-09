82a44fc3d14f495ad518a418eee6cf94e5c38b2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:57 +0000

    left callback ilovedicks tragedy keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
