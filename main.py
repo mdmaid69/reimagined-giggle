41d9dfb81638da62afecc0d849fb31a534a46993
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:51 +0000

    tap upstairs mention handle recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
