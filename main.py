c4a791aca5ff9af3c8a425262b7b22d8e6fba268
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:52 +0000

    splash undo better before side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
