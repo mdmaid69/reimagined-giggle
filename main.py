909527699e4fa201e020c4474dcf9d8dba131cf6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:31 +0000

    someone contribute lean burn rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
