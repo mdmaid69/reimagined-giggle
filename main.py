afdac952c3c368e13f183d804bd93209aa494301
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:22 +0000

    thumb final fill apologizeapologise exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
