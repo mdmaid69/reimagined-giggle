af68e2164366629e784cd4bb511627240798fd7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:14 +0000

    ahead positive least careless free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
