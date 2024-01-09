f1213dbba965a8fe0f53fa607e1950bed82835a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:20 +0000

    lower hide smile on uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
