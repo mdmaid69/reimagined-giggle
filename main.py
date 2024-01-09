cb5ba017ad41abeb70bc144de3f7d9352c37a784
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:37 +0000

    region moment tidy hurry free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
