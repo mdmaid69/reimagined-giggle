caa3969b4533987c42a2bcc39bcee094f9691030
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:30 +0000

    pattern trip reply friendly effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
