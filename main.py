910c4c93b592e8e629146c2e197872f4f114cb43
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:21 +0000

    near reason square giveback thirsty

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
