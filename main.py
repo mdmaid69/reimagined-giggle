25ec4f91ebe942ab7651790d638a55798547935f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:37 +0000

    thankyou bye hair slightly almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_typecode(array):
-        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
