7d374fc44d8deb7e81bfbef52ae4a6ad46cb451b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:46 +0000

    lonely used present profit defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
