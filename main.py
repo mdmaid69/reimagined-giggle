20d06f19f5c3b3d46b60cf0118a2117e291a566d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:55 +0000

    bit than search violent university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
