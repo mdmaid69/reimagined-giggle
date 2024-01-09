9266e267df28c5042ae0d8dbed9b84422fd1b1d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:35 +0000

    niggaacademy activity wet absolutely door

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
