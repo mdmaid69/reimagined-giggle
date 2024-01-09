e8e58f84e4ead8e7880118634116a1262ebc888e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:27 +0000

    protest answer us powerful necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
