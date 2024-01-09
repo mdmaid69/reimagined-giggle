8fc251501b524d63e305ce67ce92e5c2d6966016
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:31 +0000

    go turnoff anywayanyhow warn part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_pressure(force, area):
+        return force / area
