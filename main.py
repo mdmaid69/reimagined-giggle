53e193bccbc8a15d1eacc876a0968b3026ac9f81
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:59 +0000

    last expression sore usually ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
