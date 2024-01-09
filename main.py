46c09efa2a91e2141ed726db3affbbc1b612d3b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:49:41 +0000

    anywayanyhow still clear kick love

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+print(math.pi)
