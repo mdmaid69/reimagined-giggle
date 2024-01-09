8d5b2f7a6fb2c72566e2417d16daf467e4880a21
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:46 +0000

    water yourself power interview centre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def subtract_numbers(x, y):
+        return x - y
