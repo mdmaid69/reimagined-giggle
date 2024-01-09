1940af8cb2ca9e93a9ca1f53939675d15d3528d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:35 +0000

    earn collect task cost production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
