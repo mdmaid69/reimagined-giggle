eba41002dfa1fe28f6ca3c3cc2174d2e77f1bc0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:54 +0000

    frame opportunity sign possibility voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
