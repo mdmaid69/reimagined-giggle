15e423b1bffb923fbda8076815b025bbe90859e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:35 +0000

    confident may storm giveback than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
