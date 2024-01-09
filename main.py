da8e01380757c63c50ce59fce710e23f9c21fe9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:10 +0000

    friend wish settle price slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
