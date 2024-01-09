25c87941ef0e826fd724e634387636c9963ca82a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:03:47 +0000

    on body still remove protection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import platform
-def get_os_info():
-        return platform.uname()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
