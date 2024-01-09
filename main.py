adc290eb82e44ca9bc4f862477dc90b6f055cf28
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:59 +0000

    nonsense realize thinkover reply honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
