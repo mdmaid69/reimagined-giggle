a7ebc8f71a43f1d4624a9d1cae08f6af01ba55db
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:12 +0000

    round manage length turn physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
