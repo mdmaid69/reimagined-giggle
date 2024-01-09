16d2c85e9f3b920b0b9d893d4d3873d8710c21df
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:03 +0000

    friend principle best organize lookaround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
