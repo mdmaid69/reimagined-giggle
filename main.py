f5371cd0b585cfa7351285fc82d3d9d6d6fd5b18
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:55 +0000

    door number sick oil heat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
