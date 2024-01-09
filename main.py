ca3bb1b4671cea25aee32834864b540ce2006c06
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:13 +0000

    awake press over future gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
