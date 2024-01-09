192da77a36813e339f0f92e5de813ac9fb6b7c2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:13 +0000

    young most material sink usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
