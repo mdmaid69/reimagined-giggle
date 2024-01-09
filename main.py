80ea2d8f840c2768e823162e54ff046a9b2e48a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:08 +0000

    drop loan damp exact blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
