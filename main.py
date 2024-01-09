e1c683828279247ead1b01cd79a4a7d9913e56fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:58 +0000

    freeze early way post bush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
