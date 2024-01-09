d5bb0f2336a035912db0f6fa5ff0dfaec0da587a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:07 +0000

    family justice manner wave shopping

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def convert_to_octal(n):
+        return oct(n)
