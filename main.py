34c9f948f9dd1c46e29a277242c8ddaed07c2839
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:42 +0000

    sense congratulations thank wherever method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
