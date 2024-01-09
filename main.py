16e01f639a7c3e0ee2e2778e2eea89d9091a4106
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:27 +0000

    soon frighten remember convincing indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
