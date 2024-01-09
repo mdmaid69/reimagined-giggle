5480973d2add92e4f31cdb2b17521e575c7aa131
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:06 +0000

    kick goout telephone considering step

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
