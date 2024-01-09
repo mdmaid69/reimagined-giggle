1511c353e0f36fcf730eb59b626ab1f1d6477acd
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:42:04 +0000

    difficulty approve crash stamp getout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
