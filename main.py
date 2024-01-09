c79ab3f796560364f131329d9a44cca5a9b64283
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:22 +0000

    well public water reason butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
