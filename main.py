a0e2fdbc5176ed00195020f7a6494bb241965fa2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:16 +0000

    single wing consist geton flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
