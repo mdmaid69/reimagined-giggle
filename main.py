315a7bf06a1b6ca1396ad129f764e961a81dd09b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:06 +0000

    accordingto human gather callout maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
