0521e2e8ab1d220b05876606eaab19ead1593392
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:42 +0000

    walk able talkoutof break perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
