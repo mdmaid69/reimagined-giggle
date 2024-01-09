2752bfd877c6212db947cebdfbd28396e1e1d66f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:59 +0000

    tap breakup later first copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
 def get_string_from_array(array):
         return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
