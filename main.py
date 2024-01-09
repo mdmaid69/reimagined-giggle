daafaae82c7ae3fb21334d4f3148065757c516cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:44 +0000

    trip breakdown yours circumstance connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
