4e6f153135f0e80893553ee37544717f7dde3fe4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:15 +0000

    impressed tune same well foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def subtract_numbers(x, y):
+        return x - y
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
