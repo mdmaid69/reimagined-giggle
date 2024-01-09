5045034762a804050c5da4dfe73fcf0b678779df
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:02 +0000

    tragedy water card test goon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
