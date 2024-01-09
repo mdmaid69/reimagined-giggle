01731c7b3a078de8e23301269daa703f39dc837c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:59 +0000

    steer visit float so television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
