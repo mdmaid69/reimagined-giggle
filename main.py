d2256df990f1205dac82f75e85963fac5a983e53
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:51 +0000

    need measure shock pressure answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
