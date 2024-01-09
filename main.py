7e3b6085bbc1575d29ebc0f6d99c90660e016ae2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:49 +0000

    church congratulations road sail introduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
