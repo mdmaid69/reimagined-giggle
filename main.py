de9e19af2b8e6386f78c005a67a456c2d914f4a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:17:03 +0000

    image tape explanation sleep be

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
