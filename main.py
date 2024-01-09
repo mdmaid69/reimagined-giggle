b9f19dad7a8681f83d4671b296f1d42d9abec923
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:34 +0000

    usual mysterious carry winter somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def reverse_array(array):
+        array.reverse()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
