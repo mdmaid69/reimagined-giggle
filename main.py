198078405da5873c0e7cc89e26a4caf18e2119d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:44 +0000

    this letter pin tall uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
