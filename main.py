b7062a0e5b1a4e693e3a1a24893c0857e7e82536
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:57 +0000

    again this sweep brother how

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
