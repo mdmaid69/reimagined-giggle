703c6a444b1521cf28e0633ef56b0f6ba983deaf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:41 +0000

    trousers ill worst provide complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
