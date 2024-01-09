9ad5a105f8ddb7bdde6856c6272478735b358090
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:13 +0000

    mean crash main satisfactory meeting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def sort_list(lst):
-        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
