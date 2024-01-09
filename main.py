23a0e833e47733b871c41a7317fc6d6e377aadd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:56 +0000

    mood affect major century suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
