0bf311131c9c92f6d198d4b7348fadadbb45c050
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:25 +0000

    correct judge judge date run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+print(math.pi)
