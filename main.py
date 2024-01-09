3cfb2493c6ab2e38b2e0d80e958d3d56ca879041
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:29 +0000

    fall sound brief later explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
