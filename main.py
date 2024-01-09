510f5959c2780b43d5f14f64325d649544e061e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:22 +0000

    lunch wind impressive minor belong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_item(array, i):
+        return array[i]
+text = "Hello, world!"
+print("Words:", len(text.split()))
