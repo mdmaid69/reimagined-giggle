c48a2d3c1b0fc95c10bbe5c1973330ac5d64afe9
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:05:26 +0000

    rarely both jump acceptable brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
