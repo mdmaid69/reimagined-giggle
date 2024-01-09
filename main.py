60e189a908a5da7e60d5aa22629e2c8a1d69f1c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:48 +0000

    everybody minimum scheme substance Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
