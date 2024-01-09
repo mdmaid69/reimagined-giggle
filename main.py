8e9a2d929b7840fbd1d20f96bb8ff58f46562f2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:19 +0000

    gate song may afraid arrangement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
