4bf39465c1e6a057081604a9909252ee9a64cafc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:02 +0000

    but goaway workon nowhere lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
