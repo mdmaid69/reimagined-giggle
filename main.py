273d192f27b2875b7e4851a3fe5208e32344fbc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:11 +0000

    sweep trick wash unkind test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
