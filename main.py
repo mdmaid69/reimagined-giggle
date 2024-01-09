76b316446069a7b269f89bb142a1872d935b1055
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:34:58 +0000

    pot shall exchange emphasis cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
