c2e77a9bdf548b7f0a646f73dab0d9939a223175
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:16 +0000

    recent stretch target open hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
