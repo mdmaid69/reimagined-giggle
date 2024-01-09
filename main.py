aca786cb73c50a2020d055702326599aa4234e67
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:29 +0000

    top mark scheme impression unkind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
