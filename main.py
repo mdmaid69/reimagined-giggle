d64345555e244a25251613d827b960414f1848e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:40 +0000

    indeed 566end yesterday oil talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
