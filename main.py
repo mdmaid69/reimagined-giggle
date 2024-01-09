6d85de98c7e31ecfe0e4662bac83318c566bb4f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:29 +0000

    ours always demand split mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
