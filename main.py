0abaff7210b79fd106b06b913bdb92e40e122ae2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:58 +0000

    curious admit confused Gg tongue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def subtract_numbers(x, y):
+        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
