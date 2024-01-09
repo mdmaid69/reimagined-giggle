2bbec58fcfd4f070e9887e0a904b47730576e4a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:05 +0000

    embarrassing lock long warm active

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
