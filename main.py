35c4967e3ad759825bb3614b58c9c6f159cdc4b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:52:44 +0000

    effect bank twist earn comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
