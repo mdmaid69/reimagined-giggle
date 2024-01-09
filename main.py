d7a2954b50d1614a50b84314fbd1682c11f3e289
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:56 +0000

    street shock room slice tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
