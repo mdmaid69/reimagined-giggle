df24f3be6fe98df98efd96857ded010d3e3821ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:50 +0000

    shallow underneath deliver do blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import time
+print(time.time())
