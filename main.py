5eb0d1bd95dcef517958bcf259caf31db4543e4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:43:05 +0000

    neat search callup trade record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
