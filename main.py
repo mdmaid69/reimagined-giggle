7c9f5b792256f95e51c34bceb4e30dcffd06beee
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:35 +0000

    taste stretch neck drink regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
