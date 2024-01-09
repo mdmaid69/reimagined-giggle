1e7d31e203b9f15cce440c65f8078d8b8a9c0b94
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:10 +0000

    Oo hurt muscle wide any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
