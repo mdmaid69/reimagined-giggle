b8ab557a77089a0be54ae49491872637ac7f0088
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:48 +0000

    float walk among bin contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
