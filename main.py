58b934d605a71f5929550e7f2bc2ee76ffd7d656
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:26:43 +0000

    late smart delight bath classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
