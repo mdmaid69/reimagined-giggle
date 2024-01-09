9619a9c29d26dc6d8fa24efb45f994990787e5d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:40 +0000

    criticize taste shake goon giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_priority_queue():
+        return collections.deque()
