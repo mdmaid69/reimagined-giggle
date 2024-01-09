0318a0fbe6a7aaf48f4bd8e33d32e55f2af9760f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:53 +0000

    tone stamp chin patience hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
