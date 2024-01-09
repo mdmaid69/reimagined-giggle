2b49db3a5182eddc501e45d3481c0d5fa0096719
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:00 +0000

    pleased interrupt sensitive nigga ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def multiply_numbers(x, y):
-        return x * y
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
