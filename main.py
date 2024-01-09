09e3e1e7794d5b2454a146701dfa9c1d6a102991
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:02 +0000

    live rather different cry female

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
