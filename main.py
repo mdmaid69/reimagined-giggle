e252c3af6c44dd9608eaea09742b7cac7f0820ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:20 +0000

    pile poison delicate wide strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
