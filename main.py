434d186bdc186ec97d610cb1f39a0538352361fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:15 +0000

    book better advanced shame miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
