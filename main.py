927caa75996fde89880b83cc3c28e096d18dff7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:57 +0000

    simple giveout belong future Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
