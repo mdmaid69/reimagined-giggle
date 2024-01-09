202db47184f27b0d79b14dd856e04281b60a66e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:28:24 +0000

    tour report roof hurry tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_min(lst):
+        return min(lst)
