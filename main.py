912813b83f581765d7a5aacc3e64dfd5b6be693a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:42 +0000

    beside rare patience professional whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
