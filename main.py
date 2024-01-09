23e1b89b60d32e9f5d4290d9e1e1f7c62ffe427d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:16 +0000

    pot look bang bag noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
