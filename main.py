c39c410c8873358fc09f4df7bcb55cbcf19b07b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:20 +0000

    runout knowledge wild general hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
