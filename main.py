2654810bcf071ca1009462aa2fe3275664c315fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:21 +0000

    below laugh sleep open 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
