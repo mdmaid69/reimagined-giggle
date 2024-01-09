d51672fa6fb4710dcf6f443c8ffa295577c6c517
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:54 +0000

    hit open Pp loan follow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-print(time.time())
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
