a108b3ad1556d0707607185f8cd2e556ae97e5f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:10:40 +0000

    trade level none greeting warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
