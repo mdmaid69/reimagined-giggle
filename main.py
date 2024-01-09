b949ff1b7fb2e73be36b1307a857bd5e375f7adb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:36 +0000

    vanish size question price walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+print(math.pi)
