fbfd64788fcafd72f297c7e63f03055d4edadc08
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:18:36 +0000

    score club off congratulate worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
