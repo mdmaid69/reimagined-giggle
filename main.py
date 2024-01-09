a45434501d74e1c314769c73c29c6ac85616f1b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:28 +0000

    worst joke strike direction match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
