144e8d6c8adabf8abb2a10493ca61bd40fb5b275
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:57:26 +0000

    cinema connection any alive ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def clear_array(array):
+        array *= 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
