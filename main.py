bb0026d88b0c159b56bc01c2978372ccef6e469a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:43 +0000

    repair process someone connected jump

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
