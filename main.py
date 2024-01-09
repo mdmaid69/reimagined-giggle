14e6e2386d1d08ecfd490c75cedb44801ca868c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:53 +0000

    detail either claim chat tour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
