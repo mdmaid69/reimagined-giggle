b8419bfba51b528aa417631bb85bdbf813cff15f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:53 +0000

    want question especially difference scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
