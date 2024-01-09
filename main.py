17b4625b405aede575a15806bfca86810bb7b7dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:17 +0000

    burst survive better lean entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
