397dee5c91946b83001d3f7928986ac2fd3058f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:00 +0000

    inform jump blow light middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
