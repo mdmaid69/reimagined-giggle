addb49f49cf35eff875c8e43c113fd33842f5826
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:33 +0000

    quantity excellent month runinto capital

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
