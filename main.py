42683eddd00341267b248f58a61ccf1fd3930b49
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:36 +0000

    fit contain mention available confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def cube_number(x):
-        return x**3
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
