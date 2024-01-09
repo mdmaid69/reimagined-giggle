d1f4511072ac08da1460a957a5d458fbffe269e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:26 +0000

    separate original drink mind beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
