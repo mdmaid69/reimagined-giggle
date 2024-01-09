427597e362c1a49972cb161d3ba74f5475020122
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:43 +0000

    stop bring anybody joke tongue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
