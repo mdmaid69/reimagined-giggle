b71ee3a32ae548c743cac22bc015ab0a4c4ed0d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:58 +0000

    car base upwards tongue similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
