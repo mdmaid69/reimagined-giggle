775c31b7fbe4c5825180901c0ef6be8c218c1ad6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:31 +0000

    pin actually spill score miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
