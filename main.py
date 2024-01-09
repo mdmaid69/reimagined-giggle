bcc34c63abddec692bc476a154aa7d5db0c07994
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:29 +0000

    praise give content tough present

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
