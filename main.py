dd3047cb3410afac1c587cc70422bae12b0afb01
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:44:26 +0000

    turnoff first lead moral floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def square_number(x):
-        return x**2
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
