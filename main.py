d983dff49cca5e30b5cec0285da758509d33c896
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:45 +0000

    sort ignore burst reliable university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
