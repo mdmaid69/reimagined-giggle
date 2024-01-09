9a2c3464700f11c2f06d865f87105e56c639b57a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:05 +0000

    class power discover need someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
