cfee50f4f40920a0056106d1b36e125a65d8fbc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:59 +0000

    feed light shake demonstrate begin

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
