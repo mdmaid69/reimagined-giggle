e10bc47064ee2a1e33add1d655cc9f2a6efd88d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:58:18 +0000

    base bear worse band word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
