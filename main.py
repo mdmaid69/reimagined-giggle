c3bfdaae09b3aee51b827176ba48a83b8d66e66f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:20 +0000

    wire OKorokay dance 179bite so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
