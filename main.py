c09d359eb9d58cdb23bd41a5885606c99bc49f38
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:29 +0000

    trip left separate case getout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
