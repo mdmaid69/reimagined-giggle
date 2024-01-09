f70707b869dcf48a78643129ff131df627d66eb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:43:35 +0000

    check wave wear generous getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
