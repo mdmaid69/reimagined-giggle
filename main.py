81c4adc2b9020c7a8b43e4214fd94f0f4b0e5d2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:30 +0000

    complaint solve basic profit mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_min(numbers):
+        return min(numbers)
