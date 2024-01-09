3553f952c3e285bd735763b1e18657ec64a7a05f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:00 +0000

    married pressure accept Ss arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_queue():
+        return collections.deque()
