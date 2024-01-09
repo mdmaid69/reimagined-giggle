446149f925b462a5706b98077c6703190ad0dc57
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:30:23 +0000

    deep evening brown boil explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
