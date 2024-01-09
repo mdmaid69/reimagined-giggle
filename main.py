9600b45162c34b6077173e43623095a4ee7fbd74
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:36:58 +0000

    bang its moral aim feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
