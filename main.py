80b2743bad2686838285a40948f6dcf2117924cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:53 +0000

    popular leave match fairly immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
