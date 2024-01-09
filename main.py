48dca790195ca7edeec61d167228cea19c433c5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:43 +0000

    could region live trust process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_current_working_directory():
+        return os.getcwd()
