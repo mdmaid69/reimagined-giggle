498e588f1cdff38b601960a34258662ba47ff6fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:27:54 +0000

    damage practice burn neither thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
