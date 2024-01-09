4db2b2c15d41ac7737aa06166cc45d9acad2ad53
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:28:42 +0000

    point sad rely interview shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
