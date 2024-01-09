ceed39c01f08fdd3fcd999dc35eb4bd7f7939458
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:17 +0000

    which wet natural thanks challenge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
