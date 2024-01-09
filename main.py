a273b07fc811d61196ad6e83b81abe7db6e02edf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:12 +0000

    real consist detail welcome change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
