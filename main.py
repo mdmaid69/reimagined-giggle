728b7b7ced918f0d0051e790582ef5bec0f208ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:31 +0000

    believe tone die thanks 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def convert_to_octal(n):
+        return oct(n)
