a833c69e5cb5d3baa6504a642414d69b6c76e8e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:31 +0000

    event sorry persuade apologizeapologise short

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print([x**2 for x in range(10)])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
