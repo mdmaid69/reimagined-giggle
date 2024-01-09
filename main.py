596faddeecc2f38d24585a5ca759de10d92e077c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:43 +0000

    breathe trousers lunch steer interesting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
