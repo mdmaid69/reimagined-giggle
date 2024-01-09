5693e6c667fd1ed122c5f09710c48497dd90dfe3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:59 +0000

    eager turnback model week play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def square_number(x):
+        return x**2
