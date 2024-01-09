37d09d74068a234f25a601d4e86396c62cfb150e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:12 +0000

    respect giveout whenever foot what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+i = 0
+while i < 5:
+        print(i)
+        i += 1
