658b5574990739d358bd7b1c201d27bc6116d2df
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:22 +0000

    steal calloff cook sweep gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
