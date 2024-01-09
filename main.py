f19c858a75d870cca7b41281d860ebda42a0f702
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:47 +0000

    ocean meaning late round them

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def get_array_as_set(array):
+        return set(array)
