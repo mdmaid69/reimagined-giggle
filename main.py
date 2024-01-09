78c192f0efdf0958a6f46451a6aeb5ac6c9be9c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:52 +0000

    always between yet whether copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_environment_variable(var_name):
         return os.getenv(var_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
