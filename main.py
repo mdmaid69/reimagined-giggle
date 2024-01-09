bd951226196cad78e4bd9fe55351e265262df747
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:00 +0000

    circumstance thorough decision lay offence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
