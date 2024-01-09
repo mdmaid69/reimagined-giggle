05a03201a93e1ff9c44edec468d1042bb8caeb87
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:25 +0000

    study judge makeup call increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
