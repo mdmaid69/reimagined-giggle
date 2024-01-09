ba9bafb76c45fdde2bce53834c2e8537692f16ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:08 +0000

    ignore blue creature getout yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_density(mass, volume):
+        return mass / volume
