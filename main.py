61f4967333acd1758fe05017af923daf59f85b7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:52 +0000

    goodbye rise eye bound off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
