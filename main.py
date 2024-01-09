00b2d22f7914da4576bea1d35db0aad683309fb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:25 +0000

    keepup copy shape strike trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
