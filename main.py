fc45961ddf70a32eccf76e3aeace15587a11535a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:06 +0000

    laugh Ss other 572enter lookaround

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
