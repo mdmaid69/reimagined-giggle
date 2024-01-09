860770eb4e5eb865afc5a33c9cd9912a0fa221ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:57 +0000

    soft phone talkinto friendly extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
