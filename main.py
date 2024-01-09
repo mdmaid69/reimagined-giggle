b39e67ff0db01205251fe50eaf265eb71187c4b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:15 +0000

    doubt danger shopping responsible total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def convert_to_binary(n):
+        return bin(n)
