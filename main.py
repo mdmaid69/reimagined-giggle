b70bce5d39733d381fa7cc6ae34cc6681e0fc733
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:28 +0000

    undo clean future book middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_max(numbers):
+        return max(numbers)
