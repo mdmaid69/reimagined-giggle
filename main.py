38cc0ca7dd215a77575a04999066183de47d89ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:41 +0000

    bother nothing search interruption practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import tensorflow as tf
+print(tf.__version__)
