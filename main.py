adbb558e1f4f0b1fbadb102dc90913f0b08490f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:20 +0000

    modern roof imagine unkind rise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
