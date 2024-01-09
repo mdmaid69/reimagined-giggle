b46f44846635f772fcc699d2dd8d065a1b6d224c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:37 +0000

    letter unlucky calloff education invitation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def reverse_list(lst):
+        return lst[::-1]
