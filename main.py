53884466b3022643586c941ad1610859eeb0d6d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:59 +0000

    flood switch film crowded makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
