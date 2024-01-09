5b72d94f73a02b268bfc5fe0fafaf8b2f3829e95
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:16 +0000

    smoke meet climb cinema crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
