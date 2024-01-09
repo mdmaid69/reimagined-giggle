80d530fa09bd62de7d70e163a151a8c2927db1aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:34 +0000

    generally near geton without mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
