ea4ca476a3774a04f127797a7e3fce19683a2afc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:52 +0000

    that lost strike rain evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
