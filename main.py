021a5c6275208f0d3e8eefff1b4ad7b39491cb42
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:23 +0000

    classroom play wait terrible couple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
