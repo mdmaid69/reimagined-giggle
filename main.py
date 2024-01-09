d59fbfdc2f5cebdf64f65f8c0103caaed80acb23
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:21 +0000

    consider noone deserve swim pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
