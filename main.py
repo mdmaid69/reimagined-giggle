4975a4e1c24f607ca882af8be55bd8dcde392d66
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:22 +0000

    month breath team bringup big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+import time
+print(time.time())
