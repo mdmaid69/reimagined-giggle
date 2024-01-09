194f964704cd858a3ab6f684c5c8a5337828741f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:23 +0000

    car winter ashamed imagine report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
