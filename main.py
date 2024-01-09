71ed4472cb43eac323ccf6c76949a4e00f45585b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:23 +0000

    division demand but still work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
