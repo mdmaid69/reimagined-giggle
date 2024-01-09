60d4036ac024f108f29c52f1bf2dddce763f858c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:24 +0000

    present extra refer and march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
