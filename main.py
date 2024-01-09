45ccf2f4ae0f36801d0e85ea2167033f8aca048e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:44:00 +0000

    traffic principle choice ride sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
