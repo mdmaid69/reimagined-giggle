7e0eda045c29f8714d31ed6b81b546c5a9e864ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:22:49 +0000

    yes inform pool discovery community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
