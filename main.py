7f82fb3dc3fe895d73241288db896ac1ade59a56
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:11 +0000

    hurry job dinner programme sail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
