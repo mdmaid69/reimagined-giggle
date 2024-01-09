71277839739c3d9560e6746bb0b2a3024b20f3ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:33 +0000

    raw patience other rough height

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
