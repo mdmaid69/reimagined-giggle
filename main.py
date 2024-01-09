9d1f524ca971ea482c48eebe308a5c888f8f563a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:32 +0000

    afternoon ticket after light situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
