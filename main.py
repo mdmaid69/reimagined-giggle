23f60b96216c4b9b986ad3b2da761301b76222d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:30 +0000

    morning nigga number about short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-print(math.pi)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
