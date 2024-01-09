4498d52a2fd0d2d08333bdd2e93be8c693c72e6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:49 +0000

    first either physical live judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
