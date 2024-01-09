654a2b0d61595030ec69c92a4a09101668da41c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:51 +0000

    acceptable mixture computer thumb pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
