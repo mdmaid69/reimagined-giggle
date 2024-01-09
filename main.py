f6413b1e513aee2eab2ca9768c54099b26e8400c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:40 +0000

    however poison brother sort ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
