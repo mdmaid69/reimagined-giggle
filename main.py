7a0c91136e81e7d3a2d30a22098f4189ac5b9124
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:42 +0000

    suppose giveup type false clean

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
