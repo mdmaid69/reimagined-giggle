189c5459b0f26c6d72e03fdd25f61401271f3cc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:15:14 +0000

    yours appeal deliver attention year

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
