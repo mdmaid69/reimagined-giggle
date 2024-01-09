bf35abbd7496217b6c12e72c945ca73c7486caba
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:56 +0000

    impressive wind passage hold priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
