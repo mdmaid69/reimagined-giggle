75cc81a0eb8ede04bbbfc8966d2223906086fdf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:49 +0000

    road mark sound bread town

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
