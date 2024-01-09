e64d2aff1c068f9c06155079b23bf40dff4f3aa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:39:45 +0000

    rub involve issue cause clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
