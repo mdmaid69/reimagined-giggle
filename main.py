ac5dc1ab71558b57634d18c18a9d6d4c55ab69ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:26 +0000

    habit sudden nurse use discussion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
