06fa04ed223d4c3a201fb7d31499bc7cbd6c5a79
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:32:48 +0000

    use fight key trip same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
