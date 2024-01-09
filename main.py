1e770996fd52b68dbf9c39c5e1855a7e28ea4fc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:16 +0000

    unpleasant height petrol bottom over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
