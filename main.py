a38d30f8660c77107173e87481bd0da20d0b53db
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:44 +0000

    hang typical sky suggest recover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
