8ed03b1b5e6e4ef0dd9669169e612c03e9641a6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:22 +0000

    dinner turnup ready summer leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
