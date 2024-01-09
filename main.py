3d174f266d9324e254d1ab8da3a9c784bbef4593
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:04 +0000

    safety use leave apologizeapologise dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
