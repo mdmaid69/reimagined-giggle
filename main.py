2bc69faa0e019c52d9f81f114d207a293b444fb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:23 +0000

    show story butter getaway keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
