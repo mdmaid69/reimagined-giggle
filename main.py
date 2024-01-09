ae00ac782403549aa0cf12a07c9d2aae85334806
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:58:31 +0000

    whenever lead set trick count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
