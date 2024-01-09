d71ec30ef50298d94635cf47ad97c200b8822f93
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:37 +0000

    shop correct talk after against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
