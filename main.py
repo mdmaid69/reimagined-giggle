143575e7b3cee381e74e13e999b7d29432a144c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:42 +0000

    pleasure strange weak round someone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
