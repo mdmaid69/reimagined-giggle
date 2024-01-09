eb92ee636d26923388470f1483816b3f144509f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:35 +0000

    by message phone sleep quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def is_palindrome(s):
+        return s == s[::-1]
