51a2fd7447fb017a17f73a394a6ed87113b0f898
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:27 +0000

    deep fast far sort test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
