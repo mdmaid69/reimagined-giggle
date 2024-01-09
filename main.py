7d6866aacfa30c26be9614b4341c394a276f0913
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:13 +0000

    become comeback scare money Ww

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+import array
+def clear_array(array):
+        array *= 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
