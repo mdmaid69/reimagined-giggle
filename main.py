4272c0df985d5be9870114b76b0953f86ba78f28
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:18 +0000

    order bread active takeback skirt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+x = 10
+y = 20
+print("Sum:", x + y)
