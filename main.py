753d51fe55a2aef191f0ef764e4e89857e53f205
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:39 +0000

    post direction violence within niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
