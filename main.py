73a91f285f56e72ec25b89b232cf28ec5ba30b7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:33 +0000

    thanks matter cheap later somewhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
