0409afff46a51ba27f090d7b877dda0b1d6980d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:16 +0000

    picture trade advice march tip

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+name = "Python"
+print("Hello,", name)
