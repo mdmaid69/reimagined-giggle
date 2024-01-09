a90f810307fa371883ef5ce419cde2d14c487606
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:59:55 +0000

    warning extremely create feed trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
