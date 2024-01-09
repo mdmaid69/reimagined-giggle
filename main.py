eb07486485164cf032321dc9c04affe9beb171d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:03:31 +0000

    century almost explanation trick discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
