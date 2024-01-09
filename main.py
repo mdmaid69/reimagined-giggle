8092a00bc49b8185510dd96f14bcba894a54a2b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:20 +0000

    smile wave eat uncomfortable connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_queue():
+        return collections.deque()
