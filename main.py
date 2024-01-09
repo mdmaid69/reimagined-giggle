fc49d939cda56b738a91384265f6f7dfb841e705
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:45 +0000

    answer tend politics name file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
