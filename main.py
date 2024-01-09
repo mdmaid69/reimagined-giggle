1692337e55ab32978632c425e842a3d93f338344
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:38 +0000

    finally strong probably fill frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
