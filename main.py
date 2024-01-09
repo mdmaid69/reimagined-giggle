430167348ffbb8ab41c8844c8f196a420649649e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:58 +0000

    careful game order vanish distant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
