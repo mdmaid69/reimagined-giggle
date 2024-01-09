a576ec9714bbff42b40baed9c8ec50e8800f4aab
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:06 +0000

    up fire brain spare behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
