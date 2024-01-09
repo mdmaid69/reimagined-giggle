c4c4de0f0036bde126d703ba4e412dd2a62759a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:33 +0000

    slowly only cool anywayanyhow accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def cube_number(x):
+        return x**3
