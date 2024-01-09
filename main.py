324b175780d3ba7bd360210a7b11951ca591651e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:04:15 +0000

    giveback perform both in quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
