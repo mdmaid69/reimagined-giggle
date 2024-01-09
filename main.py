2ca2b6025cb529f65894b756ebc6b9de129916b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:10 +0000

    connected corner complete dig she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+print(re.match("h.*o", "hello world"))
