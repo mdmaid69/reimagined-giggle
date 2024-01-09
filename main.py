1578d435fe4d8eb33eef9c5ded26b7c3b95c2b20
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:44 +0000

    contents door pretty transparent goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import collections
+def create_stack():
+        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
