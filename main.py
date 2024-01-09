a00ea282af8a6f537c0f6f1c125b6c7e79dad982
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:00 +0000

    goup simple manner bright method

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
