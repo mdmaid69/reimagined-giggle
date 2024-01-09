1a135fe87102d6e04cb7aedacca1b5664a4b5bb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:00 +0000

    party tread protest crash yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_length(array):
+        return len(array)
