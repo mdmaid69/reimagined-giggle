8949d0073a9fe1946808ecf1c746b7fcf68aaadd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:18 +0000

    lookafter they wave second split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
