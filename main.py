9c508a7c213cbb6bc24560ffde26619ba9b347c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:22 +0000

    compare suggestion destroy turnoff rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
