d978e948490ba15be9d5a6fb7592fc95f05c841b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:32 +0000

    thin soak imagination spring tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+name = "Python"
+print("Hello,", name)
