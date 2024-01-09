a419eed7fb0f5310987bcb567f188fb178cc3987
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:55 +0000

    near work wrap ride learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
