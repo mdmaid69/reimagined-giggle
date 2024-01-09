0e68ab10adb16c17652f38875b37d6556554748e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:43 +0000

    afford the life exam with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_priority_queue():
+        return collections.deque()
