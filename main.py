5ddf5d64b677ad958dd27029de8adc50864c8a4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:05 +0000

    reliable willing pain neither exercise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
