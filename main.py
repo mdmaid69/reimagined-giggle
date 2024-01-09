d4171bcc783b415adec30ba631c5680f82cd222d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:11:09 +0000

    getaway defend kick space speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
