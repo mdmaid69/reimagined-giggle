ae353f18c037b50210b93a3ff8c18eb34f000aea
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:38 +0000

    keepaway suit solve Dd weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
