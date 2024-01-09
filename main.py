c534e602765597eea9b8be7d8dc881afdc9633f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:28 +0000

    strange crowded trouble become contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import logging
+def log_message(message):
+        logging.info(message)
