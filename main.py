2e636065259330b9dad674b24878d8f92b601798
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:38 +0000

    extremely water afterwards cruel account

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
