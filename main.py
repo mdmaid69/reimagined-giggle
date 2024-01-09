6ed34d8c3bff6bc51608f0e24321e3c3d56b7a85
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:41 +0000

    cap date physical watch soil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_volume(length, width, height):
+        return length * width * height
