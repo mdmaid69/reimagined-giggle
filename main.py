168b43a809e9ec85c51753c52956eab24d71b56b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:22 +0000

    alone gentle market new wise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
