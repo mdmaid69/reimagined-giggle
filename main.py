bb433c622ec29b1ad7f01dd9e922ca25cea1b502
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:09 +0000

    fetch somewhere last trade comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
