12e7de776d807f3c0543246b59777452a8341aa2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:55 +0000

    winter moon dry mess doubt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
