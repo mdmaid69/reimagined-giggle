de2a5bbb51fe3493e78d78a3d109b4acb9c4655d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:17 +0000

    turnaway influence 565end programme hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
