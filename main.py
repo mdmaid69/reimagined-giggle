c3676e7d2f8f174583443004f1b252acd038406c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:02:23 +0000

    before giveaway interest conversation public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
