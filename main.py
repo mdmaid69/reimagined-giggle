ffe3f1d232e5bd6eae52d21f88f0178c0450b2fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:19 +0000

    early prepare turnback arrive sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
