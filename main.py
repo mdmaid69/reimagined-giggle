dfe666bc0a42762340b9a0d90609ce616936a7ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:27 +0000

    disapprove tend club time disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
