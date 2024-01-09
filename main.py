1cd89c8e3563e40e3967ec2f2c0f43b84c92d16d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:40 +0000

    belief turnout skin shut shame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
