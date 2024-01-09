376badb636c86ef536b8b774bff8a17be191b41f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:18 +0000

    hurry though unkind slowly face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import os
+def remove_directory(path):
+        os.rmdir(path)
