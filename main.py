fb0928b34cad57315bc3d871fa0fcdeedaa166db
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:39:37 +0000

    available watch mine game better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
