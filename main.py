1abeb62c8cace9183b667b65e43ada3ce226b577
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:51 +0000

    break unhappy Bb likely goaway

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-print(sum(range(10)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
