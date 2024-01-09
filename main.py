02d6df61bd2ecc929fe114f197c7ef93d65cf70d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:05 +0000

    lonely provide next rescue first

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
