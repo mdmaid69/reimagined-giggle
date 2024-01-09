69d6216dbf0522205e4f5c04cecbe2a750d28ebc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:22:57 +0000

    bush loose telephone address average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 def calculate_speed(distance, time):
         return distance / time
