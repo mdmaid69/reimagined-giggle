70b35563e614ee114dcaf24f687dc568b5c7075e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:31:17 +0000

    birthday ready cut separate hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
