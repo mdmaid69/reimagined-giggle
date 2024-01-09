4eb20f2e7f2536b67efbd47a0b40d75b0501c9ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:01 +0000

    action at stop positive shelter

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
