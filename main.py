990b9427a75533da2c025b39bb3d22140f282771
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:39 +0000

    recommend money room party hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
