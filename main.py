8300726ede7f141f3d9e540f8fc3b1220eba3903
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:29 +0000

    bridge any report other compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-for i in range(10): print(i)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
