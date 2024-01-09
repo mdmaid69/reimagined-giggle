d5adee6677c84bee822894708ea107de89f8a469
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:25 +0000

    escape port may but content

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
