0cf1fc4bb3428f919e3de1826c2f58cffa653dbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:12 +0000

    suppose cross contrast hungry visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
