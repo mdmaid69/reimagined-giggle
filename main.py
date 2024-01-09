e35df1a706b338ce1418e26e636479690d78480b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:07 +0000

    sweep half test please choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
