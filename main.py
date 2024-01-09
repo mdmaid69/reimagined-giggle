770c8fab1a1f52039bdc46f4f0d7d0037b814ed4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:10 +0000

    lower second drive ready unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
