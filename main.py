cc67d223c5a4f11cd0be2f731a0638c019d827ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:24 +0000

    minority form connected back temperature

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_speed(distance, time):
+        return distance / time
