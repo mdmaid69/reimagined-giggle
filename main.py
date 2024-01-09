98ff3b7372603ec6d9767cf40cfe558aa35e75f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:09 +0000

    report view book hungry dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
