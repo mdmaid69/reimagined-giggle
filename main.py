0df2e05976968b0348f1bbd21e73c5b3e907c916
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:13 +0000

    minority exciting mind inform trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
