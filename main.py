6aa7b4726eed1dcb53a0f19f3542843bca25cec5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:24 +0000

    my afterwards test under couple

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def print_python_version():
+        print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
