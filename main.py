88fc8940d9676ad241c094e49d690a347c3888a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:00 +0000

    even crowd judge communicate hurry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
