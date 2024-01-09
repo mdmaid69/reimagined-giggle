4d452195679a9ac440e4d926a840dc9f2900bf19
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:54 +0000

    check dangerous error tone emergency

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def is_odd(n):
-        return n % 2 != 0
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
