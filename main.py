078d2592c6f2e5440d72ecd0cbb585aa0e7a98c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:10 +0000

    include shopping street rain fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
