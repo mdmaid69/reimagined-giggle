051c1ca885cf177d6962dd2be5abb7b8cd23c874
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:04 +0000

    hello string watch accidental ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
