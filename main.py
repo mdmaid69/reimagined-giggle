d8a283fc9c0f023d10b71bc8a13e08ce15422c5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:50 +0000

    suffer comeup power back help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
