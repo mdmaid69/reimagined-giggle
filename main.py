ea8ef4d980094ea727cf8254d31aa18cdc9f21a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:52:24 +0000

    ride spin doubt one lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
