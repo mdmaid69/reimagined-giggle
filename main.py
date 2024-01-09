b3088d91299d3afb1d12e8915f8309ba2b33f381
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:25:48 +0000

    question 568energy border plastic Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
