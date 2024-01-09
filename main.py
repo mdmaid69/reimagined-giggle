e9820185a9955739b55a402886fbcf5fbb669551
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:05 +0000

    spare many sea whistle comeround

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
