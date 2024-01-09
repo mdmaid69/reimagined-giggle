56bc67d93b6164ec56b5c893547d0249e20962f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:30 +0000

    value regret boat apologizeapologise no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+for i in range(5):
+        print(i)
