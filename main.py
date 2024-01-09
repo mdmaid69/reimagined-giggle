6d999172a4d2c5b77efbab99edc59326f40941ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:22 +0000

    downstairs once empty chance disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
