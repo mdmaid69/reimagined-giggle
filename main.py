65f92c37e81cbcac383ba84390dd7036d0ca5b52
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:45 +0000

    clothes acceptable me shop used

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_index(array, item):
+        return array.index(item)
