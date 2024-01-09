50cc656d1f045e09f39bd96145f0a6c69b55427d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:24 +0000

    slip Ww occasion light winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
