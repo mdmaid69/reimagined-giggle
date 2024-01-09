fb33ae531a4c2d5aa3bc6774e3451742960772ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:31 +0000

    alone his nurse scrap door

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
