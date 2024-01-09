830eadab2e5ccd07236b6c476b1c62e13f24d813
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:13 +0000

    everyone judge squeeze create support

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
