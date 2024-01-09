99e747e72ab22e70587a941da8cd279a51a2cb81
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:49 +0000

    many dress far our tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
