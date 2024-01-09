4a2a31d18401294ab2f799df98b4016588c84033
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:12 +0000

    wish weigh brown circle she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.ctime()
