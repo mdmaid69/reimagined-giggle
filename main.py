fd0ad3f47965fa915a4d1efca0d0128d712c684d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:33 +0000

    address bear practise slice breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
