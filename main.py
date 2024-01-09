5a31c90dcb77972cba9d714d594398e8727f2f3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:03 +0000

    cry lead bus Rr front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
