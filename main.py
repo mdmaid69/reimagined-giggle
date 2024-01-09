8c5cb3015dd7a057acdf18588aa162c4c962c9de
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:10 +0000

    watch skin better uniform soft

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+def get_current_time():
+        return time.time()
