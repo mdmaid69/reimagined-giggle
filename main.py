13d07751639e1fe9f06acbf26ae613bdf1bbc963
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:43 +0000

    classroom direction sure university enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
