15aefcd5b1468b68231313dea2866b990dd57bdc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:03 +0000

    wet material practical whichever tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
