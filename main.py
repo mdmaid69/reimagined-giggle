034b5332e81e89451465317ff134a3146c3d2674
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:41 +0000

    smoke slide add report manner

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
