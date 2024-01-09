c0f34a22e3c4eea0374046c8dbee7a3e3bbfe24f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:42:46 +0000

    contact everybody prefer acceptable practice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
