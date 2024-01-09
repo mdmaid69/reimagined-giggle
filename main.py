572f064bb47a8917ea0663cea39f3219e4454a9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:22 +0000

    sink feature image bend chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
