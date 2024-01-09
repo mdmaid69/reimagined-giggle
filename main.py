ff767a656b672b9991cc0da9c0c51952b017105c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:34 +0000

    fetch date afford later hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
