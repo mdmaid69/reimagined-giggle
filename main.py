fd228616cf394c6982f6a38f30a89814eb21e004
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:20 +0000

    study slide friendly operate heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
