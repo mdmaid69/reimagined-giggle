981f62a76babac957cf0c069486f56097a3f8c37
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:34 +0000

    affect lesson chance crowded past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
