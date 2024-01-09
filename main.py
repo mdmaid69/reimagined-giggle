37ce287fd09ced642b7e6009d0501b39348e6b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:09 +0000

    light rank cheap similar congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
