12d2c053b6500df1d635cabc27911f7394bf16ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:53:45 +0000

    quit head door can win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
