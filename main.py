2b78a978f226fa77e611621c583d962aaaa14719
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:29:38 +0000

    plan argue word muscle mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
