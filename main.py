f8bc4d38af91499b88de703519cff1950e3cd08f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:57 +0000

    turnoff urgent breathe odd talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
