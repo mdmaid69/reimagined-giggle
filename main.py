de82e4dd5266779ad5b0502e036df20f9509e6e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:26 +0000

    shadow male admit health boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
