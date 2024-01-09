276dd61ead75f86144ad06f385fb08644dbf41cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:03 +0000

    turn advice business low limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
