9cb1e4d1770368906b788d2abae4d59d8bb62cd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:24 +0000

    breakinto comeover waste funny narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
