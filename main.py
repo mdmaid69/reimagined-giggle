328acbee5e604c92af525080af7df29918b0b916
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:32 +0000

    picture teach support dress bye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
