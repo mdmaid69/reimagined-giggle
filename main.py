89158d9234320098d75f2f95ec94ef3226380425
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:04:27 +0000

    practical minute prove protection reasonable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+print("Hello, world!")
