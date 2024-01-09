e8270d7eebda3e541c54d3a03c6d516c39162875
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:36 +0000

    fun available company understand slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
