c65b9f6a6fb635503bce2e2f9f3702cf36668107
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:30 +0000

    patience mood across generally defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
