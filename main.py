8686746fc4b40250c69e73f72fc3914b82fc3a6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:47 +0000

    feel support alone that home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
