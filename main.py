d66e300ac9439deedfab859f986470640da2aeb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:52 +0000

    change talk spoil contact frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
