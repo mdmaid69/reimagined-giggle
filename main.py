98ea98b864627d8cc1613aaae5b087a3bb69ac17
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:35 +0000

    equipment neat part smart possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
