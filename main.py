6b0a7df82a2d14dd2c94b193f7c1f2497566779d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:29 +0000

    damp refuse window achieve flood

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ def get_array_from_file(filename, typecode):
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
