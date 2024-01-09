9cbb8b3f38435eb9cbffff4bcd3c49e37bee58a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:58 +0000

    row bush friend aim very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
