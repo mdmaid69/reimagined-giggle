1419d5d90d481d3be49db33503863bc71356d618
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:15 +0000

    bridge use bored complicated guess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
