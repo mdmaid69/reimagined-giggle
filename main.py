b51ee1603ba4afef18cb0aed1fb7ef552f03379d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:37 +0000

    dinner tip give sure once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
