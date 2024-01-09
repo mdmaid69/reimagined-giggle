eddd9a263ffd8435a925cdcfaf4f65873975229e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:31 +0000

    thin development please free cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
