58b0615f5726dc7a0f0e82ae16dadafe9ac05d28
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:01 +0000

    some teach allow weekend trade

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
