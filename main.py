26472bb0993837273399d4c6c80f98395bfb564a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:05 +0000

    rent deep him comment degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
