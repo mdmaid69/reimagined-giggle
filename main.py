5a6b5ab8449da099138d50833b4d8bec028a6da5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:17 +0000

    should rest dull stick fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
