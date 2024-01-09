8540e6b7fa340ce153f30d80bd3385453d746213
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:13 +0000

    objection summer pass direct sorry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import time
+print(time.time())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
