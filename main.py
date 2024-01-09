54106c49c6178c5e0d8531bb41487e1ac3f61daa
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:53 +0000

    straight stupid match street confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def add_numbers(x, y):
+        return x + y
