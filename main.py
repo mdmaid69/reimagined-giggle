950952f2fc4990247651191fa0416c2846743c95
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:56 +0000

    profit platform or urgent underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
