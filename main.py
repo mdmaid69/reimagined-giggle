08d126aa4d469ca060c01034b1525441c66868be
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:34 +0000

    train moon plant environment takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
