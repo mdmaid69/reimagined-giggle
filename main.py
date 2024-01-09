3f75c6674c9f8797d704cbd8c75fd45a67e31d14
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:53 +0000

    law pour watch bad excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
