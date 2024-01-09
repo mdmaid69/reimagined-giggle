7b5d8e6ad5768967898d57e4442c0537fe4a7b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:59 +0000

    equal own possible pay proper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
