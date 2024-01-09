7bb133ede56a74baae1ccf0db854847537c52354
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:06 +0000

    weigh admire get lot shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
