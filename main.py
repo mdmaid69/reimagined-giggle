02ddd4cb44d816279f99c85c427194634d16d58a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:27:52 +0000

    attempt cruelty talk after blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
