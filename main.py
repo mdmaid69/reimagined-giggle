889272e9c893cff808ff28143e724737d6b8d5bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:30:58 +0000

    lead grateful middle keen 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
