36dbe1a5803e5cc3d1f5fea8b112e658fd567efa
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:21 +0000

    pen satisfactory anxious deaf address

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import platform
+def get_os_info():
+        return platform.uname()
