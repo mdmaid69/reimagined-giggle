d91bbca130318fd64746212c46e52f25eb761633
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:33 +0000

    favourite drive mainly Rr value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
