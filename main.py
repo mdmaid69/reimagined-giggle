7510404bfd07f2ef62ce7d3788e1d587d08ddc4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:14 +0000

    over fasten let error protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
