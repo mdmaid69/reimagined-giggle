388e7124b87c9cc027d8e31ce749db70d9feb8fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:31 +0000

    support post error himself damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
