78dd73eb4f0971c981e0579557e427993d27949b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:01 +0000

    climb least comeoff 179bite tone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
