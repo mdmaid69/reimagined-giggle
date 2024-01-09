003a59881b74459850dc367de35848323b10695d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:34 +0000

    loss impressed group after second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def convert_to_binary(n):
+        return bin(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
