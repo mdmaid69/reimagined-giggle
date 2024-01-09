5c6f7e2bd30469270486f132b1801e0ae4b303f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:29:47 +0000

    somehow cheat either brush worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def create_user_list():
+        return collections.UserList()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
