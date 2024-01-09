521510e705cf371087a41b707e96009fba972c91
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:51 +0000

    activity turnback help day arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
