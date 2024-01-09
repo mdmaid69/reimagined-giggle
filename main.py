c1dbca50e343be7f9370e255e4100cc40d3c2308
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:03:24 +0000

    spring replace out television would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import sys
-def exit_program():
-        sys.exit()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
