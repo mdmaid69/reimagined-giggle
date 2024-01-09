17f204a10496acf911a94f64e7f8a29cc511bd2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:49 +0000

    569enjoy thought activity after mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
