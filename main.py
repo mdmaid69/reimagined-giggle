c37621fae01a4be5dfd17912371460368e9000ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:50:23 +0000

    complete weather make trap breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_to_binary(n):
+        return bin(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
