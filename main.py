1f376098fc501b988bc420d56380dc1d2000a810
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:03 +0000

    after duty height protest pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
