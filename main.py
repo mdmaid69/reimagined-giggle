440a1b8a761a296eefe0558899ce496a3e81649b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:28 +0000

    stage charge last along trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
