b12ea201359522887fd728c068cae2d00ac47ae9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:31 +0000

    occasional have sure false string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
