88bc91d79411ea7e64cd3de5f04b6206f1446d7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:10 +0000

    comment community combine false plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
