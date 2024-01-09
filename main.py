4c0dc10081f5fe860444770b1bb259a09c52a447
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:09:02 +0000

    slip pinch equal test excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
