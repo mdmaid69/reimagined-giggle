91de32c718f1a02b6873b9c9848b07f5801d0b34
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:05:47 +0000

    winter set evidence ago somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
