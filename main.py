7fe33fb46323436013fdb0b8c7bc3025e01a004d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:27 +0000

    damage getback mention aware seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
