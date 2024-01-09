15af0dcc4f8eaa74ed11fc51421da80073d9ea8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:18 +0000

    nerve telephone violence search influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
