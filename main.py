0545975fef1e6506f1a92766897932e34e8b6c42
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:15 +0000

    Nn nice up sharp if

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
