58c64766beb31d402c34ee9a3d40a3accba12e82
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:24 +0000

    talk available thin afford takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
