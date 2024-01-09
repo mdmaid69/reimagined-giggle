0d53f1924b125aba76b2e801633e0cd83e7102c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:26 +0000

    increase service post careless keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
