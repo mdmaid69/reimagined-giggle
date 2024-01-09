35e30cc3e0664314f40593d8f6d2045fd10b265c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:02 +0000

    knock however worried drop process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+def convert_to_octal(n):
+        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
