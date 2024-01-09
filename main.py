dc283ca598e855c1fefe747515adfa59a756b165
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:58 +0000

    hang true describe air content

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
