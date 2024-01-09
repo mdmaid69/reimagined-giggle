6603de846f69c956e5d1472a3d9f9938b182996c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:37 +0000

    goback(to) its now new case

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
