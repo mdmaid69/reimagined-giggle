0319ba59c03cbc8ca8015b9049567ba9b7578729
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:55 +0000

    even firm quick uncomfortable recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
