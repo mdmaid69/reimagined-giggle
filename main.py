2ecb64e0fe4d750b90f335f67a8214a7219fa615
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:22 +0000

    Jj loud body try write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
