b39bd30bb5ace0156227f93d430e81321082bead
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:43 +0000

    season means artificial scare influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
