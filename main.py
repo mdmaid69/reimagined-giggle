f7b816d62bee3649584d4858dcad018b368a8b18
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:54:28 +0000

    give needle sport appear complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_string(s):
+        return s[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
