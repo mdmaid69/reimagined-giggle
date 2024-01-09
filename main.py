2ba1d74cfe502e1743323b56fcc7cfffc27503dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:31 +0000

    history skirt split pay present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def get_current_working_directory():
+        return os.getcwd()
