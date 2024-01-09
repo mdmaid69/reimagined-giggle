fb86cf616b9a7c4ee312da79902fc7585f26f5cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:19 +0000

    tape boil children feature amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
