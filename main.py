d458b68e86e85cd6327161e1c020e0c17883f01e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:40 +0000

    progress meanwhile forget wear my

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
