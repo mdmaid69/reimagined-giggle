87b82656362be1282dcef2cede66d602b6167855
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:58 +0000

    secret reliable respect support poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(numbers):
-        return min(numbers)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
