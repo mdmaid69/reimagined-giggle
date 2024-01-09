0e9cafa050d3e01fc94214b878f9f127d68b0d25
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:56 +0000

    often train fire warn father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_circle(r):
+        return 3.14 * r**2
