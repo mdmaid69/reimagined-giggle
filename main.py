ce1249245cbd3d58c7fe689eb2b48a06d8268f50
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:29 +0000

    after mess colour size if

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
