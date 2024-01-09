76664c2447408622527c71783db827441ac5c9f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:04 +0000

    system temperature claim gift mixture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sys
+print(sys.version)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
