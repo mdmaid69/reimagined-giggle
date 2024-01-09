c859811c6e4a4ed7d16e8c5498989706761f03d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:35 +0000

    fit record work dry event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
