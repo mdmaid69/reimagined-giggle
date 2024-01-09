4558c48574795cacf3488f3f31fc264fd5f27e4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:08 +0000

    thing success sink improve exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
