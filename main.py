adb6911134496cfe222c418caafc49cfceddf674
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:47 +0000

    tend use original separate second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
