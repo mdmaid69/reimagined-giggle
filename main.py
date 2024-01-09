250a7367a257822a08676537eed0ef3ccb7a2fde
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:59 +0000

    standard make justice demand like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
