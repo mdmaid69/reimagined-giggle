7ab120d618af1c9952c92125db6f9fc15c219196
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:25 +0000

    relief print remember while upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
