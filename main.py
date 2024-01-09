cee88afeccf0cba98b3f1701c181540180ac3e7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:08 +0000

    receive shop flat run summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
