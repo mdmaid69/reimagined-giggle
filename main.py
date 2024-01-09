43356c62cf9f84530054e29defc8ea38b8b77480
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:34 +0000

    now shall like circumstance again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
