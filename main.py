c6bdaa4e0dc9d2498237a5957db11b8b4f4c5075
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:42 +0000

    today before debt physical satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
