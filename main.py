b5dc9c0dfa661f366a33358bbb7433d232c8881a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:01 +0000

    call increase give excuse play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
