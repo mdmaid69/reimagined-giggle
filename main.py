98a3ea10e2d5497e2014010b31b18c2b4a4a458f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:27 +0000

    plant replace know heart protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
