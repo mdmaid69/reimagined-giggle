36805654ee7017acef93f905d740acbd6abc61e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:28 +0000

    demonstrate getback band my hour

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_octal(n):
+        return oct(n)
