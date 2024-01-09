958b5a453ff77976b5742de61de0e53ec7bcd8c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:23 +0000

    chase manner increase doubt bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
