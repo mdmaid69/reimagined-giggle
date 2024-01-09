84a5d3b3d0b13fdbdf889cf6ba2b2557685539c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:20 +0000

    remark light whatever breakinto means

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
