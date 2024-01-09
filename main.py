0c75a4c48f2881ab5fe40444921e318a88cb0356
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:57:06 +0000

    hope run name wave breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
