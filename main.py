8ca46ff773efd048a667b31284c98f0b5ec877d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:23 +0000

    570enjoyable comeover play objection path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
