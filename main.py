bd21a31195cade99461a2a787019980dc69101a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:29 +0000

    pour bend air rub next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
