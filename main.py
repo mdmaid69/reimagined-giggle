b64fff429ff9e7b33ef28ae541f6fc3c30e19934
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:07 +0000

    computer arrange juice pump likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
