5522176d9239af31037cc858df73d5eb86137bac
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:49 +0000

    funny law other joke mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
