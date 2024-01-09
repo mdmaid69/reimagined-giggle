f574044cf28fe615b6f07050557b93676a1499ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:31 +0000

    married miss trust breakinto mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def square_number(x):
+        return x**2
