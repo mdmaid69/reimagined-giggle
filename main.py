1c79a751c2fe8e5a913b52b4a5d4da307e8c4ede
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:39:05 +0000

    guilty central friendly increase season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
