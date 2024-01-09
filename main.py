9d63ac90123b1e57ff1f942dac9017dd8b9d191f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:48 +0000

    film division future holdup care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
