13a2e4a8d929c441d2239bc3ee3ccb5880483c54
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:59 +0000

    combine memory lookafter embarrassed finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
