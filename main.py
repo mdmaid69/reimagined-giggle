891798ffeaa12c0e45844ba37e69341e72fbec34
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:53:56 +0000

    child slowly strength underneath unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
