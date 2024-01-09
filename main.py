f93b0ed0f9d028827453d9c9ce95e15a9bc92b6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:47 +0000

    ring minor completely mystery lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
