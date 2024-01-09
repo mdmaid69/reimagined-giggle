5c51860657a73dafb1087e51c211767cf74a273c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:57 +0000

    how connected size city opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
