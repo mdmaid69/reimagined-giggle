04f737a649891049dbbc327bab18d63fbc90e2f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:49 +0000

    lookafter congratulate probably manner off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
