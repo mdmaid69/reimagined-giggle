bbaed6f7668b408d8183bb6fcbf306c512271195
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:05 +0000

    statement shout carry suit rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
