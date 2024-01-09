0f3550b290aa0472bcb309fd914973c137cd70dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:28 +0000

    bread print cut desperate modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
