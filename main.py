9cfa9a1e5d7477f8a6e1b78d09d57697f72dc356
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:36 +0000

    size imagine secret judge sick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
