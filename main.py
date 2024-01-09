49b61300d814b30cbfdca512b51d7b50b10d59da
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:43 +0000

    stop both through way dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
