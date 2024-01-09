d5de92c41a03baf99e8fac32d3bcc6151f657f35
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:25 +0000

    go behind underneath towards usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
