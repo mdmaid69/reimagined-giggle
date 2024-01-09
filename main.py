013eda31eaf500b51e4ef872c5e603a3e3406836
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:24 +0000

    weak sorry obtain takeout damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
