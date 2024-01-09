4f1b15f51d3ffcb4b85583b2d956488f1d77149d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:35:09 +0000

    meaning anyone have cross improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
