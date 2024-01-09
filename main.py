be930e86cdea07f12f0b0b8e15ede29d98b7b382
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:47 +0000

    vision bright sensitive shall correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
