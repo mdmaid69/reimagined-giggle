b3f716fb1379329b51762f28b5b0d7088689ece1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:48 +0000

    thinkthrough afterwards convenient here everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
