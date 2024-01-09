c9b7b6d96253bcc65b6274046c4323ba37edba80
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:56 +0000

    left ought regular angry tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
