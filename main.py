8bec76da702148ca0b78dbe18dee55f4be05bca9
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:39:28 +0000

    conversation frightened far forget collection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
