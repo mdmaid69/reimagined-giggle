908d375ad3472f8375c72dab4501c7d7af73902a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:25 +0000

    key connection company radio sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
