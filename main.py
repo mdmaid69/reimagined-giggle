a231f0ff7d21c314499a57b3469871db042a9ebc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:01 +0000

    callat perhaps switch right rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
