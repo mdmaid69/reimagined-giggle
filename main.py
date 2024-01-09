61b71fcca0c588b1f76ec77bea96ec5ab71d5a98
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:16 +0000

    excuse after field street dear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
