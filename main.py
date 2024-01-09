801907d99f7ee0850a75aeb174e12fccbe4bd281
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:54 +0000

    measure tap them copy workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
