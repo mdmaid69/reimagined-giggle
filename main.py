b20871185ef617c1b808b72e1342f5aca06a8bb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:25 +0000

    argue opposite become follow whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
