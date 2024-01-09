94402743984b18b21e6d4117342686f8cc1ac71c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:04 +0000

    doubt loose snow minimum practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
