b10e3a57660d6d96e51bf7b6a4f4ec6674930c6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:55 +0000

    once badly Tt wind exact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_cosine(x):
+        return math.cos(x)
