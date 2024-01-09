36bae77b4a26f7017f2c2141e29a587cde15acd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:42:12 +0000

    actual runinto present previous dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
