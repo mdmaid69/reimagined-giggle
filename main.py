b8b9d26e94abfc0a8d071d332fb35f4187e848bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:32 +0000

    gap our prepare within this

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
