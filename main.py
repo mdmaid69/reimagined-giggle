693ff4f4821e2a8b0ede323c49c7bc52532adcfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:24 +0000

    terrible frame brush amount compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
