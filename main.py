cbd0380e82031e161bf57fea9ecc51cf235bfadf
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:12 +0000

    town operation school cause progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
