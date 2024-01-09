c7d9738d19575570f457da1ff189bae97b73fd4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:43 +0000

    bend store little keepup slip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
