4afc28463b4068a8358dcebad5a240e98bf1a3d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:22 +0000

    mark hand hot steady equipment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def subtract_numbers(x, y):
+        return x - y
