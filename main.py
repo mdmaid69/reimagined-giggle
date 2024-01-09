c5f92aefe45375bca022eb78643eae86d9a04852
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:30 +0000

    try judge independent single call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import sys
+def print_python_version():
+        print(sys.version)
