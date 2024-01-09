3f8bd9f68d8924477515e3e83c31b5a33372bf22
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:24 +0000

    mail contain straight strict strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
