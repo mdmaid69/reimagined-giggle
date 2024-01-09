8e6372c54a02d229cc088069fdddeeed363e57d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:47 +0000

    claim maybe bank thin her

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
