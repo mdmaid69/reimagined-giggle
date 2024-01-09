1c0cc993ae9417bbb8642765d600da6972fd96f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:32 +0000

    century speak keepup row wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
