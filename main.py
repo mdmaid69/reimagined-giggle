e8aee4f50403786ddc44ccca1394bc1d7b49a6a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:42 +0000

    innocent relative reach getout love

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
