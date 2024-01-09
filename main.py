87000d829480c5c4219b38a7d076ea85c3782dad
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:14 +0000

    measure method brave alive solve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_characters(sentence):
+        return len(sentence)
