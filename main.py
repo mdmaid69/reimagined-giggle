bacdbb6c580009a547a2cf01e62dac91db2806fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:44 +0000

    rob serious rather early save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
