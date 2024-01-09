fd572a32570fd046e2f996a7ff35cd2c4d0d876d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:28:20 +0000

    unite scream man Zz sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_current_directory():
+        return os.getcwd()
