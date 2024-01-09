c3ec7a141de5feedb12a0cb3a46dc5cd745788b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:48 +0000

    ago scene club protest tread

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
