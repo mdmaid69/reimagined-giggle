33d800a113bdd999d9fef2fd56d1a1800ec28c0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:44 +0000

    benefit back replace responsible aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
