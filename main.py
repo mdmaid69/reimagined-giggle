f162bf1b647337b7b39436902f742c5b1ff98471
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:21:00 +0000

    luck shut refer off tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_pressure(force, area):
+        return force / area
