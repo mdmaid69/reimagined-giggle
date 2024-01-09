57e9cd43e5f70f76266487b17cf3000b4a90c59f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:16:05 +0000

    disturb book wide warn effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
