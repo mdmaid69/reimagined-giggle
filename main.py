46c229f1dcbfbeea62588035c634cbf85b5a16b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:36 +0000

    concerning risk tough fire country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-print(math.pi)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
