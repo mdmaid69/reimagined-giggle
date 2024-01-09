083f18cdc59c8793227b83e48c4c0b7a6859f697
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:15 +0000

    Ii inform prepare fun 572enter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
