03e5fff1417901618dcff2766be20e548a8be01e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:13 +0000

    someone visit careful direct trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
