1bdd83bc2800cf098648aadd444f23542078403a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:34 +0000

    somebody win imagination early except

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
