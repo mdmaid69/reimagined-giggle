c1f27ec8104e5e28e67a138be0f749da7fa53144
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:02 +0000

    profit shall both something passenger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def get_array_typecode(array):
-        return array.typecode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
