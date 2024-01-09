73ca5062b310bb97c3a922fbce784b795c1b1c7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:03 +0000

    affect beside during interested giveup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
