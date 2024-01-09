e68831c7a48c9ac1f1fe824da217af0565a78066
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:35 +0000

    when class luck unfortunate amount

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import time
+print(time.time())
