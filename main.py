c6633379b8ade522283b6c04da6c36b1ed71d106
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:58 +0000

    spring experiment comedown crash discover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
