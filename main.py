a2070bd1a4ec5d1194c96d6dc1b2ea380b8b7a94
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:59 +0000

    repeat 566end idea bend support

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
