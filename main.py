2ed223e33c1bad02246a7dc0d9d401df76521714
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:42 +0000

    while pity variety regularly middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
