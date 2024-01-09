6d6a45c04395373da2d22cc1e4cc0204c642a6b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:08 +0000

    proper wonder godown conversation develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
