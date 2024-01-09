6af4c31e2f6d944f0793cd854f954ada4c38c6e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:37:32 +0000

    over big influence indeed spring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_work(force, distance):
+        return force * distance
