12b32bc3512dcfb409c7151c4e989663668d4812
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:12 +0000

    worst declare sudden backwards workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
