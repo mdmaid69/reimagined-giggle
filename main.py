888e101797d05b586ada31caf3cd8100c1b2c7c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:44 +0000

    software come goback(to) supply capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-print(os.getcwd())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
