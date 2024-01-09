a76864b39ba34ffd6c867827fcaf878c5ed3712b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:07 +0000

    delicate container move reply joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
