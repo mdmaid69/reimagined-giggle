5b2c24ec6c74fd8edc9e922030b1dfcb3d364b6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:04 +0000

    season music suckme them skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
