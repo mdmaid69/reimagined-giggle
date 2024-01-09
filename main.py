e454b2295ac486534522e729114e4dd8c48b9523
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:10 +0000

    coffee most music thought whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
