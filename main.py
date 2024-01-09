24c9e461ea95eb20f59504486cfa67851f330aff
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:02 +0000

    hit miss hide base buy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def square_number(x):
+        return x**2
