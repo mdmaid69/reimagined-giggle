2a938f9b424158ca7b4dc6f1f8c2d201b15bbd75
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:06 +0000

    better damp sleep height spoil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
