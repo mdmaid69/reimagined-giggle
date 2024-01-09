e47c42c0ae45f377b9e48d4c4a2aad50f11c6d55
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:36:34 +0000

    will add must comeup passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
