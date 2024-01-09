913e265abcd8ba6b44a9c93f0132e7371344ed72
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:39 +0000

    offend but can wise influence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def multiply_numbers(x, y):
+        return x * y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
