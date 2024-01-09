10fa9bde0656b73b5a5aa26a14044ba015a29941
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:20 +0000

    advise leg today soft decide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
