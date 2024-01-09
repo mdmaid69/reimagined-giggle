37e96e1d429a50bf83496709fd572035ee827983
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:27 +0000

    run Yy activity effort objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
