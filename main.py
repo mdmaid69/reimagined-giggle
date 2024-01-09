cefb738c3ad891c92a0ab245e8c189c407f48b03
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:16 +0000

    entrance level runout watch turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
