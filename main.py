5d68a3a5551ab58a1f4cc21619b165e888be4b16
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:05 +0000

    cruelty shout lesson bend suggestion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_string_from_array(array):
+        return array.tobytes()
