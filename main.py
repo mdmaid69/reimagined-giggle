e02087b026b1dac8efd48ae0e55c97abb12f4395
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:26 +0000

    tomorrow definite view eye conclusion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
