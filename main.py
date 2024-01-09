39aafc3b86ab5218ae0d1fcee050cf01077d1d77
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:34 +0000

    spare still indeed single stand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
