2fef23ebd8c02096fff6af0e2690fac140d94f53
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:56 +0000

    fix forward condition performance mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
