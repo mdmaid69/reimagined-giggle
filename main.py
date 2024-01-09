ff98215e72315d6c36d5706ec24f3a2fcb45c3ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:19 +0000

    impossible cross persuade strict Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
