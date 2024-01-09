95016757bab5e3927b9d7c355572e992f2f1b847
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:19 +0000

    consult easy accordingto pleasure length

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
