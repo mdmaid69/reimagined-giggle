d66c67e35003753e4d7f1049718e8ab4b39952ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:07 +0000

    habit black wrong uniform standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
