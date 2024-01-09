cb62ce217a5371a6c1ee51d4ee7dad79bc13aad6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:54 +0000

    Vv even across top violence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_length(array):
+        return len(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
