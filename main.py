b0e24232ae8043f640ecaf084a61b20c64541f2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:00 +0000

    charge send software discover better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
