c721daaf288f12a3265751d91fcde55d54417caa
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:57 +0000

    drive turnback hard store improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import sys
+print(sys.version)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
