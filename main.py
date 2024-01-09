d03f9cd3632ebfad0915d39c6cfe432a93aff425
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:54 +0000

    far round clever present float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
