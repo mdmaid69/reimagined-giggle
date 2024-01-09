f2b7da76c036eddd98a8f6c842f5dbb3035166fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:24 +0000

    family furniture practise fly familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
