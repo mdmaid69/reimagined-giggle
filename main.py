32214e5482576d41caf608e3eced8d8545ad3fae
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:58 +0000

    hurt demand judge reply total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
