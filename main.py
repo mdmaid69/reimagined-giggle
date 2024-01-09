6cd92c532ebfac013081b1d5ec0f0fa4ff3f9e5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:41 +0000

    describe side warning characteristic before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
