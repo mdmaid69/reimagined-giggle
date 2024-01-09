8278f8a9940f81eb5dd120d7244b6047324b3211
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:07 +0000

    bread error too obvious rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
