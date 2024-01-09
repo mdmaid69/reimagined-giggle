a116fb424a368b9b561e2f4a1bbcf7e0723a99e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:32:20 +0000

    be recover over bin snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
