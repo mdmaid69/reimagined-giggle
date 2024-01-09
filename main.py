e5548c8071b153d20699a600c7eab46e54a39f1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:48 +0000

    goout mean Itself question telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
