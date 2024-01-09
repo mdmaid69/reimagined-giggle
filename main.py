b4a6472a87b56fb141d782690cab2ca624a02dc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:15 +0000

    spot old punishment emphasize great

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
