7f1547e4f9e94a5e0d74e5226f78f3fe21dc6822
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:54:03 +0000

    pleased picture active inquire rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
