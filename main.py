d1f15c659803fe5fc114867b91bdf667cfbb7fb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:22 +0000

    at decision murder blow geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
