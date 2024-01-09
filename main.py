91bb615d69689aa7a46abe852a9e1e7b7bfaecfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:22 +0000

    under owe principle cough accidentally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
