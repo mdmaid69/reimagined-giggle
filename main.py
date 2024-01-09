78f818e83f282f21814311b97fe490b2d5c4c9e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:58:14 +0000

    occasion habit splash crash after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
