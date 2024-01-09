1e37b0bd7f3e2c1437236c99f14645cff8757b5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:40 +0000

    but branch behind opportunity hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
