41ca7f7a78f5e31fbdf2aadd55884c37cfba6e11
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:49 +0000

    range eager keep concentrate squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
