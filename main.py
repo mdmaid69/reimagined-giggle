2323a620468887ec2621c15f92ad1315a707c8b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:48:43 +0000

    possibility check amount make Jj

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
