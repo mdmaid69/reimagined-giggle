bb523c3df049f4756cbc0c747a766c85e9a83b51
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:08 +0000

    cum profit collect guess risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def clear_array(array):
+        array *= 0
