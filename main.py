af4522e42a87d7fbd16fabf33631c0e041d737e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:33:09 +0000

    pool emergency succeed personal outside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+print(sum(range(10)))
+import sys
+def exit_program():
+        sys.exit()
