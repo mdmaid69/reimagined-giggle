cae2a3c6342c86c65f23b617e8b94d144506e68f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:49 +0000

    bar whatever obtain date pinch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
