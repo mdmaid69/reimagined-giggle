90e6321bd03303f91db5d0c8aca7ce2f637efeaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:44 +0000

    switch lonely slip emphasize knowledge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
