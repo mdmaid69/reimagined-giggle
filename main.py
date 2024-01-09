70266c3ddb7bcd2b404980f222e29eff00ed9ee8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:03 +0000

    smooth grade Itself else quit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
