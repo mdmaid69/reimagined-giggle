4f7025dc1809a9621ed7e2a5e286b2904a314b0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:00 +0000

    embarrassed luck near hate straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
