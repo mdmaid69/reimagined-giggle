f2de396d16092a5e432ef660b93db89cf76efef5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:23 +0000

    goodbye centimetre test broad late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
