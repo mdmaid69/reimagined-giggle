fd6230c4e05e67a3c4772b20f30d1da7047a4a97
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:24 +0000

    stupid contribute worst rich suppose

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
