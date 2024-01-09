63b647713745a4da9955e62cdbaef463829394ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:48 +0000

    lie that classroom colour throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
