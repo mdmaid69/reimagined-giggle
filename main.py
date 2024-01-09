66ffaa89d9c0e6a89dc7620f86868b3676394039
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:39 +0000

    commit mass compare still sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
