3bbc1fce77c9262f74f2f9a51659ecb420cf7d8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:14 +0000

    interesting authority movement comeoff disagree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
