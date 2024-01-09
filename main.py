08b3c92b8030567ba2e370ea0127c9fb7a191da0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:42 +0000

    slight marry little instead hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
