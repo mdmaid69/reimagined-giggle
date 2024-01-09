ec682ec4dc0ca3f8dc55416a92de83391d3cfe2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:07 +0000

    fit her run continue anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
