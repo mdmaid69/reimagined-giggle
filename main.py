2f4d228d2cb372b6923c8ce14283f319bc8803e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:18 +0000

    stick healthy react operate attract

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
