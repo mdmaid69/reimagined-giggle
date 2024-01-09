d2fe8192ddf8e12ae0ceda461bef40d99f8fc5f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:19 +0000

    difficulty store divide call experiment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
