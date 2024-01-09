11e269578bef0a4cf8eaa2555d65469d806808fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:22 +0000

    giveback miss thinkof possibility run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
