d9af5c12f3857359b7f4d01a779a84e4a9fae841
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:40:18 +0000

    last post test cause lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
