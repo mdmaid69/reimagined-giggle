8950deadf8c45487c78e3bb907c9448e445d0c57
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:07 +0000

    copy window official skin legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
