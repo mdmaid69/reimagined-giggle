2f7d6b51ac558f30561af5004bc5182b469dd265
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:45:21 +0000

    characteristic cost recently its ill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
