81fc472e29014e66c79da7419b137759ee5d3b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:54 +0000

    past doubt theirs throw seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
