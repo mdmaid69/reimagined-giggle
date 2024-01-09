a10a185d155b8bc1d61a6dffea0b39d15240706f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:16 +0000

    agree measure happy achieve confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
