4c63f57931437482a2bccc6aaa9929cd7ac8b108
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:36 +0000

    total success Hh under bush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
