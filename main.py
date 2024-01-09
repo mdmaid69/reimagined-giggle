b96932c4d0628f2f2ee8115f1aed0ece9176eda6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:12 +0000

    control possibility software mistake land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
