c49349bad861bca147fd948f59edb9bb23d6a403
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:16 +0000

    complete violent plant contain measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def count_words(sentence):
+        return len(sentence.split())
