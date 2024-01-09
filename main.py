7f74f4ea3ada3d0703fa08be10ffdbd4ed3bb7b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:58 +0000

    visit crime in turnup office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
