8176cb1cdb1c2576ea0aed133b1d2e047bae4fbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:52 +0000

    passenger emergency proof or carry

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
