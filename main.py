0617c2f65c664fd4bb72a14c6a49a4d3fa68f3f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:12 +0000

    practical thankyou row long grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
