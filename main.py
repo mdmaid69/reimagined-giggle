f2764002d20aaf63ac55e03cb6a3a9ee77dc3363
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:54:07 +0000

    custom art replace stupid niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
