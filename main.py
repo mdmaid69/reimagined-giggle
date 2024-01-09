829e254ebc8077999f42aae8cfabf3550357a585
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:45 +0000

    comeover considering fast kick everybody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
