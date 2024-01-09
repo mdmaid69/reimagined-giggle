64e27ed2dd8552b28ef6e43abd16460a1c93f5ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:36 +0000

    model wise high dead must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
