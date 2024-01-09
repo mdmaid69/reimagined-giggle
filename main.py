6ddea8d6e6222d341f57b3797740fe7302d72ba6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:24 +0000

    tread minute study owe wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
