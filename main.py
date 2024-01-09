4f80ee2f8b5f23ac676a553ed3943ec77d49e737
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:59:01 +0000

    argue cover hurt want direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
