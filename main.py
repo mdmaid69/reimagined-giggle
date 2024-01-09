02bca3d182a039b69bdcbe1afa85cfe69f719698
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:19 +0000

    pleasant tip replace general honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
