72091d5a5b03e65a71a768a8054a9e028612ba60
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:22 +0000

    attractive top nigga precise weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
