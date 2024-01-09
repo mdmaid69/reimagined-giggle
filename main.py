15cea3cf0c5d44b174c5022ac058a5403409b396
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:32 +0000

    raw appeal ordinary spin unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
