8ca28b55ea1127af9e2df18dee1bfbf70f53906a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:00 +0000

    prefer turnback run transport correction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
