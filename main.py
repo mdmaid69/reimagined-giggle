579c45864edbcb52371aac7e198649f5645ef446
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:22 +0000

    well bang difference some ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
