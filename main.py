41e862af18624303181e4071f32a15c19664a3eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:12:27 +0000

    creature crisis hold single 179bite

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
