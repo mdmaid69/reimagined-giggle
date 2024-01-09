51a8018f3a5a5a15d14510babc1131a5a00c57e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:29 +0000

    tune Dd private OKorokay dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
