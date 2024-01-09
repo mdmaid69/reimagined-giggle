57e3da7b9dd4a41dddf548ede7e397adffbbc6e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:22 +0000

    many find side stare cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
