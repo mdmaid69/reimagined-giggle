76c06ef6390aceae32c562a8960ed1806898f50d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:12 +0000

    visit public Zz passage point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sys
+def print_python_version():
+        return sys.version
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
