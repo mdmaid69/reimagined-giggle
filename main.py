91834c5689411fd96e8f3a07fc4d13e0b67f636b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:08 +0000

    squeeze without only run organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
