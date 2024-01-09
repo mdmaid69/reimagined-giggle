859c24b316f72a9db6b21731aee1358846f171ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:36 +0000

    whoever attempt in experiment foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_string(array):
+        return array.tostring()
