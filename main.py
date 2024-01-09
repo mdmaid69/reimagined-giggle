dbd64bbf36f6507c96126b1f7f1aabf971eb47ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:04 +0000

    turnon walk effect desperate dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_stack():
+        return collections.deque()
