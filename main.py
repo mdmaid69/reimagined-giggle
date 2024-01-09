4570f7e74023404681a29dcd5899e5f4cbbc5448
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:14 +0000

    rid giveaway neither pretty fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+for i in range(5):
+        print(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
