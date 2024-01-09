3179f1c3eae2ec53eae96f5a4c26ecba572ae6b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:58 +0000

    crowded lay quantity month meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
