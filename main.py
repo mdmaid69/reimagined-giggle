881d2d98887d9a58d755699a7f82c315048af3e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:28 +0000

    curious aim poison operate clothes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-print(math.pi)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
