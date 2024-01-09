b5dfdefaf409c591fba5c3de583f69fe2b686142
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:02 +0000

    unpleasant computer 572enter experiment reach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
