ff1ba67aa15465d688726a639f17a9e3a19efa5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:07 +0000

    strict Ii hate aim violent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
