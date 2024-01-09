f17773a7865e46705b8181b69d1939cc8eae60a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:29 +0000

    fail fair today plan idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
