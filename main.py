13752cf2b911688a60c3150f624d1d906c1a9226
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:41 +0000

    stop corner post equal field

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def convert_to_octal(n):
+        return oct(n)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
