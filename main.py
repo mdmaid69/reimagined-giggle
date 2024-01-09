f48ae31ec807c452e099748c0e821ca7ada37f37
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:28 +0000

    gradually what drop advanced into

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
