d10557cd7c9297062992d60c1d2351dda1f72888
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:48 +0000

    education tap contribute turnback maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
