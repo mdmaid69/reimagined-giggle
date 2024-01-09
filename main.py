daab0d1f383606e791242b4622047fc20828b630
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:30:17 +0000

    close giveout not fit deserve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
