67eb734fa3f8fc6832b9d9cfefd4f1e47c2cfb75
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:47 +0000

    beginning confusing rush listen bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
