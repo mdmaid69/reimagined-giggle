0f56fafcdbb92fd26b80c792c5b60e91559da5ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:26 +0000

    Ll centimetre march completely on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
