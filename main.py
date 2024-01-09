dd3803dc35a90015f69167df7fbad59c47c26244
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:14 +0000

    last dry disgusting fair plastic

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
