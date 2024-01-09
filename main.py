5ebbb2ab0a5fcd56a3dc7e71f86c5010cdc1843f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:34:24 +0000

    breathe callup copy degree question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
