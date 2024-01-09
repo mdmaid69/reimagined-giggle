bbe44dce4c0215bc2d408ad8e40754fcafcf09dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:18 +0000

    confused hard connect importance neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
