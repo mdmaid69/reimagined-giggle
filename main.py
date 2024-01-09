e0b25c201b1a77325439ab18db323f238130d835
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:59 +0000

    me form available print reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
