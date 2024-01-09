5b3d29f9eb0e5f4197bda657ebd699ea08b32833
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:47 +0000

    weak wish sport idea agreement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
