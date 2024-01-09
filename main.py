c7e705393b4736e9d704027bf95b88a2c7074124
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:35 +0000

    history smart minimum trip geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-def get_current_time():
-        return time.ctime()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
