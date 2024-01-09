9b74430734e25091783ebe3552ee3d75e8cbffaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:39 +0000

    favour appeal accidental giveup clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
