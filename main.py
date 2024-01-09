2481c788ab554c5f933b1ad3b3387b6c59ed9f7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:45 +0000

    frightened recommend pick space appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
