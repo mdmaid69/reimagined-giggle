f8a2ee130adc95e77224cf1a7f71e920b38d0669
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:10:04 +0000

    take morning fairly expression sad

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
