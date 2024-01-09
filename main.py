7cb853ed3d5420deea64f094d74aef5f861f90d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:36:45 +0000

    escape pinch how keepfrom use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_sine(x):
+        return math.sin(x)
