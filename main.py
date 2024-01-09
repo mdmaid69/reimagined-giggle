7646ead441d38b0739c55ddb413b9ea5b309979b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:09 +0000

    profit bank ours cross heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def remove_duplicates(lst):
+        return list(set(lst))
