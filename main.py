8a8ad76a1a1e442c97babd164422dc1d578abd17
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:15 +0000

    involve copy emphasize never active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
