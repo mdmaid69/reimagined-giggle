171bf577e390f9c710254f04b080d37b49808f7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:04 +0000

    field feel top succeed fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
