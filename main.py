6c9dd462a4504d3b9cdbde51f395c0f1a01f814c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:57 +0000

    effect react raise national compare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
