bc7b70b4bc1dfa06f659fa68c9e341dd21b595ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:12 +0000

    coat minimum finger whichever plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
