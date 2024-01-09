0b2cb3a65a7f7e012ebe54b8ae37d40dc853544f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:37 +0000

    steal importance everywhere knock work

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
