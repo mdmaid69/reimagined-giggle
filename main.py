1bb91593396bc75dd6b6f6119f538261796df433
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:10 +0000

    shame politics justice shadow then

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def subtract_numbers(x, y):
+        return x - y
