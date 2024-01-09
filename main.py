f341054ec93386c6d5e8d4ff9aa04bc133ddb285
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:32:40 +0000

    border trap bang smart expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
