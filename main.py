98c9130430ad379fdaee32a76ed627c9724a53a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:49 +0000

    about press closed record lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
