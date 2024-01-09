aa86f9a863effefeeb914017eac6bc15573998d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:15 +0000

    coffee Vv disappear middle else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_acceleration(speed, time):
+        return speed / time
