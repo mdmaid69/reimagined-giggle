007efb715a3d0d1a5419e5fe1ba1b3bde7c6e8be
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:37 +0000

    will whistle declaration search genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
