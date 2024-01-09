09bebb9b09ab81521d4bbc0e6339b35215703e13
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:26 +0000

    book guilty equal judge environment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def cube_number(x):
-        return x**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def square_number(x):
+        return x**2
