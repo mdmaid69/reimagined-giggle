3a7963be8801a6353edd3e8bd51d5f1ca7cc2f7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:08 +0000

    small suppose trap slow patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
