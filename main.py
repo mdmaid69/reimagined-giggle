82c96b2b2cf871ae6df2fbd38c6d1708470b3e4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:42 +0000

    definite ordinary any book sure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def count_characters(sentence):
-        return len(sentence)
+print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
