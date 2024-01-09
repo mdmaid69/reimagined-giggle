08056a1022e5b82acf909fa8cc4a8d169febf8df
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:55:23 +0000

    difference whoever holdout land wind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
