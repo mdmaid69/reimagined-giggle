f8ad806b68a03f84ac70a1cea9b8fffc7dff0647
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:51:06 +0000

    before present quality strict life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
