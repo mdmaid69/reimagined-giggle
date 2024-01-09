8a5cb572359c68fe600d4fb4840fc87720d3a7cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:26 +0000

    hello absolutely centre watch spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
