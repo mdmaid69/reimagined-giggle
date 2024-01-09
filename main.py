f58a42c2f94c90e2662c8ab7dbf674878a48ccc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:55 +0000

    perform display dress forgive hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
