95d0ed6e5c163d742190fc159e3f4311f0b16694
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:14 +0000

    heat attract draw goup damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
