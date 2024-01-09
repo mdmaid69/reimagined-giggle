f983b27aa6a3808702878f513e50ba814a2994ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:21 +0000

    start length anxious goout speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
