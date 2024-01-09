e51849df0081936e4e01d8721fc8a19e994f52af
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:41 +0000

    cause giveup message change front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
