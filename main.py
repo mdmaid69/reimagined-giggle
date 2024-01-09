b5f9b904491856f26b465b2b952665c6e0a76ab1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:59 +0000

    set Ww treat bringdown experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
