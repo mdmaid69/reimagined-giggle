8df1236777c041b02f1f566a2f40f5274be7f85d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:43 +0000

    honest development rule lose his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
