6ee9f281f12a8c767ea0531241e7f12d4d32e5fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:41:05 +0000

    go disappear mark money brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
