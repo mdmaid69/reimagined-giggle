eb74c706d5d0af28784518ab9221d990ee2ffb7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:50 +0000

    heart Uu mix shoulder top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
