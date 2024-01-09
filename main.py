d1a88917e1ec287e152085e38516393a23dacbed
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:26 +0000

    taste punishment pure learn either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_set(array):
+        return set(array)
