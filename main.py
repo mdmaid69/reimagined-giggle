54dca099f782f55ce85c33d783b307462cd5633d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:06 +0000

    size properly matter finally might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_item(array, i):
+        return array[i]
