7c4d6aa8a4ca3ea949abb37a9dec5bac3fed13cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:44 +0000

    to could line slip style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
