0a7d15fcd705f92b699b16f6fed95c659b56d910
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:41 +0000

    sort petrol allow congratulations holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
