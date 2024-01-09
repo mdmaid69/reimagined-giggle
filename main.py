363da7074ff609e6e747700fc252f7800c6f7b3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:45 +0000

    wing collect call holiday meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
