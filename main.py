be5420707fb312b1a85d852fd52094a2d45a9d23
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:08 +0000

    compare realize second distant rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def is_even(n):
+        return n % 2 == 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
