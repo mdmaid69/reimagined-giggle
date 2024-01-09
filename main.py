9325bf3544ec1778104c475c5a62dea9ea5fcfae
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:54 +0000

    aware butter born key uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def count_elements(lst):
-        return len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
