f5c81b7a62a10cfe69492efe363e693840d477e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:22 +0000

    art software shock family another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
