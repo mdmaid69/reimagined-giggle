39db43da7961e91be233e20ac2897af2d77e34a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:09 +0000

    easy objection shop close subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
