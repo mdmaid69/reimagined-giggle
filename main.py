729c3c6b3b7e95e760992e7c9bd284fd33e53031
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:46 +0000

    ahead division argue watch whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
