bcf254f4dc540832a63b4bec761509157bd2da78
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:32 +0000

    active can yourself compete willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
