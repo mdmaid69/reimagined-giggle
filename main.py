a9b3082b2c5550706f006210e5cfa81525be4fb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:23 +0000

    pollution place address frame practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
