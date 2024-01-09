5713d720b28e15373d36497a70ffed8a4e11bf09
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:23 +0000

    wise separate right whichever office

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
