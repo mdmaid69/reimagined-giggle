c9c9d3aaa8a4ed3174b51fceb683380b27797bd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:40 +0000

    worse duty gentle sink or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
