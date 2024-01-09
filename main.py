9de3c7ce717981c43791547e94113f006c198111
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:39 +0000

    worth chase once noisy yet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
