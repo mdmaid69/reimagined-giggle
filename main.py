1b0e2fa471862e086f3c427b44d716d1e7904770
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:06 +0000

    street form Cc mention runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
