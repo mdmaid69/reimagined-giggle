d06cc0703662db0e37a901995bb540a28c61cf90
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:00:19 +0000

    exhibition cinema lock shallow sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
