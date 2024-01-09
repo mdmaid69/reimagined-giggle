73ff0733439e322cf84c7814604d825bc16adbd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:21 +0000

    discover soil healthy poison different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
