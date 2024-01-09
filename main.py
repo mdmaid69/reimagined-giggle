c854bde855ad66596ae52be1d3a3b9e130eef0dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:11 +0000

    tape 179bite cruel autumn person

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import csv
-def save_csv(data, filename):
+def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
