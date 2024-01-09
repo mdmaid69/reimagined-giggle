9a8c4a53a6f6ed69d53de0e9b9ad608cd4e94f37
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:20 +0000

    downstairs battery criticism communicate ought

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
