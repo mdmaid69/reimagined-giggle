b65491df0fe8bea7a839fe2325bb365304ebff38
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:05 +0000

    embarrassed uncertain front stupid holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
