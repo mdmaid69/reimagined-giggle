6ca29974649c9686e6ba32a48a2675ba63eb29e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:30:20 +0000

    row quarter story hate issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import collections
+def create_priority_queue():
+        return collections.deque()
