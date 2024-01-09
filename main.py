31a62e7bcd56e73616730790003482b0b438bf78
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:48 +0000

    hang drive thin exactly spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
