ff92cf290631b7f64ef6694596277a2fadd66d96
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:15 +0000

    store tap trust proof neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-print(os.getcwd())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
