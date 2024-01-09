8e8ec94195b0f0ec90b7090424135423686cdb74
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:21 +0000

    show brown operation past my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
