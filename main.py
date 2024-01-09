a9ac9ac8e4e963cd0d19c9f5b19807f33dc35085
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:34 +0000

    calloff naturally bridge achievement offence

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
