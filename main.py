a8f2b28606a572fd7b071db06de7f261f7c7e17e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:24 +0000

    climb minimum ever tread contribute

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def cube_number(x):
+        return x**3
