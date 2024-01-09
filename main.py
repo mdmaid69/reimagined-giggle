0c92d2080e49fbebe9b59711be06faa96862a0fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:30 +0000

    letter lost walk comeover maintain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
