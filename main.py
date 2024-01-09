d981d0bb63c52b4be27263940a3a80fd07a2d3b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:29 +0000

    each whichever her eye pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
