2d89828bfe2cd2357684826ba3ffe5d88a119624
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:16 +0000

    fond equal warning chin cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
