c73188b602710dac31f03a1ff9da2b638823d8ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:39 +0000

    drink healthy type no suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+name = "Python"
+print("Hello,", name)
