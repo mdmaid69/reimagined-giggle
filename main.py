aee2c4fa0837e3261a23b1aa15b8a477171a1e2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:27 +0000

    573entertainment takeback feature wide your

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def clear_array(array):
-        array *= 0
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
