6557a90af33cea07204712953015555b5a69668e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:03:44 +0000

    land manage duty crowd yet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
