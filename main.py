2f5f870cec933eeda9f6d5f1885a484f0cd4dd59
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:02 +0000

    back stop train phone matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
