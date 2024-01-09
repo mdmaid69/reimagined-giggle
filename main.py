70d21e89fc081742d6b2b118e412aeedfc220865
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:27 +0000

    callout detail might kill content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
