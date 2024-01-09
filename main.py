084fa5224317fba89328a8d9c276c812a6fbbe3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:44 +0000

    lonely tool 564encourage stir principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
