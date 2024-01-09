f3acc6f893f27d3cd933b1ba884f3c85bbd2d313
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:44 +0000

    concentration far friendly lesson believe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def count_elements(lst):
+        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
