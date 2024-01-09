cbaf264f2acef4e5af8e39dad92c140fa8501d4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:06 +0000

    moral makeup mean common genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
