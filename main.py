0b4656dfaaea60680f5053eaac028dcab149ae1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:32 +0000

    communication surface goback(to) anxious will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
