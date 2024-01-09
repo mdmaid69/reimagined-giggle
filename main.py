bf6fa144d6550ddf31e8ca3bb33c34dd2c7e0d87
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:33 +0000

    centre sentence use form match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
