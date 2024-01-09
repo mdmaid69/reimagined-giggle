9f40a4c000e59c14d2f27d3451092c15b17bd4b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:12:38 +0000

    pile pleasant put reach strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
