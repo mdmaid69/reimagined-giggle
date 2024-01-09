f24619fcabd9ea7e8f9cb22c189b3dad8ecb6a44
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:57 +0000

    hot polite protect anywayanyhow tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
