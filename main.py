535a4a662ef89cacd88850efe2070bf168cb33ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:18 +0000

    survive shrink cheek wish happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
