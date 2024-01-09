cbbbf1500c942bce1e5f6ebbb79db9cf1e2862cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:11 +0000

    prevent explain peace land mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
