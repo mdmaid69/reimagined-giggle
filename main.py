22875d986413f7de2c819a6df1f7f5a2f11583b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:45 +0000

    onto score crowd drop equipment

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def sort_list(lst):
-        return sorted(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
