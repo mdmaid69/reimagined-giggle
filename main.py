cdfd1adf77fa9cba685dba8c08a96c6672c34aba
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:53 +0000

    nurse warn explain Itself point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
