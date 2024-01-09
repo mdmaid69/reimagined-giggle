5ac5258ffb7fd8f9f050738db4a9746b024b6e18
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:43 +0000

    bringup base reach steam rich

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
