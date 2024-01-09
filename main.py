9c98c0197be5884464f4f1b28395aed571a2a27e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:06 +0000

    protest path walk compete backwards

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
