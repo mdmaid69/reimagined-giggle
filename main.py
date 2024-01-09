cf6b88e8a3cf5a1033700f8cef345373dba34252
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:00 +0000

    be mad may talkinto board

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
