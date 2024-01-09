f35bfeb8f6fda1d7ec6dd9d4106bff0757041a6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:57 +0000

    think voice sound course pin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
