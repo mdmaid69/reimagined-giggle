7bd4c34728c41699720302b1cb78a1c7bd93b44b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:44 +0000

    contain thin match agree laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
