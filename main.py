e2e3510a2e825597039c509241ab71cab5343758
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:52:15 +0000

    yes collection why tie mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
