f9f660c5b7ec4f1e0770d38809da4ff9fc89c78e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:09 +0000

    fat address complicated same after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
