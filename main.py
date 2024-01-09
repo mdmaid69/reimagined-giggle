dce59f813eab5ec5a82fd476579672d1d5f057dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:34 +0000

    behave unite Hh die turnout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
