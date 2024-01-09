e08c44ecda7318e33a4d82a4f7daac63e75c2f32
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:16 +0000

    rain before hate cross remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
