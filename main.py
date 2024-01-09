2eeec9e0a0098287d3deda86f9e06ca8f58dab82
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:24:16 +0000

    eye blame fair name achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
