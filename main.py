9e34faf77d3d6a3f7fd9ef44de977854f6cbca25
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:38 +0000

    famous tap question get rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
