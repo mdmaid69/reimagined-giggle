e637d0b62a1c18e1d72d052e53419dc07aba1a1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:37:54 +0000

    cut pass steam somebody innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
