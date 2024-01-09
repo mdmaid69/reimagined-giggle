abe7eef9225c9663800abf21c1fc20659606f454
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:22 +0000

    view convince joke room noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
