9e5fd42f1b1257bf7771e26789897a2f66abd329
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:00:56 +0000

    pot sudden lay pretend road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def count_elements(lst):
-        return len(lst)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
