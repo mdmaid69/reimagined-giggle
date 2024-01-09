7391515eb2e3d1183aaa245c9859b679e1ac3673
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:22 +0000

    legal outside giveback tip challenge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
