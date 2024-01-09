401eec91f58cb292641eb6de944b6b228104fb00
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:59 +0000

    quantity scrape principle government more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
