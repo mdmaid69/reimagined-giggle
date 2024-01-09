3384dbe8bd60dfcacf3a1cce967ceed64471766e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:32 +0000

    569enjoy full effect condition issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
