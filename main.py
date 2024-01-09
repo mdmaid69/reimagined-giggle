275f8035561d897ed412a985a0ad54268e17f8df
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:27 +0000

    attract rain by lock sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
