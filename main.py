fb863f94ce70335b102d3a11c74d06ea010c3c58
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:30 +0000

    murder concentration burst middle on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
