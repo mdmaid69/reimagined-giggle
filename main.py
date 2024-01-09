4ab853f2897f0827bd448e4e157dd09e2b71e68d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:18 +0000

    save belief along single rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def sort_numbers(numbers):
-        return sorted(numbers)
