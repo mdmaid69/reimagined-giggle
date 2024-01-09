34aa82ee3b2105d6747d72225de48cc6ce61932c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:47 +0000

    know flight treat experience voice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
