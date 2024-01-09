09b2a414ed6de88bc4f0c88d39f11c54a18775ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:27 +0000

    subject law break soil steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
