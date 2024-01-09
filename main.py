ae134a22d5bd3611f384c7f16e01f4d49865d319
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:14 +0000

    extraordinary comment quiet little little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
