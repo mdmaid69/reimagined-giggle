79c4de8a2b2aed3e421a64f9e8fb0d54dc04de02
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:35 +0000

    talkoutof weak uncertain react wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
