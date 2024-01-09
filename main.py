171a60aa58a270bdc4d2d41424cd7a8d917800c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:37 +0000

    sick instrument please similar different

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
