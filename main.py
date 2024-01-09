5289fe47dd4c5c14c072225f9dd95638a8c1fc98
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:46 +0000

    road concerned fierce approve absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
