34c6afd72209f271b52e2f1daf2a0042472dd122
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:08 +0000

    because work right ability increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
