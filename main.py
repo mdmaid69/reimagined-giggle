1718a7fae8d080cddce583612db4bb7a9abd14fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:37 +0000

    contact sign hour hurry quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
