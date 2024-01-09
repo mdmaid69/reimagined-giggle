d1ac283afaeac16e7aaecc3aaa7c890afee6d6df
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:19 +0000

    grand desperate giveaway scream tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_int(array):
+        return int(array[0])
