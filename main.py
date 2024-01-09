26b0492a052b185eaa4af26380f0f5bc6f8633fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:42 +0000

    ago suggest 179bite intend Jj

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
