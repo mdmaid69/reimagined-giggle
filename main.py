0cf9905209ac02ee580f80abcf765400b9d0d421
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:53 +0000

    anywhere sister generous weekend club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
