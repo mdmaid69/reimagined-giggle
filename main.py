ad707796e19822ac53e32c8963f5f87982b1219d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:40 +0000

    knowledge comfort organize get blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
