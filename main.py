8d6b9f7c735a5e4768d014246025ad4a9f137e69
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:21 +0000

    sure fix identify total true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
