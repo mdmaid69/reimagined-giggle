82c53c996e21f50e624579a2d05103d95c0a04cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:55 +0000

    admire proper start because guide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
