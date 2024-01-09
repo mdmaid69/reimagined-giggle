a27cd66c14eff4cd286316347cd55daf006c92cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:34 +0000

    happy necessary case thumb safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
