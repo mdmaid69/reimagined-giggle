7867e382fb9a064a62e07f91a312add05d32bb64
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:02 +0000

    party section hate aware smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
