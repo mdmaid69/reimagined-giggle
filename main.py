dc1457e7a1ed887f2acc74d0a312ab88ed445578
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:52 +0000

    idea loud push runinto sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
