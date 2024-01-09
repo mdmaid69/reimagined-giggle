d595144897ec4784a82efe773907cd046ba7e586
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:55 +0000

    proper then represent fence flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
