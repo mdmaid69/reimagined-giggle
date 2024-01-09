7defc2069bdfd784fbbceabfddc16c4f5592b8d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:55 +0000

    drawing world gate appeal Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
