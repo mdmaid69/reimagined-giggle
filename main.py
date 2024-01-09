636b80e1e755c65dfe3e2933cb44aef0403a53fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:38 +0000

    power draw spend argue anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
