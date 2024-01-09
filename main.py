7ede64adcd7362a55ac584c2b85737259a828bc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:29 +0000

    path night schedule job prevent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def reverse_array(array):
+        array.reverse()
