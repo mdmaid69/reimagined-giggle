c1bfb3f5fa1af11c4a52cc7eacfc994835086c0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:22 +0000

    soon foreign loose forgive visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
