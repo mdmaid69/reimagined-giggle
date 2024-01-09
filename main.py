af2dafbb18cebbcaaea63f630e37c58ab4dbf5a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:20 +0000

    club succeed general backwards main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
