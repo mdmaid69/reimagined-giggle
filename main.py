bc711b2fa991d8517736ed287ff1cf0764279b9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:40 +0000

    famous explanation whisper Uu correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
