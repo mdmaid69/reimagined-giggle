5358fcd01ce4de539640f3219bfc30c7f325ff2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:40:39 +0000

    rank zero grateful undo touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
