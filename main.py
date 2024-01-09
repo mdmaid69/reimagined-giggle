63655f76974ee24b2e57dba2b9f140c7846b7436
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:16 +0000

    fortunately height laugh sale ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
