22ca40d3980696defe34464d074f3f6b84725987
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:27 +0000

    patient draw realize sink want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
