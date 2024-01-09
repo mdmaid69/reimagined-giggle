bb13bf6b66c211ba1357a0a98bb871e0dbf8cf64
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:41 +0000

    trousers gooff necessary funny matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
