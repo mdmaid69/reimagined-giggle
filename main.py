d38806b15cc523f485040bde909df8a1d7c557e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:04 +0000

    somewhere exit friend shut trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import platform
+def get_os_info():
+        return platform.uname()
