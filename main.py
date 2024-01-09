bc48bb4effa671a9d2fa8b0400d1d0bef0481528
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:35:39 +0000

    certain spill tragedy passenger false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
