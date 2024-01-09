fbf53a3f7dfbde47e69917cd96d6a5780d886a0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:46 +0000

    bill sink reason run tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
