6cdceb31ad19d5b98374fdaf6be8131d08ec8d29
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:06 +0000

    breakup suckme operate black best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
