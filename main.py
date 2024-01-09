2ac5fea388777f9545fd263f89e24d0aecccbb1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:32:10 +0000

    fairly weigh drop mess trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
