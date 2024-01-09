1dd48509c56e6c8c97ac5d17265665290c3b5648
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:51 +0000

    nigga track new plastic air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
