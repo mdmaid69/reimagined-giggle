9bcdf7018a2534d262bfd4f520e34a04195a8ca2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:31 +0000

    popular section purpose several usual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
