e66bdd271457f3c159e028b3b0a219590bfeb2fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:13 +0000

    car present water right exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
