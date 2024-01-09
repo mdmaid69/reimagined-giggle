be7f78a146fe48a72910b57a6f7ce9097aa6afe0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:19 +0000

    whatever degree nose suit central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
