5ecddae85ecab2a8b6092b3ab5bc4de2956e1c64
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:27 +0000

    workon interview war opposite every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
