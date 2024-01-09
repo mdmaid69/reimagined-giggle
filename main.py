ea5eb6039c714526be919fa4155ff6a23a5c84eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:57 +0000

    emphasize Ll human forgive race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
