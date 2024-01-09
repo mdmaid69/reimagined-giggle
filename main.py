6490167f1b2f7c7d7c1747f019f27c9820e41f3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:23 +0000

    confidence material stop slight people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
