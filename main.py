1486f76fcc073462e081d83338570fa0e9090011
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:47 +0000

    deliver closely building concerned closed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  def is_odd(n):
+        return n % 2 != 0
