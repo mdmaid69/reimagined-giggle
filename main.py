bc64f0c1b687272f18053e119c258236a5560daa
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:37 +0000

    feeling cook splash wherever home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
