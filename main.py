59485a8d5c7b4e82a0176bb5819ed419f210e436
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:16 +0000

    uponon bringdown probably it sort

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
