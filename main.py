dbe75b83b641bfda85f6038c8941a6dda0d44a20
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:45 +0000

    limit damage accidental know makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
