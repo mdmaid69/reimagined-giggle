c331f533d1b7aa3fc7eb4e56ed22120a7d43f3b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:46:02 +0000

    close Ff class interview increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
