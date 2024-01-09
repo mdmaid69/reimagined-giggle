a808afe0cd929c9892f09bb74b96f493ed64b84b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:24 +0000

    know to generous among anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
