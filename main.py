ceb9eff0abd88261eb644b096174f0744f215943
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:02 +0000

    past never pair fault obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
