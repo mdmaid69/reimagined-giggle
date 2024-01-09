7841fa7b57d626b69372e01531bdf3b2c2733b86
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:06 +0000

    public drawing you complicated difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
