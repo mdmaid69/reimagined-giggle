b14206dd6deacd6b21069dd2e29af587b8300d28
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:17 +0000

    value nerve burn begin give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_length(array):
+        return len(array)
