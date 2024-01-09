5e92889ea962d5180527184442b6942df2df03bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:08:30 +0000

    roll proud careless sink clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
