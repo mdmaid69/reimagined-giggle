e5a8c88f0d6bcf520c39095ae493b864b0a3f43b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:38:32 +0000

    somewhere persuade worth nonsense behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
