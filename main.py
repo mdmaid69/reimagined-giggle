3137d0e35041051976ae941c14fab8b9639ae21e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:03 +0000

    correct stamp wealth breakup proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
