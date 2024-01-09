6ad1e4802387266137c6cae36f535cb776483578
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:11 +0000

    subject attractive shout within professional

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
