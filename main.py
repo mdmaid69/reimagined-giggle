69396d311c05c2754b13846d1fc3c2c95389ab8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:18:52 +0000

    just report suddenly mix marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def convert_to_hex(n):
+        return hex(n)
