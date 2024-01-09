1804477e3f778d23f50bf13ee4d89f32bfa95cbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:34 +0000

    cheap mean often breath share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
