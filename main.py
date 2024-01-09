3de3632ebfdb49ff0aaed16e01230ac9cc1d8a60
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:07 +0000

    support modern otherwise list port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
