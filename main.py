9472c62f60500fefda87afecbe2424068dd77b02
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:36 +0000

    public guard runaway back year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
