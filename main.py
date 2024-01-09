b28fab6b11049ab98bee881d827910b80a465a49
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:03 +0000

    win post trick often brush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print(sum(range(10)))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
