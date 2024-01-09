f96ac4bcf9729994dcadde546d74b690dc6949aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:18 +0000

    makeinto boat underneath mine failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
