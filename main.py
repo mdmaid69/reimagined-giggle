9fbe60773d34f8f2bf2a9bc51d72134c9a62f189
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:29:08 +0000

    attitude sound border knock tend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+def calculate_density(mass, volume):
+        return mass / volume
+  def is_odd(n):
+        return n % 2 != 0
