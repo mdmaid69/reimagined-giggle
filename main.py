ae5f20e5ec6c0b8bd76a3688369d3de0102eae7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:25 +0000

    suppose bang along callup confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
