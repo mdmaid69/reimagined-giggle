44b1fd9c587d824f1fc7b3a9d9a075e4b9c84f4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:44:32 +0000

    cover awake impressed room frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
