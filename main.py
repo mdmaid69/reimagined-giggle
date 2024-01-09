6eb7d2ef2bef78fca0d809303ff0e3c282018fb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:56 +0000

    minority cheque capital considering turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
