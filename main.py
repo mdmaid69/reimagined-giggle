653dbd4924feb5dccd7bf7103792cc54b1863c13
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:10 +0000

    rubbish glass exam arm live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
