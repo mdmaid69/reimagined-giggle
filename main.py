31d3565205c2cbe1d1fec090d0402f5e8636d219
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:40:26 +0000

    boil fire match familiar callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
