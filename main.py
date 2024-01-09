d87d6752f7ff39529f063e3404c9a48d1092ee91
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:59 +0000

    software congratulate project treatment table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
