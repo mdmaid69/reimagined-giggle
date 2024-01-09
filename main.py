e943a0810c4ce8b0e6244f6fab65fa6fd6b30f4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:23:09 +0000

    performance anybody knock bringback contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
