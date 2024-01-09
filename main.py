b906167a9764459d56df8e12269af507c2d0461a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:38 +0000

    age marry exchange there bad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
