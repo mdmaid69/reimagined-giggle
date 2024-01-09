96a4fb2cc2489d0c3868c18467b3da1a8fa89255
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:16 +0000

    warm pause stand apply translate

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
