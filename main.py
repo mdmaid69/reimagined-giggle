7a1111cdf963c1d6e70338e3cca0cf1f10a61ca2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:34 +0000

    precise keepout interfere advanced side

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print("Cube numbers:", [x**3 for x in range(n)])
