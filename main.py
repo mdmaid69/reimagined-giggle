126e5d2ebfaab22e09ebd682c272d95c5d01a132
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:35 +0000

    sharp mistake remark record trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def multiply_numbers(x, y):
-        return x * y
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
