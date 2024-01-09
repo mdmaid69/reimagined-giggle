4ef3f38b992a534a2ca94e7645447da15f2b3067
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:36:54 +0000

    uncomfortable violence touch people direction

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+print("Hello, world!")
