8f92632300298eba4f80321746d2cee09c80a6f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:36 +0000

    vision emergency more guide serve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
