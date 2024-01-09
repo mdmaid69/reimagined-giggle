91ed506fd92b095a5b437ce83fe170d413433ecd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:49 +0000

    handle it tonight roll recognize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
