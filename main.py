dedf3f59d4993d5f621339c5bfb2a66d079267ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:54:38 +0000

    floor left present section call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
