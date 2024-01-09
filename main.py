2aea7bf98d9cf3c56dcc5236256583192653f333
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:29 +0000

    generous great catch bread rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
