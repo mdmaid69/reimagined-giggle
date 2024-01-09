c900cf3a65204c7b5a486ed4ab34a00a94b24891
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:06 +0000

    win sorry false stiff always

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
