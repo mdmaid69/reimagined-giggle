0eb771646cb0b8c1ab0296f64bc9a72bbf51fac9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:27 +0000

    regard block shallow later congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
