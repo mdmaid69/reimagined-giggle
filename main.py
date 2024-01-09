3ea3bf219b56fdd700319bff1a81f7f2e16ae0db
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:25 +0000

    snow lean pride base person

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
