7f2de6c8bb54c79176f5992a26ced0ac3601308b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:02 +0000

    skirt profit neither correct naturally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_str(array):
+        return str(array)
