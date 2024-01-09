ddf1cedb751f54ea10b3e9fa65cbb6f4d8141e2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:57 +0000

    contact forever quarter murder 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
