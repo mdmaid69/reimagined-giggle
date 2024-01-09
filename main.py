15ad83aaca103d504c93af16f5381dd137059316
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:14:42 +0000

    lost smile warning sick answer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
