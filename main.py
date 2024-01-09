08eb0a40450f725bcae8cb0bdeecd0eb2d88ed78
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:04 +0000

    doctor crime die water tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
