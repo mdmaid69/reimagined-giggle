7c400d8ed522de0c8e9ee4d01b8904d2c2b8ca55
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:07 +0000

    scheme ball hospital custom once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
