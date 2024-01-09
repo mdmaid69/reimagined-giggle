a9e75b83323aaa0b85b7c1300ec362ffd8daf892
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:41 +0000

    sensible sell suck pretty between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
