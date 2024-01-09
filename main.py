9716c4be679a0d76c148619dffefbf711270c666
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:04 +0000

    forward clever pair remark design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
