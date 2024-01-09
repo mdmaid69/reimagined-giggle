55176e78e3e7d355fcd3d6a8113b29ba45b186c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:48 +0000

    judge against chin long evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
