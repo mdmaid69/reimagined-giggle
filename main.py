3211a6820f558eaa40f5cd1517864ecfc16f8508
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:04 +0000

    money declaration smell pronounce string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
