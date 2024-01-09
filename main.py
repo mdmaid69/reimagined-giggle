e366d6709cd7f7e5392ee75c6afb83b244b69adb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:35:35 +0000

    correct physical comeround bad style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
