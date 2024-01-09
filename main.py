0b6f1f16732dac1820a59a60b57d5a0fae8b10e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:03 +0000

    rather translate needle hit squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def print_python_version():
+        return sys.version
