993b0c07bf7433e7e83ecb5b518dd571ada53cbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:23 +0000

    contain attractive feature warn attitude

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
