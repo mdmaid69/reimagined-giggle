cf097cf854fc58f4a25b564ed70df5bbdc898a89
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:43 +0000

    hurt shall single common eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
