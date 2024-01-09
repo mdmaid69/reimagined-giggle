5ae5f22f25718d835f6aabc9e4667180ff0e2832
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:50 +0000

    moment machine surprise about conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
