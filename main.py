8046b285539db196f1d997eb3b1b43feb138125c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:51 +0000

    hate afraid train hope rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
