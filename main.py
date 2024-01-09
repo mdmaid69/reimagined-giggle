d9084717184c60931db7fda89a05336ed87a762a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:28 +0000

    test praise gather age charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
