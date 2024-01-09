8ebec92e647a3d52c01285b824765aeb600a4f08
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:05 +0000

    phone scream not compare reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
