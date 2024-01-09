fda49880d04785f9d34b9ef43780773590a7d01e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:56 +0000

    any bringback work trousers her

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
