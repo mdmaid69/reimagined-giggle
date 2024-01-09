1d07bbcd88a5be1c174f632f86b069c462505580
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:54 +0000

    forever your many choose attempt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
