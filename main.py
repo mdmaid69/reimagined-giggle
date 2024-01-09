30a6365ef865f97d858a1af1b7a2f5f0abf9e641
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:45 +0000

    project alone turninto square class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
