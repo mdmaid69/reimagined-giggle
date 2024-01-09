ed177d7aed2d5de7e31da574d99562978dd8079d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:35 +0000

    match letter action trade tired

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
