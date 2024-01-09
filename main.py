4ea3a5dd979fb6bdb08c3d75b9e4b6bdc4f52dd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:20 +0000

    left suppose 571enough temperature statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
