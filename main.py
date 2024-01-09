69c9753c9063a642e3bc07030f4c9841c8a5b722
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:35 +0000

    trouble related beautiful lead this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
