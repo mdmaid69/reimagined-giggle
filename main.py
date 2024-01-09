87474ae6f14db4538c8e45aebe8d25b378fd8ceb
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:16 +0000

    past border basically chance trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
