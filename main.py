d8580f6225263d2a8f740257ff9600a6743b03ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:12:56 +0000

    badly blind belong gentle used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
