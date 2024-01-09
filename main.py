5f9de44865b62d49d96b5b10629f167bd6b88f7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:03 +0000

    top sharp shelter sure period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
