155637b31d55ef6f0905f017073cde1d6374b356
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:49 +0000

    well bang state sudden improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
