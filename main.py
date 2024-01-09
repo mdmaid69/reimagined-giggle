6b2de418dfed847b51566175f1c34f9f1576dd1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:33 +0000

    within clearly allow around another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
