288be89f943e5b016701338ee4ab2070ad9910fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:05 +0000

    range comment thing tap circumstance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
