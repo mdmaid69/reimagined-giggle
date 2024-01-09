5fb924b8a2f2691841ecb4526aeab7f9d5d80b7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:36 +0000

    imagination whistle copy right of

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
