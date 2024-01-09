4607d9852ffa037bbec74c0fded66fc474f3b00d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:56 +0000

    reduction allow praise trousers steer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_set(array):
+        return set(array)
