b683c99228e283da28621cab598c40f4f5edeb93
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:14 +0000

    miss past how impressed downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
