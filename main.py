5a412a3128dd46475a43c6a1e6ed41093657f8a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:27 +0000

    light uncomfortable soil introduction market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def remove_duplicates(lst):
+        return list(set(lst))
