af4bbb1e90539a6b4c4d40ab6dc2ba8e33d02192
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:20 +0000

    question divide talkoutof thinkthrough flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
