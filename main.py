ebfac64dcf41bee6b455bd604e09c1b461578bd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:18:16 +0000

    fault official least throw familiar

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
