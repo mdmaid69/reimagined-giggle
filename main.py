3a6645b75dd7ace8c7b921721030a881863b572d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:29 +0000

    well chat call relax expensive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
