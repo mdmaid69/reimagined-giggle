07e1197cb25cbd822b567ea80da8043bb431b303
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:32 +0000

    wake her friendly comfort game

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
