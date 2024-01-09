d8ed422ec1fce728a9e46deb2a857127fb7c404c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:39 +0000

    it even smell wherever total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_int(array):
+        return int(array[0])
