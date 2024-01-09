2f002459d388a705d21daff83e2471fab3570369
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:46 +0000

    rise live noise system last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
