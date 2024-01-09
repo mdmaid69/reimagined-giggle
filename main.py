c26265973dbb784fcf3c93d1ecd968faeca7bd75
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:42 +0000

    cruel if print alone refer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
