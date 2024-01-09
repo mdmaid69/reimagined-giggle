0f6bc4ddc75fb035e57e270a642559c9d2120f37
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:52:29 +0000

    pin pass now fork cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
