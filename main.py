3651a1ccdb3c470cc5d9b0d15d69ec029acee9b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:18 +0000

    habit call its tooth maybe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
