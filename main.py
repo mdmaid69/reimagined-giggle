4163f03570ea7d40baa57d5593d1d00170c0c86c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:50 +0000

    I zero talkoutof rarely word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def multiply_numbers(x, y):
+        return x * y
