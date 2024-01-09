6a64c60ade86480c9a544b5b7df9bde774b6fca0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:51 +0000

    Ww lean expensive class cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
