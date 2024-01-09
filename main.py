fee87f327311021bffe12ee20daba6544e77b3a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:31 +0000

    allow realize send welcome pupil

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
