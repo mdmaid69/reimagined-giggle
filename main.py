4717a29ebe78f41f04a17e8f08c33754a4a1257f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:42 +0000

    radio another genuine character control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_sign(x):
+        return math.copysign(1, x)
