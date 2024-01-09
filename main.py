08eaf8014fa3115eb18c3e43806d4cc5e68c26d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:30 +0000

    bored happen under mad arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
