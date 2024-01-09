ae03d0a963a892ce317067dbd904b39161e56ebb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:41 +0000

    hurt effort break intend throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_float(array):
+        return float(array[0])
