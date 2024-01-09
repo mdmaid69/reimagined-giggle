891a21d333477cf47cd1f2949d4a7f41bca48634
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:09 +0000

    wise thin calmly pleasant unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
