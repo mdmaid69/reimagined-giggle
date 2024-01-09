278c6026f82622305bddc59f97fd597b136a3998
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:18 +0000

    tidy point work live partner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_float(array):
+        return float(array[0])
