fb3e5e81b51c6f53c0b10daa625caed6caa04c81
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:00 +0000

    change increase prepare afternoon fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
