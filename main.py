e113c4e99db92b6bd167e248dc307ed4cc03af4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:01:50 +0000

    want Ee after communication remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
