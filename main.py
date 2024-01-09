601858d0be4dfea9473a1b5ed71b2062ac985fc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:16:42 +0000

    duty takeaway character willing something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
