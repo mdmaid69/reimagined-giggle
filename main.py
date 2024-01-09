3e2bc7915338a22b07d94f0005b34b595401599a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:23 +0000

    distance commit score escape moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
