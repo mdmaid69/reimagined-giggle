3b2e5f600e8809f02cfab5f31b00b5c28f758ed6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:55 +0000

    bet instrument none private keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
