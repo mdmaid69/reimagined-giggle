24063b5216f90438a468646339e1b1e8531e58a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:14:00 +0000

    one scene police between shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
