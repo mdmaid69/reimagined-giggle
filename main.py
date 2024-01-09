8cea171fe650275759af730b7085a4fe040dcf9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:56 +0000

    pity relationship kind slice hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sine(x):
+        return math.sin(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
