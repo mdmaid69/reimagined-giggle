884575cda79759d653fafcc3f6a175d5495017bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:36:38 +0000

    within both tune light hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
