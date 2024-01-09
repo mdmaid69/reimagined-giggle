ddc39cb5ca2bd864c7fa50153bf9d38abd29eba4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:05:50 +0000

    level her interrupt cheek tidy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
