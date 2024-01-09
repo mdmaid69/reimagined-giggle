79d15ba79b22b8a0c60d3750558afdb77fddf3e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:00:07 +0000

    book medium glad free show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
