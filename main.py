0a2d5831d08bb03d84f22848765584d02c7ea8d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:29 +0000

    squash practice catch before thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
