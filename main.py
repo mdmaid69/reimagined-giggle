a7e5fe78c7e0992e82df651ce9b8a28b821d42ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:14 +0000

    include theirs settle above take

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def greet(name):
+        print(f"Hello, {name}!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
