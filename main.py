3b84ffa47a0c1b3ba07172897128cc7345aaecf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:18 +0000

    phone Dd relationship unit exist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
