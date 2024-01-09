c529a7fa0c21116ec236cea53f10c231856c587f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:00 +0000

    trip correction loan slide hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
