fd50e1aebda3075cba8fdb050a879c97493acae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:04 +0000

    afford many attitude convinced unable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-print([x**2 for x in range(10)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
