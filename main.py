433209c4146c36fb269a6afcf024d0dd33c110ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:42:05 +0000

    mention name bill she foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
