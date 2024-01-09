0766e114f43b6a35b1f290f22e6c14dfcb05e604
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:08 +0000

    meeting exciting tidy squeeze remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
