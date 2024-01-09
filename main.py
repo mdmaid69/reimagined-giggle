0009de623ec336cf0161ec852831fcf995745d73
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:20 +0000

    match would born bit free

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-text = "Hello, world!"
-print("Characters:", len(text))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
