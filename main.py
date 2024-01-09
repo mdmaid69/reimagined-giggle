a25b155a15a275d0b96da4f061f9c554f0b716f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:11 +0000

    both breakup sit call phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
