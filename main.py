15f85b666d51a791e7224dce235d9a7c36f3c21a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:51 +0000

    creature butter breakin frightened bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
