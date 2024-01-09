893a1c26aebd9b86f3b7c818c246ee851524f81c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:19 +0000

    relax design string kick clever

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
