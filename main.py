8dad724cc3a55fd6053afd4610f2b7f90f995315
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:30:55 +0000

    569enjoy confident ring production pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_current_directory():
+        return os.getcwd()
+import random
+print(random.randint(0, 100))
