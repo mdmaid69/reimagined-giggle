eeef410d9d1f976aecb97b0c35df8a6beed63d12
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:02:35 +0000

    properly offend terrible plan rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
