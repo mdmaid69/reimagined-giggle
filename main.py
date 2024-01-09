818ceab538e1ce804141c6279a4ec044bc624b16
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:37 +0000

    take offend to considering under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
