2f3b5c93f607f80ad3f3efdd43dedf1e1f9bd23a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:46 +0000

    substance what belong fast fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
