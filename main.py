bd37e79e877e75e5ec237041d63dfd8f3926afa9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:08 +0000

    match pause play speech typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
