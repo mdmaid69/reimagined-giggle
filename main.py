8799f19ca28e7e8ac820240174b1f982d3555ee4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:43 +0000

    injury bill angry contribute occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def is_odd(n):
+        return n % 2 != 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
