56c2a95471233f20541342f6b7d968c8d7fbef54
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:47 +0000

    freedom big respect disturb guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def multiply_numbers(x, y):
+        return x * y
