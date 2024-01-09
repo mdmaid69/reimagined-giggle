3a548fe120c62a2fe6edec212d260b501fb71931
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:40 +0000

    shame sky butter late prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
