b580feee3897a6ffab0f3ca0a74591964ab5e5ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:15:14 +0000

    whether future object rest get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
