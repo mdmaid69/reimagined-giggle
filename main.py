3a2bb518b0c8d483e1012bf540d5cd86dbaacf5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:46 +0000

    treat giveback smile design there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
