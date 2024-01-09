1a8269268c220645a1ec800bf9a54ece90014e07
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:08:18 +0000

    bang piece might turnback bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
