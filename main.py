4171025dc44b110caa2873c877439ed3b8c909bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:00 +0000

    should broad whisper waste circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
