2f91892baf0c69ce58857f3611f80d8ce3d979f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:27 +0000

    message after home score Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
