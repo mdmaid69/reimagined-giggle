ea10b53d48b5543cd8d3b4868b9c14942ef143c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:00 +0000

    tip even recent criticize Dd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
