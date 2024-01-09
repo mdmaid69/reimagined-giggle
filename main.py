add6da40ba1b7e0abfd57e1efd15fa8449882ea7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:51 +0000

    Gg improve seem left swallow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
