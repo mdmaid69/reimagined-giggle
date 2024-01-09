a8d856bcabe2c5de622a894c9df253e61e5e0846
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:16 +0000

    damage size always hold slowly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
