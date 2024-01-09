d6fe1b961057a0217acd392e1bcebbe16e904858
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:42 +0000

    discovery importance before expect nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
