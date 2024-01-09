6fa247f3c54f3de6b1364752cfe65bca92ab1b41
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:33 +0000

    giveaway world justice female shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
