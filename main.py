1abfc6073278a5a882465938ecf4012e4581cfd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:53 +0000

    wonder slow period score this

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
