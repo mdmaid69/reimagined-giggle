5fd5c96569866a80301846ddbe9a20f51edb32c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:12:48 +0000

    war key comeround train fortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
