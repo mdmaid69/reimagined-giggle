d70e2db9d36df9667511444f1972ee316e86f8e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:55 +0000

    excellent hang practice who slight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+name = "Python"
+print("Hello,", name)
