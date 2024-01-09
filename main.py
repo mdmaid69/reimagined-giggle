820867dac38a8812598692156ecbb6c374fa9970
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:11 +0000

    careful so trick coat pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
