6ea4f5db36cfed9e9844b22de6cfdbddbede9c10
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:18:04 +0000

    wish trouble smile paper trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
