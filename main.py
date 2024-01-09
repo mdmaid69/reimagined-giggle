0b9eb15e0b641ba49051173815db3f1c69e476bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:07 +0000

    shine mistake now rush clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
