fc0c57141c6c54f2dd8710ab2070befcd24da79b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:57 +0000

    unpleasant door press stage average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
