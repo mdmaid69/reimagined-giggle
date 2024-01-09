00e9899fbc4c15e76e0a9a229f6d4664721e689d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:25 +0000

    geton shelter official last cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
