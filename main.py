b5c28c07a4d1d7e2838ea5ce31c80e9505ddf63c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:25 +0000

    I straight whom practise inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
