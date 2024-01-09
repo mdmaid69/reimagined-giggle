94e59034bba72ac57185a21129f1cfc805c26840
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:34 +0000

    strike thirsty improve importance off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def reverse_list(lst):
+        return lst[::-1]
