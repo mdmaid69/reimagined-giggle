0eac6f28bd70e9ddc17b3ee352560e2c6d63ba10
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:57 +0000

    almost possession alive soon black

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
