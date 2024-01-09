2e600889865fb2912e59482e86c5db8aedc35a24
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:26 +0000

    just foreign keep profit under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+name = "Python"
+print("Hello,", name)
