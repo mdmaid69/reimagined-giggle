f2edd5fd6ca7c2d77aca5e32c821fe07ef1ac9fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:06 +0000

    straight unit middle patient remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
