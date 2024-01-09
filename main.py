eeae3733c3d46fdabce02c4e07824a76b5fadf71
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:28 +0000

    pause regularly difficulty which there

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print(sum(range(10)))
