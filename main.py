aea716c991df3b3fad0d59d10de20074695e4ce8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:26:30 +0000

    translate that shake zero dance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
