f7e492809ad80a85514afa3ed18e8d52950894b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:45 +0000

    how holdup name cope sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
