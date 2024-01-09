96abaa66f6fd1a6b9a78e864d585162421b59f47
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:16 +0000

    direct speed you finally fortunately

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
