f3e34e925d47fddf25c2b5512ca01566da253b38
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:48:52 +0000

    excellent settle apparently turnoff spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
