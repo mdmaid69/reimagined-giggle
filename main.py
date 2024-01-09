3a8d4245d1525d57290c32504ee33860654e1c98
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:42 +0000

    band able strong why pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
