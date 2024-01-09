323306f57caa24262f495f92538e6ec0dd33d63e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:29 +0000

    bet yourself goback(to) look church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
