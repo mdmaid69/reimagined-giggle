e60ef77dd7cb5a43f560d8860b133eb7af47214f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:48:39 +0000

    tired help rent kick treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
