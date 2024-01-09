6a34b74c8d746ae20359021dd7c4553e4ef6ed2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:03 +0000

    fairly hers exercise free work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
