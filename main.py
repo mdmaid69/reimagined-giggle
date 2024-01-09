cccd633ad2f7dff4249ffad36b6ba36c47aa3a1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:35 +0000

    pool minute goon please lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
