ffd91f928d2709c461d644e010cf0df88c75fae7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:25 +0000

    disturbing bus care tall tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_os_info():
+        return platform.uname()
