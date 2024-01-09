a0c6fb7cacc92061a61b827db2c6e4a5d9360fd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:10 +0000

    rubbish hello walk cut plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
