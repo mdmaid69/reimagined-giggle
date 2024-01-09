44c7d015841dcccf6cdf4f51f19903794b610ead
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:20 +0000

    undo half morning mouth frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
