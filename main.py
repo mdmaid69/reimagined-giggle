553bd8af76fe92c0d6a96662db8c2d85452d318d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:01 +0000

    delicate nonsense it light fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
