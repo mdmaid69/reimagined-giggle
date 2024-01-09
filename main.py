738f6f593cb537bff36a99dbe002bb37f8cb75a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:30 +0000

    previous cut lean human order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
