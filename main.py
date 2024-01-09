26c082d47a41ab5d646c36eee083c4aa6451360a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:14 +0000

    long tape will sound Oo

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
