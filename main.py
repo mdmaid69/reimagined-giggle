90c9de19a8d995388d7536682631efa4f1174988
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:53 +0000

    gap detail tough plant mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
