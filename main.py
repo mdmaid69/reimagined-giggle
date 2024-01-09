2879399ab95a20ffd8998d325553b4d3c86c9256
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:49 +0000

    usually now dear style giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_length(array):
+        return len(array)
