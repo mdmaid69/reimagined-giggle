5d3ec08d01946428cb0acb29f43f1f5324a83353
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:45 +0000

    drive change far lead blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
