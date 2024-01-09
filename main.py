fc113aeea492fbd91bf3f51a8215529413b317d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:08 +0000

    takeback aware mess off hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
