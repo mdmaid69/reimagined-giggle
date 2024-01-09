c1ac6b4703c10efa78ab61575b2566222661a3e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:49 +0000

    stranger anywayanyhow score broken behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
+import collections
+def create_stack():
+        return collections.deque()
