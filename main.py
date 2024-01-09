1b660547a0a28d23acbc916fe3876e477a18a396
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:58 +0000

    bag cause makeup below impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
