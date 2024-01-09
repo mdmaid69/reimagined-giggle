e9c2b5c8b2f9ad597958fd58ea66e5736582b340
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:14 +0000

    formal circle present light body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
