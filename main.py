d6288367f2c4c9cdf962534a3f90d99c692dd672
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:10 +0000

    then present post comein worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
