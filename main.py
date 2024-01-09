76e1ee742b5a8ec741304a4b8289d0d6e78af314
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:18 +0000

    comefrom course tie every generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
