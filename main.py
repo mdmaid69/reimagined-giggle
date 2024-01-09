bb37602fa4fdfb3b555272ff5f6605347e47c7b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:39 +0000

    comeover centre disappear true snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
