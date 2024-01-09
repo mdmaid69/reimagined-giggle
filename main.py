31bb94396989edfd49bb9304ecf0cd861ceb2618
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:36:39 +0000

    mark organization yes yourself this

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
