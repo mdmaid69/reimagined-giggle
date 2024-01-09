cca941edb5780d60a6b46712e8c724f5e50fc14d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:15 +0000

    grand watch mess shall practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
