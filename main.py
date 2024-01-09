a64164a3974926893a9f7e932b0d0dfd9245a3e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:32 +0000

    grade arrangement getoff when disagree

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
