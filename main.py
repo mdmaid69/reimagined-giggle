9c75c2953616339b2dc0ab0990208c12a1440ba3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:55 +0000

    pause onto mind crowd tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
