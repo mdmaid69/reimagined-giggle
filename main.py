b630b7aa59d8ef98e7fd21954e5c5751682234ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:08 +0000

    comeback exist crash waste guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
