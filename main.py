393cdac95d3f01dc4928ddb8c26455f73c156fc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:03 +0000

    influence plain herself very argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_stack():
-        return collections.deque()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
