fb464dde8d01685fc4660231d657248ea8074530
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:45:26 +0000

    find draw last share arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
