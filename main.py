b4538426536c73acb239759b9aea92fb1fc7fab1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:55 +0000

    today problem themselves breakup last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
