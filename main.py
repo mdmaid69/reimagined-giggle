fa3dd2f96588e39f92644e65ccc4e80fad2c86d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:40 +0000

    protect choose answer 179bite prefer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_queue():
+        return collections.deque()
