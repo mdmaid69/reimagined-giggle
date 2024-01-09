05f23d491df5d378afcd33390bb87121c0467698
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:21 +0000

    order sound remain whisper respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
