efcf64f59fde4c3970e6a685ab1f2bb8cb112790
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:23 +0000

    value list solve otherwise dead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
