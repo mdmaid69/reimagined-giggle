e6e9a754ac08e2c914b613e97b9a7836e73c637c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:46 +0000

    interest table well base skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def reverse_array(array):
+        array.reverse()
