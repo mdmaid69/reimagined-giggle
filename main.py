0fb61d9011688e979a0943fda7fe1b332557edbb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:46 +0000

    keepoff gather remove simple far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
