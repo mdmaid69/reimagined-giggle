9ddc7b495a51b806106b602759c8d2d4bdf9e8df
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:46 +0000

    breakout share makeup various leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
