b4a92e984ea8b5a763bc8254595fd1c2f789b1be
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:35 +0000

    argument spot forever expensive effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
