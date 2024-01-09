cd306a0426f8e9a4530b2e57d80d809675a3147a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:40 +0000

    die first seriously thought trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
