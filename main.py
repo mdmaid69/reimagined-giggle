26c130626e233597351eb6b61585ca006dae654c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:51 +0000

    comein around emphasis down statement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def subtract_numbers(x, y):
+        return x - y
