38af1ab43f6cfb2bc219329c16f288622c9f3cac
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:15 +0000

    order be reject 566end over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
