7f15146560dc0973606e4f61759ed7e1965c0b10
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:24:25 +0000

    brief cause really later formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
