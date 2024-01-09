4558cf14d4e880da577fd7655b7296f759d28c21
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:56 +0000

    meet reach on wash surprised

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
