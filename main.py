c261061b4b6ed6df9158cdc6d9b3f2bee5f157bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:39 +0000

    quit inquire gas narrow affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
