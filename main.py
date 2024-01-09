9c3d16f975227d3d87fedc431ae937caa43c8c4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:32 +0000

    check and reduce top pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
