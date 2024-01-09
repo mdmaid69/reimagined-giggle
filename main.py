ae3bb495e097ff9352412c9f8a9e33a6a463a081
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:04 +0000

    practice repair speed turnup worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
