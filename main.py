0439658f178f11301d47dd1d1b2458725ff17368
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:33 +0000

    trick keep belong remove clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
