ae282462f8e7bb525b21a540d85291cf0c8bed03
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:44 +0000

    pity opportunity paper use typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
