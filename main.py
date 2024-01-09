19a362309ce1680849669b44420982ffa1dcb0c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:58 +0000

    chat giveout wave sound lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
