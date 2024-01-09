5697aaa79139303b1467dfbda14f76ee1964269c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:42 +0000

    accidental fine wish declare unable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
