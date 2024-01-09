76498405a8afe5c2dfc5eff03b96dcc9c584cf49
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:30 +0000

    frighten rather careful surprising there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
