d8ffe534ac35120fd9585828a07c5301195265c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:02 +0000

    continue bringdown commit something organize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
