0237fe33a4a666c9a0514ac9fd32faf21f0575c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:33 +0000

    frame could strength their bright

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def greet(name):
+        print(f"Hello, {name}!")
