ef51fc3765be9e380f48f74440e856b393350716
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:49 +0000

    hope share second mass generally

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_odd(n):
-        return n % 2 != 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
