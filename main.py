20e63d8e8e5bd10765c32b469361571ccf1c5471
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:52:27 +0000

    today touch clean among cheat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
