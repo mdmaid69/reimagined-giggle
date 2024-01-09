8d3512b0b2a331cc7dffcd46dfbfd12de34a72fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:06 +0000

    somewhere anyone frame cut until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
