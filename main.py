1722d7a023c562cae625fba8893f1ac5887e394c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:09 +0000

    stop memory trouble frighten matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
