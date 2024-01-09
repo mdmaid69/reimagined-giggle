1ab1a30c64cbfc766fad82509032470b9989b190
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:15:20 +0000

    as Oo type call degree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
