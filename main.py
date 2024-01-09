42e021997d549c830a0869a8e3ca0b48c95ee832
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:56 +0000

    actually size hand increase mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def reverse_list(lst):
+        return lst[::-1]
