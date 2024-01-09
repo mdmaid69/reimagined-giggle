24b173f85a37461d8294c5cd5cd244a71a53aa85
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:18 +0000

    man exchange comfortable single otherwise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
