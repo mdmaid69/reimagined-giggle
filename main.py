c678c1130e30ee1c9b7102b5ee55f9835a4e7039
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:27 +0000

    apart performance slowly form steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+for i in range(5):
+        print(i)
