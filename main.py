06dee6b1d000cd37fc45e546ae31e4ff5cc306dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:59:54 +0000

    right rush stupid especially cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_density(mass, volume):
+        return mass / volume
