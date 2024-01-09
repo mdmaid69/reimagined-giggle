2a552e90d3b2673b7812cd2ff0006404a5af92c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:50 +0000

    talkinto excited line common what

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
