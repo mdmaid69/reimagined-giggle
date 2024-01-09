6c263861db6b80eec5b45cff3b72e2b3a8aac0a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:06 +0000

    birth sell ear spread certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
