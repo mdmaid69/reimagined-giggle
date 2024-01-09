65da290e254d911ce0f59e4767b524ba1bda8b36
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:20 +0000

    discovery improvement wish petrol speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
