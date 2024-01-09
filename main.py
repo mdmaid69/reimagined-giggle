d992bf3882d2fe83c3655c26a227cf17e7fc6bd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:37 +0000

    backwards rate frequent mine watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
