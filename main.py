23a41808c20cc755112186ba6d8a52d35478a77b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:07 +0000

    often suggest design takeover especially

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_queue():
+        return collections.deque()
