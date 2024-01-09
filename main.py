36263dc9283bbf6422dbeb5d450da0853bba726d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:58 +0000

    giveaway already grade especially separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
