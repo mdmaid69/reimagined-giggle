b96bfb821ced8f13fbd22cbb2ab7af30590f4fac
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:59 +0000

    pupil better here basic interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
