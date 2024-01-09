761738f5c89de2bf3835a11862d137360a2d08d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:59 +0000

    dish thinkof violent kill feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
