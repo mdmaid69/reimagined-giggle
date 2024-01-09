76f053ded38e4bccb5f50284e6e605ddf48fa347
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:12:30 +0000

    takeoff danger innocent chance sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
