a25037759bab6446f7d5ea9f30e1e12911df8caa
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:30 +0000

    course equal treat uncomfortable fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
