c8fd05fd0fea0522492f9af7671f622cf6030311
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:11 +0000

    turnover suit 572enter perfect along

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
