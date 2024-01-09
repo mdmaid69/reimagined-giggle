caa031a0355025cc1ae31e1bd0e1522748baa1f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:01 +0000

    Ss familiar dry chase correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
