c90e45e420cfabddcc646f0cf066c6eb377c4de1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:09 +0000

    reply finish drink fortunately report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
