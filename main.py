985e5006d9204ac24a5acff8b94e34325d50dadf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:36 +0000

    hour set tradition birthday operation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
