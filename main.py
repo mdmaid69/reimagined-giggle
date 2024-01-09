01605f3264fd5b0f658aa700dbb7aee67e96ae4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:11 +0000

    year refuse cut Rr past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def convert_to_octal(n):
+        return oct(n)
