9d305e7e8d418b59f342b22a17ae5e06c27f1b6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:49:32 +0000

    similar crisis couple talkinto lookafter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
