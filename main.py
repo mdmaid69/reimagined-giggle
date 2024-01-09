13e714ac8d1c9728736ad88af50041bf3e0e2b5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:03:19 +0000

    car wrap sentence lay constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def is_odd(n):
+        return n % 2 != 0
