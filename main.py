e26bfc49d5f20ef43bcfdf2db635f90c84fc1e49
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:02 +0000

    sure temper dangerous round black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import sys
+def exit_program():
+        sys.exit()
