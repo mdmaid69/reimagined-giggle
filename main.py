d083cbcddca3122c4c35cec7e844de462309b811
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:38 +0000

    frighten bridge formal flat eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
