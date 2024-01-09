48f54eafc68976149597adf90073349581a6992f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:30 +0000

    error friendly them slight protest

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def find_max(lst):
-        return max(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
