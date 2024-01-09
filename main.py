652ff09fd0fc443c3f429bcd0d92ef6a7c3db098
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:08:46 +0000

    careless scream very water party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
