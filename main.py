e847a204ffffd79d106c5093a5727c2a5d6c8bfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:09:18 +0000

    lift story runaway please money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
