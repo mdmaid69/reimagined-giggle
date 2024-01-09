b83d7d36182c93be475e8e2a385224ba94a2e272
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:00 +0000

    dinner ours Ee house point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
