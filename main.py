17b86efa19d1241e26db3104f4973ffc93931442
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:48:13 +0000

    record evening various split debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
