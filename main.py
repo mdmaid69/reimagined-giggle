886b99989ad6616aefce0527d03438073797c3d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:14 +0000

    story butter wet independent greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
