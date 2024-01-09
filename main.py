de844dbcc63fad1d5d276f758eb1ecaeee1ff29e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:13 +0000

    where month smell raw piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_speed(distance, time):
-        return distance / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
