badb19dba064c653650593fd4e6629c5e60c1281
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:13 +0000

    father short memory promise progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
