3d68d9db17259e492823208942fc741996a7ecc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:10 +0000

    fond to forever maximum minute

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def reverse_list(lst):
+        return lst[::-1]
