471df95f908bd9be97a36853ddaa6250c98ad9a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:46 +0000

    compare want name official beneath

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def count_characters(sentence):
-        return len(sentence)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
