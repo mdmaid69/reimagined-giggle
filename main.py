39f4e1c88a291c6b697068512641f6403bfac6f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:45:11 +0000

    then exit opposite Ss callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
