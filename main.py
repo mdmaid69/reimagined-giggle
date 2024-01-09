52f4a9cd3012db217555da309c29b0ad96503bba
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:30 +0000

    hide 570enjoyable ring dance blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
