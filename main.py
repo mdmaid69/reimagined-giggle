d74784bf46d5fba5bf854a296e0adca4401e936f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:20 +0000

    offend blame half drop their

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
