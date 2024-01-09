bbb5049558f355d065b453f27df81ca820516ead
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:56 +0000

    stick unit disagree moment remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
