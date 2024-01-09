b551a1305bf0acc0e22f0d880fea8d3717a9f32f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:13 +0000

    maybe off pin dress soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
