3a026ed2775c50afab1bbd96baedc49cd2fd93b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:05:10 +0000

    appearance what laugh mind male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
