34c918a509d08431fdb4c262108dde84365b3371
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:31:11 +0000

    Cc word tone radio traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
