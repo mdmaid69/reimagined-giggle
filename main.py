d8cc4b7184791a46276db53945a49f39ef94f0d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:37:17 +0000

    difficult us whole direct child

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
