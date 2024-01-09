5fd85b806081f1f900e34b62886927f837cdfa46
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:37:08 +0000

    spray Ee behind guard final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
