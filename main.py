531b3ac90dda13a6a8b19010abd80b41c33b8e20
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:48:53 +0000

    well calm trade rank brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
