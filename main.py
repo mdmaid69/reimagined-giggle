f7675438ff037f036ed83b43a23e8f025b237c75
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:44 +0000

    goback(to) embarrassed behaviour occur eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
