893650dbb02e2ec5105593b8961c489d69152fc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:06 +0000

    street to real exercise action

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
