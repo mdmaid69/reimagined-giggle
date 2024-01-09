dd71af7118a060b487ff91d2358f8453d3e914e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:39 +0000

    load soft nothing check steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
