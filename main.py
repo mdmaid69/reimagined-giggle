cb2519d25e0ac99404552c62f78e67fbfac75aa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:37:14 +0000

    game negative blame minor far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
