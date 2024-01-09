2dd860c040f5d12f036d5a1a798d2150f09d92c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:10 +0000

    rough shock evidence see flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
