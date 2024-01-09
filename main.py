cfca6f77738b23d8fde9e2060381856099fc0e33
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:37 +0000

    serve cover out now piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
