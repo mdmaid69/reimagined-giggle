f9ac74182d0ed8b0b062e62f83965fe6141e39e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:52 +0000

    come you popular rarely violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
