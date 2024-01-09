28b92f26725a1f8414ea2e0b005b7fceb5352a92
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:17:44 +0000

    statement flat job welcome claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
