e6088d03ce19a513b990987166de782b9bafe7c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:37 +0000

    strong world adjective serve command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def convert_to_octal(n):
+        return oct(n)
