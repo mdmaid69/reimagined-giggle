db6a8f5d74bc6a5eead9b76bff18353238627a14
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:54 +0000

    cum either joke bad lesson

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
