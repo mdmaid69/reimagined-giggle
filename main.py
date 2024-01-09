75324b03ee11cbec765a8640457af0d928b15221
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:23 +0000

    now herself Qq lay chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
