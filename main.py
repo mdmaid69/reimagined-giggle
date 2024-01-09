54a9f5cb519de586d0c55edb6f375b1635242b53
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:29 +0000

    close soon tidy pass wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
