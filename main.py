aec0d1d930e9b925543226acfcc423af9c25bff6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:07:15 +0000

    pretty bill depend similar furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
