19145a96a5d46e0f977b06dd98f4e4bca851b508
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:25 +0000

    inform instant chase always which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
