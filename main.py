0ea8890176b9d0d52d396adfaf15c019641ea691
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:17:31 +0000

    another go pocket exactly come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
