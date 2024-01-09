75bf1a67e34278528d634a0028272b48cd5597d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:57 +0000

    cry apologizeapologise respect attempt legal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
