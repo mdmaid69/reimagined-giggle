a08cc955c95973c50978af13d38246d518a6511e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:01:18 +0000

    second comein later mine spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_set(array):
+        return set(array)
