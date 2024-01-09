d4b842e5cc5c4ed77d34b00313050dad4ae30d8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:21 +0000

    shame blow dance oil repeat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import platform
+def get_python_version():
+        return platform.python_version()
