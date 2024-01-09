2100a89d35776e30654d84be2c7f54a72204be8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:22 +0000

    turnoff himself perfect somewhere different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
