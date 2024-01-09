59b906f0fe6decb09624bdfb2c533f6dd830424b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:45 +0000

    contain year win doctor show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
