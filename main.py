ad58e274c2e799b702df920b070d472b8df10ea9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:02 +0000

    correction recognize law mine but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
