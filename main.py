42c1f59387a77f2e5cadabaaaa554d18277ee292
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:58 +0000

    thanks bad shake deep bye

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
