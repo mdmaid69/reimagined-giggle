b201efd7c3ea2748933f954917fb0615750616fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:39 +0000

    female speak dish tour country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
