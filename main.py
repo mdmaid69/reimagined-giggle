ea4747869da7a4ccf11f6af927749cc423af286b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:29 +0000

    comment second excited great shout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
