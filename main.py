9a5e9347e3d8f83f60d4cdaba7b6e4e17c59dc45
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:45 +0000

    shopping dress wise makeup knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+for i in range(5):
+        print(i)
