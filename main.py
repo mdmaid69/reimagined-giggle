e57ab67713943c2c600790f6db275d433efa91cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:11 +0000

    everybody exaggerate justice delight equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
