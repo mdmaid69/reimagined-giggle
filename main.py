3e41b927794ae1aa8963768753e22fa8a8cba1a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:30 +0000

    Zz hot feel beat flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
