aee06d9e17dd26f448ada168579f6a2de066c602
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:28:26 +0000

    everything address Vv idea comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
