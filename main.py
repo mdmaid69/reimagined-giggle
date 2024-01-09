65e18eb7dd58cb0994b35aabf82306627ad1397f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:05 +0000

    record wave of back shop

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def reverse_string(s):
+        return s[::-1]
