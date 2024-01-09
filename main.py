5f88725ba69699389b2373108104d26db7d87df3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:36 +0000

    suit steam plastic mark serve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
