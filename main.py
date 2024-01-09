c26bc7665236c8af50fe6b1f8b27d7923af21f17
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:18 +0000

    build object count rain else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
