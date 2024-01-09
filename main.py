01787a29e4e43754256044f5377c3ef951bc5a12
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:05:35 +0000

    half comment list care continue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_power(work, time):
+        return work / time
+def divide_numbers(x, y):
+        return x / y
