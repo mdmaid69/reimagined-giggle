be32e685a481fecbf0164bf4f6a96edac4bddfc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:28 +0000

    call proud wonder fierce period

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_array_buffer_info(array):
+        return array.buffer_info()
