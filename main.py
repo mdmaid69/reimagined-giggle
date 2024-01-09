818b80c66628d8a917e7bc50f529bc3e4f8c4d5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:36 +0000

    sting measure strong broad cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
