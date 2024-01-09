8e3ac7fa331908ee1cbcbbb424508332a8f97c56
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:37:55 +0000

    noisy left lead vote exhibition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
