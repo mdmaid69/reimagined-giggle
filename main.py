0edab29d3e32c6b91e5f296403b5d14fbc29422c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:11 +0000

    destroy yourself dry squash judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
