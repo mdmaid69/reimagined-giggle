41c102a1611a861800f9ed498ccaceb27fc024e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:12 +0000

    depend respect example walk badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
