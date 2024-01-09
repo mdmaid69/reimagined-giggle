8f672207012f811f637febabb39d997cbbbea319
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:08 +0000

    replace cruel spread straight damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
