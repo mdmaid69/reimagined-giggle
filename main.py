bbe9d725293dab871260193a9a30e5e0ee463a10
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:41 +0000

    name closed guess accommodation unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
