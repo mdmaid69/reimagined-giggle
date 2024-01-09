77ef773e025dd8b8cbf034b043abae88fe30deac
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:02 +0000

    owe stare wave sometimes suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
