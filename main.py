566acdedfe7a40377eb1686f3f4f166c995a0d3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:38 +0000

    market sweep mess estimate sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
