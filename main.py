12cfaa88c8a81762e5fe69434acd5f61e183f773
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:25 +0000

    should value long need see

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-from collections import Counter
-print(Counter("hello world"))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
