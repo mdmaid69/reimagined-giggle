f7bb97fbfece78c3367f2e78767072b3ad6fb319
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:47 +0000

    meanwhile rain type schedule tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
