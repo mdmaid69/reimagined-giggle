8d45bfbfecbbb5602e72f8852605afc3ad9c02f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:46 +0000

    bar cruel other motion damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
