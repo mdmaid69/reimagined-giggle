c6cf34320d23de863580715d90ebe7fc00947b45
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:05 +0000

    relax so pin bridge create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
