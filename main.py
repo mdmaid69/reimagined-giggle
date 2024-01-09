0a3ea41aea5604f5d88096345cf6928f1c1fac72
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:08:13 +0000

    certain break glass trap seat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
