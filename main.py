c1ce0dec8f1c3ed30a712b3f69bda30ebd1344c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:10 +0000

    problem regret surface getoff skirt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
