d0985079819bd43b78f802f89172291ec02c64d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:26 +0000

    realize block sign determined far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def square_number(x):
+        return x**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
