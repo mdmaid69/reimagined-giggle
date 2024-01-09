ec5de4ab9cc1ccb63dac61a03c453cc305f339b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:23 +0000

    however lead forgive wish present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
