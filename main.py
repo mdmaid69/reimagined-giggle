922a927fe745f5738cdaf181d91f65354b29551b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:41 +0000

    interest trip meet sound runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
