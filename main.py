337682a5160d88537adccce93e34d65e7ac7db88
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:20 +0000

    length bet month safety talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
