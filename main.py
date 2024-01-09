2d7274fdc909fe9e6267ef959a14872cd913ca1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:10 +0000

    centre neither gradually somehow use

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def reverse_string(s):
-        return s[::-1]
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
