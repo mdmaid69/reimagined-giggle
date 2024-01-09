9bdd35572fdf36e65fdd6eda185c8fe6f8b95149
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:11 +0000

    turn angry because smooth judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
