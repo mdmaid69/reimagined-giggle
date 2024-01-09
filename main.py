ae34bb28260510ef2313dbc9c0c87b33bc1d5ee7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:29 +0000

    trust terrible unlikely raise imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
