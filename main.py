271930e6e65d83b4d776e0ec8c6ec3623e5fdc08
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:12 +0000

    somehow keepup unfortunate 179bite cheap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
