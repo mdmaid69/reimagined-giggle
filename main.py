518402735ece83d20ecbbddad62ae64016b353b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:30:10 +0000

    basically open trick definite pump

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
