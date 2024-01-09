8308b9fb11c22dc3adc7565b8ba0a7c498ce3e34
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:36 +0000

    Ll people recognize want dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
