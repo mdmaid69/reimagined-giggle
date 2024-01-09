526e3cde961e3118a4d288d5ca32320bb573cdb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:02 +0000

    rely attitude down woman mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
