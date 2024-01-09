3b5aaaa5086a82cdb7d54e368ce43a542cc4eab0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:42 +0000

    whether mine subject lonely generous

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
