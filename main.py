ab420cee797f1f402d4e6860490bd91314da1c52
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:45 +0000

    loose influence we risk tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
