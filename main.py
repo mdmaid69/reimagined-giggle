2f1464c30b67eba7212fd383d172d14aed4b96d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:49 +0000

    ashamed bite schedule makeinto call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_hex(n):
-        return hex(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
