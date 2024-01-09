05e7cd7cf4d068c9d2236821d758de9279b80d16
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:29 +0000

    reply majority dry border comeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
