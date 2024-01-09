4ebe6a7eeac6bd77e63a034495c0f10dd153be27
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:20 +0000

    concern opportunity tell early seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def sort_list(lst):
+        return sorted(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
