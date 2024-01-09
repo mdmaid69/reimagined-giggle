df3e482ce00adf7277b6b3a8c969b262b004e4bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:46:21 +0000

    roll become to several hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
