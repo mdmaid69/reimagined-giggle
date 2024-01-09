e940c70768669e01c1cd93ede30e1d4eab7a6374
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:51 +0000

    curious anywayanyhow way party fond

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
