7823d879625e3fe82b3dae220ac517c747b82788
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:06 +0000

    noise dick relax hurry desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_index(array, item):
+        return array.index(item)
