3622acd8f801d02c68b6237117f4843405162d67
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:57 +0000

    replace joke curious street now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
