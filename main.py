1463312e28edf5c4898323ec94219e48881b32ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:24 +0000

    dress talkoutof taste spend before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
