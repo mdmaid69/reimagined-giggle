0ca7620849dfa9cd2cd2f1b35f08f03825081d59
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:43 +0000

    steep oil comeout mind somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
