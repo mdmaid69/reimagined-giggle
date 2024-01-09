9501ab723b1b1c958fd7218f0ef6b9ab71296b5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:50 +0000

    getoff score bringdown short crash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_str(array):
+        return str(array)
