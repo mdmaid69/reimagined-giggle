5a82d96ee1410c631cc41ad2b09871a89f945d7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:58 +0000

    this touch cruel phone smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
