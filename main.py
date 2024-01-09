59666edf684a31e2bc62aba7033684c751db9357
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:01 +0000

    breakin breakthrough slightly connection downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+print(time.time())
