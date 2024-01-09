f5002969bbe81ed66ecd43cb55378df37a7bc2b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:24 +0000

    low can vanish create callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
