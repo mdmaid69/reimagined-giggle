6b6cf5da369c686bd38d8be673b0cfbe0ffe047e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:50 +0000

    excited left precise usually work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
