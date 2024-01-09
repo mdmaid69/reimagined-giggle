f827dab5518bed7c1c80b4dfa484ef09e58f94ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:48:45 +0000

    568energy finish appearance somebody neck

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_array_to_bytes(array):
+        return array.tobytes()
