13f92b57e81f6c5f2e56b76c1925e0197e7e86ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:05 +0000

    loan tradition slice across Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
