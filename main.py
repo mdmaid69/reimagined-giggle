c2f3588950e5bfbbaa2e3d728ebb9c14357b1f08
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:19 +0000

    under design concern punish gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
