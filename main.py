95ea0bb10fc119de76170b01d87987c4b9824837
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:33 +0000

    ought age comeout play where

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
