f97591b4e2d49cc4a54de5f04081579ea21587bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:31 +0000

    really book cut together ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
