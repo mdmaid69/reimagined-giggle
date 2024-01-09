39f7d548c22f1be22d1fcb338bfb88ad7ee44ee2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:02 +0000

    hair pipe separate stupid proud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
