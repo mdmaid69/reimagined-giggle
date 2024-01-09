cbc38e68f3cde2d7dcc5c464c906ecbbce6086ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:19 +0000

    frightened boat over authority mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
