87c55ef3b9b07e36cf93e28f46273c879fc28be5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:34 +0000

    confident hope apply opponent adjective

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
+def multiply_numbers(x, y):
+        return x * y
