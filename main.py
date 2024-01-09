268ccaea7683ac507f30864297c12be5a6fbc34b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:42 +0000

    thin simple law straight hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
