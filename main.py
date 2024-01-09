3a79c2cc23c228c1a132798d720f8ab2f795ff00
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:41 +0000

    getback shoulder his mix gooff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
