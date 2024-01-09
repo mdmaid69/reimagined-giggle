c932df0d746e6c10cfbc9f641211d5775100fe5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:13 +0000

    fail every onto manner which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
