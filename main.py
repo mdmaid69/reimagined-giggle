71172c11b002d8ab465d5419ebcbf1ec45fe8481
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:48 +0000

    turninto dead free operation pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
