7f457424e9defac43f5bcbe635ca9353d05516cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:01 +0000

    praise they tip bang progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
