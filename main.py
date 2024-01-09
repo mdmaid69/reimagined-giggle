ee9756180e7affd0ad0329fa4a06fa650f3b8e8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:37 +0000

    change favourite closed present willing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_as_int(array):
+        return int(array[0])
