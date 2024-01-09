66b507b30bbbdd3508a31d1e7792183c7cfa7a35
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:04:01 +0000

    left muscle event turnup compare

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
