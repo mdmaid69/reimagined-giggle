0abc80a1c77774e357cf1bfa5ab24e51db90cb8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:55 +0000

    gift describe serve boil variety

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
