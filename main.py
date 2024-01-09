b9823048cf9616e49bdf021bf827730c1a39ab79
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:04 +0000

    short whole laugh bet year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
