b0013fbe4a1177bcbf62be09ab8e304e62fa6bd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:04 +0000

    action dark trap crash then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
