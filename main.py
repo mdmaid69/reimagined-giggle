dd76e13c9c6af0a1dfa85e469b5320949b8b66e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:26 +0000

    satisfied in lead marry teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
