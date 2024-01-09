296c4884eb58de8e8e9a38a281c51ea70456e1fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:35 +0000

    autumn health poison up miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
