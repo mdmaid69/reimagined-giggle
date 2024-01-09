695c03a33024fead294e5d14859d0547f10e006b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:36 +0000

    upstairs certain long project runaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
