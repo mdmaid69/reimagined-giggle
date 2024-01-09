8729d60c2b6c70188c128fe88a38fb00b5d27303
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:10:09 +0000

    ability become trust street corner

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
