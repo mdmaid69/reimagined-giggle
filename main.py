e77f5aeb683f4a6059d6cb1a3423aa392070abbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:42 +0000

    complain simple wind press term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
