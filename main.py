561faeac3d66a39eb83806285bc5a6aa02812b91
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:51 +0000

    creature certain boil hollow so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
