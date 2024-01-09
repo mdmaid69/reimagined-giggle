0b51035b4182a7a4eab31d01424cf0cb657e09c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:36 +0000

    birth sad zero bringforward have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
