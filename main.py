39b494acb15284282f228528b9e4b33185f9cd4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:33 +0000

    down hurt must control 566end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
