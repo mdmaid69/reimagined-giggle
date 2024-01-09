29c3c0455fe02273effa17dbd4d6bccd4aa8359e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:02 +0000

    choose push become call rubbish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
