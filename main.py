c72b9fbd527838540ce4e1c3f1125aa623a42d14
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:38 +0000

    full feeling keepfrom welcome state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
