88edadc4ae115a9baf1579baf7e16a84c0950e48
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:09 +0000

    careless half towards bit ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def reverse_list(lst):
+        return lst[::-1]
