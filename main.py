ad8af414e7f7d2187ad45f722982a4b0e555aa36
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:26 +0000

    must dream poor wish process

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
