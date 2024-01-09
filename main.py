c5f4e79c3b76eee979045aec2458b7b261a28e17
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:49 +0000

    quantity move opposite practice most

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
