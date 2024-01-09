4188b23fd889051480834022ef77c731b377e434
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:22 +0000

    keepoff powerful never experiment read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
