bb01eaafd18a12ac69cd45e3eafcfbcce048d8da
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:57 +0000

    music confident runinto exploration niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
