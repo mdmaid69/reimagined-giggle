ae74fcbc6a48898fc04917a745ff9cfaeecd4c77
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:40 +0000

    equipment remain run win post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
