09c032c97ad6948711ae4e72ce9b7253c15fe48e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:38 +0000

    congratulate firm rely disturbing off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
