f64e6b022f9c0fac70f0fc67b80f3a2c6c06f33a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:11 +0000

    thirsty exist step urgent mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
