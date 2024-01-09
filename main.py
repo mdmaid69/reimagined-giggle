26247671799e178f4c906305bc5a510c3898dda7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:50 +0000

    split area exist official ball

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def square_number(x):
-        return x**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
