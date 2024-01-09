601cd44bcbd896bbf1422cc1c8711ffabebccae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:05 +0000

    event attractive sharp card divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
