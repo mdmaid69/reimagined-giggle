8b02a935efe0eb4f119c9ffbe352fde26c9cfc46
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:45 +0000

    last grateful right mouth branch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
