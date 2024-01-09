f3d5d71573e546b37afecf56a0aaebf2c2449428
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:17 +0000

    shrink tape mystery sport flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
