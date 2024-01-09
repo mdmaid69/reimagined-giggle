63fb647a5b2babede4bb7683ac5b3117932fc32a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:42 +0000

    shine play protest waste want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
