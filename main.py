f0584027a721dedf5bbda1fba02aa0759dd5c35e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:46 +0000

    even interruption niggaacademy welcome close

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
