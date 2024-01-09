937b1fe1cd387c5e9895687a1a23aa0dc8dd30bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:40 +0000

    indeed connect piece transport pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
