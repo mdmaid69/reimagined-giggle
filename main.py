b6d513bb5e4a7033dd0e63c13eb217b5adb4d967
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:22 +0000

    shake result adjective brain part

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
