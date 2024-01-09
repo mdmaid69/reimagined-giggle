744ce6af0fc84b0c27ece9896ea2d76bf6260759
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:06 +0000

    keepout our downstairs path now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+name = "Python"
+print("Hello,", name)
