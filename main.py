ce9234325c522af8459f5f67f18ae92451d4b091
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:49 +0000

    matter kind point Gg gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
