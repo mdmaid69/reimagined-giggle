a7b88ed877026bfb0824bc6007562abd85018233
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:49:12 +0000

    necessary top left talkoutof pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
