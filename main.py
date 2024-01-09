828d9139d2ddeeae429e683b5d48e97e7cdb10fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:50:12 +0000

    bear brain sting conduct acceptable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
