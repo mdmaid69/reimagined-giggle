5c91dcbc4fb6a91cf5d292d52d3c194c31fdaca2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:44 +0000

    furniture pretend cheat hurt coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
