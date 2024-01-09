a753a4b01764411ca5fb1e216f746e1bc5b1447f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:16 +0000

    cut short own listen pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_work(force, distance):
+        return force * distance
