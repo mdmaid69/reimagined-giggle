76bbc838d16665d9529aa4133bb25bbbb862e16e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:35 +0000

    tired couple makeinto gap but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
