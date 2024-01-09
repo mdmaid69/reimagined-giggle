35cfd9d1978da2de73c6579e3ec44cfa9619edf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:05 +0000

    reply answer 572enter light bend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
