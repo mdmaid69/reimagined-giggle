be5dc03f8e45d8d04bf40046d66181318cdec962
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:34 +0000

    practice consist border smell wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
