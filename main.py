f2eff94e1eb1c1765a48faceb7bfc8e05a149579
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:05:42 +0000

    trap finish excited final conversation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
