2de75596ea1870e7c7742c86c450e54907aaee4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:44 +0000

    independent there language move claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
