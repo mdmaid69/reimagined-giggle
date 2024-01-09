ae21cc7f977537fcdefeffda87af7da285c5bc48
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:37 +0000

    lunch neither track though worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
