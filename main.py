ed56c64a21bb6f0fffee0e6970a3ae1712f365d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:45:24 +0000

    free underneath communication tip under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
