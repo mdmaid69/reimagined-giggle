736360cbf9002b980a3db9f28bee7e47ba53110a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:57 +0000

    before so pay past have

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
