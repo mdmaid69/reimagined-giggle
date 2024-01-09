c0142e1a4aad11c257f2fa0a7be0d53279f79f04
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:27 +0000

    block separate married touch ticket

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
