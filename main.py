649399865af3c318c28932c2e0a578e37fd15652
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:16:09 +0000

    there though blind character nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
