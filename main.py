d26629307c992e14d33c000ba3967dcbc6d53854
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:50 +0000

    giveaway left tone to improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_speed(distance, time):
+        return distance / time
