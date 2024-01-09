bc788c9ee443d327bb9fe41491789211b6162bf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:43 +0000

    fairly mark Hh fault tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-print(math.pi)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
