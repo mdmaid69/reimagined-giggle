0f2e63e9b949138ba328b04a206b6447944e3529
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:19:49 +0000

    genuine impressive direct act recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
