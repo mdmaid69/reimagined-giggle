6069da71b38e650f00d6f5b7e50a13a6670f8c11
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:15:14 +0000

    bear talkoutof rarely spill approve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
