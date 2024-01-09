5a8cd1438b4649d4f9090afe927839e4465e64c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:09 +0000

    nearly breakthrough small aim light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
