34a6a7a75c09c7ee2be9a078cdd1ab0af289936a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:30 +0000

    real squeeze far mess bringup

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
