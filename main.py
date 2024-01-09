9b8f4f5f9203bfc61e6b7ee884dc4eb8595a32b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:20 +0000

    onto invite piece majority operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
