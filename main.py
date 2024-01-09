7797c43d02ab2eaabcc992087abe3d271cbbbe7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:37 +0000

    black direction interview contrast foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
