3974e742f3ea873bd69305e23248bc0d86d3fec4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:03 +0000

    use use after convincing opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
