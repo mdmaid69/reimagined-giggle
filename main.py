f0468640053ff314a4f1d880b8db02d204936f6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:22 +0000

    organize step pool simple influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
