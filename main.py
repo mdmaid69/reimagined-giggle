8fb70d232b4b4e5668bb9e15e76211be3f2688e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:03 +0000

    occasion really exhibition benefit level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def sort_list(lst):
+        return sorted(lst)
