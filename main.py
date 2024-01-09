3338938adf2b97ca86d052973f5f7a6c64f0c19b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:37 +0000

    off shrink relationship foot surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
