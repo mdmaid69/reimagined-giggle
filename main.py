c232e6ed231ced5691a8e1238e1e50ba7f6bc57e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:05 +0000

    develop dress position design for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
