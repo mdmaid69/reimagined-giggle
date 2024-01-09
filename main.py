61eaeaf3ed9a64fe212ebeecbc01ddfe268b246c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:17 +0000

    prepare influence race tape radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import getpass
+def get_username():
+        return getpass.getuser()
