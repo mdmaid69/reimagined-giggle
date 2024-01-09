6c47eb1bc4c0dd80f6de538ffa5ad850e4eea5a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:25:42 +0000

    decide welcome find safe properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
