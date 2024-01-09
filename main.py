4d0d3b689a564d4b4bad55261b98016afafcc3e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:02 +0000

    afternoon interest rather die perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_user_list():
+        return collections.UserList()
