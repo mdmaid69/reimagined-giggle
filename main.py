8d9e618c14b4971e56feaf4b15ad767fa3c63db9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:43 +0000

    mine rubbish say lean believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
