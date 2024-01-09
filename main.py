c49a6ba25f78b2e37600b33cf22c25e0eb672bf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:25 +0000

    shout birthday side round unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
