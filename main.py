9ba7a00bc0ea77d3876f57ea9ce99a34435ac10b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:30 +0000

    judge try priority pretty increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
