439fed546fd7271c986fc7ff517eba4b72e86831
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:12 +0000

    achieve confidence drop takeback without

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
 def get_array_as_tuple(array):
         return tuple(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
