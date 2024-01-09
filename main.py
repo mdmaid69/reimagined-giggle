5bab078d11a780f5c9c569c0b3d8273195f2e980
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:32 +0000

    mean stick natural game into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import os
 def remove_directory(path):
         os.rmdir(path)
-def find_min(numbers):
-        return min(numbers)
