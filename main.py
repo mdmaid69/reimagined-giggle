87ab7fd8e89e5671552c8da65c4746c71ef0e537
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:29 +0000

    below our secret Ss test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
