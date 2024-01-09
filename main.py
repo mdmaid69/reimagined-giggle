8a42950a5d678c1141635f09381d5d8e9bd8c378
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:31 +0000

    hurt reply receive fail big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
