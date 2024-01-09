907c46287df1f437d3c51d8567a0de6c0c53fcd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:41 +0000

    get involve seriously keepup can

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
