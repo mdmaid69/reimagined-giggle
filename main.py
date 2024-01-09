127a69b379b2b62c059bcdbf78e27e9822df3537
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:06 +0000

    its experience Ii class 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
