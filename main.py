b754daa538e0fde5e9283e96241bf498e1b0df13
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:10 +0000

    crime show mad strike large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
