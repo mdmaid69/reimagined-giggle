2be0a8d21dc15cfb85cb305af9580759301bf672
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:27 +0000

    sentence practical experiment plate weak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
