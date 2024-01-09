3f8d1aa8d36f695899bc09023920a35aa4d71aab
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:10 +0000

    point opportunity comedown remember dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
