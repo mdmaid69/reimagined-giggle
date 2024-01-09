b27a2022005d2e6efe67ed6d79eca851c4784918
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:11:53 +0000

    disapprove buy inquire cross giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
