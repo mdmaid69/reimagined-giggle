376f4e3185ef03627072dd5d0241cb0d2c273b16
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:55 +0000

    couple scheme behind argument behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
