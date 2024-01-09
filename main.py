ea13e0096300830e6bc47433ed1f38340e7bde33
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:53 +0000

    become now crime goaway storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
