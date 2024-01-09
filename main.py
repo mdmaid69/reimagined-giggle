8171ebe8d244706c775d6de51a7f3e983eebecef
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:03 +0000

    fine poor operate occur whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
