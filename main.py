bc1da00a3f5007343079140dd8151840aaac0971
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:03:51 +0000

    circle congratulate content you squash

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+print(sum(range(10)))
