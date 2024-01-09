62e1ed1a66a922fa7c9b8327c3d2340c428b65c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:46 +0000

    hungry blind neck drive long

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
