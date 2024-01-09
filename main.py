70bd38369d0722058f050bc23e01452c8b9f0b2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:54 +0000

    565end chase result rob brown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+print(sys.version)
