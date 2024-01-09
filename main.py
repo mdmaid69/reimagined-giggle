75afe44b9ee073c4e926d2d7e5ca289ef1beb9dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:05:13 +0000

    organize trick compete season total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
