ea73cadcd23e41b991645d19d3754e5bcb06a860
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:51 +0000

    answer authority 566end shrink miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
