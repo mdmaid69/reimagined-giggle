116e6c75959c2dc2dbd738035e0a6098b0d66540
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:33 +0000

    patient fortunately everything so exhibition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
