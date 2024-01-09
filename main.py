b78a36d83a230f3cbecb9a90e99c70ef776a9962
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:56:44 +0000

    switch today produce embarrassing force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
