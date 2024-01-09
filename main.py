e94be36036e0b9c599f92c3bda672fc05e8ff9a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:27 +0000

    bringback brother turnoff unite careless

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
