867684829c2135e748d8000dc608e78aa577ebcc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:18:45 +0000

    key your exchange but convenient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
