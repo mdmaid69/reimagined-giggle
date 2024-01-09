f21e7201559476920c2cd7ad67547ae440b70a40
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:02 +0000

    stiff challenge Jj slip hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
