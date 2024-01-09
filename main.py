8723885662e451912b9d99ef55b1e39be38df548
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:44 +0000

    order phone stick birthday true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
