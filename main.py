bf794306ab5f5e3716c34eac112dc93d7cd07a5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:01 +0000

    surface 567enemy loan to set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
