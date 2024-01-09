77036f0fd116ead10081d35d907dd66d16ebe589
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:02 +0000

    suck reduce deep deal married

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
