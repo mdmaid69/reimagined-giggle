2e594f2821441e2b026bcb6eb7fca7cdd2090246
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:35 +0000

    fire anyone afterwards march praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
