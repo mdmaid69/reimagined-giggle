1f466628c2766f4845fa1c804a8b70fd9d168762
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:27 +0000

    post brave tradition deep noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
