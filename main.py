d7932a653e68d5ad584b26f39d3200e804ab94f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:50 +0000

    statement sing task there win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
