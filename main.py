20ad060781e2a0b7e9c72b1cd80845f97ea5547c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:43 +0000

    heavy today shout regional beneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
