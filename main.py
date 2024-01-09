d0ed83d315a2f264f83741391c40d84f6ca21bd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:51 +0000

    unit appeal remark naturally OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
