9aa25db03ca190de3f40a9bf37eaf78bdae70742
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:26:13 +0000

    lucky suggestion mouth collection file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+print(time.time())
