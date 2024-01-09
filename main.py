f6d9c8c4ed6a4b57dadc927f017918db1fad6dee
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:27 +0000

    whisper grow level difficult fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
