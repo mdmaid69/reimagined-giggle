38e2adb7c97866aabb176e7a2724e3d7911d258c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:00 +0000

    scale shade generous innocent rely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
