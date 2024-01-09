107fe6135c88df9bcc1f154d7180ba6366f01f74
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:34 +0000

    whole trick yes represent table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
