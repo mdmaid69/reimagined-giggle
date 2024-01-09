988d24c785045bd395a62f754a91612f57194522
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:12 +0000

    confident shape second prove again

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
