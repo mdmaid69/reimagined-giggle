5c96d497fdce73d6880ce3398a989c111082972b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:54:40 +0000

    turnover continue next towards show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
