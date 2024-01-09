5ea5f3133fdaa4e63072a6475b6cf2bc7137bf13
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:55 +0000

    geton lock safe letter run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
