2ed9d7e2123b40849f17b74d74a3b763c804b665
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:14:07 +0000

    light settle partner thick right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
