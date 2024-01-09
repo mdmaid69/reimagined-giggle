6faa8f3443f8a6f4abd03ee07db0e240eb8b0ede
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:45 +0000

    wealth proud identify hollow time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
