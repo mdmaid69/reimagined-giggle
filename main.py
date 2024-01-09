aaedf9c4a5add9a67dc390e1a6ef87ec241250cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:35 +0000

    silent Pp offer pleased after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
