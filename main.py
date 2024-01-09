7273567e343850d1444e98895efc378e18a778bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:25 +0000

    taste waste out condition slowly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
