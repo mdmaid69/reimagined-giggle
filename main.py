f2a1ec26679af7b2ef09e19eef43be490e76d343
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:43 +0000

    reaction turnup back yours view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
