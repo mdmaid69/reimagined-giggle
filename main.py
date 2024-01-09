78eba1c2f7bad05364d85e5c00ef2f2ebfc9bb04
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:30:14 +0000

    murder wealth where into night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def add_numbers(a, b):
+        return a + b
