f3ea2a3f09fb9a536ce6235a862ba1a94ced7e9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:42:11 +0000

    Ww especially getout noise innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
