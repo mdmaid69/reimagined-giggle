fbe6195d6ba533ac8fc87e76c9375a45d6b076fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:03 +0000

    hospital waste surprise average deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
