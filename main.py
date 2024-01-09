cb3783a46390be2607f0218f173d9b1c43f00af9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:39 +0000

    bang short visit become in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
