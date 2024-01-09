3f1d75b63045aa25478461ec90185e6c80c4e494
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:41 +0000

    true thumb write throw consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
