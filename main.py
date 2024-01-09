221cc44b67ef41dcf45393e43fc0d59efacfac47
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:02 +0000

    opportunity of entrance powerful laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
