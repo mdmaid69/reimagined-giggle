cf41901eb65ef0555ad539f322b62c6351470911
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:36 +0000

    machine surprising quit example threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
