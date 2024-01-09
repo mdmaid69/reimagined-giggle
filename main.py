a535f76298eae2a77081e903d6869e86f057efc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:39 +0000

    introduction factory regular nor lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
