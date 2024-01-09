7b87a296091c1182965ea6db48cf58bb7c5e4f8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:05 +0000

    feed protect 566end live colour

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
