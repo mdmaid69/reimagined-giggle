c57933dc86fbf863ea118110c319bacebfead1da
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:18 +0000

    equipment that improve present bright

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 import re
-def find_pattern(pattern, string):
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
