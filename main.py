d8e02f51920a6a2d4d858c285219576abb4524d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:53 +0000

    claim rain glass taste point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
