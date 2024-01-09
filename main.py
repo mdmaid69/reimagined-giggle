c5987928bbd15bee8ca8d0a902c3e1746a1d586d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:03 +0000

    fail difference like unlikely furniture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
