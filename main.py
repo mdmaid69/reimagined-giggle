2fff8f31f83d2e18580f539fd0439ad79ec9c0f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:58 +0000

    delicate damage foot dress nice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
