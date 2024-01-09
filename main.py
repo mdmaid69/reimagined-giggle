605b1515d449adc9753b7de1d87b3db982ef58be
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:37 +0000

    rely woman ocean legal picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
