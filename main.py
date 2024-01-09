fa4d8bd330a780756c576ee3fea6a73cc4f96c49
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:24 +0000

    common exaggerate patient book priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
