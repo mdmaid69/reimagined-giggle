5bfc3f456ebbd075c2252eba5b9f020f44f74510
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:55 +0000

    delicate could hand punish habit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+print([x**2 for x in range(10)])
