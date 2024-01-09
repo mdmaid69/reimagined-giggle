8cf1959dba97881572b9863c6bf4c09e20b64a69
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:31 +0000

    appeal style actually false survive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
