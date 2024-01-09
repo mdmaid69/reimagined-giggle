14b7ceaa1ba932b20ce7004198979d4c236de734
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:18 +0000

    telephone catch strong offer through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
