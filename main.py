3e7070e819c8880ca4cba1d64eda3c0f830bf391
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:28 +0000

    solve appearance generous fashion street

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
