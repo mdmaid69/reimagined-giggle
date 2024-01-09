2d8ade4907793016cc542eceba8cd060247daed4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:39 +0000

    quick contain preparation possibility please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
