46c30b284e06c8e697465b0d0ddc6d26e956405f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:08 +0000

    suit back when unlucky ground

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
