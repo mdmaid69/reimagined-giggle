eb4f21fd19f5bd3fd1118ec8a361e984f3328c4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:48 +0000

    bringforward moment OKorokay post maximum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
