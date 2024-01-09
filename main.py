3ae010d8f0999a262dab1f4808ab860cf9cbf7b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:47 +0000

    furniture bush fact always behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
