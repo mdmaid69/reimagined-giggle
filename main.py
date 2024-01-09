06d865bc5258739576fd06d9090445305e143ae2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:01 +0000

    reserve nearly invite thinkof used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
