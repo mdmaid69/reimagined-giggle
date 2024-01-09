84dde91e4ff67a8388fa2a28037bbed0d7c1c817
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:53 +0000

    boat bank communication raw regard

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def greet(name):
+        print(f"Hello, {name}!")
