072284642496b0f4dda09d1e90ab438c06356fa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:44:07 +0000

    familiar both nearly positive scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
