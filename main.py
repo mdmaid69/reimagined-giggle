73216b6e5411094a0bc68fc7d036d19e53bd3cf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:07 +0000

    organization fence central thirsty better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
