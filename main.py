a48169a5182e336f8f599d09cfdc4850e6e1a18d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:56 +0000

    exactly finally ocean influence 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
