87e19b42d4a9036925c5d6c865714a178710b012
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:56 +0000

    question cause surprised shout mend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
