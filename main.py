32d00b7f6e8a24b5dd1967d545c3725e520ff728
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:24:12 +0000

    centimetre likely inform valuable neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
