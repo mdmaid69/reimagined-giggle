222b649526d20e680de590fe00e7b66912242c86
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:48 +0000

    limit shall real group pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
