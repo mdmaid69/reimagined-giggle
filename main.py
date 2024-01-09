1fee41c28885c3e044ecb35d784b59557a680c90
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:04:09 +0000

    include identify earth rarely couple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def create_stack():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
