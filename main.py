4cc4c9917ef41bb3c541d57def71a84fe56d1598
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:56 +0000

    necessary surprising fact defeat pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
