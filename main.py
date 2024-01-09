3be42d84372187af94e9d6088c2a92b4933b744a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:20 +0000

    nonsense language reply habit tear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_numbers(numbers):
+        return sorted(numbers)
