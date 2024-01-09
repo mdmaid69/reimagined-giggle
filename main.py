10ffad14d242fb351bdb623709154baa7cef06d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:17 +0000

    beside way promise steal best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
