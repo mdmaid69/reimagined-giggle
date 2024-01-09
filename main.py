336d84d7627fa31d3cd43d79282b87c3a6fc9a7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:17 +0000

    middle Zz bar injury step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
