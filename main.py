c84fbe1a1c88dfc8691af58ccc382e099795c876
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:24:40 +0000

    rush serve please pinch recommend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
