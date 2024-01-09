70d52e98a782e1b058852e73a8a589175415e12f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:10:19 +0000

    invitation chat point hard plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
