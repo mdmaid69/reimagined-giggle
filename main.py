bc33a0287968e04b2a93b958a37e09614443fcff
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:36:47 +0000

    neat culture steer though extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
