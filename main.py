cc6aca391011cd0d2cd9ef422f0f0c40d989a348
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:42 +0000

    shall life hope justice forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
