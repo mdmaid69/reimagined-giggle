b2c3903037548a9898f7dfbf81d0fb74bae54b06
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:28 +0000

    please copy special buy twist

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
