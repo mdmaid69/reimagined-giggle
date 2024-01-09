f1509bcee8b00137062f7fafc4f16d04151a121e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:49 +0000

    centre lead scene approve spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
