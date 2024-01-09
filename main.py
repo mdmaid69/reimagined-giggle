555073bb606fec06636c3232278290fc4a494e69
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:06 +0000

    repair equal steam attractive hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
+import getpass
+def get_username():
+        return getpass.getuser()
