2eae0167eef72d3ed1a514fb8711481e6517aee3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:41 +0000

    there broken vanish accidental stick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
