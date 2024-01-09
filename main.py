7cfd9563d5296abd46bea9432d95414cbc65b2f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:15 +0000

    tooth ago understand surface sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
