2e0583c42464f30a12a0cda80ac79a6c4b738e36
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:47 +0000

    objection exist flow complain show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def create_priority_queue():
+        return collections.deque()
