bd11f4f34301f50f48752a444d9bf54046804000
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:21 +0000

    clear high whether ahead shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
