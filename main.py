5ea87efb76eb0f16bfc9bb2bf17259fb1c1c91d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:27 +0000

    cruel insist conversation notice impatient

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
