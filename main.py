511322780621c8dd9e98d34684886ada2ffb08d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:51:08 +0000

    hers body case deliberately run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_length(array):
+        return len(array)
