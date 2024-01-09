ca9cbb9a7ce1681490aca46f0f91c323be9f4537
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:53 +0000

    exist perhaps welcome help into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_power(work, time):
-        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
