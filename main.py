310cbfa9166af6c0efe73825746214af64a84bbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:33:01 +0000

    achieve mixture cost survive lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
