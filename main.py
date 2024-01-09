7c682d77d184d2b9ce302f54b8eb892c3e16d396
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:13 +0000

    represent willing band work please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
