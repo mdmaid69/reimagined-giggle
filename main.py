7ee5e09682e1ba272875eeab8c763e2b486ac834
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:23:58 +0000

    lost ground forgive wander system

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
