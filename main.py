05d44286455d60f75561e175c5e5c9ac79b70dbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:05 +0000

    neat concerned number hospital relationship

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
