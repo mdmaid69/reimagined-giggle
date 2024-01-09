2185f5e13493236069b4672808424a606ff9cf95
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:32:55 +0000

    heavy already illness thumb off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
