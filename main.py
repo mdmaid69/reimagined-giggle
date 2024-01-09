2130dfe0561ccc180dbb2b44cdd74ceba98d7f58
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:39 +0000

    score production interesting play firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
