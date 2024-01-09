5de3cdd3b3c4c6e30aca7de017057638efb7b61a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:43 +0000

    culture sweep lose possibility study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
