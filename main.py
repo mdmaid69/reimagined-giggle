bda13c1916c8538aa294615114b65e69e9b34eda
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:22:54 +0000

    cruel shoulder comment actually argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
