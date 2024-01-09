13dbaffcd77f7d7186b42aee31dba2c4a42c7f4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:48:02 +0000

    before inquire past occur us

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
