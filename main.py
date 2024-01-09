b1f9ce7fd494a9e5ec89661722f5979f8363aaac
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:16 +0000

    anybody deal hardly hope shop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_work(force, distance):
+        return force * distance
