f61dd581b1de7c75ace8db4f711cb15fc6f4a413
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:44:35 +0000

    tradition so mine support apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
