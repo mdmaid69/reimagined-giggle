625246143d54a0eefd8e1d0cbec336f2cc9df5c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:26 +0000

    better anxious ride impression gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
