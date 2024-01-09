699f995153fa648a1c62e68f64ea22199df92c7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:28 +0000

    exam breakfast influence enthusiastic under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
