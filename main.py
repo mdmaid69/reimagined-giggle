199cdf877b2332555adcd239bd8d773bad5bb24e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:56:31 +0000

    confuse alone quality broad hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
