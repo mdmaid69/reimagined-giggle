0120143ebe4e3b432da762fde7268c6d85dd0e73
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:01:29 +0000

    slightly bang pipe exhibition invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
