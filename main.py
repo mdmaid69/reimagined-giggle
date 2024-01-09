50920133a3651afdc39a0ced10508d7e6fbb37a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:23 +0000

    influence classroom giveout good value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
