e6518dafbda43de4ea4772fe7a8bdb44bd00867f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:50 +0000

    abroad my fast soak should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
