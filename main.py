09c93e586d84ef2903fbb0625675b3768e471aad
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:56 +0000

    should cry rubbish rather shade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
