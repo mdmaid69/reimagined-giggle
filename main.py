5061343d95611a2af0fb4dee3bae047be4ac1321
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:23:48 +0000

    upstairs top gap shoot trap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
