e3851e7ec5c459923b44d0ca5b6dd316d01da936
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:32 +0000

    glass spread silent forever smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
