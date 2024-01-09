040377bd4a560517c2a56eaaefb9242f264ceda9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:05:55 +0000

    behind comeround exhibition married mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
