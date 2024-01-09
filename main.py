d160c04ab678c44078c0948e8b707ba165d0bbb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:44 +0000

    later bang half eager brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
