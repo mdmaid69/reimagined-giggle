01c7d765be0446f59658ed3bf3fa77cae0a78094
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:11:47 +0000

    under regular everyone breakthrough demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def is_palindrome(s):
-        return s == s[::-1]
+  def is_even(n):
+        return n % 2 == 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
