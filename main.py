7888e70496ec6aaad16da85e45ba13e814604705
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:06 +0000

    charge bang taste doubt furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
