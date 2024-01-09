1fe350f0ea0e5cd22ccdac32af4f0e9d7391ea7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:38:12 +0000

    our treatment nigga road knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
