f1ec776c649951228e8e7ca8a847121428e79aa3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:08 +0000

    crisis bringup than sudden she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
