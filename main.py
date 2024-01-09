8ec3c8ebea26f02533aa1696d182c8cb49f75a95
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:44 +0000

    meeting bit course hope bang

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
