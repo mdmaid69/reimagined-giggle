299c2befdc77cbde27b124594f976b4e6d445d0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:10 +0000

    familiar alive unfair bend judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
