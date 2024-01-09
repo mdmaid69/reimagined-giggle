7bca86fde58329b5ef3340b562ae036c92569c4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:49:57 +0000

    comeround lend word factory thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
