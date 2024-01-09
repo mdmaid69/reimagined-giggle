fc8c56a271b277c29f7b1bc76a819740126eec7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:43 +0000

    perfect actually light world autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
