2d61384a987386a385844646cfc29a16525a0a27
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:42 +0000

    shoot recognize back stage sound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
