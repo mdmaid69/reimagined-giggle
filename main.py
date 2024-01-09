cde07edb53158afed4833252ddf95732dbcb4820
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:30:32 +0000

    home lookup deliver tough everywhere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
