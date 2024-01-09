c7b0fc0aeba0c226cf43d8869f2b0a8e56e9d29f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:12:39 +0000

    trick afraid joke ordinary drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
