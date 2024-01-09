8e1e118552b6f9002b121db2618786a43999022f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:11 +0000

    muscle doubt mainly principle excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
