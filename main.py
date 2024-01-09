fa26d8c68536073e9aa5c7bfb1a8cb5155db601c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:07 +0000

    I free only pay promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
