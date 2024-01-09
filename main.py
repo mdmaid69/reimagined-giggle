1d7277ce6d3b8c2d4a53cf2c0409464b6912699e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:04 +0000

    stop street complete declare distance

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
