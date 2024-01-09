e5ea25fda7ab8f36ea17d00195485e0a83a6b7be
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:49 +0000

    exchange Hh bank face foot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
