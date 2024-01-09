1aeaeae36904c9c08ff63547425ee94b71d66102
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:50 +0000

    country chase cry bottom few

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
