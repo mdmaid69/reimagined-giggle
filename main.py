769219ad493a8043325244a28261a60d92858a8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:24 +0000

    move accident interview medium scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
