b23bf92b8bdda315c2c161d648ee459fe98be48d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:06 +0000

    use get spare Jj search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
