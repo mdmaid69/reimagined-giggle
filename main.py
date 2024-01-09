af065f35e0824c4e80e9e21354d360c1fa6d3df3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:46 +0000

    throughout take influence tomorrow responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
