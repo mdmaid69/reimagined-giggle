9c672e0fccc184396777239bb1aa031d9a8ccc6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:20:55 +0000

    however sick which fix address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-text = "Hello, world!"
-print("Characters:", len(text))
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
