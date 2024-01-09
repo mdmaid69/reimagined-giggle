fc744a83201945d82624845a56dd3595ca54ab18
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:15 +0000

    eat recently development getout circumstance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
