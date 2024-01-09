36df1a42de6781fea966e4986adf3eae3ea7d427
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:55 +0000

    attitude reply dress switch trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
