b82e762390c6c9ac0704b66175d1dbd8da72d231
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:45 +0000

    exhibition free memory visit below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
