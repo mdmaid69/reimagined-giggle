1f05e6096d1df2a46034a53b3f7143237313545c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:35 +0000

    factory service side sensitive world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
