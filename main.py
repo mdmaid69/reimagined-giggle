a1697fb37947edc6b71eb9a647a2b826b1db647d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:43 +0000

    act bringdown earn taste towards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
