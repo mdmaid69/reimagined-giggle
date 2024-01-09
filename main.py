747c2814be756253c24037e42353e10459454a76
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:31 +0000

    weather choose live pity plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def count_elements(lst):
-        return len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def is_odd(n):
+        return n % 2 != 0
