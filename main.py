238bb03b927f8ba88bbe24f8124b7d0a7ecb3ce3
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:52:31 +0000

    from bringup rarely large clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
