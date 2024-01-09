6473591b177654cee208271c9623ca64396714c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:25:18 +0000

    too possession when container rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
