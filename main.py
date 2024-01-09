5fb6fe488fcdddae7a1f13a52b29936085bc4ef1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:42:43 +0000

    poison union wise go promise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
