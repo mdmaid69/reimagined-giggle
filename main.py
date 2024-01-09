bc6c1a62baa9c9ebbd5e9e3584ebc6755ec84d19
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:04 +0000

    reason limit transport spring fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
