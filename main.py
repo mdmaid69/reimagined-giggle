31da6bcb393fdb489fcffc703deba13b65c062b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:44 +0000

    tomorrow split whether lead uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sys
+def exit_program():
+        sys.exit()
