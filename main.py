fa224ade1f70c8a169ccc531a0cc40c4fa16b3d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:33 +0000

    type anywayanyhow holiday comfortable contain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
