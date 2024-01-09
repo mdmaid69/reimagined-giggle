92951ac3e26dcc5d22a8160f237a1795ebb613b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:16:29 +0000

    exact callout show guilty finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
