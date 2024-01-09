eab12e9c850e27b266611f55380095f5bb335a73
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:33 +0000

    present he listen attempt model

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+def convert_to_binary(n):
+        return bin(n)
