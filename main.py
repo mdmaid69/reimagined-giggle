efb2661cf91136bd43a58492c1f9aebcde324c72
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:06 +0000

    land permission minor fix understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
