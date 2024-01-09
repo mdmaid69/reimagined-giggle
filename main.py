2b9bc4472b8531aae822b58ee60c9076d252c477
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:35 +0000

    challenge action stretch that lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_palindrome(s):
-        return s == s[::-1]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+for i in range(5):
+        print(i)
