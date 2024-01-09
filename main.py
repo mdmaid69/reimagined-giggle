bf0d9dee71628f7c976930550e12a1468ce4783e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:59 +0000

    fire address since male loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
