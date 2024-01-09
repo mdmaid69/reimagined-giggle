7e677213946a0a4784333e8089eb3d7f6df3e40b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:00 +0000

    like life future exploration plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
