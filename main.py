c742f1c0c9e8c91f2ea9f7dd5a363a60a10dbc09
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:57 +0000

    strong gooff accept reasonable but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
