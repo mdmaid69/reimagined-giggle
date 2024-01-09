42b67dd9b1f9f65b5375114a768ba67f7d108ec0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:01 +0000

    whenever appear address develop apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
