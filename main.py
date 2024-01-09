b9a23352b404b7866d29d1711e95c7aabe37638e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:18 +0000

    copy proud me news board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
