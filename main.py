3b5aa2e0611c3e77d3a01b4f0396f1a103d7813f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:10 +0000

    pretty ocean react education recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
