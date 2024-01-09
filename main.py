358f3ba266af9532c2dfe4d5f01349afbd52cf2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:17 +0000

    makeinto wrap satisfactory dick responsible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
