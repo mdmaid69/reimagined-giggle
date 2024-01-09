b7361db3fff3db8bcd1f2de9adb0f57b7e8a12d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:55 +0000

    fair confidence sad until Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
