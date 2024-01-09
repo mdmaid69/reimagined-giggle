638e7cec3f11c19eeeac28008a2ff41cd76c388e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:05 +0000

    occur side gas attempt meal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-print(datetime.datetime.now())
+def get_array_as_repr(array):
+        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
