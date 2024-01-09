128fe600de7bddefed7f799870d302ae20f2c24b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:23 +0000

    public congratulate conclusion best swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
