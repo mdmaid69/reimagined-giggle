c0e0b50ef45d42e19e395e8d6b42aecab716a45d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:29 +0000

    area business delicate they thanks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
