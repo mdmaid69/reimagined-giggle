0ab40341b374314f0c9a6cae92d79a30212f41d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:40 +0000

    contact meaning getback rough extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
