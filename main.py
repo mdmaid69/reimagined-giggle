531f5ed467c5f0863d257073bd7e53302ffcd8cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:10 +0000

    responsible guard city report forgive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
