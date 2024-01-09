408fe299d8b5a7b49100435b3b9c583172960b6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:33:39 +0000

    involve priority value takeover raise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
