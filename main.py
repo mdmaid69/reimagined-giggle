61dddd920bacf36bd1ca152d25bb3abc02e3a44e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:49 +0000

    bound contribute ready live 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
