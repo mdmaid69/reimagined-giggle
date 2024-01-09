1c7890975e0f870e67c2bf9393f58d50c9ef2165
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:34:20 +0000

    the classroom month somehow tomorrow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
+import sys
+def print_python_version():
+        print(sys.version)
