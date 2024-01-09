7bf016d8e4ab12c2b46f75eb8bda8eb2a84eacdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:44 +0000

    rely fetch noise bottom tour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
