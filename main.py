70d1dd435dfc476a182106a0db6d681a1bb8f913
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:51:58 +0000

    exciting rest kind choice dish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print("Hello, world!")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
