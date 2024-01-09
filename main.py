b4be825f0a71e1270647c90d04c89f8063f05728
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:31 +0000

    platform gas sail cover bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
