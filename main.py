a6e3bd5549babb1b931c9cfbf847c1c6de2c2979
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:39:10 +0000

    thinkof one soon politics warm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
