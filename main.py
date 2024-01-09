611c1e175e714b8060ff31cd04725e67bfe57656
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:16:10 +0000

    recovery promise crash hurry beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
