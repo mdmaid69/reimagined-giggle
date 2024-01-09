3bf979258b137dee529508f8397b5e8575406c4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:10 +0000

    holdup certain crazy usual range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
