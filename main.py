ad47de764f11815e6b53f817b948db992187a59e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:45 +0000

    aware square upstairs dinner runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
