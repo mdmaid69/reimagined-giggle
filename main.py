23c61be24af50bc9da01e45ebcb9fecc7d1dbe04
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:36:37 +0000

    fear surprising speak train pull

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
