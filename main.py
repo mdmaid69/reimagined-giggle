fb2d98d3b2d8ce3dd5f852bf6832435196bf21b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:42 +0000

    wealth loose instant school load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
