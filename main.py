7d4cd7ce93861ab97201bc5e9a584afad57034ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:53 +0000

    speech dig attempt preparation middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
