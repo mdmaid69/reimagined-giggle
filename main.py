84387bdd7b1238d46f27f2cd86b1dce93584b728
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:58 +0000

    else force heavy clean bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
