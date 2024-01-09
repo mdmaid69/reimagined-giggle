5229e803e0ba9cfd5aecd35d4cbee5034c15f7ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:55 +0000

    state failure interested by kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
