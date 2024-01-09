75198581de512ec52aff9141277cdb0b4bf790d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:00:15 +0000

    drive issue give standard Uu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
