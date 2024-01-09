9996819f023df53f5c82f9353c48053ac9b7c1e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:39 +0000

    violence especially future entrance interview

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(sum(range(10)))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_power(work, time):
+        return work / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
