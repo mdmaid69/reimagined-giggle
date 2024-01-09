b0d080f66d0b8303144c65d18481f45804db9b36
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:12 +0000

    tongue hot opposite same maintain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
