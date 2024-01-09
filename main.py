5312f92eaddf94e8e050e89f27fe19a1c0f40013
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:48 +0000

    settle start pronunciation train either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
