bdd49aead423cbe26e73e80d3bab92348bca504a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:33 +0000

    finally away eye shop cause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
