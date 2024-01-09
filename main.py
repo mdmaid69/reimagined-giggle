a6750d63721784f337eb23c4963d651c1fb6cbd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:51 +0000

    walk concentrate minute contrast she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
