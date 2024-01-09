e0adebff18f1aa54cc77110167985670159092e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:56 +0000

    before within band lookafter negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
