f22d1cd41d818256fdd2e618459bb10d79998fdd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:02 +0000

    not weekend from brush quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
