a35ec173ca5d5fa1026f23d11fc2fe14fe662d74
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:00:50 +0000

    bottom spring wake underneath group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
