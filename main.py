a90021a962c799635bd0277fdd1e8ddec64caeef
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:31:00 +0000

    makeinto continue moment bet drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
