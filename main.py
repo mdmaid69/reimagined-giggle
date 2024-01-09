29ec977ec3adc09dad9a7b3cf5544679a983f4fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:46 +0000

    pin spin thick burst arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
