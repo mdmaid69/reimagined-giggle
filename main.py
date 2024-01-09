b412ea388a7879b4e690f4c7af9fd4ee8a45419a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:28 +0000

    confuse complete deal official official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
