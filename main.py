2aab26682cfcc2a8c3625ad8cbc32a3c97509f24
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:12 +0000

    distance brush throw personal search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_as_frozenset(array):
+        return frozenset(array)
