2f62e033412ee8b1f9f0554b8931db20612fb3e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:14 +0000

    account official do take whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
