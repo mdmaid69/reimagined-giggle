f3815a47184f89083c6453e971d6cdf8024b807f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:26 +0000

    strict lie software trade run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
