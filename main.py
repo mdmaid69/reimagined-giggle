ba158f53af9133be6be754578f122f36a4ada80a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:37 +0000

    sister safe left thanks goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
