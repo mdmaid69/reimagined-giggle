d6114e01a301fa03d8cfa6329661828ba5aafc8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:20 +0000

    little bye Gg getoff approve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
