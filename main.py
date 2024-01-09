775960d5d903b182ad435dc52badc1e384f93065
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:44 +0000

    account knock idea convinced risk

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
