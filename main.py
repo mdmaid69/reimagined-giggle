f5e02f17692c608d02c7549832b58b115b18fe35
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:40 +0000

    region bite file interruption thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
