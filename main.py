287a95cf59dd1d342301ca4a76cb759c51c8e8bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:53 +0000

    estimate majority pull chance later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_density(mass, volume):
+        return mass / volume
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
