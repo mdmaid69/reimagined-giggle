93aab2708fea43199a1717ba74688ebf72a24226
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:38 +0000

    treatment lead glad large apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
