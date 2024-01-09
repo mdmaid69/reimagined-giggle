57db089bc750709d0f12e7b5b5836287a9abfd6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:02 +0000

    difference accept provide pleased give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
