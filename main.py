e8059ef09356d0764a3613fbff54e9cb911f3cbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:21:02 +0000

    centre judge grow set eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-print(random.randint(0, 100))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
