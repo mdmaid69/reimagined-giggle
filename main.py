f1938783bff48add7779302c8b340595a637b43c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:11 +0000

    freedom apart round score shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
