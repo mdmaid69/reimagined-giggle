006b59e10508ba47e82e39289c9a5ee854ec0050
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:16 +0000

    back flavour important entrance doctor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
