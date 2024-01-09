ad151de86c26762a2e27b5c31f862227e72a7065
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:41 +0000

    swallow high cover cope chat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def square_number(x):
+        return x**2
