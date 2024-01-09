453c5b357ba0c3ad84b3407a65db7c65e94b799f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:49 +0000

    snow supply opponent negative matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
