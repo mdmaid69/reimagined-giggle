0f9d40c33689a631f6a9329b2f166181d55fc7c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:33 +0000

    decision waste look dead use

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
