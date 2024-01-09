479b49a981812366d317954c0946d7f7ee25d033
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:37:44 +0000

    bye talkoutof allow plenty declaration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+for i in range(10): print(i)
