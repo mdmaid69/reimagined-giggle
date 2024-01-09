ad7ec7820fae4e5c1ca409211f9379adcb6d6ca9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:15 +0000

    instruction search along private dream

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
