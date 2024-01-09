4692e31e7980711793ad1447e2baeb10381a6a63
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:51 +0000

    bin plain conversation beside hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
