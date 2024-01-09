c8867e568eaa3a5dfcdc801642f1dedf6593984b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:52 +0000

    science world roll appearance since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sys
+def print_python_version():
+        return sys.version
