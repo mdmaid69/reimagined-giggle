4ecb36b6df90ecdb97115e406c946f2928f2d87e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:49 +0000

    passage pleasure over past disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
