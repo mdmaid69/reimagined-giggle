4d9fb4e79fb8d4ce2a8e289c69e40993d558ce71
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:26 +0000

    waste tap mainly needle either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_speed(distance, time):
-        return distance / time
+def square_number(x):
+        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
