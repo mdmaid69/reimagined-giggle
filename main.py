461593efb98fe1d699d643f9a52d1724bec8c095
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:07 +0000

    patient Yy challenge nurse talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
