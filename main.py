88793c8eed2db412e9c4e4c861d416d76ecd444d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:46 +0000

    until excellent sense doctor stare

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
