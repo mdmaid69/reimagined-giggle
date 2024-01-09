539525c47b2f92b228a002dd59fe0a431ac9826e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:55 +0000

    club letter give switch back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
