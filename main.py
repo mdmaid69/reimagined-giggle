b6eb15c0024844bb790f18bc522cba84f1747360
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:51 +0000

    soak issue do seat cruelty

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
