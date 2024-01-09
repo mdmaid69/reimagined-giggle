aba529a70f1567b60b66961708bee32879883d31
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:27 +0000

    comment kick moment fire suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
