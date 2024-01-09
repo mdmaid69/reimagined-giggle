321163e157c9ceec2ff5a30eb7b18e255abb18dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:49 +0000

    steady right snow backwards through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+for i in range(5):
+        print(i)
