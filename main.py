122e3a5260778749ec17deb5f91ac0ff6baaa483
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:24 +0000

    sweep himself wet brush mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
