49eca60dff41959993ee741519f14240613271b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:05 +0000

    project male workon spare mess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
