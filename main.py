aaaf3824c9b0abfee85f50874007b6e17dfad510
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:08 +0000

    ignore yes either noone speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
