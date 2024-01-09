b24295fb7c31a18cadd6833c0ccb27cc1df919f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:30:00 +0000

    level ask slightly profit careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_volume(length, width, height):
-        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_typecode(array):
+        return array.typecode
