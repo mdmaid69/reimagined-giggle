aaf4fcddc683a3195e5ca567bd4dd4336265eeee
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:54 +0000

    plan early building culture approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
