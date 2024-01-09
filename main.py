d707078310e0a202222fa666593036ef68887484
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:26 +0000

    rise freeze hope pick minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def reverse_array(array):
+        array.reverse()
