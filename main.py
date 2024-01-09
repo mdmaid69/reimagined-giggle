a0a728e51ff60e64564f55c133c0bd5dc2cce1c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:45 +0000

    start suffer hit beneath adjective

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
