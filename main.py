2c09ce5caa9faa04914f390589b999fae6c99242
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:22:32 +0000

    564encourage call well born chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
