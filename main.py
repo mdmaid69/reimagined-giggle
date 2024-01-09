74fb4986f7fede4b3ae2edfdec39ac1fa0ee3c20
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:59:37 +0000

    forward light sort soil escape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
