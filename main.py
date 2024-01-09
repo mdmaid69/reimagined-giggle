13070394c90bf19db1893bd0bb586cc3beab2302
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:59 +0000

    smell line convenient pleasant either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def split_path(path):
-        return os.path.split(path)
-print("Hello, world!")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
