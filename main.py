e6c1e054921908e77a5d84f10eacc017734ed700
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:21 +0000

    next basically rest thorough permission

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+print(math.pi)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
