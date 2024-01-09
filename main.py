36537afd29b4a1cf87f1a2dff32cbf3403380c64
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:07 +0000

    179bite anywhere conduct argument thing

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
