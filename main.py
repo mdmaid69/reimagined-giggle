0272102d8965252b2d56a158510b4732e7507dba
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:32 +0000

    beautiful judge rough bridge easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
