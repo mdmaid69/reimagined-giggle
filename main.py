ff241aabb8af9d03297cd0ecc9251eedce72f7fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:50 +0000

    proof neck unlucky stop soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
