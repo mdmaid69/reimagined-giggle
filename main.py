387fe6367a7d861aba608f3d1088be88199dcbb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:11 +0000

    over pen unlucky around shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
