7d95eb8cd11a9093d03966c4a837be0e60e4bd79
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:45 +0000

    goout tense thing born fall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+print(time.time())
