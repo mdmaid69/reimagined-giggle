f4a3ef55d5cb27075ab9786a89ae16f8e219b7be
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:14 +0000

    print way Jj law morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
