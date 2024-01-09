ae381d655719d5a784cf3a9f83b70f2383f91418
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:56:34 +0000

    however yet throughout relation me

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_rectangle(l, w):
+        return l * w
