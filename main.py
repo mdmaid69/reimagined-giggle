db163f390231dfaf62285d3afd507fc690216f4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:59 +0000

    fire sentence around breakin group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
