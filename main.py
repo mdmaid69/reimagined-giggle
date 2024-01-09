680109bc0faa6b886706110997298b4802509b90
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:03:15 +0000

    mouth empty manner everything foreign

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
