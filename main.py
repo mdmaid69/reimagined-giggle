519d1b4ebe6fb43366a4e750f971478837720c77
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:10 +0000

    weekend smile low tough challenge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
