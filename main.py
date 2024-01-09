c79df558bbfa6b9db9b6106602f6b615ce289320
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:13 +0000

    stop cum situation confuse welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
