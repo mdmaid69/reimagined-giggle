f28a2a6d917f4f4a0428a1a59b428b34569b4ddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:09 +0000

    sense create radio human weigh

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(x, y):
+        return x + y
+def subtract_numbers(x, y):
+        return x - y
