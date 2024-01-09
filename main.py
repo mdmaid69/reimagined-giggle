ee13f6fe9512f90ff39e252dd3eba9e44b3b4738
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:14 +0000

    load real emphasize stick welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
