1f801304ea80f2f68b0087ee4fd60e4f3e844ae4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:08 +0000

    direct except lonely fierce earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
