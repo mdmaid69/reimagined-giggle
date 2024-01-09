979018866e122dc34e660f82722407977b7b44ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:05 +0000

    rid deal justice basically hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def multiply_numbers(x, y):
-        return x * y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_area_rectangle(l, w):
+        return l * w
