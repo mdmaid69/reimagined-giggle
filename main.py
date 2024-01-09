2539e73462c370bb2bb0874d02719b3199104885
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:36 +0000

    keen uponon shop kind work

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
