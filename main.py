adb38dfc5d0c43eb90d30e5e13c4c740f0162247
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:36 +0000

    physical best generous born lunch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
