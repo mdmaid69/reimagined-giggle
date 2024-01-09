fb2fe8b5c993bd9c6aaa43d392161eb696c45bc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:47 +0000

    thankyou pronounce telephone father 565end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
