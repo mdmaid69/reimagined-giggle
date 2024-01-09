e4fbc687df407d7498de85eafb1bc2fcc56b23c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:50 +0000

    natural shout quality settle excellent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import time
+print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
