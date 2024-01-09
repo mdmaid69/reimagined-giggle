292816120d6e0420030f6fc2d7169130336656e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:05 +0000

    tune whenever yours study shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
