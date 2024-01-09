42d50cb637d1624c8ca6eb0970e4d82577fc75f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:21 +0000

    turnon learn walk finally smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
