cd406f0d848d3cfcaf3b56362118a37e8109b688
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:07 +0000

    public shine recover action become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
