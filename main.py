04dde82711a9c3ac73d5a0ec3b78fec6c049b370
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:59 +0000

    develop may popular notice kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
