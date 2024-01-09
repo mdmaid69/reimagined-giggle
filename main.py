4bb3a8ae1ac510fb4474a9ab4ccf875c3b778d4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:24:46 +0000

    love tour tongue turninto public

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
