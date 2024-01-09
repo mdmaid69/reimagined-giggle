59fa118b5d7d50031cd4411ad7946b02becd43c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:19 +0000

    muscle earn steady soak lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
