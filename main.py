d86067b1995e24a183ec83ed06df37f64fd827b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:41 +0000

    custom succeed pronounce system boat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
