5c8b435c43aca47fe4ff659428c9520ef749ef61
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:36 +0000

    second adjective willing spring soil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def count_elements(lst):
-        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
