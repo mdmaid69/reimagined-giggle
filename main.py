fb24572a7e589757175c32d313378a11841afc18
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:43 +0000

    somebody deep control ordinary close

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def convert_to_binary(n):
+        return bin(n)
