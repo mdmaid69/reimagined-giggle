f430cec09c7755118077c79cb6e2025e2d957dfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:35 +0000

    look wherever convenient fine male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
