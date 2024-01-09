815a1e7170d7f7534614c1c94bb4ba4069b2f8d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:41 +0000

    bet talkback branch so lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
