45796c440e572ac5792300dde62ab50a3eee4765
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:57 +0000

    description dinner succeed temperature old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
