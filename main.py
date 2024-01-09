320487199205fe2de81a8f9c2c1e307740c91a5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:24 +0000

    train love introduce lesson fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
