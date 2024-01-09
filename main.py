f9b682c0b19807551af240ef99257e39d597b909
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:21 +0000

    shadow back lookback(on) suit be

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
