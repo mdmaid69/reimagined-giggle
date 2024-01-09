f58d13aa6e9eefbf173d0d2899709f494cc5c01b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:08 +0000

    pencil ability practical pin suggest

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
