6097709f6be3125b8c089e36eb4bae73642f0ec3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:12 +0000

    bet likely regard callback bar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import time
-def get_current_time():
+def get_time_since_epoch():
         return time.time()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
