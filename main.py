ed3f410e178c5e2feb650c399167cd135dd9b485
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:14 +0000

    coffee before shall area flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+print(os.getcwd())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
