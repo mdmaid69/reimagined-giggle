b9a8a73f4d777fd9f515691255e1089b7ec1dbdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:05 +0000

    attach rough vanish Ss protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
