1cd707b4884f8493e11cdb1d456a8630e1c12095
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:46 +0000

    act race ball land compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
