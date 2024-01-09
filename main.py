ed7ae2bcc4f7c2fbc4a4dee536de75fd9dc62695
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:30 +0000

    divide getoff realize admire alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def square_number(x):
+        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
