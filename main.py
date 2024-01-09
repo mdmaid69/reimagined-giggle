398bbc2cd3524e4d4a371b745ae230b41ce82733
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:57 +0000

    undo cost goout free woman

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
