e88e3197cf15d0cff7458b9a055240244c021ab0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:41 +0000

    my region married Tt point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
