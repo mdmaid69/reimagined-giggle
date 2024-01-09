61776a7dfdae705fdff23a1fa2a7e060ea959efc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:34:40 +0000

    outside point skin fill shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_binary(n):
+        return bin(n)
