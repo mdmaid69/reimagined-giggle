6c4910bdb03e186ec2d3b2c9b508f4b6022a995b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:46:04 +0000

    look before improvement sleep project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
