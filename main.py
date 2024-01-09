e9b0fbe77810a8d256dd9c655c49450c690b3440
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:16 +0000

    ago task sink generous slight

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
