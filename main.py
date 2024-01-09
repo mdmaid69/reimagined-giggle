720fd269c560132c3e357ba3f9e84e0e0447e6a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:13 +0000

    trip 571enough proud stand contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
