a6b3047295aa471806269364c08bd267e7f8fd14
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:36 +0000

    raw rush nowhere love trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
