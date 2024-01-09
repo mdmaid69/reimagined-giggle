8216cfe2e9af5dba4a0d95307aba85491577e974
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:37:07 +0000

    miss discover remark rent breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
