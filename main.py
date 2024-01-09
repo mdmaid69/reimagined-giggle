8d5034a83baf24c51c8326dc59e0ee1fed90486c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:13 +0000

    accept such swim cut operation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
