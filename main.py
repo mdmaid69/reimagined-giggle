7a7338ff0845ee244df0e404875f798466873f4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:02 +0000

    accordingto country accidentally impossible should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
