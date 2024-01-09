172ddfeee2fa1bf08ccf74f388aa0e4829c666ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:02 +0000

    board dish middle full thankyou

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
