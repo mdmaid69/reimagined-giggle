a1540a879ea7e044509c3b2ec371d4b156c544df
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:50 +0000

    reply accordingto admit friendly exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
