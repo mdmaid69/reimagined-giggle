0cd13b2e633cf89f70e9071067a9d22c91b39b9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:50:15 +0000

    patient professional drawing passenger phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
