043faefa305a661d40aaab0d3c9d5dbb73744938
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:57 +0000

    belief train do hope talkinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
