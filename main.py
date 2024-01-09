de1eef77882002765edb919c746770cda6c95b87
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:50:04 +0000

    exam after summer disturb something

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
