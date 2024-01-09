c6b1879cb9a95351cb95592c58b3f033a8f09482
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:29 +0000

    sale stretch colour song kill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import tensorflow as tf
-print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+print(sys.version)
