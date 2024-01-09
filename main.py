2928f7fe0d77613b86313f698e7178bfec8b47ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:28:45 +0000

    solve subject intend 565end that

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
