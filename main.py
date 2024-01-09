61450d5ab49a85c58ed195d0f745068c94d81880
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:51 +0000

    drive which pity capital goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
