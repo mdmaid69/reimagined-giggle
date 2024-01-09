d71a5d711c4088a82a558e751f32af88a75a0a01
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:18 +0000

    spare count tour terrible firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
