040320ee14dff856585a584ba5e7ef99b1516610
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:10 +0000

    now seat left fine interrupt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
