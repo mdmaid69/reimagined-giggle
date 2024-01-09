ee614fc849adc5e35e70a5d4c39b65360fd8d9bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:15 +0000

    everywhere shut thank shame really

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
