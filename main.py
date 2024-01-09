a0808aeb279b3db2b32b83e74d102385609f332d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:06 +0000

    communication suggestion right born steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
