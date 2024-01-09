ff0df3ac3616dfa4f4260d76fa40edee175a0d5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:20 +0000

    holdout indeed notice start impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
