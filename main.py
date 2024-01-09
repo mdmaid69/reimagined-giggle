54d5239efdd0d0dc23117d939f5b956d9cbc46f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:57:19 +0000

    blue border story proud wipe

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
