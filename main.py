068b859b4c3548d5f9eeca6e4b5206f5b394b96f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:44 +0000

    knowledge ability quarter clean chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
