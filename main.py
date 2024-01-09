96f5a0b162eef60c14e0e8c18f05f8fa8219d5f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:32 +0000

    sure recover glass record relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
