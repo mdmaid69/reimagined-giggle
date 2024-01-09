e32bd83c40bbf8f593eebfb086590a30a432a23a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:58 +0000

    place bang if afterwards connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
