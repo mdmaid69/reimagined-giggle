b6b867deca10079081af15fe5b3e4efd584b4408
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:54 +0000

    manage back inquire satisfactory crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
