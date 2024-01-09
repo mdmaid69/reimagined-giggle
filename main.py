8ca903f7dbd64e1213c9be2b41baaf57e0fc980c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:44 +0000

    callback pinch window change most

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
