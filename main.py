44831a6d918111e845ab987825cde6178acbc98b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:25 +0000

    type tear laugh getout we

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
