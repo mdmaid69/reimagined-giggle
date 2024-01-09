f18463491ed44dea4ea37b1d8443cc923fca87ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:14 +0000

    low first character no damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
