920f1488a86838d2698322cc07d132d6a3175e47
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:45:00 +0000

    physical receive brother basic form

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
