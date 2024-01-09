d671e79e87e5b3fa6b44c7185d1a5386a07608df
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:47 +0000

    separate dinner sleep alive school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
