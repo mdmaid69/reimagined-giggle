676cb8e81f92782aa00ea84545ca45f8551c8894
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:41 +0000

    study history sky used argument

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
