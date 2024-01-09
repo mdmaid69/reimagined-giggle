f4a02d4b9f9fd293919f42772e15b32c09f16f87
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:55 +0000

    though past programme still game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
