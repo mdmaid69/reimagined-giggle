104afcf242ce211aaee7b74e3786a1b859f7e8c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:02 +0000

    train listenfor under out left

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-print(sum(range(10)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
