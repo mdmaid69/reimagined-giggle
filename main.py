8b9c7adecc847d49df4d4fac4bc1bf1c274e02de
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:30 +0000

    achieve computer more refer sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
