bf3dcb287b0d54686e3f3804245db57b9ee9decf
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:33:33 +0000

    nor all considering slow guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
