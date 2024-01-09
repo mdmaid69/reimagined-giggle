0315c6fdf9a1d8d659b69e0932d82a7678b03247
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:47 +0000

    needle on satisfied attract pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def reverse_list(lst):
+        return lst[::-1]
