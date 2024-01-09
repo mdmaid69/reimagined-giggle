6b11a1d201a7821916a183ea9f3574ec9b9060cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:57 +0000

    pot pot second complete confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+x = 10
+y = 20
+print("Sum:", x + y)
