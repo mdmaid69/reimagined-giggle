a1330d626379da23bc27536ac65ff212c5badb8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:56 +0000

    permission chance belong file precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
