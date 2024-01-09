3eaeb184ce007330f25bd057f3f7e8629005a202
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:59 +0000

    thinkthrough able uncertain autumn talk

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
