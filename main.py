ce320f7f07aef3fd022ad2ddfedf878947011793
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:08 +0000

    brush unite estimate try listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
