ffed123a2a5acc88d4b5aa1c81b973eddc571820
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:58 +0000

    include downstairs song ourselves service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
