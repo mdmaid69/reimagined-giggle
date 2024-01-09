7d89fb76d319e72606e0d737854ba9841215bc90
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:33:45 +0000

    bear honest stand genuine become

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
