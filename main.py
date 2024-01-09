474d2cd33a1fc7dcc4cf87fdd3717a7e9e78bc40
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:57 +0000

    play tight dig bar worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
