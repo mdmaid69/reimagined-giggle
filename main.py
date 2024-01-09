ebc7987eca2af6a091ac195e9b1a9d696dd3c856
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:48 +0000

    difficulty emphasis single behaviour smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
