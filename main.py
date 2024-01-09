99ecff8131bead3edd7ba2f6312187aaf72ae367
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:17:18 +0000

    breath fix steam paper port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
