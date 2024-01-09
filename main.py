fe031bfa27eecfa964b571f659f043ff554163ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:05 +0000

    taste really guess touch outside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+print(sys.version)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
