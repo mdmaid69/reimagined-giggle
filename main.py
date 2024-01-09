e1c9680a6d870dc25fa7b83261712486696783a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:45 +0000

    truth determined home spite tour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
