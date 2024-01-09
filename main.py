e437a5be8adb6d476e7e51632f8cbed75356cfea
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:41 +0000

    ever pain completely behind best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
