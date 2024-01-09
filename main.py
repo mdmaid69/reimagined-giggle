f08899acbd11f54e75bc584b33b2fb337f8cdf59
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:40 +0000

    usually own getaway trick mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
