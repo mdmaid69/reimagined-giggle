dbfb7a94d7f3237062ece787dc813f8b7580227f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:56 +0000

    past trick tomorrow suffer wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
