1cb991ebcfdae0f6edd53c10d25f536ae925a712
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:39 +0000

    feel workon after dinner do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
