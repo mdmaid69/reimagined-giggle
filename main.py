4776517993cc61691b1e10f3f28db1716c6e0b77
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:11 +0000

    chief cross on hello criticism

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
