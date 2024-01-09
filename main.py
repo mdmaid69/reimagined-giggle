fa0ef7c64735c84d9df8a7325afd3c7ee282cfea
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:41:25 +0000

    black rich slice church deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
