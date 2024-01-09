eb9eb208067f8f6ecd31a475b5871fc3f3095685
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:19 +0000

    567enemy takeout about fall fold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
