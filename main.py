65201b08f7fccb368cad2766ad10aed3ced3fc85
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:53:23 +0000

    anywhere crowd question afford blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
