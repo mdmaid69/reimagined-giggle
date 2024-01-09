de5850534ae2d410e4b90e455f97f82eccf615ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:15:42 +0000

    common knock tape ought so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
