6105d2403b1e68278d7617380dfde6a0c4530636
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:14 +0000

    snow visit border flavour sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
