8cc7dd4ded3e37698bdc986269c78aadaf3b7101
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:36 +0000

    praise there negative office strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
