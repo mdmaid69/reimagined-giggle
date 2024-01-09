08414fed0b6eb4cab18baf1c0bef644ff7c5884f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:09 +0000

    polite content rob him cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
