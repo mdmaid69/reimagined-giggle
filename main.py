0b39e728fb6f08d20df0e9cd14be5d53abe20f13
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:04:45 +0000

    section left pollution part possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
