4d5bbf9d179a8f0b7513067810de64acdc9f606d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:09 +0000

    female copy operation look certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def remove_duplicates(lst):
+        return list(set(lst))
