5e4d0b14944e3b0b84d1ba1a81ed47eea157229e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:37 +0000

    level fire drawing worth contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def divide_numbers(x, y):
+        return x / y
