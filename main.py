76aca78a305bd430fd4bcbd472bfc5d2e43fa26d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:54 +0000

    city juice practical here passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
