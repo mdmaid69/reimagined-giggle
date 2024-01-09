a3fba7a6b50a6a5b77b6f4d3367b5881cd9e2736
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:43 +0000

    quick pull any appear why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
