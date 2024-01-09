6aeee1e8aa4bd4b36eef40695f080c626359764d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:10:22 +0000

    target cough eat 572enter steady

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
