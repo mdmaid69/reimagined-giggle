07630cda247094143549be2e3d7262138d8f53c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:24 +0000

    most holdon ignore steam minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_sign(x):
+        return math.copysign(1, x)
