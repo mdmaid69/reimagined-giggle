7aec8f13c812701af31ff8995d05af68484c71d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:30 +0000

    prove everything Hh shake stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
