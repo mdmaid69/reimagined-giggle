dadf5f5496f014ad7f92330d3ceef04a529031b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:04:30 +0000

    convenient taste split personal squash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
