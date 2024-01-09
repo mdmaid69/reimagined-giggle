d9f75f049dda2d1143e8aa4180d3f85615274ff7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:42 +0000

    repeat hang centre reach climb

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
