ece4b2bfa5cad966e6b170a57bed471d51b15c95
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:07 +0000

    not spread cheek slow appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
