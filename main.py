75f3f92cdcf612473e440684f10ef182ae9d2553
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:28 +0000

    clue insist adjective help miss

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
