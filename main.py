fdcab36207e5395227bf5a87d68551e250163cf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:20 +0000

    trial deep fly any cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
