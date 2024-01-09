d4ec089f47947fdcd20e268d64ded3a01f82841f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:41 +0000

    home bin full succeed sad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
