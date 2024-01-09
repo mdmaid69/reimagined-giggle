c4da1bf18620a4777b67d0739910c23e8f3f7368
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:53 +0000

    instead love exit wash holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
