5c51c5bed9b1404cdf210394211df885b8f3e9f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:24:18 +0000

    advice emphasis absolutely keen quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+print(os.getcwd())
