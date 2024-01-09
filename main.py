5744cd1011fd9d08968f67ea59478b00b953cb86
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:26 +0000

    claim ball up record shop

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
