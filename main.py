c5f8e9f364b14c5c28517198512753eb16e6a2b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:52 +0000

    help pin beside open swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def is_odd(n):
+        return n % 2 != 0
