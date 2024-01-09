9600b010dc3c567e9920935092e27989c64433ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:46 +0000

    comeoff lot claim aim further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
