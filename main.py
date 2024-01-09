ebf1f0d14e8aad0817a73983673eb5f61532c93c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:53 +0000

    sign callback earth mean develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
