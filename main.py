fb11cdd6bb6823ebc87b1f3433b5e10f1e0ca03c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:55 +0000

    bring solve strange large think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
