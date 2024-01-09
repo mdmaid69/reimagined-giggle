208e74a5a277a2f1d0203fc1bd47b46d1d960739
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:46 +0000

    still flavour set cope song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
