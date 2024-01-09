7fa85561b80bce56c999e7c86e9ee38a07a5f4f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:54:46 +0000

    step rain often mark it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
