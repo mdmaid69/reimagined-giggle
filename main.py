da0b26848ebee0db570682ab09ff045d793e36f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:10 +0000

    react carry bring record plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
