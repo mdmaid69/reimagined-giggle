87a7da30d9acafa9f63fe5c88d5728112c065e64
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:55 +0000

    wheel weekend neither rank furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
