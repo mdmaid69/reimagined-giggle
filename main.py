c293b23337bb19046fe873d1ed1651b078b439f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:14:28 +0000

    hardly so steal cool holdup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
