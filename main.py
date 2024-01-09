9432f398db1d91f8d90942e0f4b996fab4e96b56
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:53 +0000

    proof star unlikely noise Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-print(math.pi)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
