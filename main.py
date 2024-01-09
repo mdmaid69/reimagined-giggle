b59a2a351697f036550438aa51088bfd4d374b28
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:55 +0000

    add complete fly kind protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_union(list1, list2):
         return set(list1) | set(list2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
