64b22e16f78d5629c56d7671fdc251bfde1126d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:26 +0000

    gather them forever early muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+print(sum(range(10)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
