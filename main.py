61fe27313e9d67d38048844357aad45ce7b49501
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:28 +0000

    various it tend ride trade

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import csv
-def write_csv_file(filename, data):
+def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
