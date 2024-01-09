a4c8c65558a0128c9cb4e00ca7c1666450cccacb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:40 +0000

    water present store mail could

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
