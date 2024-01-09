dcd9a5afb66d2a20439091409151738c1015408f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:05 +0000

    then good awake never wash

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
