9453fdd5cb0827dfcee042b1eca3be389b910fab
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:19:22 +0000

    age scream search everything rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
