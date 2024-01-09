b4d935e599fe982ebc7f50de72fc22b66569b769
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:57 +0000

    suggest minor hard closely beneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
