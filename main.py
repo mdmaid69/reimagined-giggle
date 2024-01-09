68789dff5fc690f29c01c54ec25caf0995241351
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:10 +0000

    closely rank empty obvious mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
