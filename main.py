0792bf0824f2ae73302e02bde8c58aa4edf16038
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:03 +0000

    open excellent strike ill comfortable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
