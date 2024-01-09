22a84e21cb7c1b88cfd500fb0761b47693971ee8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:13 +0000

    manner principle prepare mistake sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
