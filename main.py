1a810b14ab7b133fada9d476b620c7bb026e047b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:59 +0000

    die excited influence during provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
