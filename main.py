72cc6c4f2443cf0475331fa0a702ce58690f268a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:59 +0000

    same treatment separate hit independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
