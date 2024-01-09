07cd04ee6e35840475b48460cdfb0ff32dfa6685
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:27:00 +0000

    class difference could however plain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import random
+def roll_die():
+        return random.randint(1, 6)
