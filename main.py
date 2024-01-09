be1d967ffc2827237b385a1b59cfcfe11db95ae1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:15:56 +0000

    scared doubt turndown respect want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
