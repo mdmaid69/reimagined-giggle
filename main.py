0b21f9928895821ba8e53c52ceffbc4ddb04ca18
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:40 +0000

    shrink direction live pure smart

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+name = "Python"
+print("Hello,", name)
+def remove_duplicates(lst):
+        return list(set(lst))
