6fb4c0f218225eb0f4f73717cf01aa512d966669
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:01 +0000

    manage female narrow respect whenever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
