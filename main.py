4a0dcbcdcc3d13b65622b3db0f498ddcab64d24b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:31 +0000

    luck remember apart train estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+for i in range(5):
+        print(i)
