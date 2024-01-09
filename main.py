008fa4b20cff9a510cb4789871aa2e4296871a60
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:52 +0000

    rely bored reduce surprised excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
