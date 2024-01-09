bd628a86c5d55b69e8ed36511483171b7b6c0fb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:11 +0000

    easy pump awake trip Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
