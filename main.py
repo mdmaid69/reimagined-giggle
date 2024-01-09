ff043a50abc94c5e3260287395fce038a274ad06
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:03 +0000

    stage busy rub set introduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
