ab441fa877969865ae358c857b360dba1394813b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:15 +0000

    takeout forgive should me herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
