7c2a26e35179bb66342a5df0612c5468d1d333a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:48 +0000

    exchange telephone fun touch understand

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
