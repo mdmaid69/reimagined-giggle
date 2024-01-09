d8272cb0552111c28f47116e1c7248ecb0ccd77e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:08 +0000

    left performance interview grow sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_user_dict():
+        return collections.UserDict()
