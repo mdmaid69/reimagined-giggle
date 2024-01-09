84968f642095f9e55612303efcf0c61110d4eb61
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:07 +0000

    before holdon dark extra reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_current_working_directory():
+        return os.getcwd()
