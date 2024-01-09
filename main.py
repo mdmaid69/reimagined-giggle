6b0a09c5d2f862cc04d66cd697bd331e8f538f1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:24 +0000

    beautiful head call cross approval

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
