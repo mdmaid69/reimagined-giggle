4f082ad129fe96273d71d9018b4f89fc58d12fd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:18:29 +0000

    over make measure warm great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
