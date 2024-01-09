a5d8397e2edf82f1434f41ccbf7babcfa84d9849
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:02:59 +0000

    argue bill road base pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
