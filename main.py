4804241560e2b2bcfea9aa4f2d5a1f8cf9fc5205
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:05 +0000

    first survive contrast wide some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
