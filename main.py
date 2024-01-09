548979d946ad15907c5e27d67921114f9721f152
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:47 +0000

    behave weak wild boring corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
