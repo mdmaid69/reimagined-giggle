bf08fc670d6eeccd9a5307033d09038bcfae80f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:08 +0000

    disturbing find inquire breakin consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+print(math.pi)
