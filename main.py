0c5b0b685d43dc9c58f0cb71f2ba225ff0010226
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:06 +0000

    defend exit Ee present wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
