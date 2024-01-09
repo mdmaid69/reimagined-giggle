5f2a6dba00e299297a56bb14bb11413757887497
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:04 +0000

    moral allow precise shout home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
