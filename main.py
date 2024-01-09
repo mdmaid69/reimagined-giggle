a510ad65109bb64e06af3b5f19eebae2d0e7a8a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:52 +0000

    naturally treat spin concerning break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
