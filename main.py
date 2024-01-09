8ed18298e1a3568a25b8ed9ce29690aa047d0382
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:42 +0000

    aware last report chief precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
