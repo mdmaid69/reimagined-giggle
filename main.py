0ee856f9a2350f1ae5d8378e93d2d4c344b4662a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:15 +0000

    but contrast away callout any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
