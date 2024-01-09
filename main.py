694714e3354aff7c1edbeda4520899487ac9f6f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:07 +0000

    stranger university round of Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
