28fccb9c1f572f33d6e5429a35a539084892c9c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:09 +0000

    fond niggaacademy contact rest invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sys
+print(sys.version)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
