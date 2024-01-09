168f8b56d72bb59b8f1aa09c523cf9fdce98a47b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:38 +0000

    company life impressive tax present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
