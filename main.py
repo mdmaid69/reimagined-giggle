a045fa77a73384b66ff9fa3a33714af2d1e4b41d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:42 +0000

    punish desperate sign after later

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
