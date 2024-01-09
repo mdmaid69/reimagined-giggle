fe8c6d19b9c22c37579ae33787737affc8bf8790
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:50 +0000

    dangerous crash innocent edge himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
