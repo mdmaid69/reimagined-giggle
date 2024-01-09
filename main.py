a38bdec5b72fbcc343ed30dffb30b20d50fd7678
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:35 +0000

    contribute coat miss extraordinary record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def count_elements(lst):
+        return len(lst)
+import sys
+def exit_program():
+        sys.exit()
