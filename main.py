6185eed6957c63b60973404a2c11c3e881b3e5b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:05 +0000

    clear impressed heat central dear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
