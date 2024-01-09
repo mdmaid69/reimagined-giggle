be8d1c5fb3cd533754353bf320d9ae294b80623d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:04:51 +0000

    freedom turnup behind proof remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def multiply_numbers(x, y):
+        return x * y
