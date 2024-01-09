5f5abee612dbb3a773bb5baa8e25f8a8abcca253
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:03 +0000

    history plan field heart Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
