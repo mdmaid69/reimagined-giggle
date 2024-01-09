366bfcc2278b2dc52238f784d39ee41884e6b6aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:39 +0000

    touch block government finger home

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
