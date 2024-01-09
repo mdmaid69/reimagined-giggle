765380337c5fcae2674eb5708e46c9b1da0b2804
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:34 +0000

    Rr handle buy house exit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
