e7aa4e6303b3f841798be0df5c81acb754f28a3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:12 +0000

    frighten brave morning debt beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_length(array):
+        return len(array)
