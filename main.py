6c427a608ebe88ea6d030cad8511dde3fc83ea3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:49 +0000

    invite equipment complicated treat human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
