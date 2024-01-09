55a7acc6e85bc411b1884553270d0d59186052b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:31:59 +0000

    keep accidentally business principle know

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
