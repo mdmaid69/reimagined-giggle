a02d36c7bd436a129ba92165fda8970f43492dc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:53:07 +0000

    tight approval instant dress much

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
