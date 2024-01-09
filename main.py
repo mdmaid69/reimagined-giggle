e73b2f8271e38ae6973bf5e6422ec97e7386a1f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:36 +0000

    tidy fetch their search polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
