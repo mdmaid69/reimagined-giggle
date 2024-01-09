254bebfc7c076ce42f76e13b121114d04ee5abfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:23 +0000

    name ill traffic pair lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
