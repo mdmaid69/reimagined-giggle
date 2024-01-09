2b9089c150f169c8377bfaa69fc30e028d72be93
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:49 +0000

    impatient before suckme getoff hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
