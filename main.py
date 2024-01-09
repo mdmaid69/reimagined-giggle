dba9d065613cf54a08015b4aa6862b33650ba362
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:18 +0000

    bringforward question substance serve by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
