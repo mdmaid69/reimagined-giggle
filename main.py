26ebe6c8e94f98058f92105378aed40bb5965ac3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:58 +0000

    intelligent mystery like set long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
