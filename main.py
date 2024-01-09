3899fea0a10870f507c9fd790a01cc0b177e8564
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:12 +0000

    tooth way take there part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
 import math
 def calculate_logarithm_base_e(x):
         return math.log(x)
