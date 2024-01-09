fd3355ce72ff1c789f456bac73f9b84f24024183
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:56 +0000

    before brush sail less properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
