eb09842af80bc85451c9e39bfd5d154ed483ef32
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:13 +0000

    convinced pity mysterious want wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def convert_to_octal(n):
-        return oct(n)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
