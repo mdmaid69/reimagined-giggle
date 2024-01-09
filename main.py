593d92ce17460963d5d5a029c2bce4dbdbe2a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:13 +0000

    keepfrom brother switch choose practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
