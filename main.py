e9c807399a737b953b95b48337d9d256f209f423
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:17 +0000

    dare design trade still party

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def get_current_time():
-        return time.time()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
