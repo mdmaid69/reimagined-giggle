ed8a43fd8ddc7d4e96f77d8d726a0f8daa45d13e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:51 +0000

    characteristic relation creature deep inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
