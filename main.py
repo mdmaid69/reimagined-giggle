a9560a4131a57f1d509247e8de148f2b2f5632f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:12:49 +0000

    underneath create battery responsible prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
