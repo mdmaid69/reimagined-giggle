fdde5689efb11baf6d584d61e6ac31362f4c16cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:37:21 +0000

    aware their before front OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
