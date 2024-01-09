bb1678b1c0ec68e27336c3679b1f9ce8d7183832
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:07 +0000

    several central badly stick branch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+print(sum(range(10)))
