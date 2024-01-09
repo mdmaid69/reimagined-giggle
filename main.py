2555978eeed641a50b57fd3833946477d53e85c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:01:09 +0000

    run call money leave roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
