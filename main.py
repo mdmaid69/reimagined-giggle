457945fd0a2c9d0d105e238ba9b2729b7b2f2527
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:27 +0000

    excuse after concerned by relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
