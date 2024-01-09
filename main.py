60c08c73c29e9712def383e754c5f771c048d8dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:25 +0000

    comeout file it divide separate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
