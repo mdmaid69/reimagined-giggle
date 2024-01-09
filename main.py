627d4a7784724f4f811c0f2343ec12ed22ab3ff0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:54 +0000

    Tt decide blue nervous hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_work(force, distance):
+        return force * distance
