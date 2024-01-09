a24abdc947f8ded62c87ad29cf8a892b8724c364
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:30:48 +0000

    same past grade term bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
