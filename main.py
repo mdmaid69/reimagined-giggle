4ba013a85d15192025182a334d81f9ed587dac08
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:16 +0000

    rough second precise ring Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
