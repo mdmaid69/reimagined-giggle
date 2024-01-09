f8354f3e1896357dd891653128fa665e8f7aa242
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:50 +0000

    original breakin apart jump shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
