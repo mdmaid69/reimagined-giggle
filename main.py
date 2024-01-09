703447fa69b75b2903491cbbcfe1831bf2c98505
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:22 +0000

    whichever rich takeoff already during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
