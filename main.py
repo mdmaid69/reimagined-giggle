d3c47d9e07dcf22055f27b310738237bad8c2b04
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:37 +0000

    getout sad card street keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
