5f6f3c682ce28250e5ccc5e181b2f4322773f250
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:06 +0000

    name sense heart personal phone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
