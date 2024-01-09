f5d4225edf1681e0aa991db3a286f1362b2b3aa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:44 +0000

    juice holdon vision wing tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
