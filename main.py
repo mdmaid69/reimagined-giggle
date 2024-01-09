cb60e7e90ebf34146334474492cbd52b73a4b55f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:31 +0000

    file level Dd perhaps war

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_counter():
+        return collections.Counter()
