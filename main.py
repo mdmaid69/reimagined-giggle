63868fda8eac1b9de539a2097e6d152bc4eff25e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:12 +0000

    circle dead allow almost pipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
