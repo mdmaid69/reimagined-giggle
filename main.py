31e86b6061cf180fce9a8b2fef9b6077dfb3db95
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:16:49 +0000

    gentle muscle cost rich lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
