8b07c911cc578e90b5e7b0c32c02b4c4c41bce74
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:46 +0000

    come respect permission calm thank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
