8ef685d95cc68f61620ae8a6e534e2d7ef89e743
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:22 +0000

    part length clue order fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
