ee6f75329a5939229f6778df17d6418a6ae6ab00
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:38 +0000

    float mainly sail cheek surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
