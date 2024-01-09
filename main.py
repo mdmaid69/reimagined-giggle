f47fbd0850f79d36137474d4cf3fe3dbc37d50b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:39 +0000

    smell open worse before alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
