d9c00358a1d037b4cb5ab2089f67bb5cac58094f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:39 +0000

    spoil mark recover display bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
