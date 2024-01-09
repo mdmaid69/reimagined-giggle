cdb37c3be24462437d4a9a8e7f6887c32a3b4ae7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:17 +0000

    exercise moment Pp quantity love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def is_even(n):
+        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
