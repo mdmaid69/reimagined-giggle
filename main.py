eca55b6e68b0bab3ffa665384a2f41c2ee228540
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:11 +0000

    means busy blind price ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
