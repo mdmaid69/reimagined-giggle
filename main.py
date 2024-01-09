ba1a724271a3a3566161a1a879a72bbf95015144
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:51 +0000

    Ff describe offend getup obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
