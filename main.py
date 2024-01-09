dfdc71a21d738aecca3b8af5d5a73e40a94ae9a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:31 +0000

    sign spring expect office same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
