d36dc6b9a2cb3e13fbdfb2518c9b241f4e70a4df
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:32 +0000

    great cinema clearly value variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
