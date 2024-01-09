37e549cff9a18ab5b5c777d120caa5444b565617
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:10 +0000

    date close neck pride star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
