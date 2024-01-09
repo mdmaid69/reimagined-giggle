6f6126bde2712113ae43dc13223d5e2f49a0155e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:43 +0000

    black terrible under every bag

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
