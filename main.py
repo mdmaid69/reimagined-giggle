aec7120304d336a82defc3c615e8d3464a1b0950
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:24 +0000

    snow dish harm likely watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def read_from_json(json_string):
+        return json.loads(json_string)
