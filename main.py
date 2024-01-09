684b108b6dc895b004e0fc2c12ee3014993da30f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:29 +0000

    lesson address force computer broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def split_path(path):
+        return os.path.split(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
