e0693ce1009901680f0f57a910423f1e66850e9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:58 +0000

    journey information comefrom peaceful fire

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def remove_directory(path):
+        os.rmdir(path)
