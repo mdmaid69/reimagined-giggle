f0c69b938f496e30e29d984de0d473de3069b857
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:48 +0000

    pocket difficult tight paper judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def add_numbers(a, b):
+        return a + b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
