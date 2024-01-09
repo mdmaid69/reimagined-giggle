d8a1a4fc094cf9ace2ff822316368cb909cffb1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:47 +0000

    relationship suckme represent street march

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+print(os.getcwd())
