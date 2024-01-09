28913dcd1bbd6d578007be89d9c78f03a80b3740
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:44 +0000

    pull other sport tragedy trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
