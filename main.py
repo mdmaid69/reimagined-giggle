3d4a2fe75479d591efc945afaa447b7dc79fa84b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:27 +0000

    keen no invite tap major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
