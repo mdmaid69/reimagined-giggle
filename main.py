f88200537288b13ecb336bd32528ac098e747b72
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:16 +0000

    enthusiastic contents shall advise now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_work(force, distance):
-        return force * distance
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_username():
+        return getpass.getuser()
