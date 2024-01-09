15420f3f9bf65889dcb4b00293b83d51af0bd663
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:37:35 +0000

    way real bury ground private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
