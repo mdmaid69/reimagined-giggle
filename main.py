c643eccedc89538e73991c5e6a0edfa83dfb3b58
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:56 +0000

    where hot petrol suit shake

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
