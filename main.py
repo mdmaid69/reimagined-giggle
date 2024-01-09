57900eb5d9cd72435ad3cbbb80c42338b3bbb70f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:13 +0000

    interruption deliver negative future this

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
