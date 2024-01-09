dc3b05c114410340159c291a842dbb8b592ec1a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:08 +0000

    patient shout match agree lead

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import tensorflow as tf
+print(tf.__version__)
