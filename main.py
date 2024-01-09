6eddaf590430a4c65c454ef2d52cf066bc63c448
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:36:47 +0000

    judge tune past normal awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
