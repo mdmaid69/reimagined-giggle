9c04d122f01d7e42a70e4f7a7da9e69754f53970
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:48 +0000

    left mark reject disagree other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
