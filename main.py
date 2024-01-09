6b94f4efb6c30bad93fb6199290558aa132cc801
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:19 +0000

    company line aim as wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def multiply_numbers(x, y):
+        return x * y
