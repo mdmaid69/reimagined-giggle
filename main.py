35d1bf87cfa1d184910718616df32ecf77957164
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:28 +0000

    pretty direct effect environment part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
