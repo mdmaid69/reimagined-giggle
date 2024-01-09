6c70ca149429315690392ed6f875b818d311e315
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:37 +0000

    television spoil best lookup slice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
