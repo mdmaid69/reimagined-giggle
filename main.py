316958d9fc3c60248eafbb1046c648ac8d278b16
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:43:16 +0000

    ocean part task quantity situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
