c093101df9198afb77f082187080638736c9b960
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:56 +0000

    joke advantage protest mistake bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
