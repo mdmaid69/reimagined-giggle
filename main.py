2594cd1a28e6d755370d4682ae9a6ab83072c3d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:22 +0000

    minute favourite prefer guide giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
