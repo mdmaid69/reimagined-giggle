293a0e88e0e2f75448141eb70ec3dbd4e6b27ead
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:24:29 +0000

    relative legal once comefrom drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
