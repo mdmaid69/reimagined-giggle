5ab30590003424756e343e3142eceec55140ca93
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:02 +0000

    cut show environment clean recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def split_path(path):
+        return os.path.split(path)
