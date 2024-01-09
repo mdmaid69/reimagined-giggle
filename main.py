e70e24f7c8c6fb38d54a6b61a753e6e5ed22bf1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:22 +0000

    label bag outside call later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_even(n):
+        return n % 2 == 0
