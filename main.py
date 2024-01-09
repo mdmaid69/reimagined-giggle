c76180b08a85fcd21bc726f72313a22be3f1e992
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:42 +0000

    dig marry land concern meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
