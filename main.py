efc34244a0bed203d3e31fed3db5e2008d8100d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:45 +0000

    sensitive gift join around maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_speed(distance, time):
+        return distance / time
