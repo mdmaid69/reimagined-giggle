643f4473b2fce297c1a58682f7528e6ed1a626e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:27 +0000

    character set such talk tense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
