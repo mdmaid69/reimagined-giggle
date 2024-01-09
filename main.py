1170a60a0a71a1a4d2de0681db37b8ad1faa201b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:42:42 +0000

    suppose have any mystery badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
