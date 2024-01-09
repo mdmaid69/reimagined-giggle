4c8ed2306389d1f5d0026e0c8ad135c273e8e565
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:01 +0000

    hate conclusion exchange nature pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
