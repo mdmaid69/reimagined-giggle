ad5b585dac2627007d4b548324388726789af73a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:08 +0000

    spray put Dd comment soil

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
