f21cad16ca0f591b69d0aeb06fbd60ca6641c87d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:57 +0000

    agreement wild hit accident off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
