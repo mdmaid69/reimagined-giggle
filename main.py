87ae1101480d38699c77f9bb880c6141acdef8c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:02:52 +0000

    port factory meal hand progress

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
