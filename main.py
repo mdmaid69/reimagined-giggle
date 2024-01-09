e7543f38a42e7f37c28a9a4677f364fe621ef1a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:38:41 +0000

    difference understand noone arm programme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
+  def square_number(x):
+        return x**2
