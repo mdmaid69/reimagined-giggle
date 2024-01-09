570746fca4118b7cdf9478e3cc0ba96ec42a190f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:50 +0000

    joke usually crisis Ff compare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
