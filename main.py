9e4314876778714dd37cece8f4f29463f2aa7694
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:16 +0000

    pure eye question confident wake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
