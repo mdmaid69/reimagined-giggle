0df0a2b28b37967f72041ac1417c235957873e3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:31 +0000

    settle straight feel hers free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
