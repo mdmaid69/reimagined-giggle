04da5ec4a7112566d4611ea9ea16de5dac0faf56
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:27 +0000

    nature arrangement further break height

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
