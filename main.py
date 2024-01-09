b7ded34fde99d76add88aabc52912578640ee645
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:05:55 +0000

    treat protect uniform winter stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Characters:", len(text))
