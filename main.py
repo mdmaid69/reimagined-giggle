f5bd53eb4e3712483403685d299e644abcc5cac0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:57 +0000

    slide thank embarrassing band seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
