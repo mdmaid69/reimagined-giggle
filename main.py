bd0cb56f3fcc57d18611b124980090a58217d1f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:53 +0000

    uncomfortable dry there street negative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
