48698abbd24499cecccc8bb1232984adfb3e97a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:38 +0000

    dream provide attention matter at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
