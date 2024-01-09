4c8ad3017624098c31f610eda6e7b145051a1b82
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:35 +0000

    talk its believe position pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
