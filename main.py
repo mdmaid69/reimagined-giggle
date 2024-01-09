18947aa4ac04fe911a439089098a6dc42bb6aeee
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:59:30 +0000

    programme money even hang report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
