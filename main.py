927a8f88a8a18d694de57a641db9c2ef5d47403b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:16 +0000

    abroad strike personal depend fail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
