3b57788f5d916afd168ed161a53b9b03f0f42839
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:43 +0000

    judge tonight broad promise error

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_counter():
+        return collections.Counter()
