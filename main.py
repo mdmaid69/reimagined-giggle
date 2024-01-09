8f9f6eeaf12448c05936a1b197b4e0fd66f77de1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:52:49 +0000

    switch Qq system mother turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
