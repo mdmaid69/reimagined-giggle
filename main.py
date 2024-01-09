c6dc9602401b2adef0c2bba87c137c5caa5d6cf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:48 +0000

    unit prize intend activity interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
