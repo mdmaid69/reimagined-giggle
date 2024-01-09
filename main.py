423e8b652714500c9f61136972bdcdc7cd3a5124
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:00 +0000

    journey escape anxious important for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
