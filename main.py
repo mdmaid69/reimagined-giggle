343fba4de5cf9f7a22f4d0e16cdb1c093dd4e562
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:37:02 +0000

    broken acceptable phone public that

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
