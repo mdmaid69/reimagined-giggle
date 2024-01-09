400460e8e8c0772a39ffc71968e3c78ef7e0a774
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:14:34 +0000

    rest square help about add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
