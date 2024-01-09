4f2e7127925727c58c1cb4bdb4c2bceefa464dab
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:04 +0000

    arrival arrive communicate form find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
