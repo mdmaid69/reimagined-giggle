26f8410543d3b508da5d02cf8441e81b265685a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:23 +0000

    fast willing arrival curious even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
