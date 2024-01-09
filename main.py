0976036db19f2b36685c768edde4b580cb81c77b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:10 +0000

    all pass gas certain image

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
