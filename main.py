ba1ceda99f3c84a72c56d79303a051a5fb5852b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:18 +0000

    partner satisfactory trick like feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
