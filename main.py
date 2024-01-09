c63cac9cde98df4aea0458118e66cb46eb144ec7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:43 +0000

    spring activity though name event

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_typecode(array):
+        return array.typecode
