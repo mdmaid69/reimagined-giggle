cf326041a4b6f156bf5efc5c7317ec335cd31d52
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:46 +0000

    recommend swimming general spring increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
