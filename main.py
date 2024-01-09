d62f05faf26f0a15e135a860e41ee35cac44f645
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:28:52 +0000

    Nn tone develop read through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
