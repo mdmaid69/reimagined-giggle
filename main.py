b58535487bc71e87537d5c83a5a1be572d539fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:22 +0000

    create bright extremely politics abroad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
