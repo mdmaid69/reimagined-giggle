ce4ba85d57362b39ec5496e4a887105a3d339783
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:03:34 +0000

    lonely book watch deliberate dead

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tensorflow as tf
+print(tf.__version__)
