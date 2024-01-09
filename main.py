0cda2c0f18676402d56b000d9f34e60db374f099
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:40 +0000

    form wheel set cold instant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
