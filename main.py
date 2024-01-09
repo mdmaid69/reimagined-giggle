32a6b7f010a333b09244481ea614e1ac9e54614b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:04 +0000

    season pin cut combination attempt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
