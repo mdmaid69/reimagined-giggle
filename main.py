29e10874f12265dec164dedfbab32dbab182e0d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:21 +0000

    possibly ourselves hang ground stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
+  def calculate_area_rectangle(l, w):
         return l * w
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
