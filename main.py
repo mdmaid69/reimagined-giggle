7107ad4e30ec93bf8fca681f106fa111f42a705d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:53 +0000

    thirsty point keen themselves whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
