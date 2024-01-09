970f48e989a3f12391e216ab52a27c57729e43d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:14 +0000

    564encourage nearly away tidy emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def get_current_time():
+        return time.time()
