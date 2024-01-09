5eebe8ebda76d6bb59ae2a64053ee653e0f2001e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:29 +0000

    design order total against suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
