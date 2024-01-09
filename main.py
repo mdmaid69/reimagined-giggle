fe41dbc8ef457aa22040bded81869eed7de62f87
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:40 +0000

    deserve declaration 566end badly comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
