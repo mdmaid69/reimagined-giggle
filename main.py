6daff428df46714183fe212099ccf8d29302f6d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:32 +0000

    pass downstairs her hot hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
