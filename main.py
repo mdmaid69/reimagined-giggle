48567075ecc716f709fe36c426afc7130ce19ef3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:43 +0000

    deep hungry some share they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
