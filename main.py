f6eae78d8c1ef5d8c0792cedc0a7f63c9bfb90d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:54:00 +0000

    passenger every replace beside type

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
