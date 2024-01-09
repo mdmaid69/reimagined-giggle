6c563ed6a545d1b7d061adb609069fe4a4ed4941
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:14 +0000

    explanation wall backwards may up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
