bac825ccd8fbbf97f0d0b03aa89d804aaca3f86f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:19 +0000

    from type wild check begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
