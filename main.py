a4985025c8eff97a04ecb75e1a8af8e03d17b25f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:28 +0000

    score complain cap declare comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
