0ed2d369a871692f94c49be691988d40d29d1a38
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:08 +0000

    onto criticism luck command worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
