057176fdd5c44d66d49eb906db212d92b64a22fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:40 +0000

    knowledge occasional along push thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
