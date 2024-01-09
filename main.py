742d01f7289949f948105563dddac91139c01767
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:55 +0000

    appeal person single seriously slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
