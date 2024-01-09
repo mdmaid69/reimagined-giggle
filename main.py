1d88097d7688f14457befdbab1b8025dd2268123
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:11 +0000

    react disappear instant avoid 569enjoy

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
