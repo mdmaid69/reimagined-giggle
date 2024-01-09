046831f3f039c7d64da872607d60caeae3bf08d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:47 +0000

    put admit movement chance demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
