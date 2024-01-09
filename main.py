7d7687c579ff42c0dfb9a48706bdf5cb05602e75
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:57 +0000

    geton interfere finger early tragedy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
