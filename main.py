f46d2dc3511e995bd9a7f559ee90fc126abe4ca4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:55 +0000

    health justice off tense difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
