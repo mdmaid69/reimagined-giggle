aa7b91fe8eab2213379f07b2ee519a64228f87c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:34 +0000

    far rubbish eat relationship break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
