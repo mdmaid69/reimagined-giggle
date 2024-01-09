f6ab035e71db5327dab7430b0ef7e450ec1d9ec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:35 +0000

    mend middle action mind everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-for i in range(10): print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
