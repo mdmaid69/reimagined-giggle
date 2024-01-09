d265be51783ba5e5628a8b39a4fea880e6f5ef47
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:49 +0000

    earn produce favourite cut swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
