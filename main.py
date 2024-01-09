b1afea6e2ba8b2a035eacd00d1b105c0994ef366
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:54:26 +0000

    motion problem payment hurt string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
