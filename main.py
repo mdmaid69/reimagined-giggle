5347278260cf675fd0a496e8c15563d04d4124bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:07 +0000

    degree last offence old emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
