0be102ef165defe9aaef824ecb3078615a12f455
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:42 +0000

    nearly fortunately empty plan sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_palindrome(s):
+        return s == s[::-1]
