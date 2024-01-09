572a53019f36743e8f525292c68ff68bfcc7d115
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:02 +0000

    connected curious pleasant confidence slow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
