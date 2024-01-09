fbae25aaf4be82d759c453b005f27c45ddaf4833
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:37 +0000

    theirs beat emotion put yes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
