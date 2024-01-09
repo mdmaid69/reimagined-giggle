31da23b04cfcd0b8aec1491cdfdafa24c4659b8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:15:41 +0000

    top breakthrough load recommend flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
