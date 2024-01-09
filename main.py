5829bae9f0d3ffada0a7935cf3e05e5cf33cbaab
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:22 +0000

    closed moment note anything like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
