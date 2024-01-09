b44c22c5a597f725d41bd6980771a005dc3dcd09
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:23 +0000

    wheel love already method road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
