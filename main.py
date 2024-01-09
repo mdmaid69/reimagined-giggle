eaad589ea67b30fd462011c496967b5cf703dbac
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:27 +0000

    price show walk final turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
