71392ecc25add0fdd991e509fda828476e7de0c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:47 +0000

    stand step here project call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
