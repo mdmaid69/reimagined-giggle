04503cde5f1d140e069af930a60bb696fa0de5ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:54 +0000

    dark scared precise turn off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
