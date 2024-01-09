ac1737de2e335b209892b021fb13362d1e58522b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:47 +0000

    surprising chance record common Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
