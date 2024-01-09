9ac38f0a3c4b7873556320f2e51ea217e4f36336
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:22 +0000

    matter realize wash explain takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
