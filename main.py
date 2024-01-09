cfbe42cac3ce9523d41265d0dd1d5fd534f94f02
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:34 +0000

    house tooth Vv burn excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
