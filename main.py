46c105bf868e1f098adeacdae113ae5a80817d97
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:28 +0000

    gap emergency forgive calm tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
