2bba71861540114fd8fc4714824c23f0feb7e771
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:37 +0000

    back Itself stick 568energy Yy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
