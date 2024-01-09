31006a3e4f546a3f975a24400dc47c5aeab47c02
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:23 +0000

    temper positive sure start honest

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
