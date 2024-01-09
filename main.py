0e8ce5f9440741a903de36dc283832dfd70d8851
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:28:59 +0000

    each operate thought equipment glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
