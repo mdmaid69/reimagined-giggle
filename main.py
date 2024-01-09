4dbcba5f7f5ffc7bbef0356cba84b8a57d0b472d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:53 +0000

    drive shame hospital possibility remove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import random
+print(random.randint(0, 100))
