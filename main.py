d12ea08c41668a03db0e1029da0a327332d4543f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:24 +0000

    hit even safety defeat make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+print(math.pi)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
