9a525f86a149c3daab7fe6edfce00397aa9a0d9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:27 +0000

    second rest forget head centimetre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
