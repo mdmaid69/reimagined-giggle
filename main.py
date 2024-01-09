230afcf3cb580df35abce6a124b5ba0491023021
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:34 +0000

    short rarely though across shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
