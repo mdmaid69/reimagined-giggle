9ac9c674ab06e6fd64bbb4c32cbe7eb3c10fe284
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:42:48 +0000

    pretty steam friend add tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_power(work, time):
+        return work / time
