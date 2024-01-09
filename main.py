fc3e01d764fac054f2df815b47ff0588a54bb5cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:55:53 +0000

    mind object fat regret precise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
