12de089d78107ecdda5885ef37b5984a56c47231
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:31 +0000

    music finally floor narrow view

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
