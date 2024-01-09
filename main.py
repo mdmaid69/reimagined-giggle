7c60013cbcd01fd2ffd5a59271d1411942c880f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:48 +0000

    persuade none control right statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
