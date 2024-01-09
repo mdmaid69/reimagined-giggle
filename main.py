17634cce4e508e566d557bf0935ffc48a7c01871
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:27 +0000

    taste wash turn able produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import tensorflow as tf
-print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
