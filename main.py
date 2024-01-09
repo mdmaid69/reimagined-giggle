d9600b6743447386dfdbbf2410e4779cea654a1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:39 +0000

    bury suit plain their care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
