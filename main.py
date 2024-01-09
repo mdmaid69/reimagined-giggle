59b88fe3c67872993d6fd4b38b68e684e9800827
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:14 +0000

    fall except regret him shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
