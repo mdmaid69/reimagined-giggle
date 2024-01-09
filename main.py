e27057ffebbd47b622359c8a91310b180d077411
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:10:27 +0000

    apply active behave only pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def reverse_list(lst):
+        return lst[::-1]
