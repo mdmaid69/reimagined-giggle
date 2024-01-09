981451dbd15775d2e4d5b7040010c1571ef31840
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:51 +0000

    character think patience book whole

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
