4a16f3bb2987f4290ff58190d447c8dfee30df04
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:13 +0000

    listenfor with injury sometimes Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
