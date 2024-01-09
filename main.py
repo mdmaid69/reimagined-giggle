f77368613491d3259b3a9239b8cacdfca2bbfe86
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:11 +0000

    common agree Ss holdup bin

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+for i in range(10): print(i)
