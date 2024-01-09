d1991d5179686990641753bed3e9ad62233df6ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:09 +0000

    runinto tip shallow last again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
