c7a433c198100ca27216975894f9817cd8f2eced
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:03 +0000

    welcome upwards against lookaround nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
