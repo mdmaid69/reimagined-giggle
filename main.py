07a4947957b2770dc9852292a508beafd57f81df
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:09 +0000

    afternoon like wrap lookaround try

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
