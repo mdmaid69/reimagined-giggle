3991124ee95de884e682f7163aaa0b47d2ab361d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:52 +0000

    disappear lesson trade because hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
