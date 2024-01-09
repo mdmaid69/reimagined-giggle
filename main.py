37f963f0b8e02511e105f4c7e0018a17dfdd89ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:28 +0000

    occur touch communicate hold art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
