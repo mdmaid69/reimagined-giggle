3092c7e6f7dcb0ea0dcf79b65231fc374cefbcd9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:09 +0000

    operation shopping increase imagine thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
