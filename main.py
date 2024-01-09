9761c2a383cd9ba3e68ac6fc75a82911a5311864
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:28 +0000

    disagree variety behind personal build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_float(array):
+        return float(array[0])
