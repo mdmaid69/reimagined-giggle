d171f2b641cdbdf7ec0ee648f8c5ae7d5dc9f586
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:33 +0000

    maybe run forward flat sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
