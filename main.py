27cdfe74985b42d3b1268bd1476866372bafc218
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:03 +0000

    apart dick lookup concerning ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
