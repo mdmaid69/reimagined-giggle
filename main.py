118be04a5ab3780e16d30788d6df571b3cf41ed7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:13 +0000

    correction figure content theirs shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
