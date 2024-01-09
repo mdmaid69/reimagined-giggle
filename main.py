caf6c41477ea848d02cba790409ad58d83b39bd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:50 +0000

    part patient occur soak understand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
