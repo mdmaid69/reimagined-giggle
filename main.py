ac16ff14a3e6193eaa7adf911c6971706090e308
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:42:48 +0000

    reject talk majority loose comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
