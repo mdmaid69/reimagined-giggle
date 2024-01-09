511eac23b15424a5372f92ac81edf423d33980ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:39:14 +0000

    worth dangerous improve wide edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
