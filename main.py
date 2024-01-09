67e587cb525e2e5e36c68fc6cf7ddad6358eba81
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:17 +0000

    behaviour comment instruction offend wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
