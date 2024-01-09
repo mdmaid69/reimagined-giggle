ded4e0a02f13d62b47686d9b7ff664c857a5ea19
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:16 +0000

    finally spin please Dd criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_error_function(x):
+        return math.erf(x)
