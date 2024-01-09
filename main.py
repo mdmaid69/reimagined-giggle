4a72755f9b7e95778731e902f07f9a486cd74bf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:26 +0000

    region occasion dry chance account

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
