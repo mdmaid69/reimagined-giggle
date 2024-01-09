3869cd61ceab2b2b18617ed28a61d60c71c3c125
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:22 +0000

    pause defeat flavour search work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
