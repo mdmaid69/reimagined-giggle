b9b38adf3a7f210da6d4cacca6339b3e2e2dd2de
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:13 +0000

    height appeal us pick recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
