9889ca55aa49829e8ef43fca4a4c18198772e18f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:05:58 +0000

    variety shut crash official operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
