95bb454223ea3650f2143e09b9cea0299807fd18
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:52 +0000

    turndown face less excuse business

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
