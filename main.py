32b16367f98a0b7c2025829955ab8e5240ff9fb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:31:36 +0000

    drop into class unhappy defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
