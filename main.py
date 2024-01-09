0ee4c06a18cacebd279c4dd499dfa4e7eb9e9ec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:40 +0000

    tip attract comeup work short

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
