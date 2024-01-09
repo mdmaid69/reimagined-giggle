aea4d1c62d18568ed2a40f02344c364a39280d45
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:29 +0000

    excuse excuse road minor steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
