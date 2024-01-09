53df6c839d95d5e401780fe7859eb82a8f031e33
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:19:16 +0000

    guard anyone pity difficult type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
