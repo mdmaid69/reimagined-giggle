0690604bd9570f0e267c6369a381c9a675b58fd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:45 +0000

    payment responsibility deaf side float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
