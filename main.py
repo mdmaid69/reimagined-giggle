c5af4135b58af76e2bab7511723c94bc99af5e67
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:59 +0000

    help impressive point opinion bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
