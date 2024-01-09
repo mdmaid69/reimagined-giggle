c6676b4c912321e4fa7fcd4a05ac90ff2dcb3177
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:37 +0000

    middle seem float room estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
