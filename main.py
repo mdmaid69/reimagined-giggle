178824479d0514f5fa847b2b038faed01fb933d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:00 +0000

    we centre string getback character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_speed(distance, time):
+        return distance / time
