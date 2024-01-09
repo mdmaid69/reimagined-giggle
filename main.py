f036e2a465997e55b6d55958c0df4d6b66ed29e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:07:59 +0000

    want compare clothes thanks beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-name = "Python"
-print("Hello,", name)
+  def get_base_name(path):
+        return os.path.basename(path)
