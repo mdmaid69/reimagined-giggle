9458bb694a41fe6f7686056901a8ca592d9aae63
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:34 +0000

    considering treat score kick brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
