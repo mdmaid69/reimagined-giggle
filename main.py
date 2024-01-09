c0fb540165f771aeac54e56cc40a6a4a87600550
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:42 +0000

    my talkback steer straight include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
