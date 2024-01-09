b1739ff630fec349cb1d46ee8327e909b0b265ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:47 +0000

    fit callout stare habit shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
