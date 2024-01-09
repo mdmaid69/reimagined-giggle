a332a80b39d72d82538443ef9d00610f2a3df970
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:44 +0000

    immediate distant workupto rely wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
