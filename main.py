3a5add1113c4ef34796ca3f9f019624a1a3e347f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:20 +0000

    wild arrive kill pour any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
