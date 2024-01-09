9687533d4386e28f5224a1880e67ab9a7a699e2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:15 +0000

    near family future plan regular

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
