91ae793ee783aee0752e66182e6780014740514c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:30:31 +0000

    press weekend inform judge but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
