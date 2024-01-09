22ca4bb9e4eab3f9b0dcd1f57088d6b427ae6e5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:55 +0000

    rule know charge Ff consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
