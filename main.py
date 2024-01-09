6b55dc03f03b96feb9fd26a0e31d83fb93cd134a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:24:44 +0000

    like fond fit description police

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_power(work, time):
+        return work / time
