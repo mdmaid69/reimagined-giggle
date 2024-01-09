1f74bbe3a6b6ce09ca8550b64b247b366eda93a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:09 +0000

    act accordingto soon suggestion father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
