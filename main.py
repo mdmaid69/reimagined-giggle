47683114085859b3d43fcdb737038a14d2172e38
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:34:41 +0000

    flat press sport none equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
