bf6c998956477a7c8f31eed12fbe7ee3f749cfeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:54 +0000

    desperate flow drive across offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
