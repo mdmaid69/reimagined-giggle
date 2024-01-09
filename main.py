1e373682a01c0700da61d345c474b5f4015c96e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:54 +0000

    steer lend flat declaration science

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
