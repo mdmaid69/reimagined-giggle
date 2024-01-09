7fe928aabd11bff194ce5e86f452de8ca165b574
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:24:39 +0000

    load favourite snow health argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
