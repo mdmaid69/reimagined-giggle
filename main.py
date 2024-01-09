907438d50ed941c7e63456894ca6dd6d18f80719
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:14 +0000

    almost level complaint mention both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
