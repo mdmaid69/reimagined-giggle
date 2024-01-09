67e2a045308706c1a76e7eeddafc2b4a64d30b17
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:00 +0000

    second dream nerve wind space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
