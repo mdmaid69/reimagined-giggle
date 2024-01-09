daf5b08f0b1409c07b31d78992380a0790986736
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:58 +0000

    report odd first fortunately fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
