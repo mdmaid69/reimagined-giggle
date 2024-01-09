6f56fd65a6e9e8244797410dd1bf8d7c8af56495
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:51 +0000

    car desperate thinkover money him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
