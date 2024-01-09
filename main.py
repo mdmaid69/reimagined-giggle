246c997f51b4f02d17c4948896cd6c5c3df9c284
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:46 +0000

    collect and unable objection steady

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+def count_elements(lst):
+        return len(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
