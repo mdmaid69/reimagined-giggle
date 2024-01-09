4ca8c75c4e7e82b3b639637193f1d504ff5fb973
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:53 +0000

    badly sky bottom that giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
