21c021aa7856dd449888677f0ae7ea3d71a2f849
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:09 +0000

    prefer suggestion here round surprise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
