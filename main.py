15e4750960c5a27478feb735e87d63e6347e6657
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:14 +0000

    apart expect motion dish danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
