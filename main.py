01cdb61a5628002a38cfebd1b98186fe814459e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:03 +0000

    he brush save influence rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
