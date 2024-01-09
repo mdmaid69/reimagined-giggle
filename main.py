86f293dd64f86ff1c0ff86d0a5983c2a67cb1f75
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:36 +0000

    hit contents free bringforward benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
