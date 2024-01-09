89e680187b293baddec3573e949ae428a807575a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:57 +0000

    OKorokay threaten interrupt fun kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
