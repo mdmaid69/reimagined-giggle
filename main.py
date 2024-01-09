a1d23c9386d46d74930b865ec7ccb7b65e192785
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:56 +0000

    glass entry upstairs strike drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
