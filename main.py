0424c195f92d0ba6fe911a4ba3b0321195123e27
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:33:26 +0000

    success bread sight callat spite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
