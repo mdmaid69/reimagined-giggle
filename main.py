cec54d7673767942c87e0c425e83e0c712affeb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:27 +0000

    valuable connection belong suppose thankyou

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
