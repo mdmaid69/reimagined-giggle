5c06f0e16f5de96a69e592a9caae54aecd9f5a42
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:49 +0000

    circle slight goup claim side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
