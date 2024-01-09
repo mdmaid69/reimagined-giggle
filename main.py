05589c8185d7c46645bf26f8975f7d7eb6052e11
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:11 +0000

    guide left place interfere let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
