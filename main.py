abfa5a6ccf388cbd16c34304ee54345f3a19ac26
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:03 +0000

    yes smell education service confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
