9ab1626d7bb8b6a6a83883ef661d856b6d3dc20c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:05:52 +0000

    guard than shout ilovedicks talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
