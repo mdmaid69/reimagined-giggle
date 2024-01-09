8c4d1058a67bdfbd7c0364e913daae8fd803c249
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:56 +0000

    rob fly accidentally giveout inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
