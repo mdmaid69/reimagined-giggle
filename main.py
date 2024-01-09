70873033bed279a30e92957799c6bd821545fe99
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:32 +0000

    grow absolutely programme but preparation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
