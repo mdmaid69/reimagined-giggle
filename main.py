5e72c959e7edc5d3f1db254bed47c275a78eca9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:42 +0000

    handle relative crowded them side

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
