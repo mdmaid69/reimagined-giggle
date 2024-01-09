5b89df227f9b2dc05a9b22f429c61ce8d45807fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:10 +0000

    somewhere keen unite guess slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
