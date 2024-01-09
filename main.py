b55fa07a14243fdd653906adfa5b6601087edec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:59 +0000

    file dream hang chat stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
