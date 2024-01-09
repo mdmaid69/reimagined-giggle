8cc5c2b0566ff5e50a90ede8fc6ff7b49e8b9f31
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:19 +0000

    ilovedicks school shoot pay surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
