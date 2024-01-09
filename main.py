08eb4a7ef56167a2852a790a5ca31660f770e829
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:04:41 +0000

    satisfactory on platform money weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
