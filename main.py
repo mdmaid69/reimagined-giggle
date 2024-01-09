2611b675ca6f8d278ac2ad64c4a8904818748b18
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:13 +0000

    shock wear bend container block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
