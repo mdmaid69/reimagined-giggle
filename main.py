c346b1f1980e9d6329ee4281848fb8245f73ad3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:52 +0000

    sharp interview black no computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
