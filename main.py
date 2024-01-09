2a489f9c247b6ba47c156d9e52986b49ffa52ca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:04 +0000

    shoot she behave decide gradually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
