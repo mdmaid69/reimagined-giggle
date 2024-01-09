3d2144e95c2653d3b431f0979ea7bd0688039abd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:24 +0000

    maximum process combination 566end solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
