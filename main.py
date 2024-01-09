5cbbe65f6d6a947ad95cdcfd66df6b4490208dff
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:33 +0000

    home apologizeapologise pupil study live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
