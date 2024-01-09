a381bd5a8e9b4a4325c34f02e962e3ab97194129
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:00 +0000

    speed ball between cost because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+for i in range(10): print(i)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
