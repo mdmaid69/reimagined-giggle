20fedb32a06be6f687aa70ab3a18a7250e6ccb93
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:02:18 +0000

    press flavour pupil hollow stiff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_unique_words(sentence):
+        return set(sentence.split())
