43b3be6898102ed35218834c2c19982ebb180f04
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:55:21 +0000

    wander seriously normal collection list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.ctime()
+def convert_to_binary(n):
+        return bin(n)
