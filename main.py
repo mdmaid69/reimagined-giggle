75e956604c984a06873f60d179dfb3aa9a155f16
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:46 +0000

    behind star professional want people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
