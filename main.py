a5640019f02423a7dae3d0761eda79d9d0191c1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:51:02 +0000

    reliable lower star comeoff strange

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
