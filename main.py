b042c2ce90137217b2e4cfd9997c5b2ff02a60e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:57:41 +0000

    smell pot watch calloff hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_stack():
+        return collections.deque()
