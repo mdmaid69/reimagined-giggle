7d731ae322b934bd0acee455467287ae045169ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:22:37 +0000

    match attitude increase persuade polite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
 import json
 def convert_to_json(data):
         return json.dumps(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
