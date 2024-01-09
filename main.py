05689603e542f0d5b18b0a7613bf4a142caec362
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:50 +0000

    proud move achievement effort frightened

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
