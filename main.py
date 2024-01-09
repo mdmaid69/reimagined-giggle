f63cb7007a6c59c8485a7b93f505b0a3d828477f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:19:53 +0000

    experience motion bother tip holdup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
