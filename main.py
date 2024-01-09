27c400e6bee7b14d0cdf63ce9b15a52db14e6ccf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:52 +0000

    soft stamp impossible before telephone

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tensorflow as tf
+print(tf.__version__)
