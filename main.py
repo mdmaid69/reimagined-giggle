1b0018eb2c09908749060c69567201a929353271
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:46 +0000

    neck before row material interest

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
