4245247ed9434c10acb4333ab46ec8407780b3aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:20 +0000

    look only design term attempt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_octal(n):
+        return oct(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
