6809092f277effe0969391db6e14e88a9c8d17af
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:58 +0000

    glass callout now bar proper

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_unique_words(sentence):
+        return set(sentence.split())
