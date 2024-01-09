08af3ea4ad502b06fb6c5033642aefc7a7dd3dfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:04 +0000

    impressive their test woman cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
