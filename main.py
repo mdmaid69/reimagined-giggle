1b56ce95d734fda1ab04d80474295b2d39fedf08
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:01:07 +0000

    call authority under constantly still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
