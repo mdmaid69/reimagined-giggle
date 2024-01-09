dfc1492c9c30d63f48b88397834e7b8d45857b90
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:27 +0000

    behaviour brush because walk claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
