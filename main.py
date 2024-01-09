57bb1412f5dfa8a122f280c151d9f0af5f3183d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:07 +0000

    sure unfortunate comeoff generous carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
