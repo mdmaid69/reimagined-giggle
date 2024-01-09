0b79c0b5c08cf28f2bb611afe879ed31a99f7f45
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:59 +0000

    fear they process handle basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
