0a4fcce5ceffc33c57722c9831514d822b48c623
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:50 +0000

    command month out wander forever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_queue():
+        return collections.deque()
