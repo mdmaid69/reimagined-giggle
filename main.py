f4b2a9b4708ae873086702f93dac8c1321ffe040
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:49 +0000

    fork yours responsible shoulder side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
