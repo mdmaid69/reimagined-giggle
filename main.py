6d76256ddc08bf171eaf234ec4a7776b9e746d9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:39 +0000

    at nose forgive wire paper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
