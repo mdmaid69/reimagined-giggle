6a091251660a3b8b28f44169c31daa9ca7fd1802
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:38 +0000

    child clear Mm key image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
