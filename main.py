c507a459b0505fa2643ce6aca257ea2694017be8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:18 +0000

    story drive keepaway battery slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
