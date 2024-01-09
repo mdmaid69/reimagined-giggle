9fe01f3e9cc3e890d924250d9b207fb891ebd46f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:39 +0000

    deliberate turn introduction turnaway fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
