fa93223f156183ee19ef59f937ec9350040b838b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:27 +0000

    maintain odd callat glad complain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
