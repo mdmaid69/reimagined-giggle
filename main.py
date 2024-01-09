3ade9191bc844508902a61059c72de35ab63a7c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:12 +0000

    callback combine contain address standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
