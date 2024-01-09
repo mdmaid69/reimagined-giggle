bb389107b466dfbf54a89f91130b3c62d3e95be9
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:21 +0000

    fine finally your bush sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
