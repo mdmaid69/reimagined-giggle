51707273ae9d3f4e0d72b7c2ebae7f0de9fb0722
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:13 +0000

    delicate image trial treat near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
