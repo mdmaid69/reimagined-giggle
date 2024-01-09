462e9be693931ed2f2015d2c5fc1ea700d328f91
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:49 +0000

    present book fire question wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
