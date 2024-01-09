8ca1fda12636a1889f8ce1666a47a95467de798e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:07 +0000

    easy contact night search complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
