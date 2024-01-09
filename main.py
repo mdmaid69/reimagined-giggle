d8554c8d07cb6b6fbe91956e0bc38037355b0ff1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:38 +0000

    couple embarrassing match drink tool

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_string():
-        return collections.UserString()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
