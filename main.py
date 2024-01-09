2d85a2adc3070454841166fcc81afdc403463f2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:29 +0000

    police transport belief calm rush

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import sys
-print(sys.version)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_volume(length, width, height):
+        return length * width * height
