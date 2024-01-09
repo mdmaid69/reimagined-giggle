bd39cdea0c7d8049ac070dec715d4e28ccd5c386
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:14 +0000

    public goaway memory normal entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
