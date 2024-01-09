9b8292d0e563894cf8c68807e3bcf591e7d1cd92
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:34 +0000

    570enjoyable punish transport claim keepoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
