83f89f38ff6e30daf0d5b4c3785546abf49d97bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:23 +0000

    awake bath before quiet face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
