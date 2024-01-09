fa1812c43ee5540de8002c8ab1bd21aae424373e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:20 +0000

    mail bus quick bag principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
