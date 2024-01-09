2458ae81de72153f9b52ca9aa4dcc3c8d25f9a28
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:19 +0000

    mean excited simple skill tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
