ca92ddc3070f08910efde254bde32c0d87baf77d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:21 +0000

    fine precise cough demonstrate about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-print(math.pi)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
