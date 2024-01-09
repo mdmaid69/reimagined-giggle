af80e06c3ef017322cc804d66513077ea102c001
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:44 +0000

    theirs balance judge land raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
