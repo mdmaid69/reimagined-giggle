c2ebce2a9893aba8325c4efdc5fe5b9c159d3b56
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:44 +0000

    keepoff house movement violent sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
