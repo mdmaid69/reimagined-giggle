c2eed0b0231465a427551068eb5581edeeefaec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:07 +0000

    switch window brave possible big

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
