760d0dbbeba835d343ab4eabf400a690c98eca65
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:47 +0000

    copy separate dry runaway odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def greet(name):
+        print(f"Hello, {name}!")
