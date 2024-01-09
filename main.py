afc7ceef357a27836c1dd24c76e18e127156a627
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:32 +0000

    needle heavy convinced in urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
