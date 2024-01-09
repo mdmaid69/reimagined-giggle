0fe134e7cc38fb0eecfca696cc4030faa844d79b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:55 +0000

    principle voice single low excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_length(array):
+        return len(array)
