52ac522a37abcb845ed98d4ed90bcb0d5ffa461f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:34 +0000

    office next straight influence among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
