2d418f57fb9728e750495d8d28d9e598e19a5855
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:30:28 +0000

    pour test area disagree basically

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def count_elements(lst):
+        return len(lst)
