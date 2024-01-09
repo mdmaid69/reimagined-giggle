42b2daac2128b67feca1650f0f21c19e7be48af6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:47:42 +0000

    free within use frame refuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
