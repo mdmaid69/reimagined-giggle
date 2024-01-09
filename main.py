6b4b7c9be4493ab25e41caa8dc479f6ba867aaee
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:13:37 +0000

    finger circle satisfied protection mysterious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_bool(array):
+        return bool(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
