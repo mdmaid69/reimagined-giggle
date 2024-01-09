c4e2125239c8b2199ca502f9d7399ff5cf802840
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:56 +0000

    since talkoutof sudden 566end worried

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
