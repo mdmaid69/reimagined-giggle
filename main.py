3caddc68384eee061171599ffd1cba3e5f7337d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:49 +0000

    practical relationship enthusiastic become bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
