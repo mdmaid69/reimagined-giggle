90a174e26179c54f51825bf69fdfd8f8d2a3142a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:51:49 +0000

    holiday today tape suckme warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
