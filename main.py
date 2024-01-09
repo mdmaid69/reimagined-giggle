ac1c2df82398f01768e8bf06b098bce654071135
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:38 +0000

    connected minor sell town wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
