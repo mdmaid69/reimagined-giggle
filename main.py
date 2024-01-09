e347ce0bcf62c3a61fbc7c67bf3418ecc442d78d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:01 +0000

    combination rate mention future him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
