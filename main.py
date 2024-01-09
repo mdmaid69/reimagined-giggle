170171eea808c112556133ecb95f9391da03acc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:04 +0000

    consist wall count path now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
