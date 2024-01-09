072a53c5e091168e90479d133c49a22ac7d179e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:10 +0000

    air phone situation cross bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
