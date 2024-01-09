4f082fcb60bdca4acd10a6b8f396fde7f78cee4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:18 +0000

    threaten season beneath switch breakfast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
