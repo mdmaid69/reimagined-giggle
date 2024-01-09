d91cd5e3b174fb5657e8bf9a6dcce5f7a124ebb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:08 +0000

    trust grand teach lot you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
