c791b5a134fee56f836fc47c9adf78c16469b103
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:30 +0000

    strike walk display occasional extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
