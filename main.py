7c9851767e5a7216287f01892514bf4b76f4cedd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:31 +0000

    payment be achievement teach journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
