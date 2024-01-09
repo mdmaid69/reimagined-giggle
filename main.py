0d16dc0d548f1362ab9bf389f5ab240a09daf8be
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:12 +0000

    describe cry immediately blind obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
