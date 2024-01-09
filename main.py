f13622c76c67d70c5cdd0bfa1de50888b8248052
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:07:30 +0000

    apart clear slip shop excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
