77e867334107be6f0b79e580bf26cf6d763eec6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:11 +0000

    thirsty voice several petrol turnup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
