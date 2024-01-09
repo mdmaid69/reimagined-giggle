5b70b2b8f0444b0733acf51ac9a73b1b59c1b05a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:44:16 +0000

    bag press beautiful guess sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
