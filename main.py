452791ae6bc960223ca9e4b9071b38d6e16ff31b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:36 +0000

    effort march sky please chin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
