ed93ab195657f1f2652e160dc761a8fa4ba585c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:38 +0000

    lookup recommend wish typical closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
