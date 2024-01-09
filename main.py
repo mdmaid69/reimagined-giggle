19165fc879cd88a29ee6966d5715dd61696ae686
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:16 +0000

    replace authority dig bored suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
