541417bacd4d72c43431e053ebd5b50e751bd529
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:07 +0000

    guard drop goup below previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_list_from_array(array):
+        return array.tolist()
