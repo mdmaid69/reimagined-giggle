9b20ce04334a53d7c361fd8b6a1185a36a3806b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:19 +0000

    call remove port basis mine

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
