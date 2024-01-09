db640c767f4472ffa663b0214034e76195b26aac
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:35 +0000

    turnover rather fetch deep treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def log_message(message):
+        logging.info(message)
