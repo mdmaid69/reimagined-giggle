941f423bb76466a6a445c504d78c877e6050cf45
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:01 +0000

    expensive tragedy basically concentrate cry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
