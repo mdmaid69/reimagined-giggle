efbd1050213f8715ff5e916dad0b08aba9629f22
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:19 +0000

    lower wear major news shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
