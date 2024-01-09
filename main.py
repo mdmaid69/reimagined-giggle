19e1504bcc98bab643259aef3a101b321e5a7221
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:29:26 +0000

    to hole team spin handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
