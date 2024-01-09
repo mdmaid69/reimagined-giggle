7906cb7b8e1c88de060860c2823e10fc477b4838
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:50 +0000

    satisfied join face importance bath

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
