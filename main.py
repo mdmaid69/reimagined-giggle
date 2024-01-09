28edbc5ef49b3efb582aa6791b742fbaa3cd7479
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:56 +0000

    shall idea shall Qq remove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
