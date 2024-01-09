69890f95a917f56c96f5dbb92bfdb70790c967dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:20 +0000

    object spread relationship description become

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
