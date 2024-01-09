1c4a6590e29a11eb1418f89b63351fce885a4473
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:35 +0000

    bunch certain money hide method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
