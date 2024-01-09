e401ba2b8a34e45abaae5122d0fb80db6c5b02f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:52 +0000

    measure murder print mine forget

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
