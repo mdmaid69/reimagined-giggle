1d54d328309c5f5800fe963238e4686a1d47f543
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:34 +0000

    steal hurt content unusual pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
