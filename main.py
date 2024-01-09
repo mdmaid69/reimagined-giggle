d2029e68d269f3ce965be1f7f8244139bc4aebf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:15:38 +0000

    sharp information wide doctor term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
