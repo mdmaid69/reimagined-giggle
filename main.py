164dc66daef98c1910f38f476e399fdc8dead9b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:12 +0000

    view disappointed push change contact

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
