1b5406748098615d02708d7559402504c0dec240
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:01 +0000

    poison service check reject paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
