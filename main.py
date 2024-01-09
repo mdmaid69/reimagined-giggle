2034571377166d2aea090a28d45217562e4878d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:55 +0000

    generally inside finish pressure move

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
