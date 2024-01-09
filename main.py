a467ed2c6dd4d010d5cb78d29a356cee40c35650
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:04 +0000

    worry unfortunate failure treatment tonight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
