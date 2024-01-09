56dd48b89b4ecca47859f350f6612995acf10c60
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:25 +0000

    connected spot mail means like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
