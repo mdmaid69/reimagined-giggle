f8fc9504c4806d2de3a4c828fe1ea1a5a35f86cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:27:46 +0000

    fasten flow pretty load together

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
