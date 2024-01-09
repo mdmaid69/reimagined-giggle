391ce47247f42162c12bc82c6412e2dfb2627a8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:48:57 +0000

    my plain over meal wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
