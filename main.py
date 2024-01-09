9e6e9fc448c82ed0cbd1e5984d7a17ccb11bcc20
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:53 +0000

    about runout ear trust mass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
