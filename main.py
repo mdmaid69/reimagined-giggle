f2c41da2f5a110def0df4a2b70b1aa0c0d9a9c7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:09 +0000

    doubt pattern strike fire refer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
