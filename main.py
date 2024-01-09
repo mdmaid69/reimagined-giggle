bf72192783ab9cdf341c93de0627e015c79da857
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:12 +0000

    turnon we strange colour fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import time
+print(time.time())
