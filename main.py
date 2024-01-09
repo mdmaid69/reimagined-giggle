fc0f85b529ed12cf80413eda281d6774a0e17168
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:34 +0000

    project bringback contact pupil 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
