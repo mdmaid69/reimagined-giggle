e222229c0afacbf63a9f47107b6fb68c8f5ec610
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:54 +0000

    delicate sky sky train fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_bool(array):
+        return bool(array)
