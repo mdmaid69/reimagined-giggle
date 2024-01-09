c6461968f21841ce54bb29c35de1f95bc021747b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:39 +0000

    still dig step surprise pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def remove_from_array(array, item):
+        array.remove(item)
