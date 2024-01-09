f7e06a3fb6282db96d15fd423469ad75d2fb3176
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:29:42 +0000

    cut else in long than

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
