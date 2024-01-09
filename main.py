bf637f64f4117c379479c13d8963ace6d3ac3b1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:11 +0000

    high getoff mix path join

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
