5febf467ef062aa1699222ea92104dae83b088f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:31 +0000

    exercise dark agreement scream majority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
