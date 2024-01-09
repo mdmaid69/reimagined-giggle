2f8fe580135c6cacce60a158374c2e2719d06574
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:42:16 +0000

    involve finally introduction fact closed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
