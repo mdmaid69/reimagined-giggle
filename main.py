a7e96755007cc34d8c05b66e289bae80fa26d0e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:59 +0000

    frightened result name ignore back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
