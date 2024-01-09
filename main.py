b9edb30509d1780bce9391556d828bc90f0c46fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:58 +0000

    getaway bag no giveaway react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
