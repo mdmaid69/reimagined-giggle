284bf98d43a93af303e9f97db2decea9b824712c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:19 +0000

    cough lose story typical argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
