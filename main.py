bc8bbdd032795c9c8ac9495d4ccbbd1a3607b1ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:39 +0000

    their stick small near ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
