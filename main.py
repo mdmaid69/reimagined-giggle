6218bdd92630337ce737c8fef81541fcefd4d54e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:49 +0000

    turnback serious since forward keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
