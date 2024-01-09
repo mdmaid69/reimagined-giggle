d0f13f6d9dfec16bf03853ab23db14512a888c50
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:53 +0000

    cut heart our person left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
