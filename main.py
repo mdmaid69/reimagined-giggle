f60e2c9a29e317c172d7e2d7d2941082b47c1323
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:49 +0000

    gift willing fail record complicated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
