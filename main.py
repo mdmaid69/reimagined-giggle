9da72717f51f953c81bdba203b230139979c190e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:07:50 +0000

    female blame easy rob whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def count_characters(sentence):
+        return len(sentence)
