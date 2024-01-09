e586eb137775363be61ffe856c797826bcec19ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:41:34 +0000

    fast bar guess really disturbing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
