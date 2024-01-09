60d0fb2b7fde76a7ba376c56fdad671f1f3f7469
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:12 +0000

    around then gift negative question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
