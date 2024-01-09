8c5699ae2aac1e84fc6f12eb93c4bbb35623fee1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:42 +0000

    charge fit tap mad machine

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
