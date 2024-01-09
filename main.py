00d3e8d7cf4f844247992705a282d8f92619600e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:20 +0000

    display point news ball thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
