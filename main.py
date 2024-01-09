43a2dd97a53d1a1ac2965a4f070fe301e538879f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:31:40 +0000

    major town course cut drive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
