a81513079519f5362c8336a5b5f5075cbebedf3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:27 +0000

    forget habit opinion design 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_floor(x):
+        return math.floor(x)
