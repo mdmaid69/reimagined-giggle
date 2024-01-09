d51e89acee3cd3116eaac0df9cbfb35e0e63ca61
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:25:25 +0000

    turndown along star order cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_list_from_array(array):
+        return array.tolist()
