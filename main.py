f4436f47a127852c9144eeb58d779bda7be2bf8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:49:31 +0000

    Tt hollow bath attach spend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
