947b6251870308af93041e9b5c8ae5005f2df4cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:13:07 +0000

    meal second direct crowd by

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
+print("Hello, world!")
