d79b063635a2b6a812b1073285d7f61d2840922d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:27 +0000

    able bang take square relief

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def add_numbers(a, b):
+        return a + b
