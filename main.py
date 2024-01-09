6100b363a31dc71a0e7a29aafb1301b695cc285b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:14 +0000

    value concentration nice react priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-print(os.getcwd())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
