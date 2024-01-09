0debf8bac495deb299bd177bc3a6586e1a0c4f56
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:56 +0000

    slight lookaround mend row save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
