684bb1c9da1861f06b36c628c5fb583c78b36de7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:15 +0000

    part among manage smile occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
