0f2cb9c5e1cafe6d805efbf9830201df1946487a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:36:24 +0000

    professional contrast avoid shelter fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
