3de83d980c633b03ab51de37f8d3a773e3a3d2a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:12 +0000

    fun place responsible rough serve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
