9c383a515029130b8d3b0d51c70e3f43d72b5f1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:00 +0000

    pupil feed mark limit forward

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
