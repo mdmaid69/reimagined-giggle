f0bf05bf416c70f08eaef104b732a41a2500131a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:12 +0000

    friendly inform law peace secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
