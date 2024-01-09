59e2b782e7ad4c65a29a9cfaadae548d8f26d293
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:13 +0000

    thick love phone excited reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-print(os.getcwd())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
