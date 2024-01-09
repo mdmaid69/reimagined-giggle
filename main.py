a7b087eead2364cc06cd13ad7694ff922753e4ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:49 +0000

    unfortunate only soft blame possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
