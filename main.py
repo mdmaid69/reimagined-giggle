f68a5212c841a251971f427d1f3bc7af5e05fe0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:04 +0000

    decide straight ours period loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_min(numbers):
+        return min(numbers)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
