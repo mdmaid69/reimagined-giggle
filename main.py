3cd5e32cf99861420fc480893cd8f3d7dd5d3e3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:42 +0000

    spring label foreign far transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
