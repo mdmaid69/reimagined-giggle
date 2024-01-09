05194760d9ac540e37cc709a4568dc0d18132b1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:35:09 +0000

    old declaration sick stage town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
