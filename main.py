344a499379c4a37d14572e0be29fec6f72522cd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:46 +0000

    scare do unit declaration cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
