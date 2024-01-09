0a9cb2fc281055289bfaa1e9323bbba0b7671c83
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:19 +0000

    adjective damage clothes represent collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
