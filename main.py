5217a2deb4321bb87d0857b22eae620b4015161e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:37 +0000

    excuse strike stretch safe row

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(5):
-        print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
