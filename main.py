43e0ec9b5cc34073e83fe0368f0dd15549f73686
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:16:30 +0000

    exam call breakin bright takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
