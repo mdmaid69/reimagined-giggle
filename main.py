370da46dcbd1496ef973bf275752c0af2fd62e1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:48 +0000

    tidy heat doctor love finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
