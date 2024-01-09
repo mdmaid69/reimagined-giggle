a62670fe26cc599e0288f7721394cf5ba372481b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:45 +0000

    borrow hole avoid related keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
