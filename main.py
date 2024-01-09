24397b0c9f0e0751e7b4b29b412de12ada9b13f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:36 +0000

    clear pretty out sail switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_current_directory():
+        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
