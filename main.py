867a169d552e6a3547f06203560199b55651c41a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:51 +0000

    shop lookup estimate unless tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
