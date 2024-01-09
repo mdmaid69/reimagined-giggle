8c6149878a41f322137cbaecbfca4a277432eb2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:37 +0000

    reply finger anywhere this hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+def find_min(numbers):
+        return min(numbers)
