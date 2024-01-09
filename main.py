be8abe70f4d6703f6d475b9b45d6e152573f4766
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:46 +0000

    sea rest guess everywhere expensive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
