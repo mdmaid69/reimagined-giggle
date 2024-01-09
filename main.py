c718143fe4d1cbed0eb34b1a78e7b154efac311e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:41 +0000

    straight suckme beat she language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
