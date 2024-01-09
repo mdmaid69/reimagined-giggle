5bd493836b19eaf34aff7ebf7b2949394b6f02b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:15:11 +0000

    team cause warn label trap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
