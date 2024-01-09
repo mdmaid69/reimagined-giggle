cacb26c7de3c4a4af2189df24d8a1d0a5285756f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:57 +0000

    generous song dance prepare band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
