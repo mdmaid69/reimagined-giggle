05f328ebc440579c53a82c49f18af82de65b42ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:38 +0000

    unit instead sink minute illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
