4fd48f6809e3ea2956373b61efba22e2dd80c984
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:56 +0000

    undo forget evening content which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
