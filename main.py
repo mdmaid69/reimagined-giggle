288a281fecd5c2456e5b747e605fb96c28e1fd38
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:32 +0000

    petrol once gift quarter ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
