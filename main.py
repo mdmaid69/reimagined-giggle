b696f5fb0653d51531379c82ac88e933c6c421b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:48 +0000

    permission far cope fine behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
