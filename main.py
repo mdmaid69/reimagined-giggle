14b3b54891e9c961826bc8679128a5f6fdb22068
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:43 +0000

    temperature 179bite clever stranger honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
