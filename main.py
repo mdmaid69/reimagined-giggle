5056c147c9c27a03b8c1c6366806eef567768b3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:38 +0000

    town mistake uncertain cut meeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+print(sum(range(10)))
