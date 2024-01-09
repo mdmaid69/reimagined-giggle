6ab4d0a3b93814279edd83e4a526631985e302dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:59 +0000

    feed shout exciting lookup show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
