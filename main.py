2eb1716de0f483badb58dfc9bb26f50a3e0bf839
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:42 +0000

    arm accidental complaint tape guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
