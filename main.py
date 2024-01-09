056a4203f03e25744860bdc8723bd242211f56fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:52 +0000

    better pollution firm term complicated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sys
-print(sys.version)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
