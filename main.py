117b5c93b0f8fa2a217018bdd07382197d9dcbf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:16 +0000

    laugh building dry cope live

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
