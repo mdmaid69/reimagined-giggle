8e6016560524b5df30de600cc7dd0e8227a7dd1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:11 +0000

    crime slice this lie correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
