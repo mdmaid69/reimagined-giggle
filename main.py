9a6c0c63c2a01b6b61a05ea36fdb0a5ebf4c95c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:42:32 +0000

    goback(to) familiar save remove fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
