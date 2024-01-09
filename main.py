3aedc5f373dd64902547cbaddf94faf60b57af83
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:45 +0000

    unlikely full steal today blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_base_name(path):
+        return os.path.basename(path)
