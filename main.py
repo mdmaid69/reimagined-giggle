643f86d20b078c35f0ad2abba3f1a5b24ca698ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:30 +0000

    skirt lose Jj touch comefrom

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
