e82f6aae943c8c02eacf7bfea45cace7d8b56a7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:58 +0000

    design explanation confuse possibly lock

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
