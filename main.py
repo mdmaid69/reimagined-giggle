0309eaa90733a04d217f1eca80b9b8024864490a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:59:27 +0000

    standard fire level prove again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
