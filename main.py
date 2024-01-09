dd0de7fb5de8a485cf5019da255bbad1c934e369
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:55 +0000

    break middle much complete disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
