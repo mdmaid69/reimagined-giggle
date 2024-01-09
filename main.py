4f9f64a490d113ed735762b6d126374e489a9922
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:42:00 +0000

    history knowledge comefrom turnoff try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_power(work, time):
+        return work / time
