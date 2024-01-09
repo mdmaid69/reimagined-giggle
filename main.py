d6364fb3c1663873ac7fa39fbdce72dee3315bbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:23 +0000

    mark divide dance argue phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
