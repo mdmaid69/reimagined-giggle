62391d629c286326607f849276c6e54cbe85de4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:27 +0000

    nerve snow finish lookup rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
