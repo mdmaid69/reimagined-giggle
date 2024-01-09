c812bcc72b16a2834e2e70c13439132b40b44334
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:09:48 +0000

    previous pity beginning blind now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
