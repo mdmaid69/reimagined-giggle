fd0b3552afc16ec31908b5cbc66ab466929c358e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:09:16 +0000

    extra narrow confidence welcome in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
