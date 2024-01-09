80e7dc7b6f49ddae5c1b86c5a4564ee3b1305397
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:03 +0000

    peace weather stay damage advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
