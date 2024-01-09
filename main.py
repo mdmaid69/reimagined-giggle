fd048ce3eefda0c6986cadd4ea05bc65ca30b325
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:52:15 +0000

    argument table slowly official public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
