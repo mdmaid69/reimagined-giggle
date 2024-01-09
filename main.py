7cee54a04f21222172c87b5e472bee566f3b45af
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:29:18 +0000

    genuine argue escape smart each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
