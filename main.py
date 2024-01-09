27979a67237a1aa98efe0afcd05d24300aeaf819
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:47 +0000

    need name offend spring supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
