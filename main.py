5281f5bb384e25b29cb44857eb05a788d3e3711d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:20 +0000

    under light refer mind would

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
