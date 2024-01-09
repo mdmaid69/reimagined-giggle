578dbc1dc8cc110b00fbb448f7ed04259d2c6190
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:04 +0000

    anything arrangement obtain keepout play

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
