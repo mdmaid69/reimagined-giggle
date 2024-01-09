183dbb63bc95bc4eb54a431592d19b1f841c13c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:30 +0000

    whisper day convincing interfere zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
