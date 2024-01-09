1227152c3bf6c70bb54baf16a6914de58ac560c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:04 +0000

    get achieve ring good unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
