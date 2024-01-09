d19b36c33625b38fe67be788fe0f0b6770919682
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:05 +0000

    brown confuse please official Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
