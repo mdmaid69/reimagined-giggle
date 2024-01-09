d32ab40b29dd344ae2d93b1e3f48a41ee8a39534
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:26 +0000

    ask proper matter common happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
