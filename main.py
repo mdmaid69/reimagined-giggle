99b5d5448cb76c5c3fdd98d26e714bfdca1fa373
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:09 +0000

    welcome school soil flow argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_item(array, i):
+        return array[i]
