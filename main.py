5702d6331d61ff113ac525c1433ab7e44971b1a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:01:10 +0000

    answer by plenty tap stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
