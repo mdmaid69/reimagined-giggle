92d7913baa4943212c04bcfe69f5976637e999f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:28 +0000

    charge disgusting noisy degree limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_area_rectangle(l, w):
+        return l * w
