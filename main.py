c966681020c4635d30b6481827e1be18c8d0ad50
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:09 +0000

    thought autumn deep calloff takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
