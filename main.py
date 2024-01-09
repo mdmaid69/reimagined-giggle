3eac358adb71b0665333a8f6fbd6c007f7145ea1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:42 +0000

    seem machine Ll spot spot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
