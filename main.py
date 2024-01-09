f19da380cb19d85ea1c5fa5ad1784d34243c3fc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:11 +0000

    normal card pause strict trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
