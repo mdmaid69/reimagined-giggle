5b6653973fe626dbfe8735476aeb71a8a0335c93
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:07 +0000

    long safety professional centimetre extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
