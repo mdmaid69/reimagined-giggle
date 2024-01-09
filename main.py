13259f6083f2932d1a21f9aa60676dc4dbf086e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:34 +0000

    firm shall transparent business phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
