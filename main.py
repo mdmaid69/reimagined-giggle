8fee2c8b89b5249300caa2874cde900f09137762
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:32 +0000

    brush sick later please see

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
