82d9f113cfd1c3dce9f6dd218dbaf014acf3b0f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:31 +0000

    great myself nerve software argue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
