90fb63c6d56c2060aea880a49d42fccfbfd8604f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:05 +0000

    reply dream shade dinner refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
