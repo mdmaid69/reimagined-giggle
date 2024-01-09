6b482c338ef49a64b83f5ddb40d218e57f9a61fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:16 +0000

    complete finish properly fire start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
