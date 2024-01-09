cba63215a4b5e74769ff5cad6fd8763dac89f9c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:43 +0000

    hope angry near guess lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
