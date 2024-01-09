14cf51ef5fdd519433b318bce2f7ff117d3e6b66
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:12 +0000

    turn loud law piece pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
