8c36982bf64476e70712051517015df98a6008a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:57 +0000

    consult passage from please comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
