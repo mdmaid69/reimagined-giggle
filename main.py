2e309c21cef49a349bcd353a5a099f435ce86e3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:22 +0000

    hospital say glass very within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
