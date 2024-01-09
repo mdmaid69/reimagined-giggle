65ab9313d895a187889d00bc4ae52f2b5581244e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:21 +0000

    generous avoid even apply somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
