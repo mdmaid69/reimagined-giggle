dd03d990acb5e07770bba3d238643b50ded367fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:38 +0000

    education cheat impressed attract perhaps

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
