dc10bff7ca1f67734183c77fabc00ff0922dce98
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:46 +0000

    rich sky distant dare post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_area_rectangle(l, w):
+        return l * w
