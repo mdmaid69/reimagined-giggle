456a313e4a713ae22ecb6511885597d3c4dc6269
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:19:57 +0000

    please law breakfast fit however

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+for i in range(10): print(i)
