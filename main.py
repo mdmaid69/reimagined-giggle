e43a83782d3179a03c122ab363dab7a106359a61
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:04:47 +0000

    occur draw snow bread regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
