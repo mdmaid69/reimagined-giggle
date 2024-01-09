6aa9b185035b552d7e83e3c24917490effc2a98e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:07 +0000

    advice introduce leg early them

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
