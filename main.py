5845e8a14f1e4aeaba5a0bca87991d059f71dff7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:26 +0000

    mind pause careless giveout sister

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+print(math.pi)
