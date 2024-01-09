4c58c28accc5a5dc0115dc374ea43a61705423dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:08 +0000

    mistake trousers machine law offence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
