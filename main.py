54892f07a2d6654f5835140cae43a51424d26ca2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:32 +0000

    everywhere secret correct pressure emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
