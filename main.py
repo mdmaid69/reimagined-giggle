94dccdbc0ae50b605ad829aecad65be49f662102
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:11 +0000

    community slightly ordinary problem character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
