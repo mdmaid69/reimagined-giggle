ff7d90427081ef940ca35c6f1fd1691d46bc8413
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:03:18 +0000

    frequent wheel lookup debt breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
