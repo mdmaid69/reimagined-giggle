c97618b89ed7b5ce48fa500a47d6f1cff14ae50c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:10 +0000

    swimming crash choice keep active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
