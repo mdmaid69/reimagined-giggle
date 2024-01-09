4ebf4d09288bf6f8e0bf1706844c0264a5c0349f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:08 +0000

    rather often fortunately careless touch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
