001a919371c67d263c45c71409f17c1a97b17ec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:43 +0000

    gift demonstrate rarely respect fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
