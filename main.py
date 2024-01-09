88afbc73bede1e9bc492df6262a72dfe4be9a473
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:07 +0000

    dangerous of subject hurry stick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def greet(name):
+        print(f"Hello, {name}!")
