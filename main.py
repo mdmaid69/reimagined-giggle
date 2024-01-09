d1ab3848b2203e4c89ba562ab18f2e3fb9619006
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:53 +0000

    contact agreement skin welcome disappointed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
