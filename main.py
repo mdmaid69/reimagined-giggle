6883a90846eb4610fb2f6e899f5bd12fb1264677
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:19 +0000

    straight translate trip fight cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
