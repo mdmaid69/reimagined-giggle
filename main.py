1b016a8efbd067e98ab64284b9a1ff38439e37a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:18:34 +0000

    strange beside passenger empty rare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
