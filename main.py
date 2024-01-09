2caba9e274c138bbfbcdd0660cc5339f379a28a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:10 +0000

    pencil act taste official talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
