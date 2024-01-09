d48aac8737f26bd2bf3567683e41cce06878826f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:44 +0000

    flow trade guide when reliable

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import csv
-def read_csv_file(filename):
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+        return json.load(f)
