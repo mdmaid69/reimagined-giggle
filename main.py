23a7cad0002488b661afe9ddb46a7fb789f5b8c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:55 +0000

    purpose remark familiar anywhere carry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
