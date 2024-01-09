73290d6fa9516d516a734556b3ee495fbd27f1ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:22 +0000

    nonsense stir complicated plan awake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
