e711ddbeb5c8ee7484753a0b3dda1a0c81ded818
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:41 +0000

    beautiful deep sticky certainly entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
