876a2746b5305011ff46a7773a4ce47811ce8e34
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:45 +0000

    tradition plan Dd doctor spot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def exit_program():
-        sys.exit()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
