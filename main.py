261a28aaae1e05b3182d6b52eb7bdcb88dc2208b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:38 +0000

    yes feature takeaway either plenty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
