7d61c1f492aa3a443a2c72af8d63febcdcea22ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:03 +0000

    feature rush card fix combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
