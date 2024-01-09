afc537304a22ca1e12e49501ef917a2fcc743ea8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:15 +0000

    plastic scream friendly comeround keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
