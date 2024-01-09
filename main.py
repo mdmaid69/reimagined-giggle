39ccae43a0e08a90a5a1a48caae66fcd837d9581
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:52 +0000

    angry old word Pp decide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def is_even(n):
+        return n % 2 == 0
