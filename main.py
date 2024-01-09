7c7e54102e9043c6038489b61553f57728799e9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:00 +0000

    job notice threaten thirsty wise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_hex(n):
+        return hex(n)
