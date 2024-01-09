9267270c30a8f99fbb0ddcea5e4dabe3bc770ed3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:38 +0000

    sure our pour deaf introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
