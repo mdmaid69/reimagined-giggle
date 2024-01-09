dd3d0d5d2950bc24828f14700ddf148a7e620f0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:36 +0000

    male compete step voice long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(lst):
+        return max(lst)
