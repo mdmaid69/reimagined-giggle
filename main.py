2316592d765c96529c0a15a908114c1ceeaa86c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:07 +0000

    uncomfortable actually team education border

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
