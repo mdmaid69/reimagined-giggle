a58558b0667674f486c48abfe82ca6be5c9703a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:45:44 +0000

    before hers plant talkoutof their

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
