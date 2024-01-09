dc59890fb629a14f6795aec8e0967e5f43152ace
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:36:29 +0000

    give traffic bet stamp church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
