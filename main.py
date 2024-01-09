60f81cda6675a53104db3004c8ea2a8be82a0fae
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:41 +0000

    callout bread welcome bored or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
