9a752e96a34d64074af59595c78d59d662fdc259
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:44 +0000

    relation downstairs silent provide right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
