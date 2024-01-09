084680dd5d3e7ba456983ba9cd8a5a38f238d395
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:07 +0000

    important tense takeover trap damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
