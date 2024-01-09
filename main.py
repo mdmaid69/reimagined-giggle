5299028f23d4b86d942c4945ccd3ba39dbbd3449
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:49 +0000

    lift fence entrance ground bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
