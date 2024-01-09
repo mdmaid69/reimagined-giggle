783b599b5e8ac7588679481cbb14f769be0752f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:35 +0000

    ground house pollution chin service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
