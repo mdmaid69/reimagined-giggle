4978d48837f1aa8d94f1b121f89179403bb468da
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:52:57 +0000

    piece 567enemy little like without

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
