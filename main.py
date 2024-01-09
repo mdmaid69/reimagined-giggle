ae88fd8b5c7c3f3ccfa5e585e66568a91657a7bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:06 +0000

    wild until expression ourselves mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
