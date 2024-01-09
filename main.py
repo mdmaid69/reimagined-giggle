3bd0b2272ed50f222edf364253da0b2c242f7386
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:43 +0000

    crime national issue tap goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
