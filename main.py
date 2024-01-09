c057d4f9f60d216026a7cb24413b372aaa574d60
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:08 +0000

    point brief so talkoutof appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
