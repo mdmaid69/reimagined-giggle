e4b7938195725d35b448d647755970b4e92d39fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:48 +0000

    possibly public keepaway bread exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
