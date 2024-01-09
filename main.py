a352287432dda9b4036e7a2d668bd311d2ddbec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:29 +0000

    valuable exact physical busy professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
