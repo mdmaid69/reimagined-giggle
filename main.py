d3aa8e1813a7fae8440bcda5dae623d19574e8de
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:24:36 +0000

    interfere break somewhere difficult street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
