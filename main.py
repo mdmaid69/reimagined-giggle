2bef401b7eb3c588a075020d3b94adaae71abb53
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:35 +0000

    official guilty report dish stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_even(n):
+        return n % 2 == 0
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
