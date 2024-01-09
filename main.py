63deba8214c26e18b94d804b524979996bcfb644
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:04 +0000

    fly provide refuse middle will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
