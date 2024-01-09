4919d42b0ba9c2b33e809940e37f202c74088449
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:03 +0000

    now price 571enough think join

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_repr(array):
+        return repr(array)
