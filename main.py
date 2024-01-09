bb5a5e005b4dd0eccaef1290a5e29b622dd77b5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:16 +0000

    climb achievement care lock ring

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print([x**2 for x in range(10)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
