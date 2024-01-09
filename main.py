496a66c69e0ef9473536e1e61ef25c63fa73af00
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:14 +0000

    sound fond yours waste reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
