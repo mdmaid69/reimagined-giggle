64c9afa89634ae0458d42cdb44df2583b2d77f80
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:03:33 +0000

    score yours point grand up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def is_even(n):
+        return n % 2 == 0
+import datetime
+def get_current_date():
+        return datetime.date.today()
