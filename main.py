80a36d4eda7f8d0cb1ced45f2eec8279e14cc2fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:14 +0000

    shopping should marry mention habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
