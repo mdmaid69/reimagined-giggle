c9f21c8234f4336502f31a01092b44d8e61b1bac
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:53 +0000

    independent wide start mend window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
