6fe22c677972e026051bbefc9c3d0ff2c1142279
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:24 +0000

    key accommodation idea legal famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
