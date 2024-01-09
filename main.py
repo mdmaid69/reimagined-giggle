f5f770079a755762a1da8f68e788e83eb1ddd85c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:13 +0000

    respect Tt complete small keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
