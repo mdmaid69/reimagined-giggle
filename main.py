6f03e627f1025b30425c5ee048f35349b2a73173
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:06 +0000

    tune talkoutof hard beautiful list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
