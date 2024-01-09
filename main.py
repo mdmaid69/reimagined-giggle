5e107cf4078eb318dad05e507e9ec111a2c3db89
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:32 +0000

    compare control godown water her

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
