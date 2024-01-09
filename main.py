1ca1d0d68e4416915ff4aa0f8756fb887f92e76d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:03 +0000

    file careful any cinema even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import sys
-def print_python_version():
-        print(sys.version)
-  import sys
-  def get_python_version():
-        return sys.version
+def add_to_python_path(path):
+        sys.path.append(path)
