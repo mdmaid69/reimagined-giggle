2b57d74ab85ab3e73ee4da35d9fe8467fa55cd38
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:59 +0000

    own bringup spare form church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
