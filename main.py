f92ba65de208ddfdec59284555ca09cd8174f69b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:50 +0000

    spring earn hit mail sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
