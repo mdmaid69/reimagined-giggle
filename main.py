b3965c9a88678458edcb7d2f26a003d0b4e69961
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:14:53 +0000

    wild issue last cost miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
