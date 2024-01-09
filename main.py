a69b1af752c1cf38e179d2d6201df36578f93ba5
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:18 +0000

    task different second school declaration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
