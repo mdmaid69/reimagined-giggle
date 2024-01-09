7dd653034b9711cc5ac0a60d23ff985d2f614104
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:03 +0000

    love occasion myself matter borrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
