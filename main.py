faee5df6905e79eaf653c92c0c73e6e1a63bc181
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:25:09 +0000

    accordingto luck operate up factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
