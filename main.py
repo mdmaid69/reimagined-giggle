0bd1b73929a9a87550ed322e016614e87a4bb917
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:13 +0000

    complicated between all Ww sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
