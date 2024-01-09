7f052f61e83103ca77aa6908dc9cdcfbca70b958
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:20 +0000

    joke tradition answer answer best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
