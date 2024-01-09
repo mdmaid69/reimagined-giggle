d6c03904d76b9dbc30a2d2d08ddabd66786b34c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:52:52 +0000

    runover load lock tone soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_acceleration(speed, time):
+        return speed / time
