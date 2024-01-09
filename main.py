b7995ce854bd8d55491c229a20b638880936f450
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:52:01 +0000

    damage chief long similar cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
