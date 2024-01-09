7988a65f00f9a5f7c156fee44f306e6885c33018
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:02 +0000

    steer wheel people of instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
