6ff0c6b98c2047cf82db702dbf80538ec422b0c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:36 +0000

    warm straight regret friendly impressive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def exit_program():
+        sys.exit()
