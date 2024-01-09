6137a84b7e3b3c442aabf20aa2ff2447b9811571
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:34:18 +0000

    regional grow speed usually interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
