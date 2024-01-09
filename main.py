4f37fa72a9b299772bf53aa4cacbfc4917df5294
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:34 +0000

    sky broken past stop modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def get_array_as_bool(array):
+        return bool(array)
