b99b7559ae4c16c4e9f2dfbce498b8ddda82486e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:31:49 +0000

    myself arrival way record apparently

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
