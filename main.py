78b2f86b8fc1a6b9243083fd153d796678bf05f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:15:35 +0000

    wave every breakthrough getoff agree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
