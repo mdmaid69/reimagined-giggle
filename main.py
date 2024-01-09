401eca2b83bce95d91302e457e848996aff7cbe6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:16:50 +0000

    valuable surprised scared popular statement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+def print_python_version():
+        print(sys.version)
