50cc4580d08ba5c1cd3389802044f403d13d4c43
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:39 +0000

    wheel behave imagine Uu flavour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
