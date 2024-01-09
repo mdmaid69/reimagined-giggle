541eada2adaf57106f63daf86a57356456457f5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:15 +0000

    model lucky Oo explain silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
