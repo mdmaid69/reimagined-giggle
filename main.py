469cda88b3b3cd51facc0349d04573dd40567128
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:57 +0000

    pain no stand use fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
