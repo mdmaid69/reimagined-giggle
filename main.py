bd98058c5d758b6342ddd4679e636115fff337ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:55 +0000

    before choice sometimes lookback(on) produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
