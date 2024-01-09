006cbbb013d15e69d6f188e358edd3437c1511d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:49 +0000

    please difficulty mess exaggerate down

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
