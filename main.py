f06824d803f7812b3dd2744ab2710fdf7cf65578
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:25:03 +0000

    murder work area reason Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def remove_from_array(array, item):
+        array.remove(item)
