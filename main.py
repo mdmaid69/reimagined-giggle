08e35caa8f7eeeedcfe2dad4e435b1f32b116c5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:40 +0000

    announce very secret visit violent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
