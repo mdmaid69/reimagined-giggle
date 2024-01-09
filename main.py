2d8e1ec46d2cd3333082de24fd922019ff743d62
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:21 +0000

    interrupt nerve remark pipe control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
