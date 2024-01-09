d401f9a2b27f601e02cb14ebd499b2e6d96032e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:51 +0000

    computer thinkthrough loose ability goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
