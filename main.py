5caea67655eb9d411557a3eb57c96ef5ce03a5ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:10:41 +0000

    clothes side time proud keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
