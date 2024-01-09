4aead74d8b8ac3cbcf7a5fea8839755c853de05d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:37 +0000

    switch unfair how style solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print([x**2 for x in range(10)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
