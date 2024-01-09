e9388edbca37be415f90c1f79b7476a0de83e90f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:26 +0000

    along lead butter sign Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
