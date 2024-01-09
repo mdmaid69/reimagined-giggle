5fd86a03de88fe62cf062a52d77d699a047e2e59
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:14 +0000

    value away extra actually edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_list_from_array(array):
+def convert_array_to_list(array):
         return array.tolist()
