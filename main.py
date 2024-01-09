3b839e5bbd130844efa534e078bf1831f871bea2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:48 +0000

    trial continue welcome turn yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
