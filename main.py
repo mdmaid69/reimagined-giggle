8bb182a305fcbd00bde0e239aa685fbeb2174675
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:29:31 +0000

    damage contain various worst limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import sys
+def exit_program():
+        sys.exit()
