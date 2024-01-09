2013657a5241f58afdb404f8e133213faaa14d76
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:42 +0000

    pipe success share spring second

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
