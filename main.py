da84ee962585db06c4b02900395033f3eb7d7bb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:19 +0000

    back freeze problem mark support

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_even(n):
-        return n % 2 == 0
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
