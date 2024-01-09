aba328f1c0d10b3c5b36c8a9671026b6827877ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:39 +0000

    whenever stop fit angry before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
