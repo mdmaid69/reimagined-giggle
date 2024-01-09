597d782d17e4933a5b113e5cfc22966024b37e70
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:58 +0000

    bet simple crazy criticize mother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def reverse_list(lst):
-        return lst[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
