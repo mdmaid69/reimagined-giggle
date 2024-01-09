fbb99be446c251e3d0548c03ef8bc312745b222d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:04 +0000

    giveback question join he getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
