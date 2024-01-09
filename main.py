30c91d8ef805dc3dc831bee38f0dec9216a7e739
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:24 +0000

    recent of achievement getoff rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def greet(name):
+        print(f"Hello, {name}!")
