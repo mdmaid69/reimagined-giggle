b5d2869bbe894ca4f26131ff79eb1a6951c68e58
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:42 +0000

    trick smart ride it voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_length(array):
-        return len(array)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
