b937079cf9d56e09c5dfc7b34838d9f8242c5c01
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:58 +0000

    turnover after strong either unlikely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sys
-def print_python_version():
-        return sys.version
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
