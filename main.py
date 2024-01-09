19d9ee0ecce1f5e63fdb7c40a5139c0cd3014798
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:44 +0000

    label eager crash pause office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
