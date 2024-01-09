94ca647e2b61b6e8c0308c894d5ea893283b8be5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:16 +0000

    shrink keepup to Yy opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-print(random.randint(0, 100))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
