1fc1771b318c6e9e066482d2b65cd2b96b4cd503
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:55 +0000

    hour at ill pair finger

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import csv
-with open("some.csv", "w", newline="") as f:
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
