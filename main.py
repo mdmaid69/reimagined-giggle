91e05e675e94a58a7ffbfacdd0ffa6835893aa06
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:44 +0000

    snow only dance pull basic

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
