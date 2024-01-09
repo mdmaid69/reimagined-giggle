9ec00816f8c8f62d7b9b314f84bb28f462fea125
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:29 +0000

    special compete typical shame crazy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
+def reverse_string(s):
+        return s[::-1]
