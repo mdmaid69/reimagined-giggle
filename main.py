ced7532620a0ded569030353e75ee48545cf7df7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:12 +0000

    tread bush tune callout pen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
