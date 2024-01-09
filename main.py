96978dcad6ed37bb122a1de5ddcac759df9b20a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:54 +0000

    declaration never ball there air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
