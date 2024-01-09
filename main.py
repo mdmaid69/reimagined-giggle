2be30ad03aa29d9ee336526b16ec1fc680b370ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:37 +0000

    relief anybody so exam information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
