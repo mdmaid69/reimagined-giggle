4344e95a7e47168f7d15dbefc589c5beadc0ae83
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:20:35 +0000

    car bottom aim pain show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
