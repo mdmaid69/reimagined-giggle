88dc69f9862601b56943a08c3168ff40a6742f47
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:39 +0000

    knowledge ahead fear light common

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
