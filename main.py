a22e91e8911d9a74867478f6504ac95401f07d22
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:43 +0000

    there what surface slide guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
