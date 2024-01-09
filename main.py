e18f8ab1d0a0cce030805cc71a2524ef39666ddd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:23:14 +0000

    twist no squeeze fairly treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
