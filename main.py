7609b3993ed28ccb204309eb427b47ad19a38977
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:43 +0000

    avoid notice spot Vv out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_work(force, distance):
+        return force * distance
