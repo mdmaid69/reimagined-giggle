a3c0c3363184c879824d3181be32995b9510b53f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:58:33 +0000

    may reliable combine pain lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
