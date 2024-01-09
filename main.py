8f53acf763ff88d0de1a9a5b7a97adedd3f2c622
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:37 +0000

    whom shout remove surprise listenfor

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
