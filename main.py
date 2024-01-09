9ddf5da01a9c77e7aea954d2a30c67ad7f3a3315
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:40:06 +0000

    bye back left open quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
