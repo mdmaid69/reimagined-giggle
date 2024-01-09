27bdbb31f285fbdbc3dfa4f81711dc1914fddc96
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:01 +0000

    present fetch display step stick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
