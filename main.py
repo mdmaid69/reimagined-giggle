49b24b56ae912332c04abe2b27aad4972573d744
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:17:23 +0000

    why announce month organize underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
