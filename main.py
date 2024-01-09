6fedb77139fb487f2dd33f173c0955f33d10f8ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:02 +0000

    lookaround advise perhaps stop word

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
