8be1f0c9c5d2762358afd52a6a60b92312730ab0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:13 +0000

    interest somebody arrange child clear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
