2658b825a3217d2cac348e8a911e06a30ca819f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:44 +0000

    eye minimum turnup section deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
