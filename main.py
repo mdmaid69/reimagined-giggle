5c4e95ed9372932285e3ff1c9ad6a78599825b11
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:04 +0000

    family work fall climb maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
