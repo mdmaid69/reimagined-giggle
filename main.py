c60733cf569fe555ecf896c5ca18b9fc6a0d3f88
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:05 +0000

    broken safety recovery turn rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def multiply_numbers(x, y):
+        return x * y
