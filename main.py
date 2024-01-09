16861fee7e709982280f4a1117217a0a38b44b19
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:32 +0000

    generous limit original foot chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
