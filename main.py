5e14de068e746c2ea15479823844bccd65daf80c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:41 +0000

    pretty ball challenge concerned bringforward

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
