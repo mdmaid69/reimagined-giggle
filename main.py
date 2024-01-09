89ce34bfef8a0d5a6ce8fdc8e631480287c44d72
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:27 +0000

    comefrom ready whole meanwhile business

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
