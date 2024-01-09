bfd836e7d8b62b67b15da134118bee8768592f6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:22 +0000

    relation imagine tidy deliberately considering

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
