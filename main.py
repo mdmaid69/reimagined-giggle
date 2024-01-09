54308f016a9678097ef99f26ddf4f693ef42ba95
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:51 +0000

    neither knowledge fact too surprise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
