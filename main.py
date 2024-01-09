ce6d4d159ab79c8c1820cab125641c9367d510b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:09 +0000

    account tight start language journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
