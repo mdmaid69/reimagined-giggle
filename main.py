4d0269f2168592ddec00f9cf5174090c603674e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:17 +0000

    neck disgusting reply who miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
