2a3f3446660dbad21470905373fba7b71dcf6ef7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:07:08 +0000

    reply remove crash party keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
