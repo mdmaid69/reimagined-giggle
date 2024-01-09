4b69e761c7122e3c57683f9ffd65609d2b519257
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:52 +0000

    our keepaway result account fall

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
