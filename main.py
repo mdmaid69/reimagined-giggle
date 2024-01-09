676f41e013f466d4898f956306c0e2002f524873
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:12 +0000

    by but space beneath normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
