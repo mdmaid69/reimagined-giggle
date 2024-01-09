74b00ef78ed217a5f70b30ebe9302724366acd84
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:05 +0000

    free tip offend life quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
