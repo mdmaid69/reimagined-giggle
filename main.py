791bfa3e67b2840e4bf27dc48729a6a05b1d9472
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:42 +0000

    slice truth exam long stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
