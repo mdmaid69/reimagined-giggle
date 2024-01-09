b1f0f8982d146254d6533eb8e7de309c1d958f23
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:44 +0000

    proud big pot introduction mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
