ccaca45948d51a86a1f75a7ce7868a8c41fd3ae5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:29 +0000

    division replace guide actually deliver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
