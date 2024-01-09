6c1346ebbd3c360b88eeb04ebf19ddcb2ab7bbff
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:25 +0000

    street accident standard illness pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
