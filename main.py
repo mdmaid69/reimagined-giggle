2814945e7751ae6cef666ec71b8cc1d43bf52338
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:25 +0000

    comeoff port book yet neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_int(array):
+        return int(array[0])
