acda63bf9d8124bb06316b6e766638cac958e3d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:56 +0000

    fair flow picture press stare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_max(numbers):
+        return max(numbers)
