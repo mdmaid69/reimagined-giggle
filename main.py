27df0542fa2e70d81bea7abd96d0710055fea790
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:47 +0000

    angry tough push building shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
