044804ce20d028495295b59ddf1af875503cd3b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:10 +0000

    belong their emphasize history pressure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
