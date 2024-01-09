c5455f3ed7aac498afa54a947dcf0832c588f42a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:07 +0000

    situation happy both lonely downstairs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
