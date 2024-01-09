3df8fbc608f380a1e76ed5e607dcfb08116a70ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:22:26 +0000

    freeze splash dress people direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
