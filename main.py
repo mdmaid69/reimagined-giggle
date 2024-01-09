5e6f4b6634f695fabb23cf5e67b24a266fa9745f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:01 +0000

    pleasant advice dead fold impressed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+for i in range(10): print(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
