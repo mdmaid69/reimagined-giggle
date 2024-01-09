a6c64a82fa0d67fd1bdbf5c92e49a90362312b1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:03 +0000

    accept callup immediately night Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tensorflow as tf
-print(tf.__version__)
+  def convert_to_hex(n):
+        return hex(n)
+  def sort_list(lst):
+        return sorted(lst)
