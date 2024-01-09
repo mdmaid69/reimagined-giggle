fa35df9a9f5c8055d28586afc3514a1202033033
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:32 +0000

    Rr to ourselves description government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def divide_numbers(x, y):
+        return x / y
