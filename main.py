759744c7756e1a9bee2a5b21110aa5a236cc3c89
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:59 +0000

    target level excellent hide steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
