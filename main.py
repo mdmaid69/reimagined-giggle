9a41d77d1eadf66bfae68d583fbb9cd583d276a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:49 +0000

    moral story breakin out subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
