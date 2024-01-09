0c6b681bc66976a295e12455a99b6f7525b5a04f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:35 +0000

    duty foot stand crazy float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
