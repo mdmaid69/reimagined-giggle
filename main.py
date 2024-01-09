b4ea521a78a84f5f844d194a91f5ee5ef550a2bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:10 +0000

    tool watch worse many embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
