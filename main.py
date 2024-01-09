d7d9487a09b60700cfa4a65bbeedb1b7b669a8ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:49 +0000

    shape project heavy runinto own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
