ed8dbd7bfaab472089bc524c02bc8c7c561d01ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:58 +0000

    best rub his thick steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
