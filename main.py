0276aaf69820a6b0d86160a352a8fb87a0a6f64f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:00 +0000

    minor various transport why turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
