d320194e9da6f1d07a16f33655a2f4d0a9dc1899
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:45 +0000

    performance me bush holdout climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
