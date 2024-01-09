7f901f157001dbf8333eb0046d0e2dcd07e73fb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:10:07 +0000

    waste block sensitive strange charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def cube_number(x):
-        return x**3
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
