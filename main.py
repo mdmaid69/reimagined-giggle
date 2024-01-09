eb5217e9af36593928350f376acddb04e2389095
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:08 +0000

    all illness I welcome error

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
