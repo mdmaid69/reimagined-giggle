1f56049ad47daa8903566db5a68ce5461130f1a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:03 +0000

    tax big show valuable love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_odd(n):
+        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
