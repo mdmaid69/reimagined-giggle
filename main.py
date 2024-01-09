9e976b8393090c21ca10cd02ae286d2af030e7d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:26:11 +0000

    gap add real beneath sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
