9314f07ab33738f0270376c0135982d9f8e62b7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:59 +0000

    brain deep politics practise preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
