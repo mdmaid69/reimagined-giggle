56da02fbd39b00f3fd7c51273d766fa5903517e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:44 +0000

    meeting find hot sea strong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
