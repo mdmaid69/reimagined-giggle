8687e3d75f03e44c45fca0b007876c8b1f640724
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:29 +0000

    top purpose confusing central hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
