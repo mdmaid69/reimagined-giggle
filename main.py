811d56653c8dd0a02cabb0960753695b0ad1424a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:08 +0000

    hurry grand cool where advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
