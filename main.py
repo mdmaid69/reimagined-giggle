8ce43fef71681b6688574bee6647551fdc52b1fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:18 +0000

    power do entrance attract block

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
