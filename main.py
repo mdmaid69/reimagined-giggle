a84fdca255e19cb1922cefd26f3b5ec2fc1228f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:29 +0000

    skill comein such almost true

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
