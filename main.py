c596887dc23bff97400ade6ecce1f3804f1de8bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:12:39 +0000

    opposite which pollution cost sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
