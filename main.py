bf5ad768326ae5b9b3aaeeea877919b342d6d6bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:54 +0000

    club flat class weak excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
