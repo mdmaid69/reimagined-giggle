910ad7d28c85ce139eb4e49129892d7e5f986b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:07 +0000

    emergency protest preparation upstairs skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_work(force, distance):
+        return force * distance
