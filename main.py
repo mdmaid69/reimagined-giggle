893b44f7f87235064ae9d8da89b3508e1fd3f259
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:36:37 +0000

    foot ours contents spare impossible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
