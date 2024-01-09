3673d6c3d7e03554158a5dca468fd69a921f8b8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:29 +0000

    second spin stir choose over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
