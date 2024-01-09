16f87a094b6fa847ff81f7e88aa2ffe940b75189
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:03 +0000

    pity odd post major share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def cube_number(x):
+        return x**3
