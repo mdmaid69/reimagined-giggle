814193ff39c67e260bafd7bcf09b151f650880ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:27 +0000

    close scream do place operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_square_root(x):
+        return math.sqrt(x)
