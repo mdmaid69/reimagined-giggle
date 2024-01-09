4c260c9ea7cba13bdf01ad26e4c7422381e1a929
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:58:35 +0000

    getaway mine divide acceptable visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
