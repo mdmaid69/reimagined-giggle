b4f6b31eff1977e7d30dcfec572c8977e08eae86
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:13 +0000

    wear base explanation television exact

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
