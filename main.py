5fc0eca3c9f19bc66c8e56ceaa6731b8f9d96b4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:05 +0000

    bin soak goback(to) split behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
