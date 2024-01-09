30854730be64cef48c5f088fbec062eea8eeb818
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:25 +0000

    deaf male knock spread share

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
