ff61ca94f41e3216e240e46fbc2b57ac477be7d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:41 +0000

    difficulty delicate basic could fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
