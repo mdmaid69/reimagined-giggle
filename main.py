737a0c03ebfefacf998011648062ef1ca3e63d58
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:19 +0000

    relax lend step equal burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
