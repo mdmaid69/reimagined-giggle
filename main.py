f944b4a7a8bd5df39515682708801fb1675132ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:57 +0000

    rid different kind remember rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
