d6aecff856735aaae702244f24c63aa0f78d43ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:55 +0000

    apologizeapologise artificial second like innocent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
 def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
         a.fromunicode(unicode)
         return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
