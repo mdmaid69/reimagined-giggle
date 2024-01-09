11a9c41554fc05689ca73a9c7c8f5b5bdc031eeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:31 +0000

    will straight sale pride private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
