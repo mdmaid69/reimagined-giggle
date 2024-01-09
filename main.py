e1f311ab49c5e484ed73f6d6b170147c2bd69474
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:06 +0000

    development splash load lost cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
