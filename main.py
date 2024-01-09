48fe7871bf0c302c4a9a1cc9bbd73880f9eaf91d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:48 +0000

    keepoff load rule area forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
