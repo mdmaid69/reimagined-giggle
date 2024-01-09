69153661521b94e3c515d9da659145bbddd1de7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:53 +0000

    furniture Pp shelter takeout sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_user_string():
+        return collections.UserString()
