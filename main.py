dc7bbb27b3e64d19abcdce508bcf12b05ce7afbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:07 +0000

    ticket present thumb destroy bringforward

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
