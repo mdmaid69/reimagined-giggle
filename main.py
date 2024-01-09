2b97691c98bc411102b972b084fd5324f824a5b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:31 +0000

    emphasis wave bringup anywayanyhow why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
