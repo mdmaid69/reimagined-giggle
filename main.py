a998efa05cd1712d36219acde8127a578501cca3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:12:56 +0000

    danger shake equipment or dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
