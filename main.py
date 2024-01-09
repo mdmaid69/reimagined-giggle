9f999abe1b2ed3ae7f94932fb014b8dfb04eeea0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:11:02 +0000

    office naturally spare pipe original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
