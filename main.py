95203a7c03695c31ecbbfd32ecd525b553280246
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:16 +0000

    lookup bringforward split impressive improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
