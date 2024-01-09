9a84322417eda18f69c6e0bee7abae3fb1095aed
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:11 +0000

    destroy fall giveout radio hair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
