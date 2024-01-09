32751aac8be851840545c3a687978d7a27d5f4ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:54 +0000

    stretch sore escape capital thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
