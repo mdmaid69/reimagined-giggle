23de8d3fede6ef656644461c27f7f5929cf51d49
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:00 +0000

    real profit shock sound advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
