0c8262d5df651b3973c86641dbb1a555c7668c60
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:29 +0000

    boat birth whichever motion build

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
