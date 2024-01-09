a56ca11ba7b655f7ed38915a40ef8f47967d3801
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:47 +0000

    pause cheek tool moon quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_index(array, item):
+        return array.index(item)
