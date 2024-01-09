d15defdc7b2a1f1d1d38ffafa1e3ac5557e6fc37
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:31:45 +0000

    goout 179bite fire negative loss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
