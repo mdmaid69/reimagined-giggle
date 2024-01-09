02d5408918b48ac592694a44cec9aaeeeeb7e60e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:22:52 +0000

    uncertain introduce congratulate both prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
