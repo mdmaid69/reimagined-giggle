ec6eaef72ea07be3e9f4e309b04ba729c9debe8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:15 +0000

    moment arrive guess careful ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_block_size(file_name):
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
