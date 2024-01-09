63811c6db86cc2c19d55630b08f3abb720dd194f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:40 +0000

    complain within spot different common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
