f22fc5b575dd525d10a6a7ea3b7dfcd3b4301c5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:28:54 +0000

    trick lucky life bar left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
