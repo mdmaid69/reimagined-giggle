9efbf0ec18e5890ce9378123d9f53677c3b42d0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:09 +0000

    make left takeover shape difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
