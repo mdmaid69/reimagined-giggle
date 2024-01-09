009cc73209a81d4cb28b747abf354575e3cd679f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:47 +0000

    summer characteristic negative normal government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
