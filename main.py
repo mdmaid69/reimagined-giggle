e333a9a38f11ea1c426896ac4d5634cc861a4b09
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:49 +0000

    Kk on port branch pattern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
