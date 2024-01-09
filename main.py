3f3c431d26ae979934684c3d762b75e215bc855b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:55 +0000

    label and smart activity regional

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
