0dbb04931ea7c3536f60ac13292ed316026325d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:49 +0000

    invite invite feature rid start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
