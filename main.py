f63e46c6e9bec571ec48591c787ddaadc49f6016
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:54 +0000

    necessary for cross Jj exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
