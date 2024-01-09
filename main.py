f34b9c96026a408764df63cbcc214056e1328fbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:00:52 +0000

    they control relationship runinto tense

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
