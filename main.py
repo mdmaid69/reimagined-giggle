80ceacd28f90875acb7aa92d211e175fd7953e4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:41 +0000

    difficulty fire ashamed at length

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
