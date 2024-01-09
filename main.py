b50cb85ef2fecd765661719ca14eab55c9b4ad00
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:04 +0000

    comeout deserve but bank so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
