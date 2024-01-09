7234313a37e1269c3a412154c330a492d5d8c433
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:56 +0000

    careful taste season kill increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
