c98362cfa848beb91df9e5d28756b732f985e461
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:52 +0000

    trade create prevent afford protest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
