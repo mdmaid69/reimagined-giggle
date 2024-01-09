3c2f3a91cb0b0a24d145708e8c8cc915f1ef6ef0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:28 +0000

    mess combination at opposite oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
