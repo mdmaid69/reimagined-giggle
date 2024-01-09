ce4631db24c55172ad3c2366c25ad85748537f72
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:50 +0000

    pity butter sign constantly protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
