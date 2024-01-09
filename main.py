6f057dc07cc50a088ca541011055ec4b3487171d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:16 +0000

    copy gradually set land slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
