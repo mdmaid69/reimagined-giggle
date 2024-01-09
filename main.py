c15fdae753f8b567a27e057b76ca431f871938f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:14 +0000

    confidence 564encourage bye whole load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
