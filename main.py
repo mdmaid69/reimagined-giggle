2892ed8bfe4021ba19d44a02b9f6f71a7b8c7a68
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:45:15 +0000

    grade really mother tread present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
