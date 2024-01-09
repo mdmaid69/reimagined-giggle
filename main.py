7d196ce3d4394cd95a3b783babd8ffd6dca03a86
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:35 +0000

    most trust word system payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
