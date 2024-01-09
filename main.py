3b726a0acd2e8c29a9ff48da274961bdd3d3e2a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:17 +0000

    anywhere contact active start especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
