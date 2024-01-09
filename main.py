e23e9f1470f49e59aba7fe3d0313c38a28016656
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:08 +0000

    die introduce command stamp male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
