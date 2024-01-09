d6fc18593232ccded3f489ac8d58d6c7cb257cd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:28 +0000

    bottom still rule face Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
