e8f5f5aac73c90d91b933c795680860de4486c83
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:35:47 +0000

    closed control reliable house makeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
