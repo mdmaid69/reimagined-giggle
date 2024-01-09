72641813a3058d294601850fd2d191f25e2873d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:27 +0000

    system question short table sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
