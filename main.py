674c07507f5561aa556fc9920ab87495a2308bf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:52 +0000

    apart talk opponent quickly week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
