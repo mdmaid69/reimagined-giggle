fb96fdaa2f2ccfc0a43ea241462316efd74d535b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:57:48 +0000

    stage pronounce criticism lost scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_bytes_from_array(array):
+        return array.tobytes()
