34199f10f44f664b1f7bfca77a2341ed545c929c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:44 +0000

    estimate weak accident main container

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
