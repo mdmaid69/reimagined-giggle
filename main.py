43161bb402ac8a61233155576c3ecadb955041e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:04 +0000

    always loss walk cross branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
