a8adbdddf9a4c1cf164e180872c7878d09626ee5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:37 +0000

    bet challenge big today gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
