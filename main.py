bc459a245dd21c889ba0f447a10e5c5dbcbc791d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:34 +0000

    moral undo sit married that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
