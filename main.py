3f5dec71bd423234059d599e0a6601d234953388
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:23 +0000

    inform difference stir possession bringup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
