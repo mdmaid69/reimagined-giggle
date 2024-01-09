db081b26dd9a917f99cea855394fe102e9bc85c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:42 +0000

    object insist suit force cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
