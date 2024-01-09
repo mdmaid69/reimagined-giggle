7469b71935b9bd8073db74e662ae637792b64039
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:36 +0000

    motion me seriously pronunciation duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
