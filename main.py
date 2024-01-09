e8a4357111cdbc0244d0c2a3f1f6f7895da510c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:04 +0000

    interfere sky shoulder pot dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
