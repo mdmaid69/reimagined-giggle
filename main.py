68de8938b48e41bfdf7d465293e90c58c954d816
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:32 +0000

    obvious before consist properly afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
