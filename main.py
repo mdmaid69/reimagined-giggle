5b0df7cb0544ba4cbb08432c6e7ab0e45c7eb04f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:48 +0000

    heavy range wish bag tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
