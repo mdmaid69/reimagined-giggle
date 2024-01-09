e3844d24fe99ba8fd51a7012cdf9b04c2d195007
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:31 +0000

    before reserve shame welcome basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
