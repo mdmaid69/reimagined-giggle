8e3e54552d030d8dd9ea0ac0474f1cb6f4527c35
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:26:22 +0000

    ball undo communicate rescue confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
