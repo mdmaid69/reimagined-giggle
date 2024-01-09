08445ab86547e3c248f4bf4bb41a62af00331a5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:40 +0000

    which air body score break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
