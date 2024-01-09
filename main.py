21b9b231ee82c70b64afc539a4221109f9c11155
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:57 +0000

    Uu uniform usual die mail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
