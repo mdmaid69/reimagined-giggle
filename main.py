57aa86f9de239d69a4fd6a1901adf31937bf2183
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:30 +0000

    anxious admit narrow introduction sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
