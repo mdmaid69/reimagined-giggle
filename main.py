0ccce9731369d43869260e2bc94276021c196e32
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:44 +0000

    179bite drawing closely produce mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
