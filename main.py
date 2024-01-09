486ba99547a0b5fd1ef06322569f41b88e37fe2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:26 +0000

    very fill child brave nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
