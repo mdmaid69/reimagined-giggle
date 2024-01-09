c8626b090c96f8367ca9412b6adb9f4b96155dde
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:13 +0000

    involve connect call person mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
