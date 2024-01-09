a2b9d179a2337b8e5fcd809dc6e8661ae639602c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:50 +0000

    unhappy increase use keep complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
