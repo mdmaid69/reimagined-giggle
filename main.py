ba6292e1c1ef2ab12af6977ad059fb4bcec73e09
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:17 +0000

    part make us present pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
