e0ae5879681041bb309f2ee2d33ff53b334bcf1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:39 +0000

    cough train yours cum press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def multiply_numbers(x, y):
+        return x * y
+def reverse_list(lst):
+        return lst[::-1]
