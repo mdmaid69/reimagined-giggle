5c09c74cca9d5b1d77002746a66fa34e805893a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:35 +0000

    entrance argue explanation escape limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
