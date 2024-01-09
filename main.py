d9eca5434040516cb46766409ca89d292ae7c9ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:31:25 +0000

    danger maybe bush avoid deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_current_time():
+        return time.time()
