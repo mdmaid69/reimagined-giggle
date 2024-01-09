e74573165ae8ab1dfb765cf654a0cc7298395c4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:04 +0000

    consider Tt unlucky string disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
