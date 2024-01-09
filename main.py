1afc8640706d13df28a2cb4c86570c382042366d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:17 +0000

    taste soft advanced load charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
