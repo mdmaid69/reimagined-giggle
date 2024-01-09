53c280f619a5de137cce061834800fadc3a4cdf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:08 +0000

    view male tend reply gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
