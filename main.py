1d9f48304d9330d9acb0bab925864f57526e9d0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:03 +0000

    goodbye bright copy accidental Ll

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
