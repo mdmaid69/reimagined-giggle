43b65cca24c66853820be2c66acd78bb4e9cc912
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:38 +0000

    edge brush sound slice spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
