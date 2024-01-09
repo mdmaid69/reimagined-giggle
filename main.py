7182cd4bb1ea0ca14e1553604664e3517103aedd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:45 +0000

    refer slip fear fierce dear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
