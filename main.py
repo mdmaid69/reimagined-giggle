ea539b95eb6ab1bcebbadacbf10066d228f43636
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:26 +0000

    wear breakin thing cross aim

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
