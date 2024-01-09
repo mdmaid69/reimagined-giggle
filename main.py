d15194495f7dae17d33533f1d4ffbdf5f64859b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:55 +0000

    lie check smile answer summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
