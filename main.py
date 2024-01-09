822c40394e1235354a2003894bcfb3eac33f299e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:25 +0000

    shock squash motion comfort train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_binary(n):
+        return bin(n)
