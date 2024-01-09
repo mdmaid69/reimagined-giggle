61c9767d57c2b068b844a2cc45c812fa3526781e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:49 +0000

    dress deliver seat emotion fly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
