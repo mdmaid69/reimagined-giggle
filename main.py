11800b8f38d5e6de7be464d8bd259abdff35cdb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:43 +0000

    practise branch instruction callout he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
