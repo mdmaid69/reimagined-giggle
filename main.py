ac28a4884bd804b2720f93cad78228a5f1ac44d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:56 +0000

    juice thank tune question cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
