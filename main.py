a7f2f3f47bb455ea676a1d09091079901923febd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:20 +0000

    violence nice clear protection secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
