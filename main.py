89a56df270b60c4db1fc0be3682a661aab3b482f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:13:52 +0000

    fire joke price hope record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
