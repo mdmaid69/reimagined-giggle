1cc4a7c913f62798f61e4e05fb17902bda72740a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:45 +0000

    public group interfere contact Bb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
