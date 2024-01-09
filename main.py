24b09420de7d7f0f6144a4c1253f623321b83bed
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:42 +0000

    unfortunate evening test against like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
