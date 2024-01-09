724790b6c2df7777a6b08233ac556f619eb2122a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:20 +0000

    throughout hard ground goout start

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_acceleration(speed, time):
+        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
