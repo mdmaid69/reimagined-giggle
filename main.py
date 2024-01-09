36378bd98a06a8999858a365453735d0bd6c258f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:43 +0000

    report prize back build grand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_max(lst):
+        return max(lst)
