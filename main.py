8d7cc3e54e1db7bd720b8a2867ea3a5a15521ef6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:07 +0000

    at chance raw pressure history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+print(time.time())
+import tensorflow as tf
+print(tf.__version__)
