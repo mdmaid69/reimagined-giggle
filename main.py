d9d1a21a156325ef634ae005f5d15e41419eb8a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:38 +0000

    punish decide male break fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
