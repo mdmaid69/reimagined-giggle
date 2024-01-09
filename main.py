4d97da4f6046dea90862bb9d5d5796b599874598
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:39 +0000

    who left promise vote store

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
