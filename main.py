b6462b1ebc636c0faf7b98596fc4ac95b0ed6630
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:34 +0000

    us damage comedown relation consider

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
