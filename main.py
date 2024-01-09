d4c8f43d7f30a5b464d977114592da37629504bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:20 +0000

    before reason bet height takeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
