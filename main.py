eeb01f8c21b109c4a0830861582c3726cf1b05a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:45 +0000

    danger press tune notice die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_pressure(force, area):
+        return force / area
