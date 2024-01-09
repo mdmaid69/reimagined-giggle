d9795f574d6126f41c8d04324ab2a539759caaab
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:56 +0000

    preparation track each innocent show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
