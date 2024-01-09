9ff181abffb3e3b1cd0ff8b77ffd08aec2efbde4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:22 +0000

    minority strength likely somehow less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
