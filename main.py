774210e73f18e2e394f8556fe0338eb137ac006a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:27 +0000

    hold town runinto take pleasure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
