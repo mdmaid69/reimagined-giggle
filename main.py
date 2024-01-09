74b3f516a42aae0d6b0919e25e42bd34dfb40ef6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:11:08 +0000

    really worst talkoutof benefit draw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
