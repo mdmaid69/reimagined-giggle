2f48f01929d23b73cc2a17a37d5b9bd385242b57
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:06:32 +0000

    thumb shall lookaround fast defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+print(os.getcwd())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
