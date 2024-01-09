33364602ce2dc74ab1283390a8d00af2f699dc5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:15 +0000

    cry post shout want past

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
