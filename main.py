8d2cda0a241fe5a293165dc42603101836b8f7a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:42 +0000

    before comeround guess language bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
