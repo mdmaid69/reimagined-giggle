55e709c17eb6f6aaadfb8dc270872e186291ae51
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:42 +0000

    holiday tight majority my any

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
