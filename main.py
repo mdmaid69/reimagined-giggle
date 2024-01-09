e7cef9214cf8e7c3ea3bf2bc5e71c92e7b291daf
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:06 +0000

    study total something arrival force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
