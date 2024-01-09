61170c8f4409afb27668864b20279190d99f87bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:56 +0000

    spread spring union change shade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
