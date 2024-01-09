8a70e6235d2ad9331ad892088075985d341355b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:33 +0000

    sting divide settle easy gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
