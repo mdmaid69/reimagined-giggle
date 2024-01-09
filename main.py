419de384aea6172c5ae5e1a400ea4ff2051c532c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:12 +0000

    mend thorough joke dance imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
