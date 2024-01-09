4d714aa092109c1cac648af0bbe03c450fbf0adc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:41 +0000

    whole fit wear shock machine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
