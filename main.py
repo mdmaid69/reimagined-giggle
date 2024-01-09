5aed071c6bfb5cfe35b51b786e0e3abf11df0fc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:38:21 +0000

    label not during name show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import collections
+def create_user_string():
+        return collections.UserString()
