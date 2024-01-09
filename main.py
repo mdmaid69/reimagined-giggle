08d7fdffd9a747ce0fc2af73db84efa2894607da
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:33 +0000

    danger case doubt thinkof test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
