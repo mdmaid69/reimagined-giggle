cb3a34d7f023cad14c28a95a5b9fd48c23b23e26
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:35 +0000

    beginning since development careless distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def cube_number(x):
+        return x**3
