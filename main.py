6dcd69df6fa0a560d0ba86a65586a3351b05db36
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:11:57 +0000

    best pot thick smile touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
