c7c0da9869381adc6032d1bcaac31e5c18b35b0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:53:47 +0000

    tidy any suckme but damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
