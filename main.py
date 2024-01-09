fcdcddd3c307766c8f89d2b829bfba12a62b0f82
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:40 +0000

    Zz whoever scale respect impossible

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
