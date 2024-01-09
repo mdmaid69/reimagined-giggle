d73d62cc6f6992bc8fa1367715ac8299e6c7ac02
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:34 +0000

    visit dream cut fast measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
