5f98ffcc8d40216d37246101dc3af4bdfd1c4fd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:56 +0000

    open water characteristic hard precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
