425750e0e77825718cc84c0554c36e76dfe20369
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:26 +0000

    pleased wash underneath man join

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
