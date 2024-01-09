8d18acf481cf3af4862a6ed5c08d55a84ccf916f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:09 +0000

    final rough send false once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
