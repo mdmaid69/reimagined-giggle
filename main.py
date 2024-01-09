b9dfee706ad539512c6e6d382abb2c6a6af33255
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:35 +0000

    most sticky discovery fashion turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
