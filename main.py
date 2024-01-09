e3d50254affb0db26b480901e223ed4b125ae77b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:48 +0000

    explanation exit storm meet partner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import numpy as np
+print(np.array([1, 2, 3]))
