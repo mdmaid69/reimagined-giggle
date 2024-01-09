2347d2075692980860283c0c61cc7b2b905163d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:09 +0000

    shout cover partner demonstrate reach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
