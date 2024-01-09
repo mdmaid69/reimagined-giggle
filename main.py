ec366094a2433214fa4b6a33a2b9bbaee7c2d30d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:01 +0000

    reliable poor love female scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
