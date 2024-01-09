5ff7a15f4a3130b457a3e24073f55669e2789561
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:51 +0000

    admire destroy without pot throughout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
