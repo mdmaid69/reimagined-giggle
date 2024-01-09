1dd2b30d676b25210d5a2e5b95449082c057eaa5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:27 +0000

    goout injury laugh formal stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
