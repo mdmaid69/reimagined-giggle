405c230f67e0c3510600d06d958ba5a91bd4aafc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:02:38 +0000

    lower tap himself seem upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
