df0bc12734b366e8a2b98e82b081465d5c3d13a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:34 +0000

    wish their chase murder famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
