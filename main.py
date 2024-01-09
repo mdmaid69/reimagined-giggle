50747e2e6bf56c0e3a0f8aac9641bb6aa4c18d09
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:26 +0000

    comefrom punishment extra extremely range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
