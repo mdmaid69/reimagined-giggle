597203ee055fde63f3b1e5516d53f450fc87473d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:10 +0000

    knock inquire lose run impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
