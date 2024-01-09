0ac047ffe15f039a0e41644e905220ac5902ed17
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:31 +0000

    worst over convinced mainly familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_list(array):
+        return list(array)
+def add_numbers(x, y):
+        return x + y
