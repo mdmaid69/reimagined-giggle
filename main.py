9f9f9926092a589cdc549cd33cacddd5cbe4e996
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:12 +0000

    taste takeoff surface plant round

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def subtract_numbers(x, y):
-        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
