b4ac027f3550ad5103ddca5d3a6bc97ca076407b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:02 +0000

    draw refuse especially speed lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
