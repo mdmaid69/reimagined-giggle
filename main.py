950e5d9e78895635cf8b40faa877d4118e3f9c76
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:20 +0000

    declaration same feed black easy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
