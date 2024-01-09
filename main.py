8c40a9d4acf9bdd81539b5995ac1fd70f4eda370
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:38 +0000

    ashamed unpleasant demand will cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_floor(x):
+        return math.floor(x)
