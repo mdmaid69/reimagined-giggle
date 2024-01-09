0a10eb2f97aaf14271d20953db88e59c5157deab
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:58 +0000

    threaten cope life concerned mystery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
