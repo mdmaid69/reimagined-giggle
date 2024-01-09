d2d0e9e2f465fe8b2e1c32eb7cefd1497248beff
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:09 +0000

    ignore peace heat healthy discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
