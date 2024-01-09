808b518d7aeeee82ebfc67c1f6b186c9ca695fb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:24 +0000

    situation this wing label Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
