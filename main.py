3208a0d44d09c62218631eaef3620b2f31afdde9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:59 +0000

    tooth share later hers worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def subtract_numbers(x, y):
+        return x - y
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
