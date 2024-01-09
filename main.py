d775f3239931e1ecb6f21f2f2a8b7a728b18366b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:04:25 +0000

    enthusiastic secret confused unhappy practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(5):
+        print(i)
