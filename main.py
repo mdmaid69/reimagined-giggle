3db55c82f695f1acc668b744b401593db8c096a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:21 +0000

    boat please heavy whole Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
