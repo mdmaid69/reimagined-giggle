80e770c40f26665911fe3c7f2b3a9e019037bef7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:53 +0000

    hurt wake callat business wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_max(numbers):
+        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
