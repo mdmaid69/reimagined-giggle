fba0fddec2c5346b75227918fe6fb205ef0c7079
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:50 +0000

    sign fly considering greeting sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
