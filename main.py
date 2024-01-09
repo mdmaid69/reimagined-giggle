2bbe3e6ffb28ba21bddab2645baea104427b3fb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:04 +0000

    detail possible save detail recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
