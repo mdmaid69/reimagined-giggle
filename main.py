7f78ad99c1025cfe0f6f44b39cb529cd9ab56bd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:04 +0000

    exciting build neat like circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
