83feb5c38346373c1fa2d38ebbf5ad7ef2cab9bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:26 +0000

    snow distance criticism rush ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
