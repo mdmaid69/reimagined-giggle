efa28ad9d078c045658ab96dac1eae07a613dc5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:18 +0000

    fire pipe swim save clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def sort_list(lst):
+        return sorted(lst)
