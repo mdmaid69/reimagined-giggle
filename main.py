9ffc51b568db7a2d33739e6f647aa3dadb0344aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:57 +0000

    profit walk care file write

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def remove_duplicates(lst):
-        return list(set(lst))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
