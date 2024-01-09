6115cb73f45ec0ddb46fff77827a390a71c8946b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:12:51 +0000

    uncertain sentence disturb mother catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
