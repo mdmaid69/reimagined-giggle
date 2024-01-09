ea6f80d0a258395c4f6ff184f25eaa61d42d5d84
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:08 +0000

    among intend evening thinkthrough term

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
