5859d898faf98a6ae940653146ed60bd04735bca
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:34 +0000

    confuse keepoff approve account let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
