eeef2e0fee056280e4427c4831975329459bd88f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:21 +0000

    letter on male success tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
