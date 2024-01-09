7bfdbfb4a5263d3bcc27d4aaa62735508a900680
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:50 +0000

    door church progress knock book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+for i in range(5):
+        print(i)
