198772019925ec574f3ac26f4e3e7fce38d4e36c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:21 +0000

    hardly cruel pump manner burn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def subtract_numbers(x, y):
+        return x - y
