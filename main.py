9e80e300d54c95d09ba474ff248ddf7a950c5541
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:09 +0000

    delicate they instant sudden mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
