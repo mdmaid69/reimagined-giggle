ae3937598dedb60f258e5c561b00a4c6c02ecec4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:28:15 +0000

    common heat feel disapprove pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
