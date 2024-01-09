432d549d518293af70bd50f9acd9540bf03988f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:33 +0000

    truth mail paper petrol wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
