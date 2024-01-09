aa3de520f4d639e70b6ec9f12e77b0a8be395c22
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:16 +0000

    last post purpose cross different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import platform
+def get_python_version():
+        return platform.python_version()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
