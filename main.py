260fba6e69eb6d4dff331b58a0dbadc32dc971ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:21:39 +0000

    Itself that need change attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def add_numbers(x, y):
+        return x + y
