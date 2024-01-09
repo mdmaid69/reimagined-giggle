59b0001d06a31bf9a7b77537238997eeaa2bad6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:02 +0000

    hardly keen lost walk respect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_sine(x):
+        return math.sin(x)
