5572897b96542b190e76069f808f1c1bdef07a8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:44 +0000

    rarely exercise rely judge lay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def clear_array(array):
-        array *= 0
+def reverse_array(array):
+        array.reverse()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
