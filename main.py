4a232ac648e33a31a09a2ab90b9b455ba3449367
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:31 +0000

    during spring onto low form

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
