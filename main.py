6cb7f85802dd84db4400e0cb1f57b1a4e294c674
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:50 +0000

    why relief delicate thirsty determined

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
