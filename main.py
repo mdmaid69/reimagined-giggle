3440a30cd86a0fa5597e6e343428c19302f14282
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:25 +0000

    I drive careful fly conclusion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
