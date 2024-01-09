7b389891566317729ee17a34e7584a4ac3ba6a21
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:05:24 +0000

    exploration promise spring allow now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_int(array):
+        return int(array[0])
