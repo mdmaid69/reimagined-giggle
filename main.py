5e9efe75c010dde18d9b658980976cfc01adf186
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:55 +0000

    rich game birthday drink exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
