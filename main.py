48b4ded0b6989879b64f181e7c614e9483d38767
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:42 +0000

    noone police unit bush scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def count_words(sentence):
+        return len(sentence.split())
