e0a37155b4a3fe28128ef34329e1a553d3d50d5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:47 +0000

    borrow its afternoon star Kk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
