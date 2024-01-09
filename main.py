29fbf5d82b82a4d04f7ccfbcda6aee3977fefc97
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:34 +0000

    everyone concerned half left recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
