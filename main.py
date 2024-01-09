5d6aef352bc1752c78a2f3bd10df010c5a2015b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:57:06 +0000

    sorry neat exchange phone breakfast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
