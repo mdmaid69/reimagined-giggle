901f0f9ef89563118b0b7823dec26e4b2387dcb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:45 +0000

    rare similar mail squash twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
