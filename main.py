adfa3009b633a94b0884d5c3f44f1790ba058592
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:17:48 +0000

    protest hospital deliberate live pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def divide_numbers(x, y):
+        return x / y
