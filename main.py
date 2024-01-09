79e6e8d054a139ae0957448efabac71e31a8b40f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:11 +0000

    raise sensitive march cry autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import platform
+def get_python_version():
+        return platform.python_version()
