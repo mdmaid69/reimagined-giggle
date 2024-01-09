dd2e736a5e3d254168033081e780936a03ed3d0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:53 +0000

    crowded release suffer lock part

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
