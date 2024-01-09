460b9b5de111ca3d7a399adad48b2ddb0b68bd04
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:44 +0000

    almost loan record however worst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_pressure(force, area):
+        return force / area
+import numpy as np
+print(np.array([1, 2, 3]))
