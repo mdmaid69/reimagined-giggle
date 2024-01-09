73354c47ffdf5cfa31da48dfdc164ee5c2666cf6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:57 +0000

    start burst carry method shame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
