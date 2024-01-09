9e740064dbe4cb9fd55842099f1679e01071fe11
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:52 +0000

    interfere hardly properly board centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
