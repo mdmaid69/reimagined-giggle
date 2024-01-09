5edcc5d0a1b22acc871a59d2472e7e617cf9690f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:40 +0000

    transport moon blame movement whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
