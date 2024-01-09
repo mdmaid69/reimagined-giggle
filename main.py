8b0693e312ab6245035284311382425b37640dbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:11:29 +0000

    Ss shock party here scrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
