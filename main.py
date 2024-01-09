6cf121752755d42d91741425f9ecf1cb7e4bb31b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:54 +0000

    themselves happen large watch usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
