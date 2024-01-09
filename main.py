165f442b8275b7c784a36903f4933c032bf99025
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:05 +0000

    coffee sense against recovery free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
