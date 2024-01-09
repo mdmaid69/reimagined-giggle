e715a1a4ba0221ae963654db3d7149a4a5220ccf
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:38 +0000

    week guilty machine deaf fall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
