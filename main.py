9372d235e8efa893100794e47209ed44f02092bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:01 +0000

    touch improvement leg splash OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
