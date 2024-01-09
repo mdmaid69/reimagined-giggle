ceda4a44e5a417cded9640f337d379fc3186f39c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:18 +0000

    cry street tap meeting remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
