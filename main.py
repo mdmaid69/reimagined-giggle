b1f4d3e5eb3cf1844082f58d56ef3b671ee078b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:48 +0000

    hollow survive means between crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
