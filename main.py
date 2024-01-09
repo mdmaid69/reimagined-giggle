bb8d4775bc1c36f7a912ea20f668c6348b5b29ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:31 +0000

    waste charge wealth hang material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
