4cf7abf856bb4d1186e2d93ee11249ded819f14a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:50 +0000

    sale reaction grow turnover dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
