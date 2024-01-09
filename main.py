301ac195bacbf04334a523ed62a6d19f306cbbe8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:45 +0000

    charge project immediate false round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def exit_program():
-        sys.exit()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
