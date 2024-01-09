6f5b39d99c632109f0f378f587e28416e59634aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:36 +0000

    appeal Ll mother different window

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def set_array_item(array, i, item):
+        array[i] = item
+import getpass
+def get_username():
+        return getpass.getuser()
