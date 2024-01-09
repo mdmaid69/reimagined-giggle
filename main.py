f0930ca071717e19c36d911775410880c577105b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:26 +0000

    trousers snow direction lead respect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
