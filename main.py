137a3e1bb3da21690b10f3553ef63d687ae0c744
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:16 +0000

    plan develop substance gift hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
