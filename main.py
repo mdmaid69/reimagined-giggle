0ea5833f4e301c41bf7b1fddcd358cec7c23e84f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:31:29 +0000

    answer tidy relation side test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
