c505a1d2f10f2c5e269ffed96fa2950977203a03
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:09 +0000

    development voice why shoulder giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
