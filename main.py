7875a7b3aa60bf60087b2b8bf215b90bc1f4208a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:39 +0000

    finish interview range foot impossible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def remove_duplicates(lst):
+        return list(set(lst))
