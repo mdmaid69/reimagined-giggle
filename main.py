6e31ae614cfbc4cb4f460c8dc5c49ec12ca41671
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:07 +0000

    behind official nigga sensible recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
