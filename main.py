c6aec6dca6af92f028da1d6ca9f88b2af5004a57
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:54 +0000

    information television house mention chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
