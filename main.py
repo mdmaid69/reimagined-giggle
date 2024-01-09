fc9372093941c121f664a219ac1dc6c108c8394c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:57 +0000

    right spite price slide goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
