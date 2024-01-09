c68a75ca77cf4d43b1fd6647ff11efbeeae83aa9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:49 +0000

    plastic justice common all familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
