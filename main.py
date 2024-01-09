b063ce31931a3489985c6629c3bc66a20d22f5ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:14 +0000

    judge only school on custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
