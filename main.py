34570a4e87fc2e422c45c4f708a11d62ec5c4c6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:34 +0000

    rent provide lot by closely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
