a3bdc1e7c6f2b512a973e3e3afe36d3d8057ceec
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:38 +0000

    attempt cinema message sad possession

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_binary(n):
+        return bin(n)
