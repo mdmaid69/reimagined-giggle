e8264be7e8e9dccf02a82708c6ea765b7f1333d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:05 +0000

    activity lunch punish weekend considering

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
