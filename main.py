04ac94dbdf62c333a0f5dc52e872054d9ffd88ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:57 +0000

    name hurt frighten bad rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
