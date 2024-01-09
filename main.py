0f31d8b32c15d7a2a15cc6bad2b383896f06fd09
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:29 +0000

    action furniture the deliberately prize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
