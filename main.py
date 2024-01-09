a9327867b7dab33e1862775a61ca54036c48f738
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:30 +0000

    cold long top act entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
