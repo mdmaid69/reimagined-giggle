91e2d55df5b8be56d13a981a7694a7ef44d79dda
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:53 +0000

    convincing spray greeting custom react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
