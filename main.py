c85a6c5e56e4da3e4b88d0911ed2045c0c0533f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:48:24 +0000

    sport age back glad leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
