30fef23ceefe596050eae8b1b063d265498fa0b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:47 +0000

    fall bit blow practise promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
