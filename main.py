60bb77848af6c9a52ac4325b00c1a2a571a51bed
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:36 +0000

    slightly upstairs picture very likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
