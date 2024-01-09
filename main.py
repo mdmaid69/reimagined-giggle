029b8ecf58c7699a6c978cf94bd9396cc99cdee0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:11 +0000

    middle flat worst section disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
