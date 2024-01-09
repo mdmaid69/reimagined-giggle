37b8e5952b625ea89994368e5c9a425cbb38484c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:36 +0000

    story cover sport attempt physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
