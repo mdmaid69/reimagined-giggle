3c13079f33b0f111aceaecf468d80d788ef2ddcc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:18 +0000

    harm threaten respect wake pour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def count_words(sentence):
-        return len(sentence.split())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
