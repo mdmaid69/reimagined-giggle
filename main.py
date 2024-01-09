3025672239ee26f9dbdd51fd8e8f1880131067c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:30 +0000

    study failure squash calm now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_variance(arr):
+        return np.var(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
