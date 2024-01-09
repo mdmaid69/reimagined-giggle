2f5de3bd18b1bbb944279dedbab3fdf296c414f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:46 +0000

    train petrol taste substance support

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
