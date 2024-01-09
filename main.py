ddf32a137dba7dd46a8594e6b8dd39eecf54a359
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:02 +0000

    public cough long towards house

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
