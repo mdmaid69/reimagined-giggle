39e6614c90d318f3ef77b2aa613df20f15526f80
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:11:26 +0000

    tooth plain goout to fetch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
