5333c72c1f79ba48fd96d173776a33825b586e0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:28:09 +0000

    getaway important apart method ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
