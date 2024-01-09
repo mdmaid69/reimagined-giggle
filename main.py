a791f22147efb7e02ef45c7f80e73ef35712af12
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:38:07 +0000

    behind unhappy influence offend remain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+print(datetime.datetime.now())
+  def reverse_list(lst):
+        return lst[::-1]
