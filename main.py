f0ca12fa8fd2f66eadd3c64f2f6c21adaac51483
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:18 +0000

    high fetch take direction maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
