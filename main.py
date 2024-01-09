fa4ccbe14682833b5cf4d8a754bf62803563dfa4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:38:18 +0000

    body petrol task goodbye care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
