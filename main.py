49f4e82d7f3fa8b5100e5742c11513f07db267df
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:58 +0000

    bottom rest shine freeze theirs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(a, b):
+        return a + b
