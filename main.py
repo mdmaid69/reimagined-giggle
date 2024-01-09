ff3f773b77c989ee00fc70a32c5441b98ac7a719
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:58:59 +0000

    foot fire maintain dark lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
