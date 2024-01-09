418fde540c530dbf4280e9582bd44660a471753f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:35:20 +0000

    crowd buy suck deliver level

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
