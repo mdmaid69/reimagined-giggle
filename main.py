37959ada2393dad0d126369d43c2dbace34f1f29
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:41 +0000

    blind cinema demand we generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
-def get_current_time():
-        return time.time()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def reverse_list(lst):
+        return lst[::-1]
