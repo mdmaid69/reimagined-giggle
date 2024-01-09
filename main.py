fe4fc285a8ccf939dd6e8c3f99ee4771d29267f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:07 +0000

    past anxious cry cost hello

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+print(math.pi)
