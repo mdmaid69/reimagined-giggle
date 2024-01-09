ec07a1d0a1bb29181f031d7c2e82a10854c1e2d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:56:15 +0000

    mail afterwards course discuss joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
