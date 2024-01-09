772babdd48b6056c7569da28f5b9056b64d1476d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:24 +0000

    Bb achievement drink plate workon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-for i in range(10): print(i)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
