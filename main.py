b0615b2db3f70ab17531b07c9f6074a5e033f6ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:37 +0000

    occur comment violence best abroad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
