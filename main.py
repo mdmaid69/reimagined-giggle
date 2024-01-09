b99ffe2791d0080bb59103f2c04d0cebf6b5f8d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:42 +0000

    use pump fair sweep answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
