b1de953cee76ff8d7be99f75fa63788af5a25276
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:34 +0000

    score unfair intend serve lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
