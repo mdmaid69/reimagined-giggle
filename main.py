4de673fc406bad73351fedd89320669d128414de
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:41 +0000

    around lesson narrow sleep custom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
