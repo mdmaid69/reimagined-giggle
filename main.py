f1729be1815c7a28cc1dcc5bf7e60bec2fe3f59c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:40 +0000

    tear equal since name child

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
