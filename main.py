02aa309d7356c868d41dc4cd3dbb7c2b99a39800
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:16 +0000

    pool regret exact sense none

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
