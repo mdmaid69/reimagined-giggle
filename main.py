0865b50091318fb83cae7074ca56d6f0d60fe364
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:05 +0000

    discussion contrast harm separate serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
