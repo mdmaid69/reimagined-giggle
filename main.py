c643e3f25385bba34f8b3eb43108932bc15dbe39
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:39 +0000

    fortunate control minority mine ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
