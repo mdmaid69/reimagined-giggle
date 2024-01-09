9081a82a562f97388ac2bfa2da970e687b97d622
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:07:31 +0000

    move realize weigh calmly forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
