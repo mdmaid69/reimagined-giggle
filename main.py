233e49e06ccd837c5ace310902646a85a9628d0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:31 +0000

    whenever affect alone stir complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
