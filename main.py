8be3b3cd15ebbb73a5d6b1e92c958a912b6e55cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:17:14 +0000

    right meal disappear criticism bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
