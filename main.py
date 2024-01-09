e53a8e09ac42b5ca78f4d878df08b136df343bf2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:56 +0000

    open develop inside trap wave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
