e19d62f6e834fd8ef6884f38ac25d1050bb5ec57
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:47 +0000

    today morning keepoff national broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+print(os.getcwd())
