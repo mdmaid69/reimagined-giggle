9fec19eb861ed4ac089bdebbeeb663c08f4f94fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:34:55 +0000

    fit pen broad bread smooth

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
