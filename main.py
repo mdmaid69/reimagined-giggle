b21fa4b2a6c3e343ed34bed0d02ac6160bd87af2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:31 +0000

    relief nature relation tax nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
