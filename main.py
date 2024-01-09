92c2fb847b471abc29d3d7baa8df94c321721d5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:52 +0000

    birth task turn each connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
