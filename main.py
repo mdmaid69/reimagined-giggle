108c666721f206b7d4c9f565699f6ec80d3d0d78
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:12 +0000

    confidence shade pity awake side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-print(sum(range(10)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
