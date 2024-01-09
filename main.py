41fb1d482a239dd7d3fcb7e77a7b5aff3deb34f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:06 +0000

    price life act still frequent

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+print(sum(range(10)))
