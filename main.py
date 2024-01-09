a104211b4e83741b0307878425392ed5859b7c70
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:07 +0000

    exploration lock lend in wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
