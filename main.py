a4224fdef11e8df23c583cad4ad3f37717cac55e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:03 +0000

    instead fond one loud press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_set(array):
+        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
