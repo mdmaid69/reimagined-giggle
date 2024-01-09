685f261962f310fe789ceee35092830c0dfb7d42
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:34 +0000

    turnon authority cruelty food praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
