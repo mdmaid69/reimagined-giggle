71e4ddd5a26e7c0f9fa43656f64b7cb0f4f921c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:30 +0000

    wake under illness shape welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
