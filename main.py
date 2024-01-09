f370603f9ca7fd27ae095c4af72c54641f6d9956
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:18 +0000

    care escape sound present holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
