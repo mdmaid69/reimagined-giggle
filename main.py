f4d13dea991a0ed194a2c9d96e2d864faf5480ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:38 +0000

    open correction clear second pride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
