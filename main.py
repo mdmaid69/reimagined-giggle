dfcda1645018d3bff65eb35dfcebb686e1b27470
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:10 +0000

    experience Rr treat next pin

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
