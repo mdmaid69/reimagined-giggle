a429129db9432f05b54fdefcee74940754e95cff
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:40 +0000

    pleasant advanced everything body gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_min(lst):
+        return min(lst)
