f922bb75b68d7d8fe3f2e5607a5b177492dd5e09
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:11 +0000

    though fold several roll bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
