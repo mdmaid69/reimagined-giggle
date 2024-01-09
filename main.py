2492c2d12a9903a8a6adb54fe956ecf5e9733d4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:33 +0000

    themselves science mistake arrange passage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
