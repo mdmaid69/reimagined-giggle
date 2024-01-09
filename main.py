6c489843abc850a42fce64701b4eb078e5077237
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:40 +0000

    press below break Uu listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
