bc316d6a0c0732ed77216dc36c3353fb79bc1447
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:41 +0000

    place emotion damage illness convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
