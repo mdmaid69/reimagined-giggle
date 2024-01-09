27e3a6bf75058f2db01155175a04e8b99c5cff2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:54 +0000

    turn definite replace rid male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def cube_number(x):
-        return x**3
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
