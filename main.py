adbb5cbb7c9fdc0d07a5ed641c9343ab1185bed7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:37 +0000

    whole path disturbing thin deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
