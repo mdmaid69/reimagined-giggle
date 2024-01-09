7d3a762126fde2eb9087be372cd2ab398e54b7bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:31:08 +0000

    wait lost turn still most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
