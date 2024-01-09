7c6ca723f44ec08fb989dd1698f48fbb493930c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:48 +0000

    hurt opinion pleasant use tour

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
