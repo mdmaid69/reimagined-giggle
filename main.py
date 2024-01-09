8906f8749e3297c4e2bedc56424a1be3090cbf4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:19 +0000

    ignore callback field some influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
