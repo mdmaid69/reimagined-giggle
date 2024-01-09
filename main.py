5d6ca813d5107443acd3c00098b3962d777a79a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:08 +0000

    anywhere limit active exhibition safety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
