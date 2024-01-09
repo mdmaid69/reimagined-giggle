33063434436f4b63c2434de816303cecf8976461
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:01 +0000

    next about ordinary method offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
