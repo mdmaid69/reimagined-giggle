d175ff467618b5370704990caeef066939c433fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:19 +0000

    afterwards show point low basic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import collections
+def create_user_string():
+        return collections.UserString()
