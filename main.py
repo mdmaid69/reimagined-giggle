0c5d17726943c5d57b9d4744a305a6a812c86eb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:12 +0000

    lead clean score welcome beside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
