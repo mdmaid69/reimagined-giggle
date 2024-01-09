df8d95964197b2d4ab115d374aeaae681ca4bfa6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:19 +0000

    excuse material interrupt score scared

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
