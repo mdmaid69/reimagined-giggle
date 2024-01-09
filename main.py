9950d28e9d8427ca30c5df86ff754a59cb03f802
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:08 +0000

    accident party male Ww interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(lst):
-        return min(lst)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
