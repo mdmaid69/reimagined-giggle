3f5c51367fbbeba5b242f17002de2be3b31a47ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:23 +0000

    pass turnon mean large disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
