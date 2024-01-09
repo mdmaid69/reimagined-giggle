5a863592ab9ebc521946d4bbe0e4c3c0c959f18d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:46 +0000

    over beautiful confusing church precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
