4b90b1db999137e9fcc20163dca811fcd5cdd7fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:45 +0000

    vision stay intend perfect ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
