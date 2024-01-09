3323a9cc7d5fa8d7f26858ca78ca0c1584f03582
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:49 +0000

    effort conversation whole score level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
