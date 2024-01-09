1205f9d42ef52f89ee3ab489e9f23f1e65321376
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:07 +0000

    amount dress earn unit personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
