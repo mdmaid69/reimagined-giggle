2a94a6f3168fc000e80b44f843a675b39700740b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:07 +0000

    attention before mend herself refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
