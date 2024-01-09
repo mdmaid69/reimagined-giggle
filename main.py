be1b183ce66f58b5dcf48c206775cedeecf463db
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:03 +0000

    open question reach frighten plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
