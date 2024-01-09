709266fc4a99ebd0c8476fd988c7c37404c58276
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:36 +0000

    Nn moment seem laugh high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
