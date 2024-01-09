99eff0ee3dc49692544ce311c450ea965d1fcd59
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:07 +0000

    cry warning preparation trust rain

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_pressure(force, area):
+        return force / area
+print([x**2 for x in range(10)])
