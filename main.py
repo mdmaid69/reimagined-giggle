7263a0501e88cf9dab323dca4c67f657fe450ad1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:31 +0000

    hard big birthday basically start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
