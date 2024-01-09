3de511055ca2283245247bbdb02aa9d23d7323b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:00 +0000

    blame wide beautiful lower near

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
