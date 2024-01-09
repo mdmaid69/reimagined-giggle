ff4f2c0f5d2f0453059a5875b663ad6055c73876
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:41:34 +0000

    rush whom workon send approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
