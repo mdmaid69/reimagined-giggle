5002e5e7c13854a58a44c31d5d61f34183b87005
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:18 +0000

    copy tip authority callout organization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
