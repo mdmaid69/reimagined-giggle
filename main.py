7d67e1fb8650580c297c14866eaeec0e049c3aad
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:29 +0000

    characteristic gentle sorry achievement power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
