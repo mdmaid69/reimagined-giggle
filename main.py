41ebcdaf67fb52c4b1762b1cc484ca04041bd82a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:20 +0000

    over meaning turnout deep getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
