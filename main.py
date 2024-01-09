3644cc1130247ab8466f1ef2321d2cd4419a58e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:17 +0000

    cause fond part scale question

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
