9999e04a5c3aa9144454f279da46d4c44d25e907
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:39 +0000

    congratulations tidy broken example suck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
