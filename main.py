ccf6c138092ca936a52170d9c345c2e27e291d60
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:56 +0000

    apart odd secret violence inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
