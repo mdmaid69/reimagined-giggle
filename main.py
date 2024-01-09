ffa86d83e5a15d9264f5e01d13d35a48719c1cf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:25 +0000

    check at free pressure worried

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
