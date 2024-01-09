4db9a657cea94385e50297cb9e2ed0a4f35e061e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:25 +0000

    call giveaway accept climb eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_current_directory():
+        return os.getcwd()
