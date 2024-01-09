f57f13bcb76d900ee486fd8b3dc7ae3d9baad0fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:12 +0000

    in custom 570enjoyable do glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_int(array):
+        return int(array[0])
