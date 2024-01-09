dff94168e22eb1f98316f3ee7e62a4b193e1c862
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:44 +0000

    design appearance dead belong certain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
