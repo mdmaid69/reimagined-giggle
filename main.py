ef9d3490bec5fc5edcbe65626f59e593a9aae6b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:28 +0000

    defeat bath major from unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_octal(n):
+        return oct(n)
