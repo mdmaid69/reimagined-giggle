84e553fdc21e1c5db4f362be308b3981ae4ce49a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:22 +0000

    suckme around shine get safe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
