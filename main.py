a0eb593ae36defb848988f5086ae39cbc43e0dea
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:49 +0000

    blow crash suit set perhaps

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
