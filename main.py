87279a997584e94c71b79378a4f95689dc829d95
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:14 +0000

    violent comfort level terrible age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
