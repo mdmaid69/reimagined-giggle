16201a3f1d744a9908d56077029de68652ec20dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:09 +0000

    shame drop criticize actual sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
