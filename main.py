b8d0dcadf55e4c3d640f3f7f2f17e36a3a37df47
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:59 +0000

    free can block path steal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
