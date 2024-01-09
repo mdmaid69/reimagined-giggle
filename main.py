e535326e2ffa0b12715c922ffc218dea3ab28aaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:07 +0000

    above develop refuse declaration takeaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
