48c8ec069af39672149b782140a6d91d8ebc1b22
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:13 +0000

    he book by shame cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
