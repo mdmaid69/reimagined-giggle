c1434c583825f551268428417868551cb33f93bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:03 +0000

    discover prove narrow declaration month

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
