9d5461ab506cdc293f5019fd122a1f001696dbbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:27 +0000

    invitation change now help used

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
