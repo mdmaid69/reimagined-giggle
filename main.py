cbf95111187288bc542df976c81ed96268921fcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:02 +0000

    still brother contrast stretch exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
