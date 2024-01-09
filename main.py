e854636481fd11089a6e2aa8c4b0d37c858f66ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:53 +0000

    worth fine chief Ff continue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_binary(n):
+        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
