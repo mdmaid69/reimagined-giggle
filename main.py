620812cf855bc240ebbf76fbe4cff56850035095
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:22 +0000

    sorry out practical finally earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
