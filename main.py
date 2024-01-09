e7ee2b09906e2bc6ade5022b113a48d7e2050e4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:37 +0000

    lay relation change strike operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
