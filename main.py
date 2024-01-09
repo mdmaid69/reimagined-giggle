b9ad6ae8c3c94a4c0b2a0a3aaa60ee1b017600f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:12 +0000

    frighten behaviour tend convincing move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
