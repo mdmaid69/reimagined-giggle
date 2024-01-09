314355a507a53729ca492813458104f0a068e888
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:51 +0000

    importance no waste however see

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
