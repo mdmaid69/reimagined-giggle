039b39bb9475d5ec665f33fb95a69d8168f4a7a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:20:50 +0000

    problem independent prefer steam hit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        return sys.version
