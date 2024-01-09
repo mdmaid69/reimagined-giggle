d73b74413baf7f51e501c3d74b2e028afea7d359
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:25 +0000

    common continue insist fortunate birthday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
