406a159d22f63a8488ce2368b0f8817cc3f23f0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:07 +0000

    565end burn into calloff active

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
