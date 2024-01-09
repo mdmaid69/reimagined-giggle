30d486115b2680b9b7a467878f13bd703dce5604
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:18 +0000

    muscle throw quality punish mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
