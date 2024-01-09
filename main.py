3293fe44d6dd18a6a4eb7dcc704909927c6b4300
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:21 +0000

    violent although other drop medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
