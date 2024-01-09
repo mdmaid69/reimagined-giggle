a360e40719be8ae547a4a480ed9d0e3599a53ca0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:19 +0000

    sing finish bit tidy steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
