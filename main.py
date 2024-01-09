20abfe8b1e389cc929d63190e75826e4ce04084e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:23 +0000

    another exciting shoot later fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
