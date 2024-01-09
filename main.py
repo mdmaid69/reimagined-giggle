d6de8bb4cba8be2014d66a56a3cfd8552010b575
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:14 +0000

    step trap count please mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
