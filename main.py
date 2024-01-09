1521c6d0aae254eeeec388aa670db2edf5b3ddd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:54 +0000

    rich smile amount probably family

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
