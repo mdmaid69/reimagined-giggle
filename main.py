f32d7ae6c0caae7043049fb5f23a05582b9520f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:16 +0000

    kind majority lay tragedy failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_floor(x):
+        return math.floor(x)
