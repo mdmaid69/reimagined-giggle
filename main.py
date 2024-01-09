dff86a41e727d34c48123496c89e35ee9d969f65
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:50 +0000

    shine family bush politics measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
