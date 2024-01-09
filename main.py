1eb9a2a23d565dde55a430585e2387dd6d4562e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:05 +0000

    callout container sink progress enthusiastic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
