61063cee48d927f1478544a045812d4c37b8a08c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:03:14 +0000

    murder gift tap message lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
