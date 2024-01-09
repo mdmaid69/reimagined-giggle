6076d67721baff3b72e85854d27be394df7cb2f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:00 +0000

    pollution quick borrow appeal determined

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_even(n):
-        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
