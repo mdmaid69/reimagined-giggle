83a356e2807ab4d489e69e5031a37c605c6feecb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:06 +0000

    flavour means hang damage task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
