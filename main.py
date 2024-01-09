0f832040ba5400f87b75ba1e5f4529959d5693c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:19 +0000

    apply risk software rubbish arrange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
