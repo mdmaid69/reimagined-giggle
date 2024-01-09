7abfc2c4052f27d06159ff3d1cd960cb98760d7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:26:46 +0000

    abroad division Kk stop past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def reverse_list(lst):
+        return lst[::-1]
