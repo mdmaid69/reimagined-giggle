5231b351202eae745900458b0c39f8f5274405a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:33 +0000

    female impression ago fault dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+print(re.match("h.*o", "hello world"))
