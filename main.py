623ec1ceaa696344d81404f49287a2a56c69e1c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:59 +0000

    bread very prevent another false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
