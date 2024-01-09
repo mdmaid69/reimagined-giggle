a36590e1d55966879b1a4bd835590b1d395f5823
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:48 +0000

    add morning advice switch waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
