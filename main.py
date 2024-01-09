eb7b24b4fa83dec6e73ab147d93138700e1a4624
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:00 +0000

    strike relative pair my close

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
