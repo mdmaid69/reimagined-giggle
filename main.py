6e708383ac4fc9d37375a8cb65a9b169cc34452f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:33 +0000

    while mystery right cool apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
