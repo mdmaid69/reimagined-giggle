479c561d3ce40f8c8fadf888b091443b2d69d040
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:27 +0000

    sensible comeover concern their form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-print(os.getcwd())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_power(work, time):
+        return work / time
