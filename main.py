53ca2fa24b7e89377a82f9e6d2441151a4c94899
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:00 +0000

    cover takeout stick giveaway case

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
+for i in range(10): print(i)
