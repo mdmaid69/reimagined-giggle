931b148908b092dbdda2603698610a4f804aad93
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:39:28 +0000

    say past shout mother stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
