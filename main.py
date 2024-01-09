91a5e80de38197728b53fcaccdabc9e422298079
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:26 +0000

    risk to naturally history advise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
