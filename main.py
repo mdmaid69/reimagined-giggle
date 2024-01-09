a2c313336453281efaddbc4415afe59812fcaf20
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:37 +0000

    knock air thumb object fold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
