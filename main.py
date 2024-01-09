80f803fe12fb844d8b82cee94fe8b621067c98b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:28 +0000

    cause estimate else confident ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
