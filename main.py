b1cb7052659575fc8454200ac52d9f9d89e4c43d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:00:22 +0000

    within rub legal count sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
