516e27211311dd7d59ed2f63e28534115e0c7917
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:57 +0000

    thick bound pour argue paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
