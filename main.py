186435ea8b8aee44648c5a0a7285f525f94c2fc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:25 +0000

    safety Gg Tt challenge duty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+import os
+print(os.getcwd())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
