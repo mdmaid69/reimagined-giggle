78667a93b1d5e60c269e05177ba6390284a03dc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:38 +0000

    life thing yours pencil direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_counter():
-        return collections.Counter()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
