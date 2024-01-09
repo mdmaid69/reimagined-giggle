d4773a261c2a73b50b8471dd8a488d2b4b7d5a12
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:14:09 +0000

    shut occasional independent rule content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
