c6dfd84123c76687a12fa0c9f04d7d19d4686d80
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:18:11 +0000

    art lookaround place production conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
