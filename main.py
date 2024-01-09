7e00258ea8f0783074428b472ddb0abfabb97952
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:57 +0000

    block exit minority later search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
