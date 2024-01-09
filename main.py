9fb683cb776d4f91e114306e8876f7c1e8050a15
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:51 +0000

    deal extraordinary connect estimate drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
