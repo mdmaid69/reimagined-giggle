ce7d7b50453cfd96936b191ec537405775f49c24
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:30 +0000

    how respect takeover you apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
