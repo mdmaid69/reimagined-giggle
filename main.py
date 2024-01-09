bb3ba1c2ed12c5640a80d40a1e1d7947a4e7e7ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:46 +0000

    question punish lift well wipe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
