79a542f002f2d2a549e9d96b5ebc15cc48f70944
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:04 +0000

    cut busy morning Qq later

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
