8999b70e492c534439b4148fe628cfb18d4a6679
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:28 +0000

    confident callback cost nearly but

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_even(n):
-        return n % 2 == 0
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
