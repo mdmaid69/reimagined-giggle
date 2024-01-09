ae05c15af0040ba7f3599d08b78f1af4d0099c42
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:06 +0000

    ahead machine sit time score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
