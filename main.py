d4f72237ee853e88206661ecdec084f96c2be110
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:27:32 +0000

    process to challenge train listen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_int(array):
+        return int(array[0])
