6e68a423f34401b376a75ea83173881645cdeac8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:40 +0000

    awake no patient view thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_power(work, time):
-        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
