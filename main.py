74c37b0ce344532858819bae4e5c0f1b79098aa1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:17 +0000

    frequent work lookafter luck fall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
