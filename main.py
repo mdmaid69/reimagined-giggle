9f2ecc7ae50d9a650e57743e519e8e061afac76e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:26 +0000

    pleasant help sleep list whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def is_odd(n):
+        return n % 2 != 0
