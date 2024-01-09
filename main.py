a07bc1edd622e7e6425c981a20f1f847228bad4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:52 +0000

    round without building cost any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
