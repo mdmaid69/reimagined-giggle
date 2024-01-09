eabc3268f057130133972290de925e9f07e5c3ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:20 +0000

    buy getoff Cc limit track

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
