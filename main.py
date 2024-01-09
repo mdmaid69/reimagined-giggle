df04630d6e91bc7a6418ed823af942abaa9fbc3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:53 +0000

    like paper what what yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
