581a93cf171245152ddf807a224dc2cb1301bd87
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:43 +0000

    bunch future hit careless allow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
