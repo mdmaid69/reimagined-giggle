7fa26ec3ac6ff4105199b2910ca35d5740073cef
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:54 +0000

    holdon less sky maybe cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
