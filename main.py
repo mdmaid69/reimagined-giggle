e989127c53b07cbafb6afeaa26b323c52ff0318b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:33 +0000

    fault go lie takeback by

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
