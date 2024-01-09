6036d57b5c2133a8a3dae14dc7797e9506345361
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:22 +0000

    female worst spray claim when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
