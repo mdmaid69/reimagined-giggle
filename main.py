0c61f8109ce2627dad88628a8eab82205a7e81d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:38 +0000

    rare share bet goup hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
