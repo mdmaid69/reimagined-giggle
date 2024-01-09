2a61eb33efeb7e260f72a3574f03a21cc18d39af
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:21 +0000

    area really waste activity seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
