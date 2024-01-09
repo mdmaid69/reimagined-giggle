25a0e775334eebf6a558c056cc3eb054551d6e70
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:46 +0000

    minimum properly stone dish shape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
