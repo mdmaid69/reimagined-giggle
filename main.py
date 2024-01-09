28b3103b35087ea62283def13b6a55d57548197d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:26:02 +0000

    petrol peaceful tour read contact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
