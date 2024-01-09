66d513cf6423995d2ada84f0b0237ebfc67fd17c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:33 +0000

    change bank tooth classroom centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
