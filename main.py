9006a61ca187f1482bd2710fac32f1e88c3c1ac3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:40:02 +0000

    much hardly join fork under

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
 import array
 def get_array_from_file(filename, typecode):
         a = array.array(typecode)
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
