a1da67fb87ff5cd4751635fe633d37b1bb1835ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:21 +0000

    basis model professional friendly complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
