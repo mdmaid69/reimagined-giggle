6fd50d4b48a26c0f4fcf8588b1a1bc07404657b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:30 +0000

    explanation complete show mine sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
