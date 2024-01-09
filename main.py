939a2309f6995d1ad16f7cb934b766111d53109c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:58:04 +0000

    point emotion abroad comeover from

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
