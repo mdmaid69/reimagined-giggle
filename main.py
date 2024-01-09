8420105f16145c004b27594c773c3d3362434cf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:34 +0000

    rate lock unpleasant stupid stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
