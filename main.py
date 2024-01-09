acf12c709f56a24de4972046d817b75940f39556
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:38 +0000

    command that day war thanks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
