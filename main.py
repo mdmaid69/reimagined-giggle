9991b16a2bad4d2124febe254e5fda39288d5d34
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:18 +0000

    back takeoff poor keepfrom fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
