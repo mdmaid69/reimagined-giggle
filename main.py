bf15e9567f73d673cc56cf4b7558dd90be6d3e86
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:52 +0000

    represent memory clear extra appear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
