4deb50df5091b72a9bfbdb66314fd5f5424a9a3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:57 +0000

    embarrassing underneath today step stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
