02d1c97e30ac2c4cc4181a2e2d6be30608d2ebc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:30 +0000

    load prove difficult and Oo

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_counter():
+        return collections.Counter()
