4b8e50f4da0dc905c320eee284357f1961bf9212
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:58 +0000

    risk check favour touch tired

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_palindrome(s):
-        return s == s[::-1]
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
