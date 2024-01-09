8dcca20a868262e37838c749cb40a222cbcf00b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:54 +0000

    open flat decide correction dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-for i in range(5):
-        print(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
