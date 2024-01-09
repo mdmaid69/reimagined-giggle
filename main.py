7c1cdb985e50c4ef4086b21260c0b4dc8c921e66
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:05 +0000

    main whom experience spread instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
