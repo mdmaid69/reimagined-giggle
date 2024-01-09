227a0f902d9b3dad0ae96f63dc279fbbd5f18262
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:21 +0000

    debt want other like prize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
