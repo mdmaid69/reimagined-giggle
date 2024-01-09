5feaac0a38af7f600556fe32b9d4397f22051161
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:58 +0000

    hers judge pull stretch cope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
