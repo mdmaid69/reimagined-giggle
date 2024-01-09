a4b6b503b5c50cd61fb2213740ec2aa4ef36a95a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:16 +0000

    abroad attract stop blame part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-print(re.match("h.*o", "hello world"))
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
