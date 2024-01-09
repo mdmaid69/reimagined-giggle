b79fd6c858159ffabb073dec74b02e833c6aba89
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:54 +0000

    recover set and petrol market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import collections
+def create_user_list():
+        return collections.UserList()
+  def square_number(x):
+        return x**2
