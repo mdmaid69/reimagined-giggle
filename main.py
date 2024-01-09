edc77fef0ab781ddd78896c377f2cf9343ac9c2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:54 +0000

    post in string service show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
