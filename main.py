c3a436763595e445e6b1ee365bdcb315d6e77347
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:42:13 +0000

    speech regional hard mad mysterious

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
