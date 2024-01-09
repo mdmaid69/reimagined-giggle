ea52973ed1366b39bccf95a915b6c8d6c79cea53
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:05:52 +0000

    introduction plan computer dark will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
