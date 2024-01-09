6354e6078cecab539fe3424a46b941081a9ef691
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:48 +0000

    ticket demand fear pause half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
