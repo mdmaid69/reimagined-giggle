4438ad590f03d9ba245073e40cae2f6daa9dcc30
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:48 +0000

    week protest everywhere takeout unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
