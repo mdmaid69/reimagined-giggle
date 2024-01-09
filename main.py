50f2f594baf81095c4b222291adc37c6142ce276
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:02 +0000

    reach experiment base worry feel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
