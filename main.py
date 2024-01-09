37029801977ab0937546bfbda052f6b6da22a37c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:49 +0000

    university pen plan insist public

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
