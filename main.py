2204c0988cec788a237a314547a44be1ee67ebd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:23 +0000

    guilty tend term throughout book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
