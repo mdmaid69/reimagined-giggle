f9424b757bbf8bfb00a3fc80555e2ecea41eaf57
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:12 +0000

    shoulder slice factory quick otherwise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
