d8e1d75a23f97ee472469660445dceaba7c757b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:53:17 +0000

    likely tough intelligent contribute continue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def remove_duplicates(lst):
+        return list(set(lst))
