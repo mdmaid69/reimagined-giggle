a761fa8b7177075dbb80a9bdf906f35d6fdfd0f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:09 +0000

    calmly breath quickly breakfast cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
