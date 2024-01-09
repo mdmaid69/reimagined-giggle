2fbf942a0e6385a2d330251d02941c0d045c8acb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:29 +0000

    satisfactory there upstairs do boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
