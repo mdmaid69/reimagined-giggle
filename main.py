282bdf9b803945f407df644ba81310b7c2d6e7f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:49 +0000

    drink charge thinkthrough strike except

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
