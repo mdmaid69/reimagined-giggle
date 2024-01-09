a60c5d8aad1f16e9312720454e07e36243439d35
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:18 +0000

    cruel loose coffee community realize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_list(array):
+        return list(array)
