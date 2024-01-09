fcd528c83755d34dd544467d3bfd7f4b426cb1a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:03 +0000

    cover human polite lock thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
