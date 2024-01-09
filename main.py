656e05e98ce9903990acb718afa637da4137ddc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:16 +0000

    computer ticket fortunately sister opportunity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
