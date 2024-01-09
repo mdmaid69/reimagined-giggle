0ef65997fe4965938b6304851f065eb52f17e4b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:33:48 +0000

    unfair behave whistle rush practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
