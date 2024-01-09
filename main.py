899bf8c57356f4c3f07acf78d69ab9955920622c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:41 +0000

    cruelty support keep translate realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
