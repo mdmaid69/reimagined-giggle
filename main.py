3a2dd6390fa2454b998a59f016c3f105f1a8384b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:17 +0000

    further petrol reserve need embarrassed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
