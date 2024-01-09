fc2b041db759a2e932a814fc8959afbfa81bf615
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:42 +0000

    pour tired tense realize apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_contains_item(array, item):
+        return item in array
