e8580d654f41d7f904d0ad7f5f082afd88365073
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:25 +0000

    offer anyone getup eat breakup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_index(array, item):
+        return array.index(item)
