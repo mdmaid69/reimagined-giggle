d111fbcef1b179e2030ed8b61a0d5ef862cd8f53
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:07 +0000

    cum vote because pair cause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
