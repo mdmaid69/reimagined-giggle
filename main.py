e3e724bc29aaeaa591f1b01a378fabce07eafb9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:38 +0000

    design sport write female dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_current_directory():
-        return os.getcwd()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
