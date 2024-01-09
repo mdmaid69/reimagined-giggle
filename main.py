38c4d7212d8940cd76b84dd59e2f4427b0e8e605
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:41 +0000

    Hh upstairs frame pleasure shoot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_array_to_list(array):
+        return array.tolist()
