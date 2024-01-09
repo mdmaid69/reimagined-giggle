ad576f1fa9700a9a82396e82b7b6e13c74631ecb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:21 +0000

    state government bin recommend even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
