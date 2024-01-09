3a51b4a82fa80129e99be28a0f3471a0990f1c26
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:03 +0000

    whichever exactly correct male Hh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def print_python_version():
+        print(sys.version)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
