ddae945253b71cb2b9aeef007f29eeb049fa4436
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:16 +0000

    clear sleep spring own occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
